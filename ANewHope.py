56897ccbee2651240a75c622d57316547c5214b3
Ivan Krechetov <ikr@ikr.li>
2016-06-12 00:05:19 +0200

    Implement next_permutation; remove next_shirt

--- ANewHope.py
+++ ANewHope.py
@@ -2,21 +2,40 @@ class ANewHope:
     def count(self, firstWeek, lastWeek, D):
         return None
 
-
-def next_shirt(prev_week, week_so_far, D):
-    candidates = list(set(prev_week) - set(week_so_far))
-    candidates.sort()
-
-    while candidates:
-        shirt = candidates.pop(0)
-        if is_dry(prev_week, shirt, len(week_so_far) + 1, D):
-            return shirt
-
-    return None
-
-
 def is_dry(prev_week, shirt, week_day, D):
     N = len(prev_week)
     day_worn_prev_week = prev_week.index(shirt) + 1
     drying_days = N - day_worn_prev_week + week_day - 1
     return drying_days >= D
+
+
+def next_permutation(p):
+    k = largest_subordinate_index(p)
+
+    if (k == -1):
+        return None
+
+    l = largest_dominating_index(p, k)
+    buffer = p[:]
+    buffer[k], buffer[l] = buffer[l], buffer[k]
+    tail = buffer[k + 1:]
+    tail.reverse()
+
+    return buffer[:k + 1] + tail
+
+
+def largest_subordinate_index(xs):
+    if len(xs) < 2:
+        return -1
+
+    for i in range(len(xs) - 2, -1, -1):
+        if xs[i] < xs[i + 1]:
+            return i
+
+    return -1
+
+
+def largest_dominating_index(xs, subordinate_index):
+    for i in range(len(xs) - 1, subordinate_index, -1):
+        if xs[subordinate_index] < xs[i]:
+            return i
