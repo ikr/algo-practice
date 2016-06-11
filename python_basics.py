5e0a966533ff72d9258bd6a0305a2924f55638ed
Ivan Krechetov <ikr@ikr.su>
2016-06-11 15:19:34 +0200

    Import only the reduce

--- python_basics.py
+++ python_basics.py
@@ -1,6 +1,6 @@
-import functools
+from functools import reduce
 
 def sum (xs):
-    return functools.reduce(lambda x, y: x + y, xs, 0);
+    return reduce(lambda x, y: x + y, xs, 0);
 
 print(sum((1, 1, 1, 2, 2)))
