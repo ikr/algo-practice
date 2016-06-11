86b4945f16000f98de5f41a173a9b6db755e303e
Ivan Krechetov <ikr@ikr.su>
2016-06-12 00:56:09 +0200

    Constrain next_permutation with dryness

--- ANewHope.py
+++ ANewHope.py
@@ -2,25 +2,25 @@ class ANewHope:
     def count(self, firstWeek, lastWeek, D):
         return None
 
-def is_dry(prev_week, shirt, day_index, D):
-    N = len(prev_week)
-    drying_days = N - 1 - prev_week.index(shirt) + day_index
-    return drying_days >= D
 
-
-def next_permutation(p):
+def next_permutation(p, D):
     k = largest_subordinate_index(p)
 
-    if (k == -1):
+    if k == -1:
         return None
 
     l = largest_dominating_index(p, k)
+
     buffer = p[:]
     buffer[k], buffer[l] = buffer[l], buffer[k]
     tail = buffer[k + 1:]
     tail.reverse()
+    result = buffer[:k + 1] + tail
 
-    return buffer[:k + 1] + tail
+    if is_dry(p, k, l, D) and is_dry(p, l, k, D):
+        return result
+    else:
+        return next_permutation(result, D)
 
 
 def largest_subordinate_index(xs):
@@ -38,3 +38,9 @@ def largest_dominating_index(xs, subordinate_index):
     for i in range(len(xs) - 1, subordinate_index, -1):
         if xs[subordinate_index] < xs[i]:
             return i
+
+
+def is_dry(week, old_index, new_index, D):
+    N = len(week)
+    drying_days = N - 1 - old_index + new_index
+    return drying_days >= D
