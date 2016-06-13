3b3f1d8636377df340ff31a75e7e2d0605e7503e
Ivan Krechetov <ikr@ikr.li>
2016-06-13 21:35:15 +0200

    Preparing to go greedy

--- ANewHope.py
+++ ANewHope.py
@@ -3,44 +3,13 @@ class ANewHope:
         return None
 
 
-def next_permutation(p, D):
-    k = largest_subordinate_index(p)
-
-    if k == -1:
-        return None
-
-    l = largest_dominating_index(p, k)
-
-    buffer = p[:]
-    buffer[k], buffer[l] = buffer[l], buffer[k]
-    tail = buffer[k + 1:]
-    tail.reverse()
-    result = buffer[:k + 1] + tail
-
-    if is_dry(p, k, l, D) and is_dry(p, l, k, D):
-        return result
-    else:
-        return next_permutation(result, D)
-
-
-def largest_subordinate_index(xs):
-    if len(xs) < 2:
-        return -1
-
-    for i in range(len(xs) - 2, -1, -1):
-        if xs[i] < xs[i + 1]:
-            return i
-
-    return -1
-
-
-def largest_dominating_index(xs, subordinate_index):
-    for i in range(len(xs) - 1, subordinate_index, -1):
-        if xs[subordinate_index] < xs[i]:
+def first_wet_index(D, prev_week, week):
+    for i in range(len(prev_week)):
+        if not is_dry(D, prev_week, week, i):
             return i
 
 
-def is_dry(week, old_index, new_index, D):
-    N = len(week)
-    drying_days = N - 1 - old_index + new_index
-    return drying_days >= D
+def is_dry(D, prev_week, week, day_index):
+    shirt = week[day_index]
+    hanging_nights = len(prev_week) - prev_week.index(shirt) + day_index
+    return hanging_nights >= D
