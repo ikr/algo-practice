60cc12a09b87efb7bebe32966ad021f7cb619498
Ivan Krechetov <ikr@ikr.li>
2016-06-14 21:55:55 +0200

    Work on refine_count

--- ANewHope.py
+++ ANewHope.py
@@ -1,12 +1,36 @@
 class ANewHope:
     def count(self, firstWeek, lastWeek, D):
-        return None
+        return refine_count(D, firstWeek, lastWeek, 1 if firstWeek == lastWeek else 2)
+
+
+def refine_count(D, prev_week, week, current_count):
+    if prev_week == week:
+        return current_count
+
+    middle_week = week[:]
+    swaps_count = 0
+
+    while True:
+        first_wet = first_wet_index(D, prev_week, middle_week)
+
+        if first_wet == -1:
+            return refine_count(D, middle_week, week, current_count + 1)
+
+        dryest = dryest_index(D, prev_week, middle_week, swaps_count)
+        middle_week[first_wet], middle_week[dryest] = middle_week[dryest], middle_week[first_wet]
+        swaps_count += 1
+
+
+def dryest_index(D, prev_week, week, starting_from):
+    return week.index(prev_week[starting_from])
 
 
 def first_wet_index(D, prev_week, week):
     for i in range(len(prev_week)):
         if not is_dry(D, prev_week, week, i):
             return i
+
+    return -1
 
 
 def is_dry(D, prev_week, week, day_index):
