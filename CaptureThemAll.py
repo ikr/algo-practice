ae7bcc0862b9bf680cca296abc1e8f4a534e4cb4
Ivan Krechetov <ikr@ikr.su>
2016-06-28 17:49:42 +0200

    Add coords()

--- CaptureThemAll.py
+++ CaptureThemAll.py
@@ -2,5 +2,13 @@ class CaptureThemAll:
     def fastKnight(self, knight, rook, queen):
         return -1
 
+
+def coords(position):
+    return ord(position[0]) - ord('a'), int(position[1]) - 1
+
+
 cta = CaptureThemAll()
 print(cta.fastKnight('a1', 'b3', 'c5'))
+
+print(coords('a1'))
+print(coords('h8'))
