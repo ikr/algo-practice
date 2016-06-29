561f411d2d5e6a6fe2d54e44b5808de5095b38cb
Ivan Krechetov <ikr@ikr.su>
2016-06-29 19:12:04 +0200

    Start implementing the BFS

--- CaptureThemAll.py
+++ CaptureThemAll.py
@@ -1,10 +1,19 @@
+from collections import deque
+
+
 class CaptureThemAll:
     def fastKnight(self, knight, rook, queen):
+        G = {position_to_coord(knight): vertex(0, None)}
+        Q = deque([position_to_coord(knight)])
+
+        while len(Q) > 0:
+            current = Q.popleft()
+            for a in adjacent(current):
+                if not a in G:
+                    G[a] = vertex(G[current]['d'] + 1, current)
+                    Q.append(a)
+
         return -1
-
-
-def position_to_coord(position):
-    return ord(position[0]) - ord('a'), int(position[1]) - 1
 
 
 def adjacent(coord):
@@ -23,9 +32,14 @@ def adjacent(coord):
 def adjacent_deltas():
     return ((-2, -1), (-2, 1), (2, -1), (2, 1), (-1, -2), (1, -2), (-1, 2), (1, 2))
 
+
+def position_to_coord(position):
+    return ord(position[0]) - ord('a'), int(position[1]) - 1
+
+
+def vertex(distance, parent):
+    return {'d': distance, 'p': parent}
+
+
 cta = CaptureThemAll()
 print(cta.fastKnight('a1', 'b3', 'c5'))
-
-print(adjacent((0, 0)))
-print(adjacent((3, 3)))
-print(adjacent((7, 4)))
