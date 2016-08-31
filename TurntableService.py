4e51ce4603d455be515fe7e021a4972c2db6fec4
Ivan Krechetov <ikr@ikr.su>
2016-08-31 18:38:48 +0200

    Outline the fully_serving_vertex_sequences

--- TurntableService.py
+++ TurntableService.py
@@ -1,6 +1,29 @@
+from collections import deque
+
+
 class TurntableService:
     def calculateTime(favorites):
         return -1
+
+
+def fully_serving_vertex_sequences(favorites_lists):
+    parents = {}
+    visited = set()
+
+    initial_range = tuple(range(len(favorites_lists)))
+    q = deque((vertex(initial_range, False, initial_range),))
+
+    while len(q) > 0:
+        current_vertex = q.popleft()
+
+        for a_vertex in adjacent(current_vertex):
+            if not a_vertex in visited:
+                parents[a_vertex] = current_vertex
+
+                if all_served(a_vertex):
+                    yield sequentialize_children(a_vertex, parents)
+
+                q.append(a_vertex)
 
 
 def vertex(ttable, do_serve, hungry_indices):
