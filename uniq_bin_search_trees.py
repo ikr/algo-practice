73f04060b38f3eb93ec8943380d96aaa7dc14b0a
Ivan Krechetov <ikr@ikr.li>
2018-04-19 13:14:57 +0200

    Refine

--- uniq_bin_search_trees.py
+++ uniq_bin_search_trees.py
@@ -10,28 +10,54 @@ class Solution:
     # @return a list of TreeNode
     def generateTrees(self, A):
         result = []
-        self.recur(None, list(range(1, A + 1)), [], result)
+        self.recur(None, None, list(range(1, A + 1)), [], result)
+        print
         return result
 
-    def recur(self, root, xs_l, xs_r, memo):
+    def recur(self, root, n, xs_l, xs_r, memo):
+        print(root.val if root else '-', xs_l, xs_r, n.val if n else '-')
         if not xs_l and not xs_r:
             memo.append(root)
+            print_trees([root])
             return
         if not root:
             for i in range(len(xs_l)):
-                self.recur(TreeNode(xs_l[i]), xs_l[0:i], xs_l[i + 1:], memo)
+                o = TreeNode(xs_l[i])
+                self.recur(o, o, xs_l[0:i], xs_l[i + 1:], memo)
             return
         if xs_l:
             for i in range(len(xs_l)):
                 l = TreeNode(xs_l[i])
-                root.left = l
-                self.recur(l, xs_l[0:i], xs_l[i + 1:], memo)
+                n.left = l
+                self.recur(root, l, xs_l[0:i], xs_l[i + 1:], memo)
         if xs_r:
             for i in range(len(xs_r)):
                 r = TreeNode(xs_r[i])
-                root.right = r
-                self.recur(r, xs_r[0:i], xs_r[i + 1:], memo)
+                n.right = r
+                self.recur(root, r, xs_r[0:i], xs_r[i + 1:], memo)
 
+
+def traverse(root, memo):
+    if not root:
+        return
+    traverse(root.left, memo)
+    traverse(root.right, memo)
+    memo.append(root.val)
+
+def print_trees(ts):
+    for root in ts:
+        memo = []
+        traverse(root, memo)
+        print(memo)
+
+
+n1 = TreeNode(1)
+n2 = TreeNode(2)
+n3 = TreeNode(3)
+n1.left = n2
+n1.right = n3
 
 s = Solution()
-print(s.generateTrees(3))
+print_trees([n1])
+print
+print_trees(s.generateTrees(3))
