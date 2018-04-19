f37dd20281f5ca3feeec4634ae15c1fc7f7b7b10
Ivan Krechetov <ikr@ikr.su>
2018-04-19 14:03:11 +0200

    Draft the declarative generateTrees()

--- uniq_bin_search_trees.py
+++ uniq_bin_search_trees.py
@@ -10,31 +10,30 @@ class Solution:
     # @return a list of TreeNode
     def generateTrees(self, A):
         result = []
-        self.recur(None, None, list(range(1, A + 1)), [], result)
-        print
+        self.recur(-1, list(range(1, A + 1)), [], [], result)
         return result
 
-    def recur(self, root, n, xs_l, xs_r, memo):
-        print(root.val if root else '-', xs_l, xs_r, n.val if n else '-')
+    def recur(self, n, xs_l, xs_r, desc, memo):
         if not xs_l and not xs_r:
-            memo.append(root)
-            print_trees([root])
+            memo.append(desc)
             return
-        if not root:
+        if not desc:
             for i in range(len(xs_l)):
-                o = TreeNode(xs_l[i])
-                self.recur(o, o, xs_l[0:i], xs_l[i + 1:], memo)
+                o = xs_l[i]
+                self.recur(o, xs_l[0:i], xs_l[i + 1:], [o], memo)
             return
         if xs_l:
             for i in range(len(xs_l)):
-                l = TreeNode(xs_l[i])
-                n.left = l
-                self.recur(root, l, xs_l[0:i], xs_l[i + 1:], memo)
+                l = xs_l[i]
+                desc_prime = list(desc)
+                desc_prime.append((n, 'L', l))
+                self.recur(l, xs_l[0:i], xs_l[i + 1:], desc_prime, memo)
         if xs_r:
             for i in range(len(xs_r)):
-                r = TreeNode(xs_r[i])
-                n.right = r
-                self.recur(root, r, xs_r[0:i], xs_r[i + 1:], memo)
+                r = xs_r[i]
+                desc_prime = list(desc)
+                desc_prime.append((n, 'R', r))
+                self.recur(r, xs_r[0:i], xs_r[i + 1:], desc_prime, memo)
 
     def build_tree(self, nodes_count, desc):
         nodes = map(TreeNode, range(1, nodes_count + 1))
@@ -70,3 +69,8 @@ n2.right = n3
 s = Solution()
 print_trees([n1])
 print_trees([s.build_tree(3, (1, (1, 'R', 2), (2, 'R', 3)))])
+
+print
+
+for d in s.generateTrees(3):
+    print(d)
