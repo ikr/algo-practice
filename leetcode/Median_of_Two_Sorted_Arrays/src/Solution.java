fd374126f4843c555c77037c6170b6c1b418b596
Ivan Krechetov <ikr@ikr.su>
2018-05-06 13:04:02 +0200

    Calculate the median of an array interval

--- leetcode/Median_of_Two_Sorted_Arrays/src/Solution.java
+++ leetcode/Median_of_Two_Sorted_Arrays/src/Solution.java
@@ -1,5 +1,16 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        return -1;
+    public double findMedianSortedArrays(int[] xs, int[] ys) {
+        if (ys.length < xs.length) return findMedianSortedArrays(ys, xs);
+        return recur(xs, 0, xs.length, ys, 0, ys.length);
+    }
+
+    private static double recur(int[] xs, int xl, int xr, int[] ys, int yl, int yr) {
+        return -1.0;
+    }
+
+    public static double median(int[] xs, int l, int r) {
+        if (r - l == 0) return -1.0;
+        if ((r - l) % 2 == 0) return (xs[l + (r - l) / 2 - 1] + xs[l + (r - l) / 2]) / 2.0;
+        return (double)xs[l + (r - l) / 2];
     }
 }
