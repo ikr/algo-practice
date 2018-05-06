221e0f190784adb2edb2bc28060d686f31689b8f
Ivan Krechetov <ikr@ikr.li>
2018-05-06 16:45:48 +0200

    Debug a few edge cases

--- leetcode/Median_of_Two_Sorted_Arrays/src/SolutionTest.java
+++ leetcode/Median_of_Two_Sorted_Arrays/src/SolutionTest.java
@@ -35,28 +35,48 @@ public class SolutionTest {
     }
 
     @org.junit.Test
+    public void xs0ys1() {
+        assertEquals(7.0, s.findMedianSortedArrays(new int[0], new int[] {7}), 0.001);
+    }
+
+    @org.junit.Test
+    public void xs0ys4() {
+        assertEquals(2.5, s.findMedianSortedArrays(new int[0], new int[] {1, 2, 3, 4}), 0.001);
+    }
+
+    @org.junit.Test
+    public void xs1ys4() {
+        assertEquals(3.0, s.findMedianSortedArrays(new int[] {3}, new int[] {1, 2, 4, 5}), 0.001);
+    }
+
+    @org.junit.Test
     public void xs0ys2() {
-        assertEquals(5.0, s.findMedianSortedArrays(new int[0], new int[]{4, 6}), 0.001);
+        assertEquals(5.0, s.findMedianSortedArrays(new int[0], new int[] {4, 6}), 0.001);
+    }
+
+    @org.junit.Test
+    public void xs1ys2() {
+        assertEquals(2.0, s.findMedianSortedArrays(new int[] {2}, new int[] {1, 3}), 0.001);
     }
 
     @org.junit.Test
     public void xs1ys1() {
-        assertEquals(6.5, s.findMedianSortedArrays(new int[] {6}, new int[]{7}), 0.001);
+        assertEquals(6.5, s.findMedianSortedArrays(new int[] {6}, new int[] {7}), 0.001);
     }
 
     @org.junit.Test
     public void xs1ysOdd() {
-        assertEquals(11.0, s.findMedianSortedArrays(new int[] {5}, new int[]{5, 10, 12, 15, 20}), 0.001);
+        assertEquals(11.0, s.findMedianSortedArrays(new int[] {5}, new int[] {5, 10, 12, 15, 20}), 0.001);
     }
 
     @org.junit.Test
     public void xs1ysEven() {
-        assertEquals(10.0, s.findMedianSortedArrays(new int[] {10}, new int[]{5, 10, 12, 15}), 0.001);
+        assertEquals(10.0, s.findMedianSortedArrays(new int[] {10}, new int[] {5, 10, 12, 15}), 0.001);
     }
 
     @org.junit.Test
     public void xs2ys2() {
-        assertEquals(2.5, s.findMedianSortedArrays(new int[] {1, 3}, new int[]{2, 4}), 0.001);
+        assertEquals(2.5, s.findMedianSortedArrays(new int[] {1, 3}, new int[] {2, 4}), 0.001);
     }
 
     @org.junit.Test
@@ -87,6 +107,13 @@ public class SolutionTest {
     public void generalCase2() {
         int[] xs = {1, 2, 4, 6, 19, 23, 48, 48, 50};
         int[] ys = {24, 25, 25, 30, 31, 32, 32, 32, 33};
+        assertEquals(Merge.median(Merge.merge(xs, ys)), s.findMedianSortedArrays(xs, ys), 0.001);
+    }
+
+    @org.junit.Test
+    public void generalCase3() {
+        int[] xs = {1, 4, 5, 6};
+        int[] ys = {2, 3, 7, 8};
         assertEquals(Merge.median(Merge.merge(xs, ys)), s.findMedianSortedArrays(xs, ys), 0.001);
     }
 }
