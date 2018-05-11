ec9142b1c72b0a12c0d14d7096517fb4d9c9cc64
Ivan Krechetov <ikr@ikr.li>
2018-05-11 12:14:19 +0200

    Start with the Unique_Binary_Search_Trees in Java

---
+++ interviewbit/Unique_Binary_Search_Trees/src/Solution.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Solution {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public ArrayList<TreeNode> generateTrees(int a) {
+        return null;
+    }
+}
