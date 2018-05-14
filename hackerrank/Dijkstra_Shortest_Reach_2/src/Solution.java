1c8c2d4df46cd883279656dc790f832205de2b76
Ivan Krechetov <ikr@ikr.su>
2018-05-14 19:49:20 +0200

    Use BufferedReader to speed up the Test Case 7

--- hackerrank/Dijkstra_Shortest_Reach_2/src/Solution.java
+++ hackerrank/Dijkstra_Shortest_Reach_2/src/Solution.java
@@ -1,6 +1,4 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 import java.util.stream.IntStream;
 
@@ -110,72 +108,15 @@ public class Solution {
         return result;
     }
 
-    private static final Scanner scanner = new Scanner("1\n" +
-            "20 54\n" +
-            "1 7 45\n" +
-            "2 14 15\n" +
-            "3 7 29\n" +
-            "4 1 48\n" +
-            "5 1 66\n" +
-            "6 7 17\n" +
-            "7 14 15\n" +
-            "8 14 43\n" +
-            "9 1 27\n" +
-            "10 1 33\n" +
-            "11 14 64\n" +
-            "12 14 27\n" +
-            "13 7 66\n" +
-            "14 7 54\n" +
-            "15 14 56\n" +
-            "16 7 21\n" +
-            "17 1 20\n" +
-            "18 1 34\n" +
-            "19 7 52\n" +
-            "20 14 14\n" +
-            "9 14 9\n" +
-            "15 1 39\n" +
-            "12 1 24\n" +
-            "9 1 16\n" +
-            "1 2 33\n" +
-            "18 1 46\n" +
-            "9 1 28\n" +
-            "15 14 3\n" +
-            "12 1 27\n" +
-            "1 2 5\n" +
-            "15 1 34\n" +
-            "1 2 28\n" +
-            "9 7 16\n" +
-            "3 7 23\n" +
-            "9 7 21\n" +
-            "9 14 19\n" +
-            "3 1 20\n" +
-            "3 1 5\n" +
-            "12 14 19\n" +
-            "3 14 2\n" +
-            "12 1 46\n" +
-            "3 14 5\n" +
-            "9 14 44\n" +
-            "6 14 26\n" +
-            "9 14 16\n" +
-            "9 14 34\n" +
-            "6 7 42\n" +
-            "3 14 27\n" +
-            "1 7 9\n" +
-            "1 7 41\n" +
-            "15 14 19\n" +
-            "12 7 13\n" +
-            "3 7 10\n" +
-            "1 7 2\n" +
-            "17\n");
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("/dev/stdout"));
 
-        int t = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int t = Integer.parseInt(reader.readLine());
 
         for (int tItr = 0; tItr < t; tItr++) {
-            String[] nm = scanner.nextLine().split(" ");
+            String[] nm = reader.readLine().split(" ");
 
             int n = Integer.parseInt(nm[0]);
 
@@ -184,8 +125,7 @@ public class Solution {
             int[][] edges = new int[m][3];
 
             for (int i = 0; i < m; i++) {
-                String[] edgesRowItems = scanner.nextLine().split(" ");
-                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+                String[] edgesRowItems = reader.readLine().split(" ");
 
                 for (int j = 0; j < 3; j++) {
                     int edgesItem = Integer.parseInt(edgesRowItems[j]);
@@ -193,8 +133,7 @@ public class Solution {
                 }
             }
 
-            int s = scanner.nextInt();
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            int s = Integer.parseInt(reader.readLine());
 
             int[] result = shortestReach(n, edges, s);
 
@@ -211,6 +150,6 @@ public class Solution {
 
         bufferedWriter.close();
 
-        scanner.close();
+        reader.close();
     }
 }
