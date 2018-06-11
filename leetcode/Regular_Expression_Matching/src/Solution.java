1ec7b13371db9c987a92090235bb578588c4967e
Ivan Krechetov <ikr@ikr.su>
2018-06-11 19:33:21 +0200

    Prepare the test cases

--- leetcode/Regular_Expression_Matching/src/Solution.java
+++ leetcode/Regular_Expression_Matching/src/Solution.java
@@ -2,4 +2,20 @@ class Solution {
     public boolean isMatch(String s, String p) {
         return false;
     }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        String[][] cases = new String[][]{
+                {"aa", "a"},
+                {"aa", "a*"},
+                {"ab", ".*"},
+                {"aab", "c*a*b"},
+                {"mississippi", "mis*is*p*."}
+        };
+
+        for (String[] c : cases) {
+            System.out.printf("string %s, pattern %s → %b\n", c[0], c[1], s.isMatch(c[0], c[1]));
+        }
+    }
 }
