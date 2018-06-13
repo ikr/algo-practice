3bab51c29f5a8c1cfde6799b3846aa8820aefdaf
Ivan Krechetov <ikr@ikr.su>
2018-06-13 07:37:11 +0200

    Add another test case

--- leetcode/Largest_Rectangle_in_Histogram/src/Solution.java
+++ leetcode/Largest_Rectangle_in_Histogram/src/Solution.java
@@ -1,10 +1,11 @@
 class Solution {
-    public int largestRectangleArea(int[] heights) {
-        return -1;
-    }
-
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.largestRectangleArea(new int[]{2,1,5,6,2,3}));
+        System.out.println(s.largestRectangleArea(new int[]{2, 1, 5, 6, 2, 3})); // 10
+        System.out.println(s.largestRectangleArea(new int[]{6, 2, 5, 4, 5, 1, 6})); // 12
+    }
+
+    public int largestRectangleArea(int[] heights) {
+        return -1;
     }
 }
