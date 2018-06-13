6bc5145e68a462e383cb429d14ab98a865dedbd7
Ivan Krechetov <ikr@ikr.su>
2018-06-13 07:30:41 +0200

    Start with the Largest_Rectangle_in_Histogram

---
+++ leetcode/Largest_Rectangle_in_Histogram/src/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.largestRectangleArea(new int[]{2,1,5,6,2,3}));
+    }
+}
