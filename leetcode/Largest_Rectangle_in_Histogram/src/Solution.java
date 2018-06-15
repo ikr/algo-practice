7e588e901a07fa1d66587fc0dbb05ef7642f4f37
Ivan Krechetov <ikr@ikr.li>
2018-06-15 17:19:43 +0200

    Continue

--- leetcode/Largest_Rectangle_in_Histogram/src/Solution.java
+++ leetcode/Largest_Rectangle_in_Histogram/src/Solution.java
@@ -1,11 +1,37 @@
+import java.util.Stack;
+
 class Solution {
     public static void main(String[] args) {
         Solution s = new Solution();
         System.out.println(s.largestRectangleArea(new int[]{2, 1, 5, 6, 2, 3})); // 10
         System.out.println(s.largestRectangleArea(new int[]{6, 2, 5, 4, 5, 1, 6})); // 12
+        System.out.println(s.largestRectangleArea(new int[]{0, 0, 1, 0, 0, 1, 0})); // 1
+        System.out.println(s.largestRectangleArea(new int[0])); // 0
+        System.out.println(s.largestRectangleArea(new int[]{0})); // 0
+        System.out.println(s.largestRectangleArea(new int[]{2})); // 2
+        System.out.println(s.largestRectangleArea(new int[]{2, 2, 2})); // 6
+        System.out.println(s.largestRectangleArea(new int[]{0, 0, 0})); // 0
     }
 
     public int largestRectangleArea(int[] heights) {
-        return -1;
+        int result = 0;
+        Stack<Integer> limiters = new Stack<>();
+
+        for (int i = 0; i < heights.length; i++) {
+            if (limiters.empty() || limiters.peek() <= heights[i]) {
+                limiters.push(i);
+            } else {
+                while (!limiters.empty() && limiters.peek() > heights[i]) {
+                    limiters.pop();
+                }
+
+                int limiter = limiters.empty() ? i : limiters.pop() + 1;
+                int left = limiters.empty() ? i : limiters.peek() + 1;
+                int area = limiter * (i - left + 1);
+                if (area > result) result = area;
+            }
+        }
+
+        return result;
     }
 }
