e902101340d1c3d4378af5bc22aa60c21500a38c
Ivan Krechetov <ikr@ikr.li>
2018-07-07 23:00:05 +0200

    Solution for 3Sum v1

--- leetcode/3Sum/src/Solution.java
+++ leetcode/3Sum/src/Solution.java
@@ -12,17 +12,30 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Map<Integer, List<Pair>> ips = indexPairsBySum(nums);
         List<List<Integer>> result = new ArrayList<>();
+        HashSet<TriSet> trisets = new HashSet<>();
 
         for (int i = 0; i < nums.length; i++) {
             int key = -nums[i];
             if (ips.containsKey(key)) {
                 for (Pair pair : ips.get(key)) {
-                    if (pair.isDistinct(i)) {
-                        result.add(triplet(nums[pair.a], nums[pair.b], nums[i]));
+                    final List<Integer> t = triplet(nums[pair.a], nums[pair.b], nums[i]);
+                    final TriSet ts = new TriSet(t);
+
+                    if (pair.isDistinct(i) && !trisets.contains(ts)) {
+                        result.add(t);
+                        trisets.add(ts);
                     }
                 }
             }
         }
+
+        Collections.sort(
+            result,
+
+            Comparator.comparingInt((List<Integer> t) -> t.get(0))
+                .thenComparingInt(t -> t.get(1))
+                .thenComparingInt(t -> t.get(2))
+        );
 
         return result;
     }
@@ -47,6 +60,41 @@ class Solution {
         }
     }
 
+    private static class TriSet {
+        final int a;
+        final int b;
+        final int c;
+
+        public TriSet(List<Integer> abc) {
+            a = abc.get(0);
+            b = abc.get(1);
+            c = abc.get(2);
+        }
+
+        @Override public int hashCode() {
+            return (a - 65_000) * (b - 65_000) * (c - 65_000);
+        }
+
+        @Override public boolean equals(Object obj) {
+            TriSet ots = (TriSet) obj;
+            int oa = ots.a;
+            int ob = ots.b;
+            int oc = ots.c;
+            int[] own = new int[] {a, b, c};
+
+            return Arrays.equals(own, new int[] {oa, ob, oc})
+                || Arrays.equals(own, new int[] {oa, oc, ob})
+                || Arrays.equals(own, new int[] {ob, oa, oc})
+                || Arrays.equals(own, new int[] {ob, oc, oa})
+                || Arrays.equals(own, new int[] {oc, ob, oa})
+                || Arrays.equals(own, new int[] {oc, oa, ob});
+        }
+
+        @Override public String toString() {
+            return String.format("{%d, %d, %d}", a, b, c);
+        }
+    }
+
     private static class Pair {
         final int a;
         final int b;
@@ -59,9 +107,15 @@ class Solution {
         boolean isDistinct(int c) {
             return c != a && c != b;
         }
+
+        @Override public String toString() {
+            return String.format("{%d, %d}", a, b);
+        }
     }
 
     private static List<Integer> triplet(int a, int b, int c) {
-        return Arrays.asList(a, b, c);
+        List<Integer> result = Arrays.asList(a, b, c);
+        Collections.sort(result);
+        return result;
     }
 }
