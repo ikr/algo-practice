3883cb2481baff6db1f8f403855eeb084e845247
Ivan Krechetov <ikr@ikr.su>
2019-01-19 18:37:38 +0100

    Implement an optimal O(n^2) solution

--- leetcode/3Sum_Closest/solution.cc
+++ leetcode/3Sum_Closest/solution.cc
@@ -1,11 +1,12 @@
+#include <climits>
 #include <cmath>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-using std::cout;
-using std::vector;
+using namespace std;
 
-class Solution {
+class BruteForceSolution {
   public:
     int threeSumClosest(vector<int> &nums, int target) {
         int result = nums[0] + nums[1] + nums[2];
@@ -25,10 +26,47 @@ class Solution {
     }
 };
 
-int main() {
-    Solution s;
-    vector<int> v{-1, 2, 1, -4};
+class Solution {
+  public:
+    int threeSumClosest(vector<int> &nums, int target) {
+        sort(nums.begin(), nums.end());
+        int result = nums[0] + nums[1] + nums.back();
 
+        for (int i = 0; i < nums.size() - 2; ++i) {
+            int x = target - nums[i];
+            int l = i + 1;
+            int r = nums.size() - 1;
+
+            while (l < r) {
+                int twoSum = nums[l] + nums[r];
+
+                if (twoSum == x) {
+                    return target;
+                } else {
+                    int candidate = nums[i] + nums[l] + nums[r];
+                    if (abs(candidate - target) < abs(result - target)) {
+                        result = candidate;
+                    }
+                }
+
+                if (twoSum < x) {
+                    ++l;
+                } else {
+                    --r;
+                }
+            }
+        }
+
+        return result;
+    }
+};
+
+int main() {
+    BruteForceSolution bs;
+    Solution s;
+    vector<int> v{1, 3, 3, 4, 4, 5, 61, 79, 28, 8, 24, 22, 21, 34, 45, 88, 9};
+
+    cout << bs.threeSumClosest(v, 1) << '\n';
     cout << s.threeSumClosest(v, 1) << '\n';
 
     return 0;
