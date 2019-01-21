074b9123f1534455c2c239dfb3545a08e304f32c
Ivan Krechetov <ikr@ikr.li>
2019-01-21 20:55:01 +0100

    Define the increments_by_char map completely

--- leetcode/Letter_Combinations_of_a_Phone_Number/solution.cc
+++ leetcode/Letter_Combinations_of_a_Phone_Number/solution.cc
@@ -2,6 +2,7 @@
 #include <climits>
 #include <cmath>
 #include <iostream>
+#include <string>
 #include <unordered_map>
 #include <vector>
 
@@ -11,16 +12,25 @@ class Solution {
   public:
   private:
     static const unordered_map<char, char> increments_by_char;
-
     static char inc(char x) { return 0; }
 };
 
 const unordered_map<char, char> Solution::increments_by_char{
-    {'1', 'a'}, {'a', 'b'}, {'b', 'c'},
+    {'2', 'a'}, {'a', 'b'}, {'b', 'c'},
 
-    {'2', 'd'}, {'d', 'e'}, {'e', 'f'},
+    {'3', 'd'}, {'d', 'e'}, {'e', 'f'},
 
-    {'3', 'g'}, {'g', 'h'}, {'h', 'i'}};
+    {'4', 'g'}, {'g', 'h'}, {'h', 'i'},
+
+    {'5', 'j'}, {'j', 'k'}, {'k', 'l'},
+
+    {'6', 'm'}, {'m', 'n'}, {'n', 'o'},
+
+    {'7', 'p'}, {'p', 'q'}, {'q', 'r'}, {'r', 's'},
+
+    {'8', 't'}, {'t', 'u'}, {'u', 'v'},
+
+    {'9', 'w'}, {'w', 'x'}, {'x', 'y'}, {'y', 'z'}};
 
 int main() {
     Solution s;
