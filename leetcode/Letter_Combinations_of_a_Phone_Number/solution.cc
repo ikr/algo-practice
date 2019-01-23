f19be3d246e56d6e357c4871bea8c9a2fca2006d
Ivan Krechetov <ikr@ikr.li>
2019-01-23 20:46:30 +0100

    Write a few helpers

--- leetcode/Letter_Combinations_of_a_Phone_Number/solution.cc
+++ leetcode/Letter_Combinations_of_a_Phone_Number/solution.cc
@@ -10,9 +10,42 @@ using namespace std;
 
 class Solution {
   public:
+    vector<string> letterCombinations(string digits) {}
+
+    static string::reverse_iterator rightmost_non_final(string &s) {
+        string::reverse_iterator result = s.rbegin();
+
+        while (result != s.rend()) {
+            if (!is_final(*result)) {
+                return result;
+            }
+
+            result++;
+        }
+    }
+
+    static string first_combination(const string &digits) {
+        string result = digits;
+
+        for (char &d : result) {
+            d = inc(d);
+        }
+
+        return result;
+    }
+
+    static char inc(char x) {
+        if (increments_by_char.count(x) == 0) {
+            return -1;
+        }
+
+        return increments_by_char.at(x);
+    }
+
+    static bool is_final(char x) { return inc(x) == -1; }
+
   private:
     static const unordered_map<char, char> increments_by_char;
-    static char inc(char x) { return 0; }
 };
 
 const unordered_map<char, char> Solution::increments_by_char{
@@ -34,8 +67,10 @@ const unordered_map<char, char> Solution::increments_by_char{
 
 int main() {
     Solution s;
+    string combination = Solution::first_combination("234");
 
-    cout << '\n';
+    cout << *Solution::rightmost_non_final(combination);
+    cout << endl;
 
     return 0;
 }
