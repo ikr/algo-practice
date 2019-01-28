cbb60e55a5a7ded4a450881c309e60a785f81f5c
Ivan Krechetov <ikr@ikr.li>
2019-01-28 20:28:22 +0100

    Implement the OptimalConstantSpaceSolution

--- leetcode/4Sum/solution.cc
+++ leetcode/4Sum/solution.cc
@@ -24,7 +24,6 @@ class BruteForceSolution {
                             vector<int> v{nums[i], nums[j], nums[k], nums[l]};
                             sort(v.begin(), v.end());
                             result_set.insert(v);
-
                         }
                     }
                 }
@@ -42,8 +41,48 @@ class BruteForceSolution {
     }
 };
 
+class OptimalConstantSpaceSolution {
+  public:
+    vector<vector<int>> fourSum(vector<int> &nums, int target) {
+        sort(nums.begin(), nums.end());
+        set<vector<int>> result_set;
+
+        for (auto i = 0; i < nums.size() - 3; ++i) {
+            for (auto j = i + 1; j < nums.size() - 2; ++j) {
+                int l = j + 1;
+                int r = nums.size() - 1;
+
+                while (l < r) {
+                    int candidate = nums[i] + nums[j] + nums[l] + nums[r];
+
+                    if (candidate == target) {
+                        result_set.insert(
+                            vector<int>{nums[i], nums[j], nums[l], nums[r]});
+                        break;
+                    }
+
+                    if (candidate < target) {
+                        ++l;
+                    } else {
+                        --r;
+                    }
+                }
+            }
+        }
+
+        return vectorize(result_set);
+    }
+
+  private:
+    static vector<vector<int>> vectorize(const set<vector<int>> &source) {
+        vector<vector<int>> result;
+        copy(source.begin(), source.end(), back_inserter(result));
+        return result;
+    }
+};
+
 int main() {
-    BruteForceSolution s;
+    OptimalConstantSpaceSolution s;
     vector<int> nums{1, 0, -1, 0, -2, 2};
     auto result = s.fourSum(nums, 0);
 
