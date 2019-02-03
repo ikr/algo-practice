bc199a922523596250dfd4a783390b9fdeb7c38f
Ivan Krechetov <ikr@ikr.su>
2019-02-03 10:03:52 +0100

    Variate

--- leetcode/4Sum/solution.cc
+++ leetcode/4Sum/solution.cc
@@ -11,59 +11,7 @@
 
 using namespace std;
 
-class VectorHash {
-  public:
-    int operator()(const vector<int> &v) const {
-        if (v.empty()) {
-            return 0;
-        }
-
-        vector<int>::const_iterator it = v.begin();
-        int result = *it;
-        ++it;
-
-        while (it != v.end()) {
-            result = 31 * result + *it;
-            ++it;
-        }
-
-        return result;
-    }
-};
-
-using QuadsSet = unordered_set<vector<int>, VectorHash>;
-
-class BruteForceSolution {
-  public:
-    vector<vector<int>> fourSum(vector<int> &nums, int target) {
-        QuadsSet result_set;
-
-        for (auto i = 0; i < nums.size() - 3; ++i) {
-            for (auto j = i + 1; j < nums.size() - 2; ++j) {
-                for (auto k = j + 1; k < nums.size() - 1; ++k) {
-                    for (auto l = k + 1; l < nums.size(); ++l) {
-                        if (nums[i] + nums[j] + nums[k] + nums[l] == target) {
-                            vector<int> v{nums[i], nums[j], nums[k], nums[l]};
-                            sort(v.begin(), v.end());
-                            result_set.insert(v);
-                        }
-                    }
-                }
-            }
-        }
-
-        return vectorize(result_set);
-    }
-
-  private:
-    static vector<vector<int>> vectorize(const QuadsSet &source) {
-        vector<vector<int>> result;
-        copy(source.begin(), source.end(), back_inserter(result));
-        return result;
-    }
-};
-
-class NCubeSolution {
+class Solution {
   public:
     vector<vector<int>> fourSum(vector<int> &nums, int target) {
         if (nums.size() < 4) {
@@ -71,7 +19,7 @@ class NCubeSolution {
         }
 
         sort(nums.begin(), nums.end());
-        QuadsSet result_set;
+        vector<vector<int>> result;
 
         for (auto i = 0; i < nums.size() - 3; ++i) {
             for (auto j = i + 1; j < nums.size() - 2; ++j) {
@@ -82,8 +30,8 @@ class NCubeSolution {
                     int candidate = nums[i] + nums[j] + nums[l] + nums[r];
 
                     if (candidate == target) {
-                        result_set.insert(
-                            vector<int>{nums[i], nums[j], nums[l], nums[r]});
+                        vector<int> v{nums[i], nums[j], nums[l], nums[r]};
+                        result.push_back(v);
                         ++l;
                     } else if (candidate < target) {
                         ++l;
@@ -94,19 +42,15 @@ class NCubeSolution {
             }
         }
 
-        return vectorize(result_set);
-    }
+        sort(result.begin(), result.end());
+        result.erase(unique(result.begin(), result.end()), result.end());
 
-  private:
-    static vector<vector<int>> vectorize(const QuadsSet &source) {
-        vector<vector<int>> result;
-        copy(source.begin(), source.end(), back_inserter(result));
         return result;
     }
 };
 
 int main() {
-    NCubeSolution s;
+    Solution s;
     // vector<int> nums{1, 0, -1, 0, -2, 2};
     vector<int> nums{-3, -1, 0, 2, 4, 5};
     // vector<int> nums{-4, -3, -2, -1, 0, 0, 1, 2, 3, 4};
