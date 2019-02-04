77ac428fc2dbd57813eed8bfe877aa4ba6675e2a
Ivan Krechetov <ikr@ikr.li>
2019-02-04 20:30:47 +0100

    Start with the “Swap Nodes in Pairs”

---
+++ leetcode/Swap_Nodes_in_Pairs/solution.cc
@@ -0,0 +1,74 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <iostream>
+#include <iterator>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+  public:
+    ListNode *swapPairs(ListNode *head) {
+        return head;
+    }
+};
+
+ListNode *make_list(const vector<int> xs) {
+    ListNode *head = nullptr;
+    ListNode *tail = nullptr;
+
+    for (auto x : xs) {
+        auto n = new ListNode(x);
+
+        if (tail == nullptr) {
+            head = n;
+            tail = n;
+            continue;
+        }
+
+        tail->next = n;
+        tail = n;
+    }
+
+    return head;
+}
+
+void delete_list(ListNode *head) {
+    auto p = head;
+
+    while (p != nullptr) {
+        auto current = p;
+        p = p->next;
+        delete current;
+    }
+}
+
+ostream& operator<<(ostream &os, ListNode *head) {
+    auto p = head;
+    while (p != nullptr) {
+        os << p->val << ' ';
+        p = p->next;
+    }
+
+    return os;
+}
+
+int main() {
+    auto l = Solution().swapPairs(make_list({1, 2, 3, 4}));
+    cout << l << endl;
+    delete_list(l);
+
+    return 0;
+}
