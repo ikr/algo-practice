804e34cd8ebb9c51feaafbe8c93373d62be1ee02
Ivan Krechetov <ikr@ikr.su>
2019-02-14 17:57:13 +0100

    Implement the build_lps helper for the KMP solution

--- leetcode/Implement_strStr/solution.cc
+++ leetcode/Implement_strStr/solution.cc
@@ -18,6 +18,34 @@ using namespace std;
 class KMPSolution {
   public:
     int strStr(const string &haystack, const string &needle) { return -1; }
+
+    static vector<int> build_lps(const string &s) {
+        const int sz = s.size();
+        vector<int> lps(sz);
+
+        for (int l = 1; l <= sz; ++l) {
+            lps[l - 1] = max_proper_pre_suffix_length(s.substr(0, l));
+        }
+
+        return lps;
+    }
+
+    static int max_proper_pre_suffix_length(const string &s) {
+        int result = 0;
+
+        for (int l = 1; l != s.size(); ++l) {
+            if (head(s, l) == tail(s, l))
+                result = l;
+        }
+
+        return result;
+    }
+
+    static string head(const string &s, int l) { return s.substr(0, l); }
+
+    static string tail(const string &s, int l) {
+        return s.substr(s.size() - l, l);
+    }
 };
 
 class RKSolution {
@@ -25,8 +53,17 @@ class RKSolution {
     int strStr(const string &haystack, const string &needle) { return -1; }
 };
 
+ostream &operator<<(ostream &os, const vector<int> &v) {
+    for (auto x : v) {
+        os << x << ' ';
+    }
+
+    return os;
+}
+
 int main() {
     KMPSolution s;
+    cout << KMPSolution::build_lps("abababca") << endl;
 
     cout << s.strStr("hello", "ll") << endl;
     cout << s.strStr("aaaaa", "bba") << endl;
