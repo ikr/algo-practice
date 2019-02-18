59648bb126e4e79f4e81ab04c32903b5687a5ffc
Ivan Krechetov <ikr@ikr.li>
2019-02-18 19:53:45 +0100

    Bootstrap

---
+++ leetcode/Substring_with_Concatenation_of_All_Words/solution.cc
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <numeric>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> findSubstring(string s, vector<string> &words) {
+        return vector<int>();
+    }
+};
+
+int main() {
+    Solution s;
+
+    cout << endl;
+
+    return 0;
+}
