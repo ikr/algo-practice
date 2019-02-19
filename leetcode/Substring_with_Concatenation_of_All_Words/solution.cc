0a78140779f8973646fad49bd27cf6341207b371
Ivan Krechetov <ikr@ikr.su>
2019-02-19 20:23:35 +0100

    Top-down outline of the algorithm

--- leetcode/Substring_with_Concatenation_of_All_Words/solution.cc
+++ leetcode/Substring_with_Concatenation_of_All_Words/solution.cc
@@ -17,8 +17,65 @@ using namespace std;
 
 class Solution {
   public:
-    vector<int> findSubstring(string s, vector<string> &words) {
-        return vector<int>();
+    vector<int> findSubstring(const string &hst, const vector<string> &words) {
+        auto degenerate_case_res = degen_case(hst, words);
+        if (degenerate_case_res.first)
+            return vector<int>{degenerate_case_res.second};
+
+        vector<int> result;
+        auto word_hashes = hash_all(words);
+        auto words_set = make_set(words);
+        auto sz = words.begin()->size();
+        auto cnt = words.size();
+        auto bound = hst.size() - cnt * sz + 1;
+
+        for (int i = 0, h0 = 0; i != bound; ++i) {
+            const int h = (i == 0) ? hash_one(hst.substr(0, sz))
+                                   : rolling_hash(hst, h0, i, sz);
+
+            if (word_hashes.count(h) && words_set.count(hst.substr(i, sz))) {
+                if (stamping_match(hst, i, words))
+                    result.push_back(i);
+            }
+
+            h0 = h;
+        }
+
+        return result;
+    }
+
+  private:
+    static pair<bool, int> degen_case(const string &hst,
+                                      const vector<string> &words) {
+        // TODO
+        return make_pair(false, -1);
+    }
+
+    static unordered_set<int> hash_all(const vector<string> &words) {
+        // TODO
+        return unordered_set<int>{};
+    }
+
+    static unordered_set<string> make_set(const vector<string> &words) {
+        // TODO
+        return unordered_set<string>{};
+    }
+
+    static int hash_one(const string &s) {
+        // TODO
+        return 0;
+    }
+
+    static int rolling_hash(const string &s, const int prev_hash,
+                            const int start_index, const int sz) {
+        // TODO
+        return 0;
+    }
+
+    static bool stamping_match(const string &hst, int index,
+                               const vector<string> &words) {
+        // TODO
+        return false;
     }
 };
 
