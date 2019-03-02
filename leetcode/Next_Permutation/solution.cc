116bcbb568642f0b246e843ac92ff63e2c61b469
Ivan Krechetov <ikr@ikr.su>
2019-03-02 15:25:54 +0100

    Start with the “Next Permutation”

---
+++ leetcode/Next_Permutation/solution.cc
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <numeric>
+#include <optional>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+  public:
+    void nextPermutation(vector<int> &nums) {}
+};
+
+int main() {
+    Solution s;
+
+    cout << endl;
+
+    return 0;
+}
