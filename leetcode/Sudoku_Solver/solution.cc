72e04145e22e0125a8bd35e44b7b8819ceaae6a9
Ivan Krechetov <ikr@ikr.li>
2019-03-07 17:27:42 +0100

    Start filling in the gaps

--- leetcode/Sudoku_Solver/solution.cc
+++ leetcode/Sudoku_Solver/solution.cc
@@ -17,17 +17,34 @@
 
 using namespace std;
 
-class Solution {
-    using Rows = vector<vector<char>>;
+namespace {
+struct Coord {
+    Coord(int row, int col) : r{row}, c{col} {}
+    int row() const;
+    int col() const;
 
-  public:
+  private:
+    int r;
+    int c;
+};
+
+struct Solution {
     void solveSudoku(vector<vector<char>> &rows) const;
 
   private:
+    using Rows = vector<vector<char>>;
+    static constexpr int bsize = 3;
+    static constexpr int gsize = bsize * bsize;
+    static const vector<char> digits;
+
     static bool search_recur(Rows &rows);
+    static optional<Coord> first_absent_coord(const Rows &rows);
+    static const vector<char> init_digits();
 };
 
-void Solution::solveSudoku(vector<vector<char>> &rows) const {}
+const vector<char> Solution::digits = Solution::init_digits();
+
+void Solution::solveSudoku(Rows &rows) const {}
 
 bool Solution::search_recur(Rows &rows) {
     optional<const Coord> v_coord = first_absent_coord(rows);
@@ -35,19 +52,37 @@ bool Solution::search_recur(Rows &rows) {
     if (!v_coord)
         return true;
 
-    for (const char d : digits()) {
+    for (const char d : digits) {
         if (is_valid(rows, v_coord, d)) {
-            rows[v_coord.row()][v_coord.col()] = d;
+            rows[v_coord->row()][v_coord->col()] = d;
 
             if (search_recur(rows))
                 return true;
 
-            rows[v_coord.row()][v_coord.col()] = '.';
+            rows[v_coord->row()][v_coord->col()] = '.';
         }
     }
 
     return false;
 }
+
+optional<Coord> Solution::first_absent_coord(const Rows &rows) {
+    for (int row = 0; row != gsize; ++row) {
+        for (int col = 0; col != gsize; ++col) {
+            if (rows[row][col] == '.')
+                return {{row, col}};
+        }
+    }
+
+    return nullopt;
+}
+
+const vector<char> Solution::init_digits() {
+    vector<char> result(gsize);
+    iota(result.begin(), result.end(), '1');
+    return result;
+}
+} // namespace
 
 vector<vector<char>> input1() {
     return {{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
