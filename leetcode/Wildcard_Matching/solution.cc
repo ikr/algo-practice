ad5f7822586559ff153dfe2b9977aa15a41fd318
Ivan Krechetov <ikr@ikr.su>
2019-04-22 14:04:00 +0200

    Write tests

--- leetcode/Wildcard_Matching/solution.cc
+++ leetcode/Wildcard_Matching/solution.cc
@@ -59,8 +59,42 @@ bool Solution::isMatch(const string &s, const string &p) const {
     return is_match(p.begin(), p.end(), s.begin(), s.end());
 }
 
+struct TestCase {
+    string pattern;
+    string candidate;
+    bool expected_result;
+
+    TestCase() : pattern{}, candidate{}, expected_result(true) {}
+    ~TestCase() {}
+
+    TestCase(const char *p, const char *s, bool r)
+        : pattern{p}, candidate{s}, expected_result{r} {}
+};
+
+vector<TestCase> test_cases() {
+    return {{"", "", true},
+            {"a", "aa", false},
+            {"z", "z", true},
+            {"*", "aa", true},
+            {"?a", "cb", false},
+            {"*a*b", "adceb", true},
+            {"a*c?b", "acdcb", false},
+            {"a*c??b", "acdcb", true},
+            {"abcd*zzz", "abcdzzz", true},
+            {"a?cd*z?z", "aocdooooooooooooooooooooooooooooooooooooozzz", true},
+            {"a?cd*z?z", "aacdcdooooooooooooozz", false},
+            {"ab*cd*ok", "abocodook", false}};
+}
+
 int main() {
-    cout << Solution().isMatch("acdcb", "a*c??b") << endl;
+    for (const TestCase &tc : test_cases()) {
+        const bool result = Solution().isMatch(tc.candidate, tc.pattern);
+
+        cout << '/' << tc.pattern << "/ on \"" << tc.candidate << "\" is "
+             << result << endl;
+
+        assert(result == tc.expected_result);
+    }
 
     return 0;
 }
