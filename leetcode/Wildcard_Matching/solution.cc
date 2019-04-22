54d6b2ce86881dbf12423e8de669032250dcd0dd
Ivan Krechetov <ikr@ikr.li>
2019-04-22 13:34:52 +0200

    Implement the 1st version

--- leetcode/Wildcard_Matching/solution.cc
+++ leetcode/Wildcard_Matching/solution.cc
@@ -23,12 +23,44 @@ struct Solution {
     bool isMatch(const string &s, const string &p) const;
 };
 
+using Iter = std::string::const_iterator;
+
+bool are_all_stars(Iter begin, Iter end) {
+    return all_of(begin, end, [](const char c) { return c == '*'; });
+}
+
+bool is_alpha(const char c) { return c >= 'a' && c <= 'z'; }
+bool is_wildcard(const char c) { return c == '*' || c == '?'; }
+
+bool is_match(Iter pi, Iter pe, Iter si, Iter se) {
+    if (pi == pe && si == se)
+        return true;
+
+    if (pi == pe && si != se)
+        return false;
+
+    if (pi != pe && si == se)
+        return are_all_stars(pi, pe);
+
+    assert(is_alpha(*pi) || is_wildcard(*pi));
+    assert(is_alpha(*si));
+
+    if (is_alpha(*pi))
+        return *pi == *si && is_match(pi + 1, pe, si + 1, se);
+
+    if (*pi == '?')
+        return is_match(pi + 1, pe, si + 1, se);
+
+    assert(*pi == '*');
+    return is_match(pi, pe, si + 1, se) || is_match(pi + 1, pe, si, se);
+}
+
 bool Solution::isMatch(const string &s, const string &p) const {
-    return s == p;
+    return is_match(p.begin(), p.end(), s.begin(), s.end());
 }
 
 int main() {
-    cout << Solution().isMatch("acdcb", "a*c?b") << endl;
+    cout << Solution().isMatch("acdcb", "a*c??b") << endl;
 
     return 0;
 }
