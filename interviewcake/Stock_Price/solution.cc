56f0f8b266689dde44903d62604209efe8c454b4
Ivan Krechetov <ikr@ikr.su>
2019-05-05 18:02:59 +0200

    Solve the Stock_Price

--- interviewcake/Stock_Price/solution.cc
+++ interviewcake/Stock_Price/solution.cc
@@ -2,16 +2,17 @@
 #include <algorithm>
 #include <bitset>
 #include <cassert>
+#include <climits>
 #include <cmath>
 #include <functional>
 #include <iostream>
 #include <iterator>
-#include <limits>
 #include <locale>
 #include <numeric>
 #include <optional>
 #include <set>
 #include <stack>
+#include <stdexcept>
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
@@ -20,48 +21,65 @@
 
 using namespace std;
 
-int max_profit(const vector<int> &stock_prices) {
-    return static_cast<int>(stock_prices.size());
+int max_profit_from(const vector<int> &prices) {
+    if (prices.size() < 2)
+        throw invalid_argument("Expecting at least 2 prices");
+
+    int min_so_far = INT_MAX;
+    int max_profit = INT_MIN;
+
+    for (const int x : prices) {
+        if (min_so_far == INT_MAX) {
+            min_so_far = x;
+            continue;
+        }
+
+        const int profit = x - min_so_far;
+        if (profit > max_profit) max_profit = profit;
+        if (x < min_so_far) min_so_far = x;
+    }
+
+    return max_profit;
 }
 
 // clang-format off
 const lest::test tests[] = {
     CASE("price goes up then down") {
-        const int actual = max_profit({1, 5, 3, 2});
+        const int actual = max_profit_from({1, 5, 3, 2});
         const int expected = 4;
         EXPECT(actual == expected);
     },
 
     CASE("price goes down then up") {
-        const int actual = max_profit({7, 2, 8, 9});
+        const int actual = max_profit_from({7, 2, 8, 9});
         const int expected = 7;
         EXPECT(actual == expected);
     },
 
     CASE("price goes up all day") {
-        const int actual = max_profit({1, 6, 7, 9});
+        const int actual = max_profit_from({1, 6, 7, 9});
         const int expected = 8;
         EXPECT(actual == expected);
     },
 
     CASE("price goes down all day") {
-        const int actual = max_profit({9, 7, 4, 1});
+        const int actual = max_profit_from({9, 7, 4, 1});
         const int expected = -2;
         EXPECT(actual == expected);
     },
 
     CASE("price stays the same all day") {
-        const int actual = max_profit({1, 1, 1, 1});
+        const int actual = max_profit_from({1, 1, 1, 1});
         const int expected = 0;
         EXPECT(actual == expected);
     },
 
     CASE("exception with empty prices") {
-        EXPECT_THROWS(max_profit({}));
+        EXPECT_THROWS(max_profit_from({}));
     },
 
     CASE("exception with one price") {
-        EXPECT_THROWS(max_profit({1}));
+        EXPECT_THROWS(max_profit_from({1}));
     },
 };
 // clang-format on
