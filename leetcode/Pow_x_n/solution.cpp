e47380f1a015822075d1cc8313a7c7b13e0d603f
Ivan Krechetov <ikr@ikr.su>
2019-06-03 20:29:40 +0200

    Come up with some tests

--- leetcode/Pow_x_n/solution.cpp
+++ leetcode/Pow_x_n/solution.cpp
@@ -9,6 +9,56 @@ struct Solution final {
 
 // clang-format off
 const lest::test tests[] = {
+    CASE("42^0") {
+        const auto actual = Solution().myPow(42.0, 0);
+        const auto expected = lest::approx(1.0);
+        EXPECT(actual == expected);
+    },
+    CASE("-42^0") {
+        const auto actual = Solution().myPow(-42.0, 0);
+        const auto expected = lest::approx(1.0);
+        EXPECT(actual == expected);
+    },
+    CASE("-2^-2") {
+        const auto actual = Solution().myPow(-2.0, -2);
+        const auto expected = lest::approx(0.5);
+        EXPECT(actual == expected);
+    },
+    CASE("-2^3") {
+        const auto actual = Solution().myPow(-2.0, 3);
+        const auto expected = lest::approx(-8.0);
+        EXPECT(actual == expected);
+    },
+    CASE("1^1") {
+        const auto actual = Solution().myPow(1.0, 1);
+        const auto expected = lest::approx(1.0);
+        EXPECT(actual == expected);
+    },
+    CASE("1^-1") {
+        const auto actual = Solution().myPow(1.0, -1);
+        const auto expected = lest::approx(1.0);
+        EXPECT(actual == expected);
+    },
+    CASE("-1^-1") {
+        const auto actual = Solution().myPow(-1.0, -1);
+        const auto expected = lest::approx(-1.0);
+        EXPECT(actual == expected);
+    },
+    CASE("1^999999") {
+        const auto actual = Solution().myPow(1.0, 999999);
+        const auto expected = lest::approx(1.0);
+        EXPECT(actual == expected);
+    },
+    CASE("2^16") {
+        const auto actual = Solution().myPow(2.0, 16);
+        const auto expected = lest::approx(65536.0);
+        EXPECT(actual == expected);
+    },
+    CASE("0^-1") {
+        const auto actual = Solution().myPow(0.0, -1);
+        const auto expected = lest::approx(INFINITY);
+        EXPECT(actual == expected);
+    },
     CASE("problem statement example 1") {
         const auto actual = Solution().myPow(2.0, 10);
         const auto expected = lest::approx(1024.0);
