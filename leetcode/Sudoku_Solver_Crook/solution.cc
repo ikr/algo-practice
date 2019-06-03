c1a3a2fee4c12fe1cd71c1416d25304440e33efa
Ivan Krechetov <ikr@ikr.su>
2019-06-03 09:32:36 +0200

    Reduce more

--- leetcode/Sudoku_Solver_Crook/solution.cc
+++ leetcode/Sudoku_Solver_Crook/solution.cc
@@ -1,20 +1,4 @@
-#include <algorithm>
-#include <bitset>
-#include <cassert>
-#include <cmath>
-#include <functional>
-#include <iostream>
-#include <iterator>
-#include <limits>
-#include <numeric>
-#include <optional>
-#include <set>
-#include <stack>
-#include <string>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
