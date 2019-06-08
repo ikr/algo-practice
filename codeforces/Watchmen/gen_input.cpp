d23603c2b95f70b8494a67e51da069ea196fe1b9
Ivan Krechetov <ikr@ikr.su>
2019-06-08 10:23:46 +0200

    Parameterize the max coordinate value

--- codeforces/Watchmen/gen_input.cpp
+++ codeforces/Watchmen/gen_input.cpp
@@ -2,21 +2,19 @@
 #include <iostream>
 #include <random>
 
-constexpr int MAX_COORD = 1000 * 1000 * 1000;
-constexpr int MIN_COORD = -MAX_COORD;
-
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        std::cerr << "Usage: ./gen_input <POINTS COUNT>" << '\n';
+    if (argc != 3) {
+        std::cerr << "Usage: ./gen_input <MAX COORD VALUE> <POINTS COUNT>" << '\n';
         return 1;
     }
 
-    const int n = std::atoi(argv[1]);
+    const int max_coord = std::atoi(argv[1]);
+    const int n = std::atoi(argv[2]);
     std::cout << n << std::endl;
 
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dist(MIN_COORD, MAX_COORD);
+    std::uniform_int_distribution<> dist(-max_coord, max_coord);
 
     for (int i = 0; i != n; ++i)
         std::cout << dist(gen) << ' ' << dist(gen) << '\n';
