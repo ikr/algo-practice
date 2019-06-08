ba5d6167fc0b02f4bd5463cdff4cb69da7b0e598
Ivan Krechetov <ikr@ikr.li>
2019-06-08 13:37:00 +0200

    Implement an O(n) solution
    Based on the fact that de² = dm² + 2(x₁ - x₂)(y₁ - y₂). Thus, for the
    two distances to be equal, it is necessary and sufficient that either x₁
    == x₂ or y₁ == y₂.

--- codeforces/Watchmen/solution.cpp
+++ codeforces/Watchmen/solution.cpp
@@ -4,55 +4,41 @@ using namespace std;
 
 using XY = pair<int, int>;
 
+namespace std {
+template <> struct hash<XY> {
+    size_t operator()(const XY &p) const noexcept {
+        return 31 * static_cast<size_t>(p.first) +
+               static_cast<size_t>(p.second);
+    }
+};
+} // namespace std
+
 ostream &operator<<(ostream &os, const XY &p) {
     os << "(" << p.first << " " << p.second << ")";
     return os;
-}
-
-template <typename T> int intof(const T x) { return static_cast<int>(x); }
-
-int square(const int x) { return x * x; }
-
-optional<int> isqrt(const int x) {
-    const double accurate_sqrt = sqrt(x);
-
-    return intof(floor(accurate_sqrt)) == intof(ceil(accurate_sqrt))
-               ? make_optional(intof(trunc(accurate_sqrt)))
-               : nullopt;
-}
-
-int manhattan_distance(const XY &p1, const XY &p2) {
-    return abs(p1.first - p2.first) + abs(p1.second - p2.second);
-}
-
-optional<int> euclidian_idistance(const XY &p1, const XY &p2) {
-    return isqrt(square(p1.first - p2.first) + square(p1.second - p2.second));
 }
 
 int main() {
     int n{0};
     cin >> n;
 
-    vector<XY> points;
+    unordered_map<int, size_t> counts_by_x;
+    unordered_map<int, size_t> counts_by_y;
+    unordered_map<XY, size_t> counts_by_point;
 
+    size_t result = 0;
     for (int i = 0; i != n; ++i) {
         int x{0};
         int y{0};
         cin >> x >> y;
 
-        points.emplace_back(x, y);
-    }
+        const XY point = make_pair(x, y);
 
-    int result = 0;
+        result += counts_by_x[x] + counts_by_y[y] - counts_by_point[point];
 
-    for (int i = 0; i != n - 1; ++i) {
-        for (int j = i + 1; j != n; ++j) {
-            const auto maybe_eucl = euclidian_idistance(points[i], points[j]);
-            if (!maybe_eucl) continue;
-
-            if (manhattan_distance(points[i], points[j]) == *maybe_eucl)
-                ++result;
-        }
+        ++counts_by_x[x];
+        ++counts_by_y[y];
+        ++counts_by_point[point];
     }
 
     cout << result << endl;
