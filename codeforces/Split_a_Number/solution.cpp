d2c03ec04cd14289d6353d88f826a716ac534dfb
Ivan Krechetov <ikr@ikr.su>
2019-06-16 12:52:42 +0200

    First version of the solution

--- codeforces/Split_a_Number/solution.cpp
+++ codeforces/Split_a_Number/solution.cpp
@@ -2,6 +2,11 @@
 
 using namespace std;
 
+bool digits_less(const string &a, const string &b) {
+    if (a.size() == b.size()) return a < b;
+    return a.size() < b.size();
+}
+
 string add(const string &a, const string &b) {
     auto ia = a.rbegin();
     auto ib = b.rbegin();
@@ -10,21 +15,21 @@ string add(const string &a, const string &b) {
     string result;
 
     while (ia != a.rend() || ib != b.rend()) {
-        int d = 0;
+        int d = carry ? 1 : 0;
 
         if (ia != a.rend() && ib != b.rend()) {
-            d = *ia - '0' + *ib - '0';
+            d += *ia - '0' + *ib - '0';
             ++ia;
             ++ib;
         } else if (ia != a.rend()) {
-            d = *ia - '0';
+            d += *ia - '0';
             ++ia;
         } else { // ib != b.rend()
-            d = *ib - '0';
+            d += *ib - '0';
             ++ib;
         }
 
-        result += static_cast<char>('0' + (d % 10) + (carry ? 1 : 0));
+        result += static_cast<char>('0' + (d % 10));
         carry = (d / 10);
     }
 
@@ -39,5 +44,22 @@ int main() {
     string digits;
     cin >> length >> digits;
 
-    cout << add(digits, digits) << '\n';
+    size_t b_start = 1;
+    while (digits[b_start] == '0') ++b_start;
+
+    string a = digits.substr(0, b_start);
+    string b = digits.substr(b_start, string::npos);
+    string result = add(a, b);
+
+    for (b_start = b_start + 1; b_start <= digits.size() - 1; ++b_start) {
+        a += digits[b_start - 1];
+        b.erase(b.begin(), b.begin() + 1);
+        if (digits[b_start] == '0') continue;
+
+        const string the_sum = add(a, b);
+
+        if (digits_less(the_sum, result)) result = the_sum;
+    }
+
+    cout << result << '\n';
 }
