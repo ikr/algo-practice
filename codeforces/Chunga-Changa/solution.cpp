62098be9c9dfa963f230156bbf3be851ff5b219c
Ivan Krechetov <ikr@ikr.su>
2019-06-16 14:12:03 +0200

    Make the problem get accepted by replacing the size_t with an ull_t

--- codeforces/Chunga-Changa/solution.cpp
+++ codeforces/Chunga-Changa/solution.cpp
@@ -2,23 +2,25 @@
 
 using namespace std;
 
+using ull_t = unsigned long long;
+
 int main() {
-    size_t fund1;
-    size_t fund2;
-    size_t price;
+    ull_t fund1;
+    ull_t fund2;
+    ull_t price;
 
     cin >> fund1 >> fund2 >> price;
 
-    const size_t missing1 = price - (fund1 % price);
-    const size_t missing2 = price - (fund2 % price);
+    const ull_t missing1 = price - (fund1 % price);
+    const ull_t missing2 = price - (fund2 % price);
 
-    const size_t extra1 = fund1 % price;
-    const size_t extra2 = fund2 % price;
+    const ull_t extra1 = fund1 % price;
+    const ull_t extra2 = fund2 % price;
 
     const bool can_credit =
         (extra1 && missing2 <= extra1) || (extra2 && missing1 <= extra2);
 
-    const size_t credit = can_credit ? min(missing1, missing2) : 0;
+    const ull_t credit = can_credit ? min(missing1, missing2) : 0;
 
     cout << (fund1 / price + fund2 / price + (can_credit ? 1 : 0)) << ' '
          << credit << '\n';
