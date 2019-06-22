070eef81e25a5b4e2576014ee06acf21b8f380e5
Ivan Krechetov <ikr@ikr.li>
2019-06-22 09:37:32 +0200

    Prepare for the comparative testing
    That is, solving via brute force on a lot of small random samples.

--- codeforces/Nick_and_Array/solution.cpp
+++ codeforces/Nick_and_Array/solution.cpp
@@ -5,6 +5,50 @@ using namespace std;
 using ll_t = long long;
 template <typename T> int intof(const T x) { return static_cast<int>(x); }
 
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    const auto i_back = xs.end() - 1;
+
+    os << '[';
+
+    for (auto i = xs.begin(); i != xs.end(); ++i) {
+        os << *i;
+        if (i != i_back) os << ' ';
+    }
+
+    os << ']';
+
+    return os;
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<vector<T>> &xs) {
+    for (const auto &x : xs) {
+        os << x << ' ';
+    }
+
+    return os;
+}
+
+//------------------------------------------------------------------------------
+
+ll_t bruteforce_max_product(const vector<ll_t> &xs) {}
+
+void indices_subsets(const size_t sz, const vector<size_t> &prefix,
+                     const size_t index, vector<vector<size_t>> &result) {
+    if (index == sz) {
+        result.push_back(prefix);
+        return;
+    }
+
+    indices_subsets(sz, prefix, index + 1, result);
+
+    vector<size_t> prefix_with = prefix;
+    prefix_with.push_back(index);
+    indices_subsets(sz, prefix_with, index + 1, result);
+}
+
+//------------------------------------------------------------------------------
+
 int zeros_count_with_replace(vector<ll_t> &xs) {
     int zeroes_count = 0;
 
@@ -68,7 +112,7 @@ void compute(vector<ll_t> &xs) {
     if (sign < 0) *i_max = v_max;
 }
 
-int main() {
+void do_io() {
     int sz;
     cin >> sz;
 
@@ -81,10 +125,14 @@ int main() {
     }
 
     compute(xs);
+    cout << xs << '\n';
+}
 
-    for (auto i = xs.begin(); i != xs.end(); ++i) {
-        cout << *i;
-        if (i != xs.end() - 1) cout << ' ';
-    }
-    cout << '\n';
+int main() {
+    vector<vector<size_t>> subsets;
+    indices_subsets(4, {}, 0, subsets);
+    cout << subsets << '\n';
+
+    do_io();
+    return 0;
 }
