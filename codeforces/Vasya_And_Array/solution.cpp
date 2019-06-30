da27b754e14ccb6747ce81660b04f076af13d407
Ivan Krechetov <ikr@ikr.li>
2019-06-30 18:45:34 +0200

    Bootstrap the “Vasya And Array”

---
+++ codeforces/Vasya_And_Array/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    int facts_count;
+    cin >> n >> facts_count;
+
+    for (int i = 0; i != facts_count; ++i) {
+        int t;
+        int l;
+        int r;
+        cin >> t >> l >> r;
+    }
+}
