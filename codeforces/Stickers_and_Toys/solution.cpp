5b0fcc12a6c3174cee4a702049972e83e26d28fe
Ivan Krechetov <ikr@ikr.li>
2019-06-30 17:08:53 +0200

    V1 of A

---
+++ codeforces/Stickers_and_Toys/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int queries_count;
+    cin >> queries_count;
+
+    for (int i = 0; i != queries_count; ++i) {
+        int n;
+        int s;
+        int t;
+        cin >> n >> s >> t;
+
+        const int doubles = s + t - n;
+        const int ans = min(min(s, t), max(s, t) - doubles) + 1;
+        cout << ans << '\n';
+    }
+}
