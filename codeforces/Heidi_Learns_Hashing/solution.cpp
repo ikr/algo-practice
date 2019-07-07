1af9f984ca1bc7ae1f688547f41e72591582e4ca
Ivan Krechetov <ikr@ikr.su>
2019-07-07 10:21:10 +0200

    Get accepted

--- codeforces/Heidi_Learns_Hashing/solution.cpp
+++ codeforces/Heidi_Learns_Hashing/solution.cpp
@@ -2,29 +2,27 @@
 
 using namespace std;
 
-constexpr double real_y(const long r, const long x) {
-    return ((r - 1.0) / x - (x + 1.0)) / 2.0;
-}
+using ll_t = long long;
 
-constexpr optional<long> integer_y(const long r, const long x) {
+constexpr optional<ll_t> integer_y(const ll_t r, const ll_t x) {
     if ((r - 1L) % x) return nullopt;
-    const long twice_as_much = (r - 1L) / x - (x + 1L);
+    const ll_t twice_as_much = (r - 1L) / x - (x + 1L);
     if (twice_as_much % 2L) return nullopt;
     return {twice_as_much / 2L};
 }
 
 int main() {
-    long r;
+    ll_t r;
     cin >> r;
 
-    for (long x = 1L;; ++x) {
-        if (real_y(r, x) < 0.0) {
+    for (ll_t x = 1L;; ++x) {
+        if (x > sqrt(r - 1L)) {
             cout << "NO\n";
             break;
         }
 
         const auto y_value = integer_y(r, x);
-        if (!y_value || (*y_value) == 0L) continue;
+        if (!y_value || (*y_value) <= 0L) continue;
 
         cout << x << ' ' << (*y_value) << '\n';
         break;
