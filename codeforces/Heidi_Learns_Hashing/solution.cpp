bf867a830ed3d1f8a01d7bfda0c71d76a0e90f11
Ivan Krechetov <ikr@ikr.li>
2019-07-07 09:35:16 +0200

    V1 of A1

---
+++ codeforces/Heidi_Learns_Hashing/solution.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr double real_y(const long r, const long x) {
+    return ((r - 1.0) / x - (x + 1.0)) / 2.0;
+}
+
+constexpr optional<long> integer_y(const long r, const long x) {
+    if ((r - 1L) % x) return nullopt;
+    const long twice_as_much = (r - 1L) / x - (x + 1L);
+    if (twice_as_much % 2L) return nullopt;
+    return {twice_as_much / 2L};
+}
+
+int main() {
+    long r;
+    cin >> r;
+
+    for (long x = 1L;; ++x) {
+        if (real_y(r, x) < 0.0) {
+            cout << "NO\n";
+            break;
+        }
+
+        const auto y_value = integer_y(r, x);
+        if (!y_value) continue;
+
+        cout << x << ' ' << (*y_value) << '\n';
+        break;
+    }
+
+    return 0;
+}
