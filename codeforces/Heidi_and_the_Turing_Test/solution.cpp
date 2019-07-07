ec387fdb467d3332fb132e05f5565a9fc6f606cf
Ivan Krechetov <ikr@ikr.su>
2019-07-07 14:46:21 +0200

    Get it accepted

--- codeforces/Heidi_and_the_Turing_Test/solution.cpp
+++ codeforces/Heidi_and_the_Turing_Test/solution.cpp
@@ -4,22 +4,6 @@ using namespace std;
 
 using XY = pair<int, int>;
 
-optional<int> square_size(const XY p1, const XY p2, const XY p3, const XY p4) {
-    vector<XY> ps{p1, p2, p3, p4};
-
-    sort(ps.begin(), ps.end(),
-         [](const auto a, const auto b) { return a.first < b.first; });
-
-    const auto width = ps[3].first - ps[0].first;
-
-    sort(ps.begin(), ps.end(),
-         [](const auto a, const auto b) { return a.second < b.second; });
-
-    const auto height = ps[3].second - ps[0].second;
-
-    return (width == height) ? make_optional(width) : nullopt;
-}
-
 int main() {
     int min_count_per_side;
     cin >> min_count_per_side;
@@ -29,54 +13,39 @@ int main() {
     vector<XY> ps(sz);
     for (auto &p : ps) cin >> p.first >> p.second;
 
-    map<int, int> points_count_by_square_size;
-    multimap<XY, int> square_size_by_point;
+    map<int, int> points_count_by_x;
+    map<int, int> points_count_by_y;
 
-    for (auto i = 0; i != sz - 3; ++i) {
-        for (auto j = 1; j != sz - 2; ++j) {
-            for (auto k = 2; k != sz - 1; ++k) {
-                for (auto l = 3; l != sz; ++l) {
-                    const auto ssz = square_size(ps[i], ps[j], ps[k], ps[l]);
-
-                    if (ssz) {
-                        points_count_by_square_size[*ssz] += 4;
-
-                        for (const auto p : {ps[i], ps[j], ps[k], ps[l]}) {
-                            square_size_by_point.insert({p, *ssz});
-                        }
-                    }
-                }
-            }
-        }
+    for (const auto [x, y] : ps) {
+        ++points_count_by_x[x];
+        ++points_count_by_y[y];
     }
 
-    const auto actual_square_size = points_count_by_square_size.rbegin()->first;
+    int max_x = INT_MIN;
+    int min_x = INT_MAX;
 
-    for (const auto p : ps) {
-        map<int, int> own_count_by_square_size;
+    for (const auto [x, cnt] : points_count_by_x) {
+        if (cnt < 2) continue;
+        if (x > max_x) max_x = x;
+        if (x < min_x) min_x = x;
+    }
 
-        const auto [first, last] = square_size_by_point.equal_range(p);
+    int max_y = INT_MIN;
+    int min_y = INT_MAX;
 
-        for_each(first, last, [&own_count_by_square_size](const auto &el) {
-            const auto sz = el.second;
-            ++own_count_by_square_size[sz];
-        });
+    for (const auto [y, cnt] : points_count_by_y) {
+        if (cnt < 2) continue;
+        if (y > max_y) max_y = y;
+        if (y < min_y) min_y = y;
+    }
 
-        cout << "point " << p.first << ' ' << p.second << " has map of "
-             << own_count_by_square_size.size() << '\n';
+    for (const auto [x, y] : ps) {
+        const bool on_the_square =
+            ((x == min_x || x == max_x) && min_y <= y && y <= max_y) ||
+            ((y == min_y || y == max_y) && min_x <= x && x <= max_x);
 
-        cout << "entries are:\n";
-
-        for (const auto el : own_count_by_square_size) {
-            cout << el.first << ":" << el.second << " ";
-        }
-
-        cout << '\n';
-
-        if (own_count_by_square_size.empty() ||
-            !own_count_by_square_size
-            own_count_by_square_size.rbegin()->first != actual_square_size) {
-            cout << p.first << ' ' << p.second << '\n';
+        if (!on_the_square) {
+            cout << x << ' ' << y << '\n';
             break;
         }
     }
