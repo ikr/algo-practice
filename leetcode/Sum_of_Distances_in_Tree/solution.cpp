affcf2288f557474fd801ff732de834f7a6809a2
Ivan Krechetov <ikr@ikr.li>
2019-07-07 19:54:56 +0200

    WIP

--- leetcode/Sum_of_Distances_in_Tree/solution.cpp
+++ leetcode/Sum_of_Distances_in_Tree/solution.cpp
@@ -6,22 +6,29 @@ using namespace std;
 using Graph = unordered_multimap<int, int>;
 
 int edge_removed_partition_size(const Graph &g, const int remaining_v,
-                                const int departing_v) {
-    if (g.count(remaining_v) == 1) return 1;
+                                const int departing_v,
+                                vector<vector<int>> cache) {
+    if (cache[remaining_v][departing_v]) return cache[remaining_v][departing_v];
+
+    if (g.count(remaining_v) == 1) {
+        cache[remaining_v][departing_v] = 1;
+        return 1;
+    }
 
     int ans = 1;
     const auto [first, last] = g.equal_range(remaining_v);
     for (auto i = first; i != last; ++i) {
         const int adj = i->second;
         if (adj == departing_v) continue;
-        ans += edge_removed_partition_size(g, adj, remaining_v);
+        ans += edge_removed_partition_size(g, adj, remaining_v, cache);
     }
+
+    cache[remaining_v][departing_v] = ans;
     return ans;
 }
 
-template <size_t N>
 void dfs_distances(const Graph &g, unordered_set<int> &to_visit,
-                   const int d_so_far, const int v, array<int, N> &ans) {
+                   const int d_so_far, const int v, vector<int> &ans) {
     to_visit.erase(v);
     ans[v] = d_so_far;
 
@@ -34,9 +41,29 @@ void dfs_distances(const Graph &g, unordered_set<int> &to_visit,
     }
 }
 
+Graph make_graph(const vector<vector<int>> &edges) {
+    Graph ans;
+    for (const auto &edge : edges) {
+        ans.emplace(edge[0], edge[1]);
+        ans.emplace(edge[1], edge[0]);
+    }
+    return ans;
+}
+
 struct Solution final {
     vector<int> sumOfDistancesInTree(const int vertices_count,
-                                     const vector<vector<int>> &edges) {}
+                                     const vector<vector<int>> &edges) {
+        const auto g = make_graph(edges);
+        vector<vector<int>> erpsz(vertices_count);
+        fill(erpsz.begin(), erpsz.end(), vector<int>(vertices_count));
+
+        for (const auto &edge : edges) {
+            edge_removed_partition_size(g, edge[0], edge[1], erpsz);
+            edge_removed_partition_size(g, edge[1], edge[0], erpsz);
+        }
+
+        return vector<int>(vertices_count);
+    }
 };
 
 // clang-format off
