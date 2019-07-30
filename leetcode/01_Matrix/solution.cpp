288023fa1af666a1857fe11085f45831d1bcf225
Ivan Krechetov <ikr@ikr.su>
2019-07-30 16:14:58 +0200

    Implement the V1

--- leetcode/01_Matrix/solution.cpp
+++ leetcode/01_Matrix/solution.cpp
@@ -44,15 +44,60 @@ int min_dist_se(const VVec &rows, VVec &cache, const RowCol p) {
                       return min_dist_se(rows, cache, a);
                   });
 
-        cache[row][col] = accumulate(candidates.cbegin(), candidates.cend(),
-                                     INT_MAX, mmin<int>());
+        const auto min_neigh = accumulate(
+            candidates.cbegin(), candidates.cend(), INT_MAX, mmin<int>());
+        cache[row][col] = min_neigh == INT_MAX ? INT_MAX : min_neigh + 1;
+    }
+
+    return cache[row][col];
+}
+
+int min_dist_nw(const VVec &rows, VVec &cache, const RowCol p) {
+    const auto [row, col] = p;
+    if (cache[row][col] != INT_MAX) return cache[row][col];
+
+    if (!rows[row][col]) {
+        cache[row][col] = 0;
+    } else {
+        const auto adj = adjacent_se({rows.size(), rows[0].size()}, p);
+        Vec candidates(adj.size());
+        transform(adj.cbegin(), adj.cend(), candidates.begin(),
+                  [&rows, &cache](const RowCol a) {
+                      return min_dist_nw(rows, cache, a);
+                  });
+
+        const auto min_neigh = accumulate(
+            candidates.cbegin(), candidates.cend(), INT_MAX, mmin<int>());
+        cache[row][col] = min_neigh == INT_MAX ? INT_MAX : min_neigh + 1;
     }
 
     return cache[row][col];
 }
 
 struct Solution {
-    VVec updateMatrix(const VVec &rows) { return rows; }
+    VVec updateMatrix(const VVec &rows) {
+        if (rows.empty() || rows[0].empty()) return rows;
+
+        VVec ans(rows.size());
+
+        for (auto &ans_row : ans) {
+            ans_row = Vec(rows[0].size(), INT_MAX);
+        }
+
+        for (size_t i = 0; i != rows.size(); ++i) {
+            for (size_t j = 0; j != rows[i].size(); ++j) {
+                ans[i][j] = min_dist_se(rows, ans, {i, j});
+            }
+        }
+
+        for (size_t i = rows.size() - 1; i-- > 0;) {
+            for (size_t j = rows[i].size() - 1; j-- > 0;) {
+                ans[i][j] = min_dist_se(rows, ans, {i, j});
+            }
+        }
+
+        return ans;
+    }
 };
 
 // clang-format off
@@ -85,6 +130,11 @@ const lest::test tests[] = {
                 {1,2,1},
        };
 
+        EXPECT(actual == expected);
+    },
+    CASE("First submission failure") {
+        const auto actual = Solution().updateMatrix({{0,1,0,1,1},{1,1,0,0,1},{0,0,0,1,0},{1,0,1,1,1},{1,0,0,0,1}});
+        const VVec expected = {{0,1,0,1,2},{1,1,0,0,1},{0,0,0,1,0},{1,0,1,1,1},{1,0,0,0,1}};
         EXPECT(actual == expected);
     },
 };
