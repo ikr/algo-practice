454f228238714a2c6281ac1fd0fdb3277126819d
Ivan Krechetov <ikr@ikr.li>
2019-07-30 14:00:04 +0200

    WIP

--- leetcode/01_Matrix/solution.cpp
+++ leetcode/01_Matrix/solution.cpp
@@ -7,6 +7,50 @@ using Vec = vector<int>;
 using VVec = vector<Vec>;
 using RowCol = pair<int, int>;
 
+template <typename T> struct mmin {
+    constexpr T operator()(const T &a, const T &b) const {
+        return std::min(a, b);
+    }
+};
+
+vector<RowCol> adjacent_nw(const RowCol p) {
+    vector<RowCol> ans;
+    const auto [row, col] = p;
+    if (row > 0) ans.emplace_back(row - 1, col);
+    if (col > 0) ans.emplace_back(row, col - 1);
+    return ans;
+}
+
+vector<RowCol> adjacent_se(const RowCol sz, const RowCol p) {
+    vector<RowCol> ans;
+    const auto [h, w] = sz;
+    const auto [row, col] = p;
+    if (row < h - 1) ans.emplace_back(row + 1, col);
+    if (col < w - 1) ans.emplace_back(row, col + 1);
+    return ans;
+}
+
+int min_dist_se(const VVec &rows, VVec &cache, const RowCol p) {
+    const auto [row, col] = p;
+    if (cache[row][col] != INT_MAX) return cache[row][col];
+
+    if (!rows[row][col]) {
+        cache[row][col] = 0;
+    } else {
+        const auto adj = adjacent_nw(p);
+        Vec candidates(adj.size());
+        transform(adj.cbegin(), adj.cend(), candidates.begin(),
+                  [&rows, &cache](const RowCol a) {
+                      return min_dist_se(rows, cache, a);
+                  });
+
+        cache[row][col] = accumulate(candidates.cbegin(), candidates.cend(),
+                                     INT_MAX, mmin<int>());
+    }
+
+    return cache[row][col];
+}
+
 struct Solution {
     VVec updateMatrix(const VVec &rows) { return rows; }
 };
