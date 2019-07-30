f58fc9134f4410d2e5fad140efc08069d015fc41
Ivan Krechetov <ikr@ikr.su>
2019-07-30 18:22:24 +0200

    V1 for the C

--- codeforces/MP3/solution.cpp
+++ codeforces/MP3/solution.cpp
@@ -18,7 +18,37 @@ int main() {
         intensities.insert(a);
     }
 
+    unsigned int ans{0};
+
     const unsigned int target_size = 1 << I;
     while (intensities.size() > target_size) {
+        const auto lo = counts_by_intensity.cbegin()->first;
+        const auto hi = counts_by_intensity.crbegin()->first;
+
+        if (counts_by_intensity[lo] < counts_by_intensity[hi]) {
+            auto next_lo_it = counts_by_intensity.cbegin();
+            ++next_lo_it;
+            const auto next_lo = next_lo_it->first;
+            const auto capped_count = counts_by_intensity[lo];
+
+            counts_by_intensity.erase(lo);
+            intensities.erase(lo);
+
+            counts_by_intensity[next_lo] += capped_count;
+            ans += capped_count;
+        } else {
+            auto next_hi_it = counts_by_intensity.crbegin();
+            ++next_hi_it;
+            const auto next_hi = next_hi_it->first;
+            const auto capped_count = counts_by_intensity[hi];
+
+            counts_by_intensity.erase(hi);
+            intensities.erase(hi);
+
+            counts_by_intensity[next_hi] += capped_count;
+            ans += capped_count;
+        }
     }
+
+    cout << ans << '\n';
 }
