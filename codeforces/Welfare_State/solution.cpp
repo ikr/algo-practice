c555f114ee9cc6b1277fefa1a086b79801675b06
Ivan Krechetov <ikr@ikr.li>
2019-08-03 21:02:12 +0200

    Restart with a linear-time solution

--- codeforces/Welfare_State/solution.cpp
+++ codeforces/Welfare_State/solution.cpp
@@ -1,51 +1,37 @@
 #include <bits/stdc++.h>
+#include <variant>
 
 using namespace std;
 
 using ui_t = unsigned int;
-using BalPer = pair<ui_t, ui_t>;
-
 enum class EventType { RECEIPT = 1, PAYOFF = 2 };
 
-void handle_receipt(vector<ui_t> &balances_by_person,
-                    set<BalPer> &persons_by_balance, const BalPer receipt) {
-    const auto [new_balance, person] = receipt;
-    const ui_t old_balance = balances_by_person[person];
+struct Receipt {
+    ui_t pereson;
+    ui_t balance;
+};
 
-    balances_by_person[person] = new_balance;
+struct Payoff {
+    ui_t threshold;
+};
 
-    persons_by_balance.erase(BalPer{old_balance, person});
-    persons_by_balance.insert(receipt);
-}
-
-void handle_payoff(vector<ui_t> &balances_by_person,
-                   set<BalPer> &persons_by_balance, const ui_t threshold) {
-    vector<ui_t> persons_to_pay_off;
-    transform(persons_by_balance.cbegin(),
-              upper_bound(persons_by_balance.cbegin(),
-                          persons_by_balance.cend(), BalPer{threshold, 0U}),
-              back_inserter(persons_to_pay_off),
-              [](const auto bp) { return bp.second; });
-
-    for (const auto p : persons_to_pay_off) {
-        handle_receipt(balances_by_person, persons_by_balance, {threshold, p});
-    }
-}
+using Event = std::variant<monostate, Receipt, Payoff>;
 
 int main() {
     ui_t n;
     cin >> n;
 
     vector<ui_t> balances_by_person(n + 1, 0U);
-    set<BalPer> persons_by_balance;
+    unordered_map<ui_t, ui_t> balances_by_person_after_payoff;
 
     for (ui_t i = 1; i <= n; ++i) {
         cin >> balances_by_person[i];
-        persons_by_balance.insert(BalPer{balances_by_person[i], i});
     }
 
     ui_t events_count;
     cin >> events_count;
+
+    vector<Event> events(events_count);
 
     for (ui_t i = 0; i != events_count; ++i) {
         ui_t event_type;
@@ -53,16 +39,15 @@ int main() {
 
         if (event_type == static_cast<ui_t>(EventType::RECEIPT)) {
             ui_t person;
-            ui_t new_balance;
-            cin >> person >> new_balance;
+            ui_t balance;
+            cin >> person >> balance;
 
-            handle_receipt(balances_by_person, persons_by_balance,
-                           {new_balance, person});
+            events[i] = Receipt{person, balance};
         } else {
             ui_t threshold;
             cin >> threshold;
 
-            handle_payoff(balances_by_person, persons_by_balance, threshold);
+            events[i] = Payoff{threshold};
         }
     }
 
