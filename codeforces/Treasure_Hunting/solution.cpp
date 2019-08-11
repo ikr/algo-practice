64a24ad71f8e33384ffcf4e924102d345cac7acb
Ivan Krechetov <ikr@ikr.su>
2019-08-11 10:08:45 +0200

    Implement move_to_closest

--- codeforces/Treasure_Hunting/solution.cpp
+++ codeforces/Treasure_Hunting/solution.cpp
@@ -2,6 +2,23 @@
 
 using namespace std;
 
+using Col = int;
+using StepsCol = pair<int, Col>;
+
+StepsCol move_to_closest(const StepsCol current, const set<Col> &dest) {
+    const auto [steps, col] = current;
+    if (dest.empty() || dest.count(col)) return current;
+
+    const auto it = upper_bound(cbegin(dest), cend(dest), col);
+    if (it == cbegin(dest)) return {abs(col - *cbegin(dest)), *cbegin(dest)};
+    if (it == cend(dest)) return {abs(col - *crbegin(dest)), *crbegin(dest)};
+
+    const Col a = *prev(it);
+    const Col b = *it;
+
+    return min(make_pair(abs(a - col), a), make_pair(abs(b - col), b));
+}
+
 int main() {
     int n;
     int m;
@@ -9,7 +26,7 @@ int main() {
     int q;
     cin >> n >> m >> k >> q;
 
-    vector<set<int>> treasure_column_sets_by_row(n);
+    vector<set<Col>> treasure_column_sets_by_row(n);
     for (int i = 0; i != k; ++i) {
         int r1;
         int c1;
@@ -17,7 +34,7 @@ int main() {
         treasure_column_sets_by_row[r1 - 1].insert(c1 - 1);
     }
 
-    set<int> exit_columns;
+    set<Col> exit_columns;
     for (int i = 0; i != q; ++i) {
         int c1;
         cin >> c1;
