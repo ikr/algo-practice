79390877b69dbf8e89a81a0950f86108868492e9
Ivan Krechetov <ikr@ikr.li>
2020-02-22 11:21:31 +0100

    Implement V1

--- leetcode/Longest_String_Chain/solution.cpp
+++ leetcode/Longest_String_Chain/solution.cpp
@@ -14,6 +14,68 @@ unordered_map<T, size_t> reverse_mapping(const vector<T> &xs) {
 
     return ans;
 }
+
+vector<string> word_reductions(const string &w) {
+    vector<string> ans;
+    if (w.empty()) return ans;
+
+    for (auto i = 0U; i != w.size(); ++i) {
+        ans.push_back(w.substr(0, i) + w.substr(i + 1));
+    }
+
+    return ans;
+}
+
+vector<vector<size_t>>
+make_graph(const vector<string> &words,
+           const unordered_map<string, size_t> &indices_by_word) {
+    vector<vector<size_t>> ans(words.size(), vector<size_t>{});
+
+    for (auto i = 0U; i != words.size(); ++i) {
+        for (const auto &src : word_reductions(words[i])) {
+            if (indices_by_word.count(src)) {
+                ans[indices_by_word.at(src)].push_back(i);
+            }
+        }
+    }
+
+    return ans;
+}
+
+template <typename T> struct mmax {
+    constexpr T operator()(const T &a, const T &b) const {
+        return std::max(a, b);
+    }
+};
+
+int chain_size(const vector<vector<size_t>> &adjacent, vector<int> &memo,
+               const size_t index) {
+    if (memo[index] >= 0) return memo[index];
+
+    if (adjacent[index].empty()) {
+        memo[index] = 1;
+    } else {
+        memo[index] =
+            1 + transform_reduce(adjacent[index].cbegin(),
+                                 adjacent[index].cend(), INT_MIN, mmax<int>{},
+                                 [&adjacent, &memo](const auto i) {
+                                     return chain_size(adjacent, memo, i);
+                                 });
+    }
+
+    return memo[index];
+}
+
+int max_chain_size(const size_t words_count,
+                   const vector<vector<size_t>> &adjacent) {
+    vector<int> ans(words_count, -1);
+
+    for (auto i = 0U; i != ans.size(); ++i) {
+        ans[i] = chain_size(adjacent, ans, i);
+    }
+
+    return reduce(ans.cbegin(), ans.cend(), INT_MIN, mmax<int>{});
+}
 } // namespace
 
 struct Solution final {
@@ -22,9 +84,12 @@ struct Solution final {
             return w1.size() < w2.size();
         });
 
-        unordered_map<string, size_t> indices_by_word = reverse_mapping(words);
+        words.erase(unique(words.begin(), words.end()), words.end());
 
-        return -1;
+        auto indices_by_word = reverse_mapping(words);
+        auto adjacent = make_graph(words, indices_by_word);
+
+        return max_chain_size(words.size(), adjacent);
     }
 };
 
