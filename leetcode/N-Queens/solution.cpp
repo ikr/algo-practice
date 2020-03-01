920a93ae493fc7120cb342c5ac391e942c303487
Ivan Krechetov <ikr@ikr.su>
2020-03-01 20:30:57 +0100

    Implement the SolutionPart struct

--- leetcode/N-Queens/solution.cpp
+++ leetcode/N-Queens/solution.cpp
@@ -3,8 +3,72 @@
 
 using namespace std;
 
+namespace {
+using Board = vector<string>;
+using RowCol = pair<int, int>;
+
+struct SolutionPart final {
+    explicit SolutionPart(const int board_size)
+        : m_board_size{board_size}, m_queens{}, m_columns(board_size, false),
+          m_slash_diagonals(2 * (board_size - 1), false),
+          m_backslash_diagonals(2 * (board_size - 1), false) {}
+
+    bool place_queen(const RowCol sq) {
+        assert(m_queens.empty() || sq.first == m_queens.back().first + 1);
+
+        if (m_columns[sq.second] || m_slash_diagonals[slash_index(sq)] ||
+            m_backslash_diagonals[backslash_index(sq)]) {
+            return false;
+        }
+
+        m_queens.push_back(sq);
+        m_columns[sq.second] = true;
+        m_slash_diagonals[slash_index(sq)] = true;
+        m_backslash_diagonals[backslash_index(sq)] = true;
+    }
+
+    void remove_last_queen() {
+        assert(!m_queens.empty());
+
+        const auto sq = m_queens.back();
+
+        m_queens.pop_back();
+        m_columns[sq.second] = false;
+        m_slash_diagonals[slash_index(sq)] = false;
+        m_backslash_diagonals[backslash_index(sq)] = false;
+    }
+
+    Board capture() const {
+        Board ans(m_board_size, string(m_board_size, '.'));
+
+        for (const auto [r, c] : m_queens) {
+            ans[r][c] = 'Q';
+        }
+
+        return ans;
+    }
+
+  private:
+    int slash_index(const RowCol sq) const {
+        const auto [r, c] = sq;
+        return 2 * (m_board_size - 1) - (r + c);
+    }
+
+    int backslash_index(const RowCol sq) const {
+        const auto [r, c] = sq;
+        return r - c + (m_board_size - 1);
+    }
+
+    const int m_board_size;
+    vector<RowCol> m_queens;
+    vector<bool> m_columns;
+    vector<bool> m_slash_diagonals;
+    vector<bool> m_backslash_diagonals;
+};
+} // namespace
+
 struct Solution final {
-    vector<vector<string>> solveNQueens(int n) const { return {}; }
+    vector<Board> solveNQueens(const int n) const { return {}; }
 };
 
 // clang-format off
