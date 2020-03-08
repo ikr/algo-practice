565a9d3e4b67754cc37d26c8ad2940d466d7e6c4
Ivan Krechetov <ikr@ikr.li>
2020-03-08 08:08:46 +0100

    LeetCode Bi-Wee contest 21 submissions

---
+++ leetcode/Find_the_Longest_Substring_Containing_Vowels_in_Even_Counts/solution.cpp
@@ -0,0 +1,72 @@
+#include "lest.hpp"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Solution final {
+    int findTheLongestSubstring(const string &s) const {
+        int ans = 0;
+
+        for (auto b = 0U; b != s.size(); ++b) {
+            bool a = true;
+            bool e = true;
+            bool i = true;
+            bool o = true;
+            bool u = true;
+
+            for (auto j = b; j != s.size(); ++j) {
+                switch (s[j]) {
+                case 'a':
+                    a = !a;
+                    break;
+
+                case 'e':
+                    e = !e;
+                    break;
+
+                case 'i':
+                    i = !i;
+                    break;
+
+                case 'o':
+                    o = !o;
+                    break;
+
+                case 'u':
+                    u = !u;
+                    break;
+                }
+
+                if (a && e && i && o && u) {
+                    if (static_cast<int>(j - b + 1) > ans) {
+                        ans = j - b + 1;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+// clang-format off
+const lest::test tests[] = {
+    CASE("Ex 1") {
+        const auto actual = Solution{}.findTheLongestSubstring("eleetminicoworoep");
+        const auto expected = 13;
+        EXPECT(actual == expected);
+    },
+    CASE("Ex 2") {
+        const auto actual = Solution{}.findTheLongestSubstring("leetcodeisgreat");
+        const auto expected = 5;
+        EXPECT(actual == expected);
+    },
+    CASE("Ex 3") {
+        const auto actual = Solution{}.findTheLongestSubstring("bcbcbc");
+        const auto expected = 6;
+        EXPECT(actual == expected);
+    },
+};
+// clang-format on
+
+int main(int argc, char **argv) { return lest::run(tests, argc, argv); }
---
+++ leetcode/Increasing_Decreasing_String/solution.cpp
@@ -0,0 +1,89 @@
+#include "lest.hpp"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+namespace {
+void prune(map<char, int> &m) {
+    // m.erase(
+    //     remove_if(m.begin(), m.end(), [](auto it) { return it.second == 0;
+    //     }), m.end());
+
+    for (auto it = m.begin(); it != m.end();) {
+        if (it->second == 0) {
+            it = m.erase(it);
+        } else {
+            ++it;
+        }
+    }
+}
+} // namespace
+
+struct Solution final {
+    string sortString(const string &s) const {
+        map<char, int> counts_by_char;
+
+        for (const char c : s) {
+            ++counts_by_char[c];
+        }
+
+        string ans;
+
+        while (!counts_by_char.empty()) {
+            for (auto it = counts_by_char.begin(); it != counts_by_char.end();
+                 ++it) {
+                ans += it->first;
+                --it->second;
+            }
+
+            prune(counts_by_char);
+
+            for (auto it = counts_by_char.rbegin(); it != counts_by_char.rend();
+                 ++it) {
+                ans += it->first;
+                --it->second;
+            }
+
+            prune(counts_by_char);
+        }
+
+        return ans;
+    }
+};
+
+// clang-format off
+const lest::test tests[] = {
+    CASE("Ex 1") {
+        const auto actual = Solution{}.sortString("aaaabbbbcccc");
+        const auto expected = "abccbaabccba";
+        EXPECT(actual == expected);
+    },
+    CASE("Ex 2") {
+        const auto actual = Solution{}.sortString("rat");
+        const auto expected = "art";
+        EXPECT(actual == expected);
+    },
+    CASE("Ex 3") {
+        const auto actual = Solution{}.sortString("leetcode");
+        const auto expected = "cdelotee";
+        EXPECT(actual == expected);
+    },
+    CASE("Ex 4") {
+        const auto actual = Solution{}.sortString("ggggggg");
+        const auto expected = "ggggggg";
+        EXPECT(actual == expected);
+    },
+    CASE("Ex 5") {
+        const auto actual = Solution{}.sortString("spo");
+        const auto expected = "ops";
+        EXPECT(actual == expected);
+    },
+    CASE("Empty") {
+        const auto actual = Solution{}.sortString("");
+        const auto expected = "";
+        EXPECT(actual == expected);
+    },
+};
+// clang-format on
+
+int main(int argc, char **argv) { return lest::run(tests, argc, argv); }
