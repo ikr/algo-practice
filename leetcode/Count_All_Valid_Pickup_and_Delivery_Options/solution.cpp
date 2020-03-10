7a0441cbbbbc6df4f1f4fc86c1ceacc85a59306b
Ivan Krechetov <ikr@ikr.su>
2020-03-10 19:10:51 +0100

    Do the modulo math

--- leetcode/Count_All_Valid_Pickup_and_Delivery_Options/solution.cpp
+++ leetcode/Count_All_Valid_Pickup_and_Delivery_Options/solution.cpp
@@ -5,14 +5,19 @@ using namespace std;
 
 namespace {
 using Int = long long;
+
+static const Int P = 1000LL * 1000LL * 1000LL + 7LL;
+int intprj(const Int x) { return static_cast<int>(x % P); }
+
+Int count_recur(const Int n) {
+    if (n == 1LL) return 1LL;
+    const Int m = (2LL * (n - 1LL) + 1LL) % P;
+    return (count_recur(n - 1LL) * (((m + m * m) / 2LL) % P)) % P;
 }
+} // namespace
 
 struct Solution final {
-    int countOrders(const int n) const {
-        if (n == 1) return 1;
-        const int m = 2 * (n - 1) + 1;
-        return countOrders(n - 1) * (m + m * m) / 2;
-    }
+    int countOrders(const int n) const { return intprj(count_recur(n)); }
 };
 
 // clang-format off
@@ -35,4 +40,7 @@ const lest::test tests[] = {
 };
 // clang-format on
 
-int main(int argc, char **argv) { return lest::run(tests, argc, argv); }
+int main(int argc, char **argv) {
+    cout << Solution{}.countOrders(500) << '\n';
+    return lest::run(tests, argc, argv);
+}
