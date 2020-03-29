8c280ccf1898ec510ba9f00f9d4608bb4b4cf424
Ivan Krechetov <ikr@ikr.li>
2020-03-29 16:06:29 +0200

    AC

--- atcoder/Interactive_Sorting/solution.cpp
+++ atcoder/Interactive_Sorting/solution.cpp
@@ -2,30 +2,44 @@
 
 using namespace std;
 
+namespace {
+bool cmp(const char c1, const char c2) {
+    cout << "? " << c1 << ' ' << c2 << endl;
+
+    char res;
+    cin >> res;
+
+    return res == '<';
+}
+
+void head_triple_insert(string &y, const char c) {
+    if (cmp(c, y[1])) {
+        if (cmp(c, y[0]))
+            y.insert(0, 1, c);
+        else
+            y.insert(1, 1, c);
+    } else {
+        if (cmp(c, y[2]))
+            y.insert(2, 1, c);
+        else
+            y.insert(3, 1, c);
+    }
+}
+
+void merge_sort(string &y) {
+    if (y.size() < 2) return;
+
+    string one(y.cbegin(), y.cbegin() + y.size() / 2);
+    merge_sort(one);
+
+    string two(y.cbegin() + y.size() / 2, y.cend());
+    merge_sort(two);
+
+    merge(one.cbegin(), one.cend(), two.cbegin(), two.cend(), y.begin(), cmp);
+}
+} // namespace
+
 int main() {
-    const auto cmp = [](const char c1, const char c2) {
-        cout << "? " << c1 << ' ' << c2 << endl;
-
-        char res;
-        cin >> res;
-
-        return res == '<';
-    };
-
-    const auto head_triple_insert = [cmp](string &y, const char c) {
-        if (cmp(c, y[1])) {
-            if (cmp(c, y[0]))
-                y.insert(0, 1, c);
-            else
-                y.insert(1, 1, c);
-        } else {
-            if (cmp(c, y[2]))
-                y.insert(2, 1, c);
-            else
-                y.insert(3, 1, c);
-        }
-    };
-
     int N, Q;
     cin >> N >> Q;
 
@@ -49,7 +63,7 @@ int main() {
 
         s = y;
     } else {
-        stable_sort(s.begin(), s.end(), cmp);
+        merge_sort(s);
     }
 
     cout << "! " << s << endl;
