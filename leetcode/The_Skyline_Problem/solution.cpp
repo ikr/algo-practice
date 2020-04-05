2411ae4690dc0415d89c103ac6d2984630908582
Ivan Krechetov <ikr@ikr.su>
2020-04-05 20:14:53 +0200

    Back to square one

--- leetcode/The_Skyline_Problem/solution.cpp
+++ leetcode/The_Skyline_Problem/solution.cpp
@@ -2,26 +2,6 @@
 
 using namespace std;
 
-using Iter = map<int, int>::iterator;
-
-void histo_insert_building(map<int, int> &histo, const int left, const int right, const int height) {
-    if (histo.empty()) {
-        histo.emplace(left, heigth);
-        histo.emplace(right, height);
-        return;
-    }
-    
-    const int front = *histo.begin();
-    const int back = *histo.rbegin();
-    if (back < left || front > right) return empty;
-    
-    if (front <= left && right <= back) {
-        auto lo = hist.lower_bound(left);
-        auto hi = hist.lower_bound(right);
-        return {*lo > left ? prev(lo) : lo, hi};
-    }
-}
-
 struct Solution final {
     vector<vector<int>> getSkyline(const vector<vector<int>> &buildings) const {
         map<int, int> histo;
@@ -36,12 +16,16 @@ struct Solution final {
     }
 };
 
-int main()
-{
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
     const auto actual = Solution{}.getSkyline(
         vector<vector<int>>{{2, 9, 10}, {3, 7, 15}, {5, 12, 12}, {15, 20, 10}, {19, 24, 8}});
                                                                   
     const vector<vector<int>> expected{{2, 10}, {3, 15}, {7, 12}, {12, 0}, {15, 10}, {20, 8}, {24, 0}};
                                                                   
     assert(actual == expected);
+    
+    return 0;
 }
