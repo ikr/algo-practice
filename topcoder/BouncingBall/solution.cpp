ed4a4c1934f7c32340e4645de3ab1b498d2d97c7
Ivan Krechetov <ikr@ikr.su>
2020-04-13 14:13:28 +0200

    Finalize

--- topcoder/BouncingBall/solution.cpp
+++ topcoder/BouncingBall/solution.cpp
@@ -2,16 +2,18 @@
 
 using namespace std;
 
+static const double EPSILON = 0.000000001;
+
 double falling_duration(const double G, const double h0) {
     return sqrt(2.0 * h0 / G);
 }
 
 double falling_position(const double G, const double h0, const double dt) {
-    return G * dt * dt / 2.0;
+    return h0 - G * dt * dt / 2.0;
 }
 
 double rebound_velocity(const double v, const double energy_lost_percent) {
-    return sqrt(v * v * (1.0 - energy_lost_percent / 100.0))
+    return sqrt(v * v * (1.0 - energy_lost_percent / 100.0));
 }
 
 double rebound_duration(const double G, const double v) {
@@ -23,7 +25,37 @@ double rebound_position(const double G, const double v, const double dt) {
 }
 
 struct BouncingBall final {
-     double getPosition(int g, int h, int p, int t) const {
+     double getPosition(const int g, const int h, const int p, const int t) const {
          const double G = g / 100.0;
+         double T = t;
+         double y = h;
+         double v = 0.0;
+         bool rebounding = false;
+
+        while (abs(T) > EPSILON) {
+            const double next_stage_duration = 
+                rebounding ? rebound_duration(G, v) : falling_duration(G, y);
+
+            if (T <= next_stage_duration) {
+                y = rebounding ? rebound_position(G, v, T) : falling_position(G, y, T);
+                break;
+            }
+
+            if (rebounding) {
+                const double dt = rebound_duration(G, v);
+                T -= dt;
+                y = rebound_position(G, v, dt);
+                v = 0.0;
+            } else {
+                const double dt = falling_duration(G, y);
+                T -= dt;
+                v = rebound_velocity(G * dt, p);
+                y = 0.0;
+            }
+
+            rebounding = !rebounding;
+        }
+
+         return y;
      }
 };
