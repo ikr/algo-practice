b016077f815716b3c1c4c6c566bd108485457fb8
Ivan Krechetov <ikr@ikr.su>
2020-04-17 19:15:12 +0200

    AC

--- atcoder/RGB_Triplets/solution.cpp
+++ atcoder/RGB_Triplets/solution.cpp
@@ -2,23 +2,13 @@
 
 using namespace std;
 
-template <typename K, typename V>
-ostream &operator<<(ostream &os, const multimap<K, V> &m) {
-    for (auto i = m.cbegin(); i != m.cend(); ++i) {
-        if (i != m.cbegin()) os << ' ';
-        os << '{' << i->first << ' ' << i->second << '}';
-    }
-
-    return os;
-}
-
 using Int = long long;
 
-multimap<char, Int> gather_indices_by_color(const string &s) {
-    multimap<char, Int> ans;
+map<char, set<Int>> gather_indices_by_color(const string &s) {
+    map<char, set<Int>> ans{{'R', {}}, {'G', {}}, {'B', {}}};
 
     for (Int i = 0; i != s.size(); ++i) {
-        ans.emplace(s[i], i);
+        ans[s[i]].insert(i);
     }
 
     return ans;
@@ -35,31 +25,41 @@ vector<tuple<char, char, char>> all_triples() {
     return ans;
 }
 
-Int count_isooffsets(const multimap<char, Int> &indices_by_color) {
+Int count_isooffsets(const map<char, set<Int>> &indices_by_color) {
     Int ans = 0;
 
     for (const auto [c1, c2, c3] : all_triples()) {
+        for (auto i : indices_by_color.at(c1)) {
+            for (
+                auto itk = indices_by_color.at(c3).upper_bound(i); 
+                itk != indices_by_color.at(c3).cend();
+                ++itk
+            ) {
+                const auto k = *itk;
+                if ((k - i) % 2) continue;
+                if (indices_by_color.at(c2).count(i + (k - i) / 2)) ++ans;
+            }
+        } 
     }
 
     return ans;
 }
 
 int main() {
-    assert(all_triples().size() == 6);
-
     Int n;
     string s;
     cin >> n >> s;
 
-    auto indices_by_color = gather_indices_by_color(s);
+    const auto indices_by_color = gather_indices_by_color(s);
 
-    cout << indices_by_color << endl;
-    cout << indices_by_color.count('R') * indices_by_color.count('G') * indices_by_color.count('B') << endl;
-    cout << count_isooffsets(indices_by_color) << endl;
+    const Int ans = accumulate(
+        indices_by_color.cbegin(), 
+        indices_by_color.cend(), 
+        1LL, 
+        [](const Int memo, const auto p) { return memo * p.second.size(); }
+    ) - count_isooffsets(indices_by_color);
 
-    const Int ans = 
-        indices_by_color.count('R') * indices_by_color.count('G') * indices_by_color.count('B')
-            - count_isooffsets(indices_by_color);
-    
     cout << ans << endl;
+
+    return 0;
 }
