ba0a0ce4e76f14673900f76a2ef498732da628b0
Ivan Krechetov <ikr@ikr.li>
2020-04-24 16:45:19 +0200

    WIP

--- atcoder/Active_Infants/solution.cpp
+++ atcoder/Active_Infants/solution.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+using ll = long long;
 
-template <typename T1, typename T2> ostream &operator<<(ostream &os, const pair<T1, T2> &x) {
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &x) {
     os << '(' << x.first << ' ' << x.second << ')';
     return os;
 }
@@ -19,48 +21,41 @@ template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
     return os;
 }
 
-
-using Int = long long;
-
 int main() {
-    int n;
-    cin >> n;
-    
-    vector<int> xs(n, 0);
-    for (auto &x : xs) {
-        cin >> x;
+    ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+    int sz;
+    cin >> sz;
+
+    vector<ll> acts(sz, 0);
+    vector<pair<ll, int>> act_idx_pairs;
+    for (int i = 0; i != sz; ++i) {
+        ll act;
+        cin >> act;
+
+        acts[i] = act;
+        act_idx_pairs.emplace_back(act, i);
     }
+
+    sort(act_idx_pairs.begin(), act_idx_pairs.end(), greater<pair<ll, int>>{});
+    cout << "pairs " << act_idx_pairs << endl;
+
+    // [filled at left side][filled at right side]
+    vector<vector<ll>> dp(sz + 1, vector<ll>(sz + 1, 0));
     
-    vector<pair<int, pair<int, int>>> win_pair_heap;
-    
-    for (int i = 0; i < n - 1; ++i) {
-        for (int j = i + 1; j < n; ++j) {
-            const int win = (j - i) * (xs[i] + xs[j]);
-            win_pair_heap.push_back({win, {i, j}});
-            push_heap(win_pair_heap.begin(), win_pair_heap.end());
-        }
-    }
-    
-    cout << win_pair_heap << endl;
-    
-    unordered_set<int> used_idc;
-    Int ans = 0;
-    
-    while (!win_pair_heap.empty() && used_idc.size() < n) {
-        const auto [win, pr] = win_pair_heap.front();
-        const auto [i, j] = pr;
+    for (int i = 1; 2 * i - 1 <= sz; ++i) {
+        const auto [act, idx] = act_idx_pairs[i - 1];
+
+        cout << "pair #" << (i - 1) << endl;
+        cout << "L ACTS, DIFF " << (act + acts[i - 1]) << ", " << abs(idx - i + 1) << endl;
+        dp[i][i - 1] = (act + acts[i - 1]) * abs(idx - i + 1) + dp[i - 1][i - 1];
         
-        if (!used_idc.count(i) && !used_idc.count(j)) { 
-            ans += win;
-            used_idc.insert(i);
-            used_idc.insert(j);
-        }
-        
-        pop_heap(win_pair_heap.begin(), win_pair_heap.end());
-        win_pair_heap.pop_back();
+        cout << "R ACTS, DIFF " << (act + acts[sz - i]) << ", " << abs(idx - sz + i + 2) << endl;
+        dp[i - 1][i] = (act + acts[sz - i]) * abs(idx - sz + i + 2) + dp[i - 1][i - 1];
     }
-    
-    cout << ans << endl;
-    
+
+    cout << dp << endl;
+
     return 0;
 }
