aa4c1a70f52fdc79d3ae4a78ba63ee2db0ccc611
Ivan Krechetov <ikr@ikr.su>
2020-04-26 12:26:39 +0200

    AC

--- leetcode/Longest_Common_Subsequence/solution.cpp
+++ leetcode/Longest_Common_Subsequence/solution.cpp
@@ -1,60 +1,45 @@
-vector<string> adjacent(string s) {
-    vector<string> ans;
-    
-    for (auto i = 0U; i < s.size(); ++i) {
-        const char c = s[i];
-        s.erase(i, 1);
-        ans.push_back(s);
-        s.insert(i, 1, c);
-    }
-        
-    return ans;
-}
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
 
-string pop(queue<string> &q) {
-    assert(!q.empty());
-    string ans = q.front();
-    q.pop();
-    return ans;
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+
+    os << ']';
+    return os;
 }
 
 struct Solution final {
-    int longestCommonSubsequence(string s1, string s2) {
-        if (s1.empty() || s2.empty()) return 0;
-        if (s1 == s2) return s1.size();
+    int longestCommonSubsequence(const string &s1, const string &s2) {
+        const int sz1 = s1.size();
+        const int sz2 = s2.size();
         
-        unordered_set<string> discovered1{s1};
-        queue<string> q1;
-        q1.push(s1);
+        vector<vector<int>> dp(sz1, vector<int>(sz2, 0));
         
-        unordered_set<string> discovered2{s2};
-        queue<string> q2;
-        q2.push(s2);
-        
-        while(!q1.empty() || !q2.empty()) {
-            if (!q1.empty()) {
-                auto u1 = pop(q1);
-                
-                for (const auto v1 : adjacent(u1)) {
-                    if (discovered1.count(v1)) continue;
-                    if (discovered2.count(v1)) return v1.size();
-                    discovered1.insert(v1);
-                    q1.push(v1);
-                }
-            }
-
-            if (!q2.empty()) {
-                auto u2 = pop(q2);
-                
-                for (const auto v2 : adjacent(u2)) {
-                    if (discovered2.count(v2)) continue;
-                    if (discovered1.count(v2)) return v2.size();
-                    discovered2.insert(v2);
-                    q2.push(v2);
+        for (int i = 0; i != sz1; ++i) {
+            for (int j = 0; j != sz2; ++j) {
+                if (s1[i] == s2[j]) {
+                    dp[i][j] = 1 + (i > 0 && j > 0 ? dp[i - 1][j - 1] : 0);
+                } else {
+                    dp[i][j] = max(
+                        i > 0 ? dp[i - 1][j] : 0,
+                        j > 0 ? dp[i][j - 1] : 0
+                    );                    
                 }
             }
         }
         
-        return 0;
+        int ans = 0;
+        
+        for (int i = 0; i != sz1; ++i) {
+            ans = max(ans, dp[i][sz2 - 1]);
+        }
+        
+        for (int j = 0; j != sz2; ++j) {
+            ans = max(ans, dp[sz1 - 1][j]);
+        }
+        
+        return ans;
     }
 };
