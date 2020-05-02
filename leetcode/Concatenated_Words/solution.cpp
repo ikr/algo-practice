a52d8f5efee578bde8886c605a854e5699fdf3cd
Ivan Krechetov <ikr@ikr.su>
2020-05-02 09:55:02 +0200

    AC

--- leetcode/Concatenated_Words/solution.cpp
+++ leetcode/Concatenated_Words/solution.cpp
@@ -2,14 +2,39 @@
 
 using namespace std;
 
-bool is_compound_word(const unordered_set<string> &dict, const string &w) {
-    const int sz = w.size();
-    
-    for (int l = 1; l <= sz; ++l) {
-        if (dict.count(w.substr(0, l))) return l == sz ? true : is_compound_word(dict, w.substr(l));
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
     }
 
-    return false;
+    os << ']';
+    return os;
+}
+
+bool is_compound_word(const unordered_set<string> &dict, const string &w) {
+    if (w.empty()) return false;
+    const int sz = w.size();
+    
+    // is words concatenation ending [at index i]
+    vector<bool> dp(sz, false);
+
+    for (int i = 0; i != sz; ++i) {
+        for (int j = i; j >= -1; --j) {
+            if (j == -1) {
+                dp[i] = dict.count(w.substr(0, i + 1)); 
+            } else {
+                if (dp[j] && dict.count(w.substr(j + 1, i - j))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    return dp[sz - 1];
 }
 
 struct Solution final {
@@ -28,15 +53,31 @@ struct Solution final {
     }
 };
 
+static const vector<pair<vector<string>, vector<string>>> TCS{
+    {
+        {"cat","cats","catsdogcats","dog","dogcatsdog","hippopotamuses","rat","ratcatdogcat"},
+        {"catsdogcats","dogcatsdog","ratcatdogcat"}
+    },
+
+    {
+        {""},
+        {}
+    }
+};
+
 int main() {
     ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-    const vector<string> input{"cat","cats","catsdogcats","dog","dogcatsdog","hippopotamuses","rat","ratcatdogcat"};
-    const vector<string> expected{"catsdogcats","dogcatsdog","ratcatdogcat"};
-    const auto actual = Solution{}.findAllConcatenatedWordsInADict(input);
+    for (const auto [input, expected] : TCS) {
+        auto actual = Solution{}.findAllConcatenatedWordsInADict(input);
+        sort(actual.begin(), actual.end());
 
-    assert(actual == expected);
+        cout << "EXP " << expected << '\n';
+        cout << "ACT " << actual << '\n' << endl;
+
+        assert(actual == expected);
+    }
 
     return 0;
 }
