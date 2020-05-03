b0af4cba100a699ef525c69e97f2529be1fef63e
Ivan Krechetov <ikr@ikr.li>
2020-05-03 11:14:37 +0200

    AC

--- atcoder/Longest_Path/solution.cpp
+++ atcoder/Longest_Path/solution.cpp
@@ -2,24 +2,38 @@
 
 using namespace std;
 
-int dfs(vector<int> &memo, const unordered_multimap<int, int> &G, const int source) {
-    const auto range = G.equal_range(source); 
-
+void sort_vertices_dfs(
+    const unordered_multimap<int, int> &G, 
+    vector<int> &ans,
+    unordered_set<int> &discovered,
+    const int source
+) {
+    const auto range = G.equal_range(source);
     for (auto it = range.first; it != range.second; ++it) {
-        memo[source] = max(memo[source], 1 + dfs(memo, G, it->second));
-    }
+        if (discovered.count(it->second)) continue;
 
-    return memo[source];
+        discovered.insert(it->second);
+        sort_vertices_dfs(G, ans, discovered, it->second);
+        ans.insert(ans.cbegin(), it->second);
+    }
 }
 
 int solve(const int N, const unordered_multimap<int, int> &G) {
-    vector<int> memo(N + 1, 0);
+    vector<int> ordering;
+    unordered_set<int> discovered{0};
+    sort_vertices_dfs(G, ordering, discovered, 0);
+    ordering.insert(ordering.cbegin(), 0);
 
-    for (int i = 1; i <= N; ++i) {
-        dfs(memo, G, i);
+    vector<int> dp(N + 1, 0);
+
+    for (auto it = ordering.crbegin(); it != ordering.crend(); ++it) {
+        const auto range = G.equal_range(*it);
+        for (auto jt = range.first; jt != range.second; ++jt) {
+            dp[*it] = max(dp[*it], dp[jt->second] + 1);
+        }
     }
 
-    return *max_element(memo.cbegin(), memo.cend());
+    return *max_element(dp.cbegin(), dp.cend()) - 1;
 }
 
 int main() {
@@ -30,6 +44,10 @@ int main() {
     cin >> N >> M;
 
     unordered_multimap<int, int> G;
+    for (int i = 1; i <= N; ++i) {
+        G.emplace(0, i);
+    }
+
     for (int i = 0; i != M; ++i) {
         int x, y;
         cin >> x >> y;
