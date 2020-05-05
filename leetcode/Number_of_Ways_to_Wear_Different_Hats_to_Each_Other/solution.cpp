6cc26eee3b0948556e76da1e5b5c8b64e83b2dbb
Ivan Krechetov <ikr@ikr.su>
2020-05-05 12:02:37 +0200

    WIP

--- leetcode/Number_of_Ways_to_Wear_Different_Hats_to_Each_Other/solution.cpp
+++ leetcode/Number_of_Ways_to_Wear_Different_Hats_to_Each_Other/solution.cpp
@@ -7,8 +7,20 @@ using vll = vector<ll>;
 
 static const ll M = 1e9 + 7;
 static const ui MAX_HAT = 40;
-static const ui MAX_PERSON = 9;
-static const ui MAX_PERSON_COMBO = (1 << 10) - 1;
+static const ui MAX_PERSON = 4;
+static const ui MAX_PEOPLE_BITS = (1 << 5) - 1;
+
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+
+    os << ']';
+    return os;
+}
 
 vui people_bits_by_hat(const vector<vi> &hats_by_person) {
     const ui sz = hats_by_person.size();
@@ -22,37 +34,31 @@ vui people_bits_by_hat(const vector<vi> &hats_by_person) {
     return ans;
 }
 
-ll people_bits_popcount(const ui people_bits) {
-    return bitset<MAX_PERSON + 1>(people_bits).count();
-}
-
-ll max_overall(vector<vll> xss) {
-    ll ans = 0;
-    
-    for (const auto &xs : xss) {
-        ans = max(ans, *max_element(xs.cbegin(), xs.cend()));
-    }
-    
-    return ans;
+ui people_bits_compliment(const ui people_bits) {
+    return ~people_bits & MAX_PEOPLE_BITS;
 }
 
 struct Solution final {
     int numberWays(const vector<vi> &hats_by_person) const {
         const auto pbbh = people_bits_by_hat(hats_by_person);
         
-        // answer [up to hat i] [with people bits combo j already assigned]
-        vector<vll> dp(MAX_HAT + 1, vll(MAX_PERSON_COMBO + 1, 0));
+        // answer [up to hat i] [with people bits combo j allowed]
+        vector<vll> dp(MAX_HAT + 1, vll(MAX_PEOPLE_BITS + 1, 0LL));
         
-        for (ui j = 1; j <= MAX_PERSON_COMBO; ++j) {
-            dp[1][j] = people_bits_popcount(pbbh[1]);
-        }
-        
-        for (ui i = 2; i <= MAX_HAT; ++i) {
-            for (ui j = 1; j <= MAX_PERSON_COMBO; ++j) {
-                dp[i][j] = 
+        for (ui i = 1; i <= MAX_HAT; ++i) {
+            for (ui j = 1; j <= MAX_PEOPLE_BITS; ++j) {
+                const ll own_count = __builtin_popcount(pbbh[i]);
+                const ui compatible_combo = people_bits_compliment(pbbh[i]);
+                const ll inherited_count = dp[i - 1][compatible_combo];
+                
+                dp[i][j] = inherited_count ? ((inherited_count * own_count) % M) : own_count;
             }
         }
         
-        return max_overall(dp);
+        for (const auto &row : dp) {
+            cout << row << '\n';
+        }
+        
+        return 0;
     }
 };
