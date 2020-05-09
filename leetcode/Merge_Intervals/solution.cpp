41de62b4cf2f29bfc99eb291d26f9e52c9b4eaf0
Ivan Krechetov <ikr@ikr.su>
2020-05-09 14:28:02 +0200

    AC

--- leetcode/Merge_Intervals/solution.cpp
+++ leetcode/Merge_Intervals/solution.cpp
@@ -5,28 +5,28 @@ bool disjoint(const vi &ivl1, const vi &ivl2) {
     return ivl2[1] < ivl1[0] || ivl1[1] < ivl2[0];
 }
 
-vi merge(const vi &ivl1, const vi &ivl2) {
+vi merge_ivls(const vi &ivl1, const vi &ivl2) {
     vi both = ivl1;
     both.insert(both.cend(), ivl2.cbegin(), ivl2.cend());
     sort(both.begin(), both.end());
     return {both.front(), both.back()};
 }
 
-void adjacent_merge_right(vector<vi> &ivls, const Iter it) {
-    while (it + 1 != ivls.end() && !disjoint(*it, *(it + 1))) {
-        *it = merge(*it, *(it + 1));
-        ivls.erase(it + 1);
-    }
-}
-
 struct Solution final {
     vector<vi> merge(vector<vi> &ivls) {
+        if (ivls.size() < 2) return ivls;
+        
         sort(ivls.begin(), ivls.end(), [](const vi &lhs, const vi &rhs) { return lhs[0] < rhs[0]; });
+        vector<vi> ans{ivls.front()};
         
-        for (auto it = ivls.begin(); it != ivls.end(); ++it) {
-            adjacent_merge_right(ivls, it);
-        }        
+        for (auto i = 1U; i != ivls.size(); ++i) {
+            if (!disjoint(ivls[i], ans.back())) {
+                ans.back() = merge_ivls(ivls[i], ans.back());
+            } else {
+                ans.push_back(ivls[i]);
+            }
+        }
         
-        return ivls;
+        return ans;
     }
 };
