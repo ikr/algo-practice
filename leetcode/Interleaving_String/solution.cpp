b259e0a07f3198c4b7e35e38711bb104c3243b51
Ivan Krechetov <ikr@ikr.li>
2020-05-26 18:19:44 +0200

    WA
    "db"
    "b"
    "cbb"

--- leetcode/Interleaving_String/solution.cpp
+++ leetcode/Interleaving_String/solution.cpp
@@ -29,9 +29,11 @@ struct Solution final {
         for (int i = 1; i <= s1.size(); ++i) {
             for (int j = 1; j <= s2.size(); ++j) {
                 const int k = i + j - 1;
-                if (s1[i - 1] == t[k]) dp[i][j] = dp[i - 1][j];
-                else if (s2[j - 1] == t[k]) dp[i][j] = dp[i][j - 1];
-                else dp[i][j] = false;
+                
+                dp[i][j] = (
+                    (dp[i - 1][j] && s1[i - 1] == t[k]) ||
+                    (dp[i][j - 1] && s2[j - 1] == t[k])
+                );
             }
         }
         
