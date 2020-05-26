4d8430a69a7a708a26110914947f27efa0a6445f
Ivan Krechetov <ikr@ikr.li>
2020-05-26 11:43:04 +0200

    AC

--- leetcode/Scramble_String/solution.cpp
+++ leetcode/Scramble_String/solution.cpp
@@ -2,43 +2,43 @@ using uss = unordered_set<string>;
 using umss = unordered_map<string, uss>;
 using vi = vector<int>;
 
+template <typename T1, typename T2> struct PairHash final {
+    size_t operator()(const pair<T1, T2> &p) const {
+        return 31 * hash<T1>{}(p.first) + hash<T2>{}(p.second);
+    }
+};
+
 vi letters_count(const string &s) {
     vi ans(CHAR_MAX, 0);
     for (const char c : s) ++ans[c];
     return ans;
 }
 
-uss scrambles(umss &memo, const string &s) {
-    if (!memo.count(s)) {
-        const int sz = s.size();
+struct Solution final {
+    bool isScramble(const string &s, const string &t) {
+        pair<string, string> key{s, t};
+        if (memo.count(key)) return memo.at(key);
         
-        if (s.empty()) memo[s] = uss{};
-        else if (sz == 1) memo[s] = uss{s};
+        if (letters_count(s) != letters_count(t)) memo[key] = false;
+        else if (s == t) memo[key] = true;
         else {
-            memo[s] = uss{s};
-            
+            const int sz = s.size();
+            bool ok = false;
             for (int i = 1; i < sz; ++i) {
-                const auto aset = scrambles(memo, s.substr(0, i));
-                const auto bset = scrambles(memo, s.substr(i));
-                
-                for (const auto &a : aset) {
-                    for (const auto &b : bset) {
-                        memo[s].insert(a + b);
-                        memo[s].insert(b + a);
-                    }
+                if (
+                    (isScramble(s.substr(0, i), t.substr(0, i)) && isScramble(s.substr(i), t.substr(i))) ||
+                    (isScramble(s.substr(0, i), t.substr(sz - i)) && isScramble(s.substr(i), t.substr(0, sz - i)))
+                ) {
+                    ok = true;
+                    break;
                 }
             }
+            memo[key] = ok;
         }
+        
+        return memo.at(key);
     }
     
-    return memo.at(s);
-}
-
-struct Solution final {
-    bool isScramble(const string &s, const string &t) const {
-        if (letters_count(s) != letters_count(t)) return false;
-        umss memo;
-        const auto sset = scrambles(memo, s);
-        return sset.count(t);
-    }
+private:
+    unordered_map<pair<string, string>, bool, PairHash<string, string>> memo;
 };
