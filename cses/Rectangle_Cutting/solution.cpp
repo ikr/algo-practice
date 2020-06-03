69bb9120f1b2412719ebe95345ecbe1b074c3f56
Ivan Krechetov <ikr@ikr.li>
2020-06-03 19:47:11 +0200

    AC

--- cses/Rectangle_Cutting/solution.cpp
+++ cses/Rectangle_Cutting/solution.cpp
@@ -4,34 +4,6 @@ using vi = vector<int>;
 using vvi = vector<vi>;
 static constexpr int INF = 1e9 + 10;
 
-
-template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
-    os << '[';
-    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
-        if (i != xs.cbegin()) os << ' ';
-        os << *i;
-    }
-    os << ']';
-    return os;
-}
-
-template <typename T> ostream &operator<<(ostream &os, const vector<vector<T>> &xss) {
-    for (const auto xs : xss) os << xs << '\n';
-    return os;
-}
-
-constexpr int inf_aware_add(const int x, const int y) {
-    if (x == INF || y == INF) {
-        cout << "Got one\n";
-        return INF;
-    }
-    return x + y;
-}
-
-constexpr int inf_aware_add(const int x, const int y, const int z) {
-    return inf_aware_add(inf_aware_add(x, y), z);
-}
-
 int min_cuts(const int a, const int b) {
     const int sz = max(a, b);
     vvi dp(sz + 1, vi(sz + 1, INF));
@@ -44,11 +16,11 @@ int min_cuts(const int a, const int b) {
             }
             
             for (int ik = 1; ik < i; ++ik) {
-                dp[i][j] = min(dp[i][j], inf_aware_add(dp[ik][j], dp[i - ik][j], 1));
+                dp[i][j] = min(dp[i][j], dp[ik][j] + dp[i - ik][j] + 1);
             }
             
             for (int jk = 1; jk < j; ++jk) {
-                dp[i][j] = min(dp[i][j], inf_aware_add(dp[i][jk], dp[i][j - jk], 1)); 
+                dp[i][j] = min(dp[i][j], dp[i][jk] + dp[i][j - jk] + 1); 
             }
             
             dp[j][i] = dp[i][j];
