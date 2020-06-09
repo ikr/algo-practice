16442511ba16ace5c03cc1b2867ee1f825789ecd
Ivan Krechetov <ikr@ikr.li>
2020-06-09 07:54:57 +0200

    AC

--- leetcode/Best Time_to_Buy_and_Sell_Stock_III/solution.cpp
+++ leetcode/Best Time_to_Buy_and_Sell_Stock_III/solution.cpp
@@ -1,13 +1,26 @@
 using vi = vector<int>;
 
-int max_profit(const vi & xs, const int first, const int last) {
-    if (first >= last - 1) return 0;
-    int lo = xs[first];
-    int ans = max(0, xs[first + 1] - xs[first]);
+vi max_profits_from_all_points(const vi &xs) {
+    const int sz = xs.size();
+    int hi = xs[sz - 1];
+    vi ans(sz, 0);
     
-    for (int i = first + 1; i < last; ++i) {
-        ans = max(ans, xs[i] - lo);
-        if (xs[i] < lo) lo = xs[i];
+    for (int i = sz - 2; i >= 0; --i) {
+        ans[i] = max(ans[i + 1], hi - xs[i]);
+        hi = max(hi, xs[i]);
+    }
+    
+    return ans;
+}
+
+vi max_profits_till_all_points(const vi &xs) {
+    const int sz = xs.size();
+    int lo = xs[0];
+    vi ans(sz, 0);
+    
+    for (int i = 1; i <= sz - 1; ++i) {
+        ans[i] = max(ans[i - 1], xs[i] - lo);
+        lo = min(lo, xs[i]);
     }
     
     return ans;
@@ -17,12 +30,13 @@ struct Solution final {
     int maxProfit(const vi &xs) const {
         if(xs.empty()) return 0;
         const int sz = xs.size();
-
+        
+        const auto best_from = max_profits_from_all_points(xs);
+        const auto best_till = max_profits_till_all_points(xs);
+        
         int ans = 0;
-        for (int mid = 0; mid <= sz; ++mid) {
-            const int left = max_profit(xs, 0, mid);
-            const int right = max_profit(xs, mid, sz);
-            ans = max(ans, left + right);
+        for (int i = 0; i != sz - 1; ++i) {
+            ans = max(ans, (i > 0 ? best_till[i - 1] : 0) + best_from[i]);
         }
         return ans;
     }
