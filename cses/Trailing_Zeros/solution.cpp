bbfb2da44e4fff8847efbb65083162f213524d47
Ivan Krechetov <ikr@ikr.su>
2020-06-15 21:02:50 +0200

    AC

--- cses/Trailing_Zeros/solution.cpp
+++ cses/Trailing_Zeros/solution.cpp
@@ -1,20 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-using vi = vector<int>;
 using ll = long long;
+using vi = vector<int>;
 
-int fac_fives(int n) {
+int exp(const int base, const int power) {
+    if (!power) return 1;
+    if (power % 2) return base * exp(base, power - 1);
+
+    const int sq = exp(base, power / 2);
+    return sq * sq;
+}
+
+int max_5_exp_up_to(const int n) {
     int ans = 0;
-    while (n && n % 5 == 0) {
+    ll prod = 1;
+    while (prod * 5 <= n) {
         ++ans;
-        n /= 5;
+        prod *= 5;
     }
     return ans;
 }
 
 int zeros(const int n) {
-    if (n <= 20) return n / 5;
-    return fac_fives(n) + zeros(n - 1);
+    const int me = max_5_exp_up_to(n);
+    vi count_by_power(me + 1, 0);
+    for (int i = me; i > 0; --i) {
+        int e = exp(5, i);
+        count_by_power[i] += (n / e);
+    }
+    return accumulate(count_by_power.cbegin(), count_by_power.cend(), 0);
 }
 
 int main() {
