93ca753027a7325ee4b09f77733a990dfd2906f8
Ivan Krechetov <ikr@ikr.li>
2020-06-21 15:53:14 +0200

    AC

--- atcoder/Replacing/solution.cpp
+++ atcoder/Replacing/solution.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 using ll = long long;
 
+unordered_map<ll, int> count_values(const vector<ll> &xs) {
+    unordered_map<ll, int> ans;
+    for (const auto &x : xs) ++ans[x];
+    return ans;
+}
+
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
@@ -12,10 +18,7 @@ int main() {
     for (auto &x : xs) cin >> x;
     ll s = accumulate(xs.cbegin(), xs.cend(), 0LL);
 
-    unordered_map<ll, vector<int>> indices_by_value;
-    for (int i = 0; i < sz; ++i) {
-        indices_by_value[xs[i]].push_back(i);
-    }
+    auto cnt = count_values(xs);
 
     int q;
     cin >> q;
@@ -23,14 +26,10 @@ int main() {
     while (q--) {
         int b, c;
         cin >> b >> c;
-        int d = b - c;
-
-        for (auto i : indices_by_value[b]) {
-            s -= d;
-            xs[i] = c;
-            indices_by_value[c].push_back(i);
-        }
-        indices_by_value[b] = {};
+        const int d = b - c;
+        s -= cnt[b] * d;
+        cnt[c] += cnt[b];
+        cnt[b] = 0;
 
         cout << s << '\n';
     }
