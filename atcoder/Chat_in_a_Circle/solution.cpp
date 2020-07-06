d6d901381851a849d6cacede98b95ad97ce381ff
Ivan Krechetov <ikr@ikr.li>
2020-07-06 16:28:12 +0200

    Up-solving a day after: still a WA

--- atcoder/Chat_in_a_Circle/solution.cpp
+++ atcoder/Chat_in_a_Circle/solution.cpp
@@ -1,23 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
+using umi = unordered_map<int, int>;
 using vi = vector<int>;
+using qi = deque<int>;
 using ll = long long;
 
-ll max_total_comfort(vi xs) {
-    sort(xs.begin(), xs.end(), greater<int>{});
-    return accumulate(xs.cbegin(), xs.cend() - 1, 0LL);
+void dec_tail(umi &counts, qi &distinct_xs) {
+    const int x = distinct_xs.back();
+    --counts[x];
+
+    if (!counts[x]) {
+        counts.erase(x);
+        distinct_xs.pop_back();
+    }
+}
+
+ll max_total_comfort(umi &counts, qi distinct_xs) {
+    ll ans = 0;
+
+    int hi = 0;
+    while (!distinct_xs.empty()) {
+        const int x = distinct_xs.front();
+        distinct_xs.pop_front();
+
+        ans += hi;
+        ans += x * (counts[x] - 1);
+
+        int holes = counts[x] - 1;
+        while (holes && !distinct_xs.empty()) {
+            ans += x;
+            dec_tail(counts, distinct_xs);
+            --holes;
+        }
+
+        hi = x;
+    }
+
+    return ans;
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    int t;
-    cin >> t;
-    vi xs(t, 0);
-    for (auto &x : xs) cin >> x;
+    int sz;
+    cin >> sz;
+    umi counts;
+    vi xs(sz, 0);
+    for (auto &x : xs) {
+        cin >> x;
+        ++counts[x];
+    }
 
-    cout << max_total_comfort(xs) << '\n';
+    sort(xs.begin(), xs.end(), greater<int>{});
+    xs.erase(unique(xs.begin(), xs.end()), xs.end());
+
+    cout << max_total_comfort(counts, qi(xs.cbegin(), xs.cend())) << '\n';
 
     return 0;
 }
