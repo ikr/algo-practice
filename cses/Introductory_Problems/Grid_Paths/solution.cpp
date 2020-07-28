b28dd51fde7cf490b8a9233d9afd9e3b2c97271e
Ivan Krechetov <ikr@ikr.su>
2020-07-28 21:04:50 +0200

    Bootstrap

---
+++ cses/Introductory_Problems/Grid_Paths/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int matching_paths_count(const string &ppattern) { return -1; }
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    string s;
+    cin >> s;
+
+    return 0;
+}
