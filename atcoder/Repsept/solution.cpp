a57544a634c0a37b4f3260676baf14bdc31a4571
Ivan Krechetov <ikr@ikr.su>
2020-08-02 21:28:26 +0200

    AC

--- atcoder/Repsept/solution.cpp
+++ atcoder/Repsept/solution.cpp
@@ -1,18 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
-
-int ten_exp_mod(const int power, const int m) {
-    if (!power) return 1;
-    if (power % 2) return (10 * ten_exp_mod(power - 1, m)) % m;
-
-    const ll sq = ten_exp_mod(power / 2, m);
-    return (sq * sq) % m;
-}
 
 int solve(const int K) {
-    for (int i = 1; i < K; ++i) {
-        if (ten_exp_mod(i, K) == 1) return i;
+    for (int i = 1, sevens = 7 % K; i <= K;
+         ++i, sevens = (sevens * 10 + 7) % K) {
+        if (sevens == 0) return i;
     }
 
     return -1;
