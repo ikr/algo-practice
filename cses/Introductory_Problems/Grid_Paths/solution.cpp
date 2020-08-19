9f98ab1662d2d8791a37f1c00d44e2b73a42b0f8
Ivan Krechetov <ikr@ikr.su>
2020-08-19 16:56:26 +0200

    Make it pass

--- cses/Introductory_Problems/Grid_Paths/solution.cpp
+++ cses/Introductory_Problems/Grid_Paths/solution.cpp
@@ -4,29 +4,6 @@ using namespace std;
 static constexpr int RO_MAX = 6;
 static constexpr int CO_MAX = 6;
 static constexpr int DEST_I = (RO_MAX + 1) * (CO_MAX + 1) - 1;
-static const string ALL_DIRS{"UDLR"};
-
-static constexpr pair<int, int> UP{-1, 0};
-static constexpr pair<int, int> DOWN{1, 0};
-static constexpr pair<int, int> LEFT{0, -1};
-static constexpr pair<int, int> RIGHT{0, 1};
-static constexpr pair<int, int> ZERO{0, 0};
-
-constexpr const pair<int, int> &delta(const char dir) {
-    switch (dir) {
-    case 'U':
-        return UP;
-    case 'D':
-        return DOWN;
-    case 'L':
-        return LEFT;
-    case 'R':
-        return RIGHT;
-    default:
-        assert(false && "Invalid direction char");
-        return ZERO;
-    }
-}
 
 constexpr bool in_bounds(const int ro, const int co) {
     return ro >= 0 && ro <= RO_MAX && co >= 0 && co <= CO_MAX;
@@ -44,35 +21,56 @@ void recur(const string &pattern, int &ans, vector<vector<bool>> &covered,
         return;
     }
 
-    for (const auto dir :
-         pattern[i] == '?' ? ALL_DIRS : string(1, pattern[i])) {
-        const auto [delta_ro, delta_co] = delta(dir);
-        const int ro_prime = ro + delta_ro;
-        const int co_prime = co + delta_co;
+    if ((pattern[i] == '?' || pattern[i] == 'U') &&
+        is_possible(covered, ro - 1, co)) {
+        bool locked = !is_possible(covered, ro - 2, co) &&
+                      is_possible(covered, ro - 1, co - 1) &&
+                      is_possible(covered, ro - 1, co + 1);
 
-        if (!is_possible(covered, ro_prime, co_prime)) continue;
-
-        if (!is_possible(covered, ro_prime + delta_ro, co_prime + delta_co)) {
-            switch (dir) {
-            case 'U':
-            case 'D':
-                if (is_possible(covered, ro_prime, co_prime - 1) &&
-                    is_possible(covered, ro_prime, co_prime + 1))
-                    continue;
-                break;
-
-            case 'L':
-            case 'R':
-                if (is_possible(covered, ro_prime - 1, co_prime) &&
-                    is_possible(covered, ro_prime + 1, co_prime))
-                    continue;
-                break;
-            }
+        if (!locked) {
+            covered[ro - 1][co] = true;
+            recur(pattern, ans, covered, i + 1, ro - 1, co);
+            covered[ro - 1][co] = false;
         }
+    }
 
-        covered[ro_prime][co_prime] = true;
-        recur(pattern, ans, covered, i + 1, ro_prime, co_prime);
-        covered[ro_prime][co_prime] = false;
+    if ((pattern[i] == '?' || pattern[i] == 'D') &&
+        is_possible(covered, ro + 1, co)) {
+        bool locked = !is_possible(covered, ro + 2, co) &&
+                      is_possible(covered, ro + 1, co - 1) &&
+                      is_possible(covered, ro + 1, co + 1);
+
+        if (!locked) {
+            covered[ro + 1][co] = true;
+            recur(pattern, ans, covered, i + 1, ro + 1, co);
+            covered[ro + 1][co] = false;
+        }
+    }
+
+    if ((pattern[i] == '?' || pattern[i] == 'L') &&
+        is_possible(covered, ro, co - 1)) {
+        bool locked = !is_possible(covered, ro, co - 2) &&
+                      is_possible(covered, ro - 1, co - 1) &&
+                      is_possible(covered, ro + 1, co - 1);
+
+        if (!locked) {
+            covered[ro][co - 1] = true;
+            recur(pattern, ans, covered, i + 1, ro, co - 1);
+            covered[ro][co - 1] = false;
+        }
+    }
+
+    if ((pattern[i] == '?' || pattern[i] == 'R') &&
+        is_possible(covered, ro, co + 1)) { // R
+        bool locked = !is_possible(covered, ro, co + 2) &&
+                      is_possible(covered, ro - 1, co + 1) &&
+                      is_possible(covered, ro + 1, co + 1);
+
+        if (!locked) {
+            covered[ro][co + 1] = true;
+            recur(pattern, ans, covered, i + 1, ro, co + 1);
+            covered[ro][co + 1] = false;
+        }
     }
 }
 
