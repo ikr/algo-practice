b466dc237fe12152fd1e619f090d357bd0b2e3a4
Ivan Krechetov <ikr@ikr.su>
2020-08-21 09:54:13 +0200

    Bootstrap

---
+++ atcoder/Sushi/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double expected_operations_count(const vector<int> &xs) {
+    // TODO
+    return 0.0;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int sz = 0;
+    cin >> sz;
+    vector<int> xs(sz, 0);
+    for (auto &x : xs) cin >> x;
+
+    return 0;
+}
