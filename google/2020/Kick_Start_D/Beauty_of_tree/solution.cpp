c56bd5c73397775f33bb258142e2dc6d8b2e103b
Ivan Krechetov <ikr@ikr.li>
2020-08-26 18:24:58 +0200

    Get to AC on the test set 1

--- google/2020/Kick_Start_D/Beauty_of_tree/solution.cpp
+++ google/2020/Kick_Start_D/Beauty_of_tree/solution.cpp
@@ -1,52 +1,46 @@
-#include <algorithm>
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
-    os << '[';
-    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
-        if (i != xs.cbegin()) os << ' ';
-        os << *i;
-    }
-    os << ']';
-    return os;
-}
-
-unordered_multimap<int, int> gather_children(const vector<int> &parent) {
-    const int N = parent.size() - 1;
-    unordered_multimap<int, int> ans;
-    for (int i = 2; i <= N; ++i) {
-        ans.emplace(parent[i], i);
-    }
-    return ans;
-}
-
-vector<int>
-gather_counts_by_level(const int N,
-                       const unordered_multimap<int, int> &children) {
-    vector<int> ans(N, 0);
-    ans[0] = 1;
-
-    function<void(int, int)> recur;
-    recur = [&ans, &children, &recur](const int level, const int node) {
-        const auto fl = children.equal_range(node);
-        for (auto it = fl.first; it != fl.second; ++it) {
-            ++ans[level + 1];
-            recur(level + 1, it->second);
+int tree_beauty(const vector<int> &parent, const int step_a, const int step_b,
+                const int start_a, const int start_b) {
+    const auto step_up = [&parent](int x, int step) -> int {
+        while (x > 0 && step > 0) {
+            x = parent[x];
+            --step;
         }
+
+        return x;
     };
 
-    recur(0, 1);
-    return ans;
+    unordered_set<int> colored;
+
+    const auto color_up = [&parent, &colored, &step_up](const int start,
+                                                        const int step) {
+        int x = start;
+
+        do {
+            colored.insert(x);
+            x = step_up(x, step);
+        } while (x > 0);
+    };
+
+    color_up(start_a, step_a);
+    color_up(start_b, step_b);
+
+    return colored.size();
 }
 
-double solve(const vector<int> &parent, const int A, const int B) {
-    const auto counts_by_level =
-        gather_counts_by_level(parent.size() - 1, gather_children(parent));
+double solve(const vector<int> &parent, const int step_a, const int step_b) {
+    const int N = parent.size() - 1;
+    int s = 0;
 
-    cout << counts_by_level << '\n';
+    for (int i = 1; i <= N; ++i) {
+        for (int j = 1; j <= N; ++j) {
+            s += tree_beauty(parent, step_a, step_b, i, j);
+        }
+    }
 
-    return -1.009;
+    return static_cast<double>(s) / N / N;
 }
 
 int main() {
