409205c6d823a9e7bc075d21acad83c646467abf
Ivan Krechetov <ikr@ikr.li>
2020-09-08 18:11:37 +0200

    Bootstrap

---
+++ codeforces/Two_Platforms/solution.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int max_saved(const int k, const vector<int> &xs) {
+    return -1;
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> xs(n, 0);
+        for (auto &x : xs) cin >> x;
+
+        for (int i = 0; i < n; ++i) {
+            int y;
+            cin >> y;
+        }
+    }
+
+    return 0;
+}
