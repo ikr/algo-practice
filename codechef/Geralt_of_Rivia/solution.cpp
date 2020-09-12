59f5b9d49b10f00926fa7387eeae209d034a2a3e
Ivan Krechetov <ikr@ikr.li>
2020-09-12 14:55:46 +0200

    Found out we can use chars instead of strings

--- codechef/Geralt_of_Rivia/solution.cpp
+++ codechef/Geralt_of_Rivia/solution.cpp
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-using Graph = unordered_multimap<int, int>;
+using Graph = unordered_multimap<char, char>;
+constexpr int SZ = CHAR_MAX + 1;
 
-int max_coins(const int n, const int s, Graph &g) {
-    vector<bool> discovered(n, false);
+int max_coins(const char s, Graph &g) {
+    vector<bool> discovered(SZ, false);
     int ans = 0;
     function<void(int)> recur;
 
-    recur = [&](const int u) {
+    recur = [&](const char u) {
         discovered[u] = true;
         ans += g.count(u);
 
@@ -23,24 +24,6 @@ int max_coins(const int n, const int s, Graph &g) {
     return ans;
 }
 
-unordered_map<string, int> build_index(vector<string> cities) {
-    const int n = cities.size();
-    sort(begin(cities), end(cities));
-    unordered_map<string, int> ans;
-    for (int i = 0; i < n; ++i) ans[cities[i]] = i;
-    return ans;
-}
-
-vector<string> unique_cities(const vector<pair<string, string>> &edges,
-                             const string &origin) {
-    unordered_set<string> uq{origin};
-    for (const auto &[c1, c2] : edges) {
-        uq.insert(c1);
-        uq.insert(c2);
-    }
-    return vector<string>(cbegin(uq), cend(uq));
-}
-
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
@@ -51,23 +34,17 @@ int main() {
         int n;
         cin >> n;
 
-        string c;
+        char c;
         cin >> c;
 
-        vector<pair<string, string>> edges;
-        for (int i = 0; i < n; ++i) {
-            string a, b;
-            cin >> a >> b;
-            edges.emplace_back(a, b);
-        }
-        sort(begin(edges), end(edges));
-
-        const auto cities = unique_cities(edges, c);
-        const auto idx = build_index(cities);
-
         Graph g;
-        for (const auto &[a, b] : edges) g.emplace(idx.at(a), idx.at(b));
-        cout << max_coins(cities.size(), idx.at(c), g) << '\n';
+        for (int i = 0; i < n; ++i) {
+            char a, b;
+            cin >> a >> b;
+            g.emplace(a, b);
+        }
+
+        cout << max_coins(c, g) << '\n';
     }
 
     return 0;
