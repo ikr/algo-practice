f507096cf0f3a1210a94cf34dec6eccb3eb3208e
Ivan Krechetov <ikr@ikr.li>
2020-09-13 21:09:04 +0200

    AC

--- atcoder/Redistribution/solution.cpp
+++ atcoder/Redistribution/solution.cpp
@@ -1,11 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
-constexpr ll M = 1e9 + 7;
-constexpr ll S_MAX = 2000;
+constexpr int M = 1e9 + 7;
+
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
 
 int solve(const int s) {
-    vector<vector<int>>(S_MAX + 1, vector<int>(S_MAX + 1, 0));
+    if (s < 3) return 0;
+
+    // number of sequences [whose sum is i]
+    vector<int> dp(s + 1, 0);
+    dp[0] = 1;
+    dp[3] = 1;
+
+    for (int i = 4; i <= s; ++i) {
+        for (int j = 3; j <= s; ++j) {
+            if (i - j >= 0) {
+                dp[i] += dp[i - j];
+                dp[i] %= M;
+            }
+        }
+    }
+
+    return dp[s];
 }
 
 int main() {
