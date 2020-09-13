0c76b83301732fb765a30a5b4ff88f03505b2667
Ivan Krechetov <ikr@ikr.li>
2020-09-13 11:37:22 +0200

    AC

--- codeforces/Two_Platforms/solution.cpp
+++ codeforces/Two_Platforms/solution.cpp
@@ -1,40 +1,114 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int rhs_count(const int k, const vector<int> &xs, const int i) {
+int lhs_count(const int k, const vector<int> &xs, const int i) {
     const int n = xs.size();
     assert(i >= 0);
     assert(i < n);
+
+    const auto is_covered = [&](const int j) {
+        assert(j >= 0);
+        assert(j < n);
+        assert(j <= i);
+        return xs[i] - k <= xs[j];
+    };
+
+    if (is_covered(0)) return i + 1;
+
     int lo = 1;
-    int hi = n - i;
+    int hi = i + 1;
 
     while (lo < hi) {
+        assert(is_covered(i - lo + 1));
+        if (lo + 1 == hi) return is_covered(i - hi + 1) ? hi : lo;
+
         const int mid = lo + (hi - lo) / 2;
-        assert(i + mid - 1 >= 0);
-        assert(i + mid - 1 < n);
+        // cout << "lo:" << lo << " mid:" << mid << " hi:" << hi << endl;
 
-        if (xs[i + mid - 1] > xs[i] + k)
-            hi = mid - 1;
+        if (is_covered(i - mid + 1))
+            lo = mid;
         else
-            lo = mid + 1;
+            hi = mid - 1;
 
-        cout << "lo:" << lo << " mid:" << mid << " hi:" << hi << endl;
+        // cout << "lo:" << lo << " mid:" << mid << " hi:" << hi << endl;
     }
 
     return lo;
 }
 
+int rhs_count(const int k, const vector<int> &xs, const int i) {
+    const int n = xs.size();
+    assert(i >= 0);
+    assert(i < n);
+
+    const auto is_covered = [&](const int j) {
+        assert(j >= 0);
+        assert(j < n);
+        assert(i <= j);
+        return xs[i] + k >= xs[j];
+    };
+
+    if (is_covered(n - 1)) return n - i;
+
+    int lo = 1;
+    int hi = n - i;
+
+    while (lo < hi) {
+        assert(is_covered(i + lo - 1));
+        if (lo + 1 == hi) return is_covered(i + hi - 1) ? hi : lo;
+
+        const int mid = lo + (hi - lo) / 2;
+        // cout << "lo:" << lo << " mid:" << mid << " hi:" << hi << endl;
+
+        if (is_covered(i + mid - 1))
+            lo = mid;
+        else
+            hi = mid - 1;
+
+        // cout << "lo:" << lo << " mid:" << mid << " hi:" << hi << endl;
+    }
+
+    return lo;
+}
+
+vector<int> gather_pref(const int k, const vector<int> &xs) {
+    const int n = xs.size();
+    vector<int> ans(n, 1);
+    for (int i = 1; i < n; ++i) ans[i] = max(ans[i - 1], lhs_count(k, xs, i));
+    return ans;
+}
+
+vector<int> gather_suff(const int k, const vector<int> &xs) {
+    const int n = xs.size();
+    vector<int> ans(n, 1);
+    for (int i = n - 2; i >= 0; --i)
+        ans[i] = max(ans[i + 1], rhs_count(k, xs, i));
+    return ans;
+}
+
 int max_saved(const int k, const vector<int> &xs) {
     const int n = xs.size();
-    return n;
+    const auto pref = gather_pref(k, xs);
+    const auto suff = gather_suff(k, xs);
+    int ans = suff[0];
+    for (int i = 1; i < n; ++i) ans = max(ans, pref[i - 1] + suff[i]);
+    return ans;
 }
 
 void test_stuff() {
     vector<int> xs{1, 1, 2, 3, 4, 5, 5, 10};
-    assert(rhs_count(1, xs, 0) == 3);
+    // const int res = rhs_count(1, xs, 0);
+    // cout << "→ " << res << endl;
+    assert(lhs_count(1, xs, 0) == 1);
+    assert(lhs_count(1, xs, 1) == 2);
+    assert(lhs_count(1, xs, 2) == 3);
+    assert(lhs_count(1, xs, 3) == 2);
+    assert(lhs_count(1, xs, 4) == 2);
+    assert(lhs_count(1, xs, 5) == 2);
+    assert(lhs_count(1, xs, 6) == 3);
+    assert(lhs_count(1, xs, 7) == 1);
 
-    const int res = rhs_count(1, xs, 1);
-    cout << "→ " << res << endl;
+    assert(rhs_count(1, xs, 0) == 3);
     assert(rhs_count(1, xs, 1) == 2);
     assert(rhs_count(1, xs, 2) == 2);
     assert(rhs_count(1, xs, 3) == 2);
@@ -66,6 +140,6 @@ int main() {
         cout << max_saved(k, xs) << '\n';
     }
 
-    test_stuff();
+    // test_stuff();
     return 0;
 }
