539718a22eede951ebac0da5cffc0faf4c806b37
Ivan Krechetov <ikr@ikr.li>
2020-09-14 21:57:15 +0200

    Cover max_occurances_one_letter

--- codeforces/Subsequences_of_Length_Two/solution.cpp
+++ codeforces/Subsequences_of_Length_Two/solution.cpp
@@ -1,7 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int max_occurances(const int k, const string &s) {
+int max_occurances_one_letter(const int k, const string &s, const char l) {
+    const int n = s.size();
+    const int ls = count(cbegin(s), cend(s), l);
+    const int not_ls = n - ls;
+    const int d = min(k, not_ls);
+
+    return (ls + d) * (ls + d - 1) / 2;
+}
+
+int max_occurances(const int k, const string &s, const char a, const char b) {
+    if (a == b) return max_occurances_one_letter(k, s, a);
     const int n = s.size();
     return -1;
 }
@@ -15,6 +25,9 @@ int main() {
     string s(n, ' ');
     cin >> s;
 
-    cout << max_occurances(k, s) << '\n';
+    string t;
+    cin >> t;
+
+    cout << max_occurances(k, s, t[0], t[1]) << '\n';
     return 0;
 }
