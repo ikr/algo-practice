04455073f3e2254a529d6268b7bde80db929c28b
Ivan Krechetov <ikr@ikr.su>
2020-09-15 19:00:03 +0200

    Get going with DP

--- codeforces/Subsequences_of_Length_Two/solution.cpp
+++ codeforces/Subsequences_of_Length_Two/solution.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
+using vi = vector<int>;
+using vvi = vector<vi>;
 
 int max_occurances_one_letter(const int k, const string &s, const char l) {
     const int n = s.size();
@@ -13,6 +15,22 @@ int max_occurances_one_letter(const int k, const string &s, const char l) {
 int max_occurances(const int k, const string &s, const char a, const char b) {
     if (a == b) return max_occurances_one_letter(k, s, a);
     const int n = s.size();
+
+    // The value is -1 if the indices combination is impossible. Otherwise, it's
+    // the maximum number of occurances
+    // [up to index p] with [q letters a] after [r replacements used]
+    vector<vvi> dp(n, vvi(n + 1, vi(k + 1, -1)));
+    if (s[0] == a) {
+        dp[0][0][1] = 0;
+        dp[0][1][0] = 0;
+    } else {
+        dp[0][0][0] = 0;
+        dp[0][1][1] = 0;
+    }
+
+    for (int r = 0; r < n; ++r) {
+    }
+
     return -1;
 }
 
