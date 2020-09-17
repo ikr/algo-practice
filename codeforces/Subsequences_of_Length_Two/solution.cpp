d3adbc2f66bc133855f0a9fe5e2beba966383cfd
Ivan Krechetov <ikr@ikr.li>
2020-09-17 19:06:32 +0200

    Tip a toe

--- codeforces/Subsequences_of_Length_Two/solution.cpp
+++ codeforces/Subsequences_of_Length_Two/solution.cpp
@@ -3,6 +3,33 @@ using namespace std;
 using vi = vector<int>;
 using vvi = vector<vi>;
 
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<vector<T>> &xss) {
+    for (const auto xs : xss) os << xs << '\n';
+    return os;
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const unordered_set<T> &xs) {
+    os << '{';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << '}';
+    return os;
+}
+
 int max_occurances_one_letter(const int k, const string &s, const char l) {
     const int n = s.size();
     const int ls = count(cbegin(s), cend(s), l);
@@ -29,6 +56,16 @@ int max_occurances(const int k, const string &s, const char a, const char b) {
 
     // Maximum number of occurances [up to index p] after [r replacements used]
     vvi dp(n, vi(k + 1, 0));
+
+    for (int p = 1; p < n; ++p) {
+        dp[p][0] = dp[p - 1][0];
+        if (s[p] == b) dp[p][0] += a_freq[p];
+
+        for (int r = 1; r <= k; ++r) {
+        }
+    }
+
+    cout << dp << '\n';
 
     return *max_element(cbegin(dp.back()), cend(dp.back()));
 }
