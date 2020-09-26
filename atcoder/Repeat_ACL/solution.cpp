f6cea7ab632688bc3a0618ecd066196ff70751e8
Ivan Krechetov <ikr@ikr.li>
2020-09-26 15:23:03 +0200

    WIP

---
+++ atcoder/Repeat_ACL/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    string ans;
+    while (t--) {
+        ans += "ACL";
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}
