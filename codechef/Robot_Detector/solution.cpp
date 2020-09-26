bb5bc54d952f4b397d04dec37e74411d205403ee
Ivan Krechetov <ikr@ikr.su>
2020-09-26 19:03:17 +0200

    20 points AC

--- codechef/Robot_Detector/solution.cpp
+++ codechef/Robot_Detector/solution.cpp
@@ -1,9 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int mpow(const int x, const int n) {
+    if (n == 0) return 1;
+    if (n % 2) return x * mpow(x, n - 1);
+
+    const int sq = mpow(x, n / 2);
+    return sq * sq;
+}
+
 string moves(const int R, const int C) {
-    string ans(C - 1, 'R');
-    ans.insert(cbegin(ans), 'U');
+    string ans{"U"};
+
+    for (int y = 1; y <= R; ++y) {
+        ans += string(mpow(C, y), y % 2 ? 'R' : 'L');
+        if (y != R) ans += 'U';
+    }
+
     return ans;
 }
 
