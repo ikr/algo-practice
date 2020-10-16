b9c7304837f7da202f93c14198818480898f0d1d
Ivan Krechetov <ikr@ikr.li>
2020-10-16 22:36:23 +0200

    Copy the ac-library BIT implementation

--- codeforces/String_Reversal/solution.cpp
+++ codeforces/String_Reversal/solution.cpp
@@ -3,6 +3,98 @@ using namespace std;
 using ll = long long;
 static constexpr int SZ = 'z' - 'a' + 1;
 
+namespace atcoder {
+namespace internal {
+template <class T>
+using is_signed_int128 = typename conditional<is_same<T, __int128_t>::value ||
+                                                  is_same<T, __int128>::value,
+                                              true_type, false_type>::type;
+
+template <class T>
+using is_unsigned_int128 =
+    typename conditional<is_same<T, __uint128_t>::value ||
+                             is_same<T, unsigned __int128>::value,
+                         true_type, false_type>::type;
+
+template <class T>
+using make_unsigned_int128 =
+    typename std::conditional<std::is_same<T, __int128_t>::value, __uint128_t,
+                              unsigned __int128>;
+
+template <class T>
+using is_integral =
+    typename std::conditional<std::is_integral<T>::value ||
+                                  is_signed_int128<T>::value ||
+                                  is_unsigned_int128<T>::value,
+                              std::true_type, std::false_type>::type;
+
+template <class T>
+using is_signed_int =
+    typename std::conditional<(is_integral<T>::value &&
+                               std::is_signed<T>::value) ||
+                                  is_signed_int128<T>::value,
+                              std::true_type, std::false_type>::type;
+
+template <class T>
+using is_unsigned_int =
+    typename std::conditional<(is_integral<T>::value &&
+                               std::is_unsigned<T>::value) ||
+                                  is_unsigned_int128<T>::value,
+                              std::true_type, std::false_type>::type;
+
+template <class T>
+using to_unsigned = typename std::conditional<
+    is_signed_int128<T>::value, make_unsigned_int128<T>,
+    typename std::conditional<std::is_signed<T>::value, std::make_unsigned<T>,
+                              std::common_type<T>>::type>::type;
+
+template <class T>
+using is_signed_int_t = enable_if_t<is_signed_int<T>::value>;
+
+template <class T>
+using is_unsigned_int_t = enable_if_t<is_unsigned_int<T>::value>;
+
+template <class T> using to_unsigned_t = typename to_unsigned<T>::type;
+} // namespace internal
+
+// Reference: https://en.wikipedia.org/wiki/Fenwick_tree
+template <class T> struct fenwick_tree {
+    using U = internal::to_unsigned_t<T>;
+
+  public:
+    fenwick_tree() : _n(0) {}
+    fenwick_tree(int n) : _n(n), data(n) {}
+
+    void add(int p, T x) {
+        assert(0 <= p && p < _n);
+        p++;
+        while (p <= _n) {
+            data[p - 1] += U(x);
+            p += p & -p;
+        }
+    }
+
+    T sum(int l, int r) {
+        assert(0 <= l && l <= r && r <= _n);
+        return sum(r) - sum(l);
+    }
+
+  private:
+    int _n;
+    vector<U> data;
+
+    U sum(int r) {
+        U s = 0;
+        while (r > 0) {
+            s += data[r - 1];
+            r -= r & -r;
+        }
+        return s;
+    }
+};
+
+} // namespace atcoder
+
 string reversed(string s) {
     reverse(begin(s), end(s));
     return s;
