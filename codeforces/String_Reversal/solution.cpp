3406349bc4b4e38b0ec24541a9f56fee1df4ee94
Ivan Krechetov <ikr@ikr.li>
2020-10-16 23:12:27 +0200

    AC

--- codeforces/String_Reversal/solution.cpp
+++ codeforces/String_Reversal/solution.cpp
@@ -4,60 +4,7 @@ using ll = long long;
 static constexpr int SZ = 'z' - 'a' + 1;
 
 namespace atcoder {
-namespace internal {
-template <class T>
-using is_signed_int128 = typename conditional<is_same<T, __int128_t>::value ||
-                                                  is_same<T, __int128>::value,
-                                              true_type, false_type>::type;
-
-template <class T>
-using is_unsigned_int128 =
-    typename conditional<is_same<T, __uint128_t>::value ||
-                             is_same<T, unsigned __int128>::value,
-                         true_type, false_type>::type;
-
-template <class T>
-using make_unsigned_int128 =
-    typename std::conditional<is_same<T, __int128_t>::value, __uint128_t,
-                              unsigned __int128>;
-
-template <class T>
-using is_integral =
-    typename conditional<is_integral<T>::value || is_signed_int128<T>::value ||
-                             is_unsigned_int128<T>::value,
-                         true_type, false_type>::type;
-
-template <class T>
-using is_signed_int =
-    typename conditional<(is_integral<T>::value && is_signed<T>::value) ||
-                             is_signed_int128<T>::value,
-                         true_type, false_type>::type;
-
-template <class T>
-using is_unsigned_int =
-    typename conditional<(is_integral<T>::value && is_unsigned<T>::value) ||
-                             is_unsigned_int128<T>::value,
-                         true_type, false_type>::type;
-
-template <class T>
-using to_unsigned = typename conditional<
-    is_signed_int128<T>::value, make_unsigned_int128<T>,
-    typename conditional<is_signed<T>::value, make_unsigned<T>,
-                         common_type<T>>::type>::type;
-
-template <class T> using is_signed_int_t = enable_if_t<is_signed_int<T>::value>;
-
-template <class T>
-using is_unsigned_int_t = enable_if_t<is_unsigned_int<T>::value>;
-
-template <class T> using to_unsigned_t = typename to_unsigned<T>::type;
-} // namespace internal
-
-// Reference: https://en.wikipedia.org/wiki/Fenwick_tree
-template <class T> struct fenwick_tree {
-    using U = internal::to_unsigned_t<T>;
-
-  public:
+template <class T> struct fenwick_tree final {
     fenwick_tree() : _n(0) {}
     fenwick_tree(int n) : _n(n), data(n) {}
 
@@ -65,7 +12,7 @@ template <class T> struct fenwick_tree {
         assert(0 <= p && p < _n);
         p++;
         while (p <= _n) {
-            data[p - 1] += U(x);
+            data[p - 1] += x;
             p += p & -p;
         }
     }
@@ -77,10 +24,10 @@ template <class T> struct fenwick_tree {
 
   private:
     int _n;
-    vector<U> data;
+    vector<T> data;
 
-    U sum(int r) {
-        U s = 0;
+    T sum(int r) {
+        T s = 0;
         while (r > 0) {
             s += data[r - 1];
             r -= r & -r;
@@ -88,7 +35,6 @@ template <class T> struct fenwick_tree {
         return s;
     }
 };
-
 } // namespace atcoder
 
 string reversed(string s) {
