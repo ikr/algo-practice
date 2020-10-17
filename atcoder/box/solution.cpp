305bda12847dc0188dad143362f8dd570dbbdb11
Ivan Krechetov <ikr@ikr.li>
2020-10-17 21:08:49 +0200

    Solve A B C D

---
+++ atcoder/box/solution.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N, A, B;
+    cin >> N >> A >> B;
+    cout << N - A + B << '\n';
+
+    return 0;
+}
