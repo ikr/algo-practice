fb6f58e8ebe05ea5a3af8a5f31ba2bf0f7f40e1c
Ivan Krechetov <ikr@ikr.su>
2020-11-23 21:03:40 +0100

    AC

--- google/2020/Kick_Start_G/Merge_Cards/solution.cpp
+++ google/2020/Kick_Start_G/Merge_Cards/solution.cpp
@@ -1,20 +1,42 @@
 #include <bits/stdc++.h>
-#include <numeric>
 using namespace std;
 
 double solve(const vector<double> &xs) {
     const int n = xs.size();
-    vector<double> ss(n + 1, 0);
-    partial_sum(cbegin(xs), cend(xs), next(begin(ss)));
+    vector<vector<double>> memo(n, vector<double>(n, -1));
+    function<double(int, int)> ev_merges;
 
-    double acc = 0;
+    ev_merges = [&](const int l, const int r) {
+        if (memo[l][r] >= 0.0) return memo[l][r];
 
-    for (int i = 0; i < n - 1; ++i) {
-        acc += ss[i + 1];
-        acc += ss[n] - ss[i + 1];
+        double ans = 0;
+
+        if (l > 0) {
+            ans += 1.0 + ev_merges(l - 1, r);
+        }
+
+        if (l > 1) {
+            ans += (l - 1) * ev_merges(l - 1, r);
+        }
+
+        if (r > 0) {
+            ans += 1.0 + ev_merges(l, r - 1);
+        }
+
+        if (r > 1) {
+            ans += (r - 1) * ev_merges(l, r - 1);
+        }
+
+        if (r + l > 0) ans /= static_cast<double>(r + l);
+        memo[l][r] = ans;
+        return ans;
+    };
+
+    double ans = 0;
+    for (int i = 0; i < n; ++i) {
+        ans += xs[i] * ev_merges(i, n - i - 1);
     }
-
-    return ss[n] + acc / (n - 1.0);
+    return ans;
 }
 
 int main() {
