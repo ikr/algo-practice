5e0586778126d6616d60daf4965e5fdbec2cd46b
Ivan Krechetov <ikr@ikr.li>
2020-11-24 11:02:43 +0100

    AC

--- toki/Calculating_Cuteness/solution.cpp
+++ toki/Calculating_Cuteness/solution.cpp
@@ -2,39 +2,6 @@
 using namespace std;
 using ll = long long;
 static constexpr ll M = 1e9 + 7;
-static constexpr ll LIM = 1001;
-
-template <typename T> tuple<T, T, T> extended_gcd(const T a, const T b) {
-    auto [old_r, r] = pair{a, b};
-    auto [old_s, s] = pair<T, T>{1, 0};
-    auto [old_t, t] = pair<T, T>{0, 1};
-
-    while (r) {
-        const T quotient = old_r / r;
-        tie(old_r, r) = pair{r, old_r - quotient * r};
-        tie(old_s, s) = pair{s, old_s - quotient * s};
-        tie(old_t, t) = pair{t, old_t - quotient * t};
-    }
-
-    return {old_s, old_t, old_r};
-}
-
-template <typename T> T mod_inverse(const T x, const T m) {
-    return (get<0>(extended_gcd(x, m)) + m) % m;
-}
-
-vector<ll> gather_fact() {
-    vector<ll> ans(LIM, 1);
-    for (ll i = 2; i < LIM; ++i) {
-        ans[i] = (ans[i - 1] * i) % M;
-    }
-    return ans;
-}
-
-ll combinations(const vector<ll> &fact, const ll n, const ll k) {
-    const ll q = (fact[k] * fact[n - k]) % M;
-    return (fact[n] * mod_inverse(q, M));
-}
 
 ll mpow(const ll x, const int p) {
     if (p == 0) return 1;
@@ -47,16 +14,17 @@ ll cute_count(const ll n, const ll m) {
     if (n == 1) return 1;
     if (m == 1) return 0;
 
-    const auto fact = gather_fact();
-    ll ans = 0;
+    vector<ll> m_last(n, 1);
+    vector<ll> not_m_last(n, m - 1);
 
-    for (ll k = 1; k < n; ++k) {
-        const ll a = combinations(fact, n, k);
-        const ll b = mpow(m - 1, n - k);
-        ans += (a * b) % M;
+    for (int i = 1; i < n; ++i) {
+        m_last[i] = not_m_last[i - 1];
+
+        not_m_last[i] =
+            (((not_m_last[i - 1] + m_last[i - 1]) % M) * (m - 1)) % M;
     }
 
-    return ans;
+    return (m_last.back() + not_m_last.back() - mpow(m - 1, n) + M) % M;
 }
 
 int main() {
