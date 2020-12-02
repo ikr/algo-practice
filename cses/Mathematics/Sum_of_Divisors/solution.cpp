5fa9596962599bd3e82a6793f5104f87e34b9b8e
Ivan Krechetov <ikr@ikr.su>
2020-12-02 19:15:28 +0100

    Improve from O(n√n) → O(n)

--- cses/Mathematics/Sum_of_Divisors/solution.cpp
+++ cses/Mathematics/Sum_of_Divisors/solution.cpp
@@ -3,25 +3,10 @@ using namespace std;
 using ll = long long;
 static constexpr ll M = 1e9 + 7;
 
-ll sum_of_divisors(const ll x) {
-    const ll lim = sqrt(x);
-    ll ans = (1 + (x == 1 ? 0 : x)) % M;
-
-    for (ll i = 2; i <= lim; ++i) {
-        if (x % i) continue;
-        ans = (ans + i) % M;
-
-        const ll q = x / i;
-        if (q > lim) ans = (ans + q) % M;
-    }
-
-    return ans;
-}
-
 ll sum_of_sum_of_divisors(const ll x) {
     ll ans = 0;
     for (ll i = 1; i <= x; ++i) {
-        ans = (ans + sum_of_divisors(i)) % M;
+        ans = (ans + (x / i) * i) % M;
     }
     return ans;
 }
