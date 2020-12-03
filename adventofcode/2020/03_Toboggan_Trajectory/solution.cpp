eb36955a922098556b0fa9193b8a86562cb7b5f2
Ivan Krechetov <ikr@ikr.li>
2020-12-03 08:04:51 +0100

    Solve part 2

--- adventofcode/2020/03_Toboggan_Trajectory/solution.cpp
+++ adventofcode/2020/03_Toboggan_Trajectory/solution.cpp
@@ -1,14 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
+using pi = pair<int, int>;
+using ll = long long;
 
-int count_trees_along_vector_1_3(const vector<string> &rows) {
+int count_trees_along_direction(const vector<string> &rows, const pi dir) {
     const int H = rows.size();
     const int W = rows[0].size();
+    const auto [dr, dc] = dir;
 
     int ans = 0;
 
-    for (int r = 0, c = 0; r < H; ++r, c += 3) {
+    for (int r = 0, c = 0; r < H; r += dr, c += dc) {
         if (rows[r][c % W] == '#') ++ans;
+    }
+
+    return ans;
+}
+
+vector<pi> the_5_directions() {
+    return {{1, 1}, {1, 3}, {1, 5}, {1, 7}, {2, 1}};
+}
+
+ll product_of_trees_count_in_5_directions(const vector<string> &rows) {
+    ll ans = 1;
+
+    for (const auto dir : the_5_directions()) {
+        ans *= count_trees_along_direction(rows, dir);
     }
 
     return ans;
@@ -20,6 +37,6 @@ int main() {
         rows.push_back(row);
     }
 
-    cout << count_trees_along_vector_1_3(rows) << '\n';
+    cout << product_of_trees_count_in_5_directions(rows) << '\n';
     return 0;
 }
