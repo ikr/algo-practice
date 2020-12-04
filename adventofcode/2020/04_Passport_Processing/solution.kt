5e7fb4ec12ac1d24dd69223e7e548130efbbe4c0
Ivan Krechetov <ikr@ikr.li>
2020-12-04 08:08:22 +0100

    Solve part 2

--- adventofcode/2020/04_Passport_Processing/solution.kt
+++ adventofcode/2020/04_Passport_Processing/solution.kt
@@ -2,11 +2,14 @@ fun main() {
     var ans = 0
 
     while (true) {
-        val tags = readPassportBlockTags()
+        val values = readPassportBlockValues()
+        val tags = values.map { it.first }
         if (tags.isEmpty()) break
 
-        val rem = missingTags(tags)
-        if (rem.isEmpty() || rem == setOf(Tag.cid)) ++ans
+        val rem = missingTags(tags.toSet())
+        if (rem.isEmpty() || rem == setOf(Tag.cid)) {
+            if (values.all(::isValidValue)) ++ans
+        }
     }
 
     println(ans)
@@ -15,19 +18,68 @@ fun main() {
 private fun missingTags(tags: Set<Tag>): Set<Tag> =
     Tag.values().toSet().minus(tags)
 
-private fun readPassportBlockTags(): Set<Tag> {
-    val ans = mutableSetOf<Tag>()
+private fun readPassportBlockValues(): List<Pair<Tag, String>> {
+    val ans = mutableListOf<Pair<Tag, String>>()
 
     while (true) {
-        val line = readLine();
+        val line = readLine()
         if (line == null || line == "") break
-        ans += readPassportLineTags(line)
+        ans += readPassportLineValues(line)
     }
 
     return ans
 }
 
-private fun readPassportLineTags(line: String): Set<Tag> =
-    line.split(' ').map { it.split(':').first() }.map(Tag::valueOf).toSet()
+private fun readPassportLineValues(line: String): List<Pair<Tag, String>> =
+    line.split(' ').map {
+        val (k, v) = it.split(':')
+        Tag.valueOf(k) to v
+    }
+
+private fun isValidValue(kv: Pair<Tag, String>): Boolean =
+    when (kv.first) {
+        Tag.byr -> isValidBirthYear(kv.second)
+        Tag.iyr -> isValidIssueYear(kv.second)
+        Tag.eyr -> isValidExpirationYear(kv.second)
+        Tag.hgt -> isValidHeight(kv.second)
+        Tag.hcl -> isValidHairColor(kv.second)
+        Tag.ecl -> isValidEyeColor(kv.second)
+        Tag.pid -> isValidPassportId(kv.second)
+        Tag.cid -> true
+    }
+
+private fun isValidBirthYear(v: String) =
+    isValidYear(v) && v.toInt() in 1920..2002
+
+private fun isValidIssueYear(v: String) =
+    isValidYear(v) && v.toInt() in 2010..2020
+
+private fun isValidExpirationYear(v: String) =
+    isValidYear(v) && v.toInt() in 2020..2030
+
+private fun isValidYear(v: String) =
+    Regex("^[0-9]{4}$").matches(v)
+
+private fun isValidHeight(v: String): Boolean {
+    if (!Regex("^[0-9]{2,3}(in|cm)$").matches(v)) return false
+    if (v.endsWith("cm")) return isValidHeightCm(v.split("cm").first())
+    return isValidHeightIn(v.split("in").first())
+}
+
+private fun isValidHeightCm(v: String) =
+    v.toInt() in 150..193
+
+private fun isValidHeightIn(v: String) =
+    v.toInt() in 59..76
+
+private fun isValidHairColor(v: String) =
+    Regex("^#[0-9a-f]{6}$").matches(v)
+
+private fun isValidEyeColor(v: String) =
+    EyeColor.values().map { it.name }.contains(v)
+
+private fun isValidPassportId(v: String) =
+    Regex("^[0-9]{9}$").matches(v)
 
 private enum class Tag { byr, iyr, eyr, hgt, hcl, ecl, pid, cid }
+private enum class EyeColor { amb, blu, brn, gry, grn, hzl, oth }
