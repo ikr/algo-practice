4bcc3393a287963b48e38e40bed043966d9ac288
Ivan Krechetov <ikr@ikr.su>
2020-12-08 07:40:43 +0100

    Solve part 1

---
+++ adventofcode/2020/08_Handheld_Halting/solution.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+enum class Instr { JMP, ACC };
+
+pair<Instr, int> parse_instr(const string &code, const string &arg) {
+    if (code == "nop") return {Instr::JMP, 1};
+
+    const int x = stoi(arg);
+    if (code == "acc") return {Instr::ACC, x};
+    return {Instr::JMP, x};
+}
+
+int execute(const vector<pair<Instr, int>> &program) {
+    const int n = program.size();
+    vector<bool> done(n, false);
+    int ans = 0;
+    int pointer = 0;
+
+    while (!done[pointer]) {
+        done[pointer] = true;
+
+        const auto [op, arg] = program.at(pointer);
+        if (op == Instr::ACC) {
+            ans += arg;
+            ++pointer;
+        } else {
+            pointer += arg;
+        }
+
+        pointer = ((pointer % n) + n) % n;
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<pair<Instr, int>> program;
+
+    for (string line; getline(cin, line);) {
+        const auto s1 = line.substr(0, 3);
+        const auto s2 = line.substr(3);
+        program.push_back(parse_instr(s1, s2));
+    }
+
+    cout << execute(program) << '\n';
+    return 0;
+}
