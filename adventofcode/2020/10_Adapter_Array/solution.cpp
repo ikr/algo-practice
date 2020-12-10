a029f41ad2711194c343eb13066ae7b40d61861d
Ivan Krechetov <ikr@ikr.li>
2020-12-10 07:49:11 +0100

    Solve part 2

--- adventofcode/2020/10_Adapter_Array/solution.cpp
+++ adventofcode/2020/10_Adapter_Array/solution.cpp
@@ -1,5 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+
+ll count_ways(const vector<int> &xs) {
+    const int n = xs.size();
+    vector<ll> dp(n, 0);
+    dp[0] = 1;
+
+    for (int i = 1; i < n; ++i) {
+        for (const int j : {i - 1, i - 2, i - 3}) {
+            if (j >= 0 && xs[i] - xs[j] <= 3) dp[i] += dp[j];
+        }
+    }
+
+    return dp.back();
+}
 
 int main() {
     vector<int> xs{0};
@@ -9,12 +24,6 @@ int main() {
     sort(begin(xs), end(xs));
     xs.push_back(xs.back() + 3);
 
-    vector<int> ds(xs.size(), 0);
-    adjacent_difference(cbegin(xs), cend(xs), begin(ds));
-
-    const int d1s = count(cbegin(ds), cend(ds), 1);
-    const int d3s = count(cbegin(ds), cend(ds), 3);
-
-    cout << (d1s * d3s) << '\n';
+    cout << count_ways(xs) << '\n';
     return 0;
 }
