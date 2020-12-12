a806c258e17ac7e2ca2106b0ce0b73521ec33038
Ivan Krechetov <ikr@ikr.li>
2020-12-12 07:42:25 +0100

    Solve part 1

---
+++ adventofcode/2020/12_Rain_Risk/solution.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pi = pair<int, int>;
+
+template <typename T>
+constexpr pair<T, T> operator+(const pair<T, T> &lhs, const pair<T, T> &rhs) {
+    return {lhs.first + rhs.first, lhs.second + rhs.second};
+}
+
+constexpr pi scale(const pi d, const int factor) {
+    const auto [dx, dy] = d;
+    return {dx * factor, dy * factor};
+}
+
+constexpr pair<pi, pi> rotation_matrix_counter_clockwise(const int degrees) {
+    switch (degrees) {
+    case 90:
+        return {{0, -1}, {1, 0}};
+
+    case 180:
+        return {{-1, 0}, {0, -1}};
+
+    case 270:
+        return {{0, 1}, {-1, 0}};
+
+    default:
+        assert(false && "/o\\");
+        return {};
+    }
+}
+
+constexpr pair<pi, pi> rotation_matrix_clockwise(const int degrees) {
+    switch (degrees) {
+    case 90:
+        return rotation_matrix_counter_clockwise(270);
+
+    case 180:
+        return rotation_matrix_counter_clockwise(180);
+
+    case 270:
+        return rotation_matrix_counter_clockwise(90);
+
+    default:
+        assert(false && "/o\\");
+        return {};
+    }
+}
+
+constexpr pi rotate(const pair<pi, pi> &rotation_matrix, const pi v) {
+    const auto [vx, vy] = v;
+    const auto [ab, cd] = rotation_matrix;
+    return {ab.first * vx + ab.second * vy, cd.first * vx + cd.second * vy};
+}
+
+int main() {
+    pi pos{0, 0};
+    pi dir{1, 0};
+
+    for (string line; getline(cin, line);) {
+        const char instr_code = line[0];
+        const int arg = stoi(line.substr(1));
+
+        switch (instr_code) {
+        case 'N':
+            pos = pos + pi{0, arg};
+            break;
+
+        case 'S':
+            pos = pos + pi{0, -arg};
+            break;
+
+        case 'E':
+            pos = pos + pi{arg, 0};
+            break;
+
+        case 'W':
+            pos = pos + pi{-arg, 0};
+            break;
+
+        case 'F':
+            pos = pos + scale(dir, arg);
+            break;
+
+        case 'L':
+            dir = rotate(rotation_matrix_counter_clockwise(arg), dir);
+            break;
+
+        case 'R':
+            dir = rotate(rotation_matrix_clockwise(arg), dir);
+            break;
+
+        default:
+            assert(false && "/o\\");
+        }
+    }
+
+    const int ans = abs(pos.first) + abs(pos.second);
+    cout << ans << '\n';
+    return 0;
+}
