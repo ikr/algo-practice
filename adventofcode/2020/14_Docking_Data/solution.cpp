5c3a90d0783449f46f640a4d5cd155cb8272d9b8
Ivan Krechetov <ikr@ikr.su>
2020-12-14 14:51:03 +0100

    Parse the transformation literal

--- adventofcode/2020/14_Docking_Data/solution.cpp
+++ adventofcode/2020/14_Docking_Data/solution.cpp
@@ -7,12 +7,40 @@ string reversed(string s) {
     return s;
 }
 
-ull to_mask(const string &reversed_literal) {}
+ull to_mask(const string &reversed_literal) {
+    const int n = reversed_literal.size();
+    ull ans = ~0ULL;
+
+    for (int i = 0; i < n; ++i) {
+        if (reversed_literal[i] != 'X') {
+            ans = ans & ~(1 << i);
+        }
+    }
+
+    return ans;
+}
+
+ull to_forced_value(const string &reversed_literal) {
+    const int n = reversed_literal.size();
+    ull ans = 0ULL;
+
+    for (int i = 0; i < n; ++i) {
+        if (reversed_literal[i] == '1') {
+            ans |= (1 << i);
+        }
+    }
+
+    return ans;
+}
 
 struct Transformation final {
     ull apply(const ull x) const { return (x & m_mask) | m_forced_value; }
 
-    Transformation(string literal) {}
+    Transformation(string literal) : m_mask{0}, m_forced_value{0} {
+        reverse(begin(literal), end(literal));
+        m_mask = to_mask(literal);
+        m_forced_value = to_forced_value(literal);
+    }
 
   private:
     ull m_mask;
