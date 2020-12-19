7fd94f3a8ee079ab7629c26111294152c49a822d
Ivan Krechetov <ikr@ikr.li>
2020-12-19 09:44:12 +0100

    Implement `tokenize`

--- adventofcode/2020/18_Operation_Order/solution.cpp
+++ adventofcode/2020/18_Operation_Order/solution.cpp
@@ -3,6 +3,38 @@ using namespace std;
 using ll = long long;
 using Iter = string::const_iterator;
 
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ", ";
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
+
+string join(const vector<string> &tokens) {
+    string ans;
+    for (const auto &t : tokens) ans += t;
+    return ans;
+}
+
+pair<vector<string>, Iter> tokenize(const Iter last, const Iter it,
+                                    vector<string> agg) {
+    if (it == last || *it == ')') return {agg, it == last ? last : next(it)};
+
+    if (*it == '(') {
+        const auto [sub, subi] = tokenize(last, next(it), {});
+        const string token = "(" + join(sub) + ")";
+        agg.push_back(token);
+        return tokenize(last, subi, agg);
+    }
+
+    assert(*it == '+' || *it == '*' || ('0' <= *it && *it <= '9'));
+    agg.push_back(string(1, *it));
+    return tokenize(last, next(it), agg);
+}
+
 template <typename Iter, typename R, typename Binop, typename Unaop>
 R ttransform_reduce(Iter first, Iter last, R init, Binop binop, Unaop unaop) {
     R ans = init;
@@ -50,6 +82,7 @@ int main() {
     for (string line; getline(cin, line);) {
         line.erase(remove(begin(line), end(line), ' '), cend(line));
         lines.push_back(line);
+        cout << tokenize(cend(line), cbegin(line), {}).first << '\n';
     }
 
     cout << solve(lines) << '\n';
