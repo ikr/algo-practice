89b734e784c0103d58d72689d872795c9e5b1371
Ivan Krechetov <ikr@ikr.li>
2020-12-24 10:19:50 +0100

    Solve part 1

--- adventofcode/2020/20_Jurassic_Jigsaw/solution.cpp
+++ adventofcode/2020/20_Jurassic_Jigsaw/solution.cpp
@@ -1,14 +1,82 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+
+static constexpr int SZ = 10;
+
+template <typename Iter, typename R, typename Binop, typename Unaop>
+R ttransform_reduce(Iter first, Iter last, R init, Binop binop, Unaop unaop) {
+    R ans = init;
+
+    for (auto it = first; it != last; ++it) {
+        ans = binop(ans, unaop(*it));
+    }
+
+    return ans;
+}
+
+int seq_hash(const string &s) {
+    return bitset<SZ>(s, 0, string::npos, '.', '#').to_ulong();
+}
+
+int side_hash(string side) {
+    int direct = seq_hash(side);
+    reverse(begin(side), end(side));
+    int flipped = seq_hash(side);
+    return min(direct, flipped);
+}
 
 struct Tile final {
     Tile(const int id, const vector<string> &rows) : m_id{id}, m_rows{rows} {}
     Tile() : m_id{0}, m_rows{} {}
+    int id() const { return m_id; }
+
+    vector<int> side_hashes() const {
+        return {side_hash(m_rows[0]), side_hash(column(SZ - 1)),
+                side_hash(m_rows[SZ - 1]), side_hash(column(0))};
+    }
 
   private:
+    string column(const int co) const {
+        string ans(SZ, ' ');
+        for (int i = 0; i < SZ; ++i) {
+            ans[i] = m_rows[i][co];
+        }
+        return ans;
+    }
+
     int m_id;
     vector<string> m_rows;
 };
+
+vector<int> gather_side_hash_freqs(const vector<Tile> &tiles) {
+    vector<int> ans(1U << SZ, 0);
+
+    for (const auto &t : tiles) {
+        for (const auto h : t.side_hashes()) {
+            ++ans[h];
+        }
+    }
+
+    return ans;
+}
+
+ll solve(const vector<Tile> &tiles) {
+    const auto side_hash_freqs = gather_side_hash_freqs(tiles);
+
+    ll ans = 1;
+    for (const auto &t : tiles) {
+        const auto hs = t.side_hashes();
+
+        const auto neighs = ttransform_reduce(
+            cbegin(hs), cend(hs), 0, plus<int>{},
+            [&side_hash_freqs](const int h) { return side_hash_freqs[h] - 1; });
+
+        if (neighs == 2) ans *= t.id();
+    }
+
+    return ans;
+}
 
 int main() {
     vector<Tile> tiles;
@@ -31,5 +99,7 @@ int main() {
 
         rows.push_back(line);
     }
+
+    cout << solve(tiles) << '\n';
     return 0;
 }
