bff898b22549cea6aed47cf4d8c38774113deaa1
Ivan Krechetov <ikr@ikr.li>
2020-12-28 18:35:12 +0100

    Implement make_move

--- adventofcode/2020/23_Crab_Cups/solution.cpp
+++ adventofcode/2020/23_Crab_Cups/solution.cpp
@@ -11,11 +11,59 @@ template <typename T> ostream &operator<<(ostream &os, const list<T> &xs) {
     return os;
 }
 
-list<int> poke_one(const list<int> &xs) {
-    const auto it = find(cbegin(xs), cend(xs), 1);
+list<int> rotate_to(const list<int> &xs, const int t) {
+    const auto it = find(cbegin(xs), cend(xs), t);
     list<int> ans;
-    ans.insert(cend(ans), next(it), cend(xs));
+    ans.insert(cend(ans), it, cend(xs));
     ans.insert(cend(ans), cbegin(xs), it);
+    return ans;
+}
+
+constexpr int cyc_dec(const int x) { return x - 1 ? x - 1 : 9; }
+
+int destination(const int current, const vector<int> &stash) {
+    set<int> abc(cbegin(stash), cend(stash));
+
+    int ans = cyc_dec(current);
+    while (abc.count(ans)) ans = cyc_dec(ans);
+    return ans;
+}
+
+vector<int> copy_to_stash(const list<int> &xs) {
+    const auto first = next(cbegin(xs));
+    auto last = first;
+    advance(last, 3U);
+
+    vector<int> ans(first, last);
+    assert(ans.size() == 3U);
+    return ans;
+}
+
+list<int> erase_stash(list<int> xs) {
+    const auto first = next(cbegin(xs));
+    auto last = first;
+    advance(last, 3U);
+    assert(distance(first, last) == 3U);
+
+    xs.erase(first, last);
+    return xs;
+}
+
+list<int> make_move(const list<int> &xs) {
+    const auto stash = copy_to_stash(xs);
+    auto ys = erase_stash(xs);
+    const int dest = destination(xs.front(), stash);
+
+    cout << "dest:" << dest << '\n';
+
+    const auto dest_it = find(cbegin(ys), cend(ys), dest);
+    ys.insert(next(dest_it), cbegin(stash), cend(stash));
+    return rotate_to(ys, *next(cbegin(ys)));
+}
+
+list<int> poke_one(const list<int> &xs) {
+    auto ans = rotate_to(xs, 1);
+    ans.pop_front();
     return ans;
 }
 
@@ -35,7 +83,6 @@ int main() {
     transform(cbegin(s), cend(s), begin(xs),
               [](const char d) { return d - '0'; });
 
-    cout << join(xs) << '\n';
-    cout << join(poke_one(xs)) << '\n';
+    cout << make_move(xs) << '\n';
     return 0;
 }
