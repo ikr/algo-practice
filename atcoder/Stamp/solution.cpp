3b3dfad44bc73147833c040f51f1274a881bf3ed
Ivan Krechetov <ikr@ikr.su>
2020-12-28 11:00:57 +0100

    Oracle + generator

--- atcoder/Stamp/solution.cpp
+++ atcoder/Stamp/solution.cpp
@@ -2,13 +2,12 @@
 using namespace std;
 using ll = long long;
 
-pair<int, vector<int>> gather_stamp_and_spans(const int n,
-                                              const vector<int> &xs) {
-    int stamp = 1e9 + 41;
-    vector<int> spans;
+pair<ll, vector<ll>> gather_stamp_and_spans(const ll n, const vector<ll> &xs) {
+    ll stamp = 1e18;
+    vector<ll> spans;
     spans.reserve(xs.size());
 
-    int pre = 0;
+    ll pre = 0;
 
     for (const auto x : xs) {
         if (x == pre + 1) {
@@ -16,8 +15,9 @@ pair<int, vector<int>> gather_stamp_and_spans(const int n,
             continue;
         }
 
-        stamp = min(stamp, x - pre - 1);
-        spans.push_back(x - pre - 1);
+        const ll d = x - pre - 1;
+        stamp = min(stamp, d);
+        spans.push_back(d);
         pre = x;
     }
 
@@ -29,12 +29,12 @@ pair<int, vector<int>> gather_stamp_and_spans(const int n,
     return {stamp, spans};
 }
 
-ll solve(const int n, const vector<int> &xs) {
+ll solve(const ll n, const vector<ll> &xs) {
     const auto [k, spans] = gather_stamp_and_spans(n, xs);
 
     ll ans = 0;
 
-    for (const int y : spans) {
+    for (const ll y : spans) {
         ans += (y / k);
         if (y % k) ++ans;
     }
@@ -42,24 +42,58 @@ ll solve(const int n, const vector<int> &xs) {
     return ans;
 }
 
+ll oracle_solve(const ll n, vector<ll> a) {
+    vector<int> b;
+    a.push_back(0);
+    a.push_back(n + 1);
+    sort(a.begin(), a.end());
+
+    int tmp;
+    for (auto i = 0U; i < a.size() - 1; i++) {
+        tmp = a[i + 1] - a[i] - 1;
+        if (tmp != 0) {
+            b.push_back(tmp);
+        }
+    }
+
+    sort(b.begin(), b.end());
+
+    int k, sum = 0;
+    if (b.size() == 0)
+        return 0;
+    else {
+        k = b[0];
+
+        for (auto i = 0U; i < b.size(); i++) {
+            if (b[i] % k == 0)
+                sum += (double)b[i] / (double)k;
+            else
+                sum += (double)b[i] / (double)k + 1;
+        }
+        return sum;
+    }
+}
+
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    int n, m;
+    ll n, m;
     cin >> n >> m;
 
     if (!m) {
         cout << n << '\n';
+        cout << n << '\n';
         return 0;
     }
 
-    vector<int> xs(m, 0);
+    vector<ll> xs(m, 0);
     for (auto &x : xs) {
         cin >> x;
     }
 
     sort(begin(xs), end(xs));
     cout << solve(n, xs) << '\n';
+    cout << oracle_solve(n, xs) << '\n';
     return 0;
 }
