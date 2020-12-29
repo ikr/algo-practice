86b3b38ae16b9ad318e306abd0d7661cbd9f7886
Ivan Krechetov <ikr@ikr.su>
2020-12-29 21:28:05 +0100

    Implement map_to_deltas
    See https://www.redblobgames.com/grids/hexagons/

--- adventofcode/2020/24_Lobby_Layout/solution.cpp
+++ adventofcode/2020/24_Lobby_Layout/solution.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 using Coord = tuple<int, int, int>;
 
+template <typename T1, typename T2, typename T3>
+ostream &operator<<(ostream &os, const tuple<T1, T2, T3> &x) {
+    const auto [a, b, c] = x;
+    os << '(' << a << ' ' << b << ' ' << c << ')';
+    return os;
+}
+
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
+
 template <typename T>
 constexpr tuple<T, T, T> operator+(const tuple<T, T, T> &lhs,
                                    const tuple<T, T, T> &rhs) {
@@ -11,11 +28,58 @@ constexpr tuple<T, T, T> operator+(const tuple<T, T, T> &lhs,
 }
 
 vector<Coord> map_to_deltas(const string &s) {
+    const int n = s.size();
+    vector<Coord> ans;
 
+    for (int i = 0; i < n;) {
+        if (i < n - 1) {
+            const string h = s.substr(i, 2);
+
+            if (h == "se") {
+                ans.emplace_back(0, -1, 1);
+                i += 2;
+                continue;
+            }
+
+            if (h == "sw") {
+                ans.emplace_back(-1, 0, 1);
+                i += 2;
+                continue;
+            }
+
+            if (h == "ne") {
+                ans.emplace_back(1, 0, -1);
+                i += 2;
+                continue;
+            }
+
+            if (h == "nw") {
+                ans.emplace_back(0, 1, -1);
+                i += 2;
+                continue;
+            }
+        }
+
+        if (s[i] == 'e') {
+            ans.emplace_back(1, -1, 0);
+            ++i;
+            continue;
+        }
+
+        if (s[i] == 'w') {
+            ans.emplace_back(-1, 1, 0);
+            ++i;
+            continue;
+        }
+    }
+
+    return ans;
 }
 
 int solve(const vector<string> &lines) {
     set<Coord> black;
+
+    for (const auto &line : lines) cout << map_to_deltas(line) << '\n';
 
     return black.size();
 }
