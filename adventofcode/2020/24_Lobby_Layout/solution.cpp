5a4460e5032f681ac1e85e9af89b65cfc7665a82
Ivan Krechetov <ikr@ikr.su>
2020-12-31 10:31:48 +0100

    Factorize

--- adventofcode/2020/24_Lobby_Layout/solution.cpp
+++ adventofcode/2020/24_Lobby_Layout/solution.cpp
@@ -2,23 +2,6 @@
 using namespace std;
 using Coord = tuple<int, int, int>;
 
-template <typename T1, typename T2, typename T3>
-ostream &operator<<(ostream &os, const tuple<T1, T2, T3> &x) {
-    const auto [a, b, c] = x;
-    os << '(' << a << ' ' << b << ' ' << c << ')';
-    return os;
-}
-
-template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
-    os << '[';
-    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
-        if (i != xs.cbegin()) os << ' ';
-        os << *i;
-    }
-    os << ']';
-    return os;
-}
-
 template <typename T>
 constexpr tuple<T, T, T> operator+(const tuple<T, T, T> &lhs,
                                    const tuple<T, T, T> &rhs) {
@@ -27,60 +10,54 @@ constexpr tuple<T, T, T> operator+(const tuple<T, T, T> &lhs,
     return {a + x, b + y, c + z};
 }
 
-vector<Coord> map_to_deltas(const string &s) {
+enum class Dir { SE, SW, NE, NW, E, W };
+
+bool is_one_of(const set<string> &xs, const string &x) { return xs.count(x); }
+
+Dir parse_code(const string &code) {
+    const map<string, Dir> m{{"se", Dir::SE}, {"sw", Dir::SW}, {"ne", Dir::NE},
+                             {"nw", Dir::NW}, {"e", Dir::E},   {"w", Dir::W}};
+    return m.at(code);
+}
+
+vector<Dir> parse_line(const string &s) {
     const int n = s.size();
-    vector<Coord> ans;
+    vector<Dir> ans;
 
-    for (int i = 0; i < n;) {
-        if (i < n - 1) {
-            const string h = s.substr(i, 2);
-
-            if (h == "se") {
-                ans.emplace_back(0, -1, 1);
-                i += 2;
-                continue;
-            }
-
-            if (h == "sw") {
-                ans.emplace_back(-1, 0, 1);
-                i += 2;
-                continue;
-            }
-
-            if (h == "ne") {
-                ans.emplace_back(1, 0, -1);
-                i += 2;
-                continue;
-            }
-
-            if (h == "nw") {
-                ans.emplace_back(0, 1, -1);
-                i += 2;
-                continue;
-            }
+    for (int i = 0; i < n; ++i) {
+        if (i < n - 1 && is_one_of({"se", "sw", "ne", "nw"}, s.substr(i, 2))) {
+            ans.push_back(parse_code(s.substr(i, 2)));
+            ++i;
+            continue;
         }
 
         if (s[i] == 'e') {
-            ans.emplace_back(1, -1, 0);
-            ++i;
-            continue;
-        }
-
-        if (s[i] == 'w') {
-            ans.emplace_back(-1, 1, 0);
-            ++i;
-            continue;
+            ans.push_back(Dir::E);
+        } else if (s[i] == 'w') {
+            ans.push_back(Dir::W);
+        } else {
+            assert(false && "parse_line");
         }
     }
 
     return ans;
 }
 
+vector<Coord> dirs_to_deltas(const vector<Dir> &dirs) {
+    const map<Dir, Coord> deltas_by_dir{
+        {Dir::SE, {0, -1, 1}}, {Dir::SW, {-1, 0, 1}}, {Dir::NE, {1, 0, -1}},
+        {Dir::NW, {0, 1, -1}}, {Dir::E, {1, -1, 0}},  {Dir::W, {-1, 1, 0}}};
+
+    vector<Coord> deltas(dirs.size());
+    transform(
+        cbegin(dirs), cend(dirs), begin(deltas),
+        [&deltas_by_dir](const Dir &dir) { return deltas_by_dir.at(dir); });
+    return deltas;
+}
+
 Coord follow_path(const Coord &origin, const vector<Coord> &deltas) {
     auto ans = origin;
-    for (const auto &d : deltas) {
-        ans = ans + d;
-    }
+    for (const auto &d : deltas) ans = ans + d;
     return ans;
 }
 
@@ -88,7 +65,7 @@ int solve(const vector<string> &lines) {
     set<Coord> black;
 
     for (const auto &line : lines) {
-        const auto p = follow_path({0, 0, 0}, map_to_deltas(line));
+        const auto p = follow_path({0, 0, 0}, dirs_to_deltas(parse_line(line)));
         if (black.count(p)) {
             black.erase(p);
         } else {
@@ -101,11 +78,7 @@ int solve(const vector<string> &lines) {
 
 int main() {
     vector<string> lines;
-
-    for (string line; getline(cin, line);) {
-        lines.push_back(line);
-    }
-
+    for (string line; getline(cin, line);) lines.push_back(line);
     cout << solve(lines) << '\n';
     return 0;
 }
