f26a781bd471e3b9f1d7d956af968854b22f47f2
Ivan Krechetov <ikr@ikr.su>
2021-01-09 10:57:19 +0100

    Remove the memoization ⇒ AC

--- oj/The_3n_plus_1_problem/solution.cpp
+++ oj/The_3n_plus_1_problem/solution.cpp
@@ -2,39 +2,34 @@
 using namespace std;
 static constexpr int LIM = 2 * 1e9;
 
-int chain_size(vector<int> &memo, const int x) {
-    if (memo[x] == -1) {
-        if (x <= 1) {
-            memo[x] = x;
-        } else if (x % 2) {
-            memo[x] = 1 + chain_size(memo, 3 * x + 1);
-        } else {
-            memo[x] = 1 + chain_size(memo, x / 2);
-        }
+int chain_size(const int x) {
+    if (x <= 1) return x;
+
+    if (x % 2) {
+        return 1 + chain_size(3 * x + 1);
     }
-    return memo[x];
+
+    return 1 + chain_size(x / 2);
 }
 
-int max_chain_size_in_range(vector<int> &memo, const int lo, const int hi) {
+int max_chain_size_in_range(const int lo, const int hi) {
     assert(lo <= hi);
 
     int ans = 0;
     for (int x = lo; x <= hi; ++x) {
-        ans = max(ans, chain_size(memo, x));
+        ans = max(ans, chain_size(x));
     }
     return ans;
 }
 
 int main() {
-    vector<int> memo(LIM, -1);
-
     for (string line; getline(cin, line);) {
         stringstream ss(line);
         int a, b;
         ss >> a >> b;
 
         cout << a << ' ' << b << ' '
-             << max_chain_size_in_range(memo, min(a, b), max(a, b)) << '\n';
+             << max_chain_size_in_range(min(a, b), max(a, b)) << '\n';
     }
 
     return 0;
