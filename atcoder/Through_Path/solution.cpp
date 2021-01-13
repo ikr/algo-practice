b0e815971235d954a06c4e83bfe3b0262d431668
Ivan Krechetov <ikr@ikr.su>
2021-01-13 19:35:33 +0100

    Adjust the Command model

--- atcoder/Through_Path/solution.cpp
+++ atcoder/Through_Path/solution.cpp
@@ -3,13 +3,17 @@ using namespace std;
 using ll = long long;
 using Graph = multimap<int, int>;
 
-enum class Dir { UP, DOWN };
+enum class Opt { ONLY, EXCLUDE };
 
 struct Command final {
-    Dir dir;
-    int edge_index;
+    Opt opt;
+    int vertex;
     ll increment;
 };
+
+template <typename T> constexpr pair<T, T> flip_pair(const pair<T, T> &ab) {
+    return {ab.second, ab.first};
+}
 
 Graph build_graph(const vector<pair<int, int>> &edges) {
     Graph ans;
@@ -20,9 +24,9 @@ Graph build_graph(const vector<pair<int, int>> &edges) {
     return ans;
 }
 
-Graph as_zero_rooted_tree(const int n, const Graph &g) {
+vector<set<int>> as_zero_rooted_tree(const int n, const Graph &g) {
     vector<bool> visited(n, false);
-    Graph ans;
+    vector<set<int>> ans;
 
     function<void(int)> dfs;
     dfs = [&](const int u) {
@@ -33,7 +37,7 @@ Graph as_zero_rooted_tree(const int n, const Graph &g) {
             const auto v = it->second;
             if (visited[v]) continue;
 
-            ans.emplace(u, v);
+            ans[u].insert(v);
             dfs(v);
         }
     };
@@ -42,10 +46,17 @@ Graph as_zero_rooted_tree(const int n, const Graph &g) {
     return ans;
 }
 
-vector<ll> execute_commands(const vector<pair<int, int>> &edges,
+vector<ll> execute_commands(const int n, const vector<set<int>> &tree,
                             const vector<Command> &commands) {
-    const int n = edges.size() + 1;
-    const auto tree = as_zero_rooted_tree(n, build_graph(edges));
+    vector<ll> pro(n, 0);
+    for (const auto &[opt, vertex, increment] : commands) {
+        if (opt == Opt::EXCLUDE) {
+            pro[0] += increment;
+            pro[vertex] -= increment;
+        } else {
+            pro[vertex] += increment;
+        }
+    }
 
     vector<ll> ans(n, 0);
     return ans;
@@ -65,21 +76,35 @@ int main() {
         --b;
     }
 
+    const auto tree = as_zero_rooted_tree(n, build_graph(edges));
+
     int q;
     cin >> q;
 
     vector<Command> commands(q);
-    for (auto &[dir, edge_index, increment] : commands) {
+    for (auto &[opt, vertex, increment] : commands) {
         int t;
         cin >> t;
-        dir = (t == 1) ? Dir::UP : Dir::DOWN;
 
+        int edge_index;
         cin >> edge_index;
         --edge_index;
 
         cin >> increment;
+
+        const auto [a, b] =
+            t == 1 ? edges[edge_index] : flip_pair(edges[edge_index]);
+
+        const bool a_is_parent = tree[a].count(b);
+        if (a_is_parent) {
+            opt = Opt::EXCLUDE;
+            vertex = b;
+        } else {
+            opt = Opt::ONLY;
+            vertex = a;
+        }
     }
 
-    for (const auto c : execute_commands(edges, commands)) cout << c << '\n';
+    for (const auto c : execute_commands(n, tree, commands)) cout << c << '\n';
     return 0;
 }
