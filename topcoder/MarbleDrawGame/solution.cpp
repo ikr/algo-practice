aa1cef159d9e33538b94d7d734dd730aca3709b0
Ivan Krechetov <ikr@ikr.li>
2021-01-18 19:18:15 +0100

    Solve the remaining C

--- topcoder/MarbleDrawGame/solution.cpp
+++ topcoder/MarbleDrawGame/solution.cpp
@@ -11,13 +11,22 @@ ll factorial(const ll x) {
     for (ll i = 3; i <= x; ++i) {
         ans *= i;
     }
+    assert(ans > 0LL);
+    return ans;
+}
 
+ll product_of_range(const ll a, const ll b) {
+    ll ans = 1;
+    for (ll x = a; x <= b; ++x) ans *= x;
+    assert(ans > 0LL);
     return ans;
 }
 
 ll combinations(const ll n, const ll k) {
     assert(k <= n);
     const ll r = n - k;
+    return r < k ? product_of_range(k + 1, n) / factorial(r)
+                 : product_of_range(r + 1, n) / factorial(k);
 }
 
 template <typename T> constexpr double doof(const T x) {
@@ -26,39 +35,39 @@ template <typename T> constexpr double doof(const T x) {
 
 struct MarbleDrawGame final {
     double winningChance(const vector<int> &xs) const {
-        const int m = accumulate(xs.cbegin(), xs.cend(), 0);
-        double ans = 0.0;
+        const ll m = accumulate(xs.cbegin(), xs.cend(), 0);
+        const ll denom = combinations(m, 9);
+
+        ll t_num = 0LL;
 
         for (const int x : xs) {
-            if (x < 5) continue;
-
-            const int k = 9;
-
-            for (int i0 = 0; i0 < k - 4; ++i0) {
-                for (int i1 = i0 + 1; i1 < k - 3; ++i1) {
-                    for (int i2 = i1 + 1; i2 < k - 2; ++i2) {
-                        for (int i3 = i2 + 1; i3 < k - 1; ++i3) {
-                            for (int i4 = i3 + 1; i4 < k; ++i4) {
-                                double p = doof(x) / doof(m - i0);
-                                p *= doof(x - 1) / doof(m - i1);
-                                p *= doof(x - 2) / doof(m - i2);
-                                p *= doof(x - 3) / doof(m - i3);
-                                p *= doof(x - 4) / doof(m - i4);
-
-                                ans += p;
-                            }
-                        }
-                    }
-                }
+            for (const int s : {5, 6, 7, 8, 9}) {
+                if (x < s) break;
+                t_num += combinations(x, s) * combinations(m - x, 9 - s);
             }
         }
 
-        return ans;
+        return doof(t_num) / doof(denom);
     }
 };
 
 // clang-format off
 const lest::test tests[] = {
+    CASE("Combi 10 of 10") {
+        const auto actual = combinations(10, 10);
+        const auto expected = 1LL;
+        EXPECT(actual == expected);
+    },
+    CASE("Combi 10 of 1") {
+        const auto actual = combinations(10, 1);
+        const auto expected = 10LL;
+        EXPECT(actual == expected);
+    },
+    CASE("Combi 52 of 5") {
+        const auto actual = combinations(52, 5);
+        const auto expected = 2598960LL;
+        EXPECT(actual == expected);
+    },
     CASE("Example 0") {
         const auto actual = MarbleDrawGame{}.winningChance({5, 5});
         const auto expected = 1.0;
