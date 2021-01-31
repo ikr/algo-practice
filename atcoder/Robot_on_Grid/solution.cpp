b03dcf35ef68c37e72a399b59f2043a289c1d257
Ivan Krechetov <ikr@ikr.li>
2021-01-31 17:51:53 +0100

    Reduce

--- atcoder/Robot_on_Grid/solution.cpp
+++ atcoder/Robot_on_Grid/solution.cpp
@@ -80,57 +80,6 @@ mint count_ways(const vector<string> &rows) {
 }
 
 namespace oracle {
-static constexpr int mod = 998244353;
-using ll = long long;
-
-struct mint {
-    ll x;
-    mint(ll x = 0) : x((x % mod + mod) % mod) {}
-    mint operator-() const { return mint(-x); }
-    mint &operator+=(const mint a) {
-        if ((x += a.x) >= mod) x -= mod;
-        return *this;
-    }
-    mint &operator-=(const mint a) {
-        if ((x += mod - a.x) >= mod) x -= mod;
-        return *this;
-    }
-    mint &operator*=(const mint a) {
-        (x *= a.x) %= mod;
-        return *this;
-    }
-    mint operator+(const mint a) const { return mint(*this) += a; }
-    mint operator-(const mint a) const { return mint(*this) -= a; }
-    mint operator*(const mint a) const { return mint(*this) *= a; }
-    mint pow(ll t) const {
-        if (!t) return 1;
-        mint a = pow(t >> 1);
-        a *= a;
-        if (t & 1) a *= *this;
-        return a;
-    }
-
-    mint inv() const { return pow(mod - 2); }
-    mint &operator/=(const mint a) { return *this *= a.inv(); }
-    mint operator/(const mint a) const { return mint(*this) /= a; }
-};
-istream &operator>>(istream &is, const mint &a) { return is >> a.x; }
-ostream &operator<<(ostream &os, const mint &a) { return os << a.x; }
-struct combination {
-    vector<mint> fact, ifact;
-    combination(int n) : fact(n + 1), ifact(n + 1) {
-        assert(n < mod);
-        fact[0] = 1;
-        for (int i = 1; i <= n; ++i) fact[i] = fact[i - 1] * i;
-        ifact[n] = fact[n].inv();
-        for (int i = n; i >= 1; --i) ifact[i - 1] = ifact[i] * i;
-    }
-    mint operator()(int n, int k) {
-        if (k < 0 || k > n) return 0;
-        return fact[n] * ifact[k] * ifact[n - k];
-    }
-} c(200005);
-
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 
 int solve(const vector<string> &s, const int K) {
@@ -139,7 +88,7 @@ int solve(const vector<string> &s, const int K) {
 
     vector<vector<mint>> dp(H, vector<mint>(W));
     mint p = 2;
-    p *= mint(3).pow(mod - 2);
+    p *= mint(3).inv();
     dp[0][0] = 1;
     rep(i, H) {
         rep(j, W) {
@@ -155,7 +104,7 @@ int solve(const vector<string> &s, const int K) {
             }
         }
     }
-    return (dp[H - 1][W - 1] * mint(3).pow(H * W - K)).x;
+    return (dp[H - 1][W - 1] * mint(3).pow(H * W - K)).val();
 }
 } // namespace oracle
 
