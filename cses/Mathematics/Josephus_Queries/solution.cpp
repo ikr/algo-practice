7da7be56d8e46247f7fdd9a9d024174a11caa558
Ivan Krechetov <ikr@ikr.su>
2021-02-05 17:09:03 +0100

    WIP

--- cses/Mathematics/Josephus_Queries/solution.cpp
+++ cses/Mathematics/Josephus_Queries/solution.cpp
@@ -1,24 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct State final {
+int div_ceil(const int numerator, const int denominator) {
+    const int q = numerator / denominator;
+    return (numerator % denominator) ? (q + 1) : q;
+}
+
+enum class Start { ZERO, ONE };
+
+struct Circle final {
     int round_index;
     int lo;
     int hi;
-    int start_index;
+    int num;
+    Start start_index;
 
     int delta() const { return round_index * 2; }
-
-    int child_in_this_round(const int index) const {
-        return lo + delta() * index;
-    }
+    int child_at(const int index) const { return lo + delta() * index; }
 
     bool is_reached_this_round(const int index) const {
-        return child_in_this_round(index) <= hi;
+        return child_at(index) <= hi;
+    }
+
+    int num_removed_this_round() const {
+        return start_index == Start::ZERO ? div_ceil(num, 2) : num / 2;
     }
 };
 
-int kth_child_removed(const int n, const int k) { return 0; }
+int kth_child_removed(const int n, int k) {
+    Circle ci{0, 1, n, n, Start::ONE};
+
+    while (!ci.is_reached_this_round(k)) {
+        const int d = ci.num_removed_this_round();
+        ci = Circle{ci.round_index + 1, };
+    }
+
+    return ci.child_at(ci.start_index == Start::ZERO ? k : k + 1);
+}
 
 int main() {
     ios_base::sync_with_stdio(0);
@@ -29,6 +47,7 @@ int main() {
     while (t--) {
         int n, k;
         cin >> n >> k;
+        --k;
         cout << kth_child_removed(n, k) << '\n';
     }
 
