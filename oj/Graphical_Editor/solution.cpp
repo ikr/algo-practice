29912b397ef4e5077cd49111628875ea1e7ce1eb
Ivan Krechetov <ikr@ikr.li>
2021-02-10 20:55:12 +0100

    WIP

---
+++ oj/Graphical_Editor/solution.cpp
@@ -0,0 +1,191 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T, typename... Args>
+unique_ptr<T> make_unique(Args &&... args) {
+    return unique_ptr<T>(new T(forward<Args>(args)...));
+}
+
+using Color = char;
+static constexpr Color WHITE = 'O';
+
+enum class Code {
+    CREATE,
+    CLEAR,
+    DOT,
+    VLINE,
+    HLINE,
+    RECT,
+    FILL,
+    SAVE,
+    EXIT,
+    NOOP
+};
+
+struct Cmd {
+    explicit Cmd(const Code code_) : code(code_) {}
+    Cmd() = delete;
+    virtual ~Cmd() {}
+
+    Code code;
+};
+
+struct CmdCreate final : public Cmd {
+    CmdCreate(const int x_sz_, const int y_sz_)
+        : Cmd(Code::CREATE), x_sz(x_sz_), y_sz(y_sz_) {}
+    CmdCreate() = delete;
+
+    int x_sz;
+    int y_sz;
+};
+
+struct CmdDot final : public Cmd {
+    CmdDot(const int x_, const int y_, const Color color_)
+        : Cmd(Code::DOT), x(x_), y(y_), color(color_) {}
+    CmdDot() = delete;
+
+    int x;
+    int y;
+    Color color;
+};
+
+struct CmdVLine final : public Cmd {
+    CmdVLine(const int x_, const int y1_, const int y2_, const Color color_)
+        : Cmd(Code::VLINE), x(x_), y1(y1_), y2(y2_), color(color_) {}
+    CmdVLine() = delete;
+
+    int x;
+    int y1;
+    int y2;
+    Color color;
+};
+
+struct CmdHLine final : public Cmd {
+    CmdHLine(const int x1_, const int x2_, const int y_, const Color color_)
+        : Cmd(Code::HLINE), x1(x1_), x2(x2_), y(y_), color(color_) {}
+    CmdHLine() = delete;
+
+    int x1;
+    int x2;
+    int y;
+    Color color;
+};
+
+struct CmdRect final : public Cmd {
+    CmdRect(const int x1_, const int y1_, const int x2_, const int y2_,
+            const Color color_)
+        : Cmd(Code::RECT), x1(x1_), y1(y1_), x2(x2_), y2(y2_), color(color_) {}
+    CmdRect() = delete;
+
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+    Color color;
+};
+
+struct CmdFill final : public Cmd {
+    CmdFill(const int x_, const int y_, const Color color_)
+        : Cmd(Code::FILL), x(x_), y(y_), color(color_) {}
+    CmdFill() = delete;
+
+    int x;
+    int y;
+    Color color;
+};
+
+struct CmdSave final : public Cmd {
+    explicit CmdSave(const string &name_) : Cmd(Code::SAVE), name(name_) {}
+    CmdSave() = delete;
+    string name;
+};
+
+vector<string> split(const regex &delim, const string &s) {
+    return vector<string>(sregex_token_iterator(begin(s), end(s), delim, -1),
+                          sregex_token_iterator{});
+}
+
+Code parse_command(const char literal) {
+    static const map<char, Code> codes_by_literal{
+        {'I', Code::CREATE}, {'C', Code::CLEAR}, {'L', Code::DOT},
+        {'V', Code::VLINE},  {'H', Code::HLINE}, {'K', Code::RECT},
+        {'F', Code::FILL},   {'S', Code::SAVE},  {'X', Code::EXIT}};
+
+    return codes_by_literal.count(literal) ? codes_by_literal.at(literal)
+                                           : Code::NOOP;
+}
+
+unique_ptr<Cmd> parse_line(const string &line) {
+    const auto terms = split(regex{" "}, line);
+    const auto code = parse_command(terms[0][0]);
+
+    switch (code) {
+    case Code::CREATE:
+        return make_unique<CmdCreate>(stoi(terms[1]), stoi(terms[2]));
+    case Code::DOT:
+        return make_unique<CmdDot>(stoi(terms[1]) - 1, stoi(terms[2]) - 1,
+                                   terms[3][0]);
+    case Code::VLINE:
+        return make_unique<CmdVLine>(stoi(terms[1]) - 1, stoi(terms[2]) - 1,
+                                     stoi(terms[3]) - 1, terms[4][0]);
+    case Code::HLINE:
+        return make_unique<CmdHLine>(stoi(terms[1]) - 1, stoi(terms[2]) - 1,
+                                     stoi(terms[3]) - 1, terms[4][0]);
+    case Code::RECT:
+        return make_unique<CmdRect>(stoi(terms[1]) - 1, stoi(terms[2]) - 1,
+                                    stoi(terms[3]) - 1, stoi(terms[4]) - 1,
+                                    terms[5][0]);
+    case Code::FILL:
+        return make_unique<CmdFill>(stoi(terms[1]) - 1, stoi(terms[2]) - 1,
+                                    terms[3][0]);
+    case Code::SAVE:
+        return make_unique<CmdSave>(terms[1]);
+    default:
+        return make_unique<Cmd>(code);
+    }
+}
+
+void handle_create(vector<string> &raster, const CmdCreate &create) {
+    raster = vector<string>(create.y_sz, string(create.x_sz, WHITE));
+}
+
+int main() {
+    vector<string> raster;
+    for (string line; getline(cin, line);) {
+        const auto cmd = parse_line(line);
+
+        switch (cmd->code) {
+        case Code::CREATE:
+            handle_create(raster, dynamic_cast<const CmdCreate &>(*cmd));
+            break;
+
+        case Code::CLEAR:
+            break;
+
+        case Code::DOT:
+            break;
+
+        case Code::VLINE:
+            break;
+
+        case Code::HLINE:
+            break;
+
+        case Code::RECT:
+            break;
+
+        case Code::FILL:
+            break;
+
+        case Code::SAVE:
+            break;
+
+        case Code::NOOP:
+            break;
+
+        case Code::EXIT:
+            return 0;
+        }
+    }
+    return 0;
+}
