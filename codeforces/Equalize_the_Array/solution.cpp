e5ae4a234d14c14bef7c3665bcb0ff68b06245bd
Ivan Krechetov <ikr@ikr.su>
2021-02-24 07:27:28 +0100

    AC

--- codeforces/Equalize_the_Array/solution.cpp
+++ codeforces/Equalize_the_Array/solution.cpp
@@ -1,19 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> gather_freqs(const vector<int> &xs) {
+vector<int> sorted_freqs(const vector<int> &xs) {
     map<int, int> counts_by_x;
     for (const auto x : xs) ++counts_by_x[x];
 
     vector<int> ans(counts_by_x.size());
     transform(cbegin(counts_by_x), cend(counts_by_x), begin(ans),
               [](const auto v) { return v.second; });
+
+    sort(begin(ans), end(ans));
+    return ans;
+}
+
+vector<int> prefix_sums(const vector<int> &xs) {
+    vector<int> ans(xs.size());
+    partial_sum(cbegin(xs), cend(xs), begin(ans));
     return ans;
 }
 
 int min_removals(const vector<int> &xs) {
-    const auto fs = gather_freqs(xs);
-    return 0;
+    const auto fs = sorted_freqs(xs);
+    const auto ss = prefix_sums(fs);
+
+    const int n = fs.size();
+    int ans = ss.back() - fs[0] * n;
+
+    for (int i = 1; i < n; ++i) {
+        const int tail_sz = n - i;
+        const int tail_val = ss.back() - ss[i - 1];
+        const int candidate = ss[i - 1] + tail_val - fs[i] * tail_sz;
+
+        ans = min(ans, candidate);
+    }
+
+    return ans;
 }
 
 int main() {
