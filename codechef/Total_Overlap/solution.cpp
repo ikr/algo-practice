4911636c5f51c8a9d6e813041dd55bd1b8c5f65e
Ivan Krechetov <ikr@ikr.su>
2021-02-26 21:57:45 +0100

    WIP

--- codechef/Total_Overlap/solution.cpp
+++ codechef/Total_Overlap/solution.cpp
@@ -2,7 +2,41 @@
 using namespace std;
 using pi = pair<int, int>;
 
-int total_overlap(vector<pi> xs, vector<pi> ys) { return -1; }
+constexpr bool intersect(const pi ab, const pi cd) {
+    const auto [a, b] = ab;
+    const auto [c, d] = cd;
+    return !(b < c || d < a);
+}
+
+constexpr pi span(const pi ab, const pi cd) {
+    const auto [a, b] = ab;
+    const auto [c, d] = cd;
+    return {min({a, b, c, d}), max({a, b, c, d})};
+}
+
+vector<pi> unite_intersecting(vector<pi> xs) {
+    sort(begin(xs), end(xs));
+    vector<pi> ans;
+    ans.reserve(xs.size() / 16);
+
+    for (const auto cd : xs) {
+        if (ans.empty()) {
+            ans.push_back(cd);
+        } else {
+            const auto ab = ans.back();
+            if (intersect(ab, cd)) {
+                ans.back() = span(ab, cd);
+            }
+        }
+    }
+
+    return ans;
+}
+
+int total_overlap(vector<pi> xs, vector<pi> ys) {
+    ys = unite_intersecting(ys);
+    return -1;
+}
 
 int main() {
     ios_base::sync_with_stdio(0);
