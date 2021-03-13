0244287acdca59088d8068a54cca3924a42f2cac
Ivan Krechetov <ikr@ikr.li>
2021-03-13 09:59:08 +0100

    Clean up

--- atcoder/Candies/solution.cpp
+++ atcoder/Candies/solution.cpp
@@ -2,23 +2,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
 using vi = vector<int>;
-using vvi = vector<vi>;
-using pii = pair<int, int>;
-using vll = vector<ll>;
-using vvll = vector<vll>;
-
 using mint = atcoder::modint1000000007;
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
-}
-template <typename T> constexpr ll llof(const T x) {
-    return static_cast<ll>(x);
-}
-template <typename T> constexpr double doof(const T x) {
-    return static_cast<double>(x);
 }
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
