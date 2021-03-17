b61b4c2ace100e91d214cef6543fd7f8858128ac
Ivan Krechetov <ikr@ikr.su>
2021-03-17 19:01:05 +0100

    Clean up

--- topcoder/MoveAllMarbles/solution.cpp
+++ topcoder/MoveAllMarbles/solution.cpp
@@ -2,26 +2,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
 using vi = vector<int>;
 using vvi = vector<vi>;
-using pii = pair<int, int>;
-using vll = vector<ll>;
-using vvll = vector<vll>;
 
-template <typename T> constexpr int inof(const T x) {
-    return static_cast<int>(x);
-}
-template <typename T> constexpr ll llof(const T x) {
-    return static_cast<ll>(x);
-}
-template <typename T> constexpr double doof(const T x) {
-    return static_cast<double>(x);
-}
-
-template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
-
-void push_back_all(vi &dst, const vi &src) {
+void push_back_all(vi &dst, const array<int, 4> &src) {
     dst.insert(dst.cend(), src.cbegin(), src.cend());
 }
 
