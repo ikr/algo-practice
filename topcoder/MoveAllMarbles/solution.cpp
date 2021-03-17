3b00aa5f980f4f4983a629ff209b00485698c930
Ivan Krechetov <ikr@ikr.su>
2021-03-17 18:57:54 +0100

    Draft a solution
    Can't check it yet, as the problem doesn't yet appear in the practice area.

--- topcoder/MoveAllMarbles/solution.cpp
+++ topcoder/MoveAllMarbles/solution.cpp
@@ -21,49 +21,42 @@ template <typename T> constexpr double doof(const T x) {
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-vi path_of_one(const int h, const int w) {
-    vi ans{0, 0};
-    ans.reserve((h + w) * 2);
-
-    for (int ro = 1; ro < h; ++ro) {
-        const int i = *next(ans.rbegin());
-        const int j = ans.back();
-
-        if (sz(ans) > 2) {
-            ans.push_back(i);
-            ans.push_back(j);
-        }
-
-        ans.push_back(i + 1);
-        ans.push_back(j);
-    }
-
-    for (int co = 1; co < w; ++co) {
-        const int i = *next(ans.rbegin());
-        const int j = ans.back();
-
-        if (sz(ans) > 2) {
-            ans.push_back(i);
-            ans.push_back(j);
-        }
-
-        ans.push_back(i);
-        ans.push_back(j + 1);
-    }
-
-    return ans;
+void push_back_all(vi &dst, const vi &src) {
+    dst.insert(dst.cend(), src.cbegin(), src.cend());
 }
 
 struct MoveAllMarbles final {
     vi move(const int h, const int w, const int n) const {
-        const auto pattern = path_of_one(h, w);
-        vi ans(sz(pattern) * n, 0);
+        vvi rows(h, vi(w, 0));
+        rows[0][0] = n;
 
-        for (int i = 0; i < n; ++i) {
-            assert((i + 1) * sz(pattern) <= sz(ans));
+        vi ans;
+        ans.reserve((h + w) * n);
 
-            copy(pattern.cbegin(), pattern.cend(),
-                 begin(ans) + i * sz(pattern));
+        const auto is_free = [&](const int ro, const int co) -> bool {
+            if (ro < 0 || ro >= h || co < 0 || co >= w) return false;
+            if (ro == h - 1 && co == w - 1) return true;
+            return rows[ro][co] == 0;
+        };
+
+        while (rows.back().back() != n) {
+            for (int ro = 0; ro < h; ++ro) {
+                for (int co = 0; co < w; ++co) {
+                    if (ro == h - 1 && co == w - 1) continue;
+
+                    if (rows[ro][co]) {
+                        if (is_free(ro + 1, co)) {
+                            --rows[ro][co];
+                            ++rows[ro + 1][co];
+                            push_back_all(ans, {ro, co, ro + 1, co});
+                        } else if (is_free(ro, co + 1)) {
+                            --rows[ro][co];
+                            ++rows[ro][co + 1];
+                            push_back_all(ans, {ro, co, ro, co + 1});
+                        }
+                    }
+                }
+            }
         }
 
         return ans;
