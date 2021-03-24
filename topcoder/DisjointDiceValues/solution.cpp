17feb5534285a7bfa46b7258f225ecb9de31b00d
Ivan Krechetov <ikr@ikr.su>
2021-03-24 15:24:08 +0100

    Successfully test against an oracle implementation
    The Oracle code is from Shadek's blog post:
    https://shadekcse.wordpress.com/2021/03/20/srm-802-div2-hard-by-dp/
    
    Giving up convincing the Topcoder OJ :(

--- topcoder/DisjointDiceValues/solution.cpp
+++ topcoder/DisjointDiceValues/solution.cpp
@@ -59,6 +59,55 @@ struct DisjointDiceValues final {
     }
 };
 
+#define forl(i, a, b) for (i = a; i < b; i++)
+
+long long int dp[20][130][130];
+int n, m;
+
+bool iscommon(int m1, int m2) {
+    int i;
+    forl(i, 1, 7) {
+        if ((m1 & (1 << i)) and (m2 & (1 << i))) return true;
+    }
+    return false;
+}
+
+long long int doit(int ind, int mask1, int mask2) {
+    if (ind == n + m) {
+        return iscommon(mask1, mask2);
+    }
+    if (dp[ind][mask1][mask2] != -1) return dp[ind][mask1][mask2];
+    long long int ans = 0;
+    // fill Alice's elements
+    int i, j, k;
+    if (ind < n) {
+        forl(i, 1, 7) { ans += doit(ind + 1, mask1 | (1 << i), mask2); }
+    } else { // fill Bob's elements
+        forl(i, 1, 7) { ans += doit(ind + 1, mask1, mask2 | (1 << i)); }
+    }
+
+    dp[ind][mask1][mask2] = ans;
+    return ans;
+}
+class Oracle {
+  public:
+    double getProbability(int A, int B) {
+        long long int total = pow(6, A + B);
+        n = A;
+        m = B;
+        int i, j, k, a, b;
+        forl(i, 0, 20) {
+            forl(k, 0, 130) {
+                forl(a, 0, 130) { dp[i][k][a] = -1; }
+            }
+        }
+
+        long long int r = doit(0, 0, 0);
+        double ans = r;
+        ans /= total;
+        return ans;
+    }
+};
 // clang-format off
 const lest::test tests[] = {
     CASE("Example 0") {
@@ -83,7 +132,22 @@ const lest::test tests[] = {
     },
     CASE("Example A") {
         const auto actual = DisjointDiceValues{}.getProbability(8, 8);
-        const auto expected = 0.999585;
+        const auto expected = Oracle{}.getProbability(8, 8);
+        EXPECT(abs(actual - expected) < 0.000001);
+    },
+    CASE("Example B") {
+        const auto actual = DisjointDiceValues{}.getProbability(3, 4);
+        const auto expected = Oracle{}.getProbability(3, 4);
+        EXPECT(abs(actual - expected) < 0.000001);
+    },
+    CASE("Example C") {
+        const auto actual = DisjointDiceValues{}.getProbability(1, 8);
+        const auto expected = Oracle{}.getProbability(1, 8);
+        EXPECT(abs(actual - expected) < 0.000001);
+    },
+    CASE("Example D") {
+        const auto actual = DisjointDiceValues{}.getProbability(5, 2);
+        const auto expected = Oracle{}.getProbability(5, 2);
         EXPECT(abs(actual - expected) < 0.000001);
     },
 };
