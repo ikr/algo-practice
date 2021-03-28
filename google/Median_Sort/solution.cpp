fcf3b22a27441e68757b35d52fd0b2eca3c296f3
Ivan Krechetov <ikr@ikr.su>
2021-03-28 10:17:40 +0200

    WIP

--- google/Median_Sort/solution.cpp
+++ google/Median_Sort/solution.cpp
@@ -1,4 +1,6 @@
+#include <algorithm>
 #include <bits/stdc++.h>
+#include <iterator>
 using namespace std;
 
 using vi = vector<int>;
@@ -20,89 +22,27 @@ template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
 
 using Tri = tuple<int, int, int>;
 
-int query(const Tri ijk, int &Q, map<Tri, int> &memo) {
-    if (memo.count(ijk)) return memo.at(ijk);
+ostream &operator<<(ostream &os, const Tri &x) {
+    const auto [i, j, k] = x;
+    os << '(' << i << ' ' << j << ' ' << k << ')';
+    return os;
+}
 
+int query(const Tri ijk, int &Q) {
     assert(Q > 0);
+
     const auto [i, j, k] = ijk;
+    assert(i != j && j != k && i != k);
     cout << i << ' ' << j << ' ' << k << endl;
 
     int ans;
     cin >> ans;
     --Q;
 
-    vi p{i, j, k};
-    sort(begin(p), end(p));
-    do {
-        memo[Tri{p[0], p[1], p[2]}] = ans;
-    } while (next_permutation(begin(p), end(p)));
-
     return ans;
 }
 
 using Iter = vi::const_iterator;
-
-bool is_first_edge(const Iter first, const Iter last, const Iter x, int &Q,
-                   map<Tri, int> &memo) {
-    assert(x != last);
-    assert(distance(first, last) > 2);
-
-    for (auto it = first; next(it) != last; ++it) {
-        for (auto jt = next(it); jt != last; ++jt) {
-            if (it == x || jt == x) continue;
-
-            const auto mid = query({*it, *x, *jt}, Q, memo);
-            if (mid == *x) return false;
-        }
-    }
-
-    return true;
-}
-
-bool is_second_edge(const Iter first, const Iter last, const Iter x0,
-                    const Iter x, int &Q, map<Tri, int> &memo) {
-    assert(x != last);
-    assert(x != x0);
-    assert(distance(first, last) > 2);
-
-    for (auto it = first; it != last; ++it) {
-        if (it == x0 || it == x) continue;
-        const auto mid = query({*x0, *x, *it}, Q, memo);
-        if (mid == *x) return false;
-    }
-
-    return true;
-}
-
-vi extract_edge(vi &xs, int &Q, map<Tri, int> &memo) {
-    if (sz(xs) < 3) {
-        vi ans = xs;
-        xs.clear();
-        return ans;
-    }
-
-    vector<Iter> its;
-    for (auto it = cbegin(xs); it != cend(xs); ++it) {
-        if (its.empty()) {
-            if (is_first_edge(cbegin(xs), cend(xs), it, Q, memo)) {
-                its.push_back(it);
-            }
-        } else {
-            if (is_second_edge(cbegin(xs), cend(xs), its[0], it, Q, memo)) {
-                its.push_back(it);
-                break;
-            }
-        }
-    }
-
-    assert(sz(its) == 2);
-    sort(begin(its), end(its));
-
-    vi ans{*its[0], *its[1]};
-    xs.erase(its[1]);
-    xs.erase(its[0]);
-    return ans;
-}
 
 vi one_to(const int N) {
     vi ans(N);
@@ -110,53 +50,61 @@ vi one_to(const int N) {
     return ans;
 }
 
-vi join(vi head, vi tail) {
-    head.insert(end(head), crbegin(tail), crend(tail));
-    return head;
-}
+pair<Iter, Iter> edges(const pii bounds, const pair<Iter, Iter> range, int &Q) {
+    const auto [first, last] = range;
+    assert(distance(first, last) > 1);
+    pair<Iter, Iter> ans{first, next(first)};
 
-pii orient(const pii curr_edge, const pii new_edge, int &Q,
-           map<Tri, int> &memo) {
-    const auto [a, b] = curr_edge;
-    const auto [c, d] = new_edge;
-
-    return (query({a, c, d}, Q, memo) == c) ? pii{c, d} : pii{d, c};
-}
-
-vi derive_order(const int N, int &Q, map<Tri, int> &memo) {
-    vi head;
-    vi tail;
-
-    vi xs = one_to(N);
-
-    random_device rd;
-    mt19937 g(rd());
-    shuffle(begin(xs), end(xs), g);
-
-    while (!xs.empty()) {
-        if (!Q) return one_to(N);
-        const auto e = extract_edge(xs, Q, memo);
-
-        if (sz(e) == 1) {
-            assert(!head.empty());
-            head.push_back(e[0]);
-        } else {
-            if (head.empty()) {
-                assert(tail.empty());
-                head.push_back(e[0]);
-                tail.push_back(e[1]);
-            } else {
-                if (!Q) return one_to(N);
-
-                const auto [c, d] =
-                    orient({head.back(), tail.back()}, {e[0], e[1]}, Q, memo);
-                head.push_back(c);
-                tail.push_back(d);
-            }
+    if (bounds.first) {
+        if (query({bounds.first, *ans.first, *ans.second}, Q) != *ans.first) {
+            swap(ans.first, ans.second);
+        }
+    } else if (bounds.second) {
+        if (query({*ans.first, *ans.second, bounds.second}, Q) != *ans.second) {
+            swap(ans.first, ans.second);
         }
     }
 
-    return join(head, tail);
+    for (auto it = first; it != last; ++it) {
+        if (it == ans.first || it == ans.second) continue;
+
+        const auto mid = query({*ans.first, *ans.second, *it}, Q);
+        if (mid == *ans.first) {
+            ans.first = it;
+        } else if (mid == *ans.second) {
+            ans.second = it;
+        }
+    }
+
+    return ans;
+}
+
+vi concat(vi xs, const vi &ys, const vi &zs) {
+    xs.insert(cend(xs), cbegin(ys), cend(ys));
+    xs.insert(cend(xs), cbegin(zs), cend(zs));
+    return xs;
+}
+
+vi med_sort(const pii bounds, const pair<Iter, Iter> range, int &Q) {
+    const auto [first, last] = range;
+    if (distance(first, last) < 2) return vi(first, last);
+
+    const auto [it, jt] = edges(bounds, range, Q);
+    assert(it != last);
+    assert(jt != last);
+    assert(distance(it, jt) > 0);
+
+    const int a = *it;
+    const int b = *jt;
+
+    return concat(med_sort({bounds.first, a}, {first, it}, Q),
+                  med_sort({a, b}, {next(it), jt}, Q),
+                  med_sort({b, bounds.second}, {next(jt), last}, Q));
+}
+
+vi derive_order(const int N, int &Q) {
+    vi xs = one_to(N);
+    return med_sort({0, 0}, {cbegin(xs), cend(xs)}, Q);
 }
 
 int main() {
@@ -168,8 +116,7 @@ int main() {
     cin >> T >> N >> Q;
 
     while (T--) {
-        map<Tri, int> memo;
-        cout << derive_order(N, Q, memo) << endl;
+        cout << derive_order(N, Q) << endl;
         int verdict;
         cin >> verdict;
     }
