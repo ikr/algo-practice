b972d4e9e30c4da06c391d1b6bac6577e89b5948
Ivan Krechetov <ikr@ikr.su>
2021-03-29 18:39:17 +0200

    Restart

--- google/2021/Kick_Start_A/Checksum/solution.cpp
+++ google/2021/Kick_Start_A/Checksum/solution.cpp
@@ -4,76 +4,30 @@ using namespace std;
 using ll = long long;
 using vi = vector<int>;
 using vvi = vector<vi>;
-using pii = pair<int, int>;
-using vll = vector<ll>;
-using vvll = vector<vll>;
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
-template <typename T> constexpr ll llof(const T x) {
-    return static_cast<ll>(x);
-}
-template <typename T> constexpr double doof(const T x) {
-    return static_cast<double>(x);
-}
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-using Coord = complex<int>;
-constexpr int row(const Coord coord) { return coord.real(); }
-constexpr int col(const Coord coord) { return coord.imag(); }
-
-struct q_less final {
-    bool operator()(const pair<int, Coord> &a,
-                    const pair<int, Coord> &b) const {
-        return pair{a.first, pair{row(a.second), col(a.second)}} <
-               pair{b.first, pair{row(b.second), col(b.second)}};
-    }
-};
-
-using Queue =
-    priority_queue<pair<int, Coord>, vector<pair<int, Coord>>, q_less>;
-
-vi missing_num_by_row(const vvi &src) {
-    vi ans(sz(src), 0);
+pair<vi, vi> missing_count_by_row_by_col(const vvi &src) {
+    vi by_row(sz(src), 0);
+    vi by_col(sz(src), 0);
 
     for (int ro = 0; ro < sz(src); ++ro) {
         for (int co = 0; co < sz(src); ++co) {
-            if (src[ro][co] == -1) ++ans[ro];
+            if (src[ro][co] == -1) {
+                ++by_row[ro];
+                ++by_col[co];
+            }
         }
     }
 
-    return ans;
+    return {by_row, by_col};
 }
 
-vi missing_num_by_col(const vvi &src) {
-    vi ans(sz(src), 0);
-
-    for (int ro = 0; ro < sz(src); ++ro) {
-        for (int co = 0; co < sz(src); ++co) {
-            if (src[ro][co] == -1) ++ans[co];
-        }
-    }
-
-    return ans;
-}
-
-Queue initial_queue(const vvi &costs) {
-    Queue ans;
-
-    for (int ro = 0; ro < sz(costs); ++ro) {
-        for (int co = 0; co < sz(costs); ++co) {
-            ans.emplace(-costs[ro][co], Coord{ro, co});
-        }
-    }
-
-    return ans;
-}
-
-int solve(const vvi &src, const vvi &costs, const vi &rs, const vi &cs) {
-    return -1;
-}
+ll solve(const vvi &src, const vvi &costs) { return -1; }
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
@@ -102,7 +56,7 @@ int main() {
         vi cs(n, 0);
         for (auto &c : cs) cin >> c;
 
-        cout << "Case #" << i << ": " << solve(src, costs, rs, cs) << '\n';
+        cout << "Case #" << i << ": " << solve(src, costs) << '\n';
     }
 
     return 0;
