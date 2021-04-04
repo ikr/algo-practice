6b3e28d22030bc7dd4a0c56317326096789c6d5b
Ivan Krechetov <ikr@ikr.li>
2021-04-04 08:17:35 +0200

    Debugging the formula

--- codeforces/Triangular_Paths/solution.cpp
+++ codeforces/Triangular_Paths/solution.cpp
@@ -25,8 +25,33 @@ template <typename T> constexpr int inof(const T x) {
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-int path_min_cost(vector<pii> coords) {
-    cerr << coords << '\n';
+constexpr int div_ceil(const int x, const int y) {
+    return x ? (1 + (x - 1) / y) : 0;
+}
+
+int path_min_cost(const pii coord) {
+    const auto [ro, co] = coord;
+    if (ro == co) return ro - 1;
+
+    const int hi = (ro - 1) / 2;
+    return hi - div_ceil(co - 1, 2);
+}
+
+int path_min_cost(const vector<pii> &coords) {
+    assert(path_min_cost(pii{6, 5}) == 0);
+    assert(path_min_cost(pii{7, 2}) == 2);
+    assert(path_min_cost(pii{8, 1}) == 3);
+    assert(path_min_cost(pii{9, 1}) == 4);
+    assert(path_min_cost(pii{5, 2}) == 1);
+    assert(path_min_cost(pii{5, 1}) == 2);
+    assert(path_min_cost(pii{6, 1}) == 2);
+    assert(path_min_cost(pii{7, 1}) == 3);
+    assert(path_min_cost(pii{8, 1}) == 3);
+    assert(path_min_cost(pii{6, 3}) == 1);
+    assert(path_min_cost(pii{6, 4}) == 1);
+
+    assert(path_min_cost(pii{6, 2}) == 2);
+    assert(path_min_cost(pii{8, 2}) == 3);
     return -1;
 }
 
@@ -45,6 +70,7 @@ int main() {
         for (auto &[ro, _] : coords) cin >> ro;
         for (auto &[_, co] : coords) cin >> co;
 
+        sort(begin(coords), end(coords));
         cout << path_min_cost(coords) << '\n';
     }
 
