4ac096019b5e3811d768b7541e6d9b29f40d3d5f
Ivan Krechetov <ikr@ikr.su>
2021-04-04 07:45:06 +0200

    Bootstrap

---
+++ codeforces/Triangular_Paths/solution.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &x) {
+    os << '(' << x.first << ' ' << x.second << ')';
+    return os;
+}
+
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
+
+using pii = pair<int, int>;
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+int path_min_cost(vector<pii> coords) {
+    cerr << coords << '\n';
+    return -1;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+    cout << setprecision(9) << fixed;
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<pii> coords(n);
+        for (auto &[ro, _] : coords) cin >> ro;
+        for (auto &[_, co] : coords) cin >> co;
+
+        cout << path_min_cost(coords) << '\n';
+    }
+
+    return 0;
+}
