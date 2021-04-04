ee68dac7f03d06e3d707087468050172e4163874
Ivan Krechetov <ikr@ikr.su>
2021-04-04 20:00:25 +0200

    WA on test 2

--- codeforces/Triangular_Paths/solution.cpp
+++ codeforces/Triangular_Paths/solution.cpp
@@ -17,22 +17,30 @@ constexpr int div_ceil(const int x, const int y) {
     return x ? (1 + (x - 1) / y) : 0;
 }
 
-int path_min_cost(const pii coord) {
+int path_cost_of(const pii coord) {
     const auto [ro, co] = coord;
     if (ro == co) return ro - 1;
 
     return (ro - co) / 2;
 }
 
-int path_min_cost(const vector<pii> &coords) {
-    int ans = 0;
-
-    for (auto it = cbegin(coords); it != cend(coords); ++it) {
-        const pii last = it == cbegin(coords) ? pii{1, 1} : *prev(it);
-        ans += path_min_cost(*it) - path_min_cost(last);
+template <typename T> struct mmax final {
+    constexpr T operator()(const T &a, const T &b) const {
+        return std::max(a, b);
     }
+};
 
-    return ans;
+template <typename Iter, typename R, typename Binop, typename Unaop>
+R ttransform_reduce(Iter first, Iter last, R init, Binop binop, Unaop unaop) {
+    return inner_product(first, last, first, init, binop,
+                         [&unaop](const auto &x,
+                                  __attribute__((unused))
+                                  const auto &x_) { return unaop(x); });
+}
+
+int path_cost(const vector<pii> &coords) {
+    return ttransform_reduce(cbegin(coords), cend(coords), 0, mmax<int>{},
+                             path_cost_of);
 }
 
 int main() {
@@ -40,21 +48,21 @@ int main() {
     cin.exceptions(cin.failbit);
     cout << setprecision(9) << fixed;
 
-    exp_eq(path_min_cost(pii{2, 2}), 1);
-    exp_eq(path_min_cost(pii{1, 1}), 0);
-    exp_eq(path_min_cost(pii{6, 5}), 0);
-    exp_eq(path_min_cost(pii{7, 2}), 2);
-    exp_eq(path_min_cost(pii{8, 1}), 3);
-    exp_eq(path_min_cost(pii{9, 1}), 4);
-    exp_eq(path_min_cost(pii{5, 2}), 1);
-    exp_eq(path_min_cost(pii{5, 1}), 2);
-    exp_eq(path_min_cost(pii{6, 1}), 2);
-    exp_eq(path_min_cost(pii{7, 1}), 3);
-    exp_eq(path_min_cost(pii{8, 1}), 3);
-    exp_eq(path_min_cost(pii{6, 3}), 1);
-    exp_eq(path_min_cost(pii{6, 4}), 1);
-    exp_eq(path_min_cost(pii{6, 2}), 2);
-    exp_eq(path_min_cost(pii{8, 2}), 3);
+    exp_eq(path_cost_of(pii{2, 2}), 1);
+    exp_eq(path_cost_of(pii{1, 1}), 0);
+    exp_eq(path_cost_of(pii{6, 5}), 0);
+    exp_eq(path_cost_of(pii{7, 2}), 2);
+    exp_eq(path_cost_of(pii{8, 1}), 3);
+    exp_eq(path_cost_of(pii{9, 1}), 4);
+    exp_eq(path_cost_of(pii{5, 2}), 1);
+    exp_eq(path_cost_of(pii{5, 1}), 2);
+    exp_eq(path_cost_of(pii{6, 1}), 2);
+    exp_eq(path_cost_of(pii{7, 1}), 3);
+    exp_eq(path_cost_of(pii{8, 1}), 3);
+    exp_eq(path_cost_of(pii{6, 3}), 1);
+    exp_eq(path_cost_of(pii{6, 4}), 1);
+    exp_eq(path_cost_of(pii{6, 2}), 2);
+    exp_eq(path_cost_of(pii{8, 2}), 3);
 
     int t;
     cin >> t;
@@ -67,7 +75,7 @@ int main() {
         for (auto &[_, co] : coords) cin >> co;
 
         sort(begin(coords), end(coords));
-        cout << path_min_cost(coords) << '\n';
+        cout << path_cost(coords) << '\n';
     }
 
     return 0;
