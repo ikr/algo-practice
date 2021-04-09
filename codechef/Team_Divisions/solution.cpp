84dca4d2aad267c5b541e69020b51bfd259def57
Ivan Krechetov <ikr@ikr.li>
2021-04-09 06:35:09 +0200

    Start over

--- codechef/Team_Divisions/solution.cpp
+++ codechef/Team_Divisions/solution.cpp
@@ -4,7 +4,6 @@ using namespace std;
 using ll = long long;
 using pii = pair<int, int>;
 using vll = vector<ll>;
-using vvll = vector<vll>;
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
@@ -17,27 +16,14 @@ template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
 static constexpr ll M = 998244353LL;
 
-int last_index_of_x_dividing_right_neigh(const vector<pii> &xys) {
-    for (int i = 0; i < sz(xys) - 1; ++i) {
-        const int x0 = xys[i].first;
-        const int x1 = xys[i + 1].first;
-        if (x1 % x0 != 0) return i - 1;
-    }
-
-    return sz(xys) - 2;
+constexpr ll pow_mod(const ll base, const ll exp) {
+    if (!exp) return 1;
+    if (exp % 2) return (base * pow_mod(base, exp - 1)) % M;
+    const ll q = pow_mod(base, exp / 2);
+    return (q * q) % M;
 }
 
-int divisions_num(const vector<pii> &xys) {
-    const int hi = last_index_of_x_dividing_right_neigh(xys);
-    if (hi < 0) return 0;
-
-    if (!all_of(cbegin(xys) + hi + 2, cend(xys),
-                [hi](const pii xy) { return xy.first % hi == 0; })) {
-        return 0;
-    }
-
-    return -1;
-}
+int divisions_num(const vector<pii> &xys) { return -1; }
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
