1c68a78e2774d63223ff0c7a27aff538eac6d4d9
Ivan Krechetov <ikr@ikr.su>
2021-04-14 20:00:17 +0200

    Bootstrap

---
+++ atcoder/Flowers/solution.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+template <typename T> constexpr ll llof(const T x) {
+    return static_cast<ll>(x);
+}
+template <typename T> constexpr double doof(const T x) {
+    return static_cast<double>(x);
+}
+
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+ll max_remaining_beauties_sum(const vi &h, const vi &a) { return -1; }
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+    cout << setprecision(9) << fixed;
+
+    int n;
+    cin >> n;
+
+    vi h(n);
+    for (auto &x : h) cin >> x;
+
+    vi a(n);
+    for (auto &x : a) cin >> x;
+
+    cout << max_remaining_beauties_sum(h, a) << '\n';
+    return 0;
+}
