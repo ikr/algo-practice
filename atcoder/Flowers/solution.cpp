7b32b871b159577fee1fef0fe21a60062d73e99f
Ivan Krechetov <ikr@ikr.su>
2021-04-14 20:46:50 +0200

    AC

--- atcoder/Flowers/solution.cpp
+++ atcoder/Flowers/solution.cpp
@@ -1,34 +1,27 @@
+#include <atcoder/segtree>
 #include <bits/stdc++.h>
 using namespace std;
 
 using ll = long long;
 using vi = vector<int>;
-using vll = vector<ll>;
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
-template <typename T> constexpr ll llof(const T x) {
-    return static_cast<ll>(x);
-}
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
+constexpr ll op(ll a, ll b) { return max(a, b); }
+constexpr ll e() { return 0; }
+
 ll max_remaining_beauties_sum(const vi &h, const vi &a) {
-    vll dp(sz(h), 0);
+    atcoder::segtree<ll, op, e> dp(sz(h) + 1);
 
     for (int i = 0; i < sz(h); ++i) {
-        ll best_val = 0;
-
-        for (int j = i - 1; j >= 0; --j) {
-            if (h[j] >= h[i]) continue;
-            if (dp[j] > best_val) best_val = dp[j];
-        }
-
-        dp[i] = a[i] + best_val;
+        dp.set(h[i], a[i] + dp.prod(0, h[i]));
     }
 
-    return *max_element(cbegin(dp), cend(dp));
+    return dp.all_prod();
 }
 
 int main() {
