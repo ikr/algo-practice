c66636f32b9554412d5f52255c0706c7c0d227d9
Ivan Krechetov <ikr@ikr.su>
2021-04-17 11:10:29 +0200

    A, B, C, D — AC; E — WIP

---
+++ atcoder/Competition/solution.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+template <typename T> constexpr ll llof(const T x) {
+    return static_cast<ll>(x);
+}
+template <typename T> constexpr double doof(const T x) {
+    return static_cast<double>(x);
+}
+
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+template <typename T> constexpr T div_ceil(const T x, const T y) {
+    return x ? (1 + (x - 1) / y) : 0;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+    cout << setprecision(9) << fixed;
+
+    int x, y, z;
+    cin >> x >> y >> z;
+
+    int ans = div_ceil(z * y, x) - 1;
+    cout << ans << '\n';
+
+    return 0;
+}
