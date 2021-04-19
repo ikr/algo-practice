6605f54c0d54b293e566cc3576f1a19e07d13534
Ivan Krechetov <ikr@ikr.su>
2021-04-19 20:11:34 +0200

    Resume

--- atcoder/Level_K_Palindrome/solution.cpp
+++ atcoder/Level_K_Palindrome/solution.cpp
@@ -1,24 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
-using vi = vector<int>;
-using vvi = vector<vi>;
-using pii = pair<int, int>;
-using vll = vector<ll>;
-using vvll = vector<vll>;
-
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
-template <typename T> constexpr ll llof(const T x) {
-    return static_cast<ll>(x);
-}
-template <typename T> constexpr double doof(const T x) {
-    return static_cast<double>(x);
-}
-
-template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
 using Iter = string::const_iterator;
 
@@ -65,7 +50,6 @@ int min_ops(const int k, const string &xs) {
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
-    cout << setprecision(9) << fixed;
 
     int k;
     cin >> k;
