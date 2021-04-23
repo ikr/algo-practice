5c0194132bf889fe279bc8fd63e73a5376750a7d
Ivan Krechetov <ikr@ikr.su>
2021-04-23 20:18:30 +0200

    Do the matrix multiplication thing

--- atcoder/Walk/solution.cpp
+++ atcoder/Walk/solution.cpp
@@ -1,43 +1,87 @@
+#include <atcoder/modint>
 #include <bits/stdc++.h>
 using namespace std;
 
+using Mint = atcoder::modint1000000007;
+using Matrix = vector<vector<Mint>>;
 using ll = long long;
-using vi = vector<int>;
-using vvi = vector<vi>;
-using vll = vector<ll>;
-using vvll = vector<vll>;
+
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
+
+ostream &operator<<(ostream &os, const Mint &x) {
+    os << x.val();
+    return os;
+}
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
-template <typename T> constexpr ll llof(const T x) {
-    return static_cast<ll>(x);
-}
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-static constexpr ll M = 1e9 + 7;
-
-constexpr ll add_mod(const ll x, const ll y) {
-    const ll ans = x + y;
-    return ans >= M ? ans - M : ans;
+Mint cell_combination(const Matrix &xss, const Matrix &yss, const int ro,
+                      const int co) {
+    const int n = sz(xss);
+    Mint ans;
+    for (int i = 0; i < n; ++i) ans += xss[ro][i] * yss[i][co];
+    return ans;
 }
 
-ll k_long_paths_num(const ll k, const vvi &adj) {
-    // Number of ways to reach [in k steps] [the vertex v]
-    vvll dp(k + 1, vll(sz(adj), 0LL));
-    dp[0] = vll(sz(adj), 1LL);
+Matrix operator*(const Matrix &xss, const Matrix &yss) {
+    const int n = sz(xss);
+    Matrix ans(n, vector<Mint>(n, 0));
 
-    for (ll steps = 1; steps <= k; ++steps) {
-        for (int ro = 0; ro < sz(adj); ++ro) {
-            for (int co = 0; co < sz(adj); ++co) {
-                if (!adj[ro][co]) continue;
-                dp[steps][co] = add_mod(dp[steps][co], dp[steps - 1][ro]);
-            }
+    for (int ro = 0; ro < n; ++ro) {
+        for (int co = 0; co < n; ++co) {
+            ans[ro][co] = cell_combination(xss, yss, ro, co);
         }
     }
 
-    return accumulate(cbegin(dp.back()), cend(dp.back()), 0LL, add_mod);
+    return ans;
+}
+
+vector<Mint> operator*(const Matrix &xss, const vector<Mint> &ys) {
+    const int n = sz(xss);
+    vector<Mint> ans(n, 0);
+
+    for (int ro = 0; ro < n; ++ro) {
+        for (int i = 0; i < n; ++i) ans[ro] += xss[ro][i] + ys[i];
+    }
+
+    return ans;
+}
+
+Matrix unit(const int n) {
+    Matrix ans(n, vector<Mint>(n, 0));
+    for (int i = 0; i < n; ++i) ans[i][i] = 1;
+    return ans;
+}
+
+Matrix pow(const Matrix &xss, const ll exp) {
+    if (!exp) return unit(sz(xss));
+    if (exp % 2LL) return xss * pow(xss, exp - 1LL);
+
+    const auto q = pow(xss, exp / 2LL);
+    return q * q;
+}
+
+int k_long_paths_num(const ll k, const Matrix &adj) {
+    const auto m = pow(adj, k);
+    const auto xs = m * vector<Mint>(sz(adj), 1);
+
+    cerr << xs << '\n';
+
+    return accumulate(cbegin(xs), cend(xs), Mint{0},
+                      [](const Mint &x, const Mint &y) { return x + y; })
+        .val();
 }
 
 int main() {
@@ -50,9 +94,13 @@ int main() {
     ll k;
     cin >> k;
 
-    vvi adj(n, vi(n));
+    Matrix adj(n, vector<Mint>(n));
     for (auto &row : adj) {
-        for (auto &cell : row) cin >> cell;
+        for (auto &cell : row) {
+            int x;
+            cin >> x;
+            cell = x;
+        }
     }
 
     cout << k_long_paths_num(k, adj) << '\n';
