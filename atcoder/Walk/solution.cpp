0499ca2387889a521a19ce13eeea205b82507975
Ivan Krechetov <ikr@ikr.li>
2021-04-24 09:14:29 +0200

    AC

--- atcoder/Walk/solution.cpp
+++ atcoder/Walk/solution.cpp
@@ -6,21 +6,6 @@ using Mint = atcoder::modint1000000007;
 using Matrix = vector<vector<Mint>>;
 using ll = long long;
 
-template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
-    os << '[';
-    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
-        if (i != xs.cbegin()) os << ' ';
-        os << *i;
-    }
-    os << ']';
-    return os;
-}
-
-ostream &operator<<(ostream &os, const Mint &x) {
-    os << x.val();
-    return os;
-}
-
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
@@ -48,25 +33,8 @@ Matrix operator*(const Matrix &xss, const Matrix &yss) {
     return ans;
 }
 
-vector<Mint> operator*(const Matrix &xss, const vector<Mint> &ys) {
-    const int n = sz(xss);
-    vector<Mint> ans(n, 0);
-
-    for (int ro = 0; ro < n; ++ro) {
-        for (int i = 0; i < n; ++i) ans[ro] += xss[ro][i] + ys[i];
-    }
-
-    return ans;
-}
-
-Matrix unit(const int n) {
-    Matrix ans(n, vector<Mint>(n, 0));
-    for (int i = 0; i < n; ++i) ans[i][i] = 1;
-    return ans;
-}
-
 Matrix pow(const Matrix &xss, const ll exp) {
-    if (!exp) return unit(sz(xss));
+    if (exp == 1) return xss;
     if (exp % 2LL) return xss * pow(xss, exp - 1LL);
 
     const auto q = pow(xss, exp / 2LL);
@@ -75,12 +43,15 @@ Matrix pow(const Matrix &xss, const ll exp) {
 
 int k_long_paths_num(const ll k, const Matrix &adj) {
     const auto m = pow(adj, k);
-    const auto xs = m * vector<Mint>(sz(adj), 1);
 
-    cerr << xs << '\n';
-
-    return accumulate(cbegin(xs), cend(xs), Mint{0},
-                      [](const Mint &x, const Mint &y) { return x + y; })
+    return accumulate(cbegin(m), cend(m), Mint{0},
+                      [](const Mint agg, const vector<Mint> &xs) {
+                          return agg +
+                                 accumulate(cbegin(xs), cend(xs), Mint{0},
+                                            [](const Mint x, const Mint y) {
+                                                return x + y;
+                                            });
+                      })
         .val();
 }
 
