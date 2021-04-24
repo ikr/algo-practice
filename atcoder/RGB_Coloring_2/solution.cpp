8b54db2e93448b85779c96a131e38814b688e4cb
Ivan Krechetov <ikr@ikr.li>
2021-04-24 17:30:29 +0200

    WA

--- atcoder/RGB_Coloring_2/solution.cpp
+++ atcoder/RGB_Coloring_2/solution.cpp
@@ -1,72 +1,60 @@
+#include <atcoder/dsu>
 #include <bits/stdc++.h>
 using namespace std;
 
 using ll = long long;
 using vi = vector<int>;
 using vvi = vector<vi>;
-using pii = pair<int, int>;
-using vll = vector<ll>;
-using vvll = vector<vll>;
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
-template <typename T> constexpr ll llof(const T x) {
-    return static_cast<ll>(x);
-}
-template <typename T> constexpr double doof(const T x) {
-    return static_cast<double>(x);
-}
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-ll rgb_paintings_num(const vvi &g) {
-    const int n = sz(g);
-    vvll dp(n, vll(8, 0));
+ll component_rgb_paintings_num(const int start, const int comp_size,
+                               const vvi &g) {
+    vi coloring(sz(g), 0);
 
-    dp[0][1] = 1;
-    dp[0][2] = 1;
-    dp[0][4] = 1;
-
-    for (int u = 1; u < n; ++u) {
-        const set<int> adj(cbegin(g[u]), cend(g[u]));
-
-        ll mul = 1LL;
-
-        for (int v = 0; v < u; ++v) {
-            if (!adj.count(v)) {
-                mul *= accumulate(cbegin(dp[v]), cend(dp[v]), 0LL, plus<ll>{});
-                continue;
-            }
-
-            for (int sh = 0; sh < 3; ++sh) {
-                const int mask = 1 << sh;
-
-                for (int bits = 1; bits < 8; ++bits) {
-                    if ((bits & mask) == 0) {
-                        dp[u][bits | mask] += dp[v][bits];
-                    }
-                }
-            }
+    function<ll(int, int, int)> dfs;
+    dfs = [&](const int colored_num, const int u, const int color) -> ll {
+        for (const int v : g[u]) {
+            if (coloring[v] == color) return 0;
         }
 
-        for (int bits = 1; bits < 8; ++bits) {
-            dp[u][bits] *= mul;
-        }
-    }
+        if (colored_num + 1 == comp_size) return 1;
 
-    return accumulate(cbegin(dp.back()), cend(dp.back()), 0LL, plus<ll>{});
+        ll ans = 0;
+        coloring[u] = color;
+        for (const int v : g[u]) {
+            if (coloring[v]) continue;
+
+            for (int color_ = 1; color_ <= 3; ++color_) {
+                if (color_ == color) continue;
+                ans += dfs(colored_num + 1, v, color_);
+            }
+        }
+        return ans;
+    };
+
+    ll ans = 0;
+    ans += dfs(0, start, 1);
+    fill(begin(coloring), end(coloring), 0);
+    ans += dfs(0, start, 2);
+    fill(begin(coloring), end(coloring), 0);
+    ans += dfs(0, start, 3);
+    return ans;
 }
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
-    cout << setprecision(9) << fixed;
 
     int n, m;
     cin >> n >> m;
 
-    vvi g(n, vi{});
+    vvi g(n);
+    atcoder::dsu comps(n);
 
     while (m--) {
         int a, b;
@@ -76,8 +64,17 @@ int main() {
 
         g[a].push_back(b);
         g[b].push_back(a);
+        comps.merge(a, b);
     }
 
-    cout << rgb_paintings_num(g) << '\n';
+    set<int> leaders;
+    for (int i = 0; i < n; ++i) leaders.insert(comps.leader(i));
+
+    ll ans = 1;
+    for (const auto u : leaders) {
+        ans *= component_rgb_paintings_num(u, comps.size(u), g);
+    }
+
+    cout << ans << '\n';
     return 0;
 }
