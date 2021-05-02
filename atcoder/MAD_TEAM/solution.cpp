9241eca6fcc8685cf6497a7c39912588f0730f53
Ivan Krechetov <ikr@ikr.li>
2021-05-02 10:30:03 +0200

    Actually understood the problem statement now

--- atcoder/MAD_TEAM/solution.cpp
+++ atcoder/MAD_TEAM/solution.cpp
@@ -1,47 +1,65 @@
-#include <algorithm>
 #include <bits/stdc++.h>
 using namespace std;
-
-using ll = long long;
-using vi = vector<int>;
-using vvi = vector<vi>;
-using pii = pair<int, int>;
-using vll = vector<ll>;
-using vvll = vector<vll>;
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
-template <typename T> constexpr ll llof(const T x) {
-    return static_cast<ll>(x);
-}
-template <typename T> constexpr double doof(const T x) {
-    return static_cast<double>(x);
-}
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+using Quint = array<int, 5>;
+
+int maximin(const Quint &q1, const Quint &q2) {
+    Quint his;
+    for (int i = 0; i < sz(his); ++i) his[i] = max(q1[i], q2[i]);
+    return *min_element(cbegin(his), cend(his));
+}
+
+int maximin(const Quint &q1, const Quint &q2, const Quint &q3) {
+    Quint his;
+    for (int i = 0; i < sz(his); ++i) his[i] = max({q1[i], q2[i], q3[i]});
+    return *min_element(cbegin(his), cend(his));
+}
+
+int top_team_strength(const vector<Quint> &xss) {
+    int i0 = 0;
+    int j0 = 1;
+    int best = maximin(xss[i0], xss[j0]);
+
+    for (int i = 0; i < sz(xss) - 1; ++i) {
+        for (int j = i + 1; j < sz(xss); ++j) {
+            if (i == 0 && j == 1) continue;
+            const int challenger = maximin(xss[i], xss[j]);
+
+            if (challenger > best) {
+                i0 = i;
+                j0 = j;
+                best = challenger;
+            }
+        }
+    }
+
+    for (int i = 0; i < sz(xss); ++i) {
+        if (i == i0 || i == j0) continue;
+        best = maximin(xss[i0], xss[j0], xss[i]);
+    }
+
+    return best;
+}
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
-    cout << setprecision(9) << fixed;
 
-    int t;
-    cin >> t;
+    int n;
+    cin >> n;
 
-    vector<array<int, 5>> xs(t);
-
-    for (auto &[a, b, c, d, e] : xs) cin >> a >> b >> c >> d >> e;
-
-    array<int, 5> agg{0, 0, 0, 0, 0};
-
-    for (const auto x : xs) {
-        for (int i = 0; i < 5; ++i) {
-            agg[i] = max(agg[i], x[i]);
-        }
+    vector<Quint> xss(n);
+    for (auto &xs : xss) {
+        for (auto &x : xs) cin >> x;
     }
 
-    cout << *min_element(cbegin(agg), cend(agg)) << '\n';
+    cout << top_team_strength(xss) << '\n';
 
     return 0;
 }
