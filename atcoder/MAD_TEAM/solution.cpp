87bb0d37362763a903857108a958df968d5d48dd
Ivan Krechetov <ikr@ikr.su>
2021-05-02 13:23:24 +0200

    AC

--- atcoder/MAD_TEAM/solution.cpp
+++ atcoder/MAD_TEAM/solution.cpp
@@ -8,45 +8,53 @@ template <typename T> constexpr int inof(const T x) {
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
 using Quint = array<int, 5>;
+using vi = vector<int>;
 
-int maximin(const Quint &q1, const Quint &q2) {
-    Quint his;
-    for (int i = 0; i < sz(his); ++i) his[i] = max(q1[i], q2[i]);
-    return *min_element(cbegin(his), cend(his));
-}
+bool team_exists_not_weaker_than(const vector<Quint> &xss, const int lo) {
+    set<int> indicator_masks;
 
-int maximin(const Quint &q1, const Quint &q2, const Quint &q3) {
-    Quint his;
-    for (int i = 0; i < sz(his); ++i) his[i] = max({q1[i], q2[i], q3[i]});
-    return *min_element(cbegin(his), cend(his));
-}
+    for (const auto &xs : xss) {
+        int mask = 0;
 
-int top_team_strength(const vector<Quint> &xss) {
-    int i0 = 0;
-    int j0 = 1;
-    int best = maximin(xss[i0], xss[j0]);
+        for (int i = 0; i < sz(xs); ++i) {
+            if (xs[i] >= lo) mask |= (1 << i);
+        }
 
-    for (int i = 0; i < sz(xss) - 1; ++i) {
-        for (int j = i + 1; j < sz(xss); ++j) {
-            if (i == 0 && j == 1) continue;
-            const int challenger = maximin(xss[i], xss[j]);
+        indicator_masks.insert(mask);
+    }
 
-            if (challenger > best) {
-                i0 = i;
-                j0 = j;
-                best = challenger;
+    vi ms(cbegin(indicator_masks), cend(indicator_masks));
+    const int all_five = 0b11111;
+
+    for (int i = 0; i < sz(ms); ++i) {
+        for (int j = 0; j < sz(ms); ++j) {
+            for (int k = 0; k < sz(ms); ++k) {
+                if ((ms[i] | ms[j] | ms[k]) == all_five) return true;
             }
         }
     }
 
-    best = maximin(xss[0], xss[1], xss[2]);
+    return false;
+}
 
-    for (int i = 0; i < sz(xss); ++i) {
-        if (i == i0 || i == j0) continue;
-        best = max(best, maximin(xss[i0], xss[j0], xss[i]));
+int top_team_strength(const vector<Quint> &xss, const vi &vals) {
+    int lo = vals[0];
+    int hi = vals.back() + 1;
+
+    assert(team_exists_not_weaker_than(xss, lo));
+    assert(!team_exists_not_weaker_than(xss, hi));
+
+    while (hi - lo > 1) {
+        const int mi = lo + (hi - lo) / 2;
+
+        if (team_exists_not_weaker_than(xss, mi)) {
+            lo = mi;
+        } else {
+            hi = mi;
+        }
     }
 
-    return best;
+    return lo;
 }
 
 int main() {
@@ -57,11 +65,16 @@ int main() {
     cin >> n;
 
     vector<Quint> xss(n);
+    set<int> vals;
+
     for (auto &xs : xss) {
-        for (auto &x : xs) cin >> x;
+        for (auto &x : xs) {
+            cin >> x;
+            vals.insert(x);
+        }
     }
 
-    cout << top_team_strength(xss) << '\n';
+    cout << top_team_strength(xss, vi(cbegin(vals), cend(vals))) << '\n';
 
     return 0;
 }
