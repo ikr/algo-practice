0abb0de46aef8b87abccaa49ff809fb39da6ddb2
Ivan Krechetov <ikr@ikr.li>
2021-05-22 18:36:01 +0200

    AC

--- atcoder/aab_aba_baa/solution.cpp
+++ atcoder/aab_aba_baa/solution.cpp
@@ -2,9 +2,6 @@
 using namespace std;
 
 using ll = long long;
-using vi = vector<int>;
-using vvi = vector<vi>;
-using pii = pair<int, int>;
 using vll = vector<ll>;
 using vvll = vector<vll>;
 
@@ -17,55 +14,65 @@ template <typename T> constexpr ll llof(const T x) {
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-ll factorial(const ll x) {
-    assert(x >= 0);
-    if (x <= 1) return 1;
-
-    ll ans = 2;
-    for (ll i = 3; i <= x; ++i) {
-        ans *= i;
-    }
-    assert(ans > 0LL);
-    return ans;
-}
-
-ll product_of_range(const ll a, const ll b) {
-    ll ans = 1;
-    for (ll x = a; x <= b; ++x) ans *= x;
-    assert(ans > 0LL);
-    return ans;
-}
-
-ll combinations(const ll n, const ll k) {
+vvll combinations(const int n, const int k) {
     assert(k <= n);
-    const ll r = n - k;
-    return r < k ? product_of_range(k + 1, n) / factorial(r)
-                 : product_of_range(r + 1, n) / factorial(k);
-}
+    vvll ans(n + 1, vll(k + 1, 1));
 
-string kth_element(const int a, const int b, const ll k) {
-    for (int i = 1; i <= a; ++i) {
-        cerr << combinations(b + i - 1, i) << endl;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= min(i, k); ++j) {
+            ans[i][j] = i == j ? 1 : ans[i - 1][j - 1] + ans[i - 1][j];
+        }
     }
 
-    return "";
+    return ans;
+}
+
+string kth_element(int a, int b, ll k) {
+    const auto cs = combinations(a + b + 1, a + b + 1);
+    const auto i_choose_j = [&cs](const int i, const int j) -> ll {
+        assert(j <= i);
+        return cs[i][j];
+    };
+
+    string ans;
+
+    while (a || b) {
+        if (!a) {
+            ans += string(b, 'b');
+            break;
+        }
+
+        if (!b) {
+            ans += string(a, 'a');
+            break;
+        }
+
+        assert(a && b);
+
+        const auto starting_with_a = i_choose_j(a + b - 1, b);
+
+        if (starting_with_a < k) {
+            ans += 'b';
+            --b;
+            k -= starting_with_a;
+        } else {
+            ans += 'a';
+            --a;
+        }
+    }
+
+    return ans;
 }
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
 
-    // string xs{"000111"};
-    // do {
-    //     cout << xs << '\n';
-    // } while (next_permutation(begin(xs), end(xs)));
-
     int a, b;
     cin >> a >> b;
 
     ll k;
     cin >> k;
-    --k;
 
     cout << kth_element(a, b, k) << '\n';
     return 0;
