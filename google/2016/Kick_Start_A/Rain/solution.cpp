05243fc6340b25218cf8555d6584b826df5e7758
Ivan Krechetov <ikr@ikr.li>
2021-05-25 21:16:37 +0200

    Re-implement optimally
    See https://www.youtube.com/watch?v=cJayBq38VYw

--- google/2016/Kick_Start_A/Rain/solution.cpp
+++ google/2016/Kick_Start_A/Rain/solution.cpp
@@ -3,131 +3,114 @@ using namespace std;
 
 using vi = vector<int>;
 using vvi = vector<vi>;
-using pi = pair<int, int>;
 
-template <typename T1, typename T2> struct PairHash final {
-    size_t operator()(const pair<T1, T2> &p) const {
-        return 31 * hash<T1>{}(p.first) + hash<T2>{}(p.second);
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+using Coord = complex<int>;
+constexpr int row(const Coord coord) { return coord.real(); }
+constexpr int col(const Coord coord) { return coord.imag(); }
+
+using PrioCoord = pair<int, Coord>;
+
+struct pq_greater final {
+    bool operator()(const PrioCoord &a, const PrioCoord &b) const {
+        return a.first > b.first;
     }
 };
 
-using uspi = unordered_set<pi, PairHash<int, int>>;
+using PrioQueue = priority_queue<PrioCoord, vector<PrioCoord>, pq_greater>;
 
-template <typename T> constexpr pair<T, T> operator+(const pair<T, T> &lhs, const pair<T, T> &rhs) {
-    return {lhs.first + rhs.first, lhs.second + rhs.second};
+constexpr bool in_bounds(const int H, const int W, const Coord coord) {
+    return (0 <= row(coord) && row(coord) < H && 0 <= col(coord) &&
+            col(coord) < W);
 }
 
-bool in_bounds(const vvi &rows, const pi coord) {
-    const int H = rows.size();
-    const int W = rows[0].size();
-    const auto r = coord.first;
-    const auto c = coord.second;    
-    return (0 <= r && r < H && 0 <= c && c < W);
-}
+vector<Coord> adjacent(const int H, const int W, const Coord coord) {
+    static const vector<Coord> deltas{{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    vector<Coord> ans;
 
-bool is_on_slope(const vvi &rows, const pi coord) {
-    const int H = rows.size();
-    const int W = rows[0].size();
-    
-    uspi discovered{coord};
-    stack<pi> s;
-    s.push(coord);
-        
-    while (!s.empty()) {
-        const auto r = s.top().first;
-        const auto c = s.top().second;
-        s.pop();
-        if (r == 0 || r == H - 1 || c == 0 || c == W - 1) return true;
-        
-        for (const auto delta : {pi{-1, 0}, pi{0, 1}, pi{1, 0}, pi{0, -1}}) {
-            const auto adj = pi{r, c} + delta;
-            
-            if (
-                in_bounds(rows, adj) && 
-                rows[adj.first][adj.second] <= rows[coord.first][coord.second] &&
-                !discovered.count(adj)
-            ) {
-                s.push(adj);
-                discovered.insert(adj);
-            }
-        }
+    for (const auto delta : deltas) {
+        const auto x = coord + delta;
+        if (in_bounds(H, W, x)) ans.push_back(x);
     }
-    
-    return false;
-}
 
-uspi discover_slopes(const vvi &rows) {
-    const int H = rows.size();
-    const int W = rows[0].size();
-    
-    uspi ans;
-    for (int r = 0; r != H; ++r) {
-        for (int c = 0; c != W; ++c) {
-            if (is_on_slope(rows, {r, c})) ans.emplace(r, c);
-        }
-    }
     return ans;
 }
 
-int discover_edge_height(const vvi &rows, const uspi &slopes, const pi coord) {
-    int ans = INT_MAX;
-    uspi discovered{coord};
-    stack<pi> s;
-    s.push(coord);
-    
-    while (!s.empty()) {
-        const auto r = s.top().first;
-        const auto c = s.top().second;
-        s.pop();
-        
-        if (slopes.count({r, c})) {
-            ans = min(ans, rows[r][c]);
-        } else {
-            for (const auto delta : {pi{-1, 0}, pi{0, 1}, pi{1, 0}, pi{0, -1}}) {
-                const auto adj = pi{r, c} + delta;
+pair<PrioQueue, vector<vector<bool>>> init_queue(const vvi &grid) {
+    const int H = sz(grid);
+    const int W = sz(grid[0]);
+    vector<vector<bool>> discovered(H, vector<bool>(W, false));
 
-                if (in_bounds(rows, adj) && !discovered.count(adj)) {
-                    s.push(adj);
-                    discovered.insert(adj);
-                }
-            }
-        }
+    PrioQueue q;
+
+    for (int ro = 0; ro < H; ++ro) {
+        q.emplace(grid[ro][0], Coord{ro, 0});
+        discovered[ro][0] = true;
+
+        q.emplace(grid[ro][W - 1], Coord{ro, W - 1});
+        discovered[ro][W - 1] = true;
     }
-    
-    return ans;
+
+    for (int co = 0; co < W; ++co) {
+        q.emplace(grid[0][co], Coord{0, co});
+        discovered[0][co] = true;
+
+        q.emplace(grid[H - 1][co], Coord{H - 1, co});
+        discovered[H - 1][co] = true;
+    }
+
+    return {q, discovered};
 }
 
-int solve (const vvi &rows) {
-    const auto slopes = discover_slopes(rows);
-    const int H = rows.size();
-    const int W = rows[0].size();
-    
+int solve(const vvi &grid) {
+    const int H = sz(grid);
+    const int W = sz(grid[0]);
+    auto [q, discovered] = init_queue(grid);
     int ans = 0;
-    for (int r = 0; r != H; ++r) {
-        for (int c = 0; c != W; ++c) {
-            if (slopes.count({r, c})) continue;
-            ans += discover_edge_height(rows, slopes, {r, c}) - rows[r][c];
+    int edge_level = -1;
+
+    while (!q.empty()) {
+        const auto [h, u] = q.top();
+        q.pop();
+        edge_level = max(edge_level, h);
+
+        for (const auto v : adjacent(H, W, u)) {
+            if (discovered[row(v)][col(v)]) continue;
+            discovered[row(v)][col(v)] = true;
+
+            if (grid[row(v)][col(v)] < edge_level) {
+                ans += edge_level - grid[row(v)][col(v)];
+                q.emplace(edge_level, v);
+            } else {
+                q.emplace(grid[row(v)][col(v)], v);
+            }
         }
     }
+
     return ans;
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    
+
     int T;
     cin >> T;
     for (int i = 1; i <= T; ++i) {
         int R, C;
         cin >> R >> C;
-        vvi rows(R, vi(C, 0));
-        
-        for (auto &row : rows) {
+        vvi grid(R, vi(C, 0));
+
+        for (auto &row : grid) {
             for (auto &x : row) cin >> x;
         }
-        
-        cout << "Case #" << i << ": " << solve(rows) << '\n';
+
+        cout << "Case #" << i << ": " << solve(grid) << '\n';
     }
 
     return 0;
