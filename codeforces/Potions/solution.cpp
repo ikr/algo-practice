48884ab4ceb66ed16ba986f1547a74b3f8207b1b
Ivan Krechetov <ikr@ikr.su>
2021-05-29 07:16:44 +0200

    TLE

--- codeforces/Potions/solution.cpp
+++ codeforces/Potions/solution.cpp
@@ -2,34 +2,22 @@
 using namespace std;
 
 using ll = long long;
-using vi = vector<int>;
-using vvi = vector<vi>;
-using pii = pair<int, int>;
 using vll = vector<ll>;
-using vvll = vector<vll>;
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
+
 template <typename T> constexpr ll llof(const T x) {
     return static_cast<ll>(x);
-}
-template <typename T> constexpr double doof(const T x) {
-    return static_cast<double>(x);
 }
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-vll prefix_sums_with_zero(const vll &xs) {
-    vll ans(sz(xs));
-    partial_sum(cbegin(xs), cend(xs), begin(ans));
-    ans.insert(cbegin(ans), 0LL);
-    return ans;
-}
-
 int max_drinks(const vll &xs) {
     vector<map<ll, int>> dp(sz(xs));
 
+    dp[0][0] = 0;
     if (xs[0] >= 0) dp[0][xs[0]] = 1;
 
     for (int i = 1; i < sz(xs); ++i) {
@@ -52,7 +40,6 @@ int max_drinks(const vll &xs) {
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
-    cout << setprecision(9) << fixed;
 
     int n;
     cin >> n;
