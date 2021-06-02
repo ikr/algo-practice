e29669b495ecb9aa3033d7ed8e7b258a3fee5a34
Ivan Krechetov <ikr@ikr.li>
2021-06-02 07:50:39 +0200

    Explore a much easier solution

--- codechef/Total_Components/solution.cpp
+++ codechef/Total_Components/solution.cpp
@@ -1,180 +1,83 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T1, typename T2>
-ostream &operator<<(ostream &os, const pair<T1, T2> &x) {
-    os << '(' << x.first << ' ' << x.second << ')';
-    return os;
-}
-
-template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
-    os << '[';
-    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
-        if (i != xs.cbegin()) os << ' ';
-        os << *i;
-    }
-    os << ']';
-    return os;
-}
-
-template <typename T>
-ostream &operator<<(ostream &os, const vector<vector<T>> &xss) {
-    for (const auto xs : xss) os << xs << '\n';
-    return os;
-}
-
 using ll = long long;
 using vi = vector<int>;
-using vvi = vector<vi>;
-using pii = pair<int, int>;
-using vll = vector<ll>;
-using vvll = vector<vll>;
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
-template <typename T> constexpr ll llof(const T x) {
-    return static_cast<ll>(x);
-}
-template <typename T> constexpr double doof(const T x) {
-    return static_cast<double>(x);
-}
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-pair<vector<bool>, vi> sieve(const int n) {
-    vi smallest_prime_divisor(n + 1, 2);
-    if (n) smallest_prime_divisor[1] = -1;
+template <typename T> constexpr ll llof(const T x) {
+    return static_cast<ll>(x);
+}
 
-    vector<bool> prime(n + 1, true);
-    prime[0] = false;
-    if (n) prime[1] = false;
+vector<bool> sieve(const int n) {
+    vector<bool> ans(n + 1, true);
+    ans[0] = false;
+    ans[1] = false;
 
-    vector<bool> v(n + 1, false);
-
-    for (ll i = 3; i <= n; i += 2) {
-        if (!v[i]) {
-            smallest_prime_divisor[i] = inof(i);
-            for (ll j = i; (j * i) <= n; j += 2) {
-                if (!v[j * i])
-                    v[j * i] = true, smallest_prime_divisor[j * i] = inof(i);
-            }
-        }
-    }
-
-    for (int i = 2; i * i <= n; ++i) {
-        if (!prime[i]) continue;
+    for (int i = 2; i <= n; ++i) {
+        if (!ans[i]) continue;
+        if (llof(i) * i > llof(n)) continue;
 
         for (int j = i * i; j <= n; j += i) {
-            prime[j] = false;
-            smallest_prime_divisor[j] = i;
+            ans[j] = false;
         }
     }
 
-    return {prime, smallest_prime_divisor};
+    return ans;
 }
 
-namespace atcoder {
+vi primes(const vector<bool> &s) {
+    vi ans;
+    ans.reserve(sz(s) / 2);
 
-// Implement (union by size) + (path compression)
-// Reference:
-// Zvi Galil and Giuseppe F. Italiano,
-// Data structures and algorithms for disjoint set union problems
-struct dsu {
-  public:
-    dsu() : _n(0) {}
-    explicit dsu(int n) : _n(n), parent_or_size(n, -1) {}
-
-    int merge(int a, int b) {
-        assert(0 <= a && a < _n);
-        assert(0 <= b && b < _n);
-        int x = leader(a), y = leader(b);
-        if (x == y) return x;
-        if (-parent_or_size[x] < -parent_or_size[y]) std::swap(x, y);
-        parent_or_size[x] += parent_or_size[y];
-        parent_or_size[y] = x;
-        return x;
+    for (int i = 0; i < sz(s); ++i) {
+        if (s[i]) ans.push_back(i);
     }
 
-    bool same(int a, int b) {
-        assert(0 <= a && a < _n);
-        assert(0 <= b && b < _n);
-        return leader(a) == leader(b);
-    }
-
-    int leader(int a) {
-        assert(0 <= a && a < _n);
-        if (parent_or_size[a] < 0) return a;
-        return parent_or_size[a] = leader(parent_or_size[a]);
-    }
-
-    int size(int a) {
-        assert(0 <= a && a < _n);
-        return -parent_or_size[leader(a)];
-    }
-
-    std::vector<std::vector<int>> groups() {
-        std::vector<int> leader_buf(_n), group_size(_n);
-        for (int i = 0; i < _n; i++) {
-            leader_buf[i] = leader(i);
-            group_size[leader_buf[i]]++;
-        }
-        std::vector<std::vector<int>> result(_n);
-        for (int i = 0; i < _n; i++) {
-            result[i].reserve(group_size[i]);
-        }
-        for (int i = 0; i < _n; i++) {
-            result[leader_buf[i]].push_back(i);
-        }
-        result.erase(std::remove_if(
-                         result.begin(), result.end(),
-                         [&](const std::vector<int> &v) { return v.empty(); }),
-                     result.end());
-        return result;
-    }
-
-  private:
-    int _n;
-    // root node: -1 * component size
-    // otherwise: parent
-    std::vector<int> parent_or_size;
-};
-
-} // namespace atcoder
-
-int components_num(const pair<vector<bool>, vi> &fact, const int n) {
-    const auto &pr = fact.first;
-    const auto &sp = fact.second;
-    atcoder::dsu cs(n + 1);
-    cs.merge(0, 1);
-    cs.merge(1, 2);
-
-    for (int x = 2; x <= n; ++x) {
-        int y = x;
-        cs.merge(sp[y], x);
-
-        while (!pr[y]) {
-            y /= sp[y];
-            cs.merge(y, x);
-        }
-    }
-
-    return sz(cs.groups());
+    return ans;
 }
+
+vi prime_pair_products(const vi &ps, const int n) {
+    vi ans;
+    ans.reserve(sz(ps) / 4);
+
+    for (int i = 0; i < sz(ps) - 1; ++i) {
+        if (llof(ps[i]) * ps[i + 1] > llof(n)) break;
+
+        for (int j = i + 1; j < sz(ps); ++j) {
+            if (llof(ps[i]) * ps[j] > llof(n)) break;
+            ans.push_back(ps[i] * ps[j]);
+        }
+    }
+
+    sort(begin(ans), end(ans));
+    return ans;
+}
+
+int components_num(const vi &ps, const vi &pps, const int n) {
+    return -1;
+}
+
+static constexpr int N_MAX = 10'000'000;
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
-    cout << setprecision(9) << fixed;
 
-    const auto fact = sieve(10000000);
+    const auto ps = primes(sieve(N_MAX));
+    const auto pps = prime_pair_products(ps, N_MAX);
 
     int t;
     cin >> t;
     while (t--) {
         int n;
         cin >> n;
-        cout << components_num(fact, n) << '\n';
+        cout << components_num(ps, pps, n) << '\n';
     }
 
     return 0;
