255f7aed8ca00299f87bac5eb36c4c1bb811ca23
Ivan Krechetov <ikr@ikr.li>
2021-06-05 10:55:49 +0200

    WA-s on test set 2 and 3

--- codechef/K-Subarrays/solution.cpp
+++ codechef/K-Subarrays/solution.cpp
@@ -1,6 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<vector<T>> &xss) {
+    for (const auto xs : xss) os << xs << '\n';
+    return os;
+}
+
 using ll = long long;
 using vll = vector<ll>;
 using vvll = vector<vll>;
@@ -17,16 +33,35 @@ template <typename T> constexpr ll llof(const T x) {
 
 static constexpr ll INF = 1e17;
 
-ll max_score(const vll &xs) {
-    ll ans = -INF;
-    ll curr = 0;
+ll max_score(const vll &xs, const int k) {
+    // Max score for [i sub-arrays] when the last chosen sub-array ends [exactly
+    // at index j], inclusive.
+    vvll dp(k + 1, vll(sz(xs), -INF));
+    dp[0] = vll(sz(xs), 0);
+    dp[1][0] = xs[0];
 
-    for (const auto x : xs) {
-        curr = max(curr + x, x);
-        ans = max(curr, ans);
+    // hi[i][j] is the max(dp[i][0], dp[i][1], … dp[i][j - 1], dp[i][j])
+    vvll hi = dp;
+
+    for (int j = 1; j < sz(xs); ++j) {
+        dp[1][j] = max(xs[j], dp[1][j - 1] + xs[j]);
+        hi[1][j] = max(dp[1][j], hi[1][j - 1]);
     }
 
-    return ans;
+    for (int i = 2; i <= k; ++i) {
+        for (int j = i - 1; j < sz(xs); ++j) {
+            // 1-element sub-array #i right where we stand
+            const auto o1 = max(hi[i - 1][j - 1] + i * xs[j], i * xs[j]);
+
+            // Append current element to last sub-array
+            const auto o2 = dp[i][j - 1] + i * xs[j];
+
+            dp[i][j] = max({o1, o2, -INF}); // Also, don't accumulate the -INF-s
+            hi[i][j] = max(hi[i][j - 1], dp[i][j]);
+        }
+    }
+
+    return *max_element(cbegin(dp.back()), cend(dp.back()));
 }
 
 int main() {
@@ -42,7 +77,7 @@ int main() {
         vll xs(n);
         for (auto &x : xs) cin >> x;
 
-        cout << max_score(xs) << '\n';
+        cout << max_score(xs, k) << '\n';
     }
 
     return 0;
