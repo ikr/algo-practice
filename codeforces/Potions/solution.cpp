3a4af88f4bd4fff508100a6608586db93d3e9c2c
Ivan Krechetov <ikr@ikr.li>
2021-06-08 21:51:15 +0200

    AC on the hard version
    See https://www.youtube.com/watch?v=7aELXV30W_I&t=6044s

--- codeforces/Potions/solution.cpp
+++ codeforces/Potions/solution.cpp
@@ -1,27 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
-    os << '[';
-    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
-        if (i != xs.cbegin()) os << ' ';
-        os << *i;
-    }
-    os << ']';
-    return os;
-}
-
-template <typename T>
-ostream &operator<<(ostream &os, const vector<vector<T>> &xss) {
-    for (const auto xs : xss) os << xs << '\n';
-    return os;
-}
-
 using vi = vector<int>;
-using vvi = vector<vi>;
 using ll = long long;
-using vll = vector<ll>;
-using vvll = vector<vll>;
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
@@ -29,32 +10,33 @@ template <typename T> constexpr int inof(const T x) {
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-static constexpr ll INF = 1e15;
-
-int max_drinks(const vll &xs) {
-    // health [at index i] [after j potions drunk]
-    vvll dp(sz(xs), vll(sz(xs) + 1, -INF));
-
-    dp[0][0] = 0;
-    if (xs[0] >= 0) dp[0][1] = xs[0];
-
-    for (int i = 1; i < sz(xs); ++i) {
-        dp[i][0] = 0;
-
-        for (int j = 1; j <= i + 1; ++j) {
-            dp[i][j] = dp[i - 1][j];
-
-            if (dp[i - 1][j - 1] + xs[i] >= 0) {
-                dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + xs[i]);
-            }
-        }
-    }
-
+int max_drinks(const vi &xs) {
+    ll hp = 0;
     int ans = 0;
+    priority_queue<int> negs;
 
-    for (int i = 0; i < sz(xs); ++i) {
-        for (int j = 1; j <= i + 1; ++j) {
-            if (dp[i][j] >= 0) ans = max(ans, j);
+    for (const int x : xs) {
+        if (x >= 0) {
+            ++ans;
+            hp += x;
+            continue;
+        }
+
+        if (x + hp >= 0) {
+            negs.push(-x);
+            ++ans;
+            hp += x;
+            continue;
+        }
+
+        if (negs.empty()) continue;
+
+        const int y = -negs.top();
+        if (y < x) {
+            negs.pop();
+            negs.push(-x);
+            hp -= y;
+            hp += x;
         }
     }
 
@@ -68,7 +50,7 @@ int main() {
     int n;
     cin >> n;
 
-    vll xs(n);
+    vi xs(n);
     for (auto &x : xs) cin >> x;
 
     cout << max_drinks(xs) << '\n';
