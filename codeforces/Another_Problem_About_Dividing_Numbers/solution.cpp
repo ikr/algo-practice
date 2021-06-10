b0bffeb85e6e7807974b91ccb1907a27f22dcf73
Ivan Krechetov <ikr@ikr.su>
2021-06-10 18:49:12 +0200

    WA

--- codeforces/Another_Problem_About_Dividing_Numbers/solution.cpp
+++ codeforces/Another_Problem_About_Dividing_Numbers/solution.cpp
@@ -14,35 +14,82 @@ template <typename T> constexpr int inof(const T x) {
 template <typename T> constexpr ll llof(const T x) {
     return static_cast<ll>(x);
 }
-template <typename T> constexpr double doof(const T x) {
-    return static_cast<double>(x);
-}
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-map<int, set<int>> steps_by_num(const int x) {
-    map<int, set<int>> ans;
+using ull = unsigned long long;
 
-    function<void(int, int)> recur;
-    recur = [&](const int steps, const int y) {
-        for (int i = 2; llof(i) * i <= llof(y); ++i) {
-            if (y % i) continue;
-            const int q = y / i;
-            ans[i].insert(steps + 1);
-            ans[q].insert(steps + 1);
+ull modmul(ull a, ull b, ull M) {
+    ll ret = a * b - M * ull(1.L / M * a * b);
+    return ret + M * (ret < 0) - M * (ret >= (ll)M);
+}
 
-            recur(steps + 1, i);
-            recur(steps + 1, q);
-        }
-    };
+ull pollard(ull n) {
+    auto f = [n](ull x) { return modmul(x, x, n) + 1; };
+    ull x = 0, y = 0, t = 30, prd = 2, i = 1, q;
+    while (t++ % 40 || __gcd(prd, n) == 1) {
+        if (x == y) x = ++i, y = f(x);
+        if ((q = modmul(prd, max(x, y) - min(x, y), n))) prd = q;
+        x = f(x), y = f(f(y));
+    }
+    return __gcd(prd, n);
+}
 
-    recur(0, x);
+ull modpow(ull b, ull e, ull mod) {
+    ull ans = 1;
+    for (; e; b = modmul(b, b, mod), e /= 2)
+        if (e & 1) ans = modmul(ans, b, mod);
     return ans;
-};
+}
+
+bool isPrime(ull n) {
+    if (n < 2 || n % 6 % 4 != 1) return (n | 1) == 3;
+    ull A[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022},
+        s = __builtin_ctzll(n - 1), d = n >> s;
+    for (ull a : A) {
+        // ^ count t r a i l i n g zeroes
+        ull p = modpow(a % n, d, n), i = s;
+        while (p != 1 && p != n - 1 && a % n && i--) p = modmul(p, p, n);
+        if (p != n - 1 && i != s) return 0;
+    }
+    return 1;
+}
+
+vector<ull> factor(ull n) {
+    if (n == 1) return {};
+    if (isPrime(n)) return {n};
+    ull x = pollard(n);
+    auto l = factor(x), r = factor(n / x);
+    l.insert(l.end(), cbegin(r), cend(r));
+    return l;
+}
 
 bool is_possible(const int a, const int b, const int k) {
-    const auto sbn_a = steps_by_num(a);
-    const auto sbn_b = steps_by_num(b);
+    if (a == 1 && b == 1) return false;
+
+    const auto pa = sz(factor(a));
+    const auto pb = sz(factor(b));
+
+    if (a == 1) {
+        return pb >= k;
+    }
+
+    if (b == 1) {
+        return pa >= k;
+    }
+
+    const auto pg = sz(factor(gcd(a, b)));
+
+    for (int q = 1; q <= pg; ++q) {
+        const int hia = pa - q;
+        const int hib = pb - q;
+
+        for (int i = 0; i <= hia; ++i) {
+            for (int j = 0; j <= hib; ++j) {
+                if (i + j == k) return true;
+            }
+        }
+    }
 
     return false;
 }
@@ -50,7 +97,6 @@ bool is_possible(const int a, const int b, const int k) {
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
-    cout << setprecision(9) << fixed;
 
     int t;
     cin >> t;
