24f69ca5ff663c34ce22371447dab53c300686c3
Ivan Krechetov <ikr@ikr.su>
2021-06-15 17:28:55 +0200

    Bootstrap

---
+++ stepik/A+B/solution.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int x, y;
+        cin >> x >> y;
+        cout << (x + y) << '\n';
+    }
+
+    return 0;
+}
