69b508cb1ed0ab14aec730b201b1545f3260ce6e
Ivan Krechetov <ikr@ikr.su>
2021-06-19 10:45:30 +0200

    AC

--- codeforces/Challenging_Cliffs/solution.cpp
+++ codeforces/Challenging_Cliffs/solution.cpp
@@ -1,24 +1,11 @@
 #include <bits/stdc++.h>
-#include <functional>
 using namespace std;
 
-using ll = long long;
 using vi = vector<int>;
-using vvi = vector<vi>;
-using pii = pair<int, int>;
-using vll = vector<ll>;
-using vvll = vector<vll>;
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
-template <typename T> constexpr ll llof(const T x) {
-    return static_cast<ll>(x);
-}
-template <typename T> constexpr double doof(const T x) {
-    return static_cast<double>(x);
-}
-
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
 template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
@@ -29,45 +16,29 @@ template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
     return os;
 }
 
-int score(const vi &xs) {
-    vi ds(sz(xs));
-    adjacent_difference(cbegin(xs), cend(xs), begin(ds));
+int min_diff_index(const vi &xs) {
+    int ans = -1;
+    int best = INT_MAX;
 
-    return count_if(next(cbegin(ds)), cend(ds),
-                    [](const int x) { return x >= 0; });
+    for (int i = 0; i < sz(xs) - 1; ++i) {
+        const int curr = xs[i + 1] - xs[i];
+
+        if (curr < best) {
+            best = curr;
+            ans = i;
+        }
+    }
+
+    return ans;
 }
 
 vi optimal_course(vi xs) {
     sort(begin(xs), end(xs));
+    if (sz(xs) == 2) return xs;
 
-    vi ds(sz(xs));
-    adjacent_difference(cbegin(xs), cend(xs), begin(ds));
-    const int lo = *min_element(next(cbegin(ds)), cend(ds));
-
-    int best_i = -1;
-    int best_score = 0;
-
-    for (int i = 0; i < sz(xs) - 1; ++i) {
-        if (xs[i + 1] - xs[i] > lo) continue;
-
-        int score = sz(xs) - 1;
-        if (xs[i] > xs[0]) --score;
-        if (xs[i + 1] < xs.back()) --score;
-
-        if (score > best_score) {
-            best_i = i;
-            best_score = score;
-        }
-    }
-
-    assert(best_i >= 0);
-    swap(xs[0], xs[best_i]);
-    swap(xs[best_i + 1], xs.back());
-
-    vi ys = xs;
-    sort(next(begin(ys)), prev(end(ys)), greater<int>{});
-
-    return score(xs) > score(ys) ? xs : ys;
+    const auto p = min_diff_index(xs);
+    rotate(begin(xs), begin(xs) + p + 1, end(xs));
+    return xs;
 }
 
 int main() {
