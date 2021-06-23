cfd286f8547ac69aa2ccd39fae9f4b6dad00cfa0
Ivan Krechetov <ikr@ikr.li>
2021-06-23 07:57:48 +0200

    Restart

--- atcoder/Divide_Both/solution.cpp
+++ atcoder/Divide_Both/solution.cpp
@@ -13,66 +13,59 @@ template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
 
 using ll = long long;
 using vi = vector<int>;
-using vvi = vector<vi>;
-using pii = pair<int, int>;
-using vll = vector<ll>;
-using vvll = vector<vll>;
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
-template <typename T> constexpr ll llof(const T x) {
-    return static_cast<ll>(x);
-}
-template <typename T> constexpr double doof(const T x) {
-    return static_cast<double>(x);
-}
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-// https://www.geeksforgeeks.org/least-prime-factor-of-numbers-till-n/
-vi least_prime_factor(int n) {
-    // Create a vector to store least primes.
-    // Initialize all entries as 0.
-    vector<int> ans(n + 1, 0);
+template <typename T> constexpr ll llof(const T x) {
+    return static_cast<ll>(x);
+}
 
-    // We need to print 1 for 1.
-    ans[1] = 1;
+ll mutually_prime_pairs_num_bruteforce(const int l, const int r) {
+    ll ans = 0;
+    for (ll i = l; i <= r - 1; ++i) {
+        for (ll j = i + 1; j <= r; ++j) {
+            if (gcd(i, j) == 1) ++ans;
+        }
+    }
+    return ans;
+}
 
-    for (int i = 2; i <= n; i++) {
-        // least_prime[i] == 0
-        // means it i is prime
-        if (ans[i] == 0) {
-            // marking the prime number
-            // as its own lpf
-            ans[i] = i;
+vi precalculate_phi(const int LIM) {
+    vi phi(LIM, 0);
 
-            // mark it as a divisor for all its
-            // multiples if not already marked
-            for (ll j = llof(i) * i; j <= llof(n); j += i)
-                if (ans[j] == 0) ans[j] = i;
+    for (int i = 0; i < LIM; ++i) phi[i] = i & 1 ? i : i / 2;
+
+    for (int i = 3; i < LIM; i += 2) {
+        if (phi[i] == i) {
+            for (int j = i; j < LIM; j += i) {
+                phi[j] -= phi[j] / i;
+            }
         }
     }
 
+    return phi;
+}
+
+ll mutually_prime_pairs_num(const vi &phi, const int l, const int r) {
+    ll ans = 0;
+
+    for (int i = l; i <= r; ++i) {
+        ans += phi[r] - phi[l - 1];
+    }
+
+    --ans;
     return ans;
 }
 
 ll proper_pairs_num(const int l, const int r) {
-    const auto lpf = least_prime_factor(r);
-    ll ans = 0;
-    set<int> seen;
-
-    for (int i = l; i <= r; ++i) {
-        const int p = lpf[i];
-        if (p == i || seen.count(p)) continue;
-
-        const int d = r - i + 1;
-        const ll k = d / p + 1 - d / i;
-        ans += k * (k - 1);
-        seen.insert(p);
-    }
-
-    return ans;
+    const auto phi = precalculate_phi(r + 1);
+    cerr << mutually_prime_pairs_num_bruteforce(l, r) << " / "
+         << mutually_prime_pairs_num(phi, l, r) << endl;
+    return -1;
 }
 
 int main() {
