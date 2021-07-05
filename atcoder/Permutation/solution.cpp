7afd955202f5d4ed3c69cb4ca1a2649790bcdb7e
Ivan Krechetov <ikr@ikr.li>
2021-07-05 18:20:09 +0200

    Optimize with prefix sums into an O(n²) solution ⇒ AC

--- atcoder/Permutation/solution.cpp
+++ atcoder/Permutation/solution.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 using mint = atcoder::modint1000000007;
 
+struct mplus final {
+    mint operator()(const mint a, const mint b) const { return a + b; }
+};
+
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
@@ -17,19 +21,23 @@ unsigned int permutations_num(const string &s) {
     vector<vector<mint>> dp(n + 1, vector<mint>(n + 1, 0));
     dp[1][1] = 1;
 
+    vector<mint> ss(n + 1, 0);
+
     for (int i = 2; i <= n; ++i) {
         const bool asc = (s[i - 2] == '<');
+        partial_sum(cbegin(dp[i - 1]), cbegin(dp[i - 1]) + i, begin(ss),
+                    mplus{});
 
         for (int j = 1; j <= i; ++j) {
-            for (int k = 1; k < i; ++k) {
-                const int k_ = k >= j ? k + 1 : k;
-                if (asc == (k_ < j)) dp[i][j] += dp[i - 1][k];
+            if (asc) {
+                dp[i][j] += ss[j - 1];
+            } else {
+                dp[i][j] += ss[i - 1] - ss[j - 1];
             }
         }
     }
 
-    return accumulate(cbegin(dp.back()), cend(dp.back()), mint{0},
-                      [](const mint agg, const mint x) { return agg + x; })
+    return accumulate(cbegin(dp.back()), cend(dp.back()), mint{0}, mplus{})
         .val();
 }
 
