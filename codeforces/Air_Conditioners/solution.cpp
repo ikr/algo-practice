35d66904edc494a7bd6472249b56563c4910098f
Ivan Krechetov <ikr@ikr.li>
2021-07-10 18:42:37 +0200

    Clean up

--- codeforces/Air_Conditioners/solution.cpp
+++ codeforces/Air_Conditioners/solution.cpp
@@ -1,21 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
 using vi = vector<int>;
-using vvi = vector<vi>;
 using pii = pair<int, int>;
-using vll = vector<ll>;
-using vvll = vector<vll>;
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
-}
-template <typename T> constexpr ll llof(const T x) {
-    return static_cast<ll>(x);
-}
-template <typename T> constexpr double doof(const T x) {
-    return static_cast<double>(x);
 }
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
@@ -64,7 +54,6 @@ vi temperatures(const int n, const vi &xs, vi ts) {
         const int i = min(k - 1, inof(distance(cbegin(xs), it)));
         const int j = min(k - 1, inof(distance(cbegin(xs), jt)));
 
-        // cerr << "i:" << i << " j:" << j << endl;
         ans[x] = min(ts[i] + abs(x - xs[i]), ts[j] + abs(x - xs[j]));
     }
 
@@ -74,7 +63,6 @@ vi temperatures(const int n, const vi &xs, vi ts) {
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
-    cout << setprecision(9) << fixed;
 
     int q;
     cin >> q;
