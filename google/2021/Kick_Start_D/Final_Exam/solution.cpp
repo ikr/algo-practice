d8aba54d585944816be19c47631c72d82a0f97e9
Ivan Krechetov <ikr@ikr.li>
2021-07-18 12:24:15 +0200

    Draft the new solution

--- google/2021/Kick_Start_D/Final_Exam/solution.cpp
+++ google/2021/Kick_Start_D/Final_Exam/solution.cpp
@@ -22,8 +22,59 @@ template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
     return os;
 }
 
-vll solve(const set<pair<ll, ll>> ps, const vll &s) {
+ll closest_to(const pair<ll, ll> &ab, const ll x) {
+    const auto [a, b] = ab;
+
+    if (x <= a) return a;
+    if (b <= x) return b;
+
+    assert(a < x && x < b);
+    return x;
+}
+
+using Iter = set<pair<ll, ll>>::const_iterator;
+
+void puncture_out(set<pair<ll, ll>> &ps, const Iter it, const ll x) {
+    const auto [a, b] = *it;
+    const auto c = closest_to(*it, x);
+
+    ps.erase(it);
+
+    if (a == b) {
+        assert(c == a);
+    } else if (c == a) {
+        ps.emplace(a + 1, b);
+    } else if (c == b) {
+        ps.emplace(a, b - 1);
+    } else {
+        assert(a < c && c < b);
+        ps.emplace(a, c - 1);
+        ps.emplace(c + 1, b);
+    }
+}
+
+vll solve(set<pair<ll, ll>> ps, const vll &s) {
     vll ans(sz(s), 0);
+
+    for (int i = 0; i < sz(s); ++i) {
+        const auto jt = [&]() {
+            const auto x = ps.find({s[i], 0});
+            return x == cend(ps) ? prev(x) : x;
+        }();
+        const auto it = jt == cbegin(ps) ? jt : prev(jt);
+
+        const auto x = closest_to(*it, s[i]);
+        const auto y = closest_to(*jt, s[i]);
+
+        if (abs(s[i] - x) <= abs(s[i] - y)) {
+            ans[i] = x;
+            puncture_out(ps, it, x);
+        } else {
+            ans[i] = y;
+            puncture_out(ps, jt, y);
+        }
+    }
+
     return ans;
 }
 
@@ -47,7 +98,7 @@ int main() {
         vll ss(m);
         for (auto &s : ss) cin >> s;
 
-        cout << "Case #" << i << ": " << solve(ps, ss) << '\n';
+        cout << "Case #" << i << ": " << solve(move(ps), ss) << '\n';
     }
 
     return 0;
