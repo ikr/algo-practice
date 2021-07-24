d1058bd3554bd783de6382d640a4efebf9d89dd2
Ivan Krechetov <ikr@ikr.li>
2021-07-24 15:12:35 +0200

    TLE on D

---
+++ atcoder/Cycle_Hit/solution.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+template <typename T> constexpr ll llof(const T x) {
+    return static_cast<ll>(x);
+}
+template <typename T> constexpr double doof(const T x) {
+    return static_cast<double>(x);
+}
+
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+    cout << setprecision(9) << fixed;
+
+    set<string> xs;
+    int t = 4;
+    while (t--) {
+        string x;
+        cin >> x;
+        xs.insert(x);
+    }
+    const bool ans = sz(xs) == 4;
+    cout << (ans ? "Yes" : "No") << '\n';
+
+    return 0;
+}
