a6567bbfbd65b20433965375c025b92d54f3db5d
Ivan Krechetov <ikr@ikr.su>
2021-07-25 13:04:12 +0200

    A, B — AC; C — WA

---
+++ codechef/Maximum_Array_Xor/solution.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+template <typename T> constexpr ll llof(const T x) {
+    return static_cast<ll>(x);
+}
+
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+ll max_value(const int n, const int k) {
+    return 2LL * llof(k) * (llof(1 << n) - 1LL);
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+
+        cout << max_value(n, k) << '\n';
+    }
+
+    return 0;
+}
