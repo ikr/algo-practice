5c0512250c2b12c8d4a1a3cde0874b304bb320a8
Ivan Krechetov <ikr@ikr.li>
2021-07-25 13:04:12 +0200

    A, B — AC; C — WA

---
+++ codechef/Chess_Format/solution.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+template <typename T> constexpr ll llof(const T x) {
+    return static_cast<ll>(x);
+}
+template <typename T> constexpr double doof(const T x) {
+    return static_cast<double>(x);
+}
+
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+int mathch_format(const int a, const int b) {
+    const auto s = a + b;
+
+    if (s < 3) return 1;
+    if (s <= 10) return 2;
+    if (s <= 60) return 3;
+    return 4;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+    cout << setprecision(9) << fixed;
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+
+        cout << mathch_format(a, b) << '\n';
+    }
+
+    return 0;
+}
