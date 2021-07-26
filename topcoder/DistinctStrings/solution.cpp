8024ced4abce2cdfc557432730cbf10c1e61466e
Ivan Krechetov <ikr@ikr.li>
2021-07-26 10:24:20 +0200

    Assert on properties

--- topcoder/DistinctStrings/solution.cpp
+++ topcoder/DistinctStrings/solution.cpp
@@ -2,34 +2,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
 using vi = vector<int>;
-using vvi = vector<vi>;
-using pii = pair<int, int>;
-using vll = vector<ll>;
-using vvll = vector<vll>;
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
-template <typename T> constexpr ll llof(const T x) {
-    return static_cast<ll>(x);
-}
-template <typename T> constexpr double doof(const T x) {
-    return static_cast<double>(x);
-}
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
-
-template <typename T>
-constexpr typename vector<T>::const_iterator cbegin(const vector<T> &xs) {
-    return xs.cbegin();
-}
-
-template <typename T>
-constexpr typename vector<T>::const_iterator cend(const vector<T> &xs) {
-    return xs.cend();
-}
 
 vi next_sig(vi ds) {
     for (int i = sz(ds) - 2; i >= 0; --i) {
@@ -43,7 +22,7 @@ vi next_sig(vi ds) {
     return ds;
 }
 
-string corresponding(const string &xs, const vi &ds) {
+string encode(const string &xs, const vi &ds) {
     string ans;
 
     for (int i = 0; i < sz(xs); ++i) {
@@ -70,7 +49,7 @@ struct DistinctStrings final {
 
         vector<string> ans;
         for (int i = 0; i < N; ++i) {
-            ans.push_back(corresponding(xs, ds));
+            ans.push_back(encode(xs, ds));
             const auto ds_ = next_sig(ds);
 
             if (ds_ == ds) {
@@ -87,17 +66,44 @@ struct DistinctStrings final {
     }
 };
 
+template <typename T>
+constexpr typename vector<T>::const_iterator cbegin(const vector<T> &xs) {
+    return xs.cbegin();
+}
+
+template <typename T>
+constexpr typename vector<T>::const_iterator cend(const vector<T> &xs) {
+    return xs.cend();
+}
+
+template <typename T> bool are_all_of_length(const vector<T> &xs, const int l) {
+    return all_of(cbegin(xs), cend(xs),
+                  [l](const auto &x) { return sz(x) == l; });
+}
+
+template <typename T> bool are_all_distinct(const vector<T> &xs) {
+    return sz(set<string>(cbegin(xs), cend(xs))) == sz(xs);
+}
+
 // clang-format off
 const lest::test tests[] = {
     CASE("Example 0") {
         const auto actual = DistinctStrings{}.generate(5, "qwertyuiopasdfghjklzxcvbnm", 10);
-        const vector<string> expected{"hello", "abcde", "wwwww", "coins", "coder", "digit", "clash", "qwert", "puppy", "kitty"};
-        EXPECT(actual == expected);
+        EXPECT(sz(actual) == 10);
+        EXPECT(are_all_of_length(actual, 5));
+        EXPECT(are_all_distinct(actual));
     },
     CASE("Example 1") {
         const auto actual = DistinctStrings{}.generate(3, "ABCDE02468", 17);
-        const vector<string> expected{"ABC", "D2E", "DDD", "AAA", "EEE", "024", "042", "204", "240", "402", "420", "4AB", "4EE", "E4E", "ED4", "868", "844"};
-        EXPECT(actual == expected);
+        EXPECT(sz(actual) == 17);
+        EXPECT(are_all_of_length(actual, 3));
+        EXPECT(are_all_distinct(actual));
+    },
+    CASE("Short A") {
+        const auto actual = DistinctStrings{}.generate(3, "0123456789", 200);
+        EXPECT(sz(actual) == 200);
+        EXPECT(are_all_of_length(actual, 3));
+        EXPECT(are_all_distinct(actual));
     },
 };
 // clang-format on
