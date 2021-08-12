69414311f8475cf4611852d57706175517b235fc
Ivan Krechetov <ikr@ikr.su>
2021-08-12 21:12:30 +0200

    Extract `milestones`

--- cses/Introductory_Problems/Digit_Queries/solution.cpp
+++ cses/Introductory_Problems/Digit_Queries/solution.cpp
@@ -2,24 +2,25 @@
 using namespace std;
 
 using ll = long long;
+using vll = vector<ll>;
 
 constexpr ll mpow(const ll base, const ll exp) {
     if (!exp) return 1;
     if (exp % 2LL) return base * mpow(base, exp - 1);
-
     const auto q = mpow(base, exp / 2LL);
     return q * q;
 }
 
-int kth_digit(const ll k) {
-    ll curr = 0;
-
+vll milestones() {
+    vll ans{0};
     for (ll i = 1; i <= 17; ++i) {
-        curr += 9LL * mpow(10, i - 1) * i;
-
-        cerr << "curr: " << curr << endl;
+        ans.push_back(ans.back() + 9LL * mpow(10, i - 1) * i);
     }
+    return ans;
+}
 
+int kth_digit(const ll k) {
+    const auto ms = milestones();
     return -1;
 }
 
