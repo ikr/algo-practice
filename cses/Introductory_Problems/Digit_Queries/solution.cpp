ef29b7e8aae225631486074cc9a2f7bab6a92d6a
Ivan Krechetov <ikr@ikr.su>
2021-08-13 07:27:54 +0200

    First draft

--- cses/Introductory_Problems/Digit_Queries/solution.cpp
+++ cses/Introductory_Problems/Digit_Queries/solution.cpp
@@ -4,6 +4,16 @@ using namespace std;
 using ll = long long;
 using vll = vector<ll>;
 
+template <typename T> constexpr ll llof(const T x) {
+    return static_cast<ll>(x);
+}
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
 constexpr ll mpow(const ll base, const ll exp) {
     if (!exp) return 1;
     if (exp % 2LL) return base * mpow(base, exp - 1);
@@ -19,21 +29,48 @@ vll milestones() {
     return ans;
 }
 
-int kth_digit(const ll k) {
-    const auto ms = milestones();
-    return -1;
+int block_size(const vll &ms, const ll k) {
+    const auto it = lower_bound(cbegin(ms), cend(ms), k);
+    return inof(distance(cbegin(ms), it));
+}
+
+int kth_digit(const ll x, const int k) {
+    assert(k >= 0);
+
+    stringstream ss;
+    ss << x;
+    const string xs = ss.str();
+
+    assert(sz(xs) >= 0 && k < sz(xs));
+    return inof(xs[k] - '0');
+}
+
+int kth_digit_in_seq(const vll &ms, const ll k) {
+    const auto bs = block_size(ms, k);
+    assert(bs < sz(ms));
+    if (k == ms[bs]) return 9;
+
+    const auto i = k - ms[bs - 1];
+    const auto block_index = i / llof(bs);
+    const auto digit_index = i % llof(bs);
+    assert(digit_index < bs);
+
+    const auto block_value = mpow(10, bs - 1) + block_index;
+    return kth_digit(block_value, inof(digit_index));
 }
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
 
+    const auto ms = milestones();
+
     int q;
     cin >> q;
     while (q--) {
         ll k;
         cin >> k;
-        cout << kth_digit(k) << '\n';
+        cout << kth_digit_in_seq(ms, k) << '\n';
     }
 
     return 0;
