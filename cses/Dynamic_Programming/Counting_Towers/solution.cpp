af1a20832b7c95c0902139211e7c0bed78af0f65
Ivan Krechetov <ikr@ikr.su>
2021-08-15 10:01:54 +0200

    Bootstrap

---
+++ cses/Dynamic_Programming/Counting_Towers/solution.cpp
@@ -0,0 +1,28 @@
+#include <atcoder/modint>
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+using mint = atcoder::modint1000000007;
+
+mint towers_num(const int n) { return -1; }
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << towers_num(n).val() << '\n';
+    }
+
+    return 0;
+}
