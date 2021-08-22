53bfdf24842b56d00e48b4392b78815113a0d0d3
Ivan Krechetov <ikr@ikr.li>
2021-08-22 10:53:15 +0200

    Bootstrap

---
+++ codeforces/2021/Nearest_Beautiful_Number_easy/solution.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using vi = vector<int>;
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+template <typename T> vi digits_reversed(T n) {
+    vi ans;
+
+    while (n) {
+        ans.push_back(n % 10);
+        n /= 10;
+    }
+
+    return ans;
+}
+
+template <typename T> vi digits(T n) {
+    vi ans = digits_reversed(n);
+    reverse(begin(ans), end(ans));
+    return ans;
+}
+
+template <typename T> T number(const vector<int> &ds) {
+    if (ds.empty()) return 0;
+
+    T ans = 0;
+    T mul = 1;
+    for (auto it = ds.crbegin(); it != ds.crend(); ++it) {
+        ans += (*it) * mul;
+        mul *= static_cast<T>(10);
+    }
+    return ans;
+}
+
+int min_k_beautiful_bound(const int n, const int k) {
+    assert(number<int>(digits(n)) == n);
+    return number<int>(digits(n));
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        cout << min_k_beautiful_bound(n, k) << '\n';
+    }
+
+    return 0;
+}
