aac2ad9e0dda7e10e0f09c976f5b4bfeec8bf365
Ivan Krechetov <ikr@ikr.su>
2021-08-22 15:32:22 +0200

    Draft: missing many of the duo bounds

--- codeforces/2021/Nearest_Beautiful_Number_easy/solution.cpp
+++ codeforces/2021/Nearest_Beautiful_Number_easy/solution.cpp
@@ -6,6 +6,7 @@ using vi = vector<int>;
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
+
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
 template <typename T> vi digits_reversed(T n) {
@@ -37,21 +38,72 @@ template <typename T> T number(const vector<int> &ds) {
     return ans;
 }
 
-int min_k_beautiful_bound(const int n, const int k) {
-    assert(number<int>(digits(n)) == n);
-    return number<int>(digits(n));
+vi gather_solos() {
+    vi ans;
+    for (int i = 1; i <= 9; ++i) {
+        for (int j = 1; j <= 9; ++j) {
+            ans.push_back(number<int>(vi(i, j)));
+        }
+    }
+
+    return ans;
+}
+
+int two_digits_combo(const int d1, const int d2, const int bits) {
+    assert(d1 != d2);
+    vi ans(9, 0);
+
+    for (int i = 0; i < 9; ++i) {
+        ans[i] = ((1 << i) & bits) ? d2 : d1;
+    }
+
+    return number<int>(ans);
+}
+
+vi gather_duos() {
+    const int lim = (1 << 9);
+    vi ans{1'000'000'000};
+
+    for (int bits = 1; bits < lim - 1; ++bits) {
+        for (int i = 0; i <= 8; ++i) {
+            for (int j = i + 1; j <= 9; ++j) {
+                ans.push_back(two_digits_combo(i, j, bits));
+            }
+        }
+    }
+
+    return ans;
+}
+
+pair<vi, vi> bound_tables() {
+    auto a = gather_solos();
+    auto b = gather_duos();
+    copy(cbegin(a), cend(a), back_inserter(b));
+
+    sort(begin(a), end(a));
+    sort(begin(b), end(b));
+
+    return {a, b};
+}
+
+int min_k_beautiful_bound(const vi &a, const vi &b, const int n, const int k) {
+    const auto &xs = k == 1 ? a : b;
+    return *lower_bound(cbegin(xs), cend(xs), n);
 }
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
 
+    const auto [a, b] = bound_tables();
+    assert(find(cbegin(b), cend(b), 221) != cend(b));
+
     int t;
     cin >> t;
     while (t--) {
         int n, k;
         cin >> n >> k;
-        cout << min_k_beautiful_bound(n, k) << '\n';
+        cout << min_k_beautiful_bound(a, b, n, k) << '\n';
     }
 
     return 0;
