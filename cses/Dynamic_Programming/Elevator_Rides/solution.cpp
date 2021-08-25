39466cbec7e193c64163a07cf15ef8c7f18a9ebe
Ivan Krechetov <ikr@ikr.li>
2021-08-25 07:29:24 +0200

    Hm… WA on 3 test cases

--- cses/Dynamic_Programming/Elevator_Rides/solution.cpp
+++ cses/Dynamic_Programming/Elevator_Rides/solution.cpp
@@ -1,48 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
-    os << '[';
-    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
-        if (i != xs.cbegin()) os << ' ';
-        os << *i;
-    }
-    os << ']';
-    return os;
-}
-
-using ll = long long;
 using vi = vector<int>;
-using vll = vector<ll>;
+using pii = pair<int, int>;
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
 
-template <typename T> constexpr ll llof(const T x) {
-    return static_cast<ll>(x);
-}
-
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-vll subset_weights(const vi &xs) {
-    const auto lim = 1 << sz(xs);
-    vll ans(lim, 0);
+int min_rides(const int cap, const vi &xs) {
+    const auto n = sz(xs);
+    const auto lim = 1 << n;
+
+    // dp[i] is a pair (kᵢ wᵢ) for a subset bits of passengers i:
+    // kᵢ is the minimal number of elevator rides for the subset i
+    // wᵢ is the total weight of the last ride
+    vector<pii> dp(lim, {n + 1, cap});
+    dp[0] = {1, 0};
 
     for (int bits = 1; bits < lim; ++bits) {
-        for (int i = 0; i < sz(xs); ++i) {
-            if ((1 << i) & bits) {
-                ans[bits] += xs[i];
+        for (int j = 0; j < n; ++j) {
+            if (bits & (1 << j)) {
+                const auto [k0, w0] = dp[bits ^ (1 << j)];
+                auto &[k, w] = dp[bits];
+                const auto fits = w0 + xs[j] <= cap;
+
+                if (fits) {
+                    if (k0 < k) {
+                        k = k0;
+                        w = w0 + xs[j];
+                    }
+                } else {
+                    if (k0 + 1 < k) {
+                        k = k0 + 1;
+                        w = xs[j];
+                    }
+                }
             }
         }
     }
 
-    return ans;
-}
-
-int min_rides(const int cap, const vi &xs) {
-    cerr << subset_weights(xs) << endl;
-    return -1;
+    return dp.back().first;
 }
 
 int main() {
