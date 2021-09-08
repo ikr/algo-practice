a9a211223fa2563b05831387dea1a124bf08ea10
Ivan Krechetov <ikr@ikr.li>
2021-09-08 07:51:25 +0200

    Implement `is_possible_transition`

--- cses/Dynamic_Programming/Counting_Tilings/solution.cpp
+++ cses/Dynamic_Programming/Counting_Tilings/solution.cpp
@@ -2,9 +2,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using vi = vector<int>;
 using mint = atcoder::modint1000000007;
 
-bool is_possible_upper_row(const int n, const int bits) {
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+bool is_possible_top_row(const int n, const int bits) {
     int span = (bits & 1) ? 1 : 0;
     for (int shift = 1; shift < n; ++shift) {
         if ((1 << shift) & bits) {
@@ -18,11 +25,25 @@ bool is_possible_upper_row(const int n, const int bits) {
     return span % 2 == 0;
 }
 
-template <typename T> constexpr int inof(const T x) {
-    return static_cast<int>(x);
-}
+bool is_possible_transition(const int n, const int bits_a, const int bits_b) {
+    assert(n <= 10);
 
-template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+    vi set_vars;
+    for (int i = 0; i < n; ++i) {
+        const int a = (1 << i) & bits_a;
+        const int b = (1 << i) & bits_b;
+        if (!a && !b) return false;
+        if (a && b) set_vars.push_back(i);
+    }
+
+    if (sz(set_vars) % 2) return false;
+
+    for (int i = 0; i < sz(set_vars); ++i) {
+        if (i % 2 && set_vars[i] != set_vars[i - 1] + 1) return false;
+    }
+
+    return true;
+}
 
 int num_ways(const int n, const int m) {
     if (m == 1) return n % 2 ? 0 : 1;
@@ -33,11 +54,10 @@ int num_ways(const int n, const int m) {
     vector<vector<mint>> dp(m, vector<mint>(1 << n, 0));
 
     for (int bits = 0; bits < (1 << n); ++bits) {
-        if (is_possible_upper_row(n, bits)) dp[0][bits] = 1;
+        if (is_possible_top_row(n, bits)) dp[0][bits] = 1;
     }
 
     for (int i = 1; i < m - 1; ++i) {
-
     }
 
     return accumulate(cbegin(dp.back()), cend(dp.back()), mint{0}).val();
@@ -46,6 +66,15 @@ int num_ways(const int n, const int m) {
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
+
+    assert(is_possible_transition(10, 0, (1 << 10) - 1));
+    assert(is_possible_transition(7, 0b1100110, 0b0011001));
+    assert(!is_possible_transition(7, 0b1100110, 0b0010001));
+    assert(is_possible_transition(7, 0b1100110, 0b0011111));
+    assert(!is_possible_transition(7, 0b1100110, 0b1011001));
+    assert(is_possible_transition(7, 0b1100110, 0b1111111));
+    assert(is_possible_transition(8, (1 << 8) - 1, 0b01111110));
+    assert(!is_possible_transition(8, (1 << 8) - 1, 0b01010101));
 
     int n, m;
     cin >> n >> m;
