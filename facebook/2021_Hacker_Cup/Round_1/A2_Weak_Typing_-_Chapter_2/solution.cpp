946d65c07560dd8591327dabdc92cc724c5ce876
Ivan Krechetov <ikr@ikr.su>
2021-09-12 21:27:58 +0200

    Reset with a simpler approach

--- facebook/2021_Hacker_Cup/Round_1/A2_Weak_Typing_-_Chapter_2/solution.cpp
+++ facebook/2021_Hacker_Cup/Round_1/A2_Weak_Typing_-_Chapter_2/solution.cpp
@@ -2,8 +2,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using Mint = atcoder::modint1000000007;
-
 template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
     os << '[';
     for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
@@ -14,10 +12,8 @@ template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
     return os;
 }
 
-ostream &operator<<(ostream &os, const Mint x) {
-    os << x.val();
-    return os;
-}
+using Mint = atcoder::modint1000000007;
+using vi = vector<int>;
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
@@ -25,75 +21,39 @@ template <typename T> constexpr int inof(const T x) {
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-vector<bool> gather_milestones(const string &xs) {
-    vector<bool> ans(sz(xs), false);
+vi gather_milestones(const string &xs) {
+    vi ans;
 
-    const auto it =
+    const auto it0 =
         find_if(cbegin(xs), cend(xs), [](const char x) { return x != 'F'; });
 
-    if (it == cend(xs)) return ans;
+    if (it0 == cend(xs)) return ans;
+    ans.push_back(inof(distance(cbegin(xs), it0)));
 
-    const auto jt = find_if(next(it), cend(xs), [&](const char x) {
-        return (x != 'F') && (x != *it);
-    });
+    for (auto it = it0;;) {
+        it = find_if(next(it), cend(xs),
+                     [&](const char x) { return x != 'F' && x != *it; });
 
-    if (jt == cend(xs)) return ans;
-
-    const int i0 = inof(xs.rfind(*it, distance(cbegin(xs), jt)));
-
-    ans[i0] = true;
-    char last_m = xs[i0];
-
-    for (int i = i0 + 1; i < sz(xs); ++i) {
-        if (xs[i] == 'F') continue;
-
-        if (last_m != xs[i]) {
-            last_m = xs[i];
-            ans[i] = true;
-        }
+        if (it == cend(xs)) break;
+        ans.push_back(inof(distance(cbegin(xs), it)));
     }
+
+    cerr << ans << endl;
 
     return ans;
 }
 
-struct Milestone final {
-    int index;
-    int span;
-};
-
 int solution(const string &xs) {
     const auto ms = gather_milestones(xs);
+    Mint ans = 0;
 
-    optional<Milestone> pr = [&]() -> optional<Milestone> {
-        if (!ms[0]) return nullopt;
-        return Milestone{0, 1};
-    }();
-
-    Mint ss = 0;
-    Mint curr = 0;
-
-    // dp[i] is the answer for all the sub-strings ending exactly at index i
-    vector<Mint> dp(sz(xs), 0);
-
-    for (int i = 1; i < sz(xs); ++i) {
-        if (ms[i]) {
-            if (pr) {
-                ss += pr->span;
-                curr += ss;
-                dp[i] = curr;
-
-                pr = Milestone{i, i - pr->index};
-            } else {
-                pr = Milestone{i, i + 1};
-            }
-        } else {
-            dp[i] = dp[i - 1];
-        }
+    for (int i = 1; i < sz(ms); ++i) {
+        const Mint a{ms[i - 1] + 1};
+        const Mint b{sz(xs) - ms[i]};
+        ans += a * b;
     }
 
-    cerr << dp << endl;
-
-    return accumulate(cbegin(dp), cend(dp), Mint{0}).val();
+    return ans.val();
 }
 
 int main() {
