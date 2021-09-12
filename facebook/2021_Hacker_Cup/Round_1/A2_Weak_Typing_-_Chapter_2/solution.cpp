6c5e1efb2d573f9586119e7b82252b84684fdd0b
Ivan Krechetov <ikr@ikr.su>
2021-09-12 18:20:18 +0200

    WIP

--- facebook/2021_Hacker_Cup/Round_1/A2_Weak_Typing_-_Chapter_2/solution.cpp
+++ facebook/2021_Hacker_Cup/Round_1/A2_Weak_Typing_-_Chapter_2/solution.cpp
@@ -4,6 +4,21 @@ using namespace std;
 
 using Mint = atcoder::modint1000000007;
 
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
+
+ostream &operator<<(ostream &os, const Mint x) {
+    os << x.val();
+    return os;
+}
+
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
@@ -12,12 +27,27 @@ template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
 vector<bool> gather_milestones(const string &xs) {
     vector<bool> ans(sz(xs), false);
-    optional<char> last_m;
 
-    for (int i = 0; i < sz(xs); ++i) {
+    const auto it =
+        find_if(cbegin(xs), cend(xs), [](const char x) { return x != 'F'; });
+
+    if (it == cend(xs)) return ans;
+
+    const auto jt = find_if(next(it), cend(xs), [&](const char x) {
+        return (x != 'F') && (x != *it);
+    });
+
+    if (jt == cend(xs)) return ans;
+
+    const int i0 = inof(xs.rfind(*it, distance(cbegin(xs), jt)));
+
+    ans[i0] = true;
+    char last_m = xs[i0];
+
+    for (int i = i0 + 1; i < sz(xs); ++i) {
         if (xs[i] == 'F') continue;
 
-        if (!last_m || *last_m != xs[i]) {
+        if (last_m != xs[i]) {
             last_m = xs[i];
             ans[i] = true;
         }
@@ -40,6 +70,7 @@ int solution(const string &xs) {
     }();
 
     Mint ss = 0;
+    Mint curr = 0;
 
     // dp[i] is the answer for all the sub-strings ending exactly at index i
     vector<Mint> dp(sz(xs), 0);
@@ -48,8 +79,10 @@ int solution(const string &xs) {
         if (ms[i]) {
             if (pr) {
                 ss += pr->span;
+                curr += ss;
+                dp[i] = curr;
+
                 pr = Milestone{i, i - pr->index};
-                dp[i] = ss;
             } else {
                 pr = Milestone{i, i + 1};
             }
@@ -57,6 +90,8 @@ int solution(const string &xs) {
             dp[i] = dp[i - 1];
         }
     }
+
+    cerr << dp << endl;
 
     return accumulate(cbegin(dp), cend(dp), Mint{0}).val();
 }
