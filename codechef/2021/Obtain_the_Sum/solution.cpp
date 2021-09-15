32dc9b5f0e4831177207ff452d19ca233efc569b
Ivan Krechetov <ikr@ikr.li>
2021-09-15 19:17:08 +0200

    Solve A, B, C

---
+++ codechef/2021/Obtain_the_Sum/solution.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+template <typename T> constexpr ll llof(const T x) {
+    return static_cast<ll>(x);
+}
+template <typename T> constexpr double doof(const T x) {
+    return static_cast<double>(x);
+}
+
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+ll sought_pos(const ll n, const ll s) {
+    const ll s0 = llof(n) * llof(n + 1) / 2LL;
+    const ll ans = s0 - s;
+    return (ans > 0 && ans <= n) ? ans : -1LL;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+    cout << setprecision(9) << fixed;
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        ll s;
+        cin >> s;
+
+        cout << sought_pos(n, s) << '\n';
+    }
+
+    return 0;
+}
