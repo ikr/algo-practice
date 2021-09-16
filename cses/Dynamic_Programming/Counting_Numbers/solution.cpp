6801fb8bb5a2c4ff27c1eb0474f3cde98f3d18ac
Ivan Krechetov <ikr@ikr.li>
2021-09-16 07:58:21 +0200

    First attempt

--- cses/Dynamic_Programming/Counting_Numbers/solution.cpp
+++ cses/Dynamic_Programming/Counting_Numbers/solution.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 using ll = long long;
 using vi = vector<int>;
+using vll = vector<ll>;
+
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
@@ -34,16 +45,32 @@ vi digits(const ll x) {
     return ans;
 }
 
-vector<array<ll, 10>> sought_nums_up_to(const ll hi) {
-    const auto ds = digits(hi);
-
-    // D[i][j] is # of sought ints having i digits, with the 1st digit being j
-    vector<array<ll, 10>> dp(sz(ds) + 1, array<ll, 10>{});
-
-    return dp;
+vll counts_by_length(const int digits_num) {
+    // tbl[i] is # of sought Inst having i digits, starting with a fixed digit
+    // (no matter which one)
+    vll tbl(digits_num + 1, 0);
+    tbl[1] = 1;
+    for (int i = 2; i < sz(tbl); ++i) tbl[i] = 9 * tbl[i - 1];
+    return tbl;
 }
 
-ll sought_nums_in_between(const ll a, const ll b) { return b - a + 1; }
+ll sought_nums_up_to(const ll hi) {
+    const auto ds = digits(hi);
+    const auto tbl = counts_by_length(sz(ds));
+
+    ll ans = tbl[sz(ds), ds[0]];
+
+    for (int i = 1; i < sz(ds); ++i) {
+        ans -= tbl[sz(ds) - i] * (10 - ds[i]);
+    }
+
+    return ans;
+}
+
+ll sought_nums_in_between(const ll a, const ll b) {
+    return a ? (sought_nums_up_to(b) - sought_nums_up_to(a - 1))
+             : sought_nums_up_to(b);
+}
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
