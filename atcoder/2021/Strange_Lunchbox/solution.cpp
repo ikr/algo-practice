6f2a32236b3884748d4225f2d55f88d7c9f2f332
Ivan Krechetov <ikr@ikr.li>
2021-09-18 15:21:53 +0200

    WIP

---
+++ atcoder/2021/Strange_Lunchbox/solution.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<vector<T>> &xss) {
+    for (const auto xs : xss) os << xs << '\n';
+    return os;
+}
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+static constexpr int INF = 1e9;
+static constexpr int LIM = 8;
+
+int min_lunchboxes(const pii XY, const vector<pii> &ab) {
+    const auto [X, Y] = XY;
+
+    // tbl[i][j][k] is min boxes up to index i, in order to get exactly j of A &
+    // k of B
+    vector<vvi> tbl(sz(ab), vvi(LIM, vi(LIM, INF)));
+
+    const auto [a0, b0] = ab[0];
+    tbl[0][0][0] = 0;
+    tbl[0][a0][b0] = 1;
+
+    for (int i = 1; i < sz(ab); ++i) {
+        const auto [a, b] = ab[i];
+
+        for (int j = 1; j < LIM; ++j) {
+            for (int k = 1; k < LIM; ++k) {
+                tbl[i][j][k] = tbl[i - 1][j][k];
+
+                if (j - a >= 0 && k - b >= 0) {
+                    tbl[i][j][k] =
+                        min(tbl[i][j][k], tbl[i - 1][j - a][k - b] + 1);
+                }
+            }
+        }
+    }
+
+    int ans = INF;
+    for (int j = X; j < LIM; ++j) {
+        for (int k = Y; k < LIM; ++k) {
+            ans = min(ans, tbl.back()[j][k]);
+        }
+    }
+    return ans;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+
+    int N;
+    cin >> N;
+
+    int X, Y;
+    cin >> X >> Y;
+
+    vector<pii> ab(N);
+    for (auto &[a, b] : ab) {
+        cin >> a >> b;
+    }
+
+    const auto ans = min_lunchboxes({X, Y}, ab);
+    cout << (ans >= INF ? -1 : ans) << '\n';
+    return 0;
+}
