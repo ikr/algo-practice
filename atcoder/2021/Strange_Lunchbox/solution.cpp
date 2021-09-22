ac61f91d77a20ceb2893fe014fed29f36a6d1527
Ivan Krechetov <ikr@ikr.li>
2021-09-22 07:20:27 +0200

    Revert

--- atcoder/2021/Strange_Lunchbox/solution.cpp
+++ atcoder/2021/Strange_Lunchbox/solution.cpp
@@ -36,31 +36,6 @@ int min_lunchboxes(const pii XY, const vector<pii> &ab) {
     return D.back()[X][Y];
 }
 
-int min_lunchboxes_2(const pii XY, const vector<pii> &ab) {
-    const auto [X, Y] = XY;
-
-    // D[i][j][k] is min boxes up to box i, in order to get exactly min(j, X)
-    // of A & min(k, Y) of B
-    vector<vvi> D(sz(ab), vvi(X + 1, vi(Y + 1, INF)));
-    D[0][0][0] = 0;
-
-    const auto [a0, b0] = ab[0];
-    D[0][min(a0, X)][min(b0, Y)] = 1;
-
-    for (int i = 1; i < sz(ab); ++i) {
-        const auto [a, b] = ab[i];
-
-        for (int j = 0; j <= X; ++j) {
-            for (int k = 0; k <= Y; ++k) {
-                D[i][j][k] = min(D[i - 1][j][k],
-                                 D[i - 1][max(j - a, 0)][max(k - b, 0)] + 1);
-            }
-        }
-    }
-
-    return D.back()[X][Y];
-}
-
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
@@ -76,9 +51,7 @@ int main() {
         cin >> a >> b;
     }
 
-    assert(min_lunchboxes({X, Y}, ab) == min_lunchboxes_2({X, Y}, ab));
-
-    const auto ans = min_lunchboxes_2({X, Y}, ab);
+    const auto ans = min_lunchboxes({X, Y}, ab);
     cout << (ans >= INF ? -1 : ans) << '\n';
     return 0;
 }
