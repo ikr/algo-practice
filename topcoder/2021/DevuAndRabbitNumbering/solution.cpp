b8242a2722afdfef2d38c33742e9d5934e6de168
Ivan Krechetov <ikr@ikr.li>
2021-10-10 13:01:28 +0200

    Submit a corrected solution; still failing the sys tests

--- topcoder/2021/DevuAndRabbitNumbering/solution.cpp
+++ topcoder/2021/DevuAndRabbitNumbering/solution.cpp
@@ -2,37 +2,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
 using vi = vector<int>;
-using vvi = vector<vi>;
 using pii = pair<int, int>;
-using vll = vector<ll>;
-using vvll = vector<vll>;
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
-template <typename T> constexpr ll llof(const T x) {
-    return static_cast<ll>(x);
-}
-template <typename T> constexpr double doof(const T x) {
-    return static_cast<double>(x);
-}
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
-
-template <typename T>
-constexpr typename vector<T>::const_iterator cbegin(const vector<T> &xs) {
-    return xs.cbegin();
-}
-
-template <typename T>
-constexpr typename vector<T>::const_iterator cend(const vector<T> &xs) {
-    return xs.cend();
-}
-
-string::const_iterator cbegin(const string &xs) { return xs.cbegin(); }
-string::const_iterator cend(const string &xs) { return xs.cend(); }
 
 map<int, int> gather_freqs(const vi &xs) {
     map<int, int> ans;
@@ -41,11 +18,7 @@ map<int, int> gather_freqs(const vi &xs) {
 }
 
 vector<pii> gather_kvs(const map<int, int> &xs) {
-    vector<pii> ans;
-    for (const auto kv : xs) {
-        ans.emplace_back(kv.first, kv.second);
-    }
-    return ans;
+    return vector<pii>(xs.cbegin(), xs.cend());
 }
 
 static const string YES{"can"};
@@ -55,33 +28,28 @@ struct DevuAndRabbitNumbering final {
     string canRenumber(const vi &xs) const {
         const auto ps = gather_kvs(gather_freqs(xs));
         if (ps[0].second > 3) return NO;
-        int pre = [](const int x, const int f) -> int {
+
+        const auto new_pre = [](const int x, const int f) -> int {
             assert(f > 0);
             if (f == 1) return x - 1;
             if (f == 2) return x;
             return x + 1;
-        }(ps[0].first, ps[0].second);
+        };
+
+        int pre = new_pre(ps[0].first, ps[0].second);
 
         for (int i = 1; i < sz(ps); ++i) {
             const auto x = ps[i].first;
-            const bool conseq = x - pre == 1;
+            const auto f = ps[i].second;
+
+            const bool conseq = (x - pre <= 1);
 
             if (conseq) {
-                const auto f = ps[i].second;
                 if (f > 1) return NO;
                 pre = x + 1;
             } else {
-                const auto f = ps[i].second;
                 if (f > 3) return NO;
-
-                if (f == 1) {
-                    pre = x - 1;
-                } else if (f == 2) {
-                    pre = x;
-                } else {
-                    assert(f == 3);
-                    pre = x + 1;
-                }
+                pre = new_pre(x, f);
             }
         }
 
