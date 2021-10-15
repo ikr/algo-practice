29ea4c071f3753747326bd830139b95dbc5dc70f
Ivan Krechetov <ikr@ikr.li>
2021-10-15 17:01:51 +0200

    Satisfy the sample test case locally, but…
    CSES doesn't seem to support std::variant-s.

--- cses/Dynamic_Programming/Counting_Numbers/solution.cpp
+++ cses/Dynamic_Programming/Counting_Numbers/solution.cpp
@@ -34,6 +34,7 @@ vi digits(const ll x) {
 struct LeadingZeros final {};
 struct Digit final {
     int value;
+    bool capped;
 };
 using Pre = variant<LeadingZeros, Digit>;
 template <class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
@@ -41,17 +42,52 @@ template <class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
 
 int numerize(const Pre &pre) {
     return visit(overloaded{
-                     [](__attribute__((unused)) const LeadingZeros _) -> int {
+                     [](__attribute__((unused)) const LeadingZeros it) -> int {
                          return -2;
                      },
-                     [](const Digit it) -> int { return it.value; },
+                     [](const Digit it) -> int {
+                         return it.capped ? (it.value * 100) : it.value;
+                     },
                  },
                  pre);
 }
 
 ll sought_nums_up_to(const ll hi) {
     const auto ds = digits(hi);
-    return -1;
+
+    function<ll(int, Pre)> recur;
+    recur = [&](const int index, const Pre pre) -> ll {
+        if (index >= sz(ds)) return 1;
+
+        return visit(
+            overloaded{
+                [&](__attribute__((unused)) const LeadingZeros it) -> ll {
+                    ll ans = recur(index + 1, LeadingZeros{});
+                    for (int x = 1; x <= 9; ++x) {
+                        ans += recur(index, Digit{x, false});
+                    }
+                    return ans;
+                },
+                [&](const Digit it) -> ll {
+                    ll ans{};
+                    for (int x = 0; x <= (it.capped ? ds[index] : 9); ++x) {
+                        if (x == it.value) continue;
+                        ans += recur(index + 1,
+                                     Digit{x, it.capped && (x == ds[index])});
+                    }
+                    return ans;
+                },
+            },
+            pre);
+    };
+
+    ll ans = recur(1, LeadingZeros{});
+    for (int x = 1; x <= ds[0] - 1; ++x) {
+        ans += recur(1, Digit{ds[0], false});
+    }
+    ans += recur(1, Digit{ds[0], true});
+
+    return ans;
 }
 
 ll sought_nums_in_between(const ll a, const ll b) {
