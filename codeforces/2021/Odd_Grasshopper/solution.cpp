f1e41d862bbf7a78009b5cfdf07f2be03cb3f8df
Ivan Krechetov <ikr@ikr.li>
2021-11-02 20:11:49 +0100

    Utilize __int128_t and get it AC

--- codeforces/2021/Odd_Grasshopper/solution.cpp
+++ codeforces/2021/Odd_Grasshopper/solution.cpp
@@ -3,18 +3,20 @@ using namespace std;
 
 using ll = long long;
 
-ll end_coord(const ll x0, const ll n) {
+template <typename T> constexpr ll llof(const T x) { return static_cast<ll>(x); }
+
+ll end_coord(const __int128_t x0, const ll n) {
     const ll x0m2 = ((x0 % 2LL) + 2LL) % 2LL;
 
     if (n == 0) return x0;
     if (n == 1) return x0m2 ? (x0 + 1) : (x0 - 1);
 
-    const auto progr = [&](const ll a) -> ll {
+    const auto progr = [&](const __int128_t a) -> __int128_t {
         if (a > n) {
             return 0;
         }
 
-        ll k = (n - a) / 4LL;
+        __int128_t k = (n - a) / 4LL;
 
         if (k > 0LL) {
             return (k + 1) * ((2 * a + 4 * k) / 2LL);
@@ -24,7 +26,7 @@ ll end_coord(const ll x0, const ll n) {
     };
 
     if (x0m2 == 0) {
-        ll d = -1;
+        __int128_t d = -1;
         d += progr(2);
         d -= progr(4);
         d += progr(3);
@@ -33,7 +35,7 @@ ll end_coord(const ll x0, const ll n) {
         return x0 + d;
     }
 
-    ll d = 1;
+    __int128_t d = 1;
     d -= progr(2);
     d += progr(4);
     d -= progr(3);
