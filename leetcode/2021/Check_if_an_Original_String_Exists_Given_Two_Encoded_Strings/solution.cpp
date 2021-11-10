546eb6721efc3af899df0b3c6e4497fe4385d43b
Ivan Krechetov <ikr@ikr.su>
2021-11-10 13:54:24 +0100

    Very TLE on TC 124

--- leetcode/2021/Check_if_an_Original_String_Exists_Given_Two_Encoded_Strings/solution.cpp
+++ leetcode/2021/Check_if_an_Original_String_Exists_Given_Two_Encoded_Strings/solution.cpp
@@ -128,138 +128,63 @@ bool confirm_match(vi instance_a, vi instance_b) {
         assert(*it > 0);
         assert(*jt < 0);
         --(*it);
-        if (*it == 0) ++jt;
+        if (*it == 0) ++it;
         ++jt;
     }
 
     return it == end(instance_a) && jt == end(instance_b);
 }
 
-string number_prefix(const string &xs) {
-    string result;
-
-    for (auto it = cbegin(xs); it != cend(xs) && isdigit(*it); ++it) {
-        result += *it;
+template <typename T>
+vector<vector<T>> cartesian_product(const vector<vector<T>> &src) {
+    vector<vector<T>> result;
+    if (src.empty() || any_of(cbegin(src), cend(src),
+                              [](const auto &xs) { return xs.empty(); })) {
+        return result;
     }
 
-    assert(!result.empty());
-    assert(sz(result) <= 3);
+    result.push_back(vector<T>{});
+    for (const auto &xs : src) {
+        vector<vector<T>> next_gen;
+
+        for (const auto x : xs) {
+            for (auto curr : result) {
+                curr.push_back(x);
+                next_gen.push_back(curr);
+            }
+        }
+
+        result = next_gen;
+    }
+
     return result;
 }
 
-bool recur(map<pair<string, string>, bool> &memo, const string &a,
-           const string &b) {
-    const auto key = pair{a, b};
-    const auto it = memo.find(key);
-    if (it != cend(memo)) return it->second;
+multimap<int, vi> gather_instances_by_length(const string &xs) {
+    const auto sel = full_selector(xs);
+    multimap<int, vi> result;
 
-    const auto result = [&memo, &a, &b]() -> bool {
-        if (a.empty() && b.empty()) return true;
-        if (a.empty() != b.empty()) return false;
+    for (const vi &inst : cartesian_product(sel)) {
+        result.emplace(match_length(inst), inst);
+    }
 
-        if (!isdigit(a[0]) && !isdigit(b[0])) {
-            if (a[0] != b[0]) return false;
-            return recur(memo, a.substr(1), b.substr(1));
-        }
-
-        if (isdigit(a[0]) && isdigit(b[0])) {
-            const auto na = number_prefix(a);
-            const auto nb = number_prefix(b);
-
-            for (int i = 1; i <= sz(na); ++i) {
-                for (int j = 1; j <= sz(nb); ++j) {
-                    const int x = stoi(na.substr(0, i));
-                    const int y = stoi(nb.substr(0, j));
-
-                    const auto ra = na.substr(i);
-                    const auto rb = nb.substr(j);
-
-                    if (x == y) {
-                        if (recur(memo, ra + a.substr(sz(na)),
-                                  rb + b.substr(sz(nb)))) {
-                            return true;
-                        }
-                    } else if (x < y) {
-                        if (recur(memo, ra + a.substr(sz(na)),
-                                  to_string(y - x) + rb + b.substr(sz(nb)))) {
-                            return true;
-                        }
-                    } else {
-                        assert(x > y);
-                        if (recur(memo,
-                                  to_string(x - y) + ra + a.substr(sz(na)),
-                                  rb + b.substr(sz(nb)))) {
-                            return true;
-                        }
-                    }
-                }
-            }
-
-            return false;
-        }
-
-        if (!isdigit(b[0])) {
-            const auto na = number_prefix(a);
-            for (int i = 1; i <= sz(na); ++i) {
-                const int x = stoi(na.substr(0, i));
-                const auto ra = na.substr(i);
-
-                if (x == 0) {
-                    if (recur(memo, ra + a.substr(sz(na)), b)) {
-                        return true;
-                    }
-                } else if (x == 1) {
-                    if (recur(memo, ra + a.substr(sz(na)), b.substr(1))) {
-                        return true;
-                    }
-                } else {
-                    assert(x > 1);
-                    if (recur(memo, to_string(x - 1) + ra + a.substr(sz(na)),
-                              b.substr(1))) {
-                        return true;
-                    }
-                }
-            }
-
-            return false;
-        }
-
-        assert(!isdigit(a[0]));
-        assert(isdigit(b[0]));
-
-        const auto nb = number_prefix(b);
-        for (int j = 1; j <= sz(nb); ++j) {
-            const int y = stoi(nb.substr(0, j));
-            const auto rb = nb.substr(j);
-
-            if (y == 0) {
-                if (recur(memo, a, rb + b.substr(sz(nb)))) {
-                    return true;
-                }
-            } else if (y == 1) {
-                if (recur(memo, a.substr(1), rb + b.substr(sz(nb)))) {
-                    return true;
-                }
-            } else {
-                assert(y > 1);
-                if (recur(memo, a.substr(1),
-                          to_string(y - 1) + rb + b.substr(sz(nb)))) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }();
-
-    memo[key] = result;
     return result;
 }
 
 struct Solution final {
     bool possiblyEquals(const string &a, const string &b) const {
-        map<pair<string, string>, bool> memo;
-        return recur(memo, a, b);
+        const multimap<int, vi> a_insts_by_len = gather_instances_by_length(a);
+
+        for (auto inst_b : cartesian_product(full_selector(b))) {
+            const auto [first, last] =
+                a_insts_by_len.equal_range(match_length(inst_b));
+
+            for (auto it = first; it != last; ++it) {
+                if (confirm_match(it->second, inst_b)) return true;
+            }
+        }
+
+        return false;
     }
 };
 
@@ -325,6 +250,11 @@ const lest::test tests[] = {
         const auto expected = true;
         EXPECT(actual == expected);
     },
+    CASE("confirm_match positive C") {
+        const auto actual = confirm_match({1}, {-100});
+        const auto expected = true;
+        EXPECT(actual == expected);
+    },
     CASE("confirm_match negative A") {
         const auto actual = confirm_match({11}, {2});
         const auto expected = false;
@@ -376,6 +306,11 @@ const lest::test tests[] = {
         const auto expected = false;
         EXPECT(actual == expected);
     },
+    CASE("Test case 7") {
+        const auto actual = Solution{}.possiblyEquals("1", "z");
+        const auto expected = true;
+        EXPECT(actual == expected);
+    },
     CASE("Test case 28") {
         const auto actual = Solution{}.possiblyEquals("4p6p9p87q11", "p857q3q27");
         const auto expected = false;
@@ -396,6 +331,12 @@ const lest::test tests[] = {
     CASE("Test case 103") {
         const auto actual = Solution{}.possiblyEquals(
             "1q955g8g8q18q7g1g63q73q6g7g1g73", "43q968g34q731q98g778q2q6g48g78g");
+        const auto expected = false;
+        EXPECT(actual == expected);
+    },
+    CASE("Test case 124") {
+        const auto actual = Solution{}.possiblyEquals(
+            "v375v736v443v897v633v527v781v121v317", "475v899v753v784v438v415v431v216v968");
         const auto expected = false;
         EXPECT(actual == expected);
     },
