128b41fae1a1cb7a80a4caa5727c89698cf27ef4
Ivan Krechetov <ikr@ikr.li>
2021-12-01 06:22:16 +0100

    AC

--- adventofcode/2021/01_Sonar_Sweep/solution.cpp
+++ adventofcode/2021/01_Sonar_Sweep/solution.cpp
@@ -7,6 +7,20 @@ template <typename T> constexpr int inof(const T x) {
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
+vector<int> sliding_triple_sums(const vector<int> &xs) {
+    auto curr = accumulate(cbegin(xs), cbegin(xs) + 3, 0);
+    vector<int> ans{curr};
+    ans.reserve(sz(xs) - 2);
+
+    for (int i = 0; i + 3 < sz(xs); ++i) {
+        curr -= xs[i];
+        curr += xs[i + 3];
+        ans.push_back(curr);
+    }
+
+    return ans;
+}
+
 int incs_num(vector<int> xs) {
     adjacent_difference(cbegin(xs), cend(xs), begin(xs));
     return inof(count_if(next(cbegin(xs)), cend(xs),
@@ -20,6 +34,6 @@ int main() {
         xs.push_back(stoi(line));
     }
 
-    cout << incs_num(move(xs)) << '\n';
+    cout << incs_num(sliding_triple_sums(xs)) << '\n';
     return 0;
 }
