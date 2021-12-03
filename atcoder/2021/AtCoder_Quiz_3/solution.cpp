e93a36d2e6c3dd9884855a906c0672e577e43e4c
Ivan Krechetov <ikr@ikr.su>
2021-12-03 17:11:55 +0100

    Solve A, B, C

---
+++ atcoder/2021/AtCoder_Quiz_3/solution.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+template <typename T> constexpr ll llof(const T x) {
+    return static_cast<ll>(x);
+}
+template <typename T> constexpr double doof(const T x) {
+    return static_cast<double>(x);
+}
+
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+string pad(string x) {
+    while (sz(x) < 3) {
+        x = "0" + x;
+    }
+
+    return x;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+    cout << setprecision(9) << fixed;
+
+    int n;
+    cin >> n;
+
+    string ans = "AGC" + pad(to_string(n >= 42 ? n + 1 : n));
+    cout << ans << '\n';
+    return 0;
+}
