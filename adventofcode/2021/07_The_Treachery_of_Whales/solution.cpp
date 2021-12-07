3a83c53c03b10cf8f630e196387bcf6174aaf2f4
Ivan Krechetov <ikr@ikr.su>
2021-12-07 06:25:07 +0100

    Solve part 2

--- adventofcode/2021/07_The_Treachery_of_Whales/solution.cpp
+++ adventofcode/2021/07_The_Treachery_of_Whales/solution.cpp
@@ -1,24 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
 using vi = vector<int>;
-using vvi = vector<vi>;
-using pii = pair<int, int>;
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
+
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
 vector<string> split(const string &delim_regex, const string &s) {
     regex r(delim_regex);
     return vector<string>(sregex_token_iterator(cbegin(s), cend(s), r, -1),
                           sregex_token_iterator{});
-}
-
-template <typename T> constexpr T div_ceil(const T x, const T y) {
-    return x ? (1 + (x - 1) / y) : 0;
 }
 
 int main() {
@@ -31,12 +25,19 @@ int main() {
     transform(cbegin(src), cend(src), begin(xs),
               [](const auto &x) { return stoi(x); });
 
-    sort(begin(xs), end(xs));
-    const auto mi = div_ceil(sz(xs), 2);
+    const auto [lo, hi] = minmax_element(cbegin(xs), cend(xs));
 
-    int ans{};
-    for (int i = 0; i < sz(xs); ++i) {
-        ans += abs(xs[i] - xs[mi]);
+    int ans{INT_MAX};
+
+    for (int m = *lo; m <= *hi; ++m) {
+        int candidate{};
+
+        for (int i = 0; i < sz(xs); ++i) {
+            const int k = abs(xs[i] - m);
+            candidate += k * (k + 1) / 2;
+        }
+
+        ans = min(ans, candidate);
     }
 
     cout << ans << '\n';
