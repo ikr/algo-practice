f139e9bfd7dd8115e423f1fdfa58be735405cc39
Ivan Krechetov <ikr@ikr.li>
2021-12-15 07:21:27 +0100

    AC for part 2

--- adventofcode/2021/15_Chiton/solution.cpp
+++ adventofcode/2021/15_Chiton/solution.cpp
@@ -5,6 +5,7 @@ using ll = long long;
 using vi = vector<int>;
 using vvi = vector<vi>;
 using pii = pair<int, int>;
+using Tri = pair<int, pii>;
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
@@ -13,7 +14,7 @@ template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
 static constexpr int INF = 1e9;
 
-int min_total_risk(const vvi &grid) {
+int min_total_risk_dp(const vvi &grid) {
     const int H = sz(grid);
     const int W = sz(grid[0]);
 
@@ -35,6 +36,47 @@ int min_total_risk(const vvi &grid) {
     }
 
     return dp.back().back();
+}
+
+template <typename T>
+constexpr pair<T, T> operator+(const pair<T, T> a, const pair<T, T> b) {
+    return {a.first + b.first, a.second + b.second};
+}
+
+int min_total_risk_dijlstra(const vvi &grid) {
+    const int H = sz(grid);
+    const int W = sz(grid[0]);
+
+    const auto in_bounds = [&](const pii roco) -> bool {
+        const auto [ro, co] = roco;
+        return 0 <= ro && ro < H && 0 <= co && co < W;
+    };
+
+    vvi D(H, vi(W, INF));
+    D[0][0] = 0;
+
+    set<Tri> q;
+    q.emplace(0, pii{0, 0});
+
+    while (!q.empty()) {
+        const auto v = cbegin(q)->second;
+        q.erase(cbegin(q));
+
+        const auto [ro, co] = v;
+
+        for (const auto delta : vector<pii>{{-1, 0}, {0, 1}, {1, 0}, {0, -1}}) {
+            const auto [ro_, co_] = v + delta;
+            if (!in_bounds({ro_, co_})) continue;
+
+            if (D[ro][co] + grid[ro_][co_] < D[ro_][co_]) {
+                q.erase(Tri{D[ro_][co_], {ro_, co_}});
+                D[ro_][co_] = D[ro][co] + grid[ro_][co_];
+                q.emplace(D[ro_][co_], pii{ro_, co_});
+            }
+        }
+    }
+
+    return D.back().back();
 }
 
 template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
@@ -87,6 +129,8 @@ int main() {
         }
     }
 
-    cout << min_total_risk(mega_grid) << '\n';
+    // cerr << mega_grid << endl;
+    cout << min_total_risk_dp(mega_grid) << '\n';
+    cout << min_total_risk_dijlstra(mega_grid) << '\n';
     return 0;
 }
