60925658165f14f50603d54c541cf6bc7274373a
Ivan Krechetov <ikr@ikr.su>
2021-12-17 08:24:24 +0100

    Solve part 1

--- adventofcode/2021/17_Trick_Shot/solution.cpp
+++ adventofcode/2021/17_Trick_Shot/solution.cpp
@@ -3,6 +3,15 @@
 #include <numeric>
 using namespace std;
 
+template <typename T> ostream &operator<<(ostream &os, const optional<T> o) {
+    if (!o) {
+        os << "nullopt";
+    } else {
+        os << *o;
+    }
+    return os;
+}
+
 template <typename T1, typename T2>
 ostream &operator<<(ostream &os, const pair<T1, T2> &x) {
     os << '(' << x.first << ' ' << x.second << ')';
@@ -19,13 +28,12 @@ template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
     return os;
 }
 
+using ll = long long;
 using pii = pair<int, int>;
 
-template <typename T> struct mmin final {
-    constexpr T operator()(const T &a, const T &b) const {
-        return std::min(a, b);
-    }
-};
+template <typename T> constexpr ll llof(const T x) {
+    return static_cast<ll>(x);
+}
 
 static const string PREFIX{"target area: "};
 static const string X_EQ{"x="};
@@ -42,31 +50,35 @@ pii parse_range(const string &src) {
     return {stoi(parts[0]), stoi(parts[1])};
 }
 
-set<pii> blob_of(const pii rx, const pii ry) {
-    const auto [x_lo, x_hi] = rx;
-    const auto [y_lo, y_hi] = ry;
-
-    set<pii> result;
-    for (auto x = x_lo; x <= x_hi; ++x) {
-        for (auto y = y_lo; y <= y_hi; ++y) {
-            result.emplace(x, y);
-        }
+// Find a positive int solution n of the equation n(n + 1)/2 = d
+optional<int> find_n(const int d) {
+    for (int n = 1; n < d; ++n) {
+        if (n * (n + 1LL) / 2LL == llof(d)) return n;
     }
-    return result;
-}
-
-constexpr int manhattan(const pii a, const pii b) {
-    return abs(a.first - b.first) + abs(a.second - b.second);
-}
-
-int manhattan(const set<pii> &blob, const pii a) {
-    return transform_reduce(cbegin(blob), cend(blob), INT_MAX, mmin<int>{},
-                            [&](const pii b) { return manhattan(a, b); });
+    return nullopt;
 }
 
 int highest_y(const pii rx, const pii ry) {
-    const auto blob = blob_of(rx, ry);
-    return manhattan(blob, {0, 0});
+    const auto [x_lo, x_hi] = rx;
+    const auto [y_lo, y_hi] = ry;
+
+    int max_vy = -1;
+
+    for (auto x = x_lo; x <= x_hi; ++x) {
+        const auto vx = find_n(x);
+        if (!vx) continue;
+        const auto t_lo = *vx;
+
+        for (auto y = y_lo; y <= y_hi; ++y) {
+            for (auto t = t_lo; t < 1000; ++t) {
+                const auto y0 = y + t * (t + 1) / 2;
+                const auto vy = find_n(y0);
+                if (vy) max_vy = max(max_vy, *vy);
+            }
+        }
+    }
+
+    return max_vy * (max_vy + 1) / 2;
 }
 
 int main() {
