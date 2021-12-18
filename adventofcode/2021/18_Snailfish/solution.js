4a87d171af7f6ccaf91d48ef60955769f236ccb2
Ivan Krechetov <ikr@ikr.su>
2021-12-18 21:01:27 +0100

    Read input

---
+++ adventofcode/2021/18_Snailfish/solution.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs')
+const fullInput = fs.readFileSync(0, 'utf-8')
+
+const lines = fullInput
+      .split("\n")
+      .filter(x => x.length > 0)
+      .map(JSON.parse)
+
+console.dir(lines)
