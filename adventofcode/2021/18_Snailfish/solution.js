35a67c8112b5d7042ebb359659503a8b3c303e8c
Ivan Krechetov <ikr@ikr.su>
2021-12-19 18:06:29 +0100

    Find the exploding pair, and its left neighbor

--- adventofcode/2021/18_Snailfish/solution.js
+++ adventofcode/2021/18_Snailfish/solution.js
@@ -14,6 +14,20 @@ traverseInOrder(bufBox, lines[0])
 console.dir(bufBox)
 console.info(`Max level: ${maxLevel(0, lines[0])}`)
 
+const explodingBox = {value: null}
+findExploding(explodingBox, 0, lines[0])
+
+console.info('Exploding pair:')
+console.dir(explodingBox.value)
+
+
+const resultContanier = {pair: null, index: -1}
+assert(explodingBox.value !== null)
+const explodingSeenBox = {value: false}
+lnei(resultContanier, explodingBox.value, explodingSeenBox, lines[0])
+console.info('lnei container:')
+console.dir(resultContanier)
+
 function traverseInOrder(bufBox, n) {
     assert(typeof bufBox.value === 'string')
 
@@ -39,44 +53,54 @@ function maxLevel(level, n) {
     return Math.max(maxLevel(level + 1, n[0]), maxLevel(level + 1, n[1]))
 }
 
-function recurExplode(level, lneiArr, rneiAdditionBox, n) {
+function findExploding(resultBox, level, n) {
+    if (Number.isInteger(n) || resultBox.value !== null) return
     assert(Array.isArray(n))
     assert(n.length === 2)
 
-    if (level < 4) {
-        if (Array.isArray(n[0])) {
-            recurExplode(level + 1, lneiArr, rneiAdditionBox, n[0])
-        } else {
-            assert(Number.isInteger(n[0]))
-            lneiArr = n
-        }
-
-        if (Array.isArray(n[1])) {
-            recurExplode(level + 1, lneiArr, rneiAdditionBox, n[1])
-        } else {
-            assert(Number.isInteger(n[1]))
-            assert(Number.isInteger(rneiAdditionBox.value))
-
-            if (rneiAdditionBox.value > 0) {
-                n[1] += rneiAdditionBox.value
-                rneiAdditionBox.value = 0
-            }
-        }
-
+    if (level === 4) {
+        assert(Number.isInteger(n[0]))
+        assert(Number.isInteger(n[1]))
+        resultBox.value = n
         return
     }
 
-    assert(level === 4)
-    if (Array.isArray(n[0])) {
+    findExploding(resultBox, level + 1, n[0])
+    findExploding(resultBox, level + 1, n[1])
+}
 
+// ResultContainer is { pair: Array, index: 0 | 1 }
+function lnei(resultContanier, explodingArr, explodingSeenBox, n) {
+    assert(Array.isArray(n))
+    if (explodingSeenBox.value) return
+
+    if (n[0] === explodingArr) {
+        explodingSeenBox.value = true
+        return
+    }
+
+    if (Number.isInteger(n[0])) {
+        if (!explodingSeenBox.value) {
+            resultContanier.pair = n
+            resultContanier.index = 0
+        }
     } else {
-        assert(Number.isInteger(n[0]))
-     }
+        assert(Array.isArray(n[0]))
+        lnei(resultContanier, explodingArr, explodingSeenBox, n[0])
+    }
 
-    if (Array.isArray(n[1])) {
+    if (n[1] === explodingArr) {
+        explodingSeenBox.value = true
+        return
+    }
 
-    } else {
-        assert(Number.isInteger(n[1]))
-
+    if (Number.isInteger(n[1])) {
+        if (!explodingSeenBox.value) {
+            resultContanier.pair = n
+            resultContanier.index = 1
+        }
+      } else {
+        assert(Array.isArray(n[1]))
+        lnei(resultContanier, explodingArr, explodingSeenBox, n[1])
     }
 }
