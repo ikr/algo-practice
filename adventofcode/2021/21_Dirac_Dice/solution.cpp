d77f01c0907e0e7dd7c8246fa5893ed0dfbb18ca
Ivan Krechetov <ikr@ikr.li>
2021-12-29 10:38:09 +0100

    DP the outcomes

--- adventofcode/2021/21_Dirac_Dice/solution.cpp
+++ adventofcode/2021/21_Dirac_Dice/solution.cpp
@@ -1,30 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int roll_value(const int n) {
-    const auto cycle = [](const int x) -> int { return ((x - 1) % 100) + 1; };
-    const int base = 3 * (n - 1);
-    return cycle(base + 1) + cycle(base + 2) + cycle(base + 3);
+using ll = long long;
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
 }
 
-int simulate(array<int, 2> xs) {
-    const auto cycle = [](const int x) -> int { return ((x - 1) % 10) + 1; };
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-    array<int, 2> ss{};
-    int n{};
+template <typename T> constexpr ll llof(const T x) {
+    return static_cast<ll>(x);
+}
 
-    while (
-        all_of(cbegin(ss), cend(ss), [](const auto a) { return a < 1000; })) {
-        const auto rv = roll_value(++n);
-        const auto p = (n - 1) % 2;
-        const auto score = cycle(xs[p] + rv);
+constexpr int cycle(const int x) { return ((x - 1) % 10) + 1; };
 
-        ss[p] += score;
-        xs[p] = score;
+vector<vvi> outcomes(const int starting_space) {
+    // dp[i][j][k] is the number of ways for a player starting at the
+    // `starting_space` to obtain the score `i` after exactly `j` moves, ending
+    // up at the space `k`.
+    vector<vvi> dp(24, vvi(24, vi(11, 0)));
+    dp[0][0][starting_space] = 1;
+
+    for (int i = 0; i < 24; ++i) {
+        for (int j = 0; j < 24; ++j) {
+            for (int k = 1; k <= 10; ++k) {
+                for (int d = 1; d <= 3; ++d) {
+                    if (i + d >= sz(dp) || j + 1 >= sz(dp[0])) continue;
+                    dp[i + d][j + 1][cycle(k + d)] += dp[i][j][k];
+                }
+            }
+        }
     }
 
-    return n * 3 *
-           *find_if(cbegin(ss), cend(ss), [](const int a) { return a < 1000; });
+    return dp;
+}
+
+ll count_wins(const vector<vvi> &own_dp, const vector<vvi> &other_dp) {
+    ll ans{};
+    // TODO
+    return ans;
+}
+
+ll winning_universes_num(const array<int, 2> &xs) {
+    const auto dp0 = outcomes(xs[0]);
+    const auto dp1 = outcomes(xs[1]);
+    return max(count_wins(dp0, dp1), count_wins(dp1, dp0));
 }
 
 int main() {
@@ -37,6 +61,6 @@ int main() {
         xs[i] = stoi(line.substr(sep + 2));
     }
 
-    cout << simulate(xs) << '\n';
+    cout << winning_universes_num(xs) << '\n';
     return 0;
 }
