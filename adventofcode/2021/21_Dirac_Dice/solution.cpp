f6f2b6b1a45d6c1f557b848d20e648e7d4b75bd4
Ivan Krechetov <ikr@ikr.li>
2021-12-29 15:38:32 +0100

    Try count_wins

--- adventofcode/2021/21_Dirac_Dice/solution.cpp
+++ adventofcode/2021/21_Dirac_Dice/solution.cpp
@@ -12,25 +12,23 @@ template <typename T> constexpr int inof(const T x) {
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-template <typename T> constexpr ll llof(const T x) {
-    return static_cast<ll>(x);
-}
-
-constexpr int cycle(const int x) { return ((x - 1) % 10) + 1; };
+constexpr int cycle(const int x) { return ((x - 1) % 10) + 1; }
 
 vector<vvi> outcomes(const int starting_space) {
     // dp[i][j][k] is the number of ways for a player starting at the
     // `starting_space` to obtain the score `i` after exactly `j` moves, ending
     // up at the space `k`.
-    vector<vvi> dp(24, vvi(24, vi(11, 0)));
+    vector<vvi> dp(31, vvi(22, vi(11, 0)));
     dp[0][0][starting_space] = 1;
 
-    for (int i = 0; i < 24; ++i) {
-        for (int j = 0; j < 24; ++j) {
-            for (int k = 1; k <= 10; ++k) {
+    for (int i = 0; i < sz(dp); ++i) {
+        for (int j = 0; j < sz(dp[i]); ++j) {
+            for (int k = 1; k < sz(dp[i][j]); ++k) {
                 for (int d = 1; d <= 3; ++d) {
-                    if (i + d >= sz(dp) || j + 1 >= sz(dp[0])) continue;
-                    dp[i + d][j + 1][cycle(k + d)] += dp[i][j][k];
+                    if (i + cycle(k + d) >= sz(dp) || j + 1 >= sz(dp[0])) {
+                        continue;
+                    }
+                    dp[i + cycle(k + d)][j + 1][cycle(k + d)] += dp[i][j][k];
                 }
             }
         }
@@ -39,16 +37,37 @@ vector<vvi> outcomes(const int starting_space) {
     return dp;
 }
 
-ll count_wins(const vector<vvi> &own_dp, const vector<vvi> &other_dp) {
+ll count_wins(const vector<vvi> &own_dp, const vector<vvi> &other_dp,
+              const int other_moves_delta) {
     ll ans{};
-    // TODO
+
+    for (int score = 21; score < sz(own_dp); ++score) {
+        for (int moves = 3; moves < sz(own_dp[score]); ++moves) {
+            const int other_moves = moves + other_moves_delta;
+
+            ll own_outcomes{};
+            for (int space = 1; space <= 10; ++space) {
+                own_outcomes += own_dp[score][moves][space];
+            }
+
+            ll other_outcomes{};
+            for (int i = 0; i < 21; ++i) {
+                for (int k = 1; k < sz(other_dp[i][other_moves]); ++k) {
+                    other_outcomes += other_dp[i][other_moves][k];
+                }
+            }
+
+            ans += own_outcomes * other_outcomes;
+        }
+    }
+
     return ans;
 }
 
 ll winning_universes_num(const array<int, 2> &xs) {
     const auto dp0 = outcomes(xs[0]);
     const auto dp1 = outcomes(xs[1]);
-    return max(count_wins(dp0, dp1), count_wins(dp1, dp0));
+    return max(count_wins(dp0, dp1, -1), count_wins(dp1, dp0, 0));
 }
 
 int main() {
