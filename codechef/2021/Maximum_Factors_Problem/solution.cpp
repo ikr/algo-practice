91fa491b3e96be309338cfca1e8792bd5aed1fe4
Ivan Krechetov <ikr@ikr.li>
2021-12-30 17:43:21 +0100

    66% of WA, 33% AC

---
+++ codechef/2021/Maximum_Factors_Problem/solution.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+
+// Source:
+// https://github.com/kth-competitive-programming/kactl/blob/main/content/number-theory
+
+ull modmul(ull a, ull b, ull M) {
+    ll ret = a * b - M * ull(1.L / M * a * b);
+    return ret + M * (ret < 0) - M * (ret >= (ll)M);
+}
+
+ull modpow(ull b, ull e, ull mod) {
+    ull ans = 1;
+    for (; e; b = modmul(b, b, mod), e /= 2)
+        if (e & 1) ans = modmul(ans, b, mod);
+    return ans;
+}
+
+bool isPrime(ull n) {
+    if (n < 2 || n % 6 % 4 != 1) return (n | 1) == 3;
+    ull A[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022},
+        s = __builtin_ctzll(n - 1), d = n >> s;
+    for (ull a : A) { // ^ count trailing zeroes
+        ull p = modpow(a % n, d, n), i = s;
+        while (p != 1 && p != n - 1 && a % n && i--) p = modmul(p, p, n);
+        if (p != n - 1 && i != s) return 0;
+    }
+    return 1;
+}
+
+ull pollard(ull n) {
+    auto f = [n](ull x) { return modmul(x, x, n) + 1; };
+    ull x = 0, y = 0, t = 30, prd = 2, i = 1, q;
+    while (t++ % 40 || __gcd(prd, n) == 1) {
+        if (x == y) x = ++i, y = f(x);
+        if ((q = modmul(prd, max(x, y) - min(x, y), n))) prd = q;
+        x = f(x), y = f(f(y));
+    }
+    return __gcd(prd, n);
+}
+
+vector<ull> factor(ull n) {
+    if (n == 1) return {};
+    if (isPrime(n)) return {n};
+    ull x = pollard(n);
+    auto l = factor(x), r = factor(n / x);
+    l.insert(l.end(), begin(r), end(r));
+    return l;
+}
+
+//--------------------------------------------------------------------------------------------------
+
+map<ull, ull> gather_powers(const vector<ull> &fs) {
+    map<ull, ull> result;
+    for (const auto f : fs) {
+        ++result[f];
+    }
+    return result;
+}
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+static constexpr ull INF = 1e18;
+
+ull optimal_positive_divisor(const ull n) {
+    const auto fs = factor(n);
+    const auto ps = gather_powers(fs);
+
+    ull phi = 1;
+    for (const auto [p, k] : ps) {
+        phi *= (p - 1) * modpow(p, k - 1, INF);
+    }
+
+    ull ans = n;
+    ull divisors_of_m = 1;
+
+    for (const auto [p, k] : ps) {
+        assert(phi % (p - 1) == 0);
+        assert(k == 1 || phi % p == 0);
+
+        const auto candidate = (k == 1) ? (phi / (p - 1)) : (phi / p);
+
+        if (candidate >= divisors_of_m) {
+            divisors_of_m = candidate;
+            ans = p;
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << optimal_positive_divisor(n) << '\n';
+    }
+
+    return 0;
+}
