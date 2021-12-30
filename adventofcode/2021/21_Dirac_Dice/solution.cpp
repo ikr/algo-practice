7995354aa3327e7a2a6256a344547bdeccb65d13
Ivan Krechetov <ikr@ikr.li>
2021-12-30 12:24:36 +0100

    AC; misunderstood the statement earlier: there are 3 dice, not 1!

--- adventofcode/2021/21_Dirac_Dice/solution.cpp
+++ adventofcode/2021/21_Dirac_Dice/solution.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 using ll = long long;
-
 using vi = vector<int>;
 using vvi = vector<vi>;
 
@@ -14,18 +13,34 @@ template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
 constexpr int cycle(const int x) { return ((x - 1) % 10) + 1; }
 
+vi all_3_dice_rools() {
+    vi result;
+
+    for (int i = 1; i <= 3; ++i) {
+        for (int j = 1; j <= 3; ++j) {
+            for (int k = 1; k <= 3; ++k) {
+                result.push_back(i + j + k);
+            }
+        }
+    }
+
+    sort(begin(result), end(result));
+    return result;
+}
+
+static const auto RS = all_3_dice_rools();
+
 vector<vvi> outcomes(const int starting_space) {
     // dp[i][j][k] is the number of ways for a player starting at the
     // `starting_space` to obtain the score `i` after exactly `j` moves, ending
     // up at the space `k`.
-    vector<vvi> dp(31, vvi(22, vi(11, 0)));
+    vector<vvi> dp(31, vvi(11, vi(11, 0)));
     dp[0][0][starting_space] = 1;
 
     for (int i = 0; i < 21; ++i) {
-        for (int j = 0; j < sz(dp[i]); ++j) {
+        for (int j = 0; j < sz(dp[i]) - 1; ++j) {
             for (int k = 1; k <= 10; ++k) {
-                for (int d = 1; d <= 3; ++d) {
-                    if (j + 1 >= sz(dp[0])) continue;
+                for (const auto d : RS) {
                     dp[i + cycle(k + d)][j + 1][cycle(k + d)] += dp[i][j][k];
                 }
             }
@@ -40,7 +55,7 @@ ll count_wins(const vector<vvi> &own_dp, const vector<vvi> &other_dp,
     ll ans{};
 
     for (int score = 21; score < sz(own_dp); ++score) {
-        for (int moves = 3; moves < sz(own_dp[score]); ++moves) {
+        for (int moves = 1; moves < sz(own_dp[score]); ++moves) {
             const int other_moves = moves + other_moves_delta;
 
             ll own_outcomes{};
