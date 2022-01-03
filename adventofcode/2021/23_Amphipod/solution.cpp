b90248136615369d0b9400040e28ce41b54837e0
Ivan Krechetov <ikr@ikr.li>
2022-01-03 21:04:25 +0100

    Start implementing the adjacent states generation

--- adventofcode/2021/23_Amphipod/solution.cpp
+++ adventofcode/2021/23_Amphipod/solution.cpp
@@ -4,6 +4,11 @@ using namespace std;
 using Rooms = array<array<char, 2>, 4>;
 using Hallway = array<char, 11>;
 
+struct State final {
+    Rooms rs;
+    Hallway hw;
+};
+
 template <typename T, size_t N>
 ostream &operator<<(ostream &os, const array<T, N> &xs) {
     os << '[';
@@ -25,7 +30,33 @@ template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
 
-template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+int step_energy(const char x) {
+    const array<int, 4> es{1, 10, 100, 1000};
+    return es[inof(x) - inof('A')];
+}
+
+constexpr bool is_open_space(const char x) { return x == '.'; }
+
+bool can_move_in_hallway(const Hallway &hw, const int lo, const int hi) {
+    return all_of(cbegin(hw) + lo, cbegin(hw) + hi + 1, is_open_space);
+}
+
+vector<pair<State, int>> adjacent_for_room_0(const State &st) {
+    vector<pair<State, int>> result;
+    if (is_open_space(st.rs[0][0]) && is_open_space(st.rs[0][1])) return result;
+    const auto x = is_open_space(st.rs[0][0]) ? st.rs[0][1] : st.rs[0][0];
+    const auto takeoff = is_open_space(st.rs[0][0]) ? 2 : 1;
+
+    // TODO
+
+    return result;
+}
+
+vector<pair<State, int>> adjacent(const State &st) {
+    vector<pair<State, int>> result;
+    // TODO
+    return result;
+}
 
 int main() {
     vector<string> grid;
