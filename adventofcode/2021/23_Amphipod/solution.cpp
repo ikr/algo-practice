0993208527851306d488c960b3ad3117e60445ea
Ivan Krechetov <ikr@ikr.su>
2022-01-04 08:03:50 +0100

    Implement `adjacent_for_room_k`

--- adventofcode/2021/23_Amphipod/solution.cpp
+++ adventofcode/2021/23_Amphipod/solution.cpp
@@ -1,13 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using Rooms = array<array<char, 2>, 4>;
+using Room = array<char, 2>;
+using Rooms = array<Room, 4>;
 using Hallway = array<char, 11>;
 
 struct State final {
     Rooms rs;
     Hallway hw;
 };
+
+static constexpr char SPC = '.';
 
 template <typename T, size_t N>
 ostream &operator<<(ostream &os, const array<T, N> &xs) {
@@ -30,24 +33,69 @@ template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
 
+constexpr char chof(const int x) { return static_cast<char>(x); }
+
+Hallway empty_hallway() {
+    Hallway result;
+    result.fill(SPC);
+    return result;
+}
+
 int step_energy(const char x) {
     const array<int, 4> es{1, 10, 100, 1000};
     return es[inof(x) - inof('A')];
 }
 
-constexpr bool is_open_space(const char x) { return x == '.'; }
-
-bool can_move_in_hallway(const Hallway &hw, const int lo, const int hi) {
-    return all_of(cbegin(hw) + lo, cbegin(hw) + hi + 1, is_open_space);
+function<bool(char)> is_a(const char a) {
+    return [a](const char x) { return x == a; };
 }
 
-vector<pair<State, int>> adjacent_for_room_0(const State &st) {
-    vector<pair<State, int>> result;
-    if (is_open_space(st.rs[0][0]) && is_open_space(st.rs[0][1])) return result;
-    const auto x = is_open_space(st.rs[0][0]) ? st.rs[0][1] : st.rs[0][0];
-    const auto takeoff = is_open_space(st.rs[0][0]) ? 2 : 1;
+bool can_move_in_hallway(const Hallway &hw, const int lo, const int hi) {
+    return all_of(cbegin(hw) + lo, cbegin(hw) + hi + 1, is_a(SPC));
+}
 
-    // TODO
+vector<pair<State, int>>
+adjacent_for_room_k(const State &st, const int k,
+                    const vector<int> &hallway_destinations) {
+    const char tenant{chof(inof('A') + k)};
+
+    vector<pair<State, int>> result;
+    if (all_of(cbegin(st.rs[k]), cend(st.rs[k]), is_a(SPC))) return result;
+    if (all_of(cbegin(st.rs[k]), cend(st.rs[k]), is_a(tenant))) return result;
+    if (st.rs[k][0] == SPC && st.rs[k][1] == tenant) return result;
+
+    const auto [a, takeoff, room] = [&]() -> tuple<char, int, Room> {
+        if (st.rs[k][0] == SPC) {
+            const auto x = st.rs[k][1];
+            assert(x != SPC);
+            assert(x != tenant);
+
+            Room r = st.rs[k];
+            r[1] = '.';
+            return {x, 2, r};
+        }
+
+        Room r = st.rs[k];
+        r[0] = '.';
+        return {st.rs[k][0], 1, r};
+    }();
+
+    const auto rooms = [&](const Room &r) -> Rooms {
+        Rooms rs = st.rs;
+        rs[k] = r;
+        return rs;
+    }(room);
+
+    const auto exit_i = 2 + k * 2;
+    for (const auto i : hallway_destinations) {
+        if (can_move_in_hallway(st.hw, min(i, exit_i), max(i, exit_i))) {
+            Hallway hw = st.hw;
+            hw[i] = a;
+            const auto steps = takeoff + abs(i - exit_i);
+
+            result.emplace_back(State{rooms, hw}, steps * step_energy(a));
+        }
+    }
 
     return result;
 }
