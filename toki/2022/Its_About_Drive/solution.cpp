9644fc6e70b6e21d3e2d7e684b5dd3c5d5e726b3
Ivan Krechetov <ikr@ikr.su>
2022-01-09 15:50:23 +0100

    TLE, of course

--- toki/2022/Its_About_Drive/solution.cpp
+++ toki/2022/Its_About_Drive/solution.cpp
@@ -2,10 +2,31 @@
 using namespace std;
 
 using ll = long long;
+using tri = tuple<ll, ll, ll>;
 
 bool is_possible(const ll N, const ll F, const ll D) {
-    if (F > N) return false;
-    return (2 * F * F) <= (2 * D + N * N - N);
+    map<tri, bool> memo;
+
+    function<bool(ll, ll, ll)> recur;
+    recur = [&](const ll n, const ll f, const ll d) -> bool {
+        const tri key{n, f, d};
+
+        {
+            const auto it = memo.find(key);
+            if (it != cend(memo)) return it->second;
+        }
+
+        return memo[key] = [&]() -> bool {
+            if (f > n || f < 0 || d < 0) return false;
+            if (n == 0 && f == 0 && d == 0) return true;
+            if (n == 0) return false;
+
+            return recur(n - 1, f - 1, d - f) || recur(n - 1, f, d - f) ||
+                   recur(n - 1, f + 1, d - f);
+        }();
+    };
+
+    return recur(N, F, D);
 }
 
 int main() {
