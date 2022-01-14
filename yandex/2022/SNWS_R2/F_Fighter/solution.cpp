219e582d9a1388d6ec03aa78213e2a6ea5ff2476
Ivan Krechetov <ikr@ikr.li>
2022-01-14 18:34:09 +0100

    WA

---
+++ yandex/2022/SNWS_R2/F_Fighter/solution.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<bool> trained(m, false);
+    int ans{};
+
+    for (int i = 0; i < n; ++i) {
+        char kind;
+        cin >> kind;
+        assert(kind == '-' || kind == '+');
+
+        string xs(m, ' ');
+        cin >> xs;
+        assert(sz(xs) == m);
+
+        for (int j = 0; j < m; ++j) {
+            const auto yes = xs[j] == '1';
+
+            if (kind == '+') { // self
+                trained[j] = trained[j] || yes;
+            } else { // instructed
+                if (!yes) continue;
+                if (!trained[j]) {
+                    ++ans;
+                }
+                trained[j] = false;
+            }
+        }
+    }
+
+    cout << ans << '\n';
+    return 0;
+}
