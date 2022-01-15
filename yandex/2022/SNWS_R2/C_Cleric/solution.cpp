197fa28ded45bb9ed250cb72a97f18dfdc57c63a
Ivan Krechetov <ikr@ikr.li>
2022-01-15 11:06:52 +0100

    WA

--- yandex/2022/SNWS_R2/C_Cleric/solution.cpp
+++ yandex/2022/SNWS_R2/C_Cleric/solution.cpp
@@ -8,7 +8,41 @@ template <typename T> constexpr int inof(const T x) {
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
 int min_fights(const int n, const double p, const map<int, int> &sfs) {
-    array<int, 400> outcomes{};
+    vector<int> outcomes(n + 2, 0);
+    int ans = 1;
+
+    for (int i = 1; i <= 6; ++i) {
+        if (i < sz(outcomes)) {
+            ++outcomes[i];
+        } else {
+            ++outcomes.back();
+        }
+    }
+
+    for (;; ++ans) {
+        const double hi = outcomes[n] + outcomes[n + 1];
+        const double lo = accumulate(cbegin(outcomes) + 1, cbegin(outcomes) + n,
+                                     0.0, plus<double>{});
+        if (hi / lo >= p) return ans;
+
+        auto outcomes_ = outcomes;
+
+        for (int k = 1; k <= n; ++k) {
+            if (!outcomes[k]) continue;
+
+            for (int i = 1; i <= 6; ++i) {
+                const auto newval_src =
+                    k + i < sz(outcomes) ? (k + i) : (n + 1);
+                const auto newval =
+                    sfs.count(newval_src) ? sfs.at(newval_src) : newval_src;
+                ++outcomes_[newval];
+            }
+        }
+
+        swap(outcomes, outcomes_);
+    }
+
+    return ans;
 }
 
 int main() {
