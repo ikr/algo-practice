6a6cb4f829d0ffc484bb1e40bc9dc838d6ee7d27
Ivan Krechetov <ikr@ikr.su>
2022-01-16 10:57:29 +0100

    Some progress with a new approach: WA on test 4

--- yandex/2022/SNWS_R2/C_Cleric/solution.cpp
+++ yandex/2022/SNWS_R2/C_Cleric/solution.cpp
@@ -12,28 +12,32 @@ template <typename T> constexpr int inof(const T x) {
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
 int min_fights(const int n, const double p, const map<int, int> &sfs) {
-    vector<int> outcomes(n + 2, 0);
-    for (int i = 1; i <= 6; ++i) ++outcomes[min(i, n + 1)];
+    vector<int> outcomes(n + 1, 0);
+    assert(n >= 6);
+    for (int i = 1; i <= 6; ++i) ++outcomes[i];
 
-    for (int ans = 1;; ++ans) {
-        const auto hi = outcomes[n] + outcomes[n + 1];
-        const auto lo =
-            accumulate(cbegin(outcomes) + 1, cbegin(outcomes) + n, 0);
+    for (auto [ans, tail] = pair{1, 0LL};; ++ans) {
+        const auto hi = outcomes[n] + tail;
+        const auto lo = accumulate(cbegin(outcomes), cbegin(outcomes) + n, 0);
         if (doof(hi) / doof(lo) >= p) return ans;
 
+        tail *= 6;
+        long long addition_to_tail{};
         auto outcomes_ = outcomes;
 
-        for (int k = 1; k <= n + 1; ++k) {
+        for (int k = 1; k <= n; ++k) {
             if (!outcomes[k]) continue;
 
             for (int i = 1; i <= 6; ++i) {
-                const auto newval_src = min(k + i, n + 1);
-                const auto newval =
-                    sfs.count(newval_src) ? sfs.at(newval_src) : newval_src;
-                ++outcomes_[newval];
+                if (k + i <= n) {
+                    ++outcomes_[sfs.count(k + i) ? sfs.at(k + i) : (k + i)];
+                } else {
+                    ++addition_to_tail;
+                }
             }
         }
 
+        tail += addition_to_tail;
         swap(outcomes, outcomes_);
     }
 
