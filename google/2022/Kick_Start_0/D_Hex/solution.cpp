b73a29e8334ec7b3c9a8e7bcc6cf18a4e110ffdb
Ivan Krechetov <ikr@ikr.su>
2022-02-16 17:49:33 +0100

    OMW

--- google/2022/Kick_Start_0/D_Hex/solution.cpp
+++ google/2022/Kick_Start_0/D_Hex/solution.cpp
@@ -1,12 +1,22 @@
+#include <cassert>
+#include <functional>
 #include <iostream>
+#include <utility>
 #include <vector>
 using namespace std;
+
+using RoCo = pair<int, int>;
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+template <typename T>
+constexpr pair<T, T> operator+(const pair<T, T> a, const pair<T, T> b) {
+    return {a.first + b.first, a.second + b.second};
+}
 
 enum class Outcome { IMPOSSIBLE, BLUE_WINS, RED_WINS, NEUTRAL };
 
@@ -24,7 +34,112 @@ string outcome_literal(const Outcome x) {
     }
 }
 
-Outcome solve(const vector<string> &grid) { return Outcome::NEUTRAL; }
+constexpr bool in_bounds(const int N, const RoCo roco) {
+    const auto [ro, co] = roco;
+    return 0 <= ro && ro < N && 0 <= co && co < N;
+}
+
+static const vector<RoCo> HEXAGON_DELTAS{{-1, 0}, {1, -1}, {0, 1},
+                                         {1, 0},  {1, -1}, {0, -1}};
+
+vector<RoCo> adjacent_hexagons_of_same_color(const vector<string> &grid,
+                                             const RoCo roco) {
+    const auto [ro, co] = roco;
+    const auto color = grid[ro][co];
+    assert(color == 'B' || color == 'R');
+    vector<RoCo> result;
+
+    for (const auto d : HEXAGON_DELTAS) {
+        const auto roco_ = roco + d;
+        if (!in_bounds(sz(grid), roco_)) continue;
+
+        const auto [ro_, co_] = roco_;
+        if (grid[ro_][co_] != color) continue;
+
+        result.emplace_back(ro_, co_);
+    }
+
+    return result;
+}
+
+bool confirm_connection(const vector<string> &grid, const RoCo source,
+                        const function<bool(RoCo)> is_destination,
+                        vector<vector<bool>> &visited) {
+    assert(grid[source.first][source.second] != '.');
+
+    const auto dfs = [&](const auto &self, const RoCo u) -> bool {
+        const auto [ro, co] = u;
+        visited[ro][co] = true;
+        if (is_destination(u)) return true;
+
+        for (const auto v : adjacent_hexagons_of_same_color(grid, u)) {
+            const auto [ro_, co_] = v;
+            if (visited[ro_][co_]) continue;
+            if (self(self, v)) return true;
+        }
+
+        return false;
+    };
+
+    return dfs(dfs, source);
+}
+
+bool confirm_connection(const vector<string> &grid, const vector<RoCo> sources,
+                        const function<bool(RoCo)> is_destination) {
+    vector<vector<bool>> visited(sz(grid), vector(sz(grid[0]), false));
+
+    for (const auto source : sources) {
+        if (confirm_connection(grid, source, is_destination, visited)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+vector<RoCo> blue_sources(const vector<string> &grid) {
+    vector<RoCo> result;
+    for (int ro = 0; ro < sz(grid); ++ro) {
+        if (grid[ro][0] == 'B') {
+            result.emplace_back(ro, 0);
+        }
+    }
+    return result;
+}
+
+vector<RoCo> red_sources(const vector<string> &grid) {
+    vector<RoCo> result;
+    for (int co = 0; co < sz(grid[0]); ++co) {
+        if (grid[0][co] == 'R') {
+            result.emplace_back(0, co);
+        }
+    }
+    return result;
+}
+
+function<bool(RoCo)> make_is_blue_destiation(const int N) {
+    return [N](const RoCo roco) -> bool {
+        const auto [_, co] = roco;
+        return co == N - 1;
+    };
+}
+
+function<bool(RoCo)> make_is_red_destiation(const int N) {
+    return [N](const RoCo roco) -> bool {
+        const auto [ro, _] = roco;
+        return ro == N - 1;
+    };
+}
+
+Outcome solve(const vector<string> &grid) {
+    const auto blue_connected = confirm_connection(
+        grid, blue_sources(grid), make_is_blue_destiation(sz(grid)));
+    const auto red_connected = confirm_connection(
+        grid, red_sources(grid), make_is_red_destiation(sz(grid)));
+
+    cerr << "BC:" << blue_connected << " RC:" << red_connected << endl;
+    return Outcome::NEUTRAL;
+}
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
