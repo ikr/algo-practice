0c0ab655321caf2bb54a4390019c4a1e755f6eee
Ivan Krechetov <ikr@ikr.su>
2022-02-17 20:20:57 +0100

    Get a weird shot at Test Set 2

--- google/2022/Kick_Start_0/E_Milk_Tea/solution.cpp
+++ google/2022/Kick_Start_0/E_Milk_Tea/solution.cpp
@@ -1,12 +1,10 @@
-#include <bitset>
+#include <algorithm>
 #include <cassert>
-#include <climits>
 #include <iostream>
+#include <numeric>
 #include <set>
 #include <vector>
 using namespace std;
-
-static constexpr int BITS_NAX = 10;
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
@@ -14,34 +12,86 @@ template <typename T> constexpr int inof(const T x) {
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-set<int> to_set_of_bits(const vector<string> &xss) {
-    set<int> result;
+vector<int> freqs_of_ones(const vector<string> &xss) {
+    vector<int> result(sz(xss[0]), 0);
     for (const auto &xs : xss) {
-        bitset<BITS_NAX> bits(xs);
-        result.insert(inof(bits.to_ulong()));
+        assert(sz(xs) == sz(result));
+
+        for (int i = 0; i < sz(xs); ++i) {
+            if (xs[i] == '1') ++result[i];
+        }
     }
     return result;
 }
 
-int min_complaints(const vector<string> &preferences,
-                   const vector<string> &forbidden) {
-    const auto P = sz(preferences[0]);
-    const auto fs = to_set_of_bits(forbidden);
-    int result = INT_MAX;
+string gather_proto_result(const int N, const vector<int> &fq1) {
+    const auto P = sz(fq1);
+    string proto_result(P, '0');
 
-    for (int x = 0; x < (1 << P); ++x) {
-        if (fs.count(x)) continue;
-
-        int curr{};
-        for (const auto &p : preferences) {
-            bitset<BITS_NAX> bits(p);
-            curr += __builtin_popcount(inof(bits.to_ulong()) ^ x);
+    for (int i = 0; i < P; ++i) {
+        if (fq1[i] > N - fq1[i]) {
+            proto_result[i] = '1';
         }
+    }
 
-        result = min(result, curr);
+    return proto_result;
+}
+
+int diffs_num(const string &xs, const string &ys) {
+    assert(sz(xs) == sz(ys));
+    int result{};
+
+    for (int i = 0; i < sz(xs); ++i) {
+        if (xs[i] != ys[i]) ++result;
     }
 
     return result;
+}
+
+int diffs_num(const vector<string> &xss, const string &ys) {
+    int result{};
+    for (const auto &xs : xss) {
+        result += diffs_num(xs, ys);
+    }
+    return result;
+}
+
+void flip_at(string &xs, const int i) { xs[i] = xs[i] == '1' ? '0' : '1'; }
+
+int min_complaints(const vector<string> &preferences,
+                   const set<string> &forbidden) {
+    const auto N = sz(preferences);
+    const auto P = sz(preferences[0]);
+    const auto fq1 = freqs_of_ones(preferences);
+
+    vector<int> idx_by_flip_cost(P);
+    iota(begin(idx_by_flip_cost), end(idx_by_flip_cost), 0);
+
+    const auto flip_cost = [&](const int i) -> int {
+        return abs(N - 2 * fq1[i]);
+    };
+
+    sort(begin(idx_by_flip_cost), end(idx_by_flip_cost),
+         [&](const int i, const int j) -> bool {
+             return flip_cost(i) < flip_cost(j);
+         });
+
+    const auto proto_result = gather_proto_result(N, fq1);
+    cerr << "PR: " << proto_result << endl;
+    if (!forbidden.count(proto_result)) {
+        return diffs_num(preferences, proto_result);
+    }
+
+    for (int flips = 1;; ++flips) {
+        string result = proto_result;
+
+        for (int i = 0; i < flips; ++i) {
+            flip_at(result, idx_by_flip_cost[i]);
+        }
+
+        cerr << "R: " << result << endl;
+        if (!forbidden.count(result)) return diffs_num(preferences, result);
+    }
 }
 
 int main() {
@@ -53,7 +103,6 @@ int main() {
     for (int i = 1; i <= t; ++i) {
         int N, M, P;
         cin >> N >> M >> P;
-        assert(P <= BITS_NAX);
 
         vector<string> preferences(N);
         for (auto &p : preferences) {
@@ -61,10 +110,12 @@ int main() {
             assert(sz(p) == P);
         }
 
-        vector<string> forbidden(M);
-        for (auto &f : forbidden) {
+        set<string> forbidden;
+        for (int j = 1; j <= M; ++j) {
+            string f;
             cin >> f;
             assert(sz(f) == P);
+            forbidden.insert(f);
         }
 
         cout << "Case #" << i << ": " << min_complaints(preferences, forbidden)
