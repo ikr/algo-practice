7b7d99ce933b7888042f08af4f1eb6c2a890f262
Ivan Krechetov <ikr@ikr.su>
2022-03-25 17:14:05 +0100

    A — AC; B — WA

---
+++ prepbytes/Close_Numbers/solution.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+template <typename T> constexpr typename T::const_iterator xbegin(const T &xs) {
+    return xs.cbegin();
+}
+
+template <typename T> constexpr typename T::const_iterator xend(const T &xs) {
+    return xs.cend();
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        const int a = n / 2;
+        const int b = n - a;
+        cout << a << ' ' << b << '\n';
+    }
+
+    return 0;
+}
