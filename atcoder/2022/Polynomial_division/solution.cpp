3fb2d986bfa8b466a2bb1a569daff74d8a9b4fe9
Ivan Krechetov <ikr@ikr.su>
2022-03-27 08:55:37 +0200

    AC

--- atcoder/2022/Polynomial_division/solution.cpp
+++ atcoder/2022/Polynomial_division/solution.cpp
@@ -17,56 +17,30 @@ template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
     return os;
 }
 
+template <typename T> constexpr T safe_get(const vector<T> &xs, const int i) {
+    return (0 <= i && i < sz(xs)) ? xs[i] : 0;
+}
+
 vector<int> find_B(const vector<int> &A, const vector<int> &C) {
     const int N = sz(A) - 1;
     const int M = sz(C) - 1 - N;
     vector<int> B(M + 1, 0);
 
-    assert(C[0] % A[0] == 0);
-    B[0] = C[0] / A[0];
-
-    for (int i = 1; i <= M; ++i) {
+    for (int d = 0; d <= M; ++d) {
         const auto r = [&]() -> int {
             int result{};
 
-            for (int j = 1; j <= i; ++j) {
-                result += (j < sz(A) ? A[j] : 0) * B[i - j];
-            }
-
-            return result;
-        }();
-
-        assert((C[i] - r) % A[0] == 0);
-        B[i] = (C[i] - r) / A[0];
-    }
-
-    return B;
-}
-
-vector<int> find_B_(const vector<int> &A, const vector<int> &C) {
-    const int N = sz(A) - 1;
-    const int M = sz(C) - 1 - N;
-    vector<int> B(M + 1, 0);
-
-    assert(C[N + M] % A[N] == 0);
-    B[M] = C[N + M] / A[N];
-
-    for (int i = M - 1; i >= 0; --i) {
-        const int d = M - i;
-
-        const auto r = [&]() -> int {
-            int result{};
-
-            for (int j = 1; j <= d; ++j) {
-                result += (((N - d) < sz(A) && (N - d >= 0)) ? A[N - d] : 0) *
-                          B[M - d];
+            for (int q = 1; q <= d; ++q) {
+                const int ia = N - q;
+                const int ib = (N + M - d) - ia;
+                result += safe_get(A, ia) * safe_get(B, ib);
             }
 
             return result;
         }();
 
         assert((C[N + M - d] - r) % A[N] == 0);
-        B[i] = (C[N + M - d] - r) / A[N];
+        B[M - d] = (C[N + M - d] - r) / A[N];
     }
 
     return B;
@@ -85,6 +59,6 @@ int main() {
     vector<int> C(N + M + 1, 0);
     for (auto &c : C) cin >> c;
 
-    cout << (!A[0] ? find_B(A, C) : find_B_(A, C)) << '\n';
+    cout << find_B(A, C) << '\n';
     return 0;
 }
