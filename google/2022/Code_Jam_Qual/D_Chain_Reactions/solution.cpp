8ebe4d9e75b81463f2c1ecfb28c1496b8ec9271f
Ivan Krechetov <ikr@ikr.li>
2022-04-07 07:36:33 +0200

    Rethink

--- google/2022/Code_Jam_Qual/D_Chain_Reactions/solution.cpp
+++ google/2022/Code_Jam_Qual/D_Chain_Reactions/solution.cpp
@@ -1,8 +1,15 @@
 #include <algorithm>
 #include <cassert>
 #include <iostream>
+#include <numeric>
 #include <vector>
 using namespace std;
+
+using ll = long long;
+
+template <typename T> constexpr ll llof(const T x) {
+    return static_cast<ll>(x);
+}
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
@@ -10,49 +17,47 @@ template <typename T> constexpr int inof(const T x) {
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-vector<vector<int>> children_sorted(const vector<int> &F,
-                                    const vector<int> &P) {
+vector<vector<int>> children(const vector<int> &P) {
     vector<vector<int>> result(sz(P));
     for (int u = 0; u < sz(P); ++u) {
         if (P[u] >= 0) result[P[u]].push_back(u);
     }
-    for (auto &ch : result) {
-        sort(begin(ch), end(ch),
-             [&](const int u, const int v) { return F[u] < F[v]; });
-    }
     return result;
 }
 
-int max_fun(const vector<int> &F, const vector<int> &P) {
-    const auto ch = children_sorted(F, P);
-    vector<int> ordering;
-    vector<bool> visited(sz(P), false);
-    const auto dfs = [&](const auto self, const int u) -> void {
-        visited[u] = true;
+ll max_fun(const vector<int> &F, const vector<int> &P) {
+    const auto ch = children(P);
+    vector<ll> fun(sz(P), 0);
+
+    const auto recur = [&](const auto self, const int u) -> void {
         if (ch[u].empty()) {
-            ordering.push_back(u);
+            fun[u] = F[u];
         } else {
             for (const auto v : ch[u]) self(self, v);
+
+            const auto it = min_element(
+                cbegin(ch[u]), cend(ch[u]),
+                [&](const int a, const int b) { return F[a] < F[b]; });
+            assert(it != cend(ch[u]));
+
+            fun[u] = max(fun[*it], llof(F[u])) +
+                     accumulate(cbegin(ch[u]), it, 0LL,
+                                [&](const ll agg, const int i) {
+                                    return agg + fun[i];
+                                }) +
+                     accumulate(next(it), cend(ch[u]), 0LL,
+                                [&](const ll agg, const int i) {
+                                    return agg + fun[i];
+                                });
         }
     };
-    for (int u = 0; u < sz(P); ++u) {
-        if (!visited[u]) dfs(dfs, u);
-    }
 
-    int result{};
-    vector<bool> used(sz(P), false);
-    for (const auto u : ordering) {
-        assert(!used[u]);
-        int hi = F[u];
-        auto v = u;
-
-        while (v >= 0 && !used[v]) {
-            used[v] = true;
-            hi = max(hi, F[v]);
-            v = P[v];
+    ll result{};
+    for (int i = 0; i < sz(P); ++i) {
+        if (P[i] < 0) {
+            recur(recur, i);
+            result += fun[i];
         }
-
-        result += hi;
     }
     return result;
 }
