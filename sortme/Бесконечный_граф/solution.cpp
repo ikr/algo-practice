77bee42f5bb56c392232657b81c41b3943d99c56
Ivan Krechetov <ikr@ikr.su>
2022-04-07 18:05:12 +0200

    WA

--- sortme/Бесконечный_граф/solution.cpp
+++ sortme/Бесконечный_граф/solution.cpp
@@ -2,6 +2,12 @@
 #include <iostream>
 using namespace std;
 
+using ll = long long;
+
+template <typename T> constexpr ll llof(const T x) {
+    return static_cast<ll>(x);
+}
+
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
@@ -10,13 +16,19 @@ template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
 int min_distance(const int a, const int b) {
     if (a == b) return 0;
-    if (a % b == 0) return a / b;
-    if (b % a == 0) return b / a;
+    if (a == 1) return b;
 
     const auto g = __gcd(a, b);
     if (g == 1) return a + b;
 
-    return min(a / g + min_distance(a / g, b), b / g + min_distance(a, b / g));
+    if (g == a) {
+        if ((b / g) % a == 0) {
+            return g + min_distance(min(a, b / g), max(a, b / g));
+        }
+        return b / a;
+    }
+
+    return a / g + b / g;
 }
 
 int main() {
@@ -28,7 +40,7 @@ int main() {
     while (t--) {
         int a, b;
         cin >> a >> b;
-        cout << min_distance(a, b) << '\n';
+        cout << min_distance(min(a, b), max(a, b)) << '\n';
     }
 
     return 0;
