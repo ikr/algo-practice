036ec4b135ab44654237a51fa4b0714521dc8333
Ivan Krechetov <ikr@ikr.su>
2022-04-08 18:21:56 +0200

    A, B — AC; C — WA

---
+++ codeforces/2022/НОД_против_НОК/solution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+using ll = long long;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        ll n;
+        cin >> n;
+        cout << (n - 3LL) << " 1 1 1\n";
+    }
+
+    return 0;
+}
