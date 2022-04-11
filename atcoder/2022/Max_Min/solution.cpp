49b807a7077840cf1d959f85888cf70fa015697d
Ivan Krechetov <ikr@ikr.su>
2022-04-11 08:14:26 +0200

    AC × 20, WA × 6

--- atcoder/2022/Max_Min/solution.cpp
+++ atcoder/2022/Max_Min/solution.cpp
@@ -27,26 +27,44 @@ int proper_intervals_num(const vector<int> &A, const int HI, const int LO) {
     const auto algo = [N, &result](const vector<int> &ss, const vector<int> &xs,
                                    const vector<int> &ys) {
         for (const auto x : xs) {
-            const auto sep_it = upper_bound(cbegin(ss), cend(ss), x);
-            const auto r = sep_it == cend(ss) ? N : (*sep_it);
+            const auto sep_it = lower_bound(cbegin(ss), cend(ss), x);
+            const auto l1 = sep_it == cbegin(ss) ? -1 : *prev(sep_it);
 
-            const auto ys_it = lower_bound(cbegin(ys), cend(ys), r);
-            if (ys.empty() || ys_it == cbegin(ys)) continue;
-            const auto y = *prev(ys_it);
-            if (y < x) continue;
+            const auto ys_it0 = lower_bound(cbegin(ys), cend(ys), x);
+            const auto l2 = ys_it0 == cbegin(ys) ? -1 : *prev(ys_it0);
 
-            const auto y0_it = lower_bound(cbegin(ys), cend(ys), x);
-            const auto y0 = inof(distance(cbegin(ys), y0_it));
+            const auto xs_it0 = lower_bound(cbegin(xs), cend(xs), x);
+            const auto l3 = xs_it0 == cbegin(xs) ? -1 : *prev(xs_it0);
 
-            if (y0 > y) continue;
-            result += y - y0 + 1;
+            const auto l = max({l1, l2, l3});
+
+            const auto sep_jt = upper_bound(cbegin(ss), cend(ss), x);
+            const auto r = sep_jt == cend(ss) ? N : (*sep_jt);
+
+            const auto ys_it = upper_bound(cbegin(ys), cend(ys), x);
+            // cerr << "x:" << x << " l:" << l << " r:" << r;
+            if (ys_it == cend(ys)) {
+                // cerr << " and no y after x!" << endl;
+                continue;
+            }
+
+            const auto y = *ys_it;
+            // cerr << " y:" << y;
+            if (y >= r) {
+                // cerr << " an y is not before r!" << endl;
+                continue;
+            }
+
+            const auto d = (x - l) * (r - y);
+            // cerr << " d:" << d << endl;
+            result += d;
         }
     };
 
     algo(sep, los, his);
     algo(sep, his, los);
 
-    return HI == LO ? result / 2 : result;
+    return HI == LO ? (result / 2 + sz(los)) : result;
 }
 
 int main() {
