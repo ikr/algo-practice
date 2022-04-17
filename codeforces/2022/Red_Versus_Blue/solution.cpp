b8bcde82f66bf81f522cf9efe8a22342cf2870b8
Ivan Krechetov <ikr@ikr.li>
2022-04-17 17:11:34 +0200

    WA

---
+++ codeforces/2022/Red_Versus_Blue/solution.cpp
@@ -0,0 +1,45 @@
+#include <cassert>
+#include <iostream>
+#include <string>
+using namespace std;
+
+template <typename T> constexpr T div_ceil(const T x, const T y) {
+    return x ? (1 + (x - 1) / y) : 0;
+}
+
+string spect_seq(int r, int b) {
+    string result;
+    const auto span = div_ceil(r, b + 1);
+
+    while (r || b) {
+        if (r) {
+            const auto d = min(r, span);
+            result += string(d, 'R');
+            r -= d;
+        }
+
+        if (b) {
+            result += 'B';
+            --b;
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, r, b;
+        cin >> n >> r >> b;
+        assert(r + b == n);
+        assert(b < r);
+        cout << spect_seq(r, b) << '\n';
+    }
+
+    return 0;
+}
