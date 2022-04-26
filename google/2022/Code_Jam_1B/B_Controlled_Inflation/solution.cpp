0afdad18265b468a373cc5219075316e7faafa1f
Ivan Krechetov <ikr@ikr.li>
2022-04-26 17:21:16 +0200

    AC

--- google/2022/Code_Jam_1B/B_Controlled_Inflation/solution.cpp
+++ google/2022/Code_Jam_1B/B_Controlled_Inflation/solution.cpp
@@ -3,26 +3,9 @@
 #include <cmath>
 #include <iostream>
 #include <iterator>
-#include <numeric>
 #include <utility>
 #include <vector>
 using namespace std;
-
-template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
-    os << '[';
-    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
-        if (i != xs.cbegin()) os << ' ';
-        os << *i;
-    }
-    os << ']';
-    return os;
-}
-
-template <typename T>
-ostream &operator<<(ostream &os, const vector<vector<T>> &xss) {
-    for (const auto xs : xss) os << xs << '\n';
-    return os;
-}
 
 using ll = long long;
 
@@ -47,40 +30,26 @@ ll coverage_cost(const ll src, const pair<ll, ll> ab, const ll dst) {
 
 ll solve(const vector<vector<int>> &xss) {
     const auto N = sz(xss);
-    assert(N <= 10);
     const auto P = sz(xss[0]);
+    vector<vector<ll>> dp(N, vector<ll>(P, INF));
 
-    ll result = INF;
-    vector<int> ord(N);
-    iota(begin(ord), end(ord), 0);
+    for (int j = 0; j < P; ++j) {
+        dp[0][j] = llof(xss[0].back()) + xss[0].back() - xss[0][j];
+    }
 
-    do {
-        vector<vector<ll>> dp(N, vector<ll>(P, INF));
-
+    for (int i = 1; i < N; ++i) {
         for (int j = 0; j < P; ++j) {
-            dp[0][j] =
-                llof(xss[ord[0]].back()) + xss[ord[0]].back() - xss[ord[0]][j];
-        }
-
-        for (int i = 1; i < N; ++i) {
-            for (int j = 0; j < P; ++j) {
-                for (int k = 0; k < P; ++k) {
-                    dp[i][j] =
-                        min(dp[i][j],
-                            dp[i - 1][k] + coverage_cost(xss[ord[i - 1]][k],
-                                                         {xss[ord[i]][0],
-                                                          xss[ord[i]].back()},
-                                                         xss[ord[i]][j]));
-                }
+            for (int k = 0; k < P; ++k) {
+                dp[i][j] =
+                    min(dp[i][j],
+                        dp[i - 1][k] + coverage_cost(xss[i - 1][k],
+                                                     {xss[i][0], xss[i].back()},
+                                                     xss[i][j]));
             }
         }
+    }
 
-        cerr << "ord: " << ord << " dp:" << endl << dp << endl;
-
-        result = min(result, *min_element(cbegin(dp.back()), cend(dp.back())));
-    } while (next_permutation(begin(ord), end(ord)));
-
-    return result;
+    return *min_element(cbegin(dp.back()), cend(dp.back()));
 }
 
 int main() {
