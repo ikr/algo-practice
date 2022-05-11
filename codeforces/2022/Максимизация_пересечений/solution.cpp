0efb7fdf1085566946d7909fcb6108173d1d9cb7
Ivan Krechetov <ikr@ikr.li>
2022-05-11 07:07:16 +0200

    AC on H1 — the easy version

--- codeforces/2022/Максимизация_пересечений/solution.cpp
+++ codeforces/2022/Максимизация_пересечений/solution.cpp
@@ -10,7 +10,19 @@ template <typename T> constexpr int inof(const T x) {
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-ll max_intersections(const vector<int> &xs) { return -1; }
+ll max_intersections(const vector<int> &xs) {
+    ll result{};
+
+    for (int i = sz(xs) - 1; i >= 0; --i) {
+        const auto x = xs[i];
+
+        for (int j = 0; j < i; ++j) {
+            if (xs[j] >= x) ++result;
+        }
+    }
+
+    return result;
+}
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
@@ -23,7 +35,10 @@ int main() {
         cin >> n;
 
         vector<int> xs(n);
-        for (auto &x : xs) cin >> x;
+        for (auto &x : xs) {
+            cin >> x;
+            --x;
+        }
 
         cout << max_intersections(xs) << '\n';
     }
