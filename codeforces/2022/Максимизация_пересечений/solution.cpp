1c998dec140cd8420cc7b975a2862596bf3c80a8
Ivan Krechetov <ikr@ikr.li>
2022-05-11 08:09:27 +0200

    How does this even work?…

--- codeforces/2022/Максимизация_пересечений/solution.cpp
+++ codeforces/2022/Максимизация_пересечений/solution.cpp
@@ -1,3 +1,5 @@
+#include <atcoder/lazysegtree>
+#include <cassert>
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -10,7 +12,20 @@ template <typename T> constexpr int inof(const T x) {
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
+constexpr ll z() { return 0; }
+constexpr ll mplus(const ll x, const ll y) { return x + y; }
+constexpr ll e() { return 1; }
+
 ll max_intersections(const vector<int> &xs) {
+    atcoder::lazy_segtree<ll, mplus, z, ll, mplus, mplus, z> sgt(5);
+    sgt.apply(0, 2, 1);
+    sgt.apply(0, 5, 1);
+    sgt.apply(0, 1, 1);
+
+    assert(sgt.prod(0, 1) == 3LL);
+    cerr << "ap:" << sgt.all_prod() << endl;
+    assert(sgt.all_prod() == 8LL);
+
     ll result{};
 
     for (int i = sz(xs) - 1; i >= 0; --i) {
