6778ff716c089093450135e82f8bf253e7ea4ba3
Ivan Krechetov <ikr@ikr.li>
2022-07-10 09:59:22 +0200

    Trying to wing it ⇒ WA

--- google/2022/Kick_Start_D/C_Touchbar_Typing/solution.cpp
+++ google/2022/Kick_Start_D/C_Touchbar_Typing/solution.cpp
@@ -26,20 +26,62 @@ ll min_time(const vector<int> &S, const vector<int> &K) {
     }
 
     vector<vector<ll>> dp(N, vector(M, INF));
+    vector<ll> lo(N, INF);
+    vector<int> loi(N, -1);
+
     for (int j = 0; j < sz(idx[S[0]]); ++j) {
         dp[0][j] = 0;
     }
+    lo[0] = 0;
+    loi[0] = 0;
 
     for (int i = 1; i < N; ++i) {
         const vector<int> &sources = idx.at(S[i - 1]);
         const vector<int> &targets = idx.at(S[i]);
 
-        for (int j0 = 0; j0 < sz(sources); ++j0) {
-            for (int j1 = 0; j1 < sz(targets); ++j1) {
+        for (int j1 = 0; j1 < sz(targets); ++j1) {
+            {
+                auto it =
+                    lower_bound(cbegin(sources), cend(sources), targets[j1]);
+                const auto jt = it == cbegin(sources) ? it : prev(it);
+                if (it == cend(sources)) it = prev(it);
+
+                const auto j0 = [&]() -> int {
+                    if (abs(*it - targets[j1]) < abs(*jt - targets[j1])) {
+                        return inof(distance(cbegin(sources), it));
+                    } else {
+                        return inof(distance(cbegin(sources), jt));
+                    }
+                }();
+
+                if (dp[i - 1][j0] + abs(sources[j0] - targets[j1]) < lo[i]) {
+                    lo[i] = dp[i - 1][j0] + abs(sources[j0] - targets[j1]);
+                    loi[i] = j1;
+                }
+
+                dp[i][j1] = min(dp[i][j1],
+                                dp[i - 1][j0] + abs(sources[j0] - targets[j1]));
+            }
+
+            {
+                const auto j0 = loi[i - 1];
+                if (dp[i - 1][j0] + abs(sources[j0] - targets[j1]) < lo[i]) {
+                    lo[i] = dp[i - 1][j0] + abs(sources[j0] - targets[j1]);
+                    loi[i] = j1;
+                }
+
                 dp[i][j1] = min(dp[i][j1],
                                 dp[i - 1][j0] + abs(sources[j0] - targets[j1]));
             }
         }
+
+        // for (int j0 = 0; j0 < sz(sources); ++j0) {
+        //     for (int j1 = 0; j1 < sz(targets); ++j1) {
+        //         dp[i][j1] = min(dp[i][j1],
+        //                         dp[i - 1][j0] + abs(sources[j0] -
+        //                         targets[j1]));
+        //     }
+        // }
     }
 
     return *min_element(cbegin(dp.back()), cend(dp.back()));
