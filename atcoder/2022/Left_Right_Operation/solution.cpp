8b7f6cc29f7cc40626fbdf1d0176b203ca69ba82
Ivan Krechetov <ikr@ikr.su>
2022-08-06 15:37:11 +0200

    AC

--- atcoder/2022/Left_Right_Operation/solution.cpp
+++ atcoder/2022/Left_Right_Operation/solution.cpp
@@ -1,3 +1,4 @@
+#include <atcoder/segtree>
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -9,62 +10,43 @@ template <typename T> constexpr int inof(const T x) {
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-int optimal_prefix_replacements_num(const ll T, const vector<ll> &xs) {
+ll op(ll a, ll b) { return min(a, b); }
+
+ll e() { return LONG_LONG_MAX; }
+
+vector<ll> min_sums_by_replacements_num(const ll T, const vector<ll> &xs) {
+    vector<ll> ss(sz(xs));
+    partial_sum(cbegin(xs), cend(xs), begin(ss));
+    vector<ll> dp(sz(xs) + 1, ss.back());
+
+    for (int k = 1; k <= sz(xs); ++k) {
+        dp[k] = k * T + (ss.back() - ss[k - 1]);
+    }
+
+    return dp;
+}
+
+vector<ll> reversed(vector<ll> xs) {
+    reverse(begin(xs), end(xs));
+    return xs;
+}
+
+ll min_post_ops_sum(const ll L, const ll R, const vector<ll> &A) {
+    const auto &xs = A;
+    const auto ys = reversed(A);
+
     vector<ll> ss(sz(xs));
     partial_sum(cbegin(xs), cend(xs), begin(ss));
 
-    int result{};
-    ll opt = ss.back();
+    const auto dpy = min_sums_by_replacements_num(R, ys);
+    atcoder::segtree<ll, op, e> ty(dpy);
 
-    for (int k = 1; k <= sz(xs); ++k) {
-        ll curr = k * T + (ss.back() - ss[k - 1]);
-        if (curr < opt) {
-            result = k;
-            opt = curr;
-        }
+    ll result = accumulate(cbegin(A), cend(A), 0LL);
+    for (int k = 0; k <= sz(A); ++k) {
+        result = min(result, L * k - (k ? ss[k - 1] : 0LL) +
+                                 ty.prod(0, sz(xs) - k + 1));
     }
-
     return result;
-}
-
-ll min_post_ops_sum(const ll L, const ll R, const vector<ll> &xs) {
-    const auto o1 = [&]() -> ll {
-        auto A = xs;
-        const auto k1 = optimal_prefix_replacements_num(L, A);
-        for (int i = 1; i <= k1; ++i) {
-            A[i - 1] = L;
-        }
-
-        reverse(begin(A), end(A));
-        const auto k2 = optimal_prefix_replacements_num(R, A);
-        for (int i = 1; i <= k2; ++i) {
-            A[i - 1] = R;
-        }
-
-        return accumulate(cbegin(A), cend(A), 0LL);
-    }();
-
-    const auto o2 = [&]() -> ll {
-        auto A = xs;
-        const auto k1 = optimal_prefix_replacements_num(L, A);
-        for (int i = 1; i <= k1; ++i) {
-            A[i - 1] = L;
-        }
-        return accumulate(cbegin(A), cend(A), 0LL);
-    }();
-
-    const auto o3 = [&]() -> ll {
-        auto A = xs;
-        reverse(begin(A), end(A));
-        const auto k2 = optimal_prefix_replacements_num(R, A);
-        for (int i = 1; i <= k2; ++i) {
-            A[i - 1] = R;
-        }
-
-        return accumulate(cbegin(A), cend(A), 0LL);
-    }();
-
-    return min({o1, o2, o3});
 }
 
 int main() {
