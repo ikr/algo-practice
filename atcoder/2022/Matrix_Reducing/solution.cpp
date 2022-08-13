1996263bc2f087bb94814a300915dfa315a6ceb6
Ivan Krechetov <ikr@ikr.su>
2022-08-13 15:11:24 +0200

    WA x 2

--- atcoder/2022/Matrix_Reducing/solution.cpp
+++ atcoder/2022/Matrix_Reducing/solution.cpp
@@ -1,70 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
-    os << '[';
-    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
-        if (i != xs.cbegin()) os << ' ';
-        os << *i;
-    }
-    os << ']';
-    return os;
-}
-
-template <typename T>
-ostream &operator<<(ostream &os, const vector<vector<T>> &xss) {
-    for (const auto &xs : xss) os << xs << '\n';
-    return os;
-}
-
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-vector<int> bits_of_indices_of_xs_projection_into_ys(const vector<int> &xs,
-                                                     const vector<int> &ys) {
+vector<vector<int>> transpose(const vector<vector<int>> &grid) {
+    vector<vector<int>> result(sz(grid[0]), vector(sz(grid), 0));
+
+    for (int ro = 0; ro < sz(grid); ++ro) {
+        for (int co = 0; co < sz(grid[0]); ++co) {
+            result[co][ro] = grid[ro][co];
+        }
+    }
+
+    return result;
+}
+
+void remove_row(vector<vector<int>> &grid, const int ro) {
+    assert(ro < sz(grid));
+    grid.erase(cbegin(grid) + ro);
+}
+
+vector<vector<int>> num_indices(const int n, const int k) {
+    assert(n <= 10);
+    assert(k < n);
     vector<vector<int>> result;
 
-    const auto recur = [&](const auto self, vector<int> &cur, const int i,
-                           const int j) -> void {
-        if (i >= sz(xs)) {
-            assert(sz(cur) == sz(xs));
-            result.push_back(cur);
+    for (int bits = 1; bits < (1 << n); ++bits) {
+        if (__builtin_popcount(bits) != k) continue;
+
+        vector<int> x;
+        for (int i = 0; i < n; ++i) {
+            if (bits & (1 << i)) x.push_back(i);
         }
-
-        if (j >= sz(ys)) return;
-
-        const auto it = find(cbegin(ys) + j, cend(ys), xs[i]);
-        if (it == cend(ys)) return;
-
-        cur.push_back(inof(distance(cbegin(ys), it)));
-        self(self, cur, i + 1, j + 1);
-        cur.pop_back();
-
-        self(self, cur, i, j + 1);
-    };
-
-    {
-        vector<int> cur;
-        recur(recur, cur, 0, 0);
+        assert(sz(x) == k);
+        result.push_back(move(x));
     }
 
-    cerr << "result:\n" << result << '\n';
-
-    vector<int> bits;
-    for (int ro = 0; ro < sz(result); ++ro) {
-        int x{};
-
-        for (int i = 0; i < sz(result); ++i) {
-            x |= (1 << result[ro][i]);
-        }
-
-        assert(__builtin_popcount(x) == sz(xs));
-        bits.push_back(x);
-    }
-    return bits;
+    return result;
 }
 
 bool is_possible(const vector<vector<int>> &A, const vector<vector<int>> &B) {
@@ -73,8 +49,24 @@ bool is_possible(const vector<vector<int>> &A, const vector<vector<int>> &B) {
     const auto Hb = sz(B);
     const auto Wb = sz(B[0]);
     if (Ha < Hb || Wa < Wb) return false;
+    if (Ha == Hb || Wa == Wb) return A == B;
 
-    for (int ro = 0; ro < )
+    for (const auto &iro : num_indices(Ha, Ha - Hb)) {
+        for (const auto &ico : num_indices(Wa, Wa - Wb)) {
+            auto grid = A;
+            for (auto it = crbegin(iro); it != crend(iro); ++it) {
+                remove_row(grid, *it);
+            }
+
+            grid = transpose(grid);
+            for (auto it = crbegin(ico); it != crend(ico); ++it) {
+                remove_row(grid, *it);
+            }
+            grid = transpose(grid);
+
+            if (grid == B) return true;
+        }
+    }
 
     return false;
 }
