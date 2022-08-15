91e31f38baaa5999ac6ed70bcd403f7c2472b3f0
Ivan Krechetov <ikr@ikr.li>
2022-08-15 07:28:14 +0200

    Optimize

--- codeforces/2022/Mirror_Grid/solution.cpp
+++ codeforces/2022/Mirror_Grid/solution.cpp
@@ -13,11 +13,11 @@ int min_ops(const vector<string> &grid) {
     const auto n = sz(grid);
     int result{};
 
-    set<pii> done;
+    vector<vector<bool>> done(n, vector(n, false));
 
     for (int ro = 0; ro < n; ++ro) {
         for (int co = 0; co < n; ++co) {
-            if (done.count(pii{ro, co})) continue;
+            if (done[ro][co]) continue;
 
             int zeros{};
             int ones{};
@@ -28,19 +28,19 @@ int min_ops(const vector<string> &grid) {
             const auto co1 = n - 1 - ro;
             zeros += grid[ro1][co1] == '0';
             ones += grid[ro1][co1] == '1';
-            done.emplace(ro1, co1);
+            done[ro1][co1] = true;
 
             const auto ro2 = co1;
             const auto co2 = n - 1 - ro1;
             zeros += grid[ro2][co2] == '0';
             ones += grid[ro2][co2] == '1';
-            done.emplace(ro2, co2);
+            done[ro2][co2] = true;
 
             const auto ro3 = co2;
             const auto co3 = n - 1 - ro2;
             zeros += grid[ro3][co3] == '0';
             ones += grid[ro3][co3] == '1';
-            done.emplace(ro3, co3);
+            done[ro3][co3] = true;
 
             result += min(zeros, ones);
         }
