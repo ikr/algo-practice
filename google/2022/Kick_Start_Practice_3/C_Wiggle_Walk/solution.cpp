626c9a1e7ead4e95754f0c40114b0ca4b96f110b
Ivan Krechetov <ikr@ikr.li>
2022-08-29 19:44:13 +0200

    AC

--- google/2022/Kick_Start_Practice_3/C_Wiggle_Walk/solution.cpp
+++ google/2022/Kick_Start_Practice_3/C_Wiggle_Walk/solution.cpp
@@ -10,6 +10,16 @@ ostream &operator<<(ostream &os, const pair<T1, T2> &x) {
     return os;
 }
 
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
+
 template <typename T> ostream &operator<<(ostream &os, const set<T> &xs) {
     os << '{';
     for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
@@ -32,8 +42,8 @@ template <typename T> constexpr int inof(const T x) {
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
 int simulate_move_get_new_index(set<pii> &xs, const int i, const Dir dir) {
+    // cerr << "At " << i << " within " << xs << " and moving " << dir << endl;
     assert(!xs.empty());
-    cerr << "xs:" << xs << " i:" << i << " dir:" << dir << endl;
     const auto it = xs.lower_bound(pii{i, INT_MIN});
 
     if (dir == Dir::L) {
@@ -45,7 +55,7 @@ int simulate_move_get_new_index(set<pii> &xs, const int i, const Dir dir) {
         xs.erase(jt);
 
         if (a < b) xs.emplace(a, b - 1);
-        cerr << "xs:" << xs << " returning " << b << endl;
+        // cerr << "New xs:" << xs << " - returning " << b << endl;
         return b;
     } else {
         const auto [a, b] = *it;
@@ -53,15 +63,54 @@ int simulate_move_get_new_index(set<pii> &xs, const int i, const Dir dir) {
         xs.erase(it);
 
         if (a < b) xs.emplace(a + 1, b);
-        cerr << "xs:" << xs << " returning " << a << endl;
+        // cerr << "New xs:" << xs << " - returning " << a << endl;
         return a;
     }
+}
+
+void erase_at(set<pii> &xs, const int i) {
+    // cerr << "Will erase " << i << " from " << xs << endl;
+    if (xs.empty()) return;
+
+    const auto it = [&]() -> set<pii>::const_iterator {
+        const auto jt = xs.lower_bound(pii{i, INT_MIN});
+        if (jt == cend(xs)) return prev(jt);
+        if (i == jt->first) return jt;
+        return jt == cbegin(xs) ? jt : prev(jt);
+    }();
+
+    const auto [a, b] = *it;
+    if (!(a <= i && i <= b)) return;
+    xs.erase(it);
+
+    if (a == b) {
+        assert(i == a);
+        // cerr << "New version of xs: " << xs << endl;
+        return;
+    }
+
+    if (i == a) {
+        xs.emplace(a + 1, b);
+        // cerr << "New version of xs: " << xs << endl;
+        return;
+    }
+
+    if (i == b) {
+        xs.emplace(a, b - 1);
+        // cerr << "New version of xs: " << xs << endl;
+        return;
+    }
+
+    xs.emplace(a, i - 1);
+    xs.emplace(i + 1, b);
+    // cerr << "New version of xs: " << xs << endl;
 }
 
 pii simulate_moves_get_destination(const pii dim, const pii src,
                                    const string &commands) {
     const auto [H, W] = dim;
     const auto [ro0, co0] = src;
+    // cerr << "Starting at " << src << endl;
 
     vector<set<pii>> horz(H, set{pii{0, W - 1}});
     horz[ro0] = set<pii>{};
@@ -69,31 +118,39 @@ pii simulate_moves_get_destination(const pii dim, const pii src,
     if (co0 < W - 1) horz[ro0].emplace(co0 + 1, W - 1);
 
     vector<set<pii>> vert(W, set{pii{0, H - 1}});
+    vert[co0] = set<pii>{};
     if (0 < ro0) vert[co0].emplace(0, ro0 - 1);
-    if (ro0 < H - 1) horz[co0].emplace(ro0 + 1, H - 1);
+    if (ro0 < H - 1) vert[co0].emplace(ro0 + 1, H - 1);
+
+    // cerr << "horz: " << horz << " vert: " << vert << endl;
 
     int ro = ro0, co = co0;
     for (const auto cmd : commands) {
         switch (cmd) {
         case 'N':
             ro = simulate_move_get_new_index(vert[co], ro, Dir::L);
+            erase_at(horz[ro], co);
             break;
 
         case 'S':
             ro = simulate_move_get_new_index(vert[co], ro, Dir::R);
+            erase_at(horz[ro], co);
             break;
 
         case 'W':
             co = simulate_move_get_new_index(horz[ro], co, Dir::L);
+            erase_at(vert[co], ro);
             break;
 
         case 'E':
             co = simulate_move_get_new_index(horz[ro], co, Dir::R);
+            erase_at(vert[co], ro);
             break;
 
         default:
             assert(false && "Unknown command");
         }
+        // cerr << "horz: " << horz << " vert: " << vert << endl;
     }
     return {ro, co};
 }
