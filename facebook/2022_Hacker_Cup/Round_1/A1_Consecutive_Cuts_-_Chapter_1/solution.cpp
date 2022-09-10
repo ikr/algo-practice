1dba3d0339018b9fa20d782c9370ac3442a5953e
Ivan Krechetov <ikr@ikr.li>
2022-09-10 21:17:22 +0200

    First draft ⇒ WA

---
+++ facebook/2022_Hacker_Cup/Round_1/A1_Consecutive_Cuts_-_Chapter_1/solution.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool is_possible(const int K, vector<int> A, vector<int> B) {
+    if (K == 0) return A == B;
+
+    const auto a0 = A[0];
+    const auto i0 = distance(cbegin(B), find(cbegin(B), cend(B), a0));
+    rotate(begin(B), begin(B) + i0, end(B));
+    return A == B;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+
+    int t;
+    cin >> t;
+    for (int i = 1; i <= t; ++i) {
+        int N, K;
+        cin >> N >> K;
+
+        vector<int> A(N);
+        for (auto &a : A) cin >> a;
+
+        vector<int> B(N);
+        for (auto &b : B) cin >> b;
+
+        cout << "Case #" << i << ": "
+             << (is_possible(K, move(A), move(B)) ? "YES" : "NO") << '\n';
+    }
+
+    return 0;
+}
