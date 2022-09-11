6a9bd393504a1cc90e5faee90747edf9c2ef01c7
Ivan Krechetov <ikr@ikr.su>
2022-09-11 08:45:24 +0200

    Cover some edge cases ⇒ solution validated

--- facebook/2022_Hacker_Cup/Round_1/A1_Consecutive_Cuts_-_Chapter_1/solution.cpp
+++ facebook/2022_Hacker_Cup/Round_1/A1_Consecutive_Cuts_-_Chapter_1/solution.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 bool is_possible(const int K, vector<int> A, vector<int> B) {
     if (K == 0) return A == B;
+    if (A == B) return K != 1;
+
+    if (A.size() == 2) {
+        if (A == B) {
+            return K % 2 == 0;
+        } else {
+            return K % 2 == 1;
+        }
+    }
 
     const auto a0 = A[0];
     const auto i0 = distance(cbegin(B), find(cbegin(B), cend(B), a0));
