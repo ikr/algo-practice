6e451ab261370d9c0a93c44cbbd684adb9e70fc0
Ivan Krechetov <ikr@ikr.li>
2022-09-17 10:46:17 +0200

    Draft the DP

--- atcoder/2022/Chinese_Restaurant/solution.cpp
+++ atcoder/2022/Chinese_Restaurant/solution.cpp
@@ -7,8 +7,16 @@ template <typename T> constexpr int inof(const T x) {
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
+template <typename T> struct mmax final {
+    constexpr T operator()(const T &a, const T &b) const {
+        return std::max(a, b);
+    }
+};
+
 int max_happy(const vector<int> &xs) {
     const auto N = sz(xs);
+    const auto mod_norm = [N](const int x) -> int { return ((x % N) + N) % N; };
+
     const auto xsxs = [&]() -> vector<int> {
         auto result = xs;
         result.insert(cend(result), cbegin(xs), cend(xs));
@@ -17,9 +25,25 @@ int max_happy(const vector<int> &xs) {
 
     // D[i][j] is the length of an iota starting at i
     // from the number (xsxs[i] - 1 + j)
-    vector<array<int, 3>> D(2 * N, array{0, 0, 0});
+    vector<array<int, 3>> D(2 * N, array{1, 1, 1});
 
-    return -1;
+    for (int i = 2 * N - 2; i >= 0; --i) {
+        for (int a = 0; a < 3; ++a) {
+            for (int b = 0; b < 3; ++b) {
+                if (mod_norm(xsxs[i] - 1 + a + 1) ==
+                    mod_norm(xsxs[i + 1] - 1 + b)) {
+                    D[i][a] = D[i + 1][b] + 1;
+                }
+            }
+        }
+    }
+
+    const auto hi = transform_reduce(
+        cbegin(D), cend(D), 1, mmax<int>{}, [](const auto &tri) -> int {
+            return *max_element(cbegin(tri), cend(tri));
+        });
+
+    return min(hi, N);
 }
 
 int main() {
