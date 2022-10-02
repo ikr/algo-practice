768bda6a1421c5741aca6e1ba03168ea7317c331
Ivan Krechetov <ikr@ikr.su>
2022-10-02 15:57:01 +0200

    WA; more individual cases AC-ed though :)

--- atcoder/2022/Two_LIS_Sum/solution.cpp
+++ atcoder/2022/Two_LIS_Sum/solution.cpp
@@ -1,16 +1,5 @@
-#include <atcoder/fenwicktree>
 #include <bits/stdc++.h>
 using namespace std;
-
-template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
-    os << '[';
-    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
-        if (i != xs.cbegin()) os << ' ';
-        os << *i;
-    }
-    os << ']';
-    return os;
-}
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
@@ -18,65 +7,51 @@ template <typename T> constexpr int inof(const T x) {
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
+// https://github.com/kth-competitive-programming/kactl/blob/main/content/various/LIS.h
+namespace kactl {
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+typedef vector<int> vi;
+
+template <class I> vi lis(const vector<I> &S) {
+    if (S.empty()) return {};
+    vi prev(sz(S));
+    typedef pair<I, int> p;
+    vector<p> res;
+    rep(i, 0, sz(S)) {
+        // change 0 -> i for longest non-decreasing subsequence
+        auto it = lower_bound(all(res), p{S[i], 0});
+        if (it == res.end()) res.emplace_back(), it = res.end() - 1;
+        *it = {S[i], i};
+        prev[i] = it == res.begin() ? 0 : (it - 1)->second;
+    }
+    int L = sz(res), cur = res.back().second;
+    vi ans(L);
+    while (L--) ans[L] = cur, cur = prev[cur];
+    return ans;
+}
+} // namespace kactl
+
 static constexpr int N_MAX = 3 * 100'000;
 
 int max_target(const vector<int> &A, const vector<int> &B) {
     const auto N = sz(A);
-    atcoder::fenwick_tree<int> fa(N_MAX + 1);
-    atcoder::fenwick_tree<int> fb(N_MAX + 1);
+    vector<int> idx(N, 0);
+    iota(begin(idx), end(idx), 0);
 
-    vector<int> inv_a(N, 0);
-    vector<int> inv_b(N, 0);
+    sort(begin(idx), end(idx), [&](const int i, const int j) {
+        return min(A[i], B[i]) < min(A[j], B[j]);
+    });
 
-    for (int i = N - 1; i >= 0; --i) {
-        inv_a[i] = fa.sum(0, A[i]);
-        inv_b[i] = fb.sum(0, B[i]);
+    vector<int> A_(N, 0);
+    vector<int> B_(N, 0);
 
-        fa.add(A[i], 1);
-        fb.add(B[i], 1);
+    for (int i = 0; i < N; ++i) {
+        A_[i] = A[idx[i]];
+        B_[i] = B[idx[i]];
     }
 
-    int lis_a{}, lis_b{};
-
-    for (int i = N - 1; i >= 0; --i) {
-        const auto tail = N - 1 - i;
-
-        if (inv_a[i] == 0 && inv_b[i] == 0) {
-            ++lis_a;
-            ++lis_b;
-            continue;
-        }
-
-        if (inv_a[i] == tail && inv_b[i] == tail) {
-            ++lis_a;
-            ++lis_b;
-            continue;
-        }
-
-        if (inv_a[i] == 0) {
-            ++lis_a;
-            continue;
-        }
-
-        if (inv_b[i] == 0) {
-            ++lis_b;
-            continue;
-        }
-
-        if (inv_a[i] == tail) {
-            ++lis_a;
-            continue;
-        }
-
-        if (inv_b[i] == tail) {
-            ++lis_b;
-            continue;
-        }
-    }
-
-    // cerr << "inv_a: " << inv_a << " inv_b: " << inv_b << endl;
-
-    return lis_a + lis_b;
+    return sz(kactl::lis(A_)) + sz(kactl::lis(B_));
 }
 
 int main() {
