2fb9b1bc528d14d13238ee0336c5e8b100ea2104
Ivan Krechetov <ikr@ikr.su>
2022-10-19 12:02:58 +0200

    First submit: RE — looks like `zero_off_bit` ens up in a non-triplet state

--- codechef/2022/Tri_XOR/solution.cpp
+++ codechef/2022/Tri_XOR/solution.cpp
@@ -1,7 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T> ostream &operator<<(ostream &os, const multiset<T> &xs) {
+    os << '{';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << '}';
+    return os;
+}
+
 using tri = tuple<int, int, int>;
+
+ostream &operator<<(ostream &os, const tri &x) {
+    const auto [a, b, c] = x;
+    os << '(' << a << ' ' << b << ' ' << c << ')';
+    return os;
+}
+
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
@@ -12,35 +38,42 @@ template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 vector<tri> find_program(multiset<int> xs) {
     vector<tri> result;
 
-    while (!xs.empty()) {
-        const auto [ti, tj, tk] = [&]() -> tri {
-            for (auto i = cbegin(xs); i != cend(xs); ++i) {
-                for (auto j = cbegin(xs); j != prev(cend(xs)); ++j) {
-                    if (j == i) continue;
+    const auto zero_off_bit = [&](const int bit_shift) -> bool {
+        const int mask = 1 << bit_shift;
+        vector<int> stash;
 
-                    for (auto k = next(j); k != cend(xs); ++k) {
-                        if (k == i) continue;
-                        if (((*j) ^ (*k)) == *i) return {*i, *j, *k};
-                    }
-                }
-            }
-
-            assert(false && "/o\\");
-            return {-1, -1, -1};
-        }();
-
-        cerr << "found " << ti << ' ' << tj << ' ' << tk << endl;
-
-        if (ti == tj && tj == tk) {
-            result.emplace_back(ti, tj, tk);
-        } else {
-            result.emplace_back(ti, tj, tk);
-            result.emplace_back(ti, ti, ti);
+        for (const auto x : xs) {
+            if (x & mask) stash.push_back(x);
         }
 
-        xs.erase(xs.find(ti));
-        xs.erase(xs.find(tj));
-        xs.erase(xs.find(tk));
+        if (stash.empty()) return true;
+        if (sz(stash) % 3) return false;
+
+        for (auto i = 0; i < sz(stash); i += 3) {
+            for (int j = 0; j < 3; ++j) {
+                assert(i + j < sz(stash));
+                const auto it = xs.find(stash[i + j]);
+                assert(it != cend(xs));
+
+                xs.erase(it);
+            }
+
+            xs.insert(stash[i] ^ stash[i + 1]);
+            xs.insert(stash[i + 1] ^ stash[i + 2]);
+            xs.insert(stash[i] ^ stash[i + 2]);
+            result.emplace_back(stash[i], stash[i + 1], stash[i + 2]);
+        }
+
+        return true;
+    };
+
+    for (int bs = 31; bs >= 0; --bs) {
+        if (!zero_off_bit(bs)) {
+            // cerr << "On bs: " << bs << " rest: " << xs << " program: " <<
+            // result
+            //      << endl;
+            assert(false && "zero-off failed");
+        }
     }
 
     return result;
@@ -56,10 +89,12 @@ int main() {
         int n;
         cin >> n;
 
-        vector<int> xs(n);
-        for (auto &x : xs) cin >> x;
-        sort(rbegin(xs), rend(xs));
-        while (!xs.empty() && xs.back() == 0) xs.pop_back();
+        vector<int> xs;
+        for (int i = 0; i < n; ++i) {
+            int x;
+            cin >> x;
+            if (x) xs.push_back(x);
+        }
 
         const auto ops = find_program(multiset<int>(crbegin(xs), crend(xs)));
         cout << sz(ops) << '\n';
