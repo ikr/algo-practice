4ceaf954ce6a16bb8cab9d07847d32be11151ea8
Ivan Krechetov <ikr@ikr.li>
2022-10-23 16:49:53 +0200

    Debugging…

--- toki/2022/Djonatan_and_Adventure_Game/solution.cpp
+++ toki/2022/Djonatan_and_Adventure_Game/solution.cpp
@@ -1,6 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<vector<T>> &xss) {
+    for (const auto &xs : xss) os << xs << '\n';
+    return os;
+}
+
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
@@ -15,24 +31,54 @@ pair<int, int> solution(const vector<tri> &src, const int U) {
 
     {
         const auto [a, b, x] = src[0];
-        if (U >= a) {
-            D[0][U - a + b] = x;
+        int u = U;
+        int k = 1;
+
+        while (u >= a) {
+            D[0][u - a + b] = k * x;
+            u = u - a + b;
+            ++k;
         }
     }
+
+    cerr << "D0:\n" << D << endl;
 
     for (int i = 1; i < N; ++i) {
         const auto [a, b, x] = src[i];
 
-        if (U >= a) {
-            D[i][U - a + b] = x;
+        {
+            int u = U;
+            int k = 1;
+
+            while (u >= a) {
+                D[i][u - a + b] = k * x;
+                u = u - a + b;
+                ++k;
+            }
         }
 
-        for (int u = 0; u <= U; ++u) {
-            if (D[i - 1][u] >= 0 && (u - a + b) >= 0) {
-                D[i][u - a + b] = max(D[i][u - a + b], D[i - 1][u] + x);
+        for (int u0 = 0; u0 <= U; ++u0) {
+            if (D[i - 1][u0] >= 0 && u0 >= a) {
+
+                int u = u0;
+                int k = 1;
+
+                while (u >= a) {
+                    cerr << "i:" << i << " u0:" << u0
+                         << " prev:" << D[i - 1][u0] << " k:" << k
+                         << " o1:" << D[i][u - a + b]
+                         << " o2:" << (D[i - 1][u0] + k * x) << endl;
+
+                    D[i][u - a + b] =
+                        max(D[i][u - a + b], D[i - 1][u0] + k * x);
+                    u = u - a + b;
+                    ++k;
+                }
             }
         }
     }
+
+    cerr << "\nD:\n" << D << endl;
 
     int reward{};
     int energy = U;
@@ -56,7 +102,6 @@ int main() {
 
     vector<tri> src(N);
     for (auto &[a, b, x] : src) cin >> a >> b >> x;
-    sort(begin(src), end(src));
 
     const auto [reward, energy] = solution(src, U);
     cout << reward << ' ' << energy << '\n';
