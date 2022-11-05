a2f2eae35fcea07c1f6c42a439f8ed1f8f62998d
Ivan Krechetov <ikr@ikr.li>
2022-11-05 14:28:11 +0100

    WIP again

--- atcoder/2022/Round_Trip/solution.cpp
+++ atcoder/2022/Round_Trip/solution.cpp
@@ -23,12 +23,88 @@ pii start_coord(const vector<string> &grid) {
     return {-1, -1};
 }
 
+bool has_a_trivial_path(const vector<string> &g, const pii &roco) {
+    const auto H = sz(g);
+    const auto W = sz(g[0]);
+
+    const auto in_bounds = [&](const pii &x) -> bool {
+        const auto [ro, co] = x;
+        return 0 <= ro && ro < H && 0 <= co && co < W;
+    };
+
+    const auto at = [&](const int ro, const int co) -> char {
+        return in_bounds({ro, co}) ? g[ro][co] : '#';
+    };
+
+    const auto [ro, co] = roco;
+
+    if (at(ro, co - 1) == '.' && at(ro - 1, co - 1) == '.' &&
+        at(ro - 1, co) == '.') {
+        return true;
+    }
+
+    if (at(ro - 1, co) == '.' && at(ro - 1, co + 1) == '.' &&
+        at(ro, co + 1) == '.') {
+        return true;
+    }
+
+    if (at(ro, co + 1) == '.' && at(ro + 1, co + 1) == '.' &&
+        at(ro + 1, co) == '.') {
+        return true;
+    }
+
+    if (at(ro + 1, co) == '.' && at(ro + 1, co - 1) == '.' &&
+        at(ro, co - 1) == '.') {
+        return true;
+    }
+
+    return false;
+}
+
+bool is_navigable(const vector<string> &grid, const pii &src, const pii &dst) {
+    const auto H = sz(grid);
+    const auto W = sz(grid[0]);
+
+    const auto in_bounds = [&](const pii &roco) -> bool {
+        const auto [ro, co] = roco;
+        return 0 <= ro && ro < H && 0 <= co && co < W;
+    };
+
+    vector<vector<bool>> done(H, vector(W, false));
+    done[src.first][src.second] = true;
+
+    queue<pii> q;
+    q.push(src);
+
+    while (!q.empty()) {
+        const auto [ro, co] = q.front();
+        q.pop();
+
+        for (const auto &v : {pii{ro - 1, co}, pii{ro, co + 1}, pii{ro + 1, co},
+                              pii{ro, co - 1}}) {
+            if (!in_bounds(v)) continue;
+            const auto [ro_, co_] = v;
+            if (grid[ro_][co_] != '.') continue;
+
+            if (done[ro_][co_]) continue;
+            if (v == dst) return true;
+
+            done[ro_][co_] = true;
+            q.emplace(ro_, co_);
+        }
+    }
+
+    return false;
+}
+
 bool four_plus_trail_exists(const vector<string> &grid) {
     const auto H = sz(grid);
     const auto W = sz(grid[0]);
     const auto roco0 = start_coord(grid);
 
-    const auto in_bounds = [&](const pii roco) {
+    if (has_a_trivial_path(grid, roco0)) return true;
+
+    const auto in_bounds = [&](const pii &roco) -> bool {
         const auto [ro, co] = roco;
         return 0 <= ro && ro < H && 0 <= co && co < W;
     };
@@ -48,8 +124,16 @@ bool four_plus_trail_exists(const vector<string> &grid) {
             if (!in_bounds(v)) continue;
             const auto [ro_, co_] = v;
             if (grid[ro_][co_] == '#') continue;
+
+            if (grid[ro_][co_] == 'S' && D[ro][co] >= 3) return true;
+            if (D[ro_][co_] != -1) continue;
+
+            D[ro_][co_] = D[ro][co] + 1;
+            q.emplace(ro_, co_);
         }
     }
+
+    return false;
 }
 
 int main() {
