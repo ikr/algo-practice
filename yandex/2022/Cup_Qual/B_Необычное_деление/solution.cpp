c32eecf24274b0ae2ddcc2dae762ea59bd34265c
Ivan Krechetov <ikr@ikr.li>
2022-11-06 10:56:29 +0100

    AC

--- yandex/2022/Cup_Qual/B_Необычное_деление/solution.cpp
+++ yandex/2022/Cup_Qual/B_Необычное_деление/solution.cpp
@@ -1,6 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
+
+using ll = long long;
+
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
@@ -43,16 +55,63 @@ void remove_leading_zeros(vector<int> &ds) {
     }
 }
 
-int num_matches(const int n, const int b) {
-    if (b == 1) return n;
+constexpr int bbase(const int b) {
+    switch (b) {
+    case 2:
+        return 5;
+    case 3:
+        return 4;
+    case 4:
+        return 3;
+    default:
+        return 2;
+    }
+}
 
-    int result{};
-    for (int a = 1; a <= n; ++a) {
-        const auto ds = digits(a);
-        if (all_of(cbegin(ds), cend(ds),
-                   [b](const int d) { return d % b == 0; })) {
-            ++result;
+vector<int> closest_to_n_in_bbase(const ll n, const int b) {
+    const auto ds = digits(n);
+    vector<int> result(sz(ds), bbase(b) - 1);
+
+    for (int i = 0; i < sz(ds); ++i) {
+        if (ds[i] % b == 0) {
+            result[i] = (ds[i] / b);
+            continue;
         }
+
+        if (ds[i] < b) {
+            result[i] = 0;
+            break;
+        }
+
+        int q = 0;
+        while ((q + 1) * b < ds[i]) ++q;
+        result[i] = q;
+        break;
+    }
+
+    return result;
+}
+
+ll ipow(const int b, const int e) {
+    ll result{1};
+    for (int i = 1; i <= e; ++i) {
+        result *= b;
+    }
+    return result;
+}
+
+ll num_matches(const ll n, const int b) {
+    if (b == 1) return n;
+    if (n < b) return 0LL;
+
+    auto ds = closest_to_n_in_bbase(n, b);
+    reverse(begin(ds), end(ds));
+
+    ll result{};
+    ll mul = 1;
+    for (const auto d : ds) {
+        result += d * mul;
+        mul *= bbase(b);
     }
     return result;
 }
@@ -64,7 +123,7 @@ int main() {
     int t;
     cin >> t;
     while (t--) {
-        int n;
+        ll n;
         int b;
         cin >> n >> b;
 
