0e9ddf0c2d977876e7a20f7e57155dbd35fc8b74
Ivan Krechetov <ikr@ikr.su>
2022-11-27 09:36:55 +0100

    Solve part 2

--- adventofcode/2019/01_The_Tyranny_of_the_Rocket_Equation/solution.cpp
+++ adventofcode/2019/01_The_Tyranny_of_the_Rocket_Equation/solution.cpp
@@ -1,14 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+constexpr int req_unit(const int x) { return x / 3 - 2; }
+
+int fuel_needed(int x) {
+    int result{};
+
+    while (req_unit(x) >= 0) {
+        result += req_unit(x);
+        x = req_unit(x);
+    }
+
+    return result;
+}
+
 int main() {
     vector<int> xs;
     for (string line; getline(cin, line);) {
         xs.push_back(stoi(line));
     }
 
-    const auto result = transform_reduce(cbegin(xs), cend(xs), 0, plus<int>{},
-                                         [](const int x) { return x / 3 - 2; });
+    const auto result =
+        transform_reduce(cbegin(xs), cend(xs), 0, plus<int>{}, fuel_needed);
 
     cout << result << '\n';
     return 0;
