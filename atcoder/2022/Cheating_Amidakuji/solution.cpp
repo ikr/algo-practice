9fcbd24e0b3541aa0095c92562bf3bfcf72ceb54
Ivan Krechetov <ikr@ikr.li>
2022-11-27 09:28:30 +0100

    Didn't finish

--- atcoder/2022/Cheating_Amidakuji/solution.cpp
+++ atcoder/2022/Cheating_Amidakuji/solution.cpp
@@ -1,13 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
+
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-vector<int> positions_of_ones(const int N, const vector<int> &A) { return A; }
+vector<int> positions_of_ones(const int N, const vector<int> &A) {
+    const auto M = sz(A);
+    vector<int> xs(N);
+    iota(begin(xs), end(xs), 1);
+
+    for (int i = 0; i < M - 1; ++i) {
+        swap(xs[A[i]], xs[A[i] + 1]);
+    }
+
+    cerr << xs << endl;
+
+    vector<int> D(M, 1);
+    return D;
+}
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
@@ -17,7 +40,10 @@ int main() {
     cin >> N >> M;
 
     vector<int> A(M);
-    for (auto &a : A) cin >> a;
+    for (auto &a : A) {
+        cin >> a;
+        --a;
+    }
 
     for (const auto i : positions_of_ones(N, A)) {
         cout << i << '\n';
