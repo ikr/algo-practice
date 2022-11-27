b5628849c428e30e4d13b37f61eb3cb8f68706ab
Ivan Krechetov <ikr@ikr.li>
2022-11-27 17:32:46 +0100

    Solve part 2

--- adventofcode/2019/02_1202_Program_Alarm/solution.cpp
+++ adventofcode/2019/02_1202_Program_Alarm/solution.cpp
@@ -13,9 +13,10 @@ template <typename T> constexpr int inof(const T x) {
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-int value_at_address_zero_after_program_ends(vector<int> xs) {
-    xs[1] = 12;
-    xs[2] = 2;
+int value_at_address_zero_after_program_ends(vector<int> xs, const int noun,
+                                             const int verb) {
+    xs[1] = noun;
+    xs[2] = verb;
 
     for (int i = 0; i < sz(xs);) {
         const auto op = xs[i++];
@@ -37,6 +38,20 @@ int value_at_address_zero_after_program_ends(vector<int> xs) {
     return xs[0];
 }
 
+pair<int, int> find_proper_input(const vector<int> &xs, const int output) {
+    for (int noun{}; noun <= 99; ++noun) {
+        for (int verb{}; verb <= 99; ++verb) {
+            const auto result =
+                value_at_address_zero_after_program_ends(xs, noun, verb);
+            if (result == output) {
+                return {noun, verb};
+            }
+        }
+    }
+
+    return {-1, -1};
+}
+
 int integer_value(const string &x) { return stoi(x); }
 
 int main() {
@@ -47,6 +62,7 @@ int main() {
     vector<int> xs(sz(tokens));
     transform(cbegin(tokens), cend(tokens), begin(xs), integer_value);
 
-    cout << value_at_address_zero_after_program_ends(move(xs)) << '\n';
+    const auto [noun, verb] = find_proper_input(xs, 19690720);
+    cout << (100 * noun + verb) << '\n';
     return 0;
 }
