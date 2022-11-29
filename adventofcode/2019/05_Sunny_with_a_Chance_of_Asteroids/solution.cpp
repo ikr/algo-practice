0d0fa836b73d42db6670e96bb4b1176fa757f36f
Ivan Krechetov <ikr@ikr.su>
2022-11-29 07:47:12 +0100

    Solve part 2

--- adventofcode/2019/05_Sunny_with_a_Chance_of_Asteroids/solution.cpp
+++ adventofcode/2019/05_Sunny_with_a_Chance_of_Asteroids/solution.cpp
@@ -1,7 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-enum class Opcode { ADD = 1, MUL = 2, INP = 3, OUT = 4, HLT = 99 };
+enum class Opcode {
+    ADD = 1,
+    MUL = 2,
+    INP = 3,
+    OUT = 4,
+    JPT = 5,
+    JPF = 6,
+    LSS = 7,
+    EQL = 8,
+    HLT = 99
+};
+
 enum class Mode { POS = 0, IMM = 1 };
 
 template <typename T> constexpr int inof(const T x) {
@@ -21,47 +32,57 @@ tuple<Opcode, Mode, Mode> parse_op(const int op) {
             static_cast<Mode>((op / 1000) % 10)};
 }
 
-int input() { return 1; }
+int input() { return 5; }
 void output(const int x) { cout << x << '\n'; }
 
 void intcode_run(vector<int> xs) {
+    const auto deref = [&xs](const Mode m, const int p) -> int {
+        return m == Mode::POS ? xs[p] : p;
+    };
+
     for (int i = 0; i < sz(xs);) {
         const auto [oc, m1, m2] = parse_op(xs[i++]);
-
-        if (oc == Opcode::HLT) break;
-
-        if (oc == Opcode::INP) {
-            xs[xs[i++]] = input();
-            continue;
-        }
-
-        if (oc == Opcode::OUT) {
-            const auto p1 = xs[i++];
-            output(m1 == Mode::POS ? xs[p1] : p1);
-            continue;
-        }
 
         if (oc == Opcode::ADD) {
             const auto p1 = xs[i++];
             const auto p2 = xs[i++];
             const auto p3 = xs[i++];
-
-            xs[p3] = (m1 == Mode::POS ? xs[p1] : p1) +
-                     (m2 == Mode::POS ? xs[p2] : p2);
-            continue;
-        }
-
-        if (oc == Opcode::MUL) {
+            xs[p3] = deref(m1, p1) + deref(m2, p2);
+        } else if (oc == Opcode::MUL) {
             const auto p1 = xs[i++];
             const auto p2 = xs[i++];
             const auto p3 = xs[i++];
-
-            xs[p3] = (m1 == Mode::POS ? xs[p1] : p1) *
-                     (m2 == Mode::POS ? xs[p2] : p2);
+            xs[p3] = deref(m1, p1) * deref(m2, p2);
+        } else if (oc == Opcode::INP) {
+            xs[xs[i++]] = input();
+        } else if (oc == Opcode::OUT) {
+            const auto p1 = xs[i++];
+            output(deref(m1, p1));
             continue;
+        } else if (oc == Opcode::JPT) {
+            const auto p1 = xs[i++];
+            const auto p2 = xs[i++];
+            if (deref(m1, p1)) i = deref(m2, p2);
+            continue;
+        } else if (oc == Opcode::JPF) {
+            const auto p1 = xs[i++];
+            const auto p2 = xs[i++];
+            if (!deref(m1, p1)) i = deref(m2, p2);
+        } else if (oc == Opcode::LSS) {
+            const auto p1 = xs[i++];
+            const auto p2 = xs[i++];
+            const auto p3 = xs[i++];
+            xs[p3] = deref(m1, p1) < deref(m2, p2);
+        } else if (oc == Opcode::EQL) {
+            const auto p1 = xs[i++];
+            const auto p2 = xs[i++];
+            const auto p3 = xs[i++];
+            xs[p3] = deref(m1, p1) == deref(m2, p2);
+        } else if (oc == Opcode::HLT) {
+            break;
+        } else {
+            assert(false && "Invalid opcode");
         }
-
-        assert(false && "Invalid opcode");
     }
 }
 
