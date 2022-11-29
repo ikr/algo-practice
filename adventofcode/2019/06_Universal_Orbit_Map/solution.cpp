2048d3a1accf66140615855ee3566e043a3116b7
Ivan Krechetov <ikr@ikr.li>
2022-11-29 20:58:00 +0100

    Solve part 2

--- adventofcode/2019/06_Universal_Orbit_Map/solution.cpp
+++ adventofcode/2019/06_Universal_Orbit_Map/solution.cpp
@@ -1,63 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using Graph = map<string, string>;
-
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-vector<string> toposort(const vector<string> &vertices, const Graph &follows) {
+vector<string> ladder_up(const map<string, string> &parent,
+                         const string &from) {
     vector<string> result;
-    set<string> done;
 
-    const auto recur = [&](const auto self, const string &u) -> void {
-        done.insert(u);
-
-        if (follows.contains(u) && !done.contains(follows.at(u))) {
-            self(self, follows.at(u));
-        }
-
-        result.push_back(u);
-    };
-
-    for (int i = 0; i < sz(vertices); ++i) {
-        if (done.contains(vertices[i])) continue;
-        recur(recur, vertices[i]);
+    auto cur = from;
+    while (parent.contains(cur)) {
+        result.push_back(parent.at(cur));
+        cur = parent.at(cur);
     }
 
+    reverse(begin(result), end(result));
     return result;
 }
 
 int main() {
-    Graph follows;
-    set<string> vertices;
+    map<string, string> parent;
 
     for (string line; getline(cin, line);) {
         const auto sep = line.find(')');
         const auto u = line.substr(0, sep);
         const auto v = line.substr(sep + 1);
-        follows.emplace(v, u);
-        vertices.insert(u);
-        vertices.insert(v);
+        parent.emplace(v, u);
     }
 
-    const auto order =
-        toposort(vector(cbegin(vertices), cend(vertices)), follows);
+    const auto you = ladder_up(parent, "YOU");
+    const auto san = ladder_up(parent, "SAN");
 
-    int result{};
-    map<string, int> orbits;
-
-    for (const auto &u : order) {
-        if (follows.contains(u)) {
-            const auto v = follows.at(u);
-            orbits[u] = orbits[v] + 1;
-            result += orbits.at(u);
-        }
-    }
-
+    int k{};
+    while (you[k] == san[k]) ++k;
+    const auto result = sz(you) - k + sz(san) - k;
     cout << result << '\n';
     return 0;
 }
