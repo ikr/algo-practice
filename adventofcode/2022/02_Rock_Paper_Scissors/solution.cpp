6b4829e33b81540e0a07b9ba7f13dd2d6a5aec3e
Ivan Krechetov <ikr@ikr.li>
2022-12-02 06:22:37 +0100

    Solve part 2

--- adventofcode/2022/02_Rock_Paper_Scissors/solution.cpp
+++ adventofcode/2022/02_Rock_Paper_Scissors/solution.cpp
@@ -3,27 +3,24 @@ using namespace std;
 
 using pii = pair<int, int>;
 
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+
 int my_total_score(const vector<pii> &plays) {
     return transform_reduce(cbegin(plays), cend(plays), 0, plus<int>{},
-                            [](const auto p1p2) {
-                                const auto [p1, p2] = p1p2;
-                                if (p1 == p2) {
-                                    return 3 + p2 + 1;
+                            [](const auto p) {
+                                const auto [opp, out] = p;
+
+                                if (out == 1) {
+                                    return 3 + opp + 1;
                                 }
 
-                                if (p1 == 0 && p2 == 2) {
-                                    return 0 + p2 + 1;
+                                if (out == 0) {
+                                    return 0 + ((opp - 1 + 3) % 3) + 1;
                                 }
 
-                                if (p2 == 0 && p1 == 2) {
-                                    return 6 + p2 + 1;
-                                }
-
-                                if (p1 + 1 == p2) {
-                                    return 6 + p2 + 1;
-                                }
-
-                                return 0 + p2 + 1;
+                                return 6 + ((opp + 1) % 3) + 1;
                             });
 }
 
