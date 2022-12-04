4524c35ae5f066a50a001ee10f121130dd5a2d97
Ivan Krechetov <ikr@ikr.su>
2022-12-04 22:59:58 +0100

    Solve part 1

---
+++ adventofcode/2022/04_Camp_Cleanup/solution.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using pii = pair<int, int>;
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+vector<string> split(const string &delim_regex, const string &s) {
+    regex r(delim_regex);
+    return vector<string>(sregex_token_iterator(cbegin(s), cend(s), r, -1),
+                          sregex_token_iterator{});
+}
+
+pii parse_range(const string &src) {
+    const auto ab = split("-", src);
+    assert(sz(ab) == 2);
+    return {stoi(ab[0]), stoi(ab[1])};
+}
+
+bool contains(const pii ab, const pii cd) {
+    const auto [a, b] = ab;
+    assert(a <= b);
+    const auto [c, d] = cd;
+    assert(c <= d);
+    return a <= c && d <= b;
+}
+
+int main() {
+    int result{};
+
+    for (string line; getline(cin, line);) {
+        const auto pairs = split(",", line);
+        assert(sz(pairs) == 2);
+
+        const auto ab = parse_range(pairs[0]);
+        const auto cd = parse_range(pairs[1]);
+        result += contains(ab, cd) || contains(cd, ab);
+    }
+
+    cout << result << '\n';
+    return 0;
+}
