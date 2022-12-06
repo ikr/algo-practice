04840544bd5dea755ccff80b79d1ccbc68e9c127
Ivan Krechetov <ikr@ikr.su>
2022-12-06 06:10:30 +0100

    Solve part 2

--- adventofcode/2022/06_Tuning_Trouble/solution.cpp
+++ adventofcode/2022/06_Tuning_Trouble/solution.cpp
@@ -10,13 +10,13 @@ template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 int uniques(const deque<int> &xs) { return sz(set(cbegin(xs), cend(xs))); }
 
 int solution(const string &xs) {
-    deque<int> q(cbegin(xs), cbegin(xs) + 4);
-    if (uniques(q) == 4) return 4;
+    deque<int> q(cbegin(xs), cbegin(xs) + 14);
+    if (uniques(q) == 14) return 14;
 
-    for (int i = 4; i < sz(xs); ++i) {
+    for (int i = 14; i < sz(xs); ++i) {
         q.pop_front();
         q.push_back(xs[i]);
-        if (uniques(q) == 4) return i + 1;
+        if (uniques(q) == 14) return i + 1;
     }
 
     return 0;
