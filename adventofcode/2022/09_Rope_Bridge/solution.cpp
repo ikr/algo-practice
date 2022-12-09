ef97db3d1c3b1647bb6a5d482d623d77af20c2d1
Ivan Krechetov <ikr@ikr.li>
2022-12-09 17:45:00 +0100

    Clean up

--- adventofcode/2022/09_Rope_Bridge/solution.cpp
+++ adventofcode/2022/09_Rope_Bridge/solution.cpp
@@ -68,46 +68,6 @@ constexpr pii new_tail_coord(const pii moved_h, const pii t) {
     return t;
 }
 
-vector<string> visualize(const array<pii, ROPE_SIZE> &rope) {
-    const int H{21};
-    const int W{26};
-    const int ORO = 16;
-    const int OCO = 12;
-    vector<string> grid(H, string(W, '.'));
-
-    grid[ORO - 1][OCO - 1] = 's';
-
-    for (int i = sz(rope) - 1; i >= 1; --i) {
-        grid[rope[i].first + ORO - 1][rope[i].second + OCO - 1] =
-            static_cast<char>(inof('0') + i);
-    }
-    grid[rope[0].first + ORO - 1][rope[0].second + OCO - 1] = 'H';
-
-    return grid;
-}
-
-void dbg(const vector<string> &grid) {
-    for (const auto &row : grid) cerr << row << '\n';
-    cerr << endl;
-}
-
-template <typename T1, typename T2>
-ostream &operator<<(ostream &os, const pair<T1, T2> &x) {
-    os << '(' << x.first << ' ' << x.second << ')';
-    return os;
-}
-
-template <typename T, size_t N>
-ostream &operator<<(ostream &os, const array<T, N> &xs) {
-    os << '[';
-    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
-        if (i != xs.cbegin()) os << ' ';
-        os << *i;
-    }
-    os << ']';
-    return os;
-}
-
 int tail_areal_size(const vector<Cmd> &commands) {
     array<pii, ROPE_SIZE> rope;
     rope.fill({0, 0});
@@ -125,11 +85,8 @@ int tail_areal_size(const vector<Cmd> &commands) {
 
             tail_areal.insert(rope.back());
         }
-
-        // dbg(visualize(rope));
     }
 
-    // cerr << rope << endl;
     return sz(tail_areal);
 }
 
