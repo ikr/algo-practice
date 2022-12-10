a67bd8fac0b1d42d57301c5cd97c4b8837b6235a
Ivan Krechetov <ikr@ikr.su>
2022-12-10 09:05:27 +0100

    Clean up

--- adventofcode/2022/10_Cathode-Ray_Tube/solution.cpp
+++ adventofcode/2022/10_Cathode-Ray_Tube/solution.cpp
@@ -1,12 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T> constexpr int inof(const T x) {
-    return static_cast<int>(x);
-}
-
-template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
-
 int main() {
     vector<int> xs{};
     int carry = 1;
