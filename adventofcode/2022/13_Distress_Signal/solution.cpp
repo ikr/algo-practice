1df854d302f6f27f88162a62a9092dfdf39239ac
Ivan Krechetov <ikr@ikr.li>
2022-12-13 09:20:51 +0100

    Solve part 1

--- adventofcode/2022/13_Distress_Signal/solution.cpp
+++ adventofcode/2022/13_Distress_Signal/solution.cpp
@@ -25,10 +25,14 @@ bool equal(const pair<Iter, Iter> suf_a, const pair<Iter, Iter> suf_b) {
         if (x != y) return false;
     } else if (cur_a->is_number()) {
         const auto a_ = Json::array({cur_a->get<Json::number_integer_t>()});
-        if (!equal({cbegin(a_), cend(a_)}, suf_b)) return false;
+        if (!equal({cbegin(a_), cend(a_)}, {cur_b->cbegin(), cur_b->cend()})) {
+            return false;
+        }
     } else if (cur_b->is_number()) {
         const auto b_ = Json::array({cur_b->get<Json::number_integer_t>()});
-        if (!equal(suf_a, {cbegin(b_), cend(b_)})) return false;
+        if (!equal({cur_a->cbegin(), cur_a->cend()}, {cbegin(b_), cend(b_)})) {
+            return false;
+        }
     } else {
         assert(cur_a->is_array() && cur_b->is_array());
         if (!equal({cur_a->cbegin(), cur_a->cend()},
@@ -50,25 +54,36 @@ bool less_than(const pair<Iter, Iter> suf_a, const pair<Iter, Iter> suf_b) {
     if (cur_a->is_number() && cur_b->is_number()) {
         const auto x = cur_a->get<Json::number_integer_t>();
         const auto y = cur_b->get<Json::number_integer_t>();
+        if (x < y) return true;
         if (x > y) return false;
     } else if (cur_a->is_number()) {
         const auto a_ = Json::array({cur_a->get<Json::number_integer_t>()});
+        if (less_than({cbegin(a_), cend(a_)},
+                      {cur_b->cbegin(), cur_b->cend()})) {
+            return true;
+        }
 
-        if (!less_than({cbegin(a_), cend(a_)}, suf_b) &&
-            !equal({cbegin(a_), cend(a_)}, suf_b)) {
+        if (!equal({cbegin(a_), cend(a_)}, {cur_b->cbegin(), cur_b->cend()})) {
             return false;
         }
     } else if (cur_b->is_number()) {
         const auto b_ = Json::array({cur_b->get<Json::number_integer_t>()});
-        if (!less_than(suf_a, {cbegin(b_), cend(b_)}) &&
-            !equal(suf_a, {cbegin(b_), cend(b_)})) {
+        if (less_than({cur_a->cbegin(), cur_a->cend()},
+                      {cbegin(b_), cend(b_)})) {
+            return true;
+        }
+
+        if (!equal({cur_a->cbegin(), cur_a->cend()}, {cbegin(b_), cend(b_)})) {
             return false;
         }
     } else {
         assert(cur_a->is_array() && cur_b->is_array());
-        if (!less_than({cur_a->cbegin(), cur_a->cend()},
-                       {cur_b->cbegin(), cur_b->cend()}) &&
-            !equal({cur_a->cbegin(), cur_a->cend()},
+        if (less_than({cur_a->cbegin(), cur_a->cend()},
+                      {cur_b->cbegin(), cur_b->cend()})) {
+            return true;
+        }
+
+        if (!equal({cur_a->cbegin(), cur_a->cend()},
                    {cur_b->cbegin(), cur_b->cend()})) {
             return false;
         }
@@ -105,7 +120,62 @@ int main() {
         if (!getline(cin, line_b)) break;
     }
 
-    cerr << "Read " << sz(ps) << " pairs" << endl;
+    {
+        const auto a = Json::parse("[1,1,3,1,1]");
+        const auto b = Json::parse("[1,1,5,1,1]");
+        assert(!equal({cbegin(a), cend(a)}, {cbegin(b), cend(b)}));
+        assert(less_than({cbegin(a), cend(a)}, {cbegin(b), cend(b)}));
+    }
+
+    {
+        const auto a = Json::parse("[[1],[2,3,4]]");
+        const auto b = Json::parse("[[1],4]");
+        assert(!equal({cbegin(a), cend(a)}, {cbegin(b), cend(b)}));
+        assert(less_than({cbegin(a), cend(a)}, {cbegin(b), cend(b)}));
+    }
+
+    {
+        const auto a = Json::parse("[9]");
+        const auto b = Json::parse("[[8,7,6]]");
+        assert(!equal({cbegin(a), cend(a)}, {cbegin(b), cend(b)}));
+        assert(!less_than({cbegin(a), cend(a)}, {cbegin(b), cend(b)}));
+    }
+
+    {
+        const auto a = Json::parse("[[4,4],4,4]");
+        const auto b = Json::parse("[[4,4],4,4,4]");
+        assert(!equal({cbegin(a), cend(a)}, {cbegin(b), cend(b)}));
+        assert(less_than({cbegin(a), cend(a)}, {cbegin(b), cend(b)}));
+    }
+
+    {
+        const auto a = Json::parse("[7,7,7,7]");
+        const auto b = Json::parse("[7,7,7]");
+        assert(!equal({cbegin(a), cend(a)}, {cbegin(b), cend(b)}));
+        assert(!less_than({cbegin(a), cend(a)}, {cbegin(b), cend(b)}));
+    }
+
+    {
+        const auto a = Json::parse("[]");
+        const auto b = Json::parse("[3]");
+        assert(!equal({cbegin(a), cend(a)}, {cbegin(b), cend(b)}));
+        assert(less_than({cbegin(a), cend(a)}, {cbegin(b), cend(b)}));
+    }
+
+    {
+        const auto a = Json::parse("[[[]]]");
+        const auto b = Json::parse("[[]]");
+        assert(!equal({cbegin(a), cend(a)}, {cbegin(b), cend(b)}));
+        assert(!less_than({cbegin(a), cend(a)}, {cbegin(b), cend(b)}));
+    }
+
+    {
+        const auto a = Json::parse("[1,[2,[3,[4,[5,6,7]]]],8,9]");
+        const auto b = Json::parse("[1,[2,[3,[4,[5,6,0]]]],8,9]");
+        assert(!equal({cbegin(a), cend(a)}, {cbegin(b), cend(b)}));
+        assert(!less_than({cbegin(a), cend(a)}, {cbegin(b), cend(b)}));
+    }
+
     cout << pairs_in_right_order_positions_sum(ps) << '\n';
     return 0;
 }
