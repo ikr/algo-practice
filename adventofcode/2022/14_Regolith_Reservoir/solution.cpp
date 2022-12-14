69a0f3bd6db577fabf1629fe588b39782c50d5ae
Ivan Krechetov <ikr@ikr.su>
2022-12-14 06:12:48 +0100

    Bootstrap

---
+++ adventofcode/2022/14_Regolith_Reservoir/solution.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using pii = pair<int, int>;
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+template <typename T>
+constexpr pair<T, T> operator+(const pair<T, T> a, const pair<T, T> b) {
+    return {a.first + b.first, a.second + b.second};
+}
+
+vector<string> split(const string &delim_regex, const string &s) {
+    regex r(delim_regex);
+    return vector<string>(sregex_token_iterator(cbegin(s), cend(s), r, -1),
+                          sregex_token_iterator{});
+}
+
+int main() {
+    for (string line; getline(cin, line);) {
+    }
+
+    return 0;
+}
