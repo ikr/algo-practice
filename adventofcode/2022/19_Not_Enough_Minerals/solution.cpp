06e2d583712ba88d6a916363b837cf8845622994
Ivan Krechetov <ikr@ikr.su>
2022-12-19 20:36:03 +0100

    Bootstrap

---
+++ adventofcode/2022/19_Not_Enough_Minerals/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+int main() {
+    for (string line; getline(cin, line);) {
+    }
+
+    return 0;
+}
