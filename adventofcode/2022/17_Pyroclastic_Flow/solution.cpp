2846b68abd1b03d81c2e309352aad9a7ed559fa5
Ivan Krechetov <ikr@ikr.su>
2022-12-19 18:38:14 +0100

    First draft for the part 2

--- adventofcode/2022/17_Pyroclastic_Flow/solution.cpp
+++ adventofcode/2022/17_Pyroclastic_Flow/solution.cpp
@@ -64,7 +64,8 @@ bool collides(const Rock &rock, const Chamber &chamber) {
 
 int height(const Chamber &chamber) { return crbegin(chamber)->first + 1; }
 
-Chamber simulate(const string &mvs, const int num_rocks) {
+vector<int> simulate_return_deltas(const string &mvs, const int num_rocks) {
+    vector<int> result(num_rocks, -1);
     auto chamber = initial_chamber();
 
     for (int i_rock = 0, i_shape = 0, i_mv = 0; i_rock < num_rocks;
@@ -81,24 +82,15 @@ Chamber simulate(const string &mvs, const int num_rocks) {
             {
                 auto cur_ = move_by(cur, {-1, 0});
                 if (collides(cur_, chamber)) {
+                    const auto h_old = height(chamber);
                     chamber.insert(cbegin(cur), cend(cur));
+                    result[i_rock] = height(chamber) - h_old;
                     break;
                 } else {
                     swap(cur_, cur);
                 }
             }
         }
-    }
-
-    return chamber;
-}
-
-auto display(const Chamber &chamber) {
-    string result(height(chamber), chof(0));
-
-    for (const auto &[ro, co] : chamber) {
-        if (ro < 0) continue;
-        result[ro] = chof(inof(result[ro]) | (1 << co));
     }
 
     return result;
@@ -114,13 +106,47 @@ template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
     return os;
 }
 
+bool is_at(const vector<int> &haystack, const vector<int> &needle,
+           const int i) {
+    for (int j = 0; j < sz(needle); ++j) {
+        if (haystack[i + j] != needle[j]) return false;
+    }
+
+    return true;
+}
+
+int tail_period(const vector<int> &xs) {
+    const auto marker_size = 16;
+    const vector marker(cend(xs) - marker_size, cend(xs));
+    assert(is_at(xs, marker, sz(xs) - marker_size));
+
+    for (int i = sz(xs) - marker_size - 1; i >= 0; --i) {
+        if (is_at(xs, marker, i)) {
+            return sz(xs) - marker_size - i + 1;
+        }
+    }
+
+    return -1;
+}
+
 int main() {
     string mvs;
     cin >> mvs;
 
-    const auto LIM = 100000;
-    const auto chamber = simulate(mvs, LIM);
+    const auto LIM = 30000;
+    const auto ds = simulate_return_deltas(mvs, LIM);
 
-    cerr << display(chamber) << endl;
+    const ll p = tail_period(ds);
+    const vector period_elements(cend(ds) - p, cend(ds));
+    cerr << "Period elements " << period_elements << endl;
+
+    const auto r = 1000000000000LL - LIM;
+    const ll result = accumulate(cbegin(ds), cend(ds), 0LL, plus<ll>{}) +
+                      (r / p) * accumulate(cbegin(period_elements),
+                                           cend(period_elements), 0) +
+                      accumulate(cbegin(period_elements),
+                                 cbegin(period_elements) + inof(r % p), 0);
+
+    cout << result << '\n';
     return 0;
 }
