f21830bcc86bfd92196b16d5aa9f9990f5ffd07e
Ivan Krechetov <ikr@ikr.su>
2022-12-27 21:19:43 +0100

    Tokenize the program source

--- adventofcode/2022/22_Monkey_Map/solution.cpp
+++ adventofcode/2022/22_Monkey_Map/solution.cpp
@@ -1,11 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
+
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+vector<string> tokenize_command_source(const string &src) {
+    const regex pattern("[0-9]+|L|R");
+    vector<string> result;
+    copy(sregex_token_iterator(cbegin(src), cend(src), pattern),
+         sregex_token_iterator(), back_inserter(result));
+    return result;
+}
 
 int main() {
     vector<string> grid;
@@ -19,6 +37,6 @@ int main() {
     string command_source;
     cin >> command_source;
 
-    cerr << command_source << endl;
+    cerr << tokenize_command_source(command_source) << endl;
     return 0;
 }
