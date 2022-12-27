3b8b1e21f3d346a37cb49caa9523f971ca2db1d1
Ivan Krechetov <ikr@ikr.li>
2022-12-27 21:52:18 +0100

    Elaborate

--- adventofcode/2022/22_Monkey_Map/solution.cpp
+++ adventofcode/2022/22_Monkey_Map/solution.cpp
@@ -1,6 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using Coord = pair<int, int>;
+enum class Facing { RIGHT = 0, DOWN = 1, LEFT = 2, UP = 3 };
+
+struct State final {
+    Coord rc;
+    Facing f;
+};
+
+template <typename T>
+constexpr pair<T, T> operator+(const pair<T, T> a, const pair<T, T> b) {
+    return {a.first + b.first, a.second + b.second};
+}
+
+constexpr int RO(const Coord rc) { return rc.first; }
+constexpr int CO(const Coord rc) { return rc.second; }
+
 template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
     os << '[';
     for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
@@ -17,12 +33,22 @@ template <typename T> constexpr int inof(const T x) {
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
+constexpr int final_password(const State &st) {
+    const auto [rc, f] = st;
+    return 1000 * (RO(rc) + 1) + 4 * (CO(rc) + 1) + inof(f);
+}
+
 vector<string> tokenize_command_source(const string &src) {
     const regex pattern("[0-9]+|L|R");
     vector<string> result;
     copy(sregex_token_iterator(cbegin(src), cend(src), pattern),
          sregex_token_iterator(), back_inserter(result));
     return result;
+}
+
+State route(const vector<string> &grid, const vector<string> &commands,
+            State st) {
+    return st;
 }
 
 int main() {
@@ -32,11 +58,11 @@ int main() {
         grid.push_back(line);
     }
 
-    for (const auto &row : grid) cerr << row << endl;
-
     string command_source;
     cin >> command_source;
 
-    cerr << tokenize_command_source(command_source) << endl;
+    cout << final_password(route(grid, tokenize_command_source(command_source),
+                                 State{Coord{0, 0}, Facing::RIGHT}))
+         << '\n';
     return 0;
 }
