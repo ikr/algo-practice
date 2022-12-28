49fd8091d6926f5fb87c92e446ed61f07a50ed9e
Ivan Krechetov <ikr@ikr.su>
2022-12-28 09:10:15 +0100

    Implement horz_spreads

--- adventofcode/2022/22_Monkey_Map/solution.cpp
+++ adventofcode/2022/22_Monkey_Map/solution.cpp
@@ -1,7 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &x) {
+    os << '(' << x.first << ' ' << x.second << ')';
+    return os;
+}
+
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
+
 using Coord = pair<int, int>;
+using Seg = pair<int, int>;
 enum class Facing { RIGHT = 0, DOWN = 1, LEFT = 2, UP = 3 };
 
 struct State final {
@@ -16,16 +33,6 @@ constexpr pair<T, T> operator+(const pair<T, T> a, const pair<T, T> b) {
 
 constexpr int RO(const Coord rc) { return rc.first; }
 constexpr int CO(const Coord rc) { return rc.second; }
-
-template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
-    os << '[';
-    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
-        if (i != xs.cbegin()) os << ' ';
-        os << *i;
-    }
-    os << ']';
-    return os;
-}
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
@@ -46,8 +53,49 @@ vector<string> tokenize_command_source(const string &src) {
     return result;
 }
 
+char at(const vector<string> &grid, const Coord &rc) {
+    const auto H = sz(grid);
+    if (RO(rc) < 0 || H <= RO(rc)) return ' ';
+
+    const auto W = sz(grid[RO(rc)]);
+    if (CO(rc) < 0 || W <= CO(rc)) return ' ';
+
+    return grid[RO(rc)][CO(rc)];
+}
+
+int max_width(const vector<string> &grid) {
+    return transform_reduce(
+        cbegin(grid), cend(grid), 0,
+        [](const int a, const int b) { return max(a, b); },
+        [](const string &s) { return sz(s); });
+}
+
+vector<Seg> horz_spreads(const vector<string> &grid) {
+    const auto H = sz(grid);
+    const auto W = max_width(grid);
+    vector<Seg> result(H, {-1, -1});
+
+    for (int ro = 0; ro < H; ++ro) {
+        auto &a = result[ro].first;
+        auto &b = result[ro].second;
+
+        for (int co = 0; co < W; ++co) {
+            if (at(grid, {ro, co}) == ' ') {
+                if (a != -1 && b == -1) b = co - 1;
+            } else {
+                if (a == -1) a = co;
+            }
+        }
+
+        if (b == -1) b = W - 1;
+    }
+
+    return result;
+}
+
 State route(const vector<string> &grid, const vector<string> &commands,
             State st) {
+    cerr << horz_spreads(grid) << endl;
     return st;
 }
 
