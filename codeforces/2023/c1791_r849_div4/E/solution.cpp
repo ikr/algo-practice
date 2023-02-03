b90d9a2d99f671141fe5515e75acd8a17c8f8224
Ivan Krechetov <ikr@ikr.su>
2023-02-03 17:26:12 +0100

    AC on F

--- codeforces/2023/c1791_r849_div4/E/solution.cpp
+++ codeforces/2023/c1791_r849_div4/E/solution.cpp
@@ -1,6 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &x) {
+    os << '(' << x.first << ' ' << x.second << ')';
+    return os;
+}
+
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
+
 using ll = long long;
 using pll = pair<ll, ll>;
 
@@ -14,33 +30,36 @@ ll max_sum(const vector<ll> &xs) {
     vector<pll> D0(sz(xs)); // prev kept intact
     vector<pll> D1(sz(xs)); // swapped with prev
 
-    D0[0] = {xs[0], xs[0]}; // (s b) — max sum s, last element b
-    D1[0] = {xs[0], xs[0]};
+    D0[0] = {0, xs[0]}; // (s b) — max sum s before, last element b
+    D1[0] = {0, xs[0]};
 
     for (int i = 1; i < sz(xs); ++i) {
         {
             const auto [s0, b0] = D0[i - 1];
-            const auto [s1, b1] = D0[i - 1];
+            const auto [s1, b1] = D1[i - 1];
 
-            D0[i] = pll{(s0 >= s1 ? s0 : s1) + xs[i], xs[i]};
+            D0[i] =
+                pll{((s0 + b0) >= (s1 + b1) ? (s0 + b0) : (s1 + b1)), xs[i]};
         }
 
         D1[i] = [&]() -> pll {
             const auto [s0, b0] = D0[i - 1];
-            const auto [s1, b1] = D0[i - 1];
+            const auto [s1, b1] = D1[i - 1];
 
-            const auto sa = s0 - b0;
-            const auto sb = s1 - b1;
+            const auto sa = s0 - b0 - xs[i];
+            const auto sb = s1 - b1 - xs[i];
 
             if (sa >= sb) {
-                return {sa - xs[i], -b0};
+                return {s0 - xs[i], -b0};
             } else {
-                return {sb - xs[i], -b1};
+                return {s1 - xs[i], -b1};
             }
         }();
     }
 
-    return max(D0.back().first, D1.back().first);
+    // cerr << D0 << '\n' << D1 << endl;
+    return max(D0.back().first + D0.back().second,
+               D1.back().first + D1.back().second);
 }
 
 int main() {
