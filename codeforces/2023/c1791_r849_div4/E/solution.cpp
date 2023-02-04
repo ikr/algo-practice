2891c288e9894c3e671f00f3852667bbe4998284
Ivan Krechetov <ikr@ikr.su>
2023-02-04 09:22:05 +0100

    AC

--- codeforces/2023/c1791_r849_div4/E/solution.cpp
+++ codeforces/2023/c1791_r849_div4/E/solution.cpp
@@ -1,24 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T1, typename T2>
-ostream &operator<<(ostream &os, const pair<T1, T2> &x) {
-    os << '(' << x.first << ' ' << x.second << ')';
-    return os;
-}
-
-template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
-    os << '[';
-    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
-        if (i != xs.cbegin()) os << ' ';
-        os << *i;
-    }
-    os << ']';
-    return os;
-}
-
 using ll = long long;
-using pll = pair<ll, ll>;
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
@@ -27,39 +10,30 @@ template <typename T> constexpr int inof(const T x) {
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
 ll max_sum(const vector<ll> &xs) {
-    vector<pll> D0(sz(xs)); // prev kept intact
-    vector<pll> D1(sz(xs)); // swapped with prev
+    priority_queue<ll, vector<ll>, greater<ll>> pq(cbegin(xs), cend(xs));
 
-    D0[0] = {0, xs[0]}; // (s b) — max sum s before, last element b
-    D1[0] = {0, xs[0]};
+    for (;;) {
+        const auto a = pq.top();
+        pq.pop();
+        const auto b = pq.top();
+        pq.pop();
 
-    for (int i = 1; i < sz(xs); ++i) {
-        {
-            const auto [s0, b0] = D0[i - 1];
-            const auto [s1, b1] = D1[i - 1];
-
-            D0[i] =
-                pll{((s0 + b0) >= (s1 + b1) ? (s0 + b0) : (s1 + b1)), xs[i]};
+        if (a + b < -a - b) {
+            pq.push(-a);
+            pq.push(-b);
+        } else {
+            pq.push(a);
+            pq.push(b);
+            break;
         }
-
-        D1[i] = [&]() -> pll {
-            const auto [s0, b0] = D0[i - 1];
-            const auto [s1, b1] = D1[i - 1];
-
-            const auto sa = s0 - b0 - xs[i];
-            const auto sb = s1 - b1 - xs[i];
-
-            if (sa >= sb) {
-                return {s0 - xs[i], -b0};
-            } else {
-                return {s1 - xs[i], -b1};
-            }
-        }();
     }
 
-    // cerr << D0 << '\n' << D1 << endl;
-    return max(D0.back().first + D0.back().second,
-               D1.back().first + D1.back().second);
+    ll ans{};
+    while (!pq.empty()) {
+        ans += pq.top();
+        pq.pop();
+    }
+    return ans;
 }
 
 int main() {
