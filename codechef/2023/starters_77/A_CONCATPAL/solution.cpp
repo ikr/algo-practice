da29249d94f198a0dbfc1a1a46635a244e84d903
Ivan Krechetov <ikr@ikr.su>
2023-02-15 16:05:06 +0100

    AC

--- codechef/2023/starters_77/A_CONCATPAL/solution.cpp
+++ codechef/2023/starters_77/A_CONCATPAL/solution.cpp
@@ -9,6 +9,44 @@ template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
 static constexpr int AZ = 26;
 
+array<int, AZ> gather_freqs(const string &xs) {
+    array<int, AZ> fs;
+    fs.fill(0);
+    for (const auto &c : xs) {
+        const auto x = inof(c) - inof('a');
+        ++fs[x];
+    }
+    return fs;
+}
+
+template <typename T, size_t N>
+constexpr array<T, N> operator-(array<T, N> a, const array<T, N> &b) {
+    transform(cbegin(a), cend(a), cbegin(b), begin(a), minus<int>{});
+    return a;
+}
+
+string sorted(string xs) {
+    sort(begin(xs), end(xs));
+    return xs;
+}
+
+bool solve(string A, string B) {
+    if (sz(A) == sz(B)) return sorted(A) == sorted(B);
+    if (sz(A) > sz(B)) swap(A, B);
+
+    const auto fa = gather_freqs(A);
+    const auto fb = gather_freqs(B);
+
+    const auto df = fb - fa;
+    if (any_of(cbegin(df), cend(df), [](const int f) { return f < 0; })) {
+        return false;
+    }
+
+    const auto odds =
+        count_if(cbegin(df), cend(df), [](const int f) { return f % 2; });
+    return odds <= 1;
+}
+
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
@@ -25,21 +63,7 @@ int main() {
         assert(sz(A) == N);
         assert(sz(B) == M);
 
-        array<int, AZ> fs;
-        fs.fill(0);
-
-        for (const auto &xs : {A, B}) {
-            for (const auto &c : xs) {
-                const auto x = inof(c) - inof('a');
-                ++fs[x];
-            }
-        }
-
-        const auto odds =
-            count_if(cbegin(fs), cend(fs), [](const int f) { return f % 2; });
-
-        const auto yes = (odds <= 1);
-        cout << (yes ? "YES" : "NO") << '\n';
+        cout << (solve(A, B) ? "YES" : "NO") << '\n';
     }
     return 0;
 }
