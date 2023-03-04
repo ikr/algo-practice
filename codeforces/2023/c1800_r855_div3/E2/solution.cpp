7f61275f5eed9cc66508b6cb3e285f33a03405ac
Ivan Krechetov <ikr@ikr.su>
2023-03-04 12:01:11 +0100

    Start over

--- codeforces/2023/c1800_r855_div3/E2/solution.cpp
+++ codeforces/2023/c1800_r855_div3/E2/solution.cpp
@@ -1,3 +1,4 @@
+#include <atcoder/dsu>
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -7,33 +8,41 @@ template <typename T> constexpr int inof(const T x) {
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-bool is_possible(string s, string t, const int k) {
-    const auto n = sz(s);
-    if (n <= k) return s == t;
+vector<vector<int>> index_components(const int n, const int k) {
+    const auto in_bounds = [&](const int i) { return 0 <= i && i < n; };
 
-    if (n == k + 1) {
-        if (s == t) return true;
-        swap(s[0], s.back());
-        return s == t;
+    atcoder::dsu uf(n);
+    for (int i = 0; i < n; ++i) {
+        for (const auto d : {k, -k, k + 1, -(k + 1)}) {
+            if (in_bounds(i + d)) uf.merge(i, i + d);
+        }
     }
 
-    const auto a = k - 1;
-    const auto b = n - 1 - (k - 1);
+    return uf.groups();
+}
 
-    if (a < b) {
-        sort(begin(s), end(s));
-        sort(begin(t), end(t));
-        return s == t;
+bool are_equal_at(const string &s, const string &t, const vector<int> &ii) {
+    return all_of(cbegin(ii), cend(ii),
+                  [&](const int i) { return s[i] == t[i]; });
+}
+
+void sort_indices(const string &xs, vector<int> &ii) {
+    stable_sort(begin(ii), end(ii), [&](const int i, const int j) {
+        return inof(xs[i]) < inof(xs[j]);
+    });
+}
+
+bool is_possible(const string &s, const string &t, const int k) {
+    auto cs = index_components(sz(s), k);
+    for (auto &ii : cs) {
+        auto jj = ii;
+        sort_indices(s, ii);
+        sort_indices(t, jj);
+
+        if (ii != jj) return false;
+        if (!are_equal_at(s, t, ii)) return false;
     }
-
-    const auto d = b - a + 1;
-    if (s.substr(a, d) != t.substr(b, d)) return false;
-
-    s.erase(a, d);
-    t.erase(b, d);
-    sort(begin(s), end(s));
-    sort(begin(t), end(t));
-    return s == t;
+    return true;
 }
 
 int main() {
@@ -52,7 +61,7 @@ int main() {
         assert(sz(s) == n);
         assert(sz(t) == n);
 
-        cout << (is_possible(move(s), move(t), k) ? "YES" : "NO") << '\n';
+        cout << (is_possible(s, t, k) ? "YES" : "NO") << '\n';
     }
 
     return 0;
