44d4b29b928672a6d39fe233b333b1bfb446d2ca
Ivan Krechetov <ikr@ikr.su>
2023-04-01 15:28:23 +0200

    Improving: only 18 WA-s remaining

--- atcoder/2023/abc296/D/solution.cpp
+++ atcoder/2023/abc296/D/solution.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 using ll = long long;
-using ulll = __uint128_t;
+using ull = unsigned long long;
 using lll = __int128_t;
 
 template <typename T> constexpr ll llof(const T x) {
@@ -14,32 +14,32 @@ template <typename T> constexpr lll lllof(const T x) {
 }
 
 namespace kactl { // https://github.com/kth-competitive-programming/kactl/blob/main/content/number-theory/Factor.h
-ulll modmul(ulll a, ulll b, ulll M) {
-    lll ret = a * b - M * ulll(1.L / M * a * b);
-    return ret + M * (ret < 0) - M * (ret >= (lll)M);
+ull modmul(ull a, ull b, ull M) {
+    ll ret = a * b - M * ull(1.L / M * a * b);
+    return ret + M * (ret < 0) - M * (ret >= (ll)M);
 }
-ulll modpow(ulll b, ulll e, ulll mod) {
-    ulll ans = 1;
+ull modpow(ull b, ull e, ull mod) {
+    ull ans = 1;
     for (; e; b = modmul(b, b, mod), e /= 2)
         if (e & 1) ans = modmul(ans, b, mod);
     return ans;
 }
 
-bool isPrime(ulll n) {
+bool isPrime(ull n) {
     if (n < 2 || n % 6 % 4 != 1) return (n | 1) == 3;
-    ulll A[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022},
-         s = __builtin_ctzll(n - 1), d = n >> s;
-    for (ulll a : A) { // ^ count trailing zeroes
-        ulll p = modpow(a % n, d, n), i = s;
+    ull A[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022},
+        s = __builtin_ctzll(n - 1), d = n >> s;
+    for (ull a : A) { // ^ count trailing zeroes
+        ull p = modpow(a % n, d, n), i = s;
         while (p != 1 && p != n - 1 && a % n && i--) p = modmul(p, p, n);
         if (p != n - 1 && i != s) return 0;
     }
     return 1;
 }
 
-ulll pollard(ulll n) {
-    auto f = [n](ulll x) { return modmul(x, x, n) + 1; };
-    ulll x = 0, y = 0, t = 30, prd = 2, i = 1, q;
+ull pollard(ull n) {
+    auto f = [n](ull x) { return modmul(x, x, n) + 1; };
+    ull x = 0, y = 0, t = 30, prd = 2, i = 1, q;
     while (t++ % 40 || __gcd(prd, n) == 1) {
         if (x == y) x = ++i, y = f(x);
         if ((q = modmul(prd, max(x, y) - min(x, y), n))) prd = q;
@@ -50,17 +50,17 @@ ulll pollard(ulll n) {
 
 #define all(x) begin(x), end(x)
 
-vector<ulll> factor(ulll n) {
+vector<ull> factor(ull n) {
     if (n == 1) return {};
     if (isPrime(n)) return {n};
-    ulll x = pollard(n);
+    ull x = pollard(n);
     auto l = factor(x), r = factor(n / x);
     l.insert(l.end(), all(r));
     return l;
 }
 } // namespace kactl
 
-bool factorizable_under_N(const lll N, const lll a) {
+bool factorizable_under_N(const ll N, const ll a) {
     auto fs = kactl::factor(a);
     sort(begin(fs), end(fs));
     return lllof(fs.back()) <= N;
@@ -69,24 +69,21 @@ bool factorizable_under_N(const lll N, const lll a) {
 ll find_X(const lll N, const lll M) {
     if (N >= M) return llof(N);
     if (N * N < M) return -1;
+    if (factorizable_under_N(llof(N), llof(M))) return llof(M);
 
     lll hi = N * N;
     lll lo = M;
-    lll ans = hi;
 
     while (lo + lllof(1) < hi) {
         const auto mid = lo + (hi - lo) / lllof(2);
         if (mid >= M) {
             hi = mid;
-            if (factorizable_under_N(N, hi)) {
-                ans = hi;
-            }
         } else {
             lo = mid;
         }
     }
 
-    return llof(ans);
+    return factorizable_under_N(llof(N), llof(hi)) ? llof(hi) : -1;
 }
 
 int main() {
