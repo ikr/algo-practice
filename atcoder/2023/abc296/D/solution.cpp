c97af8e8a6d1a3e91be46a2ce069d08cb74f95f9
Ivan Krechetov <ikr@ikr.su>
2023-04-02 17:11:05 +0200

    Oh look! Only 6 WA-s now.

--- atcoder/2023/abc296/D/solution.cpp
+++ atcoder/2023/abc296/D/solution.cpp
@@ -3,75 +3,27 @@ using namespace std;
 
 using ll = long long;
 using lll = __int128_t;
-using ull = unsigned long long;
 
 template <typename T> constexpr lll lllof(const T x) {
     return static_cast<lll>(x);
 }
 
-namespace kactl { // https://github.com/kth-competitive-programming/kactl/blob/main/content/number-theory/Factor.h
-ull modmul(ull a, ull b, ull M) {
-    ll ret = a * b - M * ull(1.L / M * a * b);
-    return ret + M * (ret < 0) - M * (ret >= (ll)M);
-}
-ull modpow(ull b, ull e, ull mod) {
-    ull ans = 1;
-    for (; e; b = modmul(b, b, mod), e /= 2)
-        if (e & 1) ans = modmul(ans, b, mod);
-    return ans;
-}
-
-bool isPrime(ull n) {
-    if (n < 2 || n % 6 % 4 != 1) return (n | 1) == 3;
-    ull A[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022},
-        s = __builtin_ctzll(n - 1), d = n >> s;
-    for (ull a : A) { // ^ count trailing zeroes
-        ull p = modpow(a % n, d, n), i = s;
-        while (p != 1 && p != n - 1 && a % n && i--) p = modmul(p, p, n);
-        if (p != n - 1 && i != s) return 0;
-    }
-    return 1;
-}
-
-ull pollard(ull n) {
-    auto f = [n](ull x) { return modmul(x, x, n) + 1; };
-    ull x = 0, y = 0, t = 30, prd = 2, i = 1, q;
-    while (t++ % 40 || __gcd(prd, n) == 1) {
-        if (x == y) x = ++i, y = f(x);
-        if ((q = modmul(prd, max(x, y) - min(x, y), n))) prd = q;
-        x = f(x), y = f(f(y));
-    }
-    return __gcd(prd, n);
-}
-
-#define all(x) begin(x), end(x)
-
-vector<ull> factor(ull n) {
-    if (n == 1) return {};
-    if (isPrime(n)) return {n};
-    ull x = pollard(n);
-    auto l = factor(x), r = factor(n / x);
-    l.insert(l.end(), all(r));
-    return l;
-}
-} // namespace kactl
-
-bool factorizable_under_N(const ll N, const ll a) {
-    const auto fs = kactl::factor(a);
-    const ll hi = *max_element(begin(fs), end(fs));
-    return hi <= N && a / hi <= N;
+template <typename T> constexpr T div_ceil(const T x, const T y) {
+    return x ? (1 + (x - 1) / y) : 0;
 }
 
 ll find_X(const ll N, const ll M) {
     if (N >= M) return M;
     if (lllof(N) * N < lllof(M)) return -1;
 
-    for (ll i = M;; ++i) {
-        if (factorizable_under_N(N, i)) return i;
+    ll ans = LONG_LONG_MAX;
+
+    for (ll a = 1; a * a <= M; ++a) {
+        const auto b = div_ceil(M, a);
+        if (a <= N && b <= N) ans = min(ans, a * b);
     }
 
-    assert("/o\\" && false);
-    return 0;
+    return ans == LONG_LONG_MAX ? -1LL : ans;
 }
 
 int main() {
