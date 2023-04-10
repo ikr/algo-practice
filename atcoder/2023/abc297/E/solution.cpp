08d5faca0ea1f124ea6e0cca72909eafc82e106d
Ivan Krechetov <ikr@ikr.li>
2023-04-10 10:42:21 +0200

    Reiterate

--- atcoder/2023/abc297/E/solution.cpp
+++ atcoder/2023/abc297/E/solution.cpp
@@ -1,16 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T> ostream &operator<<(ostream &os, const set<T> &xs) {
-    os << '{';
-    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
-        if (i != xs.cbegin()) os << ' ';
-        os << *i;
-    }
-    os << '}';
-    return os;
-}
-
 template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
     os << '[';
     for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
@@ -22,6 +12,7 @@ template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
 }
 
 using ll = long long;
+using pii = pair<int, int>;
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
@@ -30,40 +21,30 @@ template <typename T> constexpr int inof(const T x) {
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
 ll kth_lowest(const vector<ll> &A, const int K) {
-    const auto N = sz(A);
-    set<ll> basis;
-    for (int bits = 1; bits < (1 << N); ++bits) {
-        ll s{};
-        for (int i = 0; i < N; ++i) {
-            if ((1 << i) & bits) {
-                s += A[i];
+    vector<int> xs{0};
+    int i0{};
+
+    const auto next_addition = [&]() -> pii {
+        for (int i = i0; i < sz(xs); ++i) {
+            for (const auto a : A) {
+                if (xs[i] + a > xs.back()) {
+                    return {xs[i] + a, i};
+                }
             }
         }
 
-        basis.insert(s);
+        assert(false && "/o\\");
+        return {};
+    };
+
+    while (sz(xs) <= K) {
+        const auto [x, i] = next_addition();
+        xs.push_back(x);
+        i0 = i;
     }
 
-    const vector<ll> bs(cbegin(basis), cend(basis));
-    set<ll> all{bs[0]};
-    for (int i = 1; i < sz(bs); ++i) {
-        const auto hi = bs[i];
-
-        for (int j = 0; j < i; ++j) {
-            ll m = 2;
-            while (sz(all) < K && bs[j] * m < hi) {
-                all.insert(bs[j] * m);
-                ++m;
-            }
-        }
-
-        all.insert(hi);
-        while (sz(all) > K) {
-            all.erase(prev(cend(all)));
-        }
-    }
-
-    cerr << all << endl;
-    return *crbegin(all);
+    cerr << xs << endl;
+    return xs[K];
 }
 
 int main() {
