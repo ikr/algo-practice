c483e358e3c427ffd3dedb201444fd24c1b2bf8b
Ivan Krechetov <ikr@ikr.li>
2023-04-23 09:00:26 +0200

    WIP

--- atcoder/2023/abc299/E/solution.cpp
+++ atcoder/2023/abc299/E/solution.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using pii = pair<int, int>;
 static constexpr int INF = 1'000'000'000;
 
 template <typename T> constexpr int inof(const T x) {
@@ -9,8 +10,8 @@ template <typename T> constexpr int inof(const T x) {
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-multimap<int, int> vertices_by_distance_from(const vector<vector<int>> &g,
-                                             const int u0) {
+void sweep_from_u0(const vector<vector<int>> &g, string &coloring, const int u0,
+                   const int req_dist) {
     const auto n = sz(g);
     vector<int> D(n, INF);
     D[u0] = 0;
@@ -27,20 +28,15 @@ multimap<int, int> vertices_by_distance_from(const vector<vector<int>> &g,
             q.push(v);
         }
     }
-
-    multimap<int, int> ans;
-    for (int v = 0; v < n; ++v) ans.emplace(D[v], v);
-    return ans;
 }
 
 string find_coloring(const vector<vector<int>> &g,
-                     const vector<int> &req_dist) {
+                     const vector<pii> &req_dist) {
     const auto n = sz(g);
-    vector<multimap<int, int>> vbds(n);
-    for (auto u0 = 0; u0 < n; ++u0) vbds[u0] = vertices_by_distance_from(g, u0);
-
-    // ??
-    return "";
+    string coloring(n, '0');
+    for (const auto &[p, d] : req_dist) {
+        if (d == 0) coloring[p] = '1';
+    }
 }
 
 int main() {
@@ -63,13 +59,10 @@ int main() {
 
     int K;
     cin >> K;
-    vector<int> req_dist(N, -1);
-    for (int i = 1; i <= K; ++i) {
-        int p, d;
+    vector<pii> req_dist(K);
+    for (auto &[p, d] : req_dist) {
         cin >> p >> d;
         --p;
-
-        req_dist[p] = d;
     }
 
     const auto ans = find_coloring(g, req_dist);
