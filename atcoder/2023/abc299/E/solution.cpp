f55c1654da3c1664c40ca55e6e9aa6ac72668cec
Ivan Krechetov <ikr@ikr.li>
2023-04-24 07:21:36 +0200

    WA

--- atcoder/2023/abc299/E/solution.cpp
+++ atcoder/2023/abc299/E/solution.cpp
@@ -25,18 +25,66 @@ void sweep_from_u0(const vector<vector<int>> &g, string &coloring, const int u0,
         for (const auto v : g[u]) {
             if (D[v] != INF) continue;
             D[v] = D[u] + 1;
+
+            if (D[v] > req_dist) return;
+            if (D[v] == req_dist) {
+                coloring[v] = '1';
+            } else {
+                assert(D[v] < req_dist);
+                coloring[v] = '0';
+            }
+
             q.push(v);
         }
     }
 }
 
+bool verify_for_u0(const vector<vector<int>> &g, const string &coloring,
+                   const int u0, const int req_dist) {
+    assert(req_dist > 0);
+    if (coloring[u0] == '1') return false;
+
+    const auto n = sz(g);
+    vector<int> D(n, INF);
+    D[u0] = 0;
+    queue<int> q;
+    q.push(u0);
+
+    while (!q.empty()) {
+        const auto u = q.front();
+        q.pop();
+
+        for (const auto v : g[u]) {
+            if (D[v] != INF) continue;
+            D[v] = D[u] + 1;
+
+            if (D[v] > req_dist) return false;
+            if (coloring[v] == '1') return D[v] == req_dist;
+            q.push(v);
+        }
+    }
+
+    return false;
+}
+
 string find_coloring(const vector<vector<int>> &g,
                      const vector<pii> &req_dist) {
     const auto n = sz(g);
+    if (req_dist.empty()) return string(n, '1');
+
     string coloring(n, '0');
     for (const auto &[p, d] : req_dist) {
         if (d == 0) coloring[p] = '1';
     }
+
+    for (const auto &[p, d] : req_dist) sweep_from_u0(g, coloring, p, d);
+
+    for (const auto &[p, d] : req_dist) {
+        if (d == 0 && coloring[p] != '1') return "";
+        if (d != 0 && !verify_for_u0(g, coloring, p, d)) return "";
+    }
+
+    return coloring;
 }
 
 int main() {
