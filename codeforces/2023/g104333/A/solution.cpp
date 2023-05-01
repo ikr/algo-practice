12af7d694149f854b35e6d0818e7d15a83d61b8e
Ivan Krechetov <ikr@ikr.li>
2023-05-01 19:08:25 +0200

    AC

--- codeforces/2023/g104333/A/solution.cpp
+++ codeforces/2023/g104333/A/solution.cpp
@@ -3,12 +3,49 @@
 using namespace std;
 
 using mint = atcoder::modint1000000007;
+using ll = long long;
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+array<int, 30> freqs_by_bit(const vector<int> &xs) {
+    array<int, 30> ans;
+    ans.fill(0);
+
+    for (const auto x : xs) {
+        for (int i = 0; i < sz(ans); ++i) {
+            if (x & (1 << i)) {
+                ++ans[i];
+            }
+        }
+    }
+
+    return ans;
+}
+
+// See https://stackoverflow.com/a/21389244/6430
+mint sum_of_pairwise_xors(const vector<int> &A, const vector<int> &B) {
+    const int n = sz(A);
+    const auto fa = freqs_by_bit(A);
+    const auto fb = freqs_by_bit(B);
+
+    mint ans{};
+    for (int i = 0; i < sz(fa); ++i) {
+        const mint a = fa[i];
+        const mint a_ = n - fa[i];
+
+        const mint b = fb[i];
+        const mint b_ = n - fb[i];
+
+        const mint m = (1 << i);
+
+        ans += m * ((a * b_) + (a_ * b));
+    }
+    return ans;
+}
 
 mint mega_sum(const vector<int> &A, const vector<int> &B) {
     const auto n = sz(A);
@@ -19,17 +56,7 @@ mint mega_sum(const vector<int> &A, const vector<int> &B) {
         n_1_fact *= i;
     }
 
-    const auto s =
-        accumulate(cbegin(A), cend(A), 0,
-                   [](const int agg, const int a) { return agg ^ a; });
-
-    mint ans{0};
-    for (const auto b : B) {
-        for (const auto a : A) {
-            ans += n_1_fact * (b ^ a);
-        }
-    }
-    return ans;
+    return n_1_fact * sum_of_pairwise_xors(A, B);
 }
 
 int main() {
