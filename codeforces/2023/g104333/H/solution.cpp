64b3e515bccac2598c1930df6d06204cbed97482
Ivan Krechetov <ikr@ikr.li>
2023-05-01 17:22:13 +0200

    Still a WA2

--- codeforces/2023/g104333/H/solution.cpp
+++ codeforces/2023/g104333/H/solution.cpp
@@ -10,6 +10,17 @@ template <typename T> constexpr int inof(const T x) {
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
 ll max_tri_prod(const vector<int> &xs) {
+    ll ans = 1LL * xs[0] * xs[1] * xs[2];
+    {
+        set<int> xss(cbegin(xs), cend(xs));
+        if (sz(xss) == 2) {
+            ans = max(ans,
+                      1LL * (*cbegin(xss)) * (*cbegin(xss)) * (*crbegin(xss)));
+            ans = max(ans,
+                      1LL * (*cbegin(xss)) * (*crbegin(xss)) * (*crbegin(xss)));
+        }
+    }
+
     set<int> izer;
     set<int> ineg;
     set<int> ipos;
@@ -42,7 +53,6 @@ ll max_tri_prod(const vector<int> &xs) {
     sort(begin(opts), end(opts));
     opts.erase(unique(begin(opts), end(opts)), end(opts));
 
-    ll ans = 1LL * xs[0] * xs[1] * xs[2];
     for (int i = 0; i < sz(opts) - 2; ++i) {
         for (int j = i + 1; j < sz(opts) - 1; ++j) {
             for (int k = j + 1; k < sz(opts); ++k) {
