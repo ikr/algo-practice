a591985266ad96bc29da254ed6054ceb1d625dfb
Ivan Krechetov <ikr@ikr.su>
2023-05-08 06:35:25 +0200

    Have to cover N = 2

--- toki/2023/troc32/C/solution.cpp
+++ toki/2023/troc32/C/solution.cpp
@@ -5,23 +5,14 @@ using namespace std;
 using mint = atcoder::modint998244353;
 
 mint num_stimulated(const int N, const int M) {
+    if (N == 2) return mint{M}.pow(2);
+
     if (N == 3) {
         if (M < 3) return 0;
         return mint{M} * mint{M - 1} * mint{M - 2};
     }
 
-    vector<mint> D(N, 0);
-    D[0] = mint{M};
-    D[1] = D[0] * mint{M - 1};
-    D[2] = D[1] * mint{M - 1};
-
-    for (int i = 3; i < N - 1; ++i) {
-        D[i] = D[i - 1] * (i % 2 == 0 ? mint{M - 1} : mint{M - 2});
-    }
-
-    D[N - 1] = D[N - 2] * mint{M - 1};
-
-    return D.back();
+    return mint{M} * mint{M - 2} * mint{M - 1}.pow(N - 2);
 }
 
 int main() {
