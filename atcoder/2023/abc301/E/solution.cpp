c54390bf23e6d5fd96b39c78fd9ba7f66b96e97d
Ivan Krechetov <ikr@ikr.su>
2023-05-17 08:11:18 +0200

    Build the graph of the start/goal/candies

--- atcoder/2023/abc301/E/solution.cpp
+++ atcoder/2023/abc301/E/solution.cpp
@@ -1,7 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &x) {
+    os << '(' << x.first << ' ' << x.second << ')';
+    return os;
+}
+
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<vector<T>> &xss) {
+    for (const auto &xs : xss) os << xs << '\n';
+    return os;
+}
+
 using pii = pair<int, int>;
+
+static constexpr int INF = 1000 * 1000 * 1000;
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
@@ -43,5 +67,64 @@ int main() {
     const auto n = sz(candies) + 2;
     vector<vector<pii>> g(n);
 
+    const auto vertex_roco = [&](const int v) -> pii {
+        if (v == n - 2) return start;
+        if (v == n - 1) return goal;
+        assert(0 <= v && v < sz(candies));
+        return candies[v];
+    };
+
+    const auto vertices_by_roco = [&]() {
+        map<pii, int> result;
+        for (int v = 0; v < n; ++v) result[vertex_roco(v)] = v;
+        return result;
+    }();
+
+    const auto trace_from = [&](const int u0) -> void {
+        vector<vector<int>> D(H, vector(W, INF));
+        const auto [ro0, co0] = vertex_roco(u0);
+        D[ro0][co0] = 0;
+
+        queue<pii> q;
+        q.push(vertex_roco(u0));
+
+        while (!q.empty()) {
+            const auto [ro, co] = q.front();
+            q.pop();
+
+            if (ro > 0 && grid[ro - 1][co] != '#' && D[ro - 1][co] == INF) {
+                D[ro - 1][co] = D[ro][co] + 1;
+                q.emplace(ro - 1, co);
+            }
+
+            if (co < W - 1 && grid[ro][co + 1] != '#' && D[ro][co + 1] == INF) {
+                D[ro][co + 1] = D[ro][co] + 1;
+                q.emplace(ro, co + 1);
+            }
+
+            if (ro < H - 1 && grid[ro + 1][co] != '#' && D[ro + 1][co] == INF) {
+                D[ro + 1][co] = D[ro][co] + 1;
+                q.emplace(ro + 1, co);
+            }
+
+            if (co > 0 && grid[ro][co - 1] != '#' && D[ro][co - 1] == INF) {
+                D[ro][co - 1] = D[ro][co] + 1;
+                q.emplace(ro, co - 1);
+            }
+        }
+
+        for (int ro = 0; ro < H; ++ro) {
+            for (int co = 0; co < W; ++co) {
+                if (D[ro][co] == INF) continue;
+                const auto it = vertices_by_roco.find({ro, co});
+                if (it == cend(vertices_by_roco)) continue;
+
+                g[u0].emplace_back(it->second, D[ro][co]);
+            }
+        }
+    };
+
+    for (int u0 = 0; u0 < n; ++u0) trace_from(u0);
+    cerr << g << endl;
     return 0;
 }
