18c298eb29a08e113e30387ff3b5d45c1b0d8491
Ivan Krechetov <ikr@ikr.su>
2023-05-20 09:48:46 +0200

    AC

--- codeforces/2023/c1833_r874_div3/D/solution.cpp
+++ codeforces/2023/c1833_r874_div3/D/solution.cpp
@@ -9,6 +9,15 @@ template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
     return os;
 }
 
+template <typename T>
+ostream &operator<<(ostream &os, const vector<vector<T>> &xss) {
+    for (auto i = xss.cbegin(); i != xss.cend(); ++i) {
+        if (i != xss.cbegin()) os << " | ";
+        os << *i;
+    }
+    return os;
+}
+
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
@@ -23,17 +32,39 @@ vector<int> optimal(vector<int> xs) {
     }
 
     const auto n = sz(xs);
-    const auto it_hi = max_element(begin(xs) + 1, end(xs));
-    const auto hi = inof(distance(begin(xs), it_hi));
+    const auto it_hi = max_element(cbegin(xs) + 1, cend(xs));
+    const auto hi = inof(distance(cbegin(xs), it_hi));
 
-    if (hi == n - 1 && xs[n - 2] < xs[0]) {
-        rotate(begin(xs), it_hi, end(xs));
-        return xs;
+    vector<vector<int>> alt;
+
+    if (hi == n - 1) {
+        int i = hi;
+        while (i > 0 && xs[i - 1] > xs[0]) --i;
+
+        vector<int> ys;
+        ys.reserve(sz(xs));
+        for (int j = hi; j >= i; --j) ys.push_back(xs[j]);
+        ys.insert(cend(ys), cbegin(xs), cbegin(xs) + i);
+
+        alt.emplace_back(move(ys));
     }
 
-    int i = hi - 1;
+    {
+        const int j = hi - 1;
+        int i = j;
+        while (i > 0 && xs[i - 1] > xs[0]) --i;
 
-    return xs;
+        vector<int> ys(cbegin(xs) + hi, cend(xs));
+        ys.reserve(sz(xs));
+
+        for (int k = j; k >= i; --k) ys.push_back(xs[k]);
+        ys.insert(cend(ys), cbegin(xs), cbegin(xs) + i);
+        alt.emplace_back(move(ys));
+    }
+
+    sort(begin(alt), end(alt));
+    // cerr << "alt: " << alt << endl;
+    return alt.back();
 }
 
 int main() {
