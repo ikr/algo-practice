371f63edd652028246e74a5bae4227759367b142
Ivan Krechetov <ikr@ikr.su>
2023-06-02 07:53:23 +0200

    WIP

--- codeforces/2023/c1833_r874_div3/G/solution.cpp
+++ codeforces/2023/c1833_r874_div3/G/solution.cpp
@@ -10,6 +10,8 @@ template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
 }
 
 using pii = pair<int, int>;
+using tri = tuple<int, int, int>;
+using Graph = vector<set<int>>;
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
@@ -22,41 +24,43 @@ template <typename T> constexpr pair<T, T> normalized(const pair<T, T> &ab) {
     return {min(a, b), max(a, b)};
 }
 
-vector<int> degree(const vector<vector<int>> &g) {
+vector<int> degree(const Graph &g) {
     vector<int> result(sz(g), 0);
     for (int u = 0; u < sz(g); ++u) result[u] = sz(g[u]);
     return result;
 }
 
-queue<int> initial_frontier(const vector<int> &deg) {
-    queue<int> result;
+vector<int> initial_frontier(const vector<int> &deg) {
+    vector<int> result;
     for (int u{}; u < sz(deg); ++u) {
-        if (deg[u] == 1) result.push(u);
+        if (deg[u] == 1) result.push_back(u);
     }
     return result;
 }
 
-optional<vector<int>> edges_cut(const vector<vector<int>> &g,
-                                const map<pii, int> &edge_ids) {
+optional<vector<int>> edges_cut(Graph g, const map<pii, int> &edge_ids) {
     auto deg = degree(g);
     auto fro = initial_frontier(deg);
-    set<int> vs_cut;
-    set<pii> es_cut;
 
-    map<int, int> mid;
-    while (!fro.empty()) {
-        const auto u = fro.front();
-        fro.pop();
+    const auto angular_vetochkas = [&]() -> vector<tri> {
+        vector<tri> result;
+        map<int, int> mi;
 
-        for (const auto v : g[u]) {
-            if (vs_cut.contains(v)) continue;
+        for (const auto u : fro) {
+            for (const auto v : g[u]) {
+                if (mi.contains(v)) {
 
-            if (mid.contains(v)) {
-
-            } else {
+                } else {
+                }
             }
         }
-    }
+
+        return result;
+    };
+
+    set<pii> es_cut;
+
+    // ??
 
     vector<int> result;
     for (const auto &e : es_cut) result.push_back(edge_ids.at(e));
@@ -74,19 +78,19 @@ int main() {
         cin >> n;
 
         map<pii, int> edge_ids;
-        vector<vector<int>> g(n);
+        Graph g(n);
         for (int i = 1; i <= n - 1; ++i) {
             int u, v;
             cin >> u >> v;
             --u;
             --v;
 
-            g[u].push_back(v);
-            g[v].push_back(u);
+            g[u].insert(v);
+            g[v].insert(u);
             edge_ids[normalized(pii{u, v})] = i;
         }
 
-        const auto result = edges_cut(g, edge_ids);
+        const auto result = edges_cut(move(g), edge_ids);
         if (result) {
             cout << sz(*result) << '\n';
             cout << *result << '\n';
