9647acddacce7c890d9999a6727f47279445ab9b
Ivan Krechetov <ikr@ikr.li>
2023-06-03 15:21:51 +0200

    Clean up

--- atcoder/2023/abc304/D/solution.cpp
+++ atcoder/2023/abc304/D/solution.cpp
@@ -3,16 +3,6 @@ using namespace std;
 
 using pii = pair<int, int>;
 
-template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
-    os << '[';
-    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
-        if (i != xs.cbegin()) os << ' ';
-        os << *i;
-    }
-    os << ']';
-    return os;
-}
-
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
