a64656ef2b817ce3852d084beee3f1b93b9d0d42
Ivan Krechetov <ikr@ikr.li>
2023-06-04 08:00:40 +0200

    Bootstrap

---
+++ atcoder/2023/abc304/E/solution.cpp
@@ -0,0 +1,46 @@
+#include <atcoder/dsu>
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+template <typename T> constexpr pair<T, T> normalized(const pair<T, T> ab) {
+    const auto [a, b] = ab;
+    return {min(a, b), max(a, b)};
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+
+    int N, M;
+    cin >> N >> M;
+
+    atcoder::dsu uf(N);
+    for (int i = 0; i < N; ++i) {
+        int u, v;
+        cin >> u >> v;
+        --u;
+        --v;
+
+        uf.merge(u, v);
+    }
+
+    set<pair<int, int>> ex;
+
+    int K;
+    cin >> K;
+
+    for (int i = 0; i < K; ++i) {
+        int x, y;
+        cin >> x >> y;
+        --x;
+        --y;
+    }
+
+    return 0;
+}
