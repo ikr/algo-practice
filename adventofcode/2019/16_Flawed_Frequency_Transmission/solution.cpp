88e9a24397a44422579e4f37fc08bcf48a8a2a12
Ivan Krechetov <ikr@ikr.li>
2023-06-04 13:25:10 +0200

    Transform

--- adventofcode/2019/16_Flawed_Frequency_Transmission/solution.cpp
+++ adventofcode/2019/16_Flawed_Frequency_Transmission/solution.cpp
@@ -3,11 +3,29 @@ using namespace std;
 
 static const vector BasePattern{0, 1, 0, -1};
 
+vector<int> digits(const string &s) {
+    vector<int> xs(size(s));
+    transform(cbegin(s), cend(s), begin(xs),
+              [](const char x) -> int { return x - '0'; });
+    return xs;
+}
+
+string bignum(const vector<int> &digits) {
+    stringstream ss;
+    for (const auto d : digits) ss << d;
+    return ss.str();
+}
+
 constexpr int keep_last_digit(const int x) { return abs(x) % 10; }
 
-int main() {
-    string xs;
-    cin >> xs;
+vector<int> apply_phase(const vector<int> &pat, const vector<int> &src) {
+    return src;
+}
 
+int main() {
+    string s;
+    cin >> s;
+
+    cout << bignum(digits(s)) << '\n';
     return 0;
 }
