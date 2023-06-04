23ee1dc1dcbeeae14f9e9ce938b011f7eb664abc
Ivan Krechetov <ikr@ikr.li>
2023-06-04 10:05:11 +0200

    Bootstrap

---
+++ adventofcode/2019/16_Flawed_Frequency_Transmission/solution.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+static const vector BasePattern{0, 1, 0, -1};
+
+constexpr int keep_last_digit(const int x) { return abs(x) % 10; }
+
+int main() {
+    string xs;
+    cin >> xs;
+
+    return 0;
+}
