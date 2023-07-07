d8d8f69ca49da6cdc2eaa537977c051b19181f1e
Ivan Krechetov <ikr@ikr.li>
2023-07-07 08:09:16 +0200

    BFS the distances

--- adventofcode/2019/18_Many-Worlds_Interpretation/solution.cpp
+++ adventofcode/2019/18_Many-Worlds_Interpretation/solution.cpp
@@ -1,29 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T1, typename T2>
-ostream &operator<<(ostream &os, const pair<T1, T2> &x) {
-    os << '(' << x.first << ' ' << x.second << ')';
-    return os;
-}
-
-template <typename K, typename V>
-ostream &operator<<(ostream &os, const map<K, V> &m) {
-    os << '{';
-    for (auto i = m.cbegin(); i != m.cend(); ++i) {
-        if (i != m.cbegin()) os << ' ';
-        os << '(' << i->first << ' ' << i->second << ')';
-    }
-    os << '}';
-    return os;
-}
-
 using Coord = pair<int, int>;
 
-template <typename T>
-constexpr pair<T, T> operator+(const pair<T, T> a, const pair<T, T> b) {
-    return {a.first + b.first, a.second + b.second};
-}
+static constexpr int Inf = 1000 * 1000 * 1000;
+
+static const auto InitialWalls = []() -> unordered_set<char> {
+    unordered_set<char> ans{'#'};
+    for (char c = 'A'; c <= 'Z'; ++c) ans.insert(c);
+    return ans;
+}();
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
@@ -31,11 +17,54 @@ template <typename T> constexpr int inof(const T x) {
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
+struct Model final {
+    vector<string> grid;
+    unordered_map<char, Coord> locations;
+    unordered_set<char> walls;
+};
+
+int distance(const Model &m, const char source, const char destination) {
+    const auto [sro, sco] = m.locations.at(source);
+    const auto [dro, dco] = m.locations.at(destination);
+
+    const auto H = sz(m.grid);
+    const auto W = sz(m.grid[0]);
+
+    vector<vector<int>> D(H, vector(W, Inf));
+    D[sro][sco] = 0;
+
+    queue<Coord> q;
+    q.push({sro, sco});
+
+    while (!q.empty()) {
+        const auto [ro, co] = q.front();
+        q.pop();
+
+        for (const auto &[ro_, co_] : vector<Coord>{
+                 {ro - 1, co}, {ro, co + 1}, {ro + 1, co}, {ro, co - 1}}) {
+            if (D[ro_][co_] != Inf || m.walls.count(m.grid[ro_][co_])) continue;
+
+            D[ro_][co_] = D[ro][co] + 1;
+            q.push({ro_, co_});
+        }
+    }
+
+    return D[dro][dco];
+}
+
+int min_total_distance_collecting_all_keys(
+    const vector<string> &grid, const unordered_map<char, Coord> &locations) {
+    const Model m{grid, locations, InitialWalls};
+    cerr << "@ → a: " << distance(m, '@', 'a')
+         << ", @ → b: " << distance(m, '@', 'b') << endl;
+    return -1;
+}
+
 int main() {
     vector<string> grid;
     for (string line; getline(cin, line);) grid.push_back(line);
 
-    map<char, Coord> locations;
+    unordered_map<char, Coord> locations;
     for (int ro = 0; ro < sz(grid); ++ro) {
         for (int co = 0; co < sz(grid[ro]); ++co) {
             const auto x = grid[ro][co];
@@ -44,6 +73,6 @@ int main() {
         }
     }
 
-    cerr << locations << endl;
+    cout << min_total_distance_collecting_all_keys(grid, locations) << '\n';
     return 0;
 }
