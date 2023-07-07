06de24a458140d0015a14e190ae562c81d3552ef
Ivan Krechetov <ikr@ikr.li>
2023-07-07 07:33:23 +0200

    Init

---
+++ adventofcode/2019/18_Many-Worlds_Interpretation/solution.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &x) {
+    os << '(' << x.first << ' ' << x.second << ')';
+    return os;
+}
+
+template <typename K, typename V>
+ostream &operator<<(ostream &os, const map<K, V> &m) {
+    os << '{';
+    for (auto i = m.cbegin(); i != m.cend(); ++i) {
+        if (i != m.cbegin()) os << ' ';
+        os << '(' << i->first << ' ' << i->second << ')';
+    }
+    os << '}';
+    return os;
+}
+
+using Coord = pair<int, int>;
+
+template <typename T>
+constexpr pair<T, T> operator+(const pair<T, T> a, const pair<T, T> b) {
+    return {a.first + b.first, a.second + b.second};
+}
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+int main() {
+    vector<string> grid;
+    for (string line; getline(cin, line);) grid.push_back(line);
+
+    map<char, Coord> locations;
+    for (int ro = 0; ro < sz(grid); ++ro) {
+        for (int co = 0; co < sz(grid[ro]); ++co) {
+            const auto x = grid[ro][co];
+            if (x == '.' || x == '#') continue;
+            locations[x] = {ro, co};
+        }
+    }
+
+    cerr << locations << endl;
+    return 0;
+}
