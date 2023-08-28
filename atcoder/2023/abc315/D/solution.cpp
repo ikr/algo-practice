c7466a0c20f4379766752eea31773275ebb76eba
Ivan Krechetov <ikr@ikr.su>
2023-08-28 07:53:48 +0200

    First complete draft

--- atcoder/2023/abc315/D/solution.cpp
+++ atcoder/2023/abc315/D/solution.cpp
@@ -7,9 +7,11 @@ template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
 
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
 vector<string> transpose(const vector<string> &m) {
-    const auto H = ssize(m[0]);
-    const auto W = ssize(m);
+    const auto H = sz(m[0]);
+    const auto W = sz(m);
 
     vector<string> ans(H, string(W, '.'));
     for (int ro = 0; ro < H; ++ro) {
@@ -34,12 +36,12 @@ list<ValRep> run_length_encoding(const string &xs) {
 }
 
 constexpr bool should_be_marked(const list<ValRep> &xs) {
-    return ssize(xs) == 1 && xs.front().second > 1;
+    return sz(xs) == 1 && xs.front().second > 1;
 }
 
 vector<int> indices_to_mark(const vector<list<ValRep>> &xss) {
     vector<int> ans;
-    for (int i = 0; i < ssize(xss); ++i) {
+    for (auto i = sz(xss) - 1; i >= 0; --i) {
         if (should_be_marked(xss[i])) ans.push_back(i);
     }
     return ans;
@@ -59,7 +61,21 @@ list<ValRep>::iterator skip_k(list<ValRep> &xs, const int k) {
 }
 
 void remove_at_index(const vector<int> hole_indices, list<ValRep> &xs,
-                     const int i) {}
+                     const int i) {
+    const auto it = skip_k(xs, i - number_of_dominated(hole_indices, i));
+    assert(it != end(xs));
+    if (it->second == 1) {
+        const auto lit = it == begin(xs) ? end(xs) : prev(it);
+        const auto rit = xs.erase(it);
+
+        if (lit != cend(xs) && rit != cend(xs) && lit->first == rit->first) {
+            lit->second += rit->second;
+            xs.erase(rit);
+        }
+    } else {
+        --it->second;
+    }
+}
 
 int cookies_num_remaining(const vector<string> &grid) {
     vector<list<ValRep>> rows(size(grid));
@@ -79,11 +95,36 @@ int cookies_num_remaining(const vector<string> &grid) {
     vector<int> xcols;
 
     while (!mrows.empty() || !mcols.empty()) {
-        for (const auto i : mrows) {
+        for (const auto iro : mrows) {
+            cerr << "Deleting row " << iro << endl;
+
+            for (auto ico = sz(cols) - 1; ico >= 0; --ico) {
+                if (empty(cols[ico])) continue;
+
+                remove_at_index(xrows, cols[ico], iro);
+                if (sz(cols[ico]) == 1 && cols[ico].front().second > 1) {
+                    mcols_.push_back(ico);
+                }
+            }
         }
 
-        for (const auto i : mcols) {
+        for (const auto ico : mcols) {
+            cerr << "Deleting col " << ico << endl;
+
+            for (auto iro = sz(rows) - 1; iro >= 0; --iro) {
+                if (empty(rows[iro])) continue;
+
+                remove_at_index(xcols, rows[iro], ico);
+                if (sz(rows[iro]) == 1 && rows[iro].front().second > 1) {
+                    mrows_.push_back(iro);
+                }
+            }
         }
+
+        xrows.insert(end(xrows), cbegin(mrows), cend(mrows));
+        xcols.insert(end(xcols), cbegin(mcols), cend(mcols));
+        ranges::sort(xrows);
+        ranges::sort(xcols);
 
         swap(mrows, mrows_);
         swap(mcols, mcols_);
@@ -106,7 +147,7 @@ int main() {
     vector<string> grid(H);
     for (auto &row : grid) {
         cin >> row;
-        assert(ssize(row) == W);
+        assert(sz(row) == W);
     }
 
     cout << cookies_num_remaining(grid) << '\n';
