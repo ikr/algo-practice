186ceae10cbe3fd9c66b70b76cef721f1ce37868
Ivan Krechetov <ikr@ikr.li>
2023-08-31 07:38:01 +0200

    Clean up for a different approach

--- adventofcode/2019/18_Many-Worlds_Interpretation/solution.cpp
+++ adventofcode/2019/18_Many-Worlds_Interpretation/solution.cpp
@@ -15,21 +15,6 @@ template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 constexpr bool is_key(const char x) { return 'a' <= x && x <= 'z'; }
 constexpr bool is_flexiwall(const char x) { return 'A' <= x && x <= 'Z'; }
 
-vector<Coord> key_locations(const vector<string> &grid) {
-    vector<Coord> result(Az + 1, {-1, -1});
-    for (int ro = 0; ro < sz(grid); ++ro) {
-        for (int co = 0; co < sz(grid[ro]); ++co) {
-            const auto x = grid[ro][co];
-            if (x == '@') {
-                result[Az] = {ro, co};
-            } else if (is_key(x)) {
-                result[x - 'a'] = {ro, co};
-            }
-        }
-    }
-    return result;
-}
-
 namespace state {
 constexpr int code(const int flexiwall_bits, const char cell) {
     const int cell_id = cell == '@' ? Az : cell - 'a';
@@ -47,46 +32,8 @@ static const auto InitialWalls = []() -> unordered_set<char> {
     return ans;
 }();
 
-struct Model final {
-    vector<string> grid;
-    unordered_map<char, Coord> locations;
-    unordered_set<char> walls;
-};
-
-int distance(const Model &m, const char source, const char destination) {
-    const auto [sro, sco] = m.locations.at(source);
-    const auto [dro, dco] = m.locations.at(destination);
-
-    const auto H = sz(m.grid);
-    const auto W = sz(m.grid[0]);
-
-    vector<vector<int>> D(H, vector(W, Inf));
-    D[sro][sco] = 0;
-
-    queue<Coord> q;
-    q.push({sro, sco});
-
-    while (!q.empty()) {
-        const auto [ro, co] = q.front();
-        q.pop();
-
-        for (const auto &[ro_, co_] : vector<Coord>{
-                 {ro - 1, co}, {ro, co + 1}, {ro + 1, co}, {ro, co - 1}}) {
-            if (D[ro_][co_] != Inf || m.walls.count(m.grid[ro_][co_])) continue;
-
-            D[ro_][co_] = D[ro][co] + 1;
-            q.push({ro_, co_});
-        }
-    }
-
-    return D[dro][dco];
-}
-
-int min_total_distance_collecting_all_keys(
-    const vector<string> &grid, const unordered_map<char, Coord> &locations) {
-    const Model m{grid, locations, InitialWalls};
-    cerr << "@ → a: " << distance(m, '@', 'a')
-         << ", @ → b: " << distance(m, '@', 'b') << endl;
+int min_total_distance_collecting_all_keys(const vector<string> &grid,
+                                           const Coord &start) {
     return -1;
 }
 
@@ -94,15 +41,16 @@ int main() {
     vector<string> grid;
     for (string line; getline(cin, line);) grid.push_back(line);
 
-    unordered_map<char, Coord> locations;
-    for (int ro = 0; ro < sz(grid); ++ro) {
-        for (int co = 0; co < sz(grid[ro]); ++co) {
-            const auto x = grid[ro][co];
-            if (x == '.' || x == '#') continue;
-            locations[x] = {ro, co};
+    const auto start = [&]() -> Coord {
+        for (int ro = 0; ro < sz(grid); ++ro) {
+            for (int co = 0; co < sz(grid[ro]); ++co) {
+                if (grid[ro][co] == '@') return {ro, co};
+            }
         }
-    }
+        assert(false && "No start found");
+        return {-1, -1};
+    }();
 
-    cout << min_total_distance_collecting_all_keys(grid, locations) << '\n';
+    cout << min_total_distance_collecting_all_keys(grid, start) << '\n';
     return 0;
 }
