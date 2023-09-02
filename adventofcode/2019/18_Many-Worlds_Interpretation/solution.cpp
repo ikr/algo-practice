43b504aea69d83792c2e53636b6604fa75445293
Ivan Krechetov <ikr@ikr.li>
2023-09-02 11:31:14 +0200

    Discover the neighbor cells

--- adventofcode/2019/18_Many-Worlds_Interpretation/solution.cpp
+++ adventofcode/2019/18_Many-Worlds_Interpretation/solution.cpp
@@ -1,13 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &x) {
+    os << '(' << x.first << ' ' << x.second << ')';
+    return os;
+}
+
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
+
 static constexpr int Inf = 1000 * 1000 * 1000;
 static constexpr int Az = 26;
 
-using ll = long long;
 using Coord = pair<int, int>;
 using Adj = pair<int, int>;
-using CellCoords = array<Coord, 2 * Az + 1>;
+using CellCoords = array<Coord, Az + 1>;
+
+static const vector<Coord> Deltas{{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 
 constexpr char chof(const int x) { return static_cast<char>(x); }
 
@@ -21,30 +38,75 @@ constexpr bool is_flexiwall(const char x) { return 'A' <= x && x <= 'Z'; }
 constexpr bool is_key(const char x) { return 'a' <= x && x <= 'z'; }
 
 namespace state {
-constexpr ll id_of(const char cell) {
-    if (is_flexiwall(cell)) return cell - 'A';
-    if (is_key(cell)) return Az + cell - 'a';
-
+constexpr int id_of(const char cell) {
+    if (is_key(cell)) return cell - 'a';
     assert(cell == '@');
-    return 2 * Az;
+    return Az;
 }
 
-constexpr char cell_of(const ll id) {
-    assert(0LL <= id && id <= 2 * Az);
-    if (id < Az) return chof('A' + inof(id));
-    if (id < 2 * Az) return chof('a' + inof(id) - Az);
+constexpr char cell_of(const int id) {
+    assert(0LL <= id && id <= Az);
+    if (id != Az) return chof('a' + id);
     return '@';
 }
 
-constexpr ll code(const ll flexiwall_bits, const char cell) {
-    assert(__builtin_popcountl(flexiwall_bits) <= 26);
-    return (flexiwall_bits << 6) | id_of(cell);
+constexpr int code(const int flexiwall_bits, const char cell) {
+    assert(__builtin_popcount(flexiwall_bits) <= 26);
+    return (flexiwall_bits << 5) | id_of(cell);
 }
 
-constexpr ll flexiwall_bits(const ll code) { return code >> 6; }
+constexpr int flexiwall_bits(const int code) { return code >> 5; }
 
-constexpr ll cell_id(const ll code) { return code & 63LL; }
+constexpr int cell_id(const int code) { return code & 31LL; }
 } // namespace state
+
+vector<pair<Coord, int>> neighbor_cells(const vector<string> &grid,
+                                        const int flexiwall_bits,
+                                        const Coord &cell) {
+    const auto H = sz(grid);
+    const auto W = sz(grid[0]);
+    const auto [ro0, co0] = cell;
+    assert(grid[ro0][co0] == '@' || is_key(grid[ro0][co0]));
+
+    const auto is_wall = [&](const int ro, const int co) -> bool {
+        const auto x = grid[ro][co];
+        if (x == '#') return true;
+        return is_flexiwall(x) && (flexiwall_bits & (1 << (x - 'A')));
+    };
+
+    const auto is_new_key = [&](const int ro, const int co) -> bool {
+        const auto x = grid[ro][co];
+        return is_key(x) && (flexiwall_bits & (1 << (x - 'a')));
+    };
+
+    vector<pair<Coord, int>> result;
+
+    vector<vector<int>> D(H, vector(W, Inf));
+    D[ro0][co0] = 0;
+
+    queue<Coord> q;
+    q.push(cell);
+
+    while (!empty(q)) {
+        const auto [ro, co] = q.front();
+        q.pop();
+
+        for (const auto &[dro, dco] : Deltas) {
+            const auto [ro_, co_] = Coord{ro + dro, co + dco};
+            if (D[ro_][co_] != Inf || is_wall(ro_, co_)) continue;
+
+            D[ro_][co_] = D[ro][co] + 1;
+
+            if (is_new_key(ro_, co_)) {
+                result.emplace_back(Coord{ro_, co_}, D[ro_][co_]);
+            } else {
+                q.emplace(ro_, co_);
+            }
+        }
+    }
+
+    return result;
+}
 
 vector<Adj> adjacent_state_codes(const vector<string> &grid,
                                  const CellCoords &cell_coords,
@@ -57,31 +119,31 @@ vector<Adj> adjacent_state_codes(const vector<string> &grid,
 
 int min_total_distance_collecting_all_keys(const vector<string> &grid,
                                            const CellCoords &cell_coords) {
+    cerr << neighbor_cells(grid, (1 << 26) - 1, cell_coords[state::id_of('@')])
+         << endl;
     return -1;
 }
 
 void unit_test() {
     {
-        const auto c = state::code((1LL << 26) - 1LL, '@');
-        assert(state::flexiwall_bits(c) == 0b11111111111111111111111111LL);
-        assert(state::cell_of(state::cell_id(c)) == '@');
+        const auto code = state::code((1 << 26) - 1, '@');
+        assert(state::flexiwall_bits(code) == 0b11111111111111111111111111);
+        assert(state::cell_of(state::cell_id(code)) == '@');
     }
     {
-        const auto c = state::code(1, 'G');
-        assert(state::flexiwall_bits(c) == 1LL);
-        assert(state::cell_of(state::cell_id(c)) == 'G');
-    }
-    {
-        const auto c = state::code(8, 'b');
-        assert(state::flexiwall_bits(c) == 8LL);
-        assert(state::cell_of(state::cell_id(c)) == 'b');
+        const auto code = state::code(8, 'b');
+        assert(state::flexiwall_bits(code) == 8);
+        assert(state::cell_of(state::cell_id(code)) == 'b');
     }
 }
 
 int main() {
     unit_test();
     vector<string> grid;
-    for (string line; getline(cin, line);) grid.push_back(line);
+    for (string line; getline(cin, line);) {
+        if (!empty(line)) grid.push_back(line);
+    }
+    assert(!empty(grid));
 
     CellCoords cell_coords;
     cell_coords.fill({-1, -1});
@@ -89,7 +151,7 @@ int main() {
     for (int ro = 0; ro < sz(grid); ++ro) {
         for (int co = 0; co < sz(grid[ro]); ++co) {
             const auto x = grid[ro][co];
-            if (x == '#' || x == '.') continue;
+            if (x == '#' || x == '.' || is_flexiwall(x)) continue;
             cell_coords[state::id_of(grid[ro][co])] = {ro, co};
         }
     }
