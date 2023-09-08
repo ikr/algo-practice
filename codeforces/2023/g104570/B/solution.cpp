d89e308a2ccc0db5fdf4fd00101dbcb869608550
Ivan Krechetov <ikr@ikr.su>
2023-09-08 17:05:04 +0200

    A — AC; B — first iteration

---
+++ codeforces/2023/g104570/B/solution.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<ll> A(n);
+        for (auto &a : A) cin >> a;
+
+        const auto X = A.back();
+        A.pop_back();
+        ranges::reverse(A);
+
+        vector<ll> SA(n);
+        partial_sum(cbegin(A), cend(A), begin(SA));
+
+        vector<ll> B(n);
+        for (auto &b : B) cin >> b;
+
+        assert(B.back() == X);
+        B.pop_back();
+        ranges::reverse(B);
+
+        vector<ll> SB(n);
+        partial_sum(cbegin(B), cend(B), begin(SB));
+
+        ll ans = X - SB.back();
+        for (int i = 0; i < sz(SA); ++i) {
+            auto a = SA[i];
+            auto b = i ? SB[i - 1] : 0;
+
+            if (b + X > SB.back()) {
+                ans = max(ans, a - (b + X));
+            } else {
+                ans = max(ans, a + X - SB.back());
+            }
+        }
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
