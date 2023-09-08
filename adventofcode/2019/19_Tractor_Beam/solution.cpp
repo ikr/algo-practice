d3688ebebb35c85869fd6c5c94b4c9a9f00f7482
Ivan Krechetov <ikr@ikr.su>
2023-09-08 22:03:53 +0200

    wip

--- adventofcode/2019/19_Tractor_Beam/solution.cpp
+++ adventofcode/2019/19_Tractor_Beam/solution.cpp
@@ -3,15 +3,130 @@ using namespace std;
 
 using ll = long long;
 
+enum class Opcode {
+    ADD = 1,
+    MUL = 2,
+    INP = 3,
+    OUT = 4,
+    JPT = 5,
+    JPF = 6,
+    LSS = 7,
+    EQL = 8,
+    RBS = 9,
+    HLT = 99
+};
+
+enum class Mode { POS = 0, IMM = 1, REL = 2 };
+
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
+
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
 vector<string> split(const string &delim_regex, const string &s) {
     regex r(delim_regex);
     return vector<string>(sregex_token_iterator(cbegin(s), cend(s), r, -1),
                           sregex_token_iterator{});
+}
+
+tuple<Opcode, Mode, Mode, Mode> parse_op(ll op) {
+    op = abs(op);
+    const auto oc = inof(op % 100LL);
+
+    const auto m1 = inof((op / 100LL) % 10LL);
+    const auto m2 = inof((op / 1000LL) % 10LL);
+    const auto m3 = inof((op / 10000LL) % 10LL);
+
+    return {static_cast<Opcode>(oc), static_cast<Mode>(m1),
+            static_cast<Mode>(m2), static_cast<Mode>(m3)};
+}
+
+// Halts immediately if input returns a nullopt
+void intcode_run(vector<ll> &xs, const function<optional<ll>(void)> input,
+                 const function<void(ll)> output) {
+    xs.resize(100'000, 0);
+    int rbase{};
+
+    const auto deref = [&](const Mode m, const ll p) -> ll {
+        switch (m) {
+        case Mode::POS:
+            return xs[p];
+
+        case Mode::IMM:
+            return p;
+
+        case Mode::REL:
+            return xs[rbase + p];
+        }
+
+        assert(false && "/o\\");
+        return 0;
+    };
+
+    for (int i = 0; 0 <= i && i < sz(xs);) {
+        const auto [oc, m1, m2, m3] = parse_op(xs[i++]);
+
+        if (oc == Opcode::ADD) {
+            const auto p1 = xs[i++];
+            const auto p2 = xs[i++];
+            const auto p3 = xs[i++];
+
+            assert(m3 != Mode::IMM);
+            xs[m3 == Mode::REL ? (rbase + p3) : p3] =
+                deref(m1, p1) + deref(m2, p2);
+        } else if (oc == Opcode::MUL) {
+            const auto p1 = xs[i++];
+            const auto p2 = xs[i++];
+            const auto p3 = xs[i++];
+
+            assert(m3 != Mode::IMM);
+            xs[m3 == Mode::REL ? (rbase + p3) : p3] =
+                deref(m1, p1) * deref(m2, p2);
+        } else if (oc == Opcode::INP) {
+            const auto p1 = xs[i++];
+
+            assert(m1 != Mode::IMM);
+            const auto maybe_val = input();
+            if (!maybe_val) return;
+
+            xs[m1 == Mode::REL ? (rbase + p1) : p1] = *maybe_val;
+        } else if (oc == Opcode::OUT) {
+            const auto p1 = xs[i++];
+            output(deref(m1, p1));
+        } else if (oc == Opcode::JPT) {
+            const auto p1 = xs[i++];
+            const auto p2 = xs[i++];
+            if (deref(m1, p1)) i = inof(deref(m2, p2));
+        } else if (oc == Opcode::JPF) {
+            const auto p1 = xs[i++];
+            const auto p2 = xs[i++];
+            if (!deref(m1, p1)) i = inof(deref(m2, p2));
+        } else if (oc == Opcode::LSS) {
+            const auto p1 = xs[i++];
+            const auto p2 = xs[i++];
+            const auto p3 = xs[i++];
+
+            assert(m3 != Mode::IMM);
+            xs[m3 == Mode::REL ? (rbase + p3) : p3] =
+                deref(m1, p1) < deref(m2, p2);
+        } else if (oc == Opcode::EQL) {
+            const auto p1 = xs[i++];
+            const auto p2 = xs[i++];
+            const auto p3 = xs[i++];
+
+            assert(m3 != Mode::IMM);
+            xs[m3 == Mode::REL ? (rbase + p3) : p3] =
+                deref(m1, p1) == deref(m2, p2);
+        } else if (oc == Opcode::RBS) {
+            const auto p1 = xs[i++];
+            rbase += inof(deref(m1, p1));
+        } else if (oc == Opcode::HLT) {
+            break;
+        } else {
+            assert(false && "Invalid opcode");
+        }
+    }
 }
 
 int main() {
@@ -23,5 +138,28 @@ int main() {
     transform(cbegin(tokens), cend(tokens), begin(ram),
               [](const auto &x) { return stoll(x); });
 
+    vector<int> todo;
+    for (int x = 0; x < 50; ++x) {
+        for (int y = 0; y < 50; ++y) {
+            todo.push_back(x);
+            todo.push_back(y);
+        }
+    }
+
+    int cur{};
+    const auto input = [&]() -> optional<ll> {
+        if (cur >= sz(todo)) return nullopt;
+        cerr << "cur:" << cur << endl;
+        return todo[cur++];
+    };
+
+    ll result{};
+    const auto output = [&](const ll x) -> void {
+        cerr << "output:" << x << endl;
+        result += x;
+    };
+
+    while (cur < sz(todo)) intcode_run(ram, input, output);
+    cout << result << '\n';
     return 0;
 }
