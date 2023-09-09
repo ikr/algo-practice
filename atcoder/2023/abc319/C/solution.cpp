8496a642f6af1580caa14df91b8a7e8a5a2c73d0
Ivan Krechetov <ikr@ikr.su>
2023-09-09 15:06:06 +0200

    wip

--- atcoder/2023/abc319/C/solution.cpp
+++ atcoder/2023/abc319/C/solution.cpp
@@ -2,11 +2,22 @@
 using namespace std;
 
 using Grid = array<array<int, 3>, 3>;
+using tri = tuple<int, int, int>;
+
+static const set<tri> LineIds{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6},
+                              {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+constexpr tri normalize(const tri &abc) {
+    const auto [a, b, c] = abc;
+    array<int, 3> xs{a, b, c};
+    ranges::sort(xs);
+    return {xs[0], xs[1], xs[2]};
+}
 
 constexpr int in_grid(const Grid &g, const int id) {
     const auto ro = id / 3;
@@ -14,18 +25,25 @@ constexpr int in_grid(const Grid &g, const int id) {
     return g[ro][co];
 }
 
-array<int, 9> seen(const Grid &g, const array<int, 9> &ids) {
-    array<int, 9> ans{};
-    transform(cbegin(ids), cend(ids), begin(ans),
-              [&](const int id) { return in_grid(g, id); });
-    return ans;
+constexpr bool are_disappointing(const int a, const int b, const int c) {
+    return (a == b && b != c);
 }
 
-bool is_disappointing(const array<int, 9> &xs) {
-    for (int i = 2; i < sz(xs); ++i) {
-        if (xs[i - 2] == xs[i - 1] && xs[i - 1] != xs[i]) return true;
+template <typename T, size_t N>
+ostream &operator<<(ostream &os, const array<T, N> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
     }
-    return false;
+    os << ']';
+    return os;
+}
+
+template <typename T, size_t N>
+ostream &operator<<(ostream &os, const array<array<T, N>, N> &xss) {
+    for (const auto &xs : xss) os << xs << '\n';
+    return os;
 }
 
 int main() {
@@ -43,8 +61,19 @@ int main() {
     double disap = 0;
 
     do {
-        ++total;
-        if (is_disappointing(seen(grid, ids))) ++disap;
+        total = total + 1.0;
+
+        for (int i = 2; i < sz(ids); ++i) {
+            const auto [ida, idb, idc] = tri{ids[i - 2], ids[i - 1], ids[i]};
+            if (!LineIds.contains(normalize({ida, idb, idc}))) continue;
+
+            if (are_disappointing(in_grid(grid, ida), in_grid(grid, idb),
+                                  in_grid(grid, idc))) {
+                disap = disap + 1.0;
+                break;
+            }
+        }
+
     } while (next_permutation(begin(ids), end(ids)));
 
     cout << (1.0 - disap / total) << '\n';
