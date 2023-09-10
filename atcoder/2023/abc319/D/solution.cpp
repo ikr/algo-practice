86ba2b63853bfc740fd0c0e860bdec9b1f7bc20e
Ivan Krechetov <ikr@ikr.su>
2023-09-10 07:36:55 +0200

    AC

--- atcoder/2023/abc319/D/solution.cpp
+++ atcoder/2023/abc319/D/solution.cpp
@@ -13,33 +13,53 @@ template <typename T> constexpr T div_ceil(const T x, const T y) {
     return x ? (1 + (x - 1) / y) : 0;
 }
 
+bool can_fit(const vector<ll> &xs, const ll H, const ll W) {
+    ll h{1};
+    ll w{};
+
+    for (const auto x : xs) {
+        if (x > W) return false;
+        const auto w_ = w ? (w + 1 + x) : x;
+
+        if (w_ > W) {
+            ++h;
+            w = x;
+        } else {
+            w = w_;
+        }
+    }
+
+    return h <= H;
+}
+
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
 
-    ll N, M;
-    cin >> N >> M;
+    ll N, H;
+    cin >> N >> H;
 
-    deque<ll> L(N);
-    for (auto &l : L) cin >> l;
+    vector<ll> xs(N);
+    for (auto &x : xs) cin >> x;
 
-    const auto words_length = accumulate(cbegin(L), cend(L), 0LL);
-    const auto W_e = div_ceil(words_length + N - M, M);
+    const auto result = [&]() -> ll {
+        if (can_fit(xs, H, 1)) return 1;
 
-    ll W = LONG_LONG_MAX;
-    ll cur{};
-    while (!empty(L)) {
-        const auto l = L.front();
-        L.pop_front();
+        ll lo{1};
+        ll hi{LONG_LONG_MAX};
+        assert(can_fit(xs, H, hi));
 
-        if (cur) ++cur;
-        cur += l;
-        if (cur >= W_e) {
-            W = min(W, cur);
-            cur = 0;
+        while (lo + 1 < hi) {
+            const auto mid = midpoint(lo, hi);
+            if (can_fit(xs, H, mid)) {
+                hi = mid;
+            } else {
+                lo = mid;
+            }
         }
-    }
 
-    cout << W << '\n';
+        return hi;
+    }();
+    cout << result << '\n';
     return 0;
 }
