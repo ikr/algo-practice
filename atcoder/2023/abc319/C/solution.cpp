9843905fd49d55e151d23e61578c4c19d2eab9e5
Ivan Krechetov <ikr@ikr.li>
2023-09-10 20:49:06 +0200

    Try yet another interpretation

--- atcoder/2023/abc319/C/solution.cpp
+++ atcoder/2023/abc319/C/solution.cpp
@@ -7,11 +7,6 @@ using tri = tuple<int, int, int>;
 static const set<tri> LineIds{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6},
                               {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
 
-template <typename T> constexpr int inof(const T x) {
-    return static_cast<int>(x);
-}
-template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
-
 constexpr int in_grid(const Grid &g, const int id) {
     const auto ro = id / 3;
     const auto co = id % 3;
@@ -19,24 +14,7 @@ constexpr int in_grid(const Grid &g, const int id) {
 }
 
 constexpr bool are_disappointing(const int a, const int b, const int c) {
-    return (a == b && b != c);
-}
-
-template <typename T, size_t N>
-ostream &operator<<(ostream &os, const array<T, N> &xs) {
-    os << '[';
-    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
-        if (i != xs.cbegin()) os << ' ';
-        os << *i;
-    }
-    os << ']';
-    return os;
-}
-
-template <typename T, size_t N>
-ostream &operator<<(ostream &os, const array<array<T, N>, N> &xss) {
-    for (const auto &xs : xss) os << xs << '\n';
-    return os;
+    return (a == b && b != c) || (b == c && b != a);
 }
 
 int main() {
@@ -50,28 +28,26 @@ int main() {
     }
 
     array<int, 9> ids{0, 1, 2, 3, 4, 5, 6, 7, 8};
-    double total = 0;
     double disap = 0;
 
     do {
-        total = total + 1.0;
+        Grid g;
 
-        for (int i = 2; i < sz(ids); ++i) {
-            const auto [ida, idb, idc] = tri{ids[i - 2], ids[i - 1], ids[i]};
-            if (!LineIds.contains({ida, idb, idc}) &&
-                !LineIds.contains({idc, idb, ida})) {
-                continue;
-            }
+        for (int i = 0; i < 9; ++i) {
+            const auto ro = i / 3;
+            const auto co = i % 3;
+            g[ro][co] = in_grid(grid, ids[i]);
+        }
 
-            if (are_disappointing(in_grid(grid, ida), in_grid(grid, idb),
-                                  in_grid(grid, idc))) {
-                disap = disap + 1.0;
+        for (const auto &[a, b, c] : LineIds) {
+            if (are_disappointing(in_grid(g, a), in_grid(g, b),
+                                  in_grid(g, c))) {
+                ++disap;
                 break;
             }
         }
-
     } while (next_permutation(begin(ids), end(ids)));
 
-    cout << (1.0 - disap / total) << '\n';
+    cout << (362880.0 - disap) / 362880.0 << '\n';
     return 0;
 }
