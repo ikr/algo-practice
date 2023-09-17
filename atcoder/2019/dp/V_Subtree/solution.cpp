4417c907bf6bbcf7cacdd32e43cc79c17d5fb273
Ivan Krechetov <ikr@ikr.li>
2023-09-17 09:51:53 +0200

    TLE, of course

--- atcoder/2019/dp/V_Subtree/solution.cpp
+++ atcoder/2019/dp/V_Subtree/solution.cpp
@@ -4,6 +4,15 @@
 using namespace std;
 
 using mint = atcoder::modint;
+using pii = pair<int, int>;
+
+template <typename T1, typename T2> struct PairHash final {
+    size_t operator()(const pair<T1, T2> &p) const {
+        return 31 * hash<T1>{}(p.first) + hash<T2>{}(p.second);
+    }
+};
+
+using Memo = unordered_map<pii, mint, PairHash<int, int>>;
 
 mint op(const mint a, const mint b) { return a * b; }
 mint e() { return -1; }
@@ -15,14 +24,20 @@ template <typename T> constexpr int inof(const T x) {
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
 vector<mint> num_ways(const vector<vector<int>> &g) {
+    Memo memo;
+
     const auto subtrees_num = [&](const auto &self, const int p,
                                   const int u) -> mint {
-        mint result{1};
-        for (const auto v : g[u]) {
-            if (v == p) continue;
-            result *= self(self, u, v) + 1;
-        }
-        return result;
+        if (memo.contains({p, u})) return memo.at({p, u});
+
+        return memo[{p, u}] = [&]() -> mint {
+            mint result{1};
+            for (const auto v : g[u]) {
+                if (v == p) continue;
+                result *= self(self, u, v) + 1;
+            }
+            return result;
+        }();
     };
 
     vector<mint> D(sz(g), 0);
