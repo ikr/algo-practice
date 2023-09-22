71bc96f7a51979560538ca4854da5d61ecc8f225
Ivan Krechetov <ikr@ikr.li>
2023-09-22 22:11:22 +0200

    Started the case work

--- facebook/2023_Hacker_Cup/Qual/C/solution.cpp
+++ facebook/2023_Hacker_Cup/Qual/C/solution.cpp
@@ -1,12 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+static constexpr int INF = INT_MAX;
+
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
+
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-int min_possible_apple_weight(const vector<int> &xs) { return -1; }
+int opt_mid_left(vector<int> xs) {
+    const auto target = xs[0] + xs.back();
+    xs.insert(begin(xs) + sz(xs) / 2, 0);
+    const auto n = sz(xs);
+    xs[n / 2 - 1] = target - xs[n / 2];
+    if (xs[n / 2 - 1] <= 0) return INF;
+
+    for (int i = n / 2 - 1, j = n / 2; i >= 0 && j < n; --i, ++j) {
+        if (xs[i] + xs[j] != target) return INF;
+    }
+
+    return xs[n / 2 - 1];
+}
+
+int min_possible_apple_weight(const vector<int> &xs) {
+    if (sz(xs) == 1) return 1;
+    int result = opt_mid_left(xs);
+    return result;
+}
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
@@ -19,9 +40,11 @@ int main() {
         cin >> N;
 
         vector<int> xs(2 * N - 1);
+        ranges::sort(xs);
         for (auto &x : xs) cin >> x;
 
-        cout << "Case #" << i << ": " << min_possible_apple_weight(xs) << '\n';
+        const auto ans = min_possible_apple_weight(xs);
+        cout << "Case #" << i << ": " << (ans == INF ? -1 : ans) << '\n';
     }
 
     return 0;
