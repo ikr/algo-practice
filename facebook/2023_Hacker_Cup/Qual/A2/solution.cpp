b33a3e9bf1e8321ff225f128b3f6a27d0f702309
Ivan Krechetov <ikr@ikr.li>
2023-09-23 10:27:40 +0200

    Bug-fix and DRY

--- facebook/2023_Hacker_Cup/Qual/A2/solution.cpp
+++ facebook/2023_Hacker_Cup/Qual/A2/solution.cpp
@@ -2,20 +2,24 @@
 using namespace std;
 
 using ll = long long;
+using pll = pair<ll, ll>;
 
 bool is_K_possible(const ll A, const ll B, const ll C, const ll K) {
-    const auto prefer_double = (B < 2LL * A);
-    if (prefer_double) {
-        const auto y = C / B;
-        const auto r = C % B;
-        const auto x = r / A;
-        return (x + y) * 2LL >= K && x + 2 * y >= K;
-    } else {
-        const auto x = C / A;
-        const auto r = C % A;
-        const auto y = r / B;
-        return (x + y) * 2LL >= K && x + 2 * y >= K;
-    }
+    const auto [x, y] = [&]() -> pll {
+        const auto prefer_double = (B < 2LL * A);
+        if (prefer_double) {
+            const auto b = C / B;
+            const auto r = C % B;
+            const auto a = r / A;
+            return {a, b};
+        } else {
+            const auto a = C / A;
+            const auto r = C % A;
+            const auto b = r / B;
+            return {a, b};
+        }
+    }();
+    return (x + y) * 2LL >= K + 1 && x + 2 * y >= K;
 }
 
 ll max_K(const ll A, const ll B, const ll C) {
