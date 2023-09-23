093c75ba7b5fa07872120a30e2d95ec72fb40aad
Ivan Krechetov <ikr@ikr.li>
2023-09-23 09:41:17 +0200

    First draft

--- facebook/2023_Hacker_Cup/Qual/C/solution.cpp
+++ facebook/2023_Hacker_Cup/Qual/C/solution.cpp
@@ -1,6 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
+
 static constexpr int INF = INT_MAX;
 
 template <typename T> constexpr int inof(const T x) {
@@ -9,24 +19,74 @@ template <typename T> constexpr int inof(const T x) {
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-int opt_mid_left(vector<int> xs) {
+int opt_mid(const vector<int> &xs) {
     const auto target = xs[0] + xs.back();
-    xs.insert(begin(xs) + sz(xs) / 2, 0);
     const auto n = sz(xs);
-    xs[n / 2 - 1] = target - xs[n / 2];
-    if (xs[n / 2 - 1] <= 0) return INF;
+    const auto m = xs[n / 2];
+    if (m >= target) return INF;
 
-    for (int i = n / 2 - 1, j = n / 2; i >= 0 && j < n; --i, ++j) {
+    for (int i = n / 2 - 1, j = n / 2 + 1; i >= 0 && j < n; --i, ++j) {
         if (xs[i] + xs[j] != target) return INF;
     }
 
-    return xs[n / 2 - 1];
+    return target - m;
+}
+
+int opt_left(const vector<int> &xs) {
+    const auto n = sz(xs);
+    const auto target = xs[n / 2 - 1] + xs[n / 2];
+    int result = INF;
+
+    cerr << "(left) xs:" << xs << " target:" << target << endl;
+
+    for (int i = n / 2 - 2, j = n / 2 + 1, replaced{}; i >= -1 && j < n; ++j) {
+        cerr << "i:" << i << " j:" << j << " replaced:" << replaced << endl;
+        if (i > 0 && xs[i] + xs[j] == target) {
+            --i;
+            cerr << "OK" << endl;
+        } else {
+            if (xs[j] >= target || replaced) {
+                cerr << "INF" << endl;
+                return INF;
+            }
+            result = target - xs[j];
+            replaced = 1;
+            cerr << "Adjusted" << endl;
+        }
+    }
+
+    return result;
+}
+
+int opt_right(const vector<int> &xs) {
+    const auto n = sz(xs);
+    const auto target = xs[n / 2] + xs[n / 2 + 1];
+    int result = INF;
+
+    cerr << "(right) xs:" << xs << " target:" << target << endl;
+
+    for (int i = n / 2 - 1, j = n / 2 + 2, replaced{}; i >= 0 && j <= n; --i) {
+        cerr << "i:" << i << " j:" << j << " replaced:" << replaced << endl;
+        if (j < n && xs[i] + xs[j] == target) {
+            ++j;
+            cerr << "OK" << endl;
+        } else {
+            if (xs[i] >= target || replaced) {
+                cerr << "INF" << endl;
+                return INF;
+            }
+            result = target - xs[i];
+            replaced = 1;
+            cerr << "Adjusted" << endl;
+        }
+    }
+
+    return result;
 }
 
 int min_possible_apple_weight(const vector<int> &xs) {
     if (sz(xs) == 1) return 1;
-    int result = opt_mid_left(xs);
-    return result;
+    return min({opt_mid(xs), opt_left(xs), opt_right(xs)});
 }
 
 int main() {
@@ -40,8 +100,8 @@ int main() {
         cin >> N;
 
         vector<int> xs(2 * N - 1);
-        ranges::sort(xs);
         for (auto &x : xs) cin >> x;
+        ranges::sort(xs);
 
         const auto ans = min_possible_apple_weight(xs);
         cout << "Case #" << i << ": " << (ans == INF ? -1 : ans) << '\n';
