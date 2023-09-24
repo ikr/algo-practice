e1d5e0c9bdb1af3bc83c3ae2cb9ce9d2e885f8ac
Ivan Krechetov <ikr@ikr.su>
2023-09-24 12:34:57 +0200

    AC

--- codeforces/2023/c1873_r898_div4/F/solution.cpp
+++ codeforces/2023/c1873_r898_div4/F/solution.cpp
@@ -1,42 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using pii = pair<int, int>;
+
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
+vector<pii> all_proper_height_intervals(const vector<int> &H) {
+    vector<pii> result;
+    for (int l{}, r{}; l < sz(H);) {
+        assert(l <= r);
+        while (r + 1 <= sz(H) - 1 && (H[r] % H[r + 1]) == 0LL) ++r;
+        result.emplace_back(l, r);
+
+        if (l == r) {
+            ++l;
+            ++r;
+        } else {
+            l = r + 1;
+            r = l;
+        }
+    }
+
+    return result;
+}
+
 int max_interesting_length(const vector<int> &A, const vector<int> &H,
                            const int K) {
+    int lo{ranges::any_of(A, [K](const int a) { return a <= K; }) ? 1 : 0};
+    if (!lo) return 0;
+
     vector<int> ss(sz(A) + 1, 0);
     partial_sum(cbegin(A), cend(A), begin(ss) + 1);
+    const auto phi = all_proper_height_intervals(H);
 
-    const auto is_possible = [&](const int interval_size) -> bool {
-        for (int l{}, r{};;) {
-            assert(l <= r);
-            if (l == sz(A) - 1) break;
-
-            while (r + 1 <= sz(A) - 1 && (H[r] % (long long)H[r + 1]) == 0LL &&
-                   ss[r + 2] - ss[l] <= K) {
-                ++r;
-                if (r - l + 1 == interval_size) return true;
+    const auto is_possible = [&](const int d) -> bool {
+        for (const auto &[l, r] : phi) {
+            if (r - l + 1 < d) continue;
+            for (int x = l, y = l + d - 1; y <= r; ++x, ++y) {
+                if (ss[y + 1] - ss[x] <= K) return true;
             }
-
-            if (r == l) {
-                ++l;
-                ++r;
-                continue;
-            }
-
-            l = r;
         }
-
         return false;
     };
 
-    int lo{ranges::any_of(A, [K](const int a) { return a <= K; }) ? 1 : 0};
-    if (!lo) return 0;
     int hi{sz(A) + 1};
 
     while (lo + 1 < hi) {
@@ -70,6 +80,5 @@ int main() {
 
         cout << max_interesting_length(A, H, K) << '\n';
     }
-
     return 0;
 }
