a215b12ac6f0debc74d865a4c219bc74f99dae20
Ivan Krechetov <ikr@ikr.li>
2023-09-28 08:03:25 +0200

    Some introductory definitions

--- adventofcode/2019/20_Donut_Maze/solution.cpp
+++ adventofcode/2019/20_Donut_Maze/solution.cpp
@@ -1,10 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using Coord = pair<int, int>;
+using Label = array<char, 2>;
+static constexpr int Az = 'Z' - 'A' + 1;
+
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
+
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+constexpr bool is_letter(const char x) { return 'A' <= x && x <= 'Z'; }
+
+constexpr int label_code(const Label &label) {
+    assert(is_letter(label[0]) && is_letter(label[1]));
+    return (label[0] - 'A') * Az + (label[1] - 'A');
+}
+
+optional<pair<Label, Coord>> portal_at(const vector<string> &grid,
+                                       const Coord &roco) {
+    return nullopt;
+}
 
 int main() {
     vector<string> grid;
