55ae881a972baa78ec3d5364cc208afedb30e4af
Ivan Krechetov <ikr@ikr.li>
2023-09-28 07:16:27 +0200

    Bootstrap

---
+++ adventofcode/2019/20_Donut_Maze/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+int main() {
+    vector<string> grid;
+    for (string line; getline(cin, line);) grid.push_back(line);
+
+    return 0;
+}
