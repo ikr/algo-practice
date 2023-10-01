34e0578b39736aa5acf24c14b0a42c7c909129ee
Ivan Krechetov <ikr@ikr.su>
2023-10-01 08:45:07 +0200

    Parse out the portals

--- adventofcode/2019/20_Donut_Maze/solution.cpp
+++ adventofcode/2019/20_Donut_Maze/solution.cpp
@@ -5,11 +5,35 @@ using Coord = pair<int, int>;
 using Label = string;
 static constexpr int Az = 'Z' - 'A' + 1;
 
+static const array<Coord, 4> Directions{Coord{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
 struct Portal final {
     int label_code;
     Coord own_coord;
     Coord hallway_coord;
 };
+
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &x) {
+    os << '(' << x.first << ' ' << x.second << ')';
+    return os;
+}
+
+ostream &operator<<(ostream &dest, const Portal &p) {
+    dest << "P(L:" << p.label_code << " O:" << p.own_coord
+         << " H:" << p.hallway_coord << ')';
+    return dest;
+}
+
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
@@ -17,33 +41,42 @@ template <typename T> constexpr int inof(const T x) {
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
+template <typename T>
+constexpr pair<T, T> operator+(const pair<T, T> a, const pair<T, T> b) {
+    return {a.first + b.first, a.second + b.second};
+}
+
+template <typename T>
+constexpr pair<T, T> operator-(const pair<T, T> a, const pair<T, T> b) {
+    return {a.first - b.first, a.second - b.second};
+}
+
 constexpr bool is_letter(const char x) { return 'A' <= x && x <= 'Z'; }
 
-constexpr int label_code(const Label &label) {
+int label_code(const Label &label) {
     assert(sz(label) == 2);
     assert(is_letter(label[0]) && is_letter(label[1]));
     return (label[0] - 'A') * Az + (label[1] - 'A');
+}
+
+char cell_at(const vector<string> &grid, const Coord &roco) {
+    return grid[roco.first][roco.second];
 }
 
 optional<Portal> portal_at(const vector<string> &grid, const Coord &roco) {
     const auto [ro, co] = roco;
     if (!is_letter(grid[ro][co])) return nullopt;
 
-    if (is_letter(grid[ro - 1][co])) {
-        assert(grid[ro + 1][co] == '.');
-        return Portal{label_code({grid[ro - 1][co], grid[ro][co]}),
-                      {ro, co},
-                      {ro + 1, co}};
-    }
+    for (const auto &dir : Directions) {
+        const auto [ro0, co0] = roco - dir;
+        const auto [ro1, co1] = roco + dir;
 
-    if (is_letter(grid[ro + 1][co])) {
-        assert(grid[ro - 1][co] == '.');
-        return Portal{label_code({grid[ro][co], grid[ro + 1][co]}),
-                      {ro, co},
-                      {ro - 1, co}};
+        if (is_letter(grid[ro0][co0]) && grid[ro1][co1] == '.') {
+            const auto a = cell_at(grid, min(roco, roco - dir));
+            const auto b = cell_at(grid, max(roco, roco - dir));
+            return Portal{label_code({a, b}), {ro, co}, {ro1, co1}};
+        }
     }
-
-    // wip
 
     return nullopt;
 }
@@ -54,5 +87,14 @@ int main() {
         if (!empty(line)) grid.push_back(line);
     }
 
+    vector<Portal> portals;
+    for (int ro = 1; ro < sz(grid) - 1; ++ro) {
+        for (int co = 1; co < sz(grid[ro]) - 1; ++co) {
+            if (const auto p = portal_at(grid, {ro, co}); p) {
+                portals.push_back(*p);
+            }
+        }
+    }
+    cerr << portals << endl;
     return 0;
 }
