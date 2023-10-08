592a1423c542bf18a54f3dd79e05059111b18d27
Ivan Krechetov <ikr@ikr.su>
2023-10-08 18:58:49 +0200

    Solve part 1

--- adventofcode/2019/22_Slam_Shuffle/solution.cpp
+++ adventofcode/2019/22_Slam_Shuffle/solution.cpp
@@ -18,6 +18,19 @@ struct Skip final {
 };
 
 using Op = variant<Reverse, Rotate, Skip>;
+template <class... Ts> struct overloaded : Ts... {
+    using Ts::operator()...;
+};
+
+static constexpr int DeckSize = 10007;
+
+vector<int> skipped(const vector<int> &xs, const int k) {
+    vector<int> ans(sz(xs));
+    for (int i = 0; i < sz(xs); ++i) {
+        ans[(i * k) % sz(xs)] = xs[i];
+    }
+    return ans;
+}
 
 int main() {
     vector<Op> ops;
@@ -33,9 +46,34 @@ int main() {
         } else if (line.starts_with("deal with increment")) {
             const auto k = stoi(line.substr(20));
             ops.push_back(Skip{k});
+        } else {
+            assert(false && "Invalid operation description");
         }
     }
 
-    cerr << sz(ops) << endl;
+    vector<int> xs(DeckSize);
+    iota(begin(xs), end(xs), 0);
+
+    for (const auto &op : ops) {
+        visit(overloaded{[&]([[maybe_unused]] const Reverse &rev) {
+                             ranges::reverse(xs);
+                         },
+                         [&](const Rotate &rot) {
+                             if (rot.k > 0) {
+                                 rotate(begin(xs), begin(xs) + rot.k, end(xs));
+                             } else {
+                                 assert(rot.k < 0);
+                                 rotate(begin(xs), end(xs) + rot.k, end(xs));
+                             }
+                         },
+                         [&](const Skip &skp) {
+                             assert(skp.k > 0);
+                             xs = skipped(xs, skp.k);
+                         }},
+              op);
+    }
+
+    const auto result = ranges::find(xs, 2019) - cbegin(xs);
+    cout << result << '\n';
     return 0;
 }
