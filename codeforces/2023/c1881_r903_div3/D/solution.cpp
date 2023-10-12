a7c9d5d565968f8548bc24f07bd161549ee13b5c
Ivan Krechetov <ikr@ikr.su>
2023-10-12 18:52:14 +0200

    WA2

---
+++ codeforces/2023/c1881_r903_div3/D/solution.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+using ull = unsigned long long;
+using ll = long long;
+
+namespace kactl { // https://github.com/kth-competitive-programming/kactl/blob/main/content/number-theory/Factor.h
+ull modmul(ull a, ull b, ull M) {
+    ll ret = a * b - M * ull(1.L / M * a * b);
+    return ret + M * (ret < 0) - M * (ret >= (ll)M);
+}
+ull modpow(ull b, ull e, ull mod) {
+    ull ans = 1;
+    for (; e; b = modmul(b, b, mod), e /= 2)
+        if (e & 1) ans = modmul(ans, b, mod);
+    return ans;
+}
+
+bool isPrime(ull n) {
+    if (n < 2 || n % 6 % 4 != 1) return (n | 1) == 3;
+    ull A[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022},
+        s = __builtin_ctzll(n - 1), d = n >> s;
+    for (ull a : A) { // ^ count trailing zeroes
+        ull p = modpow(a % n, d, n), i = s;
+        while (p != 1 && p != n - 1 && a % n && i--) p = modmul(p, p, n);
+        if (p != n - 1 && i != s) return 0;
+    }
+    return 1;
+}
+
+ull pollard(ull n) {
+    auto f = [n](ull x) { return modmul(x, x, n) + 1; };
+    ull x = 0, y = 0, t = 30, prd = 2, i = 1, q;
+    while (t++ % 40 || __gcd(prd, n) == 1) {
+        if (x == y) x = ++i, y = f(x);
+        if ((q = modmul(prd, max(x, y) - min(x, y), n))) prd = q;
+        x = f(x), y = f(f(y));
+    }
+    return __gcd(prd, n);
+}
+
+#define all(x) begin(x), end(x)
+
+vector<ull> factor(ull n) {
+    if (n == 1) return {};
+    if (isPrime(n)) return {n};
+    ull x = pollard(n);
+    auto l = factor(x), r = factor(n / x);
+    l.insert(l.end(), all(r));
+    return l;
+}
+} // namespace kactl
+
+bool is_possible(const vector<ull> &xs) {
+    vector<ull> ys;
+    ll ones{};
+    for (const auto x : xs) {
+        if (x == 1ULL) {
+            ++ones;
+            continue;
+        }
+        ys.push_back(x);
+    }
+
+    map<ull, int> fs;
+    for (const auto y : ys) {
+        const auto ps = kactl::factor(y);
+        for (const auto p : ps) ++fs[p];
+    }
+
+    vector<ull> fss;
+    for (const auto [_, f] : fs) {
+        fss.push_back(f);
+    }
+    if (ranges::all_of(fss, [&](const auto f) { return f % sz(xs) == 0; })) {
+        return true;
+    }
+
+    const ll m = sz(ys);
+    for (const auto [_, f] : fs) {
+        const auto r = f % m;
+        if (r <= ones) {
+            ones -= r;
+            continue;
+        }
+
+        return false;
+    }
+    return true;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<ull> xs(n);
+        for (auto &x : xs) cin >> x;
+
+        cout << (is_possible(xs) ? "YES" : "NO") << '\n';
+    }
+
+    return 0;
+}
