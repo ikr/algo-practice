fc04758d4450d6d5a5cf0c83da14070b25db0907
Ivan Krechetov <ikr@ikr.su>
2023-10-25 19:44:55 +0200

    AC

--- yandex/2023/Cup_Qual/B/solution.cpp
+++ yandex/2023/Cup_Qual/B/solution.cpp
@@ -3,11 +3,34 @@ using namespace std;
 
 using pii = pair<int, int>;
 
+static constexpr int Inf = 100 * 1000 * 1000;
+
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+pii optimal_k_range(const int ilo, const int ihi, const vector<int> &ii,
+                    const int k) {
+    assert(k < sz(ii));
+    auto ia0 = ii[0];
+    auto ib0 = ii[k - 1];
+    auto d0 = max(ihi, ib0) - min(ilo, ia0);
+
+    for (int j = 1; j + k - 1 < sz(ii); ++j) {
+        const auto ia = ii[j];
+        const auto ib = ii[j + k - 1];
+        const auto d = max(ihi, ib) - min(ilo, ia);
+        if (d < d0) {
+            ia0 = ia;
+            ib0 = ib;
+            d0 = d;
+        }
+    }
+
+    return {ia0, ib0};
+}
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
@@ -16,25 +39,31 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    vector<pii> xs(n);
+    map<int, vector<int>> idx;
     for (int i = 0; i < n; ++i) {
         int a;
         cin >> a;
-        xs[i] = {a, i};
-    }
-    ranges::sort(xs);
-
-    auto lo = xs[0].second;
-    auto hi = lo;
-
-    int taken{};
-    for (const auto &[x, i] : xs) {
-        ++taken;
-        lo = min(lo, i);
-        hi = max(hi, i);
-        if (taken == m) break;
+        idx[a].push_back(i);
     }
 
-    cout << (hi - lo + 1) << '\n';
+    int ilo = Inf;
+    int ihi = -Inf;
+    for (const auto &[_, ii] : idx) {
+        const auto taking = min(m, sz(ii));
+        if (taking == sz(ii)) {
+            ilo = min(ilo, ii.front());
+            ihi = max(ihi, ii.back());
+        } else {
+            assert(taking < sz(ii));
+            const auto [ia, ib] = optimal_k_range(ilo, ihi, ii, taking);
+            ilo = min(ilo, ia);
+            ihi = max(ihi, ib);
+        }
+
+        m -= taking;
+        if (!m) break;
+    }
+
+    cout << (ihi - ilo + 1) << '\n';
     return 0;
 }
