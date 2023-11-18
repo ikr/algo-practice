b4fe1addad70a6618b08e71e9d5f38e2c01809a0
Ivan Krechetov <ikr@ikr.su>
2023-11-18 14:36:14 +0100

    Solve part 2

--- adventofcode/2018/01_Chronal_Calibration/src/main.rs
+++ adventofcode/2018/01_Chronal_Calibration/src/main.rs
@@ -1,12 +1,30 @@
 use std::io::{self, BufRead};
+use std::collections::HashSet;
 
-fn main() {
-    let mut result = 0;
-
+fn read_input() -> Vec<i32> {
+    let mut xs = Vec::new();
     for line in io::stdin().lock().lines() {
         let x: i32 = line.unwrap().trim().parse().unwrap();
-        result += x;
+        xs.push(x);
     }
+    xs
+}
 
-    println!("{}", result);
+fn first_acc_repeated(xs: &Vec<i32>) -> i32 {
+    let mut seen = HashSet::new();
+    let mut acc = 0;
+    loop {
+        for x in xs {
+            acc += x;
+            if seen.contains(&acc) {
+                return acc;
+            }
+            seen.insert(acc);
+        }
+    }
+}
+
+fn main() {
+    let xs = read_input();
+    println!("{}", first_acc_repeated(&xs));
 }
