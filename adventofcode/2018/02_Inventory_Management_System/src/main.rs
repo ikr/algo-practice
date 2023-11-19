109c5180ff99c4e44a90eeab0a57054f42dd5fe1
Ivan Krechetov <ikr@ikr.su>
2023-11-19 13:48:06 +0100

    Solve part 1

--- adventofcode/2018/02_Inventory_Management_System/src/main.rs
+++ adventofcode/2018/02_Inventory_Management_System/src/main.rs
@@ -1,6 +1,7 @@
 use std::io::{self, BufRead};
 
 const AZ: usize = (b'z' - b'a' + 1) as usize;
+type Freqs = [i32; AZ];
 
 fn read_input() -> Vec<String> {
     let mut xs = Vec::new();
@@ -10,7 +11,7 @@ fn read_input() -> Vec<String> {
     xs
 }
 
-fn letter_frequences(xs: &str) -> [i32; AZ] {
+fn letter_frequences(xs: &str) -> Freqs {
     let mut freqs = [0; AZ];
     for c in xs.bytes() {
         freqs[usize::from(c - b'a')] += 1;
@@ -18,8 +19,24 @@ fn letter_frequences(xs: &str) -> [i32; AZ] {
     freqs
 }
 
+fn count_occurances(xs: &Freqs, value: i32) -> i32 {
+    xs.iter().filter(|&&x| x == value).count() as i32
+}
+
 fn main() {
     let xs = read_input();
     let fs = xs.iter().map(|x| letter_frequences(x)).collect::<Vec<_>>();
-    println!("{:?}", fs);
+
+    let mut twos = 0;
+    let mut threes = 0;
+    for f in fs {
+        if count_occurances(&f, 2) > 0 {
+            twos += 1;
+        }
+        if count_occurances(&f, 3) > 0 {
+            threes += 1;
+        }
+    }
+
+    println!("{}", twos * threes);
 }
