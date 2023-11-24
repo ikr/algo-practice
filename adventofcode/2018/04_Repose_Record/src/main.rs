5f7515a166c288eb8dc8e61a8ebf25e87fc75e82
Ivan Krechetov <ikr@ikr.li>
2023-11-24 07:58:57 +0100

    Solve part 1

--- adventofcode/2018/04_Repose_Record/src/main.rs
+++ adventofcode/2018/04_Repose_Record/src/main.rs
@@ -2,9 +2,6 @@ use regex::Regex;
 use std::collections::HashMap;
 use std::io::{self, BufRead};
 
-// Be sure to sort the input lines:
-// $ cat ./in1 | sort | ./target/debug/solution
-
 #[derive(Debug)]
 struct LogEntry {
     hour: u8,
@@ -19,6 +16,7 @@ enum Event {
     WakeUp,
 }
 
+#[derive(PartialEq)]
 enum State {
     Awake,
     Asleep,
@@ -29,6 +27,7 @@ fn read_input() -> Vec<String> {
     for line in io::stdin().lock().lines() {
         xs.push(String::from(line.unwrap().trim()));
     }
+    xs.sort();
     xs
 }
 
@@ -62,34 +61,76 @@ fn parse_begin_shift(src: &str) -> Event {
     }
 }
 
+fn minute_id(h: u8, m: u8) -> usize {
+    if h == 0 {
+        (m + 60) as usize
+    } else {
+        assert!(h == 23);
+        m as usize
+    }
+}
+
 fn main() {
     let entries = read_input()
         .iter()
         .map(|x| parse_log_entry(x))
         .collect::<Vec<_>>();
 
-    let mut sleep_minutes_by_guard_id: HashMap<u16, [u8; 120]> = HashMap::new();
+    let mut sleep_minutes_by_guard_id: HashMap<u16, [u16; 120]> = HashMap::new();
     let mut current_guard_id: u16 = 0;
-    let mut current_minute: u8 = 0;
+    let mut current_minute_id: usize = 0;
     let mut current_state: State = State::Awake;
 
     for e in entries {
         match e.event {
             Event::BeginShift { guard_id } => {
+                assert!(current_state == State::Awake);
+
                 current_guard_id = guard_id;
-                current_minute = e.minute;
+                current_minute_id = minute_id(e.hour, e.minute);
                 current_state = State::Awake;
 
                 sleep_minutes_by_guard_id
                     .entry(current_guard_id)
                     .or_insert([0; 120]);
             }
+
             Event::FallAsleep => {
-                // todo
+                current_minute_id = minute_id(e.hour, e.minute);
+                current_state = State::Asleep;
             }
+
             Event::WakeUp => {
-                // todo
+                if current_state == State::Asleep {
+                    assert!(current_minute_id < minute_id(e.hour, e.minute));
+
+                    for i in current_minute_id..minute_id(e.hour, e.minute) {
+                        sleep_minutes_by_guard_id
+                            .get_mut(&current_guard_id)
+                            .unwrap()[i] += 1;
+                    }
+                }
+
+                current_minute_id = minute_id(e.hour, e.minute);
+                current_state = State::Awake;
             }
         }
     }
+
+    let mut hi_id = 0;
+    let mut hi = 0;
+
+    for (id, &xs) in &sleep_minutes_by_guard_id {
+        let s = xs.iter().fold(0, |a, &b| a + b);
+
+        if s > hi {
+            hi_id = *id;
+            hi = s;
+        }
+    }
+
+    let xs = sleep_minutes_by_guard_id.get(&hi_id).unwrap();
+    let m = xs.iter().max().unwrap();
+    let i = xs.iter().position(|&x| x == *m).unwrap();
+    println!("{}", usize::from(hi_id) * usize::from(i - 60));
 }
