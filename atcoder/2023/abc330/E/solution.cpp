ef21537eb066e8bc1ef6d6c7a3c73557bc309a1a
Ivan Krechetov <ikr@ikr.li>
2023-11-26 09:09:45 +0100

    AC

--- atcoder/2023/abc330/E/solution.cpp
+++ atcoder/2023/abc330/E/solution.cpp
@@ -1,39 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T1, typename T2>
-ostream &operator<<(ostream &os, const pair<T1, T2> &x) {
-    os << '(' << x.first << ' ' << x.second << ')';
-    return os;
-}
-
-template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
-    os << '[';
-    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
-        if (i != xs.cbegin()) os << ' ';
-        os << *i;
-    }
-    os << ']';
-    return os;
-}
-
-template <typename T> ostream &operator<<(ostream &os, const set<T> &xs) {
-    os << '{';
-    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
-        if (i != xs.cbegin()) os << ' ';
-        os << *i;
-    }
-    os << '}';
-    return os;
-}
-
-using pii = pair<int, int>;
-
-template <typename T> constexpr int inof(const T x) {
-    return static_cast<int>(x);
-}
-template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
-
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
@@ -41,111 +8,42 @@ int main() {
     int N, Q;
     cin >> N >> Q;
 
+    vector<int> F(N + 1, 0);
     vector<int> A(N);
-    for (auto &a : A) cin >> a;
 
-    map<int, unordered_set<int>> idx;
-    for (int i = 0; i < N; ++i) idx[A[i]].insert(i);
-
-    set<pii> ii;
-    for (const auto &[k, _] : idx) {
-        if (empty(ii)) {
-            ii.emplace(k, k);
-        } else {
-            const auto [a, b] = *crbegin(ii);
-            if (k == b + 1) {
-                ii.erase(prev(cend(ii)));
-                ii.emplace(a, k);
-            } else {
-                ii.emplace(k, k);
-            }
-        }
+    for (auto &a : A) {
+        cin >> a;
+        if (a <= N) ++F[a];
     }
 
-    const auto mex = [&]() -> int {
-        if (empty(ii)) return 0;
-        const auto [a, b] = *cbegin(ii);
-        if (a) return 0;
-        return b + 1;
-    };
+    set<int> holes;
+    for (int i = 0; i <= N; ++i) {
+        if (!F[i]) holes.insert(i);
+    }
 
-    const auto remove_from_interval = [](const pii ab,
-                                         const int x) -> vector<pii> {
-        const auto [a, b] = ab;
-        assert(a <= x && x <= b);
-
-        if (a == b) return {};
-        if (x == a) return {{a + 1, b}};
-        if (x == b) return {{a, b - 1}};
-        return {{a, x - 1}, {x + 1, b}};
-    };
-
-    const auto add_to_interval = [](const pii ab, const int x) -> pii {
-        const auto [a, b] = ab;
-        if (x + 1 == a) return {x, b};
-        assert(b + 1 == x);
-        return {a, x};
-    };
-
-    const auto remove_from_intervals = [&](const int x) -> void {
-        auto it = ii.lower_bound({x, x});
-        if (it == cend(ii)) {
-            --it;
-        } else {
-            const auto [a, b] = *it;
-            if (x < a || b < x) --it;
+    const auto mutate = [&](const int i, const int x) -> void {
+        if (const auto x0 = A[i]; x0 <= N) {
+            assert(F[x0] > 0);
+            --F[x0];
+            if (F[x0] == 0) holes.insert(x0);
         }
 
-        const auto ab = *it;
-        ii.erase(it);
-        for (const auto &cd : remove_from_interval(ab, x)) ii.insert(cd);
-    };
-
-    const auto add_to_intervals = [&](const int x) -> void {
-        auto it = ii.lower_bound({x, x});
-        if (it == cend(ii)) --it;
-        const auto jt = it == cbegin(ii) ? it : prev(it);
-
-        const auto [a, b] = *it;
-        const auto [c, d] = *jt;
-        if (x + 1 == a || b + 1 == x) {
-            ii.erase({a, b});
-            ii.insert(add_to_interval({a, b}, x));
-        } else if (x + 1 == c || d + 1 == x) {
-            ii.erase({c, d});
-            ii.insert(add_to_interval({c, d}, x));
-        } else {
-            ii.insert({x, x});
-        }
-    };
-
-    const auto change = [&](const int index, const int new_value) -> void {
-        const auto old_value = A[index];
-
-        idx[old_value].erase(index);
-        if (empty(idx[old_value])) {
-            idx.erase(old_value);
-            remove_from_intervals(old_value);
+        if (x <= N) {
+            holes.erase(x);
+            ++F[x];
         }
 
-        if (idx.contains(new_value)) {
-            idx[new_value].insert(index);
-        } else {
-            idx[new_value] = {index};
-            add_to_intervals(new_value);
-        }
-
-        A[index] = new_value;
+        A[i] = x;
     };
 
-    cerr << "ii: " << ii << endl;
     for (int k = 1; k <= Q; ++k) {
         int ik, xk;
         cin >> ik >> xk;
         --ik;
-        change(ik, xk);
-        cerr << "ii: " << ii << endl;
-        cout << mex() << '\n';
+
+        mutate(ik, xk);
+        assert(!empty(holes));
+        cout << *cbegin(holes) << '\n';
     }
 
     return 0;
