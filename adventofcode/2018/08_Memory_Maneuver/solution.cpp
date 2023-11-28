f3523d6fd183f3b1144cbe3cf4fe88218eeae50a
Ivan Krechetov <ikr@ikr.su>
2023-11-28 20:35:53 +0100

    Bootstrap

---
+++ adventofcode/2018/08_Memory_Maneuver/solution.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+vector<string> split(const string &delim_regex, const string &s) {
+    regex r(delim_regex);
+    return vector<string>(sregex_token_iterator(cbegin(s), cend(s), r, -1),
+                          sregex_token_iterator{});
+}
+
+int main() {
+    string input;
+    getline(cin, input);
+
+    const auto tokens = split(" ", input);
+    vector<int> xs(sz(tokens));
+    ranges::transform(tokens, begin(xs), [](const auto &s) { return stoi(s); });
+
+    cerr << xs << endl;
+    return 0;
+}
