af310b06c0b6700b12bcb49b95f3bf22da554083
Ivan Krechetov <ikr@ikr.li>
2023-12-08 06:27:47 +0100

    Draft the part 2; too slow

--- adventofcode/2023/08/solution.cpp
+++ adventofcode/2023/08/solution.cpp
@@ -44,24 +44,42 @@ int main() {
 
     map<string, vector<string>> g;
 
+    set<string> starting_s;
+    set<string> ending_s;
+
     for (string line; getline(cin, line);) {
         const auto u = line.substr(0, 3);
         const auto v1 = line.substr(7, 3);
         const auto v2 = line.substr(12, 3);
         g.emplace(u, vector{v1, v2});
+
+        if (u.back() == 'A') starting_s.insert(u);
+        if (v1.back() == 'Z') ending_s.insert(v1);
+        if (v2.back() == 'Z') ending_s.insert(v2);
     }
+    assert(sz(starting_s) == sz(ending_s));
+    cerr << "sz:" << sz(starting_s) << endl;
+
+    const vector<string> starting(cbegin(starting_s), cend(starting_s));
+    const vector<string> ending(cbegin(ending_s), cend(ending_s));
+
+    cerr << "starting:" << starting << " ending:" << ending << endl;
 
     int steps{};
-    string cur{"AAA"};
-    while (cur != "ZZZ") {
+    auto cur = starting;
+    while (cur != ending) {
         for (const auto &instr : instrs) {
-            if (instr == 'L') {
-                cur = g[cur][0];
-            } else {
-                assert(instr == 'R');
-                cur = g[cur][1];
+            for (auto &u : cur) {
+                if (instr == 'L') {
+                    u = g[u][0];
+                } else {
+                    assert(instr == 'R');
+                    u = g[u][1];
+                }
             }
+            ranges::sort(cur);
             ++steps;
+            cerr << "cur:" << cur << " steps:" << steps << endl;
         }
     }
     cout << steps << '\n';
