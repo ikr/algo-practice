16a626b8e16117f8f468bd31af50a57ec71c0c14
Ivan Krechetov <ikr@ikr.li>
2023-12-10 11:23:05 +0100

    Return adjacent

--- adventofcode/2023/10_Pipe_Maze/solution.cpp
+++ adventofcode/2023/10_Pipe_Maze/solution.cpp
@@ -1,6 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &x) {
+    os << '(' << x.first << ' ' << x.second << ')';
+    return os;
+}
+
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
+
 using Coord = pair<int, int>;
 
 template <typename T>
@@ -11,6 +27,8 @@ constexpr pair<T, T> operator+(const pair<T, T> a, const pair<T, T> b) {
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
+
+static const vector<Coord> Deltas{{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 
 static const unordered_map<char, vector<Coord>> ConnectionDeltas{
     {'|', {{-1, 0}, {1, 0}}}, {'-', {{0, -1}, {0, 1}}},
@@ -46,11 +64,26 @@ int main() {
         return in_bounds(u) ? grid[u.first][u.second] : ' ';
     };
 
+    const auto is_connected = [&](const Coord u, const Coord delta) -> bool {
+        const auto c = cell(u + delta);
+        if (!ConnectionDeltas.contains(c)) return false;
+        for (const auto &d : ConnectionDeltas.at(c)) {
+            if (u + delta + d == u) return true;
+        }
+        return false;
+    };
+
     const auto adjacent = [&](const Coord u) -> vector<Coord> {
         const auto c = cell(u);
-        assert(c != 'S');
-        if (!ConnectionDeltas.contains(c)) return {};
+        if (c == 'S') {
+            vector<Coord> result;
+            for (const auto &d : Deltas) {
+                if (is_connected(u, d)) result.push_back(u + d);
+            }
+            return result;
+        }
 
+        if (!ConnectionDeltas.contains(c)) return {};
         vector<Coord> result;
         for (const auto &d : ConnectionDeltas.at(c)) {
             const auto v = u + d;
@@ -59,12 +92,13 @@ int main() {
         return result;
     };
 
-    const auto is_connected = [&](const Coord u, const Coord delta) -> bool {
-        for (const auto &v : adjacent(u + delta)) {
-            if (v == u) return true;
+    const auto S = start_coord(grid);
+    for (const auto &d : Deltas) {
+        if (is_connected(S, d)) {
+            cerr << S << " to " << (S + d) << endl;
         }
-        return false;
-    };
+    }
+    cerr << adjacent(S) << endl;
 
     return 0;
 }
