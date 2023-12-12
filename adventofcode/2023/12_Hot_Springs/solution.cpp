88fc2f99b10bfddab90b3eb538749e9aaa2c49e5
Ivan Krechetov <ikr@ikr.li>
2023-12-12 18:12:00 +0100

    Do the DP

--- adventofcode/2023/12_Hot_Springs/solution.cpp
+++ adventofcode/2023/12_Hot_Springs/solution.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 using ll = long long;
 
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
+
 template <typename T>
 ostream &operator<<(ostream &os, const vector<vector<T>> &xss) {
     for (const auto &xs : xss) os << xs << '\n';
@@ -29,67 +39,49 @@ vector<int> parse_ints(const string &src) {
     return ans;
 }
 
-vector<int> wildcard_indices(const string &s) {
-    vector<int> ans;
-    for (int i = 0; i < sz(s); ++i) {
-        if (s[i] == '?') ans.push_back(i);
-    }
-    return ans;
-}
+ll arrangements_count(const string &pattern, const vector<int> &digest) {
+    const auto n = sz(pattern);
+    const auto m = sz(digest);
 
-vector<pair<char, int>> run_length_encode(const string &xs) {
-    vector<pair<char, int>> ans{{xs[0], 1}};
-    for (int i = 1; i < sz(xs); ++i) {
-        if (xs[i] == ans.back().first) {
-            ++(ans.back().second);
-        } else {
-            ans.emplace_back(xs[i], 1);
-        }
-    }
-    return ans;
-}
+    const auto is_match_possible = [&](const int i, const int d) -> bool {
+        if (i + d > n) return false;
+        return all_of(cbegin(pattern) + i, cbegin(pattern) + i + d,
+                      [](const char c) { return c == '#' || c == '?'; }) &&
+               (i + d == n || pattern[i + d] == '.' || pattern[i + d] == '?');
+    };
 
-bool confirm_match(const vector<int> &digest, const string &src) {
-    const auto src_rle = run_length_encode(src);
-    vector<int> candidate;
-    for (const auto &[c, n] : src_rle) {
-        if (c == '#') {
-            candidate.push_back(n);
-        }
-    }
-    return digest == candidate;
-}
+    const auto possible_shifts_starting_at = [&](const int i,
+                                                 const int d) -> int {
+        assert(is_match_possible(i, d));
+        int result{};
+        while (is_match_possible(i + result + 1, d)) ++result;
+        return result;
+    };
 
-int arrangements_count(const string &pattern, const vector<int> &digest) {
-    const auto wi = wildcard_indices(pattern);
-    int result{};
-    for (int bits = 0; bits < (1 << sz(wi)); ++bits) {
-        string candidate = pattern;
-        for (int i = 0; i < sz(wi); ++i) {
-            if (bits & (1 << i)) {
-                candidate[wi[i]] = '#';
-            } else {
-                candidate[wi[i]] = '.';
+    vector<vector<ll>> memo(2 * n, vector(2 * m, -1LL));
+
+    const auto recur = [&](const auto self, int i, const int j) -> ll {
+        if (memo[i][j] != -1) return memo[i][j];
+        return memo[i][j] = [&]() -> ll {
+            while (i < n && pattern[i] == '.') ++i;
+            if (i >= n && j >= m) return 1LL;
+            if (i >= n || j >= m) return 0LL;
+
+            while (i < n && !is_match_possible(i, digest[j])) ++i;
+            if (i == n) return 0LL;
+
+            const auto ss = possible_shifts_starting_at(i, digest[j]);
+            ll ans{};
+            for (int s = 0; s <= ss; ++s) {
+                ans += self(self, i + s + digest[j] + 1, j + 1);
             }
-        }
+            return ans;
+        }();
+    };
 
-        if (confirm_match(digest, candidate)) ++result;
-    }
+    const auto result = recur(recur, 0, 0);
+    cerr << memo << endl;
     return result;
-}
-
-constexpr ll ipow(const ll base, const int exp) {
-    return exp == 0 ? 1 : base * ipow(base, exp - 1);
-}
-
-bool head_fixed(const string &pattern, const vector<int> &digest) {
-    const auto rle = run_length_encode(pattern);
-    return rle[0].first == '#' && rle[0].second == digest[0];
-}
-
-bool tail_fixed(const string &pattern, const vector<int> &digest) {
-    const auto rle = run_length_encode(pattern);
-    return rle.back().first == '#' && rle.back().second == digest.back();
 }
 
 int main() {
@@ -104,35 +96,8 @@ int main() {
 
     ll result{};
     for (int i = 0; i < sz(patterns); ++i) {
-        const auto a = arrangements_count(patterns[i], digests[i]);
-        const auto asuff = arrangements_count(patterns[i] + "?", digests[i]);
-        const auto apref = arrangements_count("?" + patterns[i], digests[i]);
-
-        const auto cur = [&]() -> ll {
-            if (head_fixed(patterns[i], digests[i]) ||
-                tail_fixed(patterns[i], digests[i])) {
-                return ipow(a, 5);
-            }
-            if (asuff == a && apref == a) return ipow(a, 5);
-            if (asuff != a && apref == a) return ipow(asuff, 4) * a;
-            if (asuff == a && apref != a) return a * ipow(apref, 4);
-            assert(asuff != a && apref != a);
-
-            ll ans{};
-            for (int bits = 0; bits < (1 << 4); ++bits) {
-                const ll suffs = __builtin_popcount(bits);
-                const ll prefs = 4 - suffs;
-
-                ll x = a;
-                for (int k = 1; k <= suffs; ++k) x *= asuff;
-                for (int k = 1; k <= prefs; ++k) x *= apref;
-                ans += x;
-            }
-            return ans;
-        }();
-
-        cerr << " a:" << a << " al:" << asuff << " ar:" << apref
-             << " cur:" << cur << endl;
+        const auto cur = arrangements_count(patterns[i], digests[i]);
+        cerr << "cur:" << cur << endl;
         result += cur;
     }
     cout << result << '\n';
