c9601602351b227c31ad7d9c1300c90a71f01391
Ivan Krechetov <ikr@ikr.su>
2023-12-18 07:42:27 +0100

    Clean up

--- adventofcode/2023/18/solution.cpp
+++ adventofcode/2023/18/solution.cpp
@@ -3,22 +3,6 @@ using namespace std;
 
 using ll = long long;
 
-template <typename T1, typename T2>
-ostream &operator<<(ostream &os, const pair<T1, T2> &x) {
-    os << '(' << x.first << ' ' << x.second << ')';
-    return os;
-}
-
-template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
-    os << '[';
-    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
-        if (i != xs.cbegin()) os << ' ';
-        os << *i;
-    }
-    os << ']';
-    return os;
-}
-
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
@@ -30,12 +14,6 @@ using Coord = pair<ll, ll>;
 enum class Dir { Up, Right, Down, Left };
 static constexpr array Delta{Coord{-1, 0}, Coord{0, 1}, Coord{1, 0},
                              Coord{0, -1}};
-
-constexpr Dir dir_of(const Coord delta) {
-    const int i = static_cast<int>(ranges::find(Delta, delta) - cbegin(Delta));
-    assert(0 <= 1 && i < ssize(Delta));
-    return static_cast<Dir>(i);
-}
 
 constexpr Coord delta_of(const Dir dir) {
     const int i = static_cast<int>(dir);
@@ -51,24 +29,6 @@ constexpr pair<T, T> operator+(const pair<T, T> a, const pair<T, T> b) {
 template <typename T>
 constexpr pair<T, T> operator-(const pair<T, T> a, const pair<T, T> b) {
     return {a.first - b.first, a.second - b.second};
-}
-
-ostream &operator<<(ostream &os, const Dir &d) {
-    const auto a = [&]() -> string {
-        switch (d) {
-        case Dir::Up:
-            return "U";
-        case Dir::Right:
-            return "R";
-        case Dir::Down:
-            return "D";
-        default:
-            assert(d == Dir::Left);
-            return "L";
-        }
-    }();
-    cout << a;
-    return os;
 }
 
 template <typename T>
@@ -101,8 +61,6 @@ int main() {
     for (string line; getline(cin, line);) {
         auto token = line.substr(sz(line) - 7);
         token.pop_back();
-        cerr << token << ' ' << decode_step(token) << endl;
-
         const auto [dir, distance] = decode_step(token);
         path.push_back(path.back() + scaled_by(delta_of(dir), distance));
     }
@@ -113,10 +71,8 @@ int main() {
         const auto [dx, dy] = path[i] - path[i - 1];
         border += abs(dx) + abs(dy);
     }
-    cerr << "border:" << border << endl;
 
     ranges::reverse(path);
-    cerr << path << endl;
 
     ll total{};
     for (int i = 0; i < sz(path) - 1; ++i) {
