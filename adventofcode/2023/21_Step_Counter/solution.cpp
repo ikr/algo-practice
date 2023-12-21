9622116866b60e061eb30bf66bdd1fe1d5bc74ac
Ivan Krechetov <ikr@ikr.li>
2023-12-21 14:01:18 +0100

    May be we're onto something

--- adventofcode/2023/21_Step_Counter/solution.cpp
+++ adventofcode/2023/21_Step_Counter/solution.cpp
@@ -3,44 +3,6 @@ using namespace std;
 
 using ll = long long;
 
-template <typename T1, typename T2>
-ostream &operator<<(ostream &os, const pair<T1, T2> &x) {
-    os << '(' << x.first << ' ' << x.second << ')';
-    return os;
-}
-
-template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
-    os << '[';
-    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
-        if (i != xs.cbegin()) os << ' ';
-        os << *i;
-    }
-    os << ']';
-    return os;
-}
-
-template <typename T>
-ostream &operator<<(ostream &os, const vector<vector<T>> &xss) {
-    for (const auto &xs : xss) os << xs << '\n';
-    return os;
-}
-
-ostream &operator<<(ostream &os, const vector<string> &xss) {
-    for (const auto &xs : xss) os << xs << '\n';
-    return os;
-}
-
-template <typename K, typename V>
-ostream &operator<<(ostream &os, const map<K, V> &m) {
-    os << '{';
-    for (auto i = m.cbegin(); i != m.cend(); ++i) {
-        if (i != m.cbegin()) os << ' ';
-        os << '(' << i->first << ' ' << i->second << ')';
-    }
-    os << '}';
-    return os;
-}
-
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
@@ -57,21 +19,14 @@ constexpr pair<T, T> operator+(const pair<T, T> a, const pair<T, T> b) {
     return {a.first + b.first, a.second + b.second};
 }
 
-vector<string> multigrid(const vector<string> &grid, const int k) {
-    const auto H = sz(grid);
-    const auto W = sz(grid[0]);
-    vector<string> result(H * k, string(W * k, ' '));
-    for (int r = 0; r < H * k; ++r) {
-        for (int c = 0; c < W * k; ++c) {
-            result[r][c] = grid[r % H][c % W];
-        }
-    }
-    return result;
+template <typename T> size_t combine_hashes(const T &xs) {
+    size_t seed = xs.size();
+    for (const auto i : xs) seed ^= i + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+    return seed;
 }
 
 int main() {
     vector<string> grid;
-
     for (string line; getline(cin, line);) {
         grid.push_back(line);
     }
@@ -89,58 +44,47 @@ int main() {
     }
     assert(src.first != -1 && src.second != -1);
 
-    const auto K = 9;
-    src = {K / 2 * sz(grid) + src.first, K / 2 * sz(grid[0]) + src.second};
-    grid = multigrid(grid, K);
-    // grid[src.first][src.second] = 'S';
+    const int M = sz(grid);
+    assert(sz(grid) == sz(grid[0]));
 
-    const int H = sz(grid);
-    const int W = sz(grid[0]);
-
-    const auto in_bounds = [H, W](const Coord p) {
-        return 0 <= p.first && p.first < H && 0 <= p.second && p.second < W;
+    const auto inf_grid_at = [&](const Coord p) -> char {
+        const auto r = ((p.first % M) + M) % M;
+        const auto c = ((p.second % M) + M) % M;
+        return grid[r][c];
     };
 
     const auto adjacent = [&](const Coord p) {
         vector<Coord> ans;
         for (const auto &d : Delta) {
             const auto np = p + d;
-            assert(in_bounds(np));
-            if (grid[np.first][np.second] == '#') continue;
+            if (inf_grid_at(np) == '#') continue;
             ans.push_back(np);
         }
         return ans;
     };
 
     const int MaxSteps = 500;
-    vector<ll> xs;
-    unordered_set<int> gen{src.first * W + src.second};
+    set<Coord> gen{src};
+
+    const auto tile_digest = [&]() -> size_t {
+        vector<int> xs;
+        for (int r = 0 - 3 * M; r < M - 3 * M; ++r) {
+            for (int c = 0 - 5 * M; c < M - 5 * M; ++c) {
+                if (gen.contains({r, c})) xs.push_back(r * M + c);
+            }
+        }
+        return combine_hashes(xs);
+    };
 
     for (int step = 1; step <= MaxSteps; ++step) {
-        unordered_set<int> gen_;
+        set<Coord> gen_;
 
-        for (const auto p : gen) {
-            const auto r = p / W;
-            const auto c = p % W;
-
-            for (const auto &np : adjacent({r, c})) {
-                gen_.insert(np.first * W + np.second);
-            }
+        for (const auto &p : gen) {
+            for (const auto &np : adjacent(p)) gen_.insert(np);
         }
 
         swap(gen, gen_);
-        xs.push_back(sz(gen));
+        cerr << "step:" << step << " center_digest:" << tile_digest() << endl;
     }
-
-    vector<vector<ll>> xss{xs};
-    for (int k = 1; k <= 2; ++k) {
-        vector<ll> ys(sz(xss.back()) - 1);
-        for (int i = sz(ys) - 1; i >= 0; --i) {
-            ys[i] = xss.back()[i + 1] - xss.back()[i];
-        }
-        xss.push_back(ys);
-    }
-
-    cerr << xss << endl;
     return 0;
 }
