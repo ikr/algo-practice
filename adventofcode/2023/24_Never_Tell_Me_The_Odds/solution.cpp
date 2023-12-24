71a8fa5a2d1cb1349b29cb1789bdff215fd09d45
Ivan Krechetov <ikr@ikr.li>
2023-12-24 11:06:22 +0100

    Observe pairwise distances

--- adventofcode/2023/24_Never_Tell_Me_The_Odds/solution.cpp
+++ adventofcode/2023/24_Never_Tell_Me_The_Odds/solution.cpp
@@ -62,6 +62,12 @@ template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
     return os;
 }
 
+template <typename T>
+ostream &operator<<(ostream &os, const vector<vector<T>> &xss) {
+    for (const auto &xs : xss) os << xs << '\n';
+    return os;
+}
+
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
@@ -79,6 +85,35 @@ Coord parse_coord(const string &s) {
     return {stoll(token[0]), stoll(token[1]), stoll(token[2])};
 }
 
+using Matrix = vector<vector<lll>>;
+
+Matrix pairwise_dist2(const vector<Coord> &ps) {
+    const auto n = sz(ps);
+    Matrix result(n, vector<lll>(n, 0));
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            result[i][j] = dist2(ps[i], ps[j]);
+            result[j][i] = result[i][j];
+        }
+    }
+    return result;
+}
+
+void min_special(Matrix &a, const Matrix &b, Matrix &T, const lll t) {
+    const auto n = sz(a);
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            if (b[i][j] < a[i][j]) {
+                a[i][j] = b[i][j];
+                T[i][j] = t;
+            }
+
+            a[j][i] = a[i][j];
+            T[j][i] = T[i][j];
+        }
+    }
+}
+
 int main() {
     vector<Coord> initial_locations;
     vector<Coord> velocities;
@@ -88,9 +123,31 @@ int main() {
         initial_locations.push_back(parse_coord(parts[0]));
         velocities.push_back(parse_coord(parts[1]));
     }
+    const auto n = sz(initial_locations);
 
-    cerr << initial_locations << endl;
-    cerr << velocities << endl;
+    const auto locations_at_time = [&](const lll t) -> vector<Coord> {
+        vector<Coord> result(n);
+        ranges::transform(initial_locations, velocities, begin(result),
+                          [&](const Coord &p0, const Coord &v) -> Coord {
+                              return p0 + scaled_by(v, t);
+                          });
+        return result;
+    };
 
+    cerr << "Distances @ 0:\n" << pairwise_dist2(initial_locations) << endl;
+
+    Matrix min_dist2 = pairwise_dist2(initial_locations);
+    Matrix min_dist2_t(n, vector<lll>(n, 0));
+
+    for (lll t = 1; t <= 8; ++t) {
+        cerr << "Distances @ " << t << ":\n"
+             << pairwise_dist2(locations_at_time(t)) << endl;
+
+        min_special(min_dist2, pairwise_dist2(locations_at_time(t)),
+                    min_dist2_t, t);
+    }
+
+    cerr << min_dist2 << endl;
+    cerr << min_dist2_t << endl;
     return 0;
 }
