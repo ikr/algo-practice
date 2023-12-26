27ff7b00845a13a4d369dd8abb09d66f064f1ca6
Ivan Krechetov <ikr@ikr.su>
2023-12-26 16:13:41 +0100

    Clean up

--- adventofcode/2023/24_Never_Tell_Me_The_Odds/solution.cpp
+++ adventofcode/2023/24_Never_Tell_Me_The_Odds/solution.cpp
@@ -6,39 +6,6 @@ using ll = int64_t;
 
 using Coord = array<ll, 3>;
 
-ostream &operator<<(ostream &os, const Coord &p) {
-    os << '(' << p[0] << ' ' << p[1] << ' ' << p[2] << ')';
-    return os;
-}
-
-template <typename T> constexpr T operator+(T a, const T &b) {
-    assert(a.size() == b.size());
-    ranges::transform(a, b, begin(a),
-                      [](const auto x, const auto y) { return x + y; });
-    return a;
-}
-
-constexpr Coord scaled_by(Coord p, const ll k) {
-    ranges::transform(p, begin(p), [k](const auto a) { return a * k; });
-    return p;
-};
-
-template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
-    os << '[';
-    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
-        if (i != xs.cbegin()) os << ' ';
-        os << *i;
-    }
-    os << ']';
-    return os;
-}
-
-template <typename T>
-ostream &operator<<(ostream &os, const vector<vector<T>> &xss) {
-    for (const auto &xs : xss) os << xs << '\n';
-    return os;
-}
-
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
@@ -132,7 +99,6 @@ int main() {
         initial_locations.push_back(parse_coord(parts[0]));
         velocities.push_back(parse_coord(parts[1]));
     }
-    cerr << initial_locations << '\n' << velocities << endl;
 
     const auto n = sz(initial_locations);
     for (int i = 0; i < n - 2; ++i) {
