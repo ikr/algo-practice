787c64ddc8866609c2d74676c743b39c13db5f8c
Ivan Krechetov <ikr@ikr.su>
2023-12-27 15:47:41 +0100

    Factorize

--- adventofcode/2018/12_Subterranean_Sustainability/src/main.rs
+++ adventofcode/2018/12_Subterranean_Sustainability/src/main.rs
@@ -2,12 +2,23 @@ use std::io::{self, BufRead};
 
 const INPUT_PREFIX: &str = "initial state: ";
 
-fn main() {
+fn read_initial_state_source() -> String{
     let mut line = String::new();
-    let stdin = io::stdin();
-    stdin
-        .lock()
+    io::stdin()
         .read_line(&mut line)
         .expect("1st input line read failed");
-    println!("{}", &line[INPUT_PREFIX.len()..].trim_end());
+
+    line[INPUT_PREFIX.len()..].trim_end().to_string()
+}
+
+fn skip_input_line() {
+    let mut line = String::new();
+    io::stdin()
+        .read_line(&mut line)
+        .expect("Skipping an empty line failed");
+}
+
+fn main() {
+    println!("{}", read_initial_state_source());
+    skip_input_line();
 }
