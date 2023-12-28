54854634534ba53010045d3cf162fd3a906319d9
Ivan Krechetov <ikr@ikr.su>
2023-12-28 10:43:20 +0100

    Parse the rules

--- adventofcode/2018/12_Subterranean_Sustainability/src/main.rs
+++ adventofcode/2018/12_Subterranean_Sustainability/src/main.rs
@@ -2,7 +2,7 @@ use std::io::{self, BufRead};
 
 const INPUT_PREFIX: &str = "initial state: ";
 
-fn read_initial_state_source() -> String{
+fn read_initial_state_source() -> String {
     let mut line = String::new();
     io::stdin()
         .read_line(&mut line)
@@ -18,7 +18,39 @@ fn skip_input_line() {
         .expect("Skipping an empty line failed");
 }
 
+fn parse_neigh_bits(s: &str) -> u8 {
+    let mut result: u8 = 0;
+    for i in 0..s.len() {
+        if s.chars().nth(i).unwrap() == '#' {
+            result |= 1 << i;
+        }
+    }
+    result
+}
+
+fn parse_rule(s: &str) -> (u8, bool) {
+    let tokens: Vec<&str> = s.split(" => ").collect();
+    (parse_neigh_bits(tokens[0]), tokens[1] == "#")
+}
+
 fn main() {
     println!("{}", read_initial_state_source());
     skip_input_line();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_neigh_bits_on_single_self() {
+        let result = parse_neigh_bits("..#..");
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn parse_neigh_bits_on_complete_mask() {
+        let result = parse_neigh_bits("#####");
+        assert_eq!(result, 31);
+    }
 }
