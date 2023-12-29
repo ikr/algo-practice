c729e3f65bede11eee82cbd5e8a2ead24fb5c07a
Ivan Krechetov <ikr@ikr.li>
2023-12-29 09:05:06 +0100

    Experimenting

--- codeforces/2023/c1915_r918_div4/F/solution.cpp
+++ codeforces/2023/c1915_r918_div4/F/solution.cpp
@@ -3,6 +3,22 @@
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
 
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &x) {
+    os << '(' << x.first << ' ' << x.second << ')';
+    return os;
+}
+
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
+
 using namespace __gnu_pbds;
 template <typename T>
 using ordered_set =
@@ -18,14 +34,18 @@ template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
 int num_greetings(vector<pii> AB) {
     ranges::sort(AB, [](const auto &lhs, const auto &rhs) {
-        return lhs.second - lhs.first < rhs.second - rhs.first;
+        return pair{lhs, lhs.second - lhs.first} <
+               pair{rhs, rhs.second - rhs.first};
     });
 
     int result{};
+    ordered_set<int> as;
     ordered_set<int> bs;
     for (const auto &[a, b] : AB) {
+        as.insert(a);
         bs.insert(b);
-        result += inof(bs.order_of_key(b));
+        result += min(inof(bs.order_of_key(b)),
+                      sz(as) - inof(as.order_of_key(a) - 1));
     }
     return result;
 }
