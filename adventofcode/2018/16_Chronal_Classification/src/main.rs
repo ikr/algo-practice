f674114d66fa3019d81b5b4da5ca55c4a40c0629
Ivan Krechetov <ikr@ikr.su>
2024-01-10 21:38:15 +0100

    Go on

--- adventofcode/2018/16_Chronal_Classification/src/main.rs
+++ adventofcode/2018/16_Chronal_Classification/src/main.rs
@@ -16,6 +16,7 @@ type Val = u32;
 type Regs = [Val; 4];
 type Args = [Val; 3];
 type Opcode = u8;
+type AbstractInstruction = (Opcode, Args);
 
 enum Op {
     Addr,
@@ -34,6 +35,45 @@ enum Op {
     Eqir,
     Eqri,
     Eqrr,
+}
+
+#[derive(Debug)]
+struct Sample {
+    before: Regs,
+    instruction: AbstractInstruction,
+    after: Regs,
+}
+
+impl Sample {
+    fn parse(s: &str) -> Sample {
+        let lines = s.split("\n").collect::<Vec<&str>>();
+        let abstract_instruction_quad = parse_quad(" ", &lines[1]);
+        Sample {
+            before: parse_quad(", ", &Sample::infix(lines[0])),
+            instruction: (
+                abstract_instruction_quad[0] as u8,
+                [
+                    abstract_instruction_quad[1],
+                    abstract_instruction_quad[2],
+                    abstract_instruction_quad[3],
+                ],
+            ),
+            after: parse_quad(", ", &Sample::infix(lines[2])),
+        }
+    }
+
+    fn infix(s: &str) -> String {
+        let prefix_length = "Before: [".len();
+        s[prefix_length..s.len() - 1].to_string()
+    }
+}
+
+fn parse_quad(sep: &str, src: &str) -> [Val; 4] {
+    src.split(sep)
+        .map(|s| s.parse::<Val>().unwrap())
+        .collect::<Vec<Val>>()
+        .try_into()
+        .unwrap()
 }
 
 fn main() {
