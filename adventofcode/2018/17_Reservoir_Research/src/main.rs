8c3baf036b1a7a440cf7b37ad1d80e430503181b
Ivan Krechetov <ikr@ikr.su>
2024-01-13 15:50:09 +0100

    Rasterize

--- adventofcode/2018/17_Reservoir_Research/src/main.rs
+++ adventofcode/2018/17_Reservoir_Research/src/main.rs
@@ -23,8 +23,28 @@ fn parse_tri(src: &str) -> (i32, i32, i32) {
     )
 }
 
+fn parse_vertical_line(src: &str) -> Vec<Coord> {
+    assert!(src.starts_with('x'));
+    let (x, y1, y2) = parse_tri(src);
+    (y1..=y2).into_iter().map(|y| Coord(x, y)).collect()
+}
+
+fn parse_horizontal_line(src: &str) -> Vec<Coord> {
+    assert!(src.starts_with('y'));
+    let (y, x1, x2) = parse_tri(src);
+    (x1..=x2).into_iter().map(|x| Coord(x, y)).collect()
+}
+
+fn parse_src_line(src: &str) -> Vec<Coord> {
+    if src.starts_with('x') {
+        parse_vertical_line(src)
+    } else {
+        parse_horizontal_line(src)
+    }
+}
+
 fn main() {
     for line in io::stdin().lock().lines() {
-        eprintln!("{:?}", parse_tri(&line.unwrap()))
+        eprintln!("{:?}", parse_src_line(&line.unwrap()))
     }
 }
