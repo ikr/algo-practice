90e1b0386a8f9fc2a0b20a14ed5f3c2ed6699d54
Ivan Krechetov <ikr@ikr.li>
2024-01-15 20:36:38 +0100

    Solve part 1

--- adventofcode/2018/18_Settlers_of_The_North_Pole/src/main.rs
+++ adventofcode/2018/18_Settlers_of_The_North_Pole/src/main.rs
@@ -1,11 +1,97 @@
 use std::io::{self, BufRead};
 
+fn dbg_grid(grid: &[Vec<char>]) {
+    for row in grid {
+        eprintln!("{}", row.iter().collect::<String>());
+    }
+}
+
+fn safe_at(grid: &[Vec<char>], ro: i32, co: i32) -> char {
+    let h = grid.len() as i32;
+    assert!(!grid.is_empty());
+    let w = grid[0].len() as i32;
+    if 0 <= ro && ro < h && 0 <= co && co < w {
+        grid[ro as usize][co as usize]
+    } else {
+        ' '
+    }
+}
+
+fn neighs_count(grid: &[Vec<char>], kind: char, ro: i32, co: i32) -> i32 {
+    let mut count = 0;
+    for dr in -1..=1 {
+        for dc in -1..=1 {
+            if dr == 0 && dc == 0 {
+                continue;
+            }
+            if safe_at(grid, ro + dr, co + dc) == kind {
+                count += 1;
+            }
+        }
+    }
+    count
+}
+
+fn evolve(grid: &[Vec<char>]) -> Vec<Vec<char>> {
+    let h = grid.len();
+    assert!(!grid.is_empty());
+    let w = grid[0].len();
+    let mut result = vec![vec![' '; w]; h];
+
+    for ro in 0..h {
+        for co in 0..w {
+            let u = grid[ro][co];
+            result[ro][co] = u;
+
+            if u == '.' {
+                if neighs_count(grid, '|', ro as i32, co as i32) >= 3 {
+                    result[ro][co] = '|';
+                }
+            } else if u == '|' {
+                if neighs_count(grid, '#', ro as i32, co as i32) >= 3 {
+                    result[ro][co] = '#';
+                }
+            } else {
+                assert!(u == '#');
+                if neighs_count(grid, '#', ro as i32, co as i32) >= 1
+                    && neighs_count(grid, '|', ro as i32, co as i32) >= 1
+                {
+                    result[ro][co] = '#';
+                } else {
+                    result[ro][co] = '.';
+                }
+            }
+        }
+    }
+
+    result
+}
+
+fn resource_value(grid: &[Vec<char>]) -> i32 {
+    let mut trees = 0;
+    let mut lumberyards = 0;
+    for row in grid {
+        for u in row {
+            if *u == '|' {
+                trees += 1;
+            } else if *u == '#' {
+                lumberyards += 1;
+            }
+        }
+    }
+    trees * lumberyards
+}
+
 fn main() {
-    let initial_grid: Vec<Vec<char>> = io::stdin()
+    let mut grid: Vec<Vec<char>> = io::stdin()
         .lock()
         .lines()
         .map(|line| line.unwrap().chars().collect())
         .collect();
 
-    eprintln!("{:?}", initial_grid);
+    for _ in 0..10 {
+        grid = evolve(&grid);
+    }
+    dbg_grid(&grid);
+    println!("{}", resource_value(&grid));
 }
