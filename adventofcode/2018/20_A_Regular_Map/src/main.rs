8932dbe40067c52f0bd3dac140c73b6c119d73a0
Ivan Krechetov <ikr@ikr.su>
2024-01-26 07:55:48 +0100

    Dir and Vert

--- adventofcode/2018/20_A_Regular_Map/src/main.rs
+++ adventofcode/2018/20_A_Regular_Map/src/main.rs
@@ -1,5 +1,37 @@
 use std::io;
 
+enum Dir {
+    N,
+    E,
+    S,
+    W,
+}
+
+impl Dir {
+    fn decode(c: char) -> Self {
+        match c {
+            'N' => Dir::N,
+            'E' => Dir::E,
+            'S' => Dir::S,
+            'W' => Dir::W,
+            _ => panic!("Invalid Dir {}", c),
+        }
+    }
+}
+
+struct Vert(i32, i32);
+
+impl Vert {
+    fn move_to(&self, dir: &Dir) -> Self {
+        match dir {
+            Dir::N => Vert(self.0, self.1 + 1),
+            Dir::E => Vert(self.0 + 1, self.1),
+            Dir::S => Vert(self.0, self.1 - 1),
+            Dir::W => Vert(self.0 - 1, self.1),
+        }
+    }
+}
+
 fn read_program() -> Vec<char> {
     let mut line = String::new();
     io::stdin().read_line(&mut line).unwrap();
