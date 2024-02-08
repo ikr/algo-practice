403cfedde8922ea34ee00f8b5c3c034d86f19cc3
Ivan Krechetov <ikr@ikr.li>
2024-02-08 07:41:35 +0100

    Parse the input

---
+++ adventofcode/2018/23_Experimental_Emergency_Teleportation/src/main.rs
@@ -0,0 +1,48 @@
+use regex::Regex;
+use std::io::{self, BufRead};
+
+type Xyz = [i32; 3];
+
+fn inof(src: &str) -> i32 {
+    src.parse().unwrap()
+}
+
+#[derive(Debug)]
+struct Bot {
+    position: Xyz,
+    signal_radius: i32,
+}
+
+impl Bot {
+    fn parse(src: &str) -> Bot {
+        let re = Regex::new(r"^pos=<(-?\d+),(-?\d+),(-?\d+)>, r=(\d+)$").unwrap();
+        let caps = re.captures(src).unwrap();
+        Bot {
+            position: [inof(&caps[1]), inof(&caps[2]), inof(&caps[3])],
+            signal_radius: inof(&caps[4]),
+        }
+    }
+
+    fn distance(&self, a: &Xyz) -> i32 {
+        let mut result: i32 = 0;
+        for i in 0..3 {
+            result += (self.position[i] - a[i]).abs();
+        }
+        result
+    }
+
+    fn in_range(&self, a: &Xyz) -> bool {
+        self.distance(a) <= self.signal_radius
+    }
+}
+
+fn main() {
+    let mut bots: Vec<Bot> = io::stdin()
+        .lock()
+        .lines()
+        .map(|l| Bot::parse(&l.unwrap()))
+        .collect();
+
+    bots.sort_by_key(|b| -b.signal_radius);
+    eprintln!("{:?}", bots);
+}
