b7c5651551ded0dc755dd61ae0c813cb3d52394d
Ivan Krechetov <ikr@ikr.li>
2024-02-18 10:42:29 +0100

    Parse the Receptivity

--- adventofcode/2018/24_Immune_System_Simulator_20XX/src/main.rs
+++ adventofcode/2018/24_Immune_System_Simulator_20XX/src/main.rs
@@ -1,3 +1,55 @@
+#[derive(Debug)]
+struct AttackType {
+    name: String,
+}
+
+impl AttackType {
+    fn parse_csv(src: &str) -> Vec<Self> {
+        src.split(", ")
+            .map(|token| AttackType {
+                name: token.to_string(),
+            })
+            .collect()
+    }
+}
+
+#[derive(Debug)]
+struct Receptivity {
+    immune_to: Vec<AttackType>,
+    weak_to: Vec<AttackType>,
+}
+
+const WEAK_TO_PREFIX: &str = "weak to ";
+const IMMUNE_TO_PREFIX: &str = "immune to ";
+
+impl Receptivity {
+    fn parse(src: &str) -> Self {
+        let parts: Vec<&str> = src.split("; ").collect();
+        let mut immune_to: Vec<AttackType> = vec![];
+        let mut weak_to: Vec<AttackType> = vec![];
+
+        for part in parts {
+            if part.starts_with(IMMUNE_TO_PREFIX) {
+                immune_to.append(&mut AttackType::parse_csv(&part[IMMUNE_TO_PREFIX.len()..]))
+            } else if part.starts_with(WEAK_TO_PREFIX) {
+                weak_to.append(&mut AttackType::parse_csv(&part[WEAK_TO_PREFIX.len()..]))
+            }
+        }
+
+        Self { immune_to, weak_to }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    eprintln!("{:?}", Receptivity::parse(""));
+    eprintln!("{:?}", Receptivity::parse("immune to slashing"));
+    eprintln!("{:?}", Receptivity::parse("weak to slashing, cold"));
+    eprintln!(
+        "{:?}",
+        Receptivity::parse("weak to radiation; immune to fire, cold")
+    );
+    eprintln!(
+        "{:?}",
+        Receptivity::parse("immune to cold; weak to bludgeoning")
+    );
 }
