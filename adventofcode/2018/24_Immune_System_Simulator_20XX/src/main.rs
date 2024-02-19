88dcf0c98d0c12b013c5a5e4d7ea0d8a966b9984
Ivan Krechetov <ikr@ikr.su>
2024-02-19 07:53:15 +0100

    Parse the groups

--- adventofcode/2018/24_Immune_System_Simulator_20XX/src/main.rs
+++ adventofcode/2018/24_Immune_System_Simulator_20XX/src/main.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 #[derive(Debug)]
 struct AttackType {
     name: String,
@@ -29,14 +31,59 @@ impl Receptivity {
         let mut weak_to: Vec<AttackType> = vec![];
 
         for part in parts {
-            if part.starts_with(IMMUNE_TO_PREFIX) {
-                immune_to.append(&mut AttackType::parse_csv(&part[IMMUNE_TO_PREFIX.len()..]))
-            } else if part.starts_with(WEAK_TO_PREFIX) {
-                weak_to.append(&mut AttackType::parse_csv(&part[WEAK_TO_PREFIX.len()..]))
+            if let Some(infix) = part.strip_prefix(IMMUNE_TO_PREFIX) {
+                immune_to.append(&mut AttackType::parse_csv(infix));
+            } else if let Some(infix) = part.strip_prefix(WEAK_TO_PREFIX) {
+                weak_to.append(&mut AttackType::parse_csv(infix))
             }
         }
 
         Self { immune_to, weak_to }
+    }
+}
+
+fn inof(src: &str) -> i32 {
+    src.parse().unwrap()
+}
+
+#[derive(Debug)]
+struct Group {
+    units: i32,
+    unit_hit_points: i32,
+    receptivity: Receptivity,
+    unit_damage: i32,
+    attack_type: AttackType,
+    initiative: i32,
+}
+
+impl Group {
+    fn parse(src: &str) -> Self {
+        let r_src = receptivity_source(src);
+
+        let re = Regex::new(
+            r"^(\d+) units each with (\d+) hit points.* with an attack that does (\d+) ([a-z]+) damage at initiative (\d+)$",
+        ).unwrap();
+        let caps = re.captures(src).unwrap();
+
+        Self {
+            units: inof(&caps[1]),
+            unit_hit_points: inof(&caps[2]),
+            receptivity: Receptivity::parse(&r_src),
+            unit_damage: inof(&caps[3]),
+            attack_type: AttackType {
+                name: caps[4].to_string(),
+            },
+            initiative: inof(&caps[5]),
+        }
+    }
+}
+
+fn receptivity_source(group_source: &str) -> String {
+    if let Some(a) = group_source.find('(') {
+        let b = group_source.find(')').unwrap();
+        group_source[a + 1..b].to_string()
+    } else {
+        "".to_string()
     }
 }
 
@@ -55,5 +102,14 @@ fn main() {
 
     let whole_input: String = std::io::read_to_string(std::io::stdin()).unwrap();
     let team_sources: Vec<&str> = whole_input.split("\n\n").collect();
-    eprintln!("{:?}", team_sources);
+
+    for tsrc in team_sources {
+        let lines: Vec<&str> = tsrc.split('\n').filter(|x| !x.is_empty()).collect();
+        eprintln!("{}", lines[0]);
+
+        for line in &lines[1..] {
+            let g = Group::parse(line);
+            eprintln!("{:?}", g);
+        }
+    }
 }
