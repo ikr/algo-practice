ec8a64fe4aa8e2fdfe3a3ad87627f3fd3fa132f0
Ivan Krechetov <ikr@ikr.su>
2024-02-25 16:07:41 +0100

    Solve part 2

--- adventofcode/2017/02_Corruption_Checksum/src/main.rs
+++ adventofcode/2017/02_Corruption_Checksum/src/main.rs
@@ -11,11 +11,30 @@ fn read_input() -> Vec<Vec<i32>> {
     input.lines().map(parse_line).collect()
 }
 
+fn whole_division_result(xs: &[i32]) -> i32 {
+    for (i, x) in xs.iter().enumerate() {
+        for j in i + 1..xs.len() {
+            let y = xs[j];
+            if x % y == 0 {
+                return x / y;
+            }
+            if y % x == 0 {
+                return y / x;
+            }
+        }
+    }
+    panic!("Not found");
+}
+
 fn main() {
     let xss = read_input();
+
     let part_1_result: i32 = xss
         .iter()
         .map(|xs| xs.iter().max().unwrap() - xs.iter().min().unwrap())
         .sum();
     println!("{}", part_1_result);
+
+    let part_2_result: i32 = xss.iter().map(|xs| whole_division_result(xs)).sum();
+    println!("{}", part_2_result);
 }
