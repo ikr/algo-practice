f34d744e70eb72a2d7f059e5d6166bdad8ede1f9
Ivan Krechetov <ikr@ikr.su>
2024-02-25 09:11:36 +0100

    Solve part 1

---
+++ adventofcode/2017/01_Inverse_Captcha/src/main.rs
@@ -0,0 +1,55 @@
+fn run_length_encoding(xs: &[u32]) -> Vec<(u32, u32)> {
+    let mut result: Vec<(u32, u32)> = vec![(xs[0], 1)];
+
+    for &x in &xs[1..] {
+        if x == result.last().unwrap().0 {
+            result.last_mut().unwrap().1 += 1;
+        } else {
+            result.push((x, 1));
+        }
+    }
+    result
+}
+
+fn cycle_the_chain(mut xs: Vec<(u32, u32)>) -> Vec<(u32, u32)> {
+    if xs.len() < 2 {
+        xs
+    } else {
+        if xs[0].0 == xs.last().unwrap().0 {
+            xs[0].1 += xs.last().unwrap().1;
+            xs.pop();
+        }
+        xs
+    }
+}
+
+fn part_1_sum(xs: &[(u32, u32)]) -> u32 {
+    match xs.len() {
+        0 => 0,
+        1 => {
+            if xs[0].1 > 1 {
+                xs[0].0 * xs[0].1
+            } else {
+                0
+            }
+        }
+        _ => xs
+            .iter()
+            .filter(|(_, k)| *k > 1)
+            .fold(0u32, |acc, (x, k)| acc + (k - 1u32) * x),
+    }
+}
+
+fn main() {
+    let digits: Vec<u32> = std::io::read_to_string(std::io::stdin())
+        .unwrap()
+        .trim()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect();
+
+    println!(
+        "{}",
+        part_1_sum(&cycle_the_chain(run_length_encoding(&digits)))
+    );
+}
