a05ef2f2e5d166e79a84343b840535bc693d2b7e
Ivan Krechetov <ikr@ikr.li>
2024-02-29 07:07:22 +0100

    Solve part 1

--- adventofcode/2017/03_Spiral_Memory/src/main.rs
+++ adventofcode/2017/03_Spiral_Memory/src/main.rs
@@ -1,13 +1,13 @@
-fn containing_diameter(n: u32) -> u32 {
-    let sqr = (n as f64).sqrt() as u32;
-    let mut result = sqr.saturating_sub(2);
-    while result % 2 == 0 || result * result < n {
+fn containing_diameter(n: i64) -> i64 {
+    let sqr = (n as f64).sqrt() as i64;
+    let mut result = sqr - 2;
+    while result < 0 || result % 2 == 0 || result * result < n {
         result += 1;
     }
     result
 }
 
-fn lowest_on_perimeter(d: u32) -> u32 {
+fn lowest_on_perimeter(d: i64) -> i64 {
     assert!(d > 0 && d % 2 == 1);
     if d == 1 {
         1
@@ -16,18 +16,22 @@ fn lowest_on_perimeter(d: u32) -> u32 {
     }
 }
 
-fn steps_to_access_port(n: u32) -> u32 {
+fn steps_to_access_port(n: i64) -> i64 {
     if n == 1 {
         0
     } else {
         let d = containing_diameter(n);
         let lo = lowest_on_perimeter(d);
         let nn = (n - lo) % (d - 1);
+
+        assert!((d - 1) % 2 == 0);
+        let side_center = (d - 1) / 2 - 1;
+        d / 2 + (nn - side_center).abs()
     }
 }
 
 fn main() {
-    let n: u32 = std::io::read_to_string(std::io::stdin())
+    let n: i64 = std::io::read_to_string(std::io::stdin())
         .unwrap()
         .trim()
         .parse()
