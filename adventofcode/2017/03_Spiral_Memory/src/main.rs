8217ae6f624cee5b59116598b3ca034682ae8422
Ivan Krechetov <ikr@ikr.li>
2024-02-29 07:00:10 +0100

    Go on

--- adventofcode/2017/03_Spiral_Memory/src/main.rs
+++ adventofcode/2017/03_Spiral_Memory/src/main.rs
@@ -7,11 +7,30 @@ fn containing_diameter(n: u32) -> u32 {
     result
 }
 
+fn lowest_on_perimeter(d: u32) -> u32 {
+    assert!(d > 0 && d % 2 == 1);
+    if d == 1 {
+        1
+    } else {
+        (d - 2) * (d - 2) + 1
+    }
+}
+
+fn steps_to_access_port(n: u32) -> u32 {
+    if n == 1 {
+        0
+    } else {
+        let d = containing_diameter(n);
+        let lo = lowest_on_perimeter(d);
+        let nn = (n - lo) % (d - 1);
+    }
+}
+
 fn main() {
     let n: u32 = std::io::read_to_string(std::io::stdin())
         .unwrap()
         .trim()
         .parse()
         .unwrap();
-    eprintln!("{}", containing_diameter(n));
+    println!("{}", steps_to_access_port(n));
 }
