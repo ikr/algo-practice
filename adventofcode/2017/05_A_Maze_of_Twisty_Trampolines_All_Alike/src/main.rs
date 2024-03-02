35e20ae139e75b9ee67f3680254d05f68bb83df6
Ivan Krechetov <ikr@ikr.su>
2024-03-02 09:21:13 +0100

    Solve part 1

--- adventofcode/2017/05_A_Maze_of_Twisty_Trampolines_All_Alike/src/main.rs
+++ adventofcode/2017/05_A_Maze_of_Twisty_Trampolines_All_Alike/src/main.rs
@@ -1,3 +1,18 @@
+fn solve_part_1(mut xs: Vec<i32>) {
+    let mut steps: u32 = 0;
+    let mut ip: i32 = 0;
+    loop {
+        steps += 1;
+        let next_ip = ip + xs[ip as usize];
+        if next_ip < 0 || next_ip >= xs.len() as i32 {
+            break;
+        }
+        xs[ip as usize] += 1;
+        ip = next_ip;
+    }
+    println!("{}", steps);
+}
+
 fn main() {
     let xs: Vec<i32> = std::io::read_to_string(std::io::stdin())
         .unwrap()
@@ -5,5 +20,5 @@ fn main() {
         .map(|x| x.parse().unwrap())
         .collect();
 
-    eprintln!("{:?}", xs);
+    solve_part_1(xs);
 }
