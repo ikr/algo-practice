9cc408398519289d148e2c4d7a9926b415d2fee1
Ivan Krechetov <ikr@ikr.su>
2024-03-02 07:28:33 +0100

    Fix a typo

--- adventofcode/2017/04_High-Entropy_Passphrases/src/main.rs
+++ adventofcode/2017/04_High-Entropy_Passphrases/src/main.rs
@@ -2,7 +2,7 @@ fn line_words(line: &str) -> Vec<String> {
     line.split_whitespace().map(|x| x.to_string()).collect()
 }
 
-fn unique_words_coung(xs: &[String]) -> usize {
+fn unique_words_count(xs: &[String]) -> usize {
     let mut seen = std::collections::HashSet::new();
     for x in xs {
         seen.insert(x);
@@ -20,7 +20,7 @@ fn main() {
     println!(
         "{}",
         xss.iter()
-            .filter(|xs| unique_words_coung(xs) == xs.len())
+            .filter(|xs| unique_words_count(xs) == xs.len())
             .count()
     );
 }
