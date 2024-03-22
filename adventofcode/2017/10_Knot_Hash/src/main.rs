2b00cf1b56d9b93db562d696677528735c5898b7
Ivan Krechetov <ikr@ikr.su>
2024-03-22 07:51:15 +0100

    Solve part 1

--- adventofcode/2017/10_Knot_Hash/src/main.rs
+++ adventofcode/2017/10_Knot_Hash/src/main.rs
@@ -24,8 +24,10 @@ fn main() {
     let n = xs.len();
 
     for l in lengths {
-        let j = (i + n + l - 1) % n;
-        tricky_reverse(&mut xs, i, j);
+        if l > 0 {
+            let j = (i + n + l - 1) % n;
+            tricky_reverse(&mut xs, i, j);
+        }
         i = (i + l + skip) % n;
         skip += 1;
     }
