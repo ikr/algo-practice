74fdabed39f5fefd224f3857651ccbbbbf870a46
Ivan Krechetov <ikr@ikr.su>
2024-03-22 07:49:31 +0100

    WA: 12644 is too low

--- adventofcode/2017/10_Knot_Hash/src/main.rs
+++ adventofcode/2017/10_Knot_Hash/src/main.rs
@@ -1,10 +1,34 @@
+fn tricky_reverse(xs: &mut [usize], mut i: usize, mut j: usize) {
+    let n = xs.len();
+    while i != j {
+        xs.swap(i, j);
+        i = (i + 1) % n;
+        if i == j {
+            break;
+        }
+        j = (j + n - 1) % n;
+    }
+}
+
 fn main() {
-    let xs: Vec<i32> = std::io::read_to_string(std::io::stdin())
+    let lengths: Vec<usize> = std::io::read_to_string(std::io::stdin())
         .unwrap()
         .trim()
         .split(',')
         .map(|x| x.parse().unwrap())
         .collect();
 
-    eprintln!("{:?}", xs);
+    let mut xs: Vec<usize> = (0..256).collect();
+    let mut i: usize = 0;
+    let mut skip: usize = 0;
+    let n = xs.len();
+
+    for l in lengths {
+        let j = (i + n + l - 1) % n;
+        tricky_reverse(&mut xs, i, j);
+        i = (i + l + skip) % n;
+        skip += 1;
+    }
+
+    println!("{}", xs[0] * xs[1]);
 }
