8b3f230f0e7fa7bebc1f241c22f4892645edf95e
Ivan Krechetov <ikr@ikr.li>
2024-03-23 07:51:12 +0100

    Make `skip` a loop-internal

--- adventofcode/2017/10_Knot_Hash/src/main.rs
+++ adventofcode/2017/10_Knot_Hash/src/main.rs
@@ -15,19 +15,21 @@ fn solve_part_1(input: &str) -> usize {
 
     let mut xs: Vec<usize> = (0..256).collect();
     let mut i: usize = 0;
-    let mut skip: usize = 0;
     let n = xs.len();
 
-    for l in lengths {
-        if l > 0 {
+    for (skip, l) in lengths.iter().enumerate() {
+        if *l > 0 {
             let j = (i + n + l - 1) % n;
             tricky_reverse(&mut xs, i, j);
         }
         i = (i + l + skip) % n;
-        skip += 1;
     }
 
     xs[0] * xs[1]
+}
+
+fn solve_part_2(input: &str) -> String {
+    todo!()
 }
 
 fn main() {
@@ -37,4 +39,5 @@ fn main() {
         .to_string();
 
     println!("{}", solve_part_1(&input));
+    println!("{}", solve_part_2(&input));
 }
