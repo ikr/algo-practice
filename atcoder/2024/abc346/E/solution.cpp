115593c93dc15eb1eef80d9ffd67f8464da30b88
Ivan Krechetov <ikr@ikr.su>
2024-03-23 14:59:02 +0100

    Read the input

--- atcoder/2024/abc346/E/solution.cpp
+++ atcoder/2024/abc346/E/solution.cpp
@@ -1,7 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T> constexpr int inof(const T x) { return static_cast<int>(x); }
+enum class OpCode { Ro = 1, Co = 2 };
+
+struct Op final {
+    OpCode opcode;
+    int i;
+    int x;
+};
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
@@ -9,10 +19,18 @@ int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
 
-    int t;
-    cin >> t;
-    while (t--) {
+    int H, W, M;
+    cin >> H >> W >> M;
+
+    vector<Op> ops(M);
+    for (auto &[opcode, i, x] : ops) {
+        int t;
+        cin >> t;
+        opcode = static_cast<OpCode>(t);
+        cin >> i >> x;
+        --i;
     }
+    reverse(begin(ops), end(ops));
 
     return 0;
 }
