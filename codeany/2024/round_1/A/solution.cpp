dce713357ad9d9ca357734c9548a475ff45bb2a9
Ivan Krechetov <ikr@ikr.su>
2024-04-14 15:49:54 +0200

    AC on A & B

--- codeany/2024/round_1/A/solution.cpp
+++ codeany/2024/round_1/A/solution.cpp
@@ -1,11 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool divides_just_once(const int d, const int x) {
-    assert(x % d == 0);
-    return (x / d) % d;
-}
-
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
@@ -16,12 +11,10 @@ int main() {
     int result{};
     for (int x = a; x <= b; ++x) {
         for (const auto d : {2, 3, 4, 6, 7}) {
-            bool divs{false};
             if (x % d == 0) {
-                divs = true;
-                if (!divides_just_once(d, x)) break;
+                ++result;
+                break;
             }
-            result += divs;
         }
     }
     cout << result << '\n';
