ee8b2aae2270a991d74eec6efb469e06d8b8b9e7
Ivan Krechetov <ikr@ikr.su>
2024-04-20 07:54:50 +0200

    Solve part 2
    Suuuuuper slow. Don't think it's a proper implementation of an order statistic tree.

--- adventofcode/2017/17_Spinlock/src/main.rs
+++ adventofcode/2017/17_Spinlock/src/main.rs
@@ -1,5 +1,6 @@
 use indexset::BTreeMap;
-use num_rational::Rational64;
+use num_bigint::BigInt;
+use num_rational::BigRational;
 
 fn solve_part_1(jump_distance: usize) -> usize {
     let mut buf: Vec<usize> = vec![0, 1];
@@ -15,15 +16,33 @@ fn solve_part_1(jump_distance: usize) -> usize {
 }
 
 fn solve_part_2(jump_distance: usize) -> u32 {
-    let mut buf: BTreeMap<Rational64, u32> =
-        BTreeMap::from_iter([(Rational64::from(0), 0), (Rational64::from(1), 1)]);
+    let mut buf: BTreeMap<BigRational, u32> = BTreeMap::from_iter([
+        (BigRational::from(BigInt::from(1_000_000)), 0),
+        (BigRational::from(BigInt::from(1_000_000_000)), 1),
+    ]);
     let mut i: usize = 1;
-    let mut y: u32 = 0;
-    for x in 2usize..=2017usize {
+    let mut y: u32 = 1;
+    for x in 2u32..=50000000u32 {
         i += jump_distance + 1;
-        i %= x;
+        i %= x as usize;
+        assert!(x as usize == buf.len());
 
-        let key = buf.get_index(i).unwrap().0;
+        let key_b: BigRational = buf.get_index(i).unwrap().0.clone();
+        let key_a: BigRational = if i == 0 {
+            &key_b - BigInt::from(1_000_000)
+        } else {
+            buf.get_index(i - 1).unwrap().0.clone()
+        };
+
+        if x % 100_000 == 0 {
+            eprintln!("{}", x);
+        }
+
+        buf.insert((key_a + key_b) / BigRational::from(BigInt::from(2)), x);
+        let n = buf.len();
+        if *buf.get_index((i + n - 1) % n).unwrap().1 == 0 {
+            y = x
+        };
     }
     y
 }
