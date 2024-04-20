7b6921a7b0070119594bc1b851fdfb5bf583a4e1
Ivan Krechetov <ikr@ikr.li>
2024-04-20 15:20:39 +0200

    First draft

--- atcoder/2024/abc350/E/solution.cpp
+++ atcoder/2024/abc350/E/solution.cpp
@@ -1,18 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T> constexpr int inof(const T x) { return static_cast<int>(x); }
+using ll = long long;
 
-template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+static constexpr double Eps = 0.000000000000001;
+
+template <typename T> constexpr double doof(const T x) {
+    return static_cast<double>(x);
+}
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
+    cout << setprecision(7) << fixed;
 
-    int t;
-    cin >> t;
-    while (t--) {
-    }
+    ll N;
+    cin >> N;
 
+    int A;
+    cin >> A;
+
+    int X, Y;
+    cin >> X >> Y;
+
+    const auto recur = [&](const auto self, const double p0,
+                           const ll n) -> double {
+        if (!n || p0 < Eps) return 0;
+        vector<double> o(7, 0);
+        o[0] = X + self(self, p0, n / A);
+        for (ll i = 1; i <= 6; ++i) {
+            o[i] = Y + self(self, p0 / 6.0, n / i);
+        }
+        return p0 * (*min_element(cbegin(o), cend(o)));
+    };
+
+    cout << recur(recur, 1.0, N) << '\n';
     return 0;
 }
