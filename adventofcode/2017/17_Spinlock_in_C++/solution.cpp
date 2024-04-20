6ad8d6dbbca67cb297097a295373e22a92c8faab
Ivan Krechetov <ikr@ikr.li>
2024-04-20 12:55:06 +0200

    Faster: a bit over 2 minutes

--- adventofcode/2017/17_Spinlock_in_C++/solution.cpp
+++ adventofcode/2017/17_Spinlock_in_C++/solution.cpp
@@ -1,6 +1,4 @@
 #include <bits/stdc++.h>
-#include <boost/multiprecision/cpp_int.hpp>
-#include <boost/rational.hpp>
 using namespace std;
 
 #include <ext/pb_ds/assoc_container.hpp>
@@ -10,9 +8,6 @@ template <typename T>
 using ordered_set =
     tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
-using lll = boost::multiprecision::cpp_int;
-using Ratio = boost::rational<lll>;
-
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
@@ -20,9 +15,9 @@ template <typename T> constexpr int inof(const T x) {
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
 int solve_part_2(const int jump_distance) {
-    ordered_set<pair<Ratio, int>> xs;
-    xs.insert({Ratio(1'000'000), 0});
-    xs.insert({Ratio(1'000'000'000), 1});
+    ordered_set<pair<double, int>> xs;
+    xs.insert({1'000'000, 0});
+    xs.insert({1'000'000'000, 1});
 
     int y{1};
     for (int x = 2, i = 1; x <= 50000000; ++x) {
@@ -32,8 +27,8 @@ int solve_part_2(const int jump_distance) {
 
         const auto key_b = xs.find_by_order(i)->first;
         const auto key_a =
-            (i == 0) ? (key_b - 1'000'000) : (xs.find_by_order(i - 1)->first);
-        xs.insert({(key_a + key_b) / Ratio(2), x});
+            (i == 0) ? (key_b - 1'000'000.0) : (xs.find_by_order(i - 1)->first);
+        xs.insert({(key_a + key_b) / 2.0, x});
 
         if (x % 100'000 == 0) cerr << x << endl;
 
