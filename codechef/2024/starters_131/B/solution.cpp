2f27b8241e7e31c0b9400a0e83fe0049cf299f98
Ivan Krechetov <ikr@ikr.li>
2024-04-24 17:48:45 +0200

    Another iteration

--- codechef/2024/starters_131/B/solution.cpp
+++ codechef/2024/starters_131/B/solution.cpp
@@ -15,21 +15,27 @@ template <typename T> constexpr int inof(const T x) {
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-ll opt_diff(const vector<ll> &A, const ll K) {
+ll opt_diff(vector<ll> A, const ll K) {
     const auto T = A.back();
-    atcoder::segtree<ll, lo_op, lo_e> t_lo(A);
-    atcoder::segtree<ll, hi_op, hi_e> t_hi(A);
-
-    ll result = t_hi.all_prod() - t_lo.all_prod();
     for (int i = 0; i < sz(A); ++i) {
         const ll q = (T - A[i]) / K;
-        const auto one = A[i] + q * K;
-        const auto two = A[i] + (q + 1) * K;
+        const auto a = A[i] + q * K;
+        A[i] = a;
+    }
+    sort(begin(A), end(A));
 
-        for (const auto a : {one, two}) {
-            t_lo.set(i, a);
-            t_hi.set(i, a);
-            result = min(result, t_hi.all_prod() - t_lo.all_prod());
+    atcoder::segtree<ll, lo_op, lo_e> t_lo(A);
+    atcoder::segtree<ll, hi_op, hi_e> t_hi(A);
+    ll result = t_hi.all_prod() - t_lo.all_prod();
+    for (int i = 0; i < sz(A); ++i) {
+        t_lo.set(i, A[i] + K);
+        t_hi.set(i, A[i] + K);
+        const ll result_ = t_hi.all_prod() - t_lo.all_prod();
+
+        if (result_ < result) {
+            result = result_;
+        } else {
+            break;
         }
     }
     return result;
@@ -48,7 +54,7 @@ int main() {
         vector<ll> A(N);
         for (auto &a : A) cin >> a;
         sort(begin(A), end(A));
-        cout << opt_diff(A, K) << '\n';
+        cout << opt_diff(std::move(A), K) << '\n';
     }
 
     return 0;
