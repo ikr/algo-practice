1be771f2174a56ec307187054ff66eba04781139
Ivan Krechetov <ikr@ikr.li>
2024-04-24 17:25:14 +0200

    WA

--- codechef/2024/starters_131/B/solution.cpp
+++ codechef/2024/starters_131/B/solution.cpp
@@ -1,17 +1,13 @@
+#include <atcoder/segtree>
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T> ostream &operator<<(ostream &os, const multiset<T> &xs) {
-    os << '{';
-    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
-        if (i != xs.cbegin()) os << ' ';
-        os << *i;
-    }
-    os << '}';
-    return os;
-}
-
 using ll = long long;
+
+ll lo_op(ll a, ll b) { return min(a, b); }
+ll lo_e() { return (ll)(1e9); }
+ll hi_op(ll a, ll b) { return max(a, b); }
+ll hi_e() { return 0; }
 
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
@@ -20,19 +16,22 @@ template <typename T> constexpr int inof(const T x) {
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
 ll opt_diff(const vector<ll> &A, const ll K) {
-    multiset<ll> xs(cbegin(A), cend(A));
-    ll result = *crbegin(xs) - *cbegin(xs);
+    const auto T = A.back();
+    atcoder::segtree<ll, lo_op, lo_e> t_lo(A);
+    atcoder::segtree<ll, hi_op, hi_e> t_hi(A);
 
-    for (;;) {
-        const auto x = *cbegin(xs);
-        xs.erase(cbegin(xs));
-        xs.insert(x + K);
-        cerr << xs << endl;
-        const ll result_ = *crbegin(xs) - *cbegin(xs);
-        if (result_ > result) break;
-        result = min(result, result_);
+    ll result = t_hi.all_prod() - t_lo.all_prod();
+    for (int i = 0; i < sz(A); ++i) {
+        const ll q = (T - A[i]) / K;
+        const auto one = A[i] + q * K;
+        const auto two = A[i] + (q + 1) * K;
+
+        for (const auto a : {one, two}) {
+            t_lo.set(i, a);
+            t_hi.set(i, a);
+            result = min(result, t_hi.all_prod() - t_lo.all_prod());
+        }
     }
-
     return result;
 }
 
@@ -48,6 +47,7 @@ int main() {
 
         vector<ll> A(N);
         for (auto &a : A) cin >> a;
+        sort(begin(A), end(A));
         cout << opt_diff(A, K) << '\n';
     }
 
