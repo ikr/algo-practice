7bcdefb677854489658604e04ca0ceb0731cb67a
Ivan Krechetov <ikr@ikr.li>
2024-04-27 16:47:12 +0200

    More def-s

--- adventofcode/2017/18_Duet/src/main.rs
+++ adventofcode/2017/18_Duet/src/main.rs
@@ -8,9 +8,26 @@ enum OpCode {
     Jgz,
 }
 
+struct LvalReg(char);
+
+impl LvalReg {
+    fn decode(xs: &str) -> LvalReg {
+        match xs.chars().collect::<Vec<char>>()[..] {
+            [x, ..] if x.is_ascii_lowercase() => LvalReg(x),
+            _ => panic!("{} is not a Reg literal", xs),
+        }
+    }
+}
+
 enum Rval {
     Reg(char),
     Int(i32),
+}
+
+struct Op {
+    op_code: OpCode,
+    lval_reg: LvalReg,
+    rval: Rval,
 }
 
 fn main() {
