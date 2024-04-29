b468b4bb32cca8b80161fba375b3b38ddc6950f6
Ivan Krechetov <ikr@ikr.li>
2024-04-29 18:16:54 +0200

    Elaborate

--- adventofcode/2017/19_A_Series_of_Tubes/src/main.rs
+++ adventofcode/2017/19_A_Series_of_Tubes/src/main.rs
@@ -1,5 +1,33 @@
 use std::io::{self, BufRead};
 
+#[derive(Debug, Clone, Copy)]
+struct Roco(usize, usize);
+
+fn index_of_entrance(top_row: &[char]) -> usize {
+    top_row.iter().position(|&x| x == '|').unwrap()
+}
+
+fn dimensions(grid: &[Vec<char>]) -> Roco {
+    Roco(grid.len(), grid[0].len())
+}
+
+fn vertex_index(grid_width: usize, roco: Roco) -> usize {
+    assert!(roco.1 < grid_width);
+    grid_width * roco.0 + roco.1
+}
+
+fn grid_roco(grid_width: usize, v_idx: usize) -> Roco {
+    Roco(v_idx / grid_width, v_idx % grid_width)
+}
+
+fn at(grid: &[Vec<char>], roco: Roco) -> char {
+    let Roco(h, w) = dimensions(&grid);
+    let Roco(ro, co) = roco;
+    assert!(ro < h);
+    assert!(co < w);
+    grid[ro][co]
+}
+
 fn main() {
     let grid: Vec<Vec<char>> = io::stdin()
         .lock()
@@ -8,4 +36,6 @@ fn main() {
         .collect();
 
     eprintln!("{:?}", grid);
+    eprintln!("Dimensions: {:?}", dimensions(&grid));
+    eprintln!("Entrance at column {}", index_of_entrance(&grid[0]));
 }
