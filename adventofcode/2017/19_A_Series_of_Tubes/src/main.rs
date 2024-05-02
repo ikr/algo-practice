3a5d24c2f8ff5765e43ff40695380aa5b8817dd9
Ivan Krechetov <ikr@ikr.li>
2024-05-02 20:59:41 +0200

    WIP

--- adventofcode/2017/19_A_Series_of_Tubes/src/main.rs
+++ adventofcode/2017/19_A_Series_of_Tubes/src/main.rs
@@ -1,4 +1,6 @@
+use std::cell;
 use std::io::{self, BufRead};
+use std::ops::Add;
 
 #[derive(Debug, Clone, Copy)]
 enum Dir {
@@ -9,23 +11,51 @@ enum Dir {
 }
 
 #[derive(Debug, Clone, Copy)]
-struct Roco(usize, usize);
+struct Roco(i32, i32);
+
+impl Add<Roco> for Roco {
+    type Output = Roco;
+
+    fn add(self, o: Roco) -> Roco {
+        Roco(self.0 + o.0, self.1 + o.1)
+    }
+}
+
+fn dir_delta(dir: Dir) -> Roco {
+    match dir {
+        Dir::N => Roco(-1, 0),
+        Dir::E => Roco(0, 1),
+        Dir::S => Roco(1, 0),
+        Dir::W => Roco(-1, 0),
+    }
+}
 
 fn index_of_entrance(top_row: &[char]) -> usize {
     top_row.iter().position(|&x| x == '|').unwrap()
 }
 
 fn dimensions(grid: &[Vec<char>]) -> Roco {
-    Roco(grid.len(), grid[0].len())
+    Roco(grid.len() as i32, grid[0].len() as i32)
 }
 
-fn vertex_index(grid_width: usize, roco: Roco) -> usize {
-    assert!(roco.1 < grid_width);
-    grid_width * roco.0 + roco.1
-}
+fn trace(grid: &[Vec<char>]) -> String {
+    let mut result = String::new();
+    let mut roco = Roco(0, index_of_entrance(&grid[0]) as i32);
+    let mut dir = Dir::S;
 
-fn grid_roco(grid_width: usize, v_idx: usize) -> Roco {
-    Roco(v_idx / grid_width, v_idx % grid_width)
+    loop {
+        while grid[roco.0 as usize][roco.1 as usize] != ' '
+            && grid[roco.0 as usize][roco.1 as usize] != '+'
+        {
+            roco = roco + dir_delta(dir);
+        }
+
+        if grid[roco.0 as usize][roco.1 as usize] == '+' {
+            break;
+        }
+    }
+
+    result
 }
 
 fn main() {
@@ -38,4 +68,6 @@ fn main() {
     eprintln!("{:?}", grid);
     eprintln!("Dimensions: {:?}", dimensions(&grid));
     eprintln!("Entrance at column {}", index_of_entrance(&grid[0]));
+
+    println!("{}", trace(&grid));
 }
