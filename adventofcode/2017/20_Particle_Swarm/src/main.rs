a9287865dbe3c05feb1cfe7606742af2dfd126ca
Ivan Krechetov <ikr@ikr.su>
2024-05-05 16:04:08 +0200

    Start implementing the ternary search

--- adventofcode/2017/20_Particle_Swarm/src/main.rs
+++ adventofcode/2017/20_Particle_Swarm/src/main.rs
@@ -1,6 +1,8 @@
 use regex::Regex;
 use std::io::{self, BufRead};
 
+const END_OF_TIME: i64 = 10_000_000;
+
 fn decode_triple(src: &str) -> [i64; 3] {
     let parts: Vec<String> = src.split(',').map(|x| x.to_string()).collect();
     assert!(parts.len() == 3);
@@ -52,6 +54,40 @@ impl Particle {
             result.p[i] = self.p[i] + t * (2 * self.v[i] + self.a[i]) / 2 + (self.a[i] * t * t) / 2;
         }
         result
+    }
+
+    fn is_getting_closer_to(&self, other: Particle) -> bool {
+        manhattan_distance(&self.p, &other.p)
+            > manhattan_distance(&self.simulate_t_ticks(1).p, &other.simulate_t_ticks(1).p)
+    }
+
+    fn min_distance_to_other_with_time(&self, other: Particle) -> (i64, i64) {
+        assert!(self.p != other.p);
+        assert!(self.is_getting_closer_to(other));
+        assert!(!self
+            .simulate_t_ticks(END_OF_TIME)
+            .is_getting_closer_to(other.simulate_t_ticks(END_OF_TIME)));
+
+        let mut l = 0i64;
+        let mut r = END_OF_TIME;
+        while r - l >= 3 {}
+    }
+
+    fn time_to_collision(&self, other: Particle) -> Option<i64> {
+        if self.p == other.p {
+            Some(0)
+        } else {
+            if self.is_getting_closer_to(other) {
+                let (d, t) = self.min_distance_to_other_with_time(other);
+                if d == 0 {
+                    Some(t)
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        }
     }
 }
 
