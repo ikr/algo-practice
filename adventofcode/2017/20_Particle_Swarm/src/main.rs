31fca858f13cd6d3ea5b15a8339a9d6b6b4b68a4
Ivan Krechetov <ikr@ikr.li>
2024-05-06 20:22:57 +0200

    Clean up

--- adventofcode/2017/20_Particle_Swarm/src/main.rs
+++ adventofcode/2017/20_Particle_Swarm/src/main.rs
@@ -4,8 +4,6 @@ use std::{
     io::{self, BufRead},
 };
 
-const END_OF_TIME: i64 = 100_000_000;
-
 fn decode_triple(src: &str) -> [i64; 3] {
     let parts: Vec<String> = src.split(',').map(|x| x.to_string()).collect();
     assert!(parts.len() == 3);
@@ -65,53 +63,6 @@ impl Particle {
         }
         result
     }
-
-    fn is_getting_closer_to(&self, other: Particle) -> bool {
-        manhattan_distance(&self.p, &other.p)
-            > manhattan_distance(&self.simulate_t_ticks(1).p, &other.simulate_t_ticks(1).p)
-    }
-
-    fn min_distance_to_other_with_time(&self, other: Particle) -> (i64, i64) {
-        assert!(self.p != other.p);
-        assert!(self.is_getting_closer_to(other));
-        assert!(self.simulate_t_ticks(END_OF_TIME).p != other.simulate_t_ticks(END_OF_TIME).p);
-        assert!(!self
-            .simulate_t_ticks(END_OF_TIME)
-            .is_getting_closer_to(other.simulate_t_ticks(END_OF_TIME)));
-
-        let f = |t| manhattan_distance(&self.simulate_t_ticks(t).p, &other.simulate_t_ticks(t).p);
-
-        let mut l = 0i64;
-        let mut r = END_OF_TIME;
-        while r - l >= 3 {
-            let m1 = l + (r - l) / 3;
-            let m2 = r - (r - l) / 3;
-            let f1 = f(m1);
-            let f2 = f(m2);
-
-            if f1 < f2 {
-                r = m2;
-            } else {
-                l = m1;
-            }
-        }
-        (l..=r).map(|t| (f(t), t)).min().unwrap()
-    }
-
-    fn time_to_collision(&self, other: Particle) -> Option<i64> {
-        if self.p == other.p {
-            Some(0)
-        } else if self.is_getting_closer_to(other) {
-            let (d, t) = self.min_distance_to_other_with_time(other);
-            if d == 0 {
-                Some(t)
-            } else {
-                None
-            }
-        } else {
-            None
-        }
-    }
 }
 
 fn index_of_particle_closest_to_origin(ps: &[Particle]) -> usize {
@@ -126,13 +77,12 @@ fn index_of_particle_closest_to_origin(ps: &[Particle]) -> usize {
 }
 
 fn survivors(mut gen: Vec<Particle>) -> Vec<Particle> {
-    eprintln!("gen size is {}\n", gen.len());
     loop {
         let mut colliding_particles_by_time: BTreeMap<i64, HashSet<usize>> = BTreeMap::new();
         let n = gen.len();
         for i in 0..n - 1 {
             for j in i + 1..n {
-                for t in 0..50 {
+                for t in 0..20 {
                     let a = gen[i].simulate_t_ticks(t);
                     let b = gen[j].simulate_t_ticks(t);
                     if a.p == b.p {
@@ -147,8 +97,6 @@ fn survivors(mut gen: Vec<Particle>) -> Vec<Particle> {
             }
         }
 
-        eprintln!("{:?}", colliding_particles_by_time);
-
         if let Some((t, gone_indices)) = colliding_particles_by_time.first_key_value() {
             let mut gen_prime: Vec<Particle> = vec![];
             for i in 0..n {
@@ -157,18 +105,9 @@ fn survivors(mut gen: Vec<Particle>) -> Vec<Particle> {
                 }
             }
             gen = gen_prime;
-            eprintln!("gen size is {}\n", gen.len());
         } else {
             break;
         }
-    }
-
-    gen
-}
-
-fn time_shift(mut gen: Vec<Particle>, dt: i64) -> Vec<Particle> {
-    for p in gen.iter_mut() {
-        *p = p.simulate_t_ticks(dt);
     }
     gen
 }
@@ -185,8 +124,6 @@ fn main() {
         println!("{}", index_of_particle_closest_to_origin(&qs));
     }
 
-    let mut gen = ps.clone();
-    gen = survivors(gen);
-
-    println!("{}", gen.len());
+    let srv = survivors(ps);
+    println!("{}", srv.len());
 }
