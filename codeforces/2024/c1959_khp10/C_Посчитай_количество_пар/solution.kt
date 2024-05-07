605e76334f100544ebe1a0f047a160d21e66cf9a
Ivan Krechetov <ikr@ikr.li>
2024-05-07 20:52:25 +0200

    AC

--- codeforces/2024/c1959_khp10/C_Посчитай_количество_пар/solution.kt
+++ codeforces/2024/c1959_khp10/C_Посчитай_количество_пар/solution.kt
@@ -1,8 +1,45 @@
+fun flip(c: Char): Char {
+    return if (c.isUpperCase()) c.lowercaseChar() else c.uppercaseChar()
+}
+
+fun maxReward(s: String, k: Int): Int {
+    val fq = mutableMapOf<Char, Int>()
+    for (c in ('a'..'z')) {
+        fq[c] = 0;
+        fq[c.uppercaseChar()] = 0
+    }
+    for (c in s) {
+        fq[c] = fq[c]!! + 1
+    }
+
+    var result = 0
+    val rs = mutableListOf<Int>()
+
+    for (x in ('a'..'z')) {
+        val f = fq[x]!!
+        val y = flip(x)
+        val g = fq[y]!!
+
+        val d = Math.min(f, g)
+        result += d;
+        rs.add(f + g - 2 * d)
+    }
+
+    var rk = k
+    for (x in rs) {
+        val d = Math.min(rk, x / 2)
+        result += d
+        rk -= d
+    }
+    return result
+}
+
 fun main() {
     repeat(readInt()) {
         val (n, k) = readInts()
         val s = readLn()
-        println("($n, $k) $s")
+        assert(s.length == n)
+        println(maxReward(s, k))
     }
 }
 
