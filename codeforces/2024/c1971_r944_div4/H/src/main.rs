c1e93b395834afdd6e91ab1b2da15f7a95802676
Ivan Krechetov <ikr@ikr.su>
2024-05-12 15:18:45 +0200

    Introduce a Trio

--- codeforces/2024/c1971_r944_div4/H/src/main.rs
+++ codeforces/2024/c1971_r944_div4/H/src/main.rs
@@ -1,7 +1,48 @@
 use std::collections::VecDeque;
 use std::io::Write;
 
+#[derive(Debug, Clone, Copy)]
+struct Trio {
+    bitmask: u8,
+    indices: [usize; 3],
+}
+
+impl Trio {
+    fn decode(grid_codes: [i16; 3]) -> Trio {
+        let mut bitmask: u8 = 0;
+        let mut indices: [usize; 3] = [0, 0, 0];
+
+        for (i, code) in grid_codes.iter().enumerate() {
+            indices[i] = (code.abs() - 1) as usize;
+            if *code < 0 {
+                bitmask |= 1 << i;
+            };
+        }
+
+        Trio { bitmask, indices }
+    }
+
+    fn is_ok_on(&self, xs: u32) -> bool {
+        let mut sample: u8 = 0;
+        for (i, j) in self.indices.iter().enumerate() {
+            assert!(*j < 33);
+            if (1 << *j) & xs != 0 {
+                sample |= 1 << i;
+            }
+        }
+        (self.bitmask ^ sample).count_ones() <= 1
+    }
+}
+
+fn grid_trios(grid: &[Vec<i16>]) -> Vec<Trio> {
+    let n = grid[0].len();
+    (0..n)
+        .map(|co| Trio::decode([grid[0][co], grid[1][co], grid[2][co]]))
+        .collect()
+}
+
 fn alice_can_win(grid: &[Vec<i16>]) -> bool {
+    eprintln!("{:?}", grid_trios(grid));
     false
 }
 
