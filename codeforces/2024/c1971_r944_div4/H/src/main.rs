e0a966506042e56f4693f295be8e1301d5ee051c
Ivan Krechetov <ikr@ikr.su>
2024-05-12 14:35:25 +0200

    Decode the input

--- codeforces/2024/c1971_r944_div4/H/src/main.rs
+++ codeforces/2024/c1971_r944_div4/H/src/main.rs
@@ -1,4 +1,9 @@
 use std::collections::VecDeque;
+use std::io::Write;
+
+fn alice_can_win(grid: &[Vec<i16>]) -> bool {
+    false
+}
 
 fn main() {
     let mut istream: VecDeque<i16> = std::io::read_to_string(std::io::stdin())
@@ -7,5 +12,20 @@ fn main() {
         .map(|x| x.parse::<i16>().unwrap())
         .collect();
 
-    eprintln!("{:?}", istream)
+    let mut out = std::io::stdout().lock();
+
+    for _ in 0..istream.pop_front().unwrap() {
+        let n = istream.pop_front().unwrap() as usize;
+        let mut grid: Vec<Vec<i16>> = vec![vec![0; n]; 3];
+        for row in grid.iter_mut() {
+            for x in row.iter_mut() {
+                *x = istream.pop_front().unwrap();
+            }
+        }
+
+        eprintln!("{:?}", grid);
+
+        let result = if alice_can_win(&grid) { "YES" } else { "NO" };
+        writeln!(out, "{}", result).unwrap();
+    }
 }
