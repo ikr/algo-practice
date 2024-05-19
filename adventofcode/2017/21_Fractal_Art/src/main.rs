c145d958a2b9227dbe9a5352d73a3113d5ab65ff
Ivan Krechetov <ikr@ikr.su>
2024-05-19 17:22:56 +0200

    Introduce the bits representation of the Tile-s

--- adventofcode/2017/21_Fractal_Art/src/main.rs
+++ adventofcode/2017/21_Fractal_Art/src/main.rs
@@ -20,6 +20,32 @@ impl Tile {
 
     fn decode(source: &str) -> Tile {
         let grid: Vec<Vec<bool>> = source.split('/').map(Self::decode_row).collect();
+        assert!(2 <= grid.len() && grid.len() <= 4);
+        assert!(grid.iter().all(|row| row.len() == grid.len()));
+        Tile { grid }
+    }
+
+    fn to_bits(&self) -> u16 {
+        let n = self.grid.len();
+        let mut result = 0u16;
+        for (ro, row) in self.grid.iter().enumerate() {
+            for (co, x) in row.iter().enumerate() {
+                if *x {
+                    result |= 1u16 << (ro * n + co);
+                }
+            }
+        }
+        result
+    }
+
+    fn from_bits(n: usize, bits: u16) -> Tile {
+        assert!(2 <= n && n <= 4);
+        let mut grid = vec![vec![false; n]; n];
+        for shift in 0..(n * n) {
+            if bits & (1u16 << shift) != 0 {
+                grid[shift / n][shift % n] = true;
+            }
+        }
         Tile { grid }
     }
 }
@@ -52,5 +78,39 @@ fn main() {
 
     for (a, b) in rule_sides {
         println!("{}\n,\n{}\n\n", Tile::decode(&a), Tile::decode(&b));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn to_bits_with_the_top_left_set() {
+        let t = Tile::decode("#./..");
+        assert_eq!(t.to_bits(), 1u16);
+    }
+
+    #[test]
+    fn to_bits_with_the_bottom_right_set() {
+        let t = Tile::decode("..../..../..../...#");
+        assert_eq!(t.to_bits(), 32768u16);
+    }
+
+    #[test]
+    fn to_bits_with_all_set() {
+        let t = Tile::decode("###/###/###");
+        assert_eq!(t.to_bits(), 511u16);
+    }
+
+    #[test]
+    fn to_bits_and_back_again() {
+        for n in 2..=4 {
+            for bits in 0..=((1u32 << (n * n)) - 1) as u16 {
+                let t = Tile::from_bits(n, bits);
+                assert_eq!(t.grid.len(), n);
+                assert_eq!(t.to_bits(), bits);
+            }
+        }
     }
 }
