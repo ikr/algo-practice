713a7935e1b6e745376479d913a28f6c78f134e3
Ivan Krechetov <ikr@ikr.li>
2024-06-02 14:55:28 +0200

    Try memoizing ⇒ consumes too much memory

--- adventofcode/2017/24_Electromagnetic_Moat/src/main.rs
+++ adventofcode/2017/24_Electromagnetic_Moat/src/main.rs
@@ -1,3 +1,4 @@
+use memoize::memoize;
 use std::io::{self, BufRead};
 
 fn decode_pair(src: &str) -> [i32; 2] {
@@ -10,24 +11,18 @@ fn have_common_port(p: &[i32], q: &[i32]) -> bool {
     p.iter().any(|x| q.contains(x))
 }
 
-struct State {
-    ps: Vec<[i32; 2]>,
-}
-
-impl State {
-    fn max_strength_starting_with(&self, current: i32, used_bits: u64, p0: [i32; 2]) -> i32 {
-        let mut result = current;
-        for (i, p) in self.ps.iter().enumerate() {
-            if (1u64 << i) & used_bits == 0u64 && have_common_port(&p0, p) {
-                result = result.max(self.max_strength_starting_with(
-                    current + p.iter().sum::<i32>(),
-                    used_bits | (1u64 << i),
-                    *p,
-                ));
-            }
+#[memoize(Ignore: ps)]
+fn max_strength_starting_with(ps: &[[i32; 2]], used_bits: u64, p0: [i32; 2]) -> i32 {
+    let mut result: i32 = p0.iter().sum();
+    for (i, p) in ps.iter().enumerate() {
+        if (1u64 << i) & used_bits == 0u64 && have_common_port(&p0, p) {
+            result = result.max(
+                p0.iter().sum::<i32>()
+                    + max_strength_starting_with(ps, used_bits | (1u64 << i), *p),
+            );
         }
-        result
     }
+    result
 }
 
 fn main() {
@@ -39,12 +34,7 @@ fn main() {
 
     let (sources, segments): (Vec<_>, Vec<_>) = pairs.into_iter().partition(|ab| ab[0] == 0);
 
-    let state = State { ps: segments };
-
     for s in sources {
-        println!(
-            "{:?}",
-            state.max_strength_starting_with(s.iter().sum(), 0, s)
-        );
+        println!("{:?}", max_strength_starting_with(&segments, 0, s));
     }
 }
