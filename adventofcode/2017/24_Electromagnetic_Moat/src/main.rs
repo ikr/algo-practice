34e4495a69b301f53977eb1e3d41d292f89a4027
Ivan Krechetov <ikr@ikr.li>
2024-06-02 17:30:58 +0200

    Tried an in-between answer: "2141 is too high"
    Hm. We're definitely doing something wrong.

--- adventofcode/2017/24_Electromagnetic_Moat/src/main.rs
+++ adventofcode/2017/24_Electromagnetic_Moat/src/main.rs
@@ -1,32 +1,40 @@
 use memoize::memoize;
 use std::io::{self, BufRead};
 
-fn decode_pair(src: &str) -> [i32; 2] {
-    let mut result: Vec<i32> = src.split('/').map(|part| part.parse().unwrap()).collect();
+fn decode_pair(src: &str) -> [u8; 2] {
+    let mut result: Vec<u8> = src.split('/').map(|part| part.parse().unwrap()).collect();
     result.sort();
     result.try_into().unwrap()
 }
 
-fn have_common_port(p: &[i32], q: &[i32]) -> bool {
+fn have_common_port(p: &[u8], q: &[u8]) -> bool {
     p.iter().any(|x| q.contains(x))
 }
 
-#[memoize(Ignore: ps)]
-fn max_strength_starting_with(ps: &[[i32; 2]], used_bits: u64, p0: [i32; 2]) -> i32 {
-    let mut result: i32 = p0.iter().sum();
+static mut ANS: u16 = 0;
+
+#[memoize(Ignore: ps, Capacity: 59999999)]
+fn max_strength_starting_with(ps: &[[u8; 2]], used_bits: u64, p0: [u8; 2]) -> u16 {
+    let mut result: u16 = p0.iter().sum::<u8>() as u16;
     for (i, p) in ps.iter().enumerate() {
         if (1u64 << i) & used_bits == 0u64 && have_common_port(&p0, p) {
             result = result.max(
-                p0.iter().sum::<i32>()
+                p0.iter().sum::<u8>() as u16
                     + max_strength_starting_with(ps, used_bits | (1u64 << i), *p),
             );
+        }
+    }
+    unsafe {
+        if result > ANS {
+            ANS = result;
+            eprintln!("{}", ANS)
         }
     }
     result
 }
 
 fn main() {
-    let pairs: Vec<[i32; 2]> = io::stdin()
+    let pairs: Vec<[u8; 2]> = io::stdin()
         .lock()
         .lines()
         .map(|line| decode_pair(&line.unwrap()))
