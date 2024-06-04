86598425e9b16625f4323ed2d3f3d331377dc130
Ivan Krechetov <ikr@ikr.su>
2024-06-04 17:18:48 +0200

    Solve part 2

--- adventofcode/2016/02_Bathroom_Security/src/main.rs
+++ adventofcode/2016/02_Bathroom_Security/src/main.rs
@@ -1,41 +1,81 @@
+use std::collections::HashMap;
 use std::io::{self, BufRead};
 
 struct Numpad {
-    grid: Vec<Vec<char>>,
-    current_row: i8,
-    current_col: i8,
+    graph: HashMap<(char, char), char>,
+    current: char,
 }
 
 impl Numpad {
     fn new() -> Numpad {
         Numpad {
-            grid: vec![
-                vec!['1', '2', '3'],
-                vec!['4', '5', '6'],
-                vec!['7', '8', '9'],
-            ],
-            current_row: 1,
-            current_col: 1,
+            graph: HashMap::from([
+                (('1', 'U'), '1'),
+                (('1', 'R'), '1'),
+                (('1', 'D'), '3'),
+                (('1', 'L'), '1'),
+                (('2', 'U'), '2'),
+                (('2', 'R'), '3'),
+                (('2', 'D'), '6'),
+                (('2', 'L'), '2'),
+                (('3', 'U'), '1'),
+                (('3', 'R'), '4'),
+                (('3', 'D'), '7'),
+                (('3', 'L'), '2'),
+                (('4', 'U'), '4'),
+                (('4', 'R'), '4'),
+                (('4', 'D'), '8'),
+                (('4', 'L'), '3'),
+                (('5', 'U'), '5'),
+                (('5', 'R'), '6'),
+                (('5', 'D'), '5'),
+                (('5', 'L'), '5'),
+                (('6', 'U'), '2'),
+                (('6', 'R'), '7'),
+                (('6', 'D'), 'A'),
+                (('6', 'L'), '5'),
+                (('7', 'U'), '3'),
+                (('7', 'R'), '8'),
+                (('7', 'D'), 'B'),
+                (('7', 'L'), '6'),
+                (('8', 'U'), '4'),
+                (('8', 'R'), '9'),
+                (('8', 'D'), 'C'),
+                (('8', 'L'), '7'),
+                (('9', 'U'), '9'),
+                (('9', 'R'), '9'),
+                (('9', 'D'), '9'),
+                (('9', 'L'), '8'),
+                (('A', 'U'), '6'),
+                (('A', 'R'), 'B'),
+                (('A', 'D'), 'A'),
+                (('A', 'L'), 'A'),
+                (('B', 'U'), '7'),
+                (('B', 'R'), 'C'),
+                (('B', 'D'), 'D'),
+                (('B', 'L'), 'A'),
+                (('C', 'U'), '8'),
+                (('C', 'R'), 'C'),
+                (('C', 'D'), 'C'),
+                (('C', 'L'), 'B'),
+                (('D', 'U'), 'B'),
+                (('D', 'R'), 'D'),
+                (('D', 'D'), 'D'),
+                (('D', 'L'), 'D'),
+            ]),
+            current: '5',
         }
     }
 
     fn move_once(&mut self, direction_code: char) {
-        match direction_code {
-            'U' => self.current_row -= 1,
-            'R' => self.current_col += 1,
-            'D' => self.current_row += 1,
-            'L' => self.current_col -= 1,
-            _ => panic!("Invalid direction code {}", direction_code),
-        }
-        self.current_row = self.current_row.clamp(0, 2);
-        self.current_col = self.current_col.clamp(0, 2);
+        self.current = *self.graph.get(&(self.current, direction_code)).unwrap()
     }
 
     fn dial_digit(&mut self, direction_codes: &[char]) -> char {
         for c in direction_codes {
             self.move_once(*c);
         }
-        self.grid[self.current_row as usize][self.current_col as usize]
+        self.current
     }
 }
 
@@ -49,7 +89,7 @@ fn main() {
     let mut np = Numpad::new();
     let mut result = String::new();
 
-    for line in instruction_lines{
+    for line in instruction_lines {
         result.push(np.dial_digit(&line));
     }
 
