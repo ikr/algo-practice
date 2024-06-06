552ed025e3fcab67fac61fdfc69f5f439ac41a55
Ivan Krechetov <ikr@ikr.su>
2024-06-06 08:03:39 +0200

    WA: 449 is too low

---
+++ adventofcode/2016/04_Security_Through_Obscurity/src/main.rs
@@ -0,0 +1,56 @@
+use regex::Regex;
+use std::{
+    collections::HashMap,
+    io::{self, BufRead},
+};
+
+fn room_code_parts(src: &str) -> Vec<String> {
+    Regex::new(r"[a-z0-9]+")
+        .unwrap()
+        .find_iter(src)
+        .map(|x| x.as_str().to_string())
+        .collect()
+}
+
+fn all_room_letters(rcp: &[String]) -> Vec<char> {
+    let n = rcp.len();
+    rcp[0..n - 2].iter().fold(vec![], |mut acc, s| {
+        acc.extend(s.chars());
+        acc
+    })
+}
+
+fn room_checksum(rcp: &[String]) -> String {
+    rcp.iter().last().unwrap().to_string()
+}
+
+fn frequencies(xs: &[char]) -> HashMap<char, u8> {
+    xs.iter().fold(HashMap::new(), |mut acc, c| {
+        acc.entry(*c).and_modify(|f| *f += 1).or_insert(1);
+        acc
+    })
+}
+
+fn letters_checksum(xs: &[char]) -> String {
+    let mut ps: Vec<(i8, char)> = frequencies(xs)
+        .into_iter()
+        .map(|(k, v)| (-1 * v as i8, k))
+        .collect();
+    ps.sort();
+
+    ps[0..5].into_iter().map(|(_, c)| c).collect()
+}
+
+fn main() {
+    let xss: Vec<Vec<String>> = io::stdin()
+        .lock()
+        .lines()
+        .map(|line| room_code_parts(&line.unwrap()))
+        .collect();
+
+    let result = xss
+        .into_iter()
+        .filter(|xs| letters_checksum(&all_room_letters(&xs)) == room_checksum(&xs))
+        .count();
+    println!("{}", result);
+}
