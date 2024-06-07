ca9e1fff5ce80a5522942db0f9bf0d16fc204f6f
Ivan Krechetov <ikr@ikr.li>
2024-06-07 08:00:01 +0200

    Solve part 2

--- adventofcode/2016/05_How_About_a_Nice_Game_of_Chess/src/main.rs
+++ adventofcode/2016/05_How_About_a_Nice_Game_of_Chess/src/main.rs
@@ -1,9 +1,15 @@
-fn password_char(salt: &str, x: u32) -> Option<char> {
+fn password_char(salt: &str, x: u32) -> Option<(usize, char)> {
     let input: String = salt.to_owned() + &x.to_string();
     let hash: String = format!("{:?}", md5::compute(input));
 
     if &hash[0..5] == "00000" {
-        Some(hash.chars().nth(5).unwrap())
+        let p = hash.chars().nth(5).unwrap();
+        if p.is_digit(8) {
+            let x = hash.chars().nth(6).unwrap();
+            Some((p as usize - b'0' as usize, x))
+        } else {
+            None
+        }
     } else {
         None
     }
@@ -15,14 +21,23 @@ fn main() {
         .trim()
         .to_string();
 
-    let mut result: String = String::new();
+    let mut result = vec!['.'; 8];
     for i in 0..999_999_999 {
-        if let Some(c) = password_char(&salt, i) {
-            result.push(c);
-            if result.len() == 8 {
+        if i > 0 && i % 1_000_000 == 0 {
+            eprintln!("i:{}", i);
+        }
+
+        if let Some((p, c)) = password_char(&salt, i) {
+            if result[p] == '.' {
+                result[p] = c;
+            }
+
+            if result.iter().any(|x| *x == '.') {
+                eprintln!("{}", result.iter().collect::<String>());
+            } else {
                 break;
             }
         }
     }
-    println!("{}", result);
+    println!("{}", result.iter().collect::<String>());
 }
