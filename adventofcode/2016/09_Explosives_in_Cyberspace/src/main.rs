5db4cc2c5b404480b6d95088824a761daa377637
Ivan Krechetov <ikr@ikr.su>
2024-06-10 17:48:23 +0200

    Solve part 1

--- adventofcode/2016/09_Explosives_in_Cyberspace/src/main.rs
+++ adventofcode/2016/09_Explosives_in_Cyberspace/src/main.rs
@@ -6,6 +6,17 @@ struct RepeatOp {
     times: usize,
 }
 
+impl RepeatOp {
+    fn apply(&self, xs: &mut Rope, i0: usize) -> usize {
+        let pat: String = xs.byte_slice(i0..i0 + self.length).chars().collect();
+        let ys = pat.repeat(self.times);
+        xs.delete(i0..i0 + self.length);
+        let n = ys.len();
+        xs.insert(i0, ys);
+        i0 + n
+    }
+}
+
 fn decode_marker(src: &str) -> RepeatOp {
     let xs: Vec<usize> = src
         .strip_prefix('(')
@@ -22,7 +33,7 @@ fn decode_marker(src: &str) -> RepeatOp {
 }
 
 fn main() {
-    let xs: Rope = Rope::from(
+    let mut xs: Rope = Rope::from(
         std::io::read_to_string(std::io::stdin())
             .unwrap()
             .trim_end(),
@@ -44,9 +55,10 @@ fn main() {
                 .skip_while(|(_, x)| *x != ')');
 
             let (j, _) = *jt.peekable().peek().unwrap();
+            let m = j - i + 1;
             let op = decode_marker(&xs.byte_slice(*i..=j).chars().collect::<String>());
-            eprintln!("{:?}", op);
-            i0 = j;
+            xs.delete(*i..=j);
+            i0 = op.apply(&mut xs, j + 1 - m);
         } else {
             break;
         }
