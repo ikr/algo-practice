22a8d194e72e2c36c1fd512cb6e4486e38993241
Ivan Krechetov <ikr@ikr.li>
2024-06-10 08:00:46 +0200

    Try out the Rope

--- adventofcode/2016/09_Explosives_in_Cyberspace/src/main.rs
+++ adventofcode/2016/09_Explosives_in_Cyberspace/src/main.rs
@@ -7,5 +7,29 @@ fn main() {
             .trim_end(),
     );
 
-    eprintln!("{}", xs.byte_len());
+    let mut i0: usize = 0;
+    loop {
+        let it = xs
+            .chars()
+            .enumerate()
+            .skip(i0)
+            .skip_while(|(_, x)| *x != '(');
+
+        if let Some((i, _)) = it.peekable().peek() {
+            let jt = xs
+                .chars()
+                .enumerate()
+                .skip(*i)
+                .skip_while(|(_, x)| *x != ')');
+
+            let (j, _) = *jt.peekable().peek().unwrap();
+            eprintln!("{} {}", i, j);
+            i0 = j;
+        } else {
+            break;
+        }
+    }
+
+    eprintln!("{:?}", xs);
+    println!("{}", xs.byte_len());
 }
