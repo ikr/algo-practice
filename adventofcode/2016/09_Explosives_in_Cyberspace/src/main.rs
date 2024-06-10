35b19ad8216d3f39db906c15ba313e3e1d095c3e
Ivan Krechetov <ikr@ikr.li>
2024-06-10 17:26:04 +0200

    Decode markers

--- adventofcode/2016/09_Explosives_in_Cyberspace/src/main.rs
+++ adventofcode/2016/09_Explosives_in_Cyberspace/src/main.rs
@@ -1,5 +1,26 @@
 use crop::Rope;
 
+#[derive(Debug)]
+struct RepeatOp {
+    length: usize,
+    times: usize,
+}
+
+fn decode_marker(src: &str) -> RepeatOp {
+    let xs: Vec<usize> = src
+        .strip_prefix('(')
+        .unwrap()
+        .strip_suffix(')')
+        .unwrap()
+        .split('x')
+        .map(|x| x.parse().unwrap())
+        .collect();
+    let [length, times] = xs[..] else {
+        panic!("{:?} isn't a pair", xs)
+    };
+    RepeatOp { length, times }
+}
+
 fn main() {
     let xs: Rope = Rope::from(
         std::io::read_to_string(std::io::stdin())
@@ -23,7 +44,8 @@ fn main() {
                 .skip_while(|(_, x)| *x != ')');
 
             let (j, _) = *jt.peekable().peek().unwrap();
-            eprintln!("{} {}", i, j);
+            let op = decode_marker(&xs.byte_slice(*i..=j).chars().collect::<String>());
+            eprintln!("{:?}", op);
             i0 = j;
         } else {
             break;
