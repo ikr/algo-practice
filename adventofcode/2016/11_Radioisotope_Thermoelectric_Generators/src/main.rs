5109318f30048ea4b1f1bd5600bd0252c8c31602
Ivan Krechetov <ikr@ikr.li>
2024-06-14 07:59:18 +0200

    Compute the possible src → dst floor moves

--- adventofcode/2016/11_Radioisotope_Thermoelectric_Generators/src/main.rs
+++ adventofcode/2016/11_Radioisotope_Thermoelectric_Generators/src/main.rs
@@ -1,6 +1,10 @@
 const N: usize = 2;
 
-#[derive(Clone, Copy, Debug)]
+fn intersect<const K: usize>(a: [bool; K], b: [bool; K]) -> bool {
+    a.into_iter().enumerate().any(|(i, x)| x && x == b[i])
+}
+
+#[derive(Clone, Copy, Debug, Hash)]
 struct Floor {
     microchips: [bool; N],
     generators: [bool; N],
@@ -14,15 +18,27 @@ impl Floor {
         }
     }
 
-    fn with_microchip(&self, x: u8) -> Floor {
+    fn with_microchip(&self, x: usize) -> Floor {
         let mut result = *self;
-        result.microchips[x as usize] = true;
+        result.microchips[x] = true;
         result
     }
 
-    fn with_generator(&self, x: u8) -> Floor {
+    fn with_generator(&self, x: usize) -> Floor {
         let mut result = *self;
-        result.generators[x as usize] = true;
+        result.generators[x] = true;
+        result
+    }
+
+    fn without_microchip(&self, x: usize) -> Floor {
+        let mut result = *self;
+        result.microchips[x] = false;
+        result
+    }
+
+    fn without_generator(&self, x: usize) -> Floor {
+        let mut result = *self;
+        result.generators[x] = false;
         result
     }
 
@@ -37,9 +53,129 @@ impl Floor {
             .filter(|(_, m)| **m)
             .fold(true, |acc, (i, m)| acc && *m && self.generators[i])
     }
+
+    fn is_safe(&self) -> bool {
+        !self.has_generators() || self.all_microchips_shielded()
+    }
+
+    fn intersects(&self, oth: Floor) -> bool {
+        intersect(self.microchips, oth.microchips) || intersect(self.generators, oth.generators)
+    }
+
+    fn couple_moves(&self, dst: Floor) -> Vec<(Floor, Floor)> {
+        assert!(self.is_safe());
+        assert!(dst.is_safe());
+        assert!(!self.intersects(dst));
+        let mut result = Vec::new();
+
+        for (i, m) in self.microchips.iter().enumerate() {
+            if *m && self.generators[i] {
+                let dst_prime = dst.with_microchip(i).with_generator(i);
+                if dst_prime.is_safe() {
+                    result.push((self.without_microchip(i).without_generator(i), dst_prime));
+                }
+            }
+        }
+
+        result
+    }
+
+    fn one_microchip_moves(&self, dst: Floor) -> Vec<(Floor, Floor)> {
+        assert!(self.is_safe());
+        assert!(dst.is_safe());
+        assert!(!self.intersects(dst));
+        let mut result = Vec::new();
+
+        for (i, m) in self.microchips.iter().enumerate() {
+            if *m {
+                let dst_prime = dst.with_microchip(i);
+                if dst_prime.is_safe() {
+                    result.push((self.without_microchip(i), dst_prime));
+                }
+            }
+        }
+
+        result
+    }
+
+    fn two_microchips_moves(&self, dst: Floor) -> Vec<(Floor, Floor)> {
+        assert!(self.is_safe());
+        assert!(dst.is_safe());
+        assert!(!self.intersects(dst));
+        let mut result = Vec::new();
+
+        for i in 0..N - 1 {
+            for j in i + 1..N {
+                if self.microchips[i] && self.microchips[j] {
+                    let dst_prime = dst.with_microchip(i).with_microchip(j);
+                    if dst_prime.is_safe() {
+                        result.push((self.without_microchip(i).without_microchip(j), dst_prime));
+                    }
+                }
+            }
+        }
+
+        result
+    }
+
+    fn one_generator_moves(&self, dst: Floor) -> Vec<(Floor, Floor)> {
+        assert!(self.is_safe());
+        assert!(dst.is_safe());
+        assert!(!self.intersects(dst));
+        let mut result = Vec::new();
+
+        for (i, g) in self.generators.iter().enumerate() {
+            if *g {
+                let src_prime = self.without_generator(i);
+                if src_prime.is_safe() {
+                    let dst_prime = dst.with_generator(i);
+                    if dst_prime.is_safe() {
+                        result.push((self.without_generator(i), dst_prime));
+                    }
+                }
+            }
+        }
+
+        result
+    }
+
+    fn two_generators_moves(&self, dst: Floor) -> Vec<(Floor, Floor)> {
+        assert!(self.is_safe());
+        assert!(dst.is_safe());
+        assert!(!self.intersects(dst));
+        let mut result = Vec::new();
+
+        for i in 0..N - 1 {
+            for j in i + 1..N {
+                if self.generators[i] && self.generators[j] {
+                    let src_prime = self.without_generator(i).without_generator(j);
+                    if src_prime.is_safe() {
+                        let dst_prime = dst.with_generator(i).with_generator(j);
+                        if dst_prime.is_safe() {
+                            result
+                                .push((self.without_generator(i).without_generator(j), dst_prime));
+                        }
+                    }
+                }
+            }
+        }
+
+        result
+    }
+
+    fn all_moves(&self, dst: Floor) -> Vec<(Floor, Floor)> {
+        [
+            self.couple_moves(dst),
+            self.one_microchip_moves(dst),
+            self.two_microchips_moves(dst),
+            self.one_generator_moves(dst),
+            self.two_generators_moves(dst),
+        ]
+        .concat()
+    }
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, Hash)]
 struct Vertex {
     current_floor: u8,
     floors: [Floor; 4],
@@ -67,6 +203,8 @@ fn main() {
     let u = in_a();
     eprintln!("{:?}", u);
     for f in u.floors {
-        assert!(!f.has_generators() || f.all_microchips_shielded());
+        assert!(f.is_safe());
     }
+
+    eprintln!("{:?}", u.floors[0].all_moves(u.floors[1]));
 }
