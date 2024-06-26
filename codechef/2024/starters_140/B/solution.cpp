0d876e3e7b34cf17196dcaaeea9cdf4c90ae3e4c
Ivan Krechetov <ikr@ikr.su>
2024-06-26 17:06:21 +0200

    AC

--- codechef/2024/starters_140/B/solution.cpp
+++ codechef/2024/starters_140/B/solution.cpp
@@ -3,46 +3,26 @@ using namespace std;
 
 using ll = long long;
 
-static constexpr ll Inf = 1e12;
-
 template <typename T> constexpr int inof(const T x) {
     return static_cast<int>(x);
 }
 
 template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
 
-static vector<vector<ll>> M(101);
-
-void precompute() {
-    for (ll m = 2; m < sz(M); ++m) {
-        const ll base = m;
-        M[m].push_back(base);
-
-        while (M[m].back() < Inf) {
-            M[m].push_back(M[m].back() * base);
-        }
-    }
-}
-
-ll num_ways(const ll m, const vector<ll> &A, const vector<ll> &B) {
+ll num_ways(const ll M, const vector<ll> &A, const vector<ll> &B) {
     const ll n = sz(A);
-    if (m == 1) return n * n;
+    if (M == 1) return n * n;
 
     map<ll, int> bfq;
-    for (const auto b : B) ++bfq[b];
+    for (const auto b : B) ++bfq[b % M];
 
     ll result{};
-    for (const auto target : M[m]) {
-        for (const auto a : A) {
-            cerr << "target:" << target << " a:" << a << endl;
-            const auto b = target - a;
-            const auto it = bfq.find(b);
-            if (it != cend(bfq)) {
-                result += it->second;
-                cerr << "found:" << it->second << endl;
-            } else {
-                cerr << "not found" << endl;
-            }
+    for (const auto a : A) {
+        const auto b = ((M - (a % M)) + M) % M;
+        const auto it = bfq.find(b);
+        if (it != cend(bfq)) {
+            result += it->second;
+        } else {
         }
     }
 
@@ -52,7 +32,6 @@ ll num_ways(const ll m, const vector<ll> &A, const vector<ll> &B) {
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
-    precompute();
 
     int t;
     cin >> t;
