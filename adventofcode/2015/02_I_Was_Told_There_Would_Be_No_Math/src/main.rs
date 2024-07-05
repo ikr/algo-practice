2cb72fa228eb3bb1fd27bd33bdd258547976a8b5
Ivan Krechetov <ikr@ikr.li>
2024-07-05 22:25:52 +0200

    Solve part 2

--- adventofcode/2015/02_I_Was_Told_There_Would_Be_No_Math/src/main.rs
+++ adventofcode/2015/02_I_Was_Told_There_Would_Be_No_Math/src/main.rs
@@ -5,6 +5,11 @@ fn wrap_area(xs: [i32; 3]) -> i32 {
     3 * l * w + 2 * w * h + 2 * h * l
 }
 
+fn ribbon_length(xs: [i32; 3]) -> i32 {
+    let [l, w, h] = xs;
+    2 * l + 2 * w + l * w * h
+}
+
 fn parse_tri(src: &str) -> [i32; 3] {
     let mut xs = src
         .split('x')
@@ -21,6 +26,9 @@ fn main() {
         .map(|line| parse_tri(&line.unwrap()))
         .collect();
 
-    let result1 = xss.into_iter().fold(0, |acc, xs| acc + wrap_area(xs));
+    let result1 = xss.iter().fold(0, |acc, xs| acc + wrap_area(*xs));
     println!("{}", result1);
+
+    let result2 = xss.into_iter().fold(0, |acc, xs| acc + ribbon_length(xs));
+    println!("{}", result2);
 }
