0e7d5e9cb8eeff25e08aacf082c0c39dba73abfd
Ivan Krechetov <ikr@ikr.li>
2024-07-11 17:20:28 +0200

    Solve part 1

--- adventofcode/2015/08_Matchsticks/src/main.rs
+++ adventofcode/2015/08_Matchsticks/src/main.rs
@@ -1,12 +1,34 @@
 use std::io::{self, BufRead};
 
 fn decode(src: &str) -> Vec<u8> {
-    let a: &str = src
+    let xs: &[u8] = src
         .strip_prefix('"')
         .and_then(|x| x.strip_suffix('"'))
-        .unwrap();
+        .unwrap()
+        .as_bytes();
 
-    todo!()
+    let mut result: Vec<u8> = vec![];
+    let mut i: usize = 0;
+
+    while i < xs.len() {
+        if xs[i] == b'\\' {
+            assert!(i + 1 < xs.len());
+            if xs[i + 1] == b'x' {
+                let hex = String::from_utf8(xs[i + 2..i + 4].to_vec()).unwrap();
+                let b: u8 = u8::from_str_radix(&hex, 16).unwrap();
+                result.push(b);
+                i += 4;
+            } else {
+                result.push(xs[i + 1]);
+                i += 2;
+            }
+        } else {
+            result.push(xs[i]);
+            i += 1;
+        }
+    }
+
+    result
 }
 
 fn main() {
