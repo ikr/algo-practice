2de1ac2846ec9869f48382a37d3a7c4440ae7ba3
Ivan Krechetov <ikr@ikr.su>
2024-07-11 18:17:06 +0200

    Compute an adjacency matrix

--- adventofcode/2015/09_All_in_a_Single_Night/src/main.rs
+++ adventofcode/2015/09_All_in_a_Single_Night/src/main.rs
@@ -7,21 +7,27 @@ fn parse_line_parts(src: &str) -> (String, i32) {
     (a.to_owned(), b.parse().unwrap())
 }
 
-fn parse_cities(src: &str) -> (String, String) {
+fn parse_locations(src: &str) -> (String, String) {
     let [a, b] = src.split(" to ").collect::<Vec<_>>()[..] else {
-        panic!("Invalid cities pair {}", src)
+        panic!("Invalid locations pair {}", src)
     };
     (a.to_owned(), b.to_owned())
 }
 
 fn parse_line(src: &str) -> (String, String, i32) {
     let (ab, distance) = parse_line_parts(src);
-    let (a, b) = parse_cities(&ab);
+    let (a, b) = parse_locations(&ab);
     (a, b, distance)
 }
 
 fn index_of(haystack: &[String], needle: &str) -> usize {
     haystack.iter().position(|s| s == needle).unwrap()
+}
+
+fn distinct<T: Ord>(mut xs: Vec<T>) -> Vec<T> {
+    xs.sort();
+    xs.dedup();
+    xs
 }
 
 fn main() {
@@ -32,4 +38,24 @@ fn main() {
         .collect();
 
     eprintln!("{:?}", facts);
+
+    let locations: Vec<String> = distinct(facts.iter().fold(vec![], |mut acc, (u, v, _)| {
+        acc.push(u.to_string());
+        acc.push(v.to_string());
+        acc
+    }));
+
+    eprintln!("{:?}", locations);
+
+    let n = locations.len();
+    let mut g: Vec<Vec<i32>> = vec![vec![0; n]; n];
+
+    for (a, b, distance) in facts.into_iter() {
+        let u = index_of(&locations, &a);
+        let v = index_of(&locations, &b);
+        g[u][v] = distance;
+        g[v][u] = distance;
+    }
+
+    eprintln!("{:?}", g);
 }
