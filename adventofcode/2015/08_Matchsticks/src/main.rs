3cc9e336f5d79513793620e073d2e9ce554fee4a
Ivan Krechetov <ikr@ikr.li>
2024-07-11 07:11:14 +0200

    WA: 1315 is too low

--- adventofcode/2015/08_Matchsticks/src/main.rs
+++ adventofcode/2015/08_Matchsticks/src/main.rs
@@ -2,7 +2,6 @@ use regex::{Captures, Regex};
 use std::io::{self, BufRead};
 
 fn decode_ascii_hex(src: &str) -> String {
-    eprintln!("hex {}", src);
     let a = u8::from_str_radix(src, 16).unwrap();
     let b: u8 = if a > 127 { a - 127 } else { a };
     String::from_utf8(vec![b]).unwrap()
@@ -13,11 +12,10 @@ fn decode(src: &str) -> String {
         .strip_prefix('"')
         .and_then(|x| x.strip_suffix('"'))
         .unwrap()
+        .replace("\\\\x", "\\\\X")
         .replace("\\\\", "\\")
         .replace("\\\"", "\"")
         .to_owned();
-
-    eprintln!("a: {}", a);
 
     let re = Regex::new(r"(\\x)([a-z0-9][a-z0-9])").unwrap();
     re.replace_all(&a, |caps: &Captures| decode_ascii_hex(&caps[2]))
@@ -30,7 +28,6 @@ fn main() {
         .lines()
         .map(|line| line.unwrap())
         .collect();
-    eprintln!("{}", decode(lines.last().unwrap()));
 
     let result = lines
         .into_iter()
