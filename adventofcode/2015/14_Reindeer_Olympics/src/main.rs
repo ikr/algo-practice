0b702dbb1317eb75b881d7b9c690d9d229e31f4d
Ivan Krechetov <ikr@ikr.su>
2024-08-03 10:21:12 +0200

    Go on

--- adventofcode/2015/14_Reindeer_Olympics/src/main.rs
+++ adventofcode/2015/14_Reindeer_Olympics/src/main.rs
@@ -8,12 +8,22 @@ struct Mobility {
     rest_seconds: i64,
 }
 
+impl Mobility {
+    fn distance(&self, t: i64) -> i64 {
+        let p = self.flight_seconds + self.rest_seconds;
+        let full_cycles = t / p;
+        let cycle_distance = self.velocity * self.flight_seconds;
+        let r = (t % p).min(self.flight_seconds);
+        full_cycles * cycle_distance + self.velocity * r
+    }
+}
+
 fn parse_fact(src: &str) -> (String, Mobility) {
     let re = Regex::new(r"^([A-Z][a-z]+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.$").unwrap();
     let caps = re.captures(src).unwrap();
-    let velocity: i64 = caps[2].parse().unwrap();
-    let flight_seconds: i64 = caps[3].parse().unwrap();
-    let rest_seconds: i64 = caps[4].parse().unwrap();
+    let velocity = caps[2].parse().unwrap();
+    let flight_seconds = caps[3].parse().unwrap();
+    let rest_seconds = caps[4].parse().unwrap();
     (
         caps[1].to_string(),
         Mobility {
