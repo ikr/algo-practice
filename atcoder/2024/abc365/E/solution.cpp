a69f2063a6615c0683a6b2d2ff9f2172ffc42879
Ivan Krechetov <ikr@ikr.li>
2024-08-07 07:54:28 +0200

    Debug

--- atcoder/2024/abc365/E/solution.cpp
+++ atcoder/2024/abc365/E/solution.cpp
@@ -3,7 +3,23 @@ using namespace std;
 
 using ll = long long;
 
-static constexpr int M = 27;
+static constexpr int M = 4;
+
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<vector<T>> &xss) {
+    for (const auto &xs : xss) os << xs << '\n';
+    return os;
+}
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
@@ -24,6 +40,8 @@ int main() {
     for (int i = 0; i < M; ++i) {
         if (xs[0] & (1LL << i)) {
             odd[i][0] = 1;
+        } else {
+            evn[i][0] = 1;
         }
 
         for (int j = 1; j < n; ++j) {
@@ -32,10 +50,13 @@ int main() {
                 evn[i][j] = odd[i][j - 1];
             } else {
                 odd[i][j] = odd[i][j - 1];
-                evn[i][j] = evn[i][j - 1];
+                evn[i][j] = 1 + evn[i][j - 1];
             }
         }
     }
+
+    cerr << odd << '\n' << endl;
+    cerr << evn << endl;
 
     ll ans = -1LL * accumulate(cbegin(xs), cend(xs), 0LL);
 
