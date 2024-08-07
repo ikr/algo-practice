33a4f2690d7bbbb3cf89cd107e8c7e3f6ac7fc7c
Ivan Krechetov <ikr@ikr.li>
2024-08-07 07:44:22 +0200

    First draft

--- atcoder/2024/abc365/E/solution.cpp
+++ atcoder/2024/abc365/E/solution.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 using ll = long long;
 
+static constexpr int M = 27;
+
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
@@ -13,12 +15,36 @@ int main() {
     vector<ll> xs(n);
     for (auto &x : xs) cin >> x;
 
-    vector<ll> ss(n, xs.back());
-    for (int i = n - 2; i >= 0; --i) ss[i] = xs[i] ^ ss[i - 1];
+    // odd[i][j] is the number of sub-arrays having odd number of elements with
+    // bit i set, that end at index j
+    vector<vector<ll>> odd(M, vector(n, 0LL));
 
-    const auto sub = [&](const int i, const int j) -> ll {
-        return (j == n - 1) ? ss[i] : (ss[i] ^ ss[i + 1]);
-    };
+    vector<vector<ll>> evn(M, vector(n, 0LL));
 
+    for (int i = 0; i < M; ++i) {
+        if (xs[0] & (1LL << i)) {
+            odd[i][0] = 1;
+        }
+
+        for (int j = 1; j < n; ++j) {
+            if (xs[j] & (1LL << i)) {
+                odd[i][j] = 1 + evn[i][j - 1];
+                evn[i][j] = odd[i][j - 1];
+            } else {
+                odd[i][j] = odd[i][j - 1];
+                evn[i][j] = evn[i][j - 1];
+            }
+        }
+    }
+
+    ll ans = -1LL * accumulate(cbegin(xs), cend(xs), 0LL);
+
+    for (int i = 0; i < M; ++i) {
+        for (int j = 0; j < n; ++j) {
+            ans += (1LL << i) * odd[i][j];
+        }
+    }
+
+    cout << ans << '\n';
     return 0;
 }
