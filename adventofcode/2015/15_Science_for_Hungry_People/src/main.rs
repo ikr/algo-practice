67f1106a1ac9d401e97b00888ff9bfc7a8a97681
Ivan Krechetov <ikr@ikr.su>
2024-08-09 07:52:56 +0200

    DRY

--- adventofcode/2015/15_Science_for_Hungry_People/src/main.rs
+++ adventofcode/2015/15_Science_for_Hungry_People/src/main.rs
@@ -1,18 +1,17 @@
 use regex::Regex;
 use std::io::{self, BufRead};
 
-fn parse_fact(src: &str) -> (String, [i32; 5]) {
+fn parse_fact(src: &str) -> (String, [i8; 5]) {
     let re = Regex::new(r"^([A-Z][a-z]+): capacity (-?\d+), durability (-?\d+), flavor (-?\d+), texture (-?\d+), calories (-?\d+)$").unwrap();
     let caps = re.captures(src).unwrap();
     (
         caps[1].to_string(),
-        [
-            caps[2].parse().unwrap(),
-            caps[3].parse().unwrap(),
-            caps[4].parse().unwrap(),
-            caps[5].parse().unwrap(),
-            caps[6].parse().unwrap(),
-        ],
+        caps.iter()
+            .skip(2)
+            .map(|x| x.unwrap().as_str().parse::<i8>().unwrap())
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap(),
     )
 }
 
