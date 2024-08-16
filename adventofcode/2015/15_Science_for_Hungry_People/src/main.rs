612479df06fee0841e453a9923ef55354f8617d7
Ivan Krechetov <ikr@ikr.li>
2024-08-16 08:35:18 +0200

    Solve part 1

--- adventofcode/2015/15_Science_for_Hungry_People/src/main.rs
+++ adventofcode/2015/15_Science_for_Hungry_People/src/main.rs
@@ -3,8 +3,11 @@ use regex::Regex;
 use std::io::{self, BufRead};
 
 const TOTAL_SPOONS: i8 = 100;
+const M: usize = 4;
 
-fn parse_fact(src: &str) -> (String, [i8; 5]) {
+type Ingredient = (String, [i8; 5]);
+
+fn parse_fact(src: &str) -> Ingredient {
     let re = Regex::new(r"^([A-Z][a-z]+): capacity (-?\d+), durability (-?\d+), flavor (-?\d+), texture (-?\d+), calories (-?\d+)$").unwrap();
     let caps = re.captures(src).unwrap();
     (
@@ -18,9 +21,9 @@ fn parse_fact(src: &str) -> (String, [i8; 5]) {
     )
 }
 
-fn all_ratios(ingridients_count: usize) -> Vec<Vec<i8>> {
-    assert!(ingridients_count > 1);
-    (1..ingridients_count)
+fn all_ratios(ingredients_count: usize) -> Vec<Vec<i8>> {
+    assert!(ingredients_count > 1);
+    (1..ingredients_count)
         .map(|_| (1..TOTAL_SPOONS as i64).collect::<Vec<_>>())
         .multi_cartesian_product()
         .filter(|xs| xs.iter().sum::<i64>() < 100)
@@ -33,25 +36,41 @@ fn all_ratios(ingridients_count: usize) -> Vec<Vec<i8>> {
         .collect()
 }
 
-fn dot_product(xs: &[i8], ys: &[i8]) -> i32 {
-    xs.iter()
-        .zip(ys.iter())
-        .map(|(x, y)| *x as i32 * *y as i32)
-        .sum::<i32>()
+fn property_value_in_a_recipe(
+    ingredients: &[Ingredient],
+    ratio: &[i8],
+    property_index: usize,
+) -> i64 {
+    let ingredient_property_values = ingredients
+        .iter()
+        .map(|(_, xs)| xs[property_index] as i64)
+        .collect::<Vec<_>>();
+
+    ingredient_property_values
+        .into_iter()
+        .zip(ratio.iter())
+        .map(|(x, k)| x * *k as i64)
+        .sum()
+}
+
+fn score(ingredients: &[Ingredient], ratio: &[i8]) -> i64 {
+    (0..M)
+        .map(|i| property_value_in_a_recipe(ingredients, ratio, i))
+        .fold(1, |acc, x| if x > 0 { acc * x } else { 0 })
 }
 
 fn main() {
-    let facts: Vec<_> = io::stdin()
+    let ingredients: Vec<_> = io::stdin()
         .lock()
         .lines()
         .map(|line| parse_fact(&line.unwrap()))
         .collect();
 
-    let rss = all_ratios(facts.first().unwrap().1.len() - 1);
-    let result = facts
+    let rss = all_ratios(ingredients.len());
+    let result = rss
         .into_iter()
-        .map(|(_, cs)| rss.iter().map(|rs| dot_product(&rs, &cs)).max().unwrap())
-        .collect::<Vec<_>>();
-
-    eprintln!("{:?}", result);
+        .map(|rs| score(&ingredients, &rs))
+        .max()
+        .unwrap();
+    println!("{}", result);
 }
