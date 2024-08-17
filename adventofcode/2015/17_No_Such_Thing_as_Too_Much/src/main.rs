1d382e80aa69eb6cc147dd11d125e5b77b233ff6
Ivan Krechetov <ikr@ikr.li>
2024-08-17 13:59:46 +0200

    Solve part 1

---
+++ adventofcode/2015/17_No_Such_Thing_as_Too_Much/src/main.rs
@@ -0,0 +1,27 @@
+use std::io::{self, BufRead};
+
+const A: i32 = 150;
+
+fn subset_value(xs: &[i32], bits: u32) -> i32 {
+    (0..xs.len()).fold(0, |acc, i| {
+        if (1u32 << i) & bits != 0 {
+            acc + xs[i]
+        } else {
+            acc
+        }
+    })
+}
+
+fn main() {
+    let xs: Vec<_> = io::stdin()
+        .lock()
+        .lines()
+        .map(|line| line.unwrap().parse::<i32>().unwrap())
+        .collect();
+
+    let n = xs.len();
+    let result = (0..(1 << n))
+        .filter(|bits| subset_value(&xs, *bits) == A)
+        .count();
+    println!("{}", result);
+}
