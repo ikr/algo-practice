f5dff6e52dfac07249b7c0e3ddca78ac5089ebc4
Ivan Krechetov <ikr@ikr.li>
2024-08-25 16:52:31 +0200

    WA: 3'603'600 is too high

--- adventofcode/2015/20_Infinite_Elves_and_Infinite_Houses/src/main.rs
+++ adventofcode/2015/20_Infinite_Elves_and_Infinite_Houses/src/main.rs
@@ -1,8 +1,8 @@
 use itertools::Itertools;
 
-const P: i64 = 36000000;
+const P: u32 = 36000000;
 
-fn divisors(x: i64) -> Vec<i64> {
+fn divisors(x: u32) -> Vec<u32> {
     let mut result = (1..=x)
         .take_while(|i| i * i <= x)
         .fold(vec![], |mut acc, i| {
@@ -18,11 +18,11 @@ fn divisors(x: i64) -> Vec<i64> {
     result.into_iter().dedup().collect()
 }
 
-fn number_of_presents(house_number: i64) -> i64 {
+fn number_of_presents(house_number: u32) -> u32 {
     divisors(house_number).into_iter().map(|i| i * 10).sum()
 }
 
-fn solve_part_1() -> i64 {
+fn solve_part_1() -> u32 {
     (1..=P)
         .take_while(|i| number_of_presents(*i) < P)
         .max()
@@ -30,8 +30,18 @@ fn solve_part_1() -> i64 {
         + 1
 }
 
-fn solve_part_2() -> i64 {
-    todo!()
+fn solve_part_2() -> usize {
+    let mut presents: Vec<u32> = vec![0; (P + 1) as usize];
+    for e in 1..=P {
+        for m in 1..=50 {
+            let h = (e * m) as usize;
+            presents[h] += e * 11;
+            if presents[h] >= P {
+                return h;
+            }
+        }
+    }
+    P as usize
 }
 
 fn main() {
