1f6412f2bc90c1be1cd1d00ca0f61e08c9eb410e
Ivan Krechetov <ikr@ikr.li>
2024-08-25 11:13:57 +0200

    And a greedy approach worked! Solved part 2.

--- adventofcode/2015/19_Medicine_for_Rudolph/src/main.rs
+++ adventofcode/2015/19_Medicine_for_Rudolph/src/main.rs
@@ -5,8 +5,8 @@ use std::{
 
 fn all_replacements(xs: &str, pattern: &str, replacement: &str) -> Vec<String> {
     let m = pattern.len();
-    let ii = xs.match_indices(pattern);
-    ii.into_iter()
+    xs.match_indices(pattern)
+        .into_iter()
         .map(|(i, _)| xs[0..i].to_owned() + replacement + &xs[i + m..])
         .collect::<Vec<_>>()
 }
@@ -24,14 +24,36 @@ fn solve_part_1(medicine_molecule: &str, rules: &[(String, String)]) -> usize {
         .len()
 }
 
-fn solve_part_2(medicine_molecule: &str, rules: &[(String, String)]) -> u32 {
-    let rhss = rules.iter().map(|(_, x)| x).collect::<Vec<_>>();
-    let xs = rhss
-        .into_iter()
-        .filter(|rhs| medicine_molecule.contains(*rhs))
-        .collect::<Vec<_>>();
-    eprintln!("{:?}", xs);
-    todo!()
+fn replace_first(xs: &str, pattern: &str, replacement: &str) -> String {
+    let m = pattern.len();
+    let i = xs.find(pattern).unwrap();
+    xs[0..i].to_owned() + replacement + &xs[i + m..]
+}
+
+fn suggest_rule_for_deflation(
+    medicine_molecule: &str,
+    rules: &[(String, String)],
+) -> Option<(String, String)> {
+    rules
+        .iter()
+        .filter(|(_, inflated)| medicine_molecule.contains(inflated))
+        .max_by_key(|(deflated, inflated)| inflated.len() - deflated.len())
+        .cloned()
+}
+
+fn solve_part_2(medicine_molecule: &str, rules: &[(String, String)]) -> u16 {
+    eprintln!("{}", medicine_molecule);
+
+    let mut xs = medicine_molecule.to_owned();
+    let mut result: u16 = 0;
+
+    while let Some((deflated, inflated)) = suggest_rule_for_deflation(&xs, rules) {
+        xs = replace_first(&xs, &inflated, &deflated);
+        result += 1;
+        eprintln!("{} → {}: {}", inflated, deflated, xs);
+    }
+
+    result
 }
 
 fn main() {
