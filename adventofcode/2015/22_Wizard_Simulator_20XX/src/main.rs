f68b57f3db0649c9097a391a18962478eb1efac1
Ivan Krechetov <ikr@ikr.su>
2024-08-30 07:56:31 +0200

    Go on

--- adventofcode/2015/22_Wizard_Simulator_20XX/src/main.rs
+++ adventofcode/2015/22_Wizard_Simulator_20XX/src/main.rs
@@ -1,14 +1,14 @@
 #[derive(Clone, Copy, Debug)]
-enum Turn {
+enum WhoseTurn {
     Wizard,
     Boss,
 }
 
-impl Turn {
-    fn next(&self) -> Turn {
+impl WhoseTurn {
+    fn next(&self) -> WhoseTurn {
         match self {
-            Turn::Wizard => Turn::Boss,
-            Turn::Boss => Turn::Wizard,
+            WhoseTurn::Wizard => WhoseTurn::Boss,
+            WhoseTurn::Boss => WhoseTurn::Wizard,
         }
     }
 }
@@ -22,11 +22,25 @@ enum Spell {
     Recharge,
 }
 
+impl Spell {
+    fn cost_mana(&self) -> u16 {
+        match self {
+            Spell::MagicMissile => 53,
+            Spell::Drain => 73,
+            Spell::Shield => 113,
+            Spell::Poison => 173,
+            Spell::Recharge => 229,
+        }
+    }
+}
+
 #[derive(Clone, Copy, Debug)]
 struct Wizard {
-    hit_points: i32,
-    armor: i32,
-    mana: i32,
+    hit_points: i8,
+    armor: u8,
+    mana: u16,
+    shield_left: u8,
+    recharge_left: u8,
 }
 
 impl Wizard {
@@ -35,15 +49,27 @@ impl Wizard {
             hit_points: 50,
             armor: 0,
             mana: 500,
+            shield_left: 0,
+            recharge_left: 0,
+        }
+    }
+
+    fn example_a() -> Wizard {
+        Wizard {
+            hit_points: 10,
+            armor: 0,
+            mana: 250,
+            shield_left: 0,
+            recharge_left: 0,
         }
     }
 }
 
 #[derive(Clone, Copy, Debug)]
 struct Boss {
-    hit_points: i32,
-    damage: i32,
-    poison_left: i32,
+    hit_points: i8,
+    damage: u8,
+    poison_left: u8,
 }
 
 impl Boss {
@@ -51,6 +77,22 @@ impl Boss {
         Boss {
             hit_points: 51,
             damage: 9,
+            poison_left: 0,
+        }
+    }
+
+    fn example_a() -> Boss {
+        Boss {
+            hit_points: 13,
+            damage: 8,
+            poison_left: 0,
+        }
+    }
+
+    fn example_b() -> Boss {
+        Boss {
+            hit_points: 14,
+            damage: 8,
             poison_left: 0,
         }
     }
