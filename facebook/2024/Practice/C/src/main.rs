c5e424958210a991c8e1992c559e434181a50c41
Ivan Krechetov <ikr@ikr.li>
2024-09-23 08:28:59 +0200

    Submit C

--- facebook/2024/Practice/C/src/main.rs
+++ facebook/2024/Practice/C/src/main.rs
@@ -1,6 +1,8 @@
 use itertools::Itertools;
+use num_integer::sqrt;
 use proconio::input;
 use proconio_derive::fastout;
+use rand::seq::IteratorRandom;
 
 fn cross_product(xy1: (i64, i64), xy2: (i64, i64)) -> i64 {
     let (x1, y1) = xy1;
@@ -12,6 +14,21 @@ fn vector_of(xy1: (i64, i64), xy2: (i64, i64)) -> (i64, i64) {
     let (x1, y1) = xy1;
     let (x2, y2) = xy2;
     (x2 - x1, y2 - y1)
+}
+
+fn outliers_for_line(xys: &[(i64, i64)], i0: usize, j0: usize) -> usize {
+    let xy1 = xys[i0];
+    let xy2 = xys[j0];
+    let v0 = vector_of(xy1, xy2);
+
+    xys.iter()
+        .enumerate()
+        .filter(|(i, _)| *i != i0 && *i != j0)
+        .filter(|(_, xy)| {
+            let v1 = vector_of(**xy, xy1);
+            cross_product(v0, v1) != 0
+        })
+        .count()
 }
 
 fn outliers_exact(xys: &[(i64, i64)]) -> usize {
@@ -37,7 +54,24 @@ fn outliers_exact(xys: &[(i64, i64)]) -> usize {
 }
 
 fn outliers_estimation(xys: &[(i64, i64)]) -> usize {
-    outliers_exact(xys)
+    if xys.len() < 1000 {
+        outliers_exact(xys)
+    } else {
+        let mut rng = &mut rand::thread_rng();
+        let n = xys.len();
+
+        let mut result = usize::MAX;
+        let samples_num = sqrt(n);
+
+        for _ in 0..samples_num {
+            let mut ij = [usize::MAX, usize::MAX];
+            (0..n).choose_multiple_fill(&mut rng, &mut ij);
+            let [i, j] = ij;
+
+            result = result.min(outliers_for_line(xys, i, j));
+        }
+        result
+    }
 }
 
 #[fastout]
@@ -51,8 +85,6 @@ fn main() {
             n: usize,
             xys: [(i64, i64); n],
         }
-
-        eprintln!("{:?}", xys);
 
         let result = outliers_estimation(&xys);
         println!("Case #{}: {}", t, result);
