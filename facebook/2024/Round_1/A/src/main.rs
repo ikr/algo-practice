ce1cb456237b427a03e662ecc568639603e260b1
Ivan Krechetov <ikr@ikr.li>
2024-10-05 20:10:51 +0200

    Submit A

--- facebook/2024/Round_1/A/src/main.rs
+++ facebook/2024/Round_1/A/src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 use proconio::input;
 use proconio_derive::fastout;
 
@@ -14,44 +16,51 @@ fn preprocess_ab(ab: &[(i32, i32)]) -> Vec<(f64, f64)> {
         .collect::<Vec<_>>()
 }
 
-fn can_make_it(ab: &[(f64, f64)], v: f64) -> bool {
-    for (i, (a, b)) in ab.iter().enumerate() {
-        let d: f64 = (i + 1) as f64;
-        let t = d / v;
-        if t < *a || t > *b {
-            return false;
-        }
+fn intersection(ab: (f64, f64), cd: (f64, f64)) -> Option<(f64, f64)> {
+    let (a, b) = ab;
+    let (c, d) = cd;
+    if b < c || d < a {
+        None
+    } else {
+        let mut xs = [a, b, c, d];
+        xs.sort_by(|a, b| {
+            if (a - b).abs() < EPS {
+                Ordering::Equal
+            } else {
+                a.partial_cmp(b).unwrap()
+            }
+        });
+        Some((xs[1], xs[2]))
     }
-    true
 }
 
 fn min_speed(ab: Vec<(f64, f64)>) -> Option<f64> {
     let n = ab.len();
-    let xs = &ab[..n - 1];
-
     let (last_a, last_b) = ab.last().unwrap();
     let last_d = n as f64;
 
     let mut lo = last_d / last_b;
-    if can_make_it(xs, lo) {
-        return Some(lo);
-    }
-
     let mut hi = last_d / last_a;
-    if !can_make_it(xs, hi) {
-        return None;
-    }
 
-    while hi - lo >= EPS {
-        let mid = lo + (hi - lo) / 2.0;
-        if can_make_it(xs, mid) {
-            hi = mid;
-        } else {
-            lo = mid;
+    for i in (0..n - 1).rev() {
+        let (a, b) = ab[i];
+
+        let d = (i + 1) as f64;
+        let cur_lo = d / b;
+        let cur_hi = d / a;
+
+        match intersection((lo, hi), (cur_lo, cur_hi)) {
+            Some((x, y)) => {
+                lo = x;
+                hi = y;
+            }
+            None => {
+                return None;
+            }
         }
     }
 
-    Some(hi)
+    Some(lo)
 }
 
 #[fastout]
