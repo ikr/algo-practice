5839b59fb4470811fc8bc59577e506c51cfe977e
Ivan Krechetov <ikr@ikr.li>
2024-10-19 14:04:00 +0200

    Implement an oracle

--- yandex/2024/Cup_Qual/C/src/main.rs
+++ yandex/2024/Cup_Qual/C/src/main.rs
@@ -72,6 +72,38 @@ fn solve(xs: String) -> i32 {
     (lo - hi).abs()
 }
 
+fn span_when_all_definied(xs: &[u8]) -> i32 {
+    let (lo, hi, _) = xs
+        .iter()
+        .fold((0i32, 0i32, 0i32), |(lo, hi, cur), x| match x {
+            b'L' => (lo.min(cur - 1), hi, cur - 1),
+            b'R' => (lo, hi.max(cur + 1), cur + 1),
+            _ => panic!("Expecting only L-s and R-s; got {:?}", xs),
+        });
+    (lo - hi).abs()
+}
+
+fn bruteforce(xs: &[u8]) -> i32 {
+    let q_indices = xs
+        .iter()
+        .enumerate()
+        .filter(|(_, x)| **x == b'?')
+        .map(|(i, _)| i)
+        .collect::<Vec<_>>();
+    let m = q_indices.len();
+    assert!(m <= 20, "Brute-force is unfeasible with {} ?-s", m);
+
+    let mut ys: Vec<u8> = xs.iter().cloned().collect();
+    let mut result = 0;
+    for bits in 0..(1 << m) {
+        for i in 0..m {
+            ys[q_indices[i]] = if (bits & (1 << i)) != 0 { b'L' } else { b'R' }
+        }
+        result = result.max(span_when_all_definied(&ys));
+    }
+    result
+}
+
 fn main() {
     let stdout = io::stdout();
     let handle = stdout.lock();
@@ -79,8 +111,22 @@ fn main() {
 
     let mut scanner = Scanner::default();
     let s: String = scanner.next_string();
-    let result = solve(s);
+    let bs: Vec<u8> = s.bytes().collect();
+    let result = bruteforce(&bs);
     writeln!(writer, "{}", result).unwrap();
 
     writer.flush().unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn span_when_all_defined_works() {
+        assert_eq!(span_when_all_definied(b""), 0);
+        assert_eq!(span_when_all_definied(b"RLRLRL"), 1);
+        assert_eq!(span_when_all_definied(b"LLLL"), 4);
+        assert_eq!(span_when_all_definied(b"LLLRRRRRR"), 6);
+    }
 }
