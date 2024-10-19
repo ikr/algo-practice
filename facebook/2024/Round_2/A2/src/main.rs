55959a1926ca2587ba6b723c4ee61b9372356880
Ivan Krechetov <ikr@ikr.su>
2024-10-19 20:43:46 +0200

    Submit A2

--- facebook/2024/Round_2/A2/src/main.rs
+++ facebook/2024/Round_2/A2/src/main.rs
@@ -13,30 +13,50 @@ fn num_from_source(xs: &VecDeque<u8>) -> u128 {
         .unwrap()
 }
 
+fn digits(mut n: u64) -> VecDeque<u8> {
+    let mut result: VecDeque<u8> = VecDeque::new();
+    while n != 0 {
+        result.push_front((n % 10) as u8);
+        n /= 10;
+    }
+    result
+}
+
 fn all_mountain_nums() -> BTreeSet<u64> {
     let mut result: BTreeSet<u64> = BTreeSet::new();
-    let mut q: VecDeque<VecDeque<u8>> = VecDeque::new();
+    let mut q: VecDeque<u64> = VecDeque::new();
 
     for d in 1..10 {
         let xs = VecDeque::from([d]);
         result.insert(num_from_source(&xs) as u64);
-        q.push_back(xs);
+        q.push_back(d as u64);
     }
 
-    while let Some(xs0) = q.pop_front() {
+    while let Some(x0) = q.pop_front() {
+        let xs0 = digits(x0);
         let a = xs0[0];
         let b = xs0.back().unwrap();
+        let c = xs0[xs0.len() / 2];
 
         for aa in 1..=a {
+            if aa == c {
+                continue;
+            }
+
             for bb in 1..=*b {
+                if bb == c {
+                    continue;
+                }
+
                 let mut xs = xs0.clone();
                 xs.push_front(aa);
                 xs.push_back(bb);
                 if num_from_source(&xs) > MAX_B {
                     break;
                 } else {
-                    result.insert(num_from_source(&xs) as u64);
-                    q.push_back(xs);
+                    let x = num_from_source(&xs) as u64;
+                    result.insert(x);
+                    q.push_back(x);
                 }
             }
         }
@@ -79,5 +99,10 @@ mod tests {
     fn num_from_source_works() {
         assert_eq!(num_from_source(&VecDeque::from([1])), 1);
         assert_eq!(num_from_source(&VecDeque::from([9, 9, 4])), 994);
+    }
+
+    #[test]
+    fn digits_works() {
+        assert_eq!(digits(31415), VecDeque::from([3, 1, 4, 1, 5]));
     }
 }
