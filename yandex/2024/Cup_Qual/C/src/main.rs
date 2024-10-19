cd8f38f7929fa3f9c0493bc97da3af19885a7b90
Ivan Krechetov <ikr@ikr.su>
2024-10-19 14:13:52 +0200

    Scrape it

--- yandex/2024/Cup_Qual/C/src/main.rs
+++ yandex/2024/Cup_Qual/C/src/main.rs
@@ -18,58 +18,9 @@ impl Scanner {
     }
 }
 
-fn solve(xs: String) -> i32 {
-    let bs = xs.bytes().collect::<Vec<_>>();
+fn solve(bs: &[u8]) -> i32 {
     let n = bs.len();
-    let mut dpl: Vec<(i32, i32)> = vec![(0, 0); n];
-    let mut dpr: Vec<(i32, i32)> = vec![(0, 0); n];
-
-    dpl[0] = match bs[0] {
-        b'L' => (-1, -1),
-        b'R' => (1, 1),
-        _ => (-1, -1),
-    };
-
-    dpr[0] = match bs[0] {
-        b'L' => (-1, -1),
-        b'R' => (1, 1),
-        _ => (1, 1),
-    };
-
-    for i in 1..n {
-        let (a, b) = dpl[i - 1];
-        let (c, d) = dpr[i - 1];
-
-        let lo = a.min(c);
-        let hi = b.max(d);
-
-        dpl[i] = match bs[i] {
-            b'L' => (lo - 1, hi - 1),
-            b'R' => (lo + 1, hi + 1),
-            _ => (lo - 1, hi - 1),
-        };
-
-        dpr[i] = match bs[i] {
-            b'L' => (lo - 1, hi - 1),
-            b'R' => (lo + 1, hi + 1),
-            _ => (lo + 1, hi + 1),
-        };
-    }
-
-    let mut lo: i32 = 0;
-    let mut hi: i32 = 0;
-
-    for (a, b) in dpl {
-        lo = lo.min(a);
-        hi = hi.max(b);
-    }
-
-    for (a, b) in dpr {
-        lo = lo.min(a);
-        hi = hi.max(b);
-    }
-
-    (lo - hi).abs()
+    todo!();
 }
 
 fn span_when_all_definied(xs: &[u8]) -> i32 {
@@ -112,7 +63,7 @@ fn main() {
     let mut scanner = Scanner::default();
     let s: String = scanner.next_string();
     let bs: Vec<u8> = s.bytes().collect();
-    let result = bruteforce(&bs);
+    let result = solve(&bs);
     writeln!(writer, "{}", result).unwrap();
 
     writer.flush().unwrap();
