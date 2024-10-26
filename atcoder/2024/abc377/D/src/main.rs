4b75f4cea4db6a45097d5c1f30572e59af4e8c7e
Ivan Krechetov <ikr@ikr.su>
2024-10-26 14:51:03 +0200

    Prototype

---
+++ atcoder/2024/abc377/D/src/main.rs
@@ -0,0 +1,23 @@
+use proconio::input;
+
+fn num_sought_pairs(m: i64, lrs: &[(i64, i64)]) -> i64 {
+    let mut containint_pairs: i64 = 0;
+
+    for (l, r) in lrs {
+        containint_pairs += l * (m - r + 1);
+    }
+
+    let total_pairs = m * (m + 1) / 2;
+    total_pairs - containint_pairs
+}
+
+fn main() {
+    input! {
+        n: usize,
+        m: i64,
+        lrs: [(i64, i64); n],
+    }
+
+    let result = num_sought_pairs(m, &lrs);
+    println!("{}", result);
+}
