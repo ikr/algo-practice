2b6287937a23944268b5e6bac41080e8ce174476
Ivan Krechetov <ikr@ikr.li>
2024-10-26 15:41:12 +0200

    Explore

--- atcoder/2024/abc377/D/src/main.rs
+++ atcoder/2024/abc377/D/src/main.rs
@@ -1,15 +1,28 @@
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap};
+
 use itertools::Itertools;
 use proconio::input;
 
 fn num_sought_pairs(m: i64, lrs: &[(i64, i64)]) -> i64 {
-    let mut containint_pairs: i64 = 0;
+    let mut ops: BTreeMap<i64, Vec<usize>> = BTreeMap::new();
+    let mut cls: BTreeMap<i64, Vec<usize>> = BTreeMap::new();
+    let mut mss: BTreeSet<i64> = BTreeSet::new();
 
-    for (l, r) in lrs {
-        containint_pairs += l * (m - r + 1);
+    for (i, (l, r)) in lrs.iter().enumerate() {
+        ops.entry(*l)
+            .and_modify(|indices| indices.push(i))
+            .or_insert(vec![i]);
+        cls.entry(*r)
+            .and_modify(|indices| indices.push(i))
+            .or_insert(vec![i]);
+        mss.insert(*l);
+        mss.insert(*r);
     }
 
-    let total_pairs = m * (m + 1) / 2;
-    total_pairs - containint_pairs
+    let mut currently_open: BinaryHeap<(i64, usize)> = BinaryHeap::new();
+
+    let mut result = m * (m + 1) / 2;
+    result
 }
 
 fn main() {
