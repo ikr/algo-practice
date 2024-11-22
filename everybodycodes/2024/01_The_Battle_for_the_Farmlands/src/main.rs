0edac44eb5c7a92eac77b42902ddd5438827ec6a
Ivan Krechetov <ikr@ikr.su>
2024-11-22 12:51:35 +0100

    Solve part 3

--- everybodycodes/2024/01_The_Battle_for_the_Farmlands/src/main.rs
+++ everybodycodes/2024/01_The_Battle_for_the_Farmlands/src/main.rs
@@ -10,12 +10,13 @@ fn potions_for_one(x: u8) -> usize {
     }
 }
 
-fn potions_for_pair(pq: (u8, u8)) -> usize {
-    match pq {
-        (b'x', b'x') => 0,
-        (b'x', q) => potions_for_one(q),
-        (p, b'x') => potions_for_one(p),
-        (p, q) => potions_for_one(p) + potions_for_one(q) + 2,
+fn potions_for_tri(mut pqr: [u8; 3]) -> usize {
+    pqr.sort();
+    match pqr {
+        [b'x', b'x', b'x'] => 0,
+        [p, b'x', b'x'] => potions_for_one(p),
+        [p, q, b'x'] => potions_for_one(p) + potions_for_one(q) + 2,
+        [p, q, r] => potions_for_one(p) + potions_for_one(q) + potions_for_one(r) + 6,
     }
 }
 
@@ -23,13 +24,16 @@ fn main() {
     let mut buf = String::new();
     std::io::stdin().read_to_string(&mut buf).unwrap();
 
-    let xss: Vec<(u8, u8)> = buf
+    let xss: Vec<(u8, u8, u8)> = buf
         .trim_end()
         .as_bytes()
-        .chunks(2)
-        .map(|ys| (ys[0], ys[1]))
+        .chunks(3)
+        .map(|ys| (ys[0], ys[1], ys[2]))
         .collect();
 
-    let result: usize = xss.into_iter().map(potions_for_pair).sum();
+    let result: usize = xss
+        .into_iter()
+        .map(|(p, q, r)| potions_for_tri([p, q, r]))
+        .sum();
     println!("{}", result);
 }
