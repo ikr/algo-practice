62c3ebaff7890a2e3c921553566dc01c6ecec8c3
Ivan Krechetov <ikr@ikr.li>
2024-11-22 12:38:26 +0100

    Solve part 2

--- everybodycodes/2024/01_The_Battle_for_the_Farmlands/src/main.rs
+++ everybodycodes/2024/01_The_Battle_for_the_Farmlands/src/main.rs
@@ -1,12 +1,35 @@
 use std::io::Read;
 
+fn potions_for_one(x: u8) -> usize {
+    match x {
+        b'A' => 0,
+        b'B' => 1,
+        b'C' => 3,
+        b'D' => 5,
+        _ => panic!("Unknown unit {}", x),
+    }
+}
+
+fn potions_for_pair(pq: (u8, u8)) -> usize {
+    match pq {
+        (b'x', b'x') => 0,
+        (b'x', q) => potions_for_one(q),
+        (p, b'x') => potions_for_one(p),
+        (p, q) => potions_for_one(p) + potions_for_one(q) + 2,
+    }
+}
+
 fn main() {
     let mut buf = String::new();
     std::io::stdin().read_to_string(&mut buf).unwrap();
 
-    let xs: Vec<u8> = buf.trim_end().as_bytes().to_vec();
+    let xss: Vec<(u8, u8)> = buf
+        .trim_end()
+        .as_bytes()
+        .chunks(2)
+        .map(|ys| (ys[0], ys[1]))
+        .collect();
 
-    let result =
-        xs.iter().filter(|&&x| x == b'B').count() + xs.iter().filter(|&&x| x == b'C').count() * 3;
+    let result: usize = xss.into_iter().map(potions_for_pair).sum();
     println!("{}", result);
 }
