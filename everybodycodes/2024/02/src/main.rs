9649bce2929a2e55dcd47c8118557f123a4c9cf4
Ivan Krechetov <ikr@ikr.li>
2024-11-24 11:23:04 +0100

    Go on

--- everybodycodes/2024/02/src/main.rs
+++ everybodycodes/2024/02/src/main.rs
@@ -10,6 +10,38 @@ impl Crd {
     fn mul_by(self, scalar: i16) -> Crd {
         Crd(self.0 * scalar, self.1 * scalar)
     }
+
+    fn interval_right(&self, length: i16) -> Vec<Crd> {
+        (0..length).map(|i| *self + Crd(0, i)).collect()
+    }
+
+    fn interval_down(&self, length: i16) -> Vec<Crd> {
+        (0..length).map(|i| *self + Crd(i, 0)).collect()
+    }
+
+    fn all_containing_horizontal_intervals(&self, exact_interval_length: i16) -> Vec<Vec<Crd>> {
+        let mut cur = self.interval_right(exact_interval_length);
+        let mut result: Vec<Vec<Crd>> = vec![cur.clone()];
+
+        for _ in 1..exact_interval_length {
+            cur = shift_left(cur);
+            result.push(cur.clone());
+        }
+
+        result
+    }
+
+    fn all_containing_vertical_intervals(&self, exact_interval_length: i16) -> Vec<Vec<Crd>> {
+        let mut cur = self.interval_down(exact_interval_length);
+        let mut result: Vec<Vec<Crd>> = vec![cur.clone()];
+
+        for _ in 1..exact_interval_length {
+            cur = shift_up(cur);
+            result.push(cur.clone());
+        }
+
+        result
+    }
 }
 
 impl std::ops::Add<Crd> for Crd {
@@ -20,23 +52,14 @@ impl std::ops::Add<Crd> for Crd {
     }
 }
 
-#[derive(Clone, Copy, Debug)]
-enum Dir {
-    N,
-    E,
-    S,
-    W,
+fn shift_up(mut crds: Vec<Crd>) -> Vec<Crd> {
+    crds.iter_mut().for_each(|Crd(ro, _)| *ro -= 1);
+    crds
 }
 
-impl Dir {
-    fn delta(&self) -> Crd {
-        match self {
-            Dir::N => Crd(-1, 0),
-            Dir::E => Crd(0, 1),
-            Dir::S => Crd(1, 0),
-            Dir::W => Crd(0, -1),
-        }
-    }
+fn shift_left(mut crds: Vec<Crd>) -> Vec<Crd> {
+    crds.iter_mut().for_each(|Crd(_, co)| *co -= 1);
+    crds
 }
 
 struct Grid {
@@ -44,13 +67,25 @@ struct Grid {
 }
 
 impl Grid {
-    fn height(&self) -> usize {
-        self.rows.len()
+    fn height(&self) -> i16 {
+        self.rows.len() as i16
     }
 
-    fn width(&self) -> usize {
+    fn width(&self) -> i16 {
         assert!(!self.rows.is_empty());
-        self.rows[0].len()
+        self.rows[0].len() as i16
+    }
+
+    fn cell(&self, crd: Crd) -> u8 {
+        let n = self.height();
+        let m = self.width();
+        let ro = (crd.0 + n) % n;
+        let co = (crd.1 + m) % m;
+        self.rows[ro as usize][co as usize]
+    }
+
+    fn interval(&self, crds: &[Crd]) -> Vec<u8> {
+        crds.iter().map(|&crd| self.cell(crd)).collect()
     }
 }
 
@@ -60,11 +95,14 @@ fn reversed<T: Clone>(s: &[T]) -> Vec<T> {
     xs
 }
 
+fn all_reversed<T: Clone>(s: &[Vec<T>]) -> Vec<Vec<T>> {
+    s.iter().map(|xs| reversed(xs)).collect()
+}
+
 fn code_of(xs: &[u8]) -> u64 {
     (0..xs.len() as u32)
         .rev()
         .map(|e| 27u64.pow(e))
-        .into_iter()
         .zip(xs)
         .map(|(b, x)| b * (x - b'@') as u64)
         .sum()
@@ -84,11 +122,16 @@ fn main() {
         .map(|s| s.trim().to_string())
         .collect();
 
-    let max_word_length = words.iter().map(|s| s.len()).max().unwrap();
-    dbg!(max_word_length);
+    let word_lengths = words.iter().map(|s| s.len()).collect::<Vec<_>>();
+    let min_word_length = word_lengths.iter().min().unwrap();
+    let max_word_length = word_lengths.iter().max().unwrap();
+    dbg!(min_word_length, max_word_length);
 
     let grid: Vec<Vec<u8>> = lines[2..]
         .iter()
-        .map(|line| line.as_bytes().iter().cloned().collect())
+        .map(|line| line.as_bytes().to_vec())
         .collect();
+
+    let a = Crd(0, 0).all_containing_vertical_intervals(3);
+    dbg!(a);
 }
