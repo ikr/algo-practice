5f145a1c9aaabfb2ab498603e42719c01cda06c1
Ivan Krechetov <ikr@ikr.su>
2024-11-24 16:39:00 +0100

    Solve part 3

--- everybodycodes/2024/04_Royal_Smiths_Puzzle/src/main.rs
+++ everybodycodes/2024/04_Royal_Smiths_Puzzle/src/main.rs
@@ -1,5 +1,9 @@
 use std::io::{self, BufRead};
 
+fn ops_to_level_at(xs: &[i32], x0: i32) -> i32 {
+    xs.iter().map(|x| x.abs_diff(x0) as i32).sum()
+}
+
 fn main() {
     let xs: Vec<i32> = io::stdin()
         .lock()
@@ -7,7 +11,14 @@ fn main() {
         .map(|line| line.unwrap().parse().unwrap())
         .collect();
 
-    let lo: i32 = *xs.iter().min().unwrap();
-    let result: i32 = xs.iter().map(|x| x - lo).sum();
+    let mut result = i32::MAX;
+
+    for x0 in xs.iter() {
+        let candidate = ops_to_level_at(&xs, *x0);
+        if candidate < result {
+            result = candidate;
+        }
+    }
+
     println!("{}", result);
 }
