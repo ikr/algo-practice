c669f6960446923ac370439343ac480f1ed3cdd5
Ivan Krechetov <ikr@ikr.su>
2024-11-25 06:51:53 +0100

    Solve part 1

--- everybodycodes/2024/05_Pseudo-Random_Clap_Dance/src/main.rs
+++ everybodycodes/2024/05_Pseudo-Random_Clap_Dance/src/main.rs
@@ -25,13 +25,13 @@ struct Chain {
 impl Chain {
     fn do_clapping(&mut self) {
         let n = self.xs.len() as u8;
-        let x0 = (self.xs.pop_front().unwrap()) % (2 * n);
+        let x0 = self.xs.pop_front().unwrap();
 
         if x0 <= n {
             self.xs.insert((x0 - 1) as usize, x0);
         } else {
             let r = x0 - n;
-            self.xs.insert(r as usize, x0);
+            self.xs.insert((n - 1 - r) as usize, x0);
         }
     }
 }
@@ -48,6 +48,30 @@ impl Machine {
             chains,
             current_chain: 0,
         }
+    }
+
+    fn play_round(&mut self) {
+        let x0 = self.chains[self.current_chain].xs.pop_front().unwrap();
+        let next_chain = (self.current_chain + 1) % self.chains.len();
+        self.chains[next_chain].xs.push_front(x0);
+
+        self.chains[next_chain].do_clapping();
+        self.current_chain = next_chain;
+    }
+
+    fn current_result(&self) -> u64 {
+        let n = self.chains.len() as u64;
+        let ms = (0..n)
+            .rev()
+            .map(|i| 10u64.pow(i as u32))
+            .collect::<Vec<_>>();
+        let xs = self
+            .chains
+            .iter()
+            .map(|c| c.xs.front().unwrap())
+            .copied()
+            .collect::<Vec<_>>();
+        ms.iter().zip(xs.iter()).map(|(m, x)| m * (*x as u64)).sum()
     }
 }
 
@@ -70,5 +94,9 @@ fn main() {
         })
         .collect();
 
-    eprintln!("{:?}", column_chains);
+    let mut m = Machine::new(column_chains);
+    for _ in 0..10 {
+        m.play_round();
+        println!("{}", m.current_result());
+    }
 }
