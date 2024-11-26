cb4ef6d99314857aa19bf3d0e030ad50d9ead9a6
Ivan Krechetov <ikr@ikr.su>
2024-11-26 18:19:40 +0100

    Solve part 2

--- everybodycodes/2024/08_A_Shrine_for_Nullpointer/src/main.rs
+++ everybodycodes/2024/08_A_Shrine_for_Nullpointer/src/main.rs
@@ -1,26 +1,30 @@
 use std::io::{self, Read};
 
-fn lowest_layer(num_levels: i64) -> i64 {
-    1 + (num_levels - 1) * 2
-}
-
 fn main() {
     let mut buf = String::new();
     io::stdin().read_to_string(&mut buf).unwrap();
-    let initial_number_of_blocks: i64 = buf.trim().parse().unwrap();
+    let num_priests: i64 = buf.trim().parse().unwrap();
+    let num_acolytes: i64 = 1111;
+    //let num_acolytes: i64 = 5;
+    let initially_available_blocks: i64 = 20240000;
+    //let initially_available_blocks: i64 = 50;
 
-    let mut h: i64 = 1;
+    let mut available_blocks = initially_available_blocks - 1;
+    let mut width: i64 = 1;
+    let mut thickness: i64 = 1;
+
     loop {
-        let an = lowest_layer(h);
-        let s = ((1 + an) * h) / 2;
-        if s >= initial_number_of_blocks {
+        let new_thickness = (thickness * num_priests) % num_acolytes;
+        let needed_for_new_layer = 2 * new_thickness + width * new_thickness;
+
+        if available_blocks < needed_for_new_layer {
+            let result = (width + 2) * (needed_for_new_layer - available_blocks);
+            println!("{}", result);
             break;
         }
-        h += 1;
-    }
 
-    let s = ((1 + lowest_layer(h)) * h) / 2;
-    let missing = s - initial_number_of_blocks;
-    let result = missing * lowest_layer(h);
-    println!("{}", result);
+        available_blocks -= needed_for_new_layer;
+        width += 2;
+        thickness = new_thickness;
+    }
 }
