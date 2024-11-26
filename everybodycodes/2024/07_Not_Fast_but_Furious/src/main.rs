de0c014d51b8135319c8bf8ddc2ca657ffe1fd2c
Ivan Krechetov <ikr@ikr.su>
2024-11-26 07:46:24 +0100

    Solve part 2

--- everybodycodes/2024/07_Not_Fast_but_Furious/src/main.rs
+++ everybodycodes/2024/07_Not_Fast_but_Furious/src/main.rs
@@ -15,17 +15,54 @@ fn parse_line(s: &str) -> (String, Vec<i32>) {
     (lhs, rhs)
 }
 
-fn total_essence_gathered(segments: usize, initial_essence: i32, xs: &[i32]) -> i32 {
+fn total_essence_gathered(track: &[i32], loops: usize, initial_essence: i32, xs: &[i32]) -> i32 {
+    let mut t: usize = 0;
     let mut cur = initial_essence;
     let mut result = 0;
     let m = xs.len();
 
-    for i in 0..segments {
-        cur += xs[i % m];
-        result += cur;
+    for _ in 0..loops {
+        for terrain in track {
+            match (terrain, xs[t % m]) {
+                (-1, _) => cur -= 1,
+                (0, x) => cur += x,
+                (1, _) => cur += 1,
+                _ => panic!("Unknown terrain {} or op {}", terrain, xs[t % m]),
+            }
+            result += cur;
+            t += 1;
+        }
     }
 
     result
+}
+
+fn reversed(s: &str) -> String {
+    s.chars().rev().collect()
+}
+
+fn track_src_a() -> String {
+    "+===".to_string() + "+" + &reversed("=+=-+") + "-S"
+}
+
+fn track_src_1() -> String {
+    "-=++=-==++=++=-=+=-=+=+=--=-=++=-==++=-+=-=+=-=+=+=++=-+==++=++=-=-=--".to_string()
+        + "-=++==-"
+        + &reversed("--==++++==+=+++-=+=-=+=-+-=+-=+-=+=-=+=--=+++=++=+++==++==--=+=++==+++-")
+        + "-=+=+=-S"
+}
+
+fn track_from(track_src: &str) -> Vec<i32> {
+    track_src
+        .chars()
+        .map(|c| match c {
+            '-' => -1,
+            '=' => 0,
+            '+' => 1,
+            'S' => 0,
+            _ => panic!("Unknown char {}", c),
+        })
+        .collect()
 }
 
 fn main() {
@@ -35,7 +72,7 @@ fn main() {
         .map(|line| parse_line(&line.unwrap()))
         .collect::<Vec<_>>();
 
-    lines.sort_by_key(|(_, xs)| -total_essence_gathered(10, 10, &xs));
+    lines.sort_by_key(|(_, xs)| -total_essence_gathered(&track_from(&track_src_1()), 10, 10, xs));
     let result = lines
         .into_iter()
         .map(|(s, _)| s)
