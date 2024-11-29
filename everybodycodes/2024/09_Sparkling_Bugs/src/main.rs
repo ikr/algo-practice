1fcbb3abed58f0c2162a8c605fc7af75b8c17587
Ivan Krechetov <ikr@ikr.su>
2024-11-29 08:12:56 +0100

    Solve part 2

--- everybodycodes/2024/09_Sparkling_Bugs/src/main.rs
+++ everybodycodes/2024/09_Sparkling_Bugs/src/main.rs
@@ -1,46 +1,39 @@
 use std::io::{self, BufRead};
 
-use itertools::Itertools;
+use memoise::memoise;
 
-fn greedy_min_stamps_for(stamps: &[i32], x: i32) -> i32 {
-    let mut r = x;
-    let mut result = 0;
+const INF: u32 = 1_000_000;
 
-    for stamp in stamps.iter().rev() {
-        result += r / stamp;
-        r %= stamp;
-    }
-
-    if r != 0 {
-        i32::MAX
+#[memoise(stamps.len(), x)]
+fn min_stamps_for(stamps: &[u16], x: u16) -> u32 {
+    if x == 0 {
+        0
+    } else if let Some(y) = stamps.last() {
+        let fewer_stamps: Vec<u16> = stamps[0..stamps.len() - 1].to_vec();
+        if y > &x {
+            min_stamps_for(&fewer_stamps, x)
+        } else {
+            min_stamps_for(&fewer_stamps, x).min(1 + min_stamps_for(&stamps, x - y))
+        }
     } else {
-        result
+        INF
     }
-}
-
-fn min_stamps_for(stamps: &[i32], x: i32) -> i32 {
-    stamps
-        .iter()
-        .powerset()
-        .map(|ys| greedy_min_stamps_for(&ys.into_iter().cloned().collect::<Vec<_>>(), x))
-        .min()
-        .unwrap()
 }
 
 fn main() {
-    let stamps = [1, 3, 5, 10, 15, 16, 20, 24, 25, 30];
+    let stamps: Vec<u16> = vec![1, 3, 5, 10, 15, 16, 20, 24, 25, 30];
 
-    let xs: Vec<i32> = io::stdin()
+    let xs: Vec<u16> = io::stdin()
         .lock()
         .lines()
         .map(|line| line.unwrap().parse().unwrap())
         .collect();
 
     let ys = xs
-        .iter()
-        .map(|&x| min_stamps_for(&stamps, x))
+        .into_iter()
+        .map(|x| min_stamps_for(&stamps, x))
         .collect::<Vec<_>>();
     eprintln!("{:?}", ys);
-    let result: i32 = ys.into_iter().sum();
+    let result: u32 = ys.into_iter().sum();
     println!("{}", result);
 }
