06f6e89dba9803567f74669a2b23ae78f9d84b01
Ivan Krechetov <ikr@ikr.li>
2024-11-29 16:42:35 +0100

    Solve part 2

--- everybodycodes/2024/10_Shrine_Needs_to_Shine/src/main.rs
+++ everybodycodes/2024/10_Shrine_Needs_to_Shine/src/main.rs
@@ -17,20 +17,49 @@ fn keep_letters(xs: &[char]) -> HashSet<char> {
         .collect()
 }
 
+fn center_piece(grid: &[Vec<char>]) -> String {
+    let mut s: String = String::new();
+    for ro in 2..=5 {
+        for co in 2..=5 {
+            let xs = keep_letters(&grid[ro]);
+            let ys = keep_letters(&column(grid, co));
+            s.push(*xs.intersection(&ys).next().unwrap());
+        }
+    }
+    s
+}
+
+fn power(s: &str) -> usize {
+    s.bytes()
+        .enumerate()
+        .map(|(i, b)| (i + 1) * ((b - b'A' + 1) as usize))
+        .sum()
+}
+
 fn main() {
-    let grid: Vec<Vec<char>> = io::stdin()
+    eprintln!("{}", power("PTBVRCZHFLJWGMNS"));
+
+    let lines: Vec<Vec<char>> = io::stdin()
         .lock()
         .lines()
         .map(|line| line.unwrap().chars().collect())
         .collect();
 
-    let mut s: String = String::new();
-    for ro in 2..=5 {
-        for co in 2..=5 {
-            let xs = keep_letters(&grid[ro]);
-            let ys = keep_letters(&column(&grid, co));
-            s.push(*xs.intersection(&ys).next().unwrap());
+    let h = lines.len().div_ceil(9);
+    let w = lines[0].len().div_ceil(9);
+    eprintln!("{} x {} grids", h, w);
+
+    let mut result: usize = 0;
+    for i in 0..h {
+        for j in 0..w {
+            let mut grid: Vec<Vec<char>> = vec![];
+
+            for line in lines.iter().skip(i * 9).take(8) {
+                grid.push(line[j * 9..j * 9 + 8].to_vec());
+            }
+
+            result += power(&center_piece(&grid));
         }
     }
-    println!("{}", s);
+    println!("{}", result);
 }
