3ca3e5e511ccd9a4094a8f6965f7d2228bebd08f
Ivan Krechetov <ikr@ikr.li>
2024-11-29 07:37:44 +0100

    WA: 5019

--- everybodycodes/2024/09_Sparkling_Bugs/src/main.rs
+++ everybodycodes/2024/09_Sparkling_Bugs/src/main.rs
@@ -1,6 +1,8 @@
 use std::io::{self, BufRead};
 
-fn min_stamps_for(stamps: &[i32], x: i32) -> i32 {
+use itertools::Itertools;
+
+fn greedy_min_stamps_for(stamps: &[i32], x: i32) -> i32 {
     let mut r = x;
     let mut result = 0;
 
@@ -9,11 +11,24 @@ fn min_stamps_for(stamps: &[i32], x: i32) -> i32 {
         r %= stamp;
     }
 
-    result
+    if r != 0 {
+        i32::MAX
+    } else {
+        result
+    }
+}
+
+fn min_stamps_for(stamps: &[i32], x: i32) -> i32 {
+    stamps
+        .iter()
+        .powerset()
+        .map(|ys| greedy_min_stamps_for(&ys.into_iter().cloned().collect::<Vec<_>>(), x))
+        .min()
+        .unwrap()
 }
 
 fn main() {
-    let stamps = [1, 3, 5, 10];
+    let stamps = [1, 3, 5, 10, 15, 16, 20, 24, 25, 30];
 
     let xs: Vec<i32> = io::stdin()
         .lock()
@@ -21,6 +36,11 @@ fn main() {
         .map(|line| line.unwrap().parse().unwrap())
         .collect();
 
-    let result: i32 = xs.into_iter().map(|x| min_stamps_for(&stamps, x)).sum();
-    eprintln!("{}", result);
+    let ys = xs
+        .iter()
+        .map(|&x| min_stamps_for(&stamps, x))
+        .collect::<Vec<_>>();
+    eprintln!("{:?}", ys);
+    let result: i32 = ys.into_iter().sum();
+    println!("{}", result);
 }
