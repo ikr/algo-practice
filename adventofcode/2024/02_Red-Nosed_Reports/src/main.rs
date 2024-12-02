639d68aadfd72822ebc1d53bd65297870b00db2b
Ivan Krechetov <ikr@ikr.su>
2024-12-02 06:14:55 +0100

    Solve part 2

--- adventofcode/2024/02_Red-Nosed_Reports/src/main.rs
+++ adventofcode/2024/02_Red-Nosed_Reports/src/main.rs
@@ -4,9 +4,23 @@ fn parse_line(s: &str) -> Vec<i32> {
     s.split_whitespace().map(|x| x.parse().unwrap()).collect()
 }
 
-fn is_safe(xs: &[i32]) -> bool {
-    (xs.windows(2).all(|w| w[0] < w[1]) || xs.windows(2).all(|w| w[0] > w[1]))
-        && xs.windows(2).all(|w| w[0].abs_diff(w[1]) <= 3)
+fn without(xs: &[i32], i: usize) -> Vec<i32> {
+    let mut result = xs.to_vec();
+    result.remove(i);
+    result
+}
+
+fn is_safe(xs0: &[i32]) -> bool {
+    let n = xs0.len();
+    for i in 0..n {
+        let xs = without(xs0, i);
+        if (xs.windows(2).all(|w| w[0] < w[1]) || xs.windows(2).all(|w| w[0] > w[1]))
+            && xs.windows(2).all(|w| w[0].abs_diff(w[1]) <= 3)
+        {
+            return true;
+        }
+    }
+    false
 }
 
 fn main() {
