3aacf564c0675eaff0d6f5378989ad441eb29745
Ivan Krechetov <ikr@ikr.su>
2024-12-03 06:51:48 +0100

    Solve part 2

--- adventofcode/2024/03_Mull_It_Over/src/main.rs
+++ adventofcode/2024/03_Mull_It_Over/src/main.rs
@@ -1,27 +1,58 @@
 use regex::Regex;
 use std::io::{self, Read};
 
-fn parse_token(s: &str) -> (i32, i32) {
-    let xs: Vec<i32> = s
-        .strip_prefix("mul(")
-        .and_then(|s| s.strip_suffix(")"))
-        .unwrap()
-        .split(",")
-        .map(|x| x.parse().unwrap())
-        .collect();
-    (xs[0], xs[1])
+#[derive(Debug)]
+enum Op {
+    Mul(i32, i32),
+    Do,
+    Dont,
+}
+
+fn parse_token(s: &str) -> Op {
+    eprintln!("{}", s);
+    let parts: Vec<_> = s.strip_suffix(')').unwrap().split('(').collect();
+
+    match parts[0] {
+        "mul" => {
+            let args: Vec<i32> = parts[1].split(',').map(|x| x.parse().unwrap()).collect();
+            Op::Mul(args[0], args[1])
+        }
+        "do" => Op::Do,
+        "don't" => Op::Dont,
+        _ => panic!("Invalid opcode {}", parts[0]),
+    }
 }
 
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
 
-    let tokens: Vec<_> = Regex::new(r"mul\(\d+,\d+\)")
+    let tokens: Vec<_> = Regex::new(r"mul\(\d+,\d+\)|do\(\)|don't\(\)")
         .unwrap()
         .find_iter(&input)
         .map(|x| parse_token(x.as_str()))
         .collect();
 
-    let result: i32 = tokens.iter().map(|(x, y)| x * y).sum();
+    eprintln!("{:?}", tokens);
+
+    let mut yes = true;
+    let mut result: i32 = 0;
+
+    for op in tokens {
+        match op {
+            Op::Mul(a, b) => {
+                if yes {
+                    result += a * b;
+                }
+            }
+            Op::Do => {
+                yes = true;
+            }
+            Op::Dont => {
+                yes = false;
+            }
+        }
+    }
+
     println!("{}", result);
 }
