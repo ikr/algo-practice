2a6b530e7ea8995f1e5e3553cc76fe27decc4b01
Ivan Krechetov <ikr@ikr.li>
2024-12-06 06:22:40 +0100

    Solve part 2

--- adventofcode/2024/06_Guard_Gallivant/src/main.rs
+++ adventofcode/2024/06_Guard_Gallivant/src/main.rs
@@ -14,7 +14,7 @@ impl std::ops::Add<Crd> for Crd {
     }
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 enum Dir {
     N,
     E,
@@ -53,37 +53,56 @@ fn guard_position(grid: &[Vec<char>]) -> Crd {
     panic!("^ not found");
 }
 
-fn main() {
-    let grid: Vec<Vec<char>> = io::stdin()
-        .lock()
-        .lines()
-        .map(|line| line.unwrap().chars().collect())
-        .collect();
-
+fn is_looped(grid: &[Vec<char>], initial_pos: Crd) -> bool {
     let h = grid.len() as i32;
     let w = grid[0].len() as i32;
 
     let in_bounds = |Crd(ro, co): Crd| 0 <= ro && ro < h && 0 <= co && co < w;
     let cell_at = |Crd(ro, co): Crd| grid[ro as usize][co as usize];
 
-    let mut pos = guard_position(&grid);
+    let mut pos = initial_pos;
     let mut dir = Dir::N;
-    let mut visited: HashSet<Crd> = HashSet::new();
-    visited.insert(pos);
+
+    let mut states: HashSet<(Crd, Dir)> = HashSet::new();
+    states.insert((pos, dir));
 
     loop {
         let next_pos = pos + dir.delta();
         if !in_bounds(next_pos) {
-            break;
+            return false;
         }
 
         if cell_at(next_pos) == '#' {
             dir = dir.turn_right();
         } else {
             pos = next_pos;
-            visited.insert(pos);
+        }
+        if states.contains(&(pos, dir)) {
+            return true;
+        }
+        states.insert((pos, dir));
+    }
+}
+
+fn main() {
+    let grid0: Vec<Vec<char>> = io::stdin()
+        .lock()
+        .lines()
+        .map(|line| line.unwrap().chars().collect())
+        .collect();
+
+    let initial_pos = guard_position(&grid0);
+    let mut result = 0;
+    for (i, row) in grid0.iter().enumerate() {
+        for (j, &cell) in row.iter().enumerate() {
+            if cell == '.' && Crd(i as i32, j as i32) != initial_pos {
+                let mut grid = grid0.clone();
+                grid[i][j] = '#';
+                if is_looped(&grid, initial_pos) {
+                    result += 1;
+                }
+            }
         }
     }
-
-    println!("{}", visited.len());
+    println!("{}", result);
 }
