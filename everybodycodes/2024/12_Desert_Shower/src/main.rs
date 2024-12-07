313a1d21649098d2eda7438b02496cf96a6f4154
Ivan Krechetov <ikr@ikr.su>
2024-12-07 16:46:40 +0100

    Introduce `simulate_tick_return_hits_ranking_score`

--- everybodycodes/2024/12_Desert_Shower/src/main.rs
+++ everybodycodes/2024/12_Desert_Shower/src/main.rs
@@ -112,8 +112,50 @@ fn parse_line(s: &str) -> Crd {
     Crd(xs[0], xs[1])
 }
 
+fn simulate_tick_return_hits_ranking_score(
+    mut ps: Vec<Projectile>,
+    mut ms: Vec<Meteor>,
+) -> (u64, Vec<Projectile>, Vec<Meteor>) {
+    let mut hits_ranking_score: u64 = 0;
+
+    ps = ps
+        .into_iter()
+        .map(|p| p.tick())
+        .filter(|p| !p.has_landed())
+        .collect();
+    ps.sort_by_key(|p| p.ranking());
+
+    let mut hit_projectiles: HashSet<Projectile> = HashSet::new();
+    let mut hit_meteors: HashSet<Meteor> = HashSet::new();
+
+    ms = ms.iter().map(|x| x.tick()).collect();
+
+    for &m in ms.iter() {
+        assert!(!m.has_landed());
+        for &p in ps.iter() {
+            if p.crd == m.crd {
+                eprintln!("{:?} hits {:?}", p, m);
+                hit_projectiles.insert(p);
+                hit_meteors.insert(m);
+                hits_ranking_score += p.ranking() as u64;
+                break;
+            }
+        }
+    }
+
+    (
+        hits_ranking_score,
+        ps.into_iter()
+            .filter(|p| !hit_projectiles.contains(p))
+            .collect(),
+        ms.into_iter()
+            .filter(|m| !hit_meteors.contains(m))
+            .collect(),
+    )
+}
+
 fn main() {
-    let mut ms: HashSet<Meteor> = io::stdin()
+    let mut ms: Vec<Meteor> = io::stdin()
         .lock()
         .lines()
         .map(|line| Meteor {
@@ -121,55 +163,30 @@ fn main() {
         })
         .collect();
 
-    let catapults = vec![Crd(0, 0), Crd(0, 1), Crd(0, 2)];
-    let highest_shooting_power: u16 = 4000;
+    let catapults = [Crd(0, 0), Crd(0, 1), Crd(0, 2)];
 
-    let mut ps: HashSet<Projectile> = catapults
-        .into_iter()
-        .enumerate()
-        .flat_map(|(i, crd)| {
-            (1..=highest_shooting_power)
-                .map(|shooting_power| Projectile::new(crd, i, shooting_power))
-                .collect::<HashSet<_>>()
-        })
-        .collect();
-
+    let mut ps: Vec<Projectile> = vec![];
     let mut result: u64 = 0;
-    while !ms.is_empty() {
-        let mut new_ms: HashSet<Meteor> = HashSet::new();
-        let mut hit_projectiles: Vec<Projectile> = vec![];
 
-        for m0 in ms {
-            let m = m0.tick();
-            if m.has_landed() {
-                panic!("Missed the {:?}", m);
-            }
+    // ps.push(Projectile::new(catapults[0], 0, 2));
+    // ps.push(Projectile::new(catapults[2], 2, 2));
+    // ps.push(Projectile::new(catapults[2], 2, 1));
 
-            let mut hit = false;
-            for &p in ps.iter() {
-                if p.crd == m.crd {
-                    eprintln!("{:?} hits {:?}", p, m);
-                    hit_projectiles.push(p);
-                    result += p.ranking() as u64;
-                    hit = true;
-                    break;
-                }
-            }
-            if !hit {
-                new_ms.insert(m);
-            }
+    for t in 1..16 {
+        if ms.is_empty() {
+            break;
         }
 
-        for p in hit_projectiles {
-            ps.remove(&p);
-        }
-
+        eprintln!("t = {}", t);
+        let (gain, new_ps, new_ms) = simulate_tick_return_hits_ranking_score(ps, ms);
+        result += gain;
+        ps = new_ps;
         ms = new_ms;
-        ps = ps
-            .into_iter()
-            .map(|p| p.tick())
-            .filter(|p| !p.has_landed())
-            .collect();
+
+        if t == 1 {
+            ps.push(Projectile::new(catapults[0], 0, 2));
+        }
     }
+
     println!("{}", result);
 }
