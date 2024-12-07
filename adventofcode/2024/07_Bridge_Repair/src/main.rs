2b4310830f7328d95aaaac8c258c957c3d680495
Ivan Krechetov <ikr@ikr.li>
2024-12-07 07:00:00 +0100

    Solve part 2

--- adventofcode/2024/07_Bridge_Repair/src/main.rs
+++ adventofcode/2024/07_Bridge_Repair/src/main.rs
@@ -11,20 +11,37 @@ fn parse_line(s: &str) -> (u64, Vec<u64>) {
     (parts[0].parse().unwrap(), rhs)
 }
 
-fn evaluate(xs: &[u64], mul_indices: &[usize]) -> u64 {
+fn concat(x: u64, y: u64) -> u64 {
+    (x.to_string() + &y.to_string()).parse().unwrap()
+}
+
+fn evaluate(xs: &[u64], mul_indices: &[usize], concat_indices: &[usize]) -> u64 {
     xs.iter().enumerate().skip(1).fold(xs[0], |acc, (i, &x)| {
         if mul_indices.contains(&(i - 1)) {
             acc * x
+        } else if concat_indices.contains(&(i - 1)) {
+            concat(acc, x)
         } else {
             acc + x
         }
     })
 }
 
+fn without(ii: &[usize], jj: &[usize]) -> Vec<usize> {
+    ii.iter().filter(|i| !jj.contains(i)).cloned().collect()
+}
+
 fn is_satisfiable(lhs: u64, xs: &[u64]) -> bool {
-    (0..xs.len() - 1)
-        .powerset()
-        .any(|ii| evaluate(xs, &ii) == lhs)
+    let n = xs.len();
+    for mul_indices in (0..n - 1).powerset() {
+        let ri = without(&(0..n - 1).collect::<Vec<_>>(), &mul_indices);
+        for concat_indices in ri.into_iter().powerset() {
+            if evaluate(xs, &mul_indices, &concat_indices) == lhs {
+                return true;
+            }
+        }
+    }
+    false
 }
 
 fn main() {
