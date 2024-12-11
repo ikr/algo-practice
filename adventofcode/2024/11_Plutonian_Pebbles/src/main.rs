a5e63c265e5b8b25828345a7498138034d64a822
Ivan Krechetov <ikr@ikr.li>
2024-12-11 06:12:53 +0100

    Solve part 1

---
+++ adventofcode/2024/11_Plutonian_Pebbles/src/main.rs
@@ -0,0 +1,38 @@
+use std::io::{self, BufRead};
+
+fn evolve(x: u64) -> Vec<u64> {
+    if x == 0 {
+        vec![1]
+    } else {
+        let ds: Vec<u8> = x.to_string().bytes().collect();
+        if ds.len() % 2 == 0 {
+            let a: String = String::from_utf8(ds[0..ds.len() / 2].to_vec()).unwrap();
+            let b: String = String::from_utf8(ds[ds.len() / 2..].to_vec()).unwrap();
+            [a, b].iter().map(|x| x.parse().unwrap()).collect()
+        } else {
+            vec![x * 2024]
+        }
+    }
+}
+
+fn main() {
+    let lines: Vec<String> = io::stdin()
+        .lock()
+        .lines()
+        .map(|line| line.unwrap().to_string())
+        .collect();
+
+    let mut xs: Vec<u64> = lines[0]
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
+    eprintln!("{:?}", xs);
+
+    for _ in 0..25 {
+        let new_xs: Vec<u64> = xs.iter().flat_map(|&x| evolve(x)).collect();
+        xs = new_xs;
+        eprintln!("{:?}", xs);
+    }
+
+    println!("{}", xs.len());
+}
