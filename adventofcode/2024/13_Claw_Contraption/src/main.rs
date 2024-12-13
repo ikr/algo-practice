005af4672a187c73f7d7953aa6df7683d5f32ea8
Ivan Krechetov <ikr@ikr.su>
2024-12-13 07:28:19 +0100

    Clean up

--- adventofcode/2024/13_Claw_Contraption/src/main.rs
+++ adventofcode/2024/13_Claw_Contraption/src/main.rs
@@ -3,35 +3,19 @@ use std::io::{self, BufRead};
 use itertools::Itertools;
 use regex::Regex;
 
-#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 struct Crd(i64, i64);
 
 const A_COST: i64 = 3;
 const B_COST: i64 = 1;
 const ADDENDUM: i64 = 10000000000000;
 
-impl std::ops::Add<Crd> for Crd {
-    type Output = Crd;
-
-    fn add(self, o: Crd) -> Crd {
-        Crd(self.0 + o.0, self.1 + o.1)
-    }
-}
-
-impl Crd {
-    fn mul_by(&self, n: i64) -> Crd {
-        Crd(self.0 * n, self.1 * n)
-    }
-}
-
-#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 struct Machine {
     da: Crd,
     db: Crd,
     prize: Crd,
 }
 
-fn parse_dbutton(s: &str) -> Crd {
+fn parse_button(s: &str) -> Crd {
     let re = Regex::new(r"^Button .: X\+(\d+), Y\+(\d+)$").unwrap();
     let caps = re.captures(s).unwrap();
     Crd(caps[1].parse().unwrap(), caps[2].parse().unwrap())
@@ -49,8 +33,8 @@ fn parse_prize(s: &str) -> Crd {
 fn parse_machine(tri: &[String]) -> Machine {
     assert_eq!(tri.len(), 3);
     Machine {
-        da: parse_dbutton(&tri[0]),
-        db: parse_dbutton(&tri[1]),
+        da: parse_button(&tri[0]),
+        db: parse_button(&tri[1]),
         prize: parse_prize(&tri[2]),
     }
 }
@@ -99,10 +83,6 @@ fn main() {
 
     let result = machines
         .into_iter()
-        .map(|m| {
-            eprintln!("{:?}", m.optimal_cost());
-            m
-        })
         .filter_map(|m| m.optimal_cost())
         .sum::<i64>();
 
