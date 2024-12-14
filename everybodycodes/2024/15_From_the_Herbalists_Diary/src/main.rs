8ac27b5ab48ceb8e91273b4e465ce7ddd94980cf
Ivan Krechetov <ikr@ikr.li>
2024-12-14 13:49:22 +0100

    Draft the TSP

--- everybodycodes/2024/15_From_the_Herbalists_Diary/src/main.rs
+++ everybodycodes/2024/15_From_the_Herbalists_Diary/src/main.rs
@@ -9,7 +9,6 @@ use memoize::memoize;
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 struct Crd(i16, i16);
 
-#[memoize(Ignore: grid)]
 fn min_distance(grid: &[Vec<u8>], source: Crd, destination: Crd) -> u16 {
     let in_bounds = |crd: Crd| -> bool {
         0 <= crd.0 && crd.0 < grid.len() as i16 && 0 <= crd.1 && crd.1 < grid[0].len() as i16
@@ -36,17 +35,21 @@ fn min_distance(grid: &[Vec<u8>], source: Crd, destination: Crd) -> u16 {
     while let Some(u) = q.pop_front() {
         for v in adjacent(u) {
             if !distances.contains_key(&v) {
+                if v == destination {
+                    return distances[&u] + 1;
+                }
                 distances.insert(v, distances[&u] + 1);
                 q.push_back(v);
             }
         }
     }
 
-    distances[&destination]
+    panic!("Couldn't reach the destination")
 }
 
-#[memoize(Ignore: source, Ignore: herb_kind_crds)]
-fn min_tour_distance(
+#[memoize(Ignore: grid, Ignore: source, Ignore: herb_kind_crds)]
+fn min_route_length(
+    grid: &[Vec<u8>],
     source: Crd,
     herb_kind_crds: &HashMap<u8, Vec<Crd>>,
     visited_kind_bits: u16,
@@ -54,7 +57,43 @@ fn min_tour_distance(
     last_visited_crd_index: u8,
 ) -> u16 {
     let ks: Vec<u8> = herb_kind_crds.keys().copied().sorted().collect();
-    todo!()
+    assert!(last_visited_kind_index < ks.len() as u8);
+    assert!(
+        last_visited_crd_index < herb_kind_crds[&ks[last_visited_kind_index as usize]].len() as u8
+    );
+
+    if visited_kind_bits.count_ones() == 1 {
+        min_distance(
+            grid,
+            source,
+            herb_kind_crds[&ks[last_visited_kind_index as usize]][last_visited_crd_index as usize],
+        )
+    } else {
+        let mut sub = u16::MAX;
+        let n = ks.len() as u8;
+        for i in 0..n {
+            if i != last_visited_kind_index && (1u16 << i) & visited_kind_bits != 0 {
+                for j in 0..herb_kind_crds[&ks[i as usize]].len() as u8 {
+                    sub = sub.min(
+                        min_distance(
+                            grid,
+                            herb_kind_crds[&ks[last_visited_kind_index as usize]]
+                                [last_visited_crd_index as usize],
+                            herb_kind_crds[&ks[i as usize]][j as usize],
+                        ) + min_route_length(
+                            grid,
+                            source,
+                            herb_kind_crds,
+                            visited_kind_bits ^ (1u16 << i),
+                            i,
+                            j,
+                        ),
+                    );
+                }
+            }
+        }
+        sub
+    }
 }
 
 fn main() {
@@ -94,4 +133,21 @@ fn main() {
         herb_kind_counts,
         herb_kind_counts.iter().product::<usize>()
     );
+
+    let ks: Vec<u8> = herb_kind_crds.keys().copied().sorted().collect();
+    let n = ks.len() as u8;
+    let visited_kind_bits = (1u16 << n) - 1;
+    let mut result: u16 = u16::MAX;
+
+    for i in 0..n {
+        for j in 0..herb_kind_crds[&ks[i as usize]].len() as u8 {
+            let crd = herb_kind_crds[&ks[i as usize]][j as usize];
+            result = result.min(
+                min_route_length(&grid, source, &herb_kind_crds, visited_kind_bits, i, j)
+                    + min_distance(&grid, source, crd),
+            )
+        }
+    }
+
+    println!("{}", result);
 }
