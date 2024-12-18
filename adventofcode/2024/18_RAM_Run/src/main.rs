89ef6bb0f990a396f704adc2380206d6abcd7302
Ivan Krechetov <ikr@ikr.li>
2024-12-18 06:20:08 +0100

    Solve part 2

--- adventofcode/2024/18_RAM_Run/src/main.rs
+++ adventofcode/2024/18_RAM_Run/src/main.rs
@@ -3,10 +3,8 @@ use std::{
     io::{self, BufRead},
 };
 
-// const HI: usize = 6;
-// const BYTES: usize = 12;
+//const HI: usize = 6;
 const HI: usize = 70;
-const BYTES: usize = 1024;
 
 fn parse_pair(s: &str) -> (i8, i8) {
     let parts = s.split(',').collect::<Vec<_>>();
@@ -20,41 +18,44 @@ fn main() {
         .map(|line| parse_pair(&line.unwrap()))
         .collect();
 
-    eprintln!("{:?}", ps);
+    for lim in 1..=ps.len() {
+        let obstacles: HashSet<(i8, i8)> = ps[0..lim].iter().cloned().collect();
 
-    let obstacles: HashSet<(i8, i8)> = ps[0..BYTES].iter().cloned().collect();
+        let mut distances: HashMap<(i8, i8), u16> = HashMap::new();
+        distances.insert((0, 0), 0);
+        let mut queue: VecDeque<(i8, i8)> = VecDeque::new();
+        queue.push_back((0, 0));
 
-    let mut distances: HashMap<(i8, i8), u16> = HashMap::new();
-    distances.insert((0, 0), 0);
-    let mut queue: VecDeque<(i8, i8)> = VecDeque::new();
-    queue.push_back((0, 0));
+        let adjacent = |(x, y): (i8, i8)| -> Vec<(i8, i8)> {
+            let mut qs = vec![];
+            if x > 0 && !obstacles.contains(&(x - 1, y)) {
+                qs.push((x - 1, y));
+            }
+            if y > 0 && !obstacles.contains(&(x, y - 1)) {
+                qs.push((x, y - 1));
+            }
+            if x < HI as i8 && !obstacles.contains(&(x + 1, y)) {
+                qs.push((x + 1, y));
+            }
+            if y < HI as i8 && !obstacles.contains(&(x, y + 1)) {
+                qs.push((x, y + 1));
+            }
+            qs
+        };
 
-    let adjacent = |(x, y): (i8, i8)| -> Vec<(i8, i8)> {
-        let mut qs = vec![];
-        if x > 0 && !obstacles.contains(&(x - 1, y)) {
-            qs.push((x - 1, y));
-        }
-        if y > 0 && !obstacles.contains(&(x, y - 1)) {
-            qs.push((x, y - 1));
-        }
-        if x < HI as i8 && !obstacles.contains(&(x + 1, y)) {
-            qs.push((x + 1, y));
-        }
-        if y < HI as i8 && !obstacles.contains(&(x, y + 1)) {
-            qs.push((x, y + 1));
-        }
-        qs
-    };
-
-    while let Some((x, y)) = queue.pop_front() {
-        let d = distances[&(x, y)];
-        for (nx, ny) in adjacent((x, y)) {
-            if !distances.contains_key(&(nx, ny)) {
-                distances.insert((nx, ny), d + 1);
-                queue.push_back((nx, ny));
+        while let Some((x, y)) = queue.pop_front() {
+            let d = distances[&(x, y)];
+            for (nx, ny) in adjacent((x, y)) {
+                if !distances.contains_key(&(nx, ny)) {
+                    distances.insert((nx, ny), d + 1);
+                    queue.push_back((nx, ny));
+                }
             }
         }
-    }
 
-    println!("{}", distances[&(HI as i8, HI as i8)]);
+        if !distances.contains_key(&(HI as i8, HI as i8)) {
+            println!("{:?}", ps[lim - 1]);
+            break;
+        }
+    }
 }
