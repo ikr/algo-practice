15e910ebceaa06f084c65d1fd34f16e3c1cf6de7
Ivan Krechetov <ikr@ikr.su>
2024-12-23 07:10:54 +0100

    Explore

--- adventofcode/2024/23_LAN_Party/src/main.rs
+++ adventofcode/2024/23_LAN_Party/src/main.rs
@@ -1,8 +1,5 @@
 use std::io::{self, BufRead};
 
-use ac_library::Dsu;
-use itertools::Itertools;
-
 fn parse_line(s: &str) -> (String, String) {
     let parts = s.split("-").collect::<Vec<_>>();
     (parts[0].to_string(), parts[1].to_string())
@@ -17,6 +14,15 @@ fn decode(x: usize) -> String {
     String::from_utf8(vec![(x / 27) as u8 + b'`', (x % 27) as u8 + b'`']).unwrap()
 }
 
+fn contains_t(u: usize) -> bool {
+    decode(u).contains('t')
+}
+
+fn normalize(mut tri: [usize; 3]) -> [usize; 3] {
+    tri.sort();
+    tri
+}
+
 fn main() {
     assert_eq!(decode(encode("fx")), "fx");
     assert_eq!(decode(encode("aa")), "aa");
@@ -29,22 +35,18 @@ fn main() {
         .map(|line| parse_line(&line.unwrap()))
         .collect();
 
-    let mut dsu = Dsu::new(27 * 27);
-    for (a, b) in pairs {
-        dsu.merge(encode(&a), encode(&b));
+    let mut g: Vec<Vec<usize>> = vec![vec![]; 27 * 27];
+    for (u, v) in pairs {
+        g[encode(&u)].push(encode(&v));
+        g[encode(&v)].push(encode(&u));
     }
 
-    let g = dsu.groups().into_iter().find(|g| g.len() > 1).unwrap();
-    eprintln!("{:?}", g);
+    let are_connected = |u: usize, v: usize| g[u].contains(&v);
 
-    let result = g
-        .into_iter()
-        .combinations(3)
-        .filter(|tri| {
-            let s: String = tri.iter().map(|&x| decode(x)).collect();
-            s.contains('t')
-        })
-        .count();
-
-    println!("{}", result);
+    for (u, vs) in g.into_iter().enumerate() {
+        if !vs.is_empty() {
+            let vss: Vec<String> = vs.iter().map(|v| decode(*v)).collect();
+            eprintln!("{} {:?}", decode(u), vss);
+        }
+    }
 }
