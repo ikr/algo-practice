ee2151eba2b0073e5744a4200aa644f8cdc6c302
Ivan Krechetov <ikr@ikr.su>
2024-12-24 07:06:07 +0100

    Solve part 1

--- adventofcode/2024/24_Crossed_Wires/src/main.rs
+++ adventofcode/2024/24_Crossed_Wires/src/main.rs
@@ -3,7 +3,7 @@ use std::{
     io::{self, BufRead},
 };
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum OpCode {
     And,
     Or,
@@ -21,7 +21,7 @@ impl OpCode {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Gate {
     opcode: OpCode,
     lhs_id: String,
@@ -57,6 +57,50 @@ fn parse_input(s: &str) -> (String, u8) {
     (parts[0].to_string(), parts[1].parse().unwrap())
 }
 
+struct Machine {
+    values: BTreeMap<String, u8>,
+    gates_by_input: BTreeMap<String, Vec<Gate>>,
+}
+
+impl Machine {
+    fn propagate(&mut self, id: &str) {
+        let mut ids_to_propagate: Vec<String> = vec![];
+
+        if let Some(gates) = self.gates_by_input.get(id) {
+            for gate in gates.iter() {
+                if let Some(lhs) = self.values.get(&gate.lhs_id) {
+                    if let Some(rhs) = self.values.get(&gate.rhs_id) {
+                        let value = match gate.opcode {
+                            OpCode::And => lhs & rhs,
+                            OpCode::Or => lhs | rhs,
+                            OpCode::Xor => lhs ^ rhs,
+                        };
+
+                        self.values.insert(gate.out_id.clone(), value);
+                        ids_to_propagate.push(gate.out_id.clone());
+                    }
+                }
+            }
+        }
+
+        for id in ids_to_propagate {
+            self.propagate(&id);
+        }
+    }
+
+    fn gather_zvalues(&self) -> usize {
+        let mut result = 0;
+        for i in 0..63 {
+            let id: String = format!("z{:02}", i);
+            if let Some(value) = self.values.get(&id) {
+                let x = (*value as usize) << i;
+                result |= x;
+            }
+        }
+        result
+    }
+}
+
 fn main() {
     let lines: Vec<String> = io::stdin()
         .lock()
@@ -65,9 +109,28 @@ fn main() {
         .collect();
 
     let isep = lines.iter().position(|s| s.is_empty()).unwrap();
-
-    let inputs: BTreeMap<String, u8> = lines[0..isep].iter().map(|s| parse_input(&s)).collect();
-    eprintln!("{:?}", inputs);
+    let values: BTreeMap<String, u8> = lines[0..isep].iter().map(|s| parse_input(&s)).collect();
     let gates: Vec<Gate> = lines[isep + 1..].iter().map(|s| Gate::parse(&s)).collect();
-    eprintln!("{:?}", gates);
+    let input_ids: Vec<String> = values.keys().cloned().collect();
+
+    let gates_by_input: BTreeMap<String, Vec<Gate>> =
+        gates.iter().fold(BTreeMap::new(), |mut acc, gate| {
+            acc.entry(gate.lhs_id.clone())
+                .or_insert_with(Vec::new)
+                .push(gate.clone());
+            acc
+        });
+
+    let mut m = Machine {
+        values,
+        gates_by_input,
+    };
+
+    for _ in 0..1000 {
+        for id in input_ids.iter() {
+            m.propagate(&id);
+        }
+    }
+
+    println!("{}", m.gather_zvalues());
 }
