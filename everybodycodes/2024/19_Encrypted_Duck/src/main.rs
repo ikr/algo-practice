b1a23ae4a7bd0eee51d030c4ad33635b042c04be
Ivan Krechetov <ikr@ikr.su>
2025-01-04 20:51:39 +0100

    Let's try something

--- everybodycodes/2024/19_Encrypted_Duck/src/main.rs
+++ everybodycodes/2024/19_Encrypted_Duck/src/main.rs
@@ -1,5 +1,9 @@
 use std::io::{self, BufRead};
 
+use num_integer::lcm;
+
+const ROUNDS: usize = 1_048_576_000;
+
 #[derive(Clone, Copy, Debug)]
 enum Dir {
     L,
@@ -37,11 +41,14 @@ impl Crd {
     }
 }
 
-struct Grid {
-    rows: Vec<Vec<u8>>,
+struct Grid<T> {
+    rows: Vec<Vec<T>>,
 }
 
-impl Grid {
+impl<T> Grid<T>
+where
+    T: Copy,
+{
     fn height(&self) -> usize {
         self.rows.len()
     }
@@ -59,13 +66,15 @@ impl Grid {
             Dir::R => dst.rotate_left(1),
         }
 
-        let xs: Vec<u8> = src.into_iter().map(|Crd(i, j)| self.rows[i][j]).collect();
+        let xs: Vec<T> = src.into_iter().map(|Crd(i, j)| self.rows[i][j]).collect();
 
         for (x, Crd(i, j)) in xs.into_iter().zip(dst) {
             self.rows[i][j] = x;
         }
     }
+}
 
+impl Grid<u8> {
     fn eprintln(&self) {
         for row in self.rows.iter() {
             eprintln!("{}", String::from_utf8(row.to_vec()).unwrap());
@@ -82,18 +91,41 @@ fn main() {
 
     let keys: Vec<Dir> = lines[0].iter().map(|x| Dir::from(*x)).collect();
 
-    let mut g = Grid {
+    let g = Grid {
         rows: lines[2..].to_vec(),
     };
 
-    for _ in 0..1_048_576_000 {
-        for i in 1..g.height() - 1 {
-            for j in 1..g.width() - 1 {
-                let k = (i - 1) * (g.width() - 2) + j - 1;
-                g.rotate_at(Crd(i, j), keys[k % keys.len()]);
+    let mut crd_rows: Vec<Vec<Crd>> = vec![vec![Crd(0, 0); g.width()]; g.height()];
+    for i in 0..g.height() {
+        for j in 0..g.width() {
+            crd_rows[i][j] = Crd(i, j);
+        }
+    }
+    let mut cg = Grid { rows: crd_rows };
+
+    let rotation_points_num = (g.height() - 2) * (g.width() - 2);
+    let m = lcm(rotation_points_num, keys.len());
+    let mut k: usize = 0;
+    'outer: loop {
+        for i in 1..cg.height() - 1 {
+            for j in 1..cg.width() - 1 {
+                cg.rotate_at(Crd(i, j), keys[k % keys.len()]);
+                k += 1;
+                if k == m {
+                    break 'outer;
+                }
             }
         }
     }
 
-    g.eprintln();
+    // for _ in 0..1_048_576_000 {
+    //     for i in 1..g.height() - 1 {
+    //         for j in 1..g.width() - 1 {
+    //             let k = (i - 1) * (g.width() - 2) + j - 1;
+    //             g.rotate_at(Crd(i, j), keys[k % keys.len()]);
+    //         }
+    //     }
+    // }
+
+    // g.eprintln();
 }
