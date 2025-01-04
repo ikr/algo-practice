c56671a5d4f465efd43f214ddb2541ead1376f86
Ivan Krechetov <ikr@ikr.su>
2025-01-04 09:23:54 +0100

    Solve part 3

--- everybodycodes/2024/18_The_Ring/src/main.rs
+++ everybodycodes/2024/18_The_Ring/src/main.rs
@@ -8,11 +8,11 @@ const INF: usize = 1_000_000;
 #[derive(Clone, Copy, Debug)]
 struct Crd(usize, usize);
 
-fn source_crds(grid: &[Vec<char>]) -> Vec<Crd> {
+fn crds_of(grid: &[Vec<char>], c: char) -> Vec<Crd> {
     let mut result = vec![];
     for (i, row) in grid.iter().enumerate() {
-        for j in [0, row.len() - 1] {
-            if row[j] == '.' {
+        for (j, cell) in row.iter().enumerate() {
+            if *cell == c {
                 result.push(Crd(i, j));
             }
         }
@@ -20,16 +20,32 @@ fn source_crds(grid: &[Vec<char>]) -> Vec<Crd> {
     result
 }
 
-fn palm_crds(grid: &[Vec<char>]) -> Vec<Crd> {
-    let mut result = vec![];
-    for (i, row) in grid.iter().enumerate() {
-        for (j, cell) in row.iter().enumerate() {
-            if *cell == 'P' {
-                result.push(Crd(i, j));
+fn distances_for_given_source(grid: &[Vec<char>], source: Crd) -> Vec<Vec<usize>> {
+    let h = grid.len();
+    let w = grid[0].len();
+
+    let mut distance: Vec<Vec<usize>> = vec![vec![INF; w]; h];
+    distance[source.0][source.1] = 0;
+
+    let mut q: VecDeque<Crd> = VecDeque::new();
+    q.push_back(source);
+
+    while let Some(u) = q.pop_front() {
+        let Crd(ro, co) = u;
+        for Crd(i, j) in [
+            Crd(ro - 1, co),
+            Crd(ro, co + 1),
+            Crd(ro + 1, co),
+            Crd(ro, co - 1),
+        ] {
+            if distance[i][j] == INF && grid[i][j] != '#' {
+                distance[i][j] = distance[ro][co] + 1;
+                q.push_back(Crd(i, j));
             }
         }
     }
-    result
+
+    distance
 }
 
 fn main() {
@@ -39,36 +55,14 @@ fn main() {
         .map(|line| line.unwrap().chars().collect())
         .collect();
 
-    let h = grid.len();
-    let w = grid[0].len();
-
-    let mut distance: Vec<Vec<usize>> = vec![vec![INF; w]; h];
-    let mut q: VecDeque<Crd> = VecDeque::new();
-    for Crd(i, j) in source_crds(&grid) {
-        distance[i][j] = 0;
-        q.push_back(Crd(i, j));
-    }
-
-    while let Some(u) = q.pop_front() {
-        let Crd(ro, co) = u;
-        for Crd(i, j) in [
-            Crd(ro - 1, co),
-            Crd(ro, (co + 1).min(w - 1)),
-            Crd(ro + 1, co),
-            Crd(ro, co.saturating_sub(1)),
-        ] {
-            if distance[i][j] == INF && grid[i][j] != '#' {
-                distance[i][j] = distance[ro][co] + 1;
-                q.push_back(Crd(i, j));
-            }
-        }
-    }
-
-    let result: usize = palm_crds(&grid)
+    let ps = crds_of(&grid, 'P');
+    let result = crds_of(&grid, '.')
         .into_iter()
-        .map(|Crd(i, j)| distance[i][j])
-        .max()
+        .map(|source| -> usize {
+            let dist = distances_for_given_source(&grid, source);
+            ps.iter().map(|p| dist[p.0][p.1]).sum()
+        })
+        .min()
         .unwrap();
-
     println!("{}", result);
 }
