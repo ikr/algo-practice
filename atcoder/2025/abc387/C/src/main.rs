c49dbfb38bfc99c9faeb178b4e410bf4bfd75ebc
Ivan Krechetov <ikr@ikr.su>
2025-01-05 12:39:22 +0100

    Roll back

--- atcoder/2025/abc387/C/src/main.rs
+++ atcoder/2025/abc387/C/src/main.rs
@@ -11,45 +11,8 @@ fn digits(mut n: u64) -> Vec<u64> {
     ans
 }
 
-fn count_k_digit_snake_numbers_starting_with_d0(k: usize, d0: u64) -> u64 {
-    assert!(k > 1);
-    assert_ne!(d0, 0);
-    d0.pow(k as u32 - 1)
-}
-
-fn count_capped(ds: &[u64], d0: u64) -> u64 {
-    assert_ne!(d0, 0);
-    if ds.len() == 1 {
-        d0.min(ds[0] + 1)
-    } else {
-        let mut result = 0;
-        for _ in 1..ds[0].min(d0 - 1) {
-            result += count_capped(&ds[1..], d0);
-        }
-        result
-    }
-}
-
 fn snake_numers_up_to(hi: u64) -> u64 {
-    if hi < 10 {
-        0
-    } else {
-        let ds = digits(hi);
-        let mut result = 0;
-
-        for k in 2..ds.len() - 1 {
-            for d0 in 1..10 {
-                result += count_k_digit_snake_numbers_starting_with_d0(k, d0);
-            }
-        }
-
-        for d0 in 1..ds[0] {
-            result += count_k_digit_snake_numbers_starting_with_d0(ds.len(), d0);
-        }
-
-        result += count_capped(&ds[1..], ds[0]);
-        result
-    }
+    todo!()
 }
 
 fn snake_numbers_within(l: u64, r: u64) -> u64 {
@@ -69,14 +32,4 @@ fn main() {
     let result = snake_numbers_within(l, r);
     writeln!(writer, "{}", result).unwrap();
     writer.flush().unwrap();
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn count_capped_works() {
-        assert_eq!(count_capped(&[1, 0], 1), 1);
-    }
 }
