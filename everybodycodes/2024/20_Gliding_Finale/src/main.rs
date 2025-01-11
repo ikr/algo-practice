bae7c4e569fe2aa374ba813ba15fe802d070c47d
Ivan Krechetov <ikr@ikr.li>
2025-01-11 14:29:30 +0100

    WIP

--- everybodycodes/2024/20_Gliding_Finale/src/main.rs
+++ everybodycodes/2024/20_Gliding_Finale/src/main.rs
@@ -1,14 +1,10 @@
 use std::{
-    collections::{HashMap, HashSet},
+    collections::HashSet,
     io::{self, BufRead},
 };
 
-use memoize::memoize;
-
-const T_HORIZON: u16 = 200;
-
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
-struct Crd(i16, i16);
+struct Crd(i32, i32);
 
 impl std::ops::Add<Crd> for Crd {
     type Output = Crd;
@@ -28,7 +24,7 @@ impl Crd {
     }
 
     fn from_indices(i: usize, j: usize) -> Crd {
-        Crd(i as i16, j as i16)
+        Crd(i as i32, j as i32)
     }
 }
 
@@ -84,16 +80,16 @@ impl Dir {
 }
 
 #[derive(Debug)]
-struct World {
+struct WorldTile {
     blocks: HashSet<Crd>,
     ups: HashSet<Crd>,
     downs: HashSet<Crd>,
     start: Crd,
-    grid_height: usize,
-    grid_width: usize,
+    height: usize,
+    width: usize,
 }
 
-impl World {
+impl WorldTile {
     fn from(grid: &[Vec<char>]) -> Self {
         let mut blocks = HashSet::new();
         let mut ups = HashSet::new();
@@ -126,12 +122,12 @@ impl World {
             ups,
             downs,
             start,
-            grid_height: grid.len(),
-            grid_width: grid[0].len(),
+            height: grid.len(),
+            width: grid[0].len(),
         }
     }
 
-    fn alt_delta_at(&self, crd: Crd) -> i16 {
+    fn alt_delta_at(&self, crd: Crd) -> i32 {
         if self.ups.contains(&crd) {
             1
         } else if self.downs.contains(&crd) {
@@ -142,88 +138,54 @@ impl World {
     }
 
     fn in_bounds(&self, crd: Crd) -> bool {
-        let h = self.grid_height as i16;
-        let w = self.grid_width as i16;
+        let h = self.height as i32;
+        let w = self.width as i32;
         let Crd(r, c) = crd;
         0 <= r && r < h && 0 <= c && c < w
     }
-}
 
-fn group_by_t_and_alt(tris: Vec<(Dir, u16, i16)>) -> Vec<(Vec<Dir>, u16, i16)> {
-    let xs: HashMap<(u16, i16), Vec<Dir>> =
-        tris.into_iter()
-            .fold(HashMap::new(), |mut acc, (dir, t, alt)| {
-                acc.entry((t, alt))
-                    .and_modify(|dirs| dirs.push(dir))
-                    .or_insert(vec![dir]);
-                acc
-            });
+    fn max_flight_altitudes(&self, start_row: &[Vec<Option<i32>>]) -> Vec<Vec<Vec<Option<i32>>>> {
+        let dp0 = vec![vec![vec![None; 4]; self.width]; self.height + 1];
 
-    let mut result: Vec<(Vec<Dir>, u16, i16)> = xs
-        .into_iter()
-        .map(|((t, alt), dirs)| (dirs, t, alt))
-        .collect();
+        // dp(i j d) is the max possible altitude at the coordinate (i, j) when facing direction d
+        // (0 - North, 1 - East, 2 - South, 3 - West), at the current moment of time.
+        let mut dp: Vec<Vec<Vec<Option<i32>>>> = dp0.clone();
+        dp[0] = start_row.to_vec();
 
-    result.sort_by_key(|(_, t, alt)| (*t, -*alt));
-    result
-}
+        let t_horizon = self.width + self.height;
 
-#[memoize(Ignore: world)]
-fn arrival_possibilities(
-    world: &World,
-    src_crd: Crd,
-    src_dirs: Vec<Dir>,
-    dst_crd: Crd,
-) -> Vec<(Dir, u16, i16)> {
-    let dp0 = vec![vec![vec![None; 4]; world.grid_width]; world.grid_height];
+        for _ in 1..=t_horizon {
+            let mut dp_new = dp0.clone();
 
-    // dp(i j d) is the max possible altitude at the coordinate (i, j) when facing direction d
-    // (0 - North, 1 - East, 2 - South, 3 - West), at the current moment of time.
-    let mut dp: Vec<Vec<Vec<Option<i16>>>> = dp0.clone();
+            for (i, row) in dp.iter().enumerate() {
+                for (j, cell) in row.iter().enumerate() {
+                    for (dir_code, _) in cell.iter().enumerate() {
+                        let dir = Dir::from_code(dir_code);
+                        let v = Crd::from_indices(i, j);
 
-    for d in src_dirs {
-        dp[src_crd.ro()][src_crd.co()][d.code()] = Some(0);
-    }
-
-    let mut result = vec![];
-
-    for t in 1..=T_HORIZON {
-        let mut dp_new = dp0.clone();
-
-        for (i, row) in dp.iter().enumerate() {
-            for (j, cell) in row.iter().enumerate() {
-                for (dir_code, _) in cell.iter().enumerate() {
-                    let dir = Dir::from_code(dir_code);
-                    let v = Crd::from_indices(i, j);
-
-                    for (d, u) in Dir::all()
-                        .into_iter()
-                        .filter(|d| *d != dir.opposite())
-                        .map(|d| (d, v + d.delta()))
-                        .filter(|(_, p)| world.in_bounds(*p) && !world.blocks.contains(p))
-                    {
-                        if let Some(a) = dp[u.ro()][u.co()][d.code()] {
-                            dp_new[i][j][dir_code] = Some(
-                                dp_new[i][j][dir_code]
-                                    .unwrap_or(i16::MIN)
-                                    .max(a + world.alt_delta_at(v)),
-                            );
-                        }
-                    }
-
-                    if v == dst_crd {
-                        if let Some(a) = dp_new[i][j][dir_code] {
-                            result.push((dir, t, a));
+                        for (d, u) in Dir::all()
+                            .into_iter()
+                            .filter(|d| *d != dir.opposite())
+                            .map(|d| (d, v + d.delta()))
+                            .filter(|(_, p)| self.in_bounds(*p) && !self.blocks.contains(p))
+                        {
+                            if let Some(a) = dp[u.ro()][u.co()][d.code()] {
+                                dp_new[i][j][dir_code] = Some(
+                                    dp_new[i][j][dir_code]
+                                        .unwrap_or(i32::MIN)
+                                        .max(a + self.alt_delta_at(v)),
+                                );
+                            }
                         }
                     }
                 }
             }
+
+            dp = dp_new;
         }
 
-        dp = dp_new;
+        dp
     }
-
-    result
 }
 
 fn main() {
@@ -236,9 +198,12 @@ fn main() {
         })
         .collect();
 
-    let world = World::from(&grid);
-    eprintln!(
-        "{}x{} grid\n{:?}",
-        world.grid_height, world.grid_width, grid
-    );
+    let world_tile = WorldTile::from(&grid);
+    let initial_altitude: i32 = 384400;
+    let mut initial_start_row: Vec<Vec<Option<i32>>> =
+        vec![vec![None; Dir::all().len()]; world_tile.width];
+    initial_start_row[world_tile.start.co()] = vec![Some(initial_altitude); Dir::all().len()];
+
+    let dp = world_tile.max_flight_altitudes(&initial_start_row);
+    eprintln!("{:?}", dp);
 }
