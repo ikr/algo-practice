e79651392556e153872eda0b071d54e3ec170588
Ivan Krechetov <ikr@ikr.su>
2025-03-12 16:49:58 +0100

    AC

--- i18npuzzles/2025/06_Mojibake_puzzle_dictionary/src/main.rs
+++ i18npuzzles/2025/06_Mojibake_puzzle_dictionary/src/main.rs
@@ -18,8 +18,20 @@ fn squash_all_non_ascii_subsequences(subsequence_length: usize, mut xs: Vec<char
     xs
 }
 
+fn matches(xs: &str, pattern: &str) -> bool {
+    xs.chars().count() == pattern.chars().count()
+        && xs
+            .chars()
+            .zip(pattern.chars())
+            .all(|(x, y)| y == '.' || x == y)
+}
+
+fn matches_any(xs: &str, patterns: &[String]) -> bool {
+    patterns.iter().any(|p| matches(xs, p))
+}
+
 fn main() {
-    let lines: Vec<String> = std::fs::read_to_string("inA")
+    let lines: Vec<String> = std::fs::read_to_string("in1")
         .unwrap()
         .lines()
         .map(|line| line.to_string())
@@ -27,8 +39,41 @@ fn main() {
 
     let isep: usize = lines.iter().position(|s| s.is_empty()).unwrap();
 
-    eprintln!("{:?}", lines);
-    eprintln!("isep:{}", isep);
+    let words: Vec<(usize, String)> = lines[..isep]
+        .iter()
+        .enumerate()
+        .map(|(i, s)| {
+            let pos = i + 1;
+            let mb_m: Option<usize> = if pos % 5 == 0 && pos % 3 == 0 {
+                Some(4)
+            } else if pos % 5 == 0 || pos % 3 == 0 {
+                Some(2)
+            } else {
+                None
+            };
+
+            let cs = s.chars().collect();
+            let xs: Vec<char> = if let Some(m) = mb_m {
+                squash_all_non_ascii_subsequences(m, cs)
+            } else {
+                cs
+            };
+            (pos, xs.into_iter().collect())
+        })
+        .collect();
+
+    let patterns: Vec<String> = lines[isep + 1..]
+        .iter()
+        .map(|s| s.trim().to_string())
+        .collect();
+
+    let result: usize = words
+        .into_iter()
+        .filter(|(_, s)| matches_any(s, &patterns))
+        .map(|(pos, _)| pos)
+        .sum();
+
+    println!("{}", result);
 }
 
 #[cfg(test)]
@@ -91,5 +136,19 @@ mod tests {
             squash_all_non_ascii_subsequences(2, "abc".chars().collect::<Vec<_>>()),
             "abc".chars().collect::<Vec<_>>()
         );
+    }
+
+    #[test]
+    fn matches_works() {
+        for (xs, pat, res) in [
+            ("", "", true),
+            ("abc", "...", true),
+            ("oops", ".o..", true),
+            ("oops", ".....", false),
+            ("oops", ".z..", false),
+            ("träffs", "....f.", true),
+        ] {
+            assert_eq!(matches(xs, pat), res);
+        }
     }
 }
