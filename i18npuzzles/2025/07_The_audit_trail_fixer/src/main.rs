1c36a2fd44c45a701d27136c1ff684120b95b920
Ivan Krechetov <ikr@ikr.li>
2025-03-14 07:58:45 +0100

    Bootstrap

---
+++ i18npuzzles/2025/07_The_audit_trail_fixer/src/main.rs
@@ -0,0 +1,11 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    let lines: Vec<String> = io::stdin()
+        .lock()
+        .lines()
+        .map(|line| line.unwrap())
+        .collect();
+
+    eprintln!("{:?}", lines);
+}
