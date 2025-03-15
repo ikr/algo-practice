50bbaf337ca63ceb86b935b8041fb6bd234015c4
Ivan Krechetov <ikr@ikr.su>
2025-03-15 17:30:03 +0100

    Still WA x 9

--- atcoder/2025/abc397/D/src/main.rs
+++ atcoder/2025/abc397/D/src/main.rs
@@ -3,26 +3,52 @@ use std::io::{self, BufWriter, Write};
 
 const MIO: i128 = 1_000_000;
 
-fn isqrt(x: i128) -> Option<i128> {
-    let fx = x as f64;
-    let q = fx.sqrt().round();
-    let i = q as i128;
-    if i * i == x { Some(i) } else { None }
+fn foo(d: i128, y: i128, n: i128) -> i128 {
+    3 * d * y.pow(2) + 3 * d * d * y + d.pow(3) - n
+}
+
+fn sign(x: i128) -> i8 {
+    if x == 0 {
+        0
+    } else if x < 0 {
+        -1
+    } else {
+        1
+    }
 }
 
 fn find_y(n: i128, d: i128) -> Option<i128> {
-    let discr: i128 = 9 * d.pow(4) - 12 * d * (d.pow(3) - n);
-    if discr < 0 {
+    let left_s = sign(foo(d, 1, n));
+    if left_s == 0 {
+        return Some(1);
+    }
+
+    let right_s = sign(foo(d, MIO, n));
+    if right_s == 0 {
+        return Some(MIO);
+    }
+
+    if left_s == right_s {
         return None;
     }
 
-    let dd = isqrt(discr)?;
-    let p = -3 * d * d + dd;
-    if p <= 0 || p % (6 * d) != 0 {
-        return None;
+    let mut l = 1;
+    let mut r = MIO;
+    while l + 1 < r {
+        let mid = l + (r - l) / 2;
+        let mid_s = sign(foo(d, mid, n));
+        if mid_s == 0 {
+            return Some(mid);
+        }
+
+        if mid_s == left_s {
+            l = mid;
+        } else {
+            r = mid;
+        }
     }
 
-    Some(p / 6 * d)
+    None
 }
 
 fn solve(n: i128) -> Option<(i128, i128)> {
@@ -50,4 +76,19 @@ fn main() {
         writeln!(writer, "-1").unwrap();
     }
     writer.flush().unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn isqrt_works() {
+        assert_eq!(isqrt(10i128.pow(22)), Some(10i128.pow(11)));
+        assert_eq!(isqrt(234245348979898i128.pow(2)), Some(234245348979898i128));
+        assert_eq!(
+            isqrt(234245348979898111i128.pow(2)),
+            Some(234245348979898111i128)
+        );
+    }
 }
