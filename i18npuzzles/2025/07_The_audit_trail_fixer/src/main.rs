3ec1dac56ea90ed241437d883dceb6265d7b11ff
Ivan Krechetov <ikr@ikr.su>
2025-03-15 11:20:15 +0100

    Parse the source date-time

--- i18npuzzles/2025/07_The_audit_trail_fixer/src/main.rs
+++ i18npuzzles/2025/07_The_audit_trail_fixer/src/main.rs
@@ -1,5 +1,32 @@
 use std::io::{self, BufRead};
 
+use chrono::NaiveDateTime;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct SourceDateTime {
+    naive_date_time: NaiveDateTime,
+    gmt_offset_hours: i64,
+}
+
+impl SourceDateTime {
+    fn parse(s: &str) -> Self {
+        let n = s.len();
+        let suff_len = "-04:00".len();
+
+        let naive_date_time: NaiveDateTime = s[..n - suff_len].parse().unwrap();
+        let gmt_offset_hours = -s[n - suff_len + 2..]
+            .chars()
+            .next()
+            .map(|c| c.to_digit(10).unwrap() as i64)
+            .unwrap();
+
+        Self {
+            naive_date_time,
+            gmt_offset_hours,
+        }
+    }
+}
+
 fn main() {
     let lines: Vec<String> = io::stdin()
         .lock()
@@ -8,4 +35,25 @@ fn main() {
         .collect();
 
     eprintln!("{:?}", lines);
+}
+
+#[cfg(test)]
+mod tests {
+    use chrono::{NaiveDate, NaiveTime};
+
+    use super::*;
+
+    #[test]
+    fn source_date_time_parse_works() {
+        assert_eq!(
+            SourceDateTime::parse("2012-11-05T09:39:00.000-04:00"),
+            SourceDateTime {
+                naive_date_time: NaiveDateTime::new(
+                    NaiveDate::from_ymd_opt(2012, 11, 05).unwrap(),
+                    NaiveTime::from_hms_opt(9, 39, 0).unwrap()
+                ),
+                gmt_offset_hours: -4,
+            }
+        )
+    }
 }
