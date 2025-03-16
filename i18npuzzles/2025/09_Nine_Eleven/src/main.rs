4588183aaf3f978bd31f1233c3b9100be0f08393
Ivan Krechetov <ikr@ikr.su>
2025-03-16 10:19:57 +0100

    Go on

--- i18npuzzles/2025/09_Nine_Eleven/src/main.rs
+++ i18npuzzles/2025/09_Nine_Eleven/src/main.rs
@@ -1,4 +1,9 @@
-use std::io::{self, BufRead};
+use std::{
+    collections::BTreeMap,
+    io::{self, BufRead},
+};
+
+use chrono::NaiveDate;
 
 fn parse_date_source(s: &str) -> [i32; 3] {
     let parts: Vec<i32> = s.split('-').map(|x| x.parse().unwrap()).collect();
@@ -14,6 +19,18 @@ fn parse_line(s: &str) -> ([i32; 3], Vec<String>) {
     (parse_date_source(parts[0]), parse_names_list(parts[1]))
 }
 
+fn ifer_ymd_indices(tris: &[[i32; 3]]) -> [usize; 3] {
+    for (y, m, d) in [(0, 1, 2), (2, 0, 1), (2, 1, 0)] {
+        if tris
+            .iter()
+            .all(|tri| NaiveDate::from_ymd_opt(tri[y], tri[m] as u32, tri[d] as u32).is_some())
+        {
+            return [y, m, d];
+        }
+    }
+    panic!("Can't deduce the date parts in {:?}", tris);
+}
+
 fn main() {
     let lines: Vec<_> = io::stdin()
         .lock()
@@ -21,5 +38,22 @@ fn main() {
         .map(|line| parse_line(&line.unwrap()))
         .collect();
 
-    eprintln!("{:?}", lines);
+    let date_sources_by_name: BTreeMap<String, Vec<[i32; 3]>> =
+        lines.iter().fold(BTreeMap::new(), |mut acc, (tri, names)| {
+            for name in names {
+                acc.entry(name.clone())
+                    .and_modify(|tris| tris.push(*tri))
+                    .or_insert(vec![*tri]);
+            }
+            acc
+        });
+
+    eprintln!("{:?}", date_sources_by_name);
+
+    let ymd_indices_by_name: BTreeMap<String, [usize; 3]> = date_sources_by_name
+        .iter()
+        .map(|(name, tris)| (name.clone(), ifer_ymd_indices(tris)))
+        .collect();
+
+    eprintln!("{:?}", ymd_indices_by_name);
 }
