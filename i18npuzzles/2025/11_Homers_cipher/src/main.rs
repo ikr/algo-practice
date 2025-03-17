ce8cb3f3446a89afde60ccf2ca456386b12c5784
Ivan Krechetov <ikr@ikr.li>
2025-03-17 20:11:00 +0100

    Go on

--- i18npuzzles/2025/11_Homers_cipher/src/main.rs
+++ i18npuzzles/2025/11_Homers_cipher/src/main.rs
@@ -1,16 +1,26 @@
 use std::io::{self, BufRead};
 
+const NEEDLES: [&str; 5] = ["Οδυσσευσ", "Οδυσσεωσ", "Οδυσσει", "Οδυσσεα", "Οδυσσευ"];
+const AL_HI: &str = "ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ";
+const AL_LO: &str = "αβγδεζηθικλμνξοπρστυφχψω";
+
 fn remove_punctuation(s: String) -> String {
     s.chars()
         .filter(|c| c.is_alphabetic() || *c == ' ')
         .collect()
 }
 
+fn normalize_sigma(s: String) -> String {
+    s.chars().map(|c| if c == 'ς' { 'σ' } else { c }).collect()
+}
+
 fn main() {
     let lines: Vec<String> = io::stdin()
         .lock()
         .lines()
-        .map(|line| remove_punctuation(line.expect("Next stdin line available")))
+        .map(|line| line.expect("Next stdin line available"))
+        .map(remove_punctuation)
+        .map(normalize_sigma)
         .collect();
 
     eprintln!("{:?}", lines)
