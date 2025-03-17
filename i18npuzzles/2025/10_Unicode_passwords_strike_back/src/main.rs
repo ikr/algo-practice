a2f1f0822a78a85b188ce42de30de9beb1fd8b09
Ivan Krechetov <ikr@ikr.li>
2025-03-17 18:35:17 +0100

    AC; only took 6 minutes :)

--- i18npuzzles/2025/10_Unicode_passwords_strike_back/src/main.rs
+++ i18npuzzles/2025/10_Unicode_passwords_strike_back/src/main.rs
@@ -1,13 +1,50 @@
 use bcrypt::verify;
+use itertools::Itertools;
 use std::{
     collections::HashMap,
     io::{self, BufRead},
 };
-use unicode_normalization::UnicodeNormalization;
+use unicode_normalization::{UnicodeNormalization, is_nfc};
 
 fn parse_pair(s: &String) -> (String, String) {
     let parts: Vec<_> = s.split_whitespace().collect();
     (parts[0].to_string(), parts[1].to_string())
+}
+
+fn is_decomposable(c: char) -> bool {
+    c.nfd().count() != 1
+}
+
+fn decomposable_indices(cs: &[char]) -> Vec<usize> {
+    cs.iter()
+        .enumerate()
+        .filter(|(_, c)| is_decomposable(**c))
+        .map(|(i, _)| i)
+        .collect()
+}
+
+fn any_variation_hash_matches(password: &str, target_hash: &str) -> bool {
+    assert!(is_nfc(&password));
+    let cs: Vec<char> = password.chars().collect();
+    let all_ii = decomposable_indices(&cs);
+
+    for ii in all_ii.into_iter().powerset() {
+        let mut cur: Vec<char> = vec![];
+
+        for (j, c) in cs.iter().enumerate() {
+            if ii.contains(&j) {
+                cur.extend(c.nfd());
+            } else {
+                cur.push(*c);
+            }
+        }
+
+        if verify(&cur.into_iter().collect::<String>(), target_hash).expect("Valid bcrypt hash") {
+            return true;
+        }
+    }
+
+    false
 }
 
 fn main() {
@@ -27,6 +64,19 @@ fn main() {
         .map(parse_pair)
         .map(|(u, p)| (u, p.nfc().to_string()))
         .collect();
+
+    let result = login_attempts
+        .into_iter()
+        .filter(|(username, password)| {
+            if let Some(h) = hashes_by_username.get(username) {
+                any_variation_hash_matches(&password, h)
+            } else {
+                false
+            }
+        })
+        .count();
+
+    println!("{}", result);
 }
 
 #[cfg(test)]
@@ -44,5 +94,15 @@ mod tests {
             .unwrap(),
             true
         );
+    }
+
+    #[test]
+    fn is_decomposable_yes() {
+        assert!(is_decomposable('È'));
+    }
+
+    #[test]
+    fn is_decomposable_no() {
+        assert!(!is_decomposable('E'));
     }
 }
