fbd5b73e596b98bafd306e201cb02ece53284b95
Ivan Krechetov <ikr@ikr.li>
2025-03-18 19:35:37 +0100

    wip

--- i18npuzzles/2025/12_Sorting_it_out/src/main.rs
+++ i18npuzzles/2025/12_Sorting_it_out/src/main.rs
@@ -2,12 +2,30 @@ use std::io::{self, BufRead};
 
 use regex::Regex;
 
-fn parse_line(s: &str) -> (String, String, u64) {
+#[derive(Clone, Debug)]
+struct Name {
+    first: String,
+    last: String,
+}
+
+impl Name {
+    fn english_sort_key(&self) -> (String, String) {
+        let ab: Vec<String> = [&self.last, &self.first]
+            .into_iter()
+            .map(|src| todo!())
+            .collect();
+        (ab[0], ab[1])
+    }
+}
+
+fn parse_line(s: &str) -> (Name, u64) {
     let re = Regex::new(r"^(.+), (.+): (\d+)$").unwrap();
     let caps = re.captures(s).unwrap();
     (
-        caps[1].to_string(),
-        caps[2].to_string(),
+        Name {
+            first: caps[2].to_string(),
+            last: caps[1].to_string(),
+        },
         caps[3].parse().unwrap(),
     )
 }
