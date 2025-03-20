25eda81fa3f22ca2ee862fbac786df6deee0645c
Ivan Krechetov <ikr@ikr.li>
2025-03-20 10:49:54 +0100

    Solve part 3

--- codyssi/2025/02_Absurd_Arithmetic/src/main.rs
+++ codyssi/2025/02_Absurd_Arithmetic/src/main.rs
@@ -1,5 +1,7 @@
 use std::io::BufRead;
 
+const HI: i64 = 15000000000000;
+
 fn decode_arg_in_line(s: &str) -> i64 {
     s.split_ascii_whitespace().last().unwrap().parse().unwrap()
 }
@@ -18,8 +20,13 @@ fn main() {
         .try_into()
         .unwrap();
 
+    let price_for_quality = |quality: i64| -> i64 { quality.pow(c as u32) * b + a };
+
     let xs: Vec<i64> = lines[4..].iter().map(|s| s.parse().unwrap()).collect();
-    let x0: i64 = xs.into_iter().filter(|x| x % 2 == 0).sum();
-    let result = x0.pow(c as u32) * b + a;
-    println!("{}", result);
+    let x0: i64 = xs
+        .into_iter()
+        .filter(|&x| price_for_quality(x) <= HI)
+        .max()
+        .unwrap();
+    println!("{}", x0);
 }
