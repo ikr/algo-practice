9fada63daf651219dffa33f5c8c8042a5d8d5c00
Ivan Krechetov <ikr@ikr.su>
2025-03-20 16:22:38 +0100

    Solve part 2

--- codyssi/2025/03_Supplies_in_Surplus/src/main.rs
+++ codyssi/2025/03_Supplies_in_Surplus/src/main.rs
@@ -12,6 +12,26 @@ fn parse_line(s: &str) -> (i32, i32, i32, i32) {
     (a, b, c, d)
 }
 
+fn intersect(ab: (i32, i32), cd: (i32, i32)) -> bool {
+    let (a, b) = ab;
+    let (c, d) = cd;
+    !(b < c || d < a)
+}
+
+fn union_count(ab: (i32, i32), cd: (i32, i32)) -> i32 {
+    let (a, b) = ab;
+    let (c, d) = cd;
+
+    if intersect(ab, cd) {
+        let mut xs = [a, b, c, d];
+        xs.sort();
+        let [l, _, _, r] = xs;
+        r - l + 1
+    } else {
+        b - a + 1 + d - c + 1
+    }
+}
+
 fn main() {
     let lines: Vec<_> = io::stdin()
         .lock()
@@ -21,7 +41,7 @@ fn main() {
 
     let result: i32 = lines
         .into_iter()
-        .map(|(a, b, c, d)| b - a + 1 + d - c + 1)
+        .map(|(a, b, c, d)| union_count((a, b), (c, d)))
         .sum();
     println!("{}", result);
 }
