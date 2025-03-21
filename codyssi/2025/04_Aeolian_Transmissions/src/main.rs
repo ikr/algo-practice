ef1d75ac6db12be8c07f8191551177bd243a33c0
Ivan Krechetov <ikr@ikr.su>
2025-03-21 15:46:37 +0100

    Solve part 2

--- codyssi/2025/04_Aeolian_Transmissions/src/main.rs
+++ codyssi/2025/04_Aeolian_Transmissions/src/main.rs
@@ -1,15 +1,27 @@
 use std::io::{self, BufRead};
 
 fn memory_units(c: char) -> usize {
-    ((c as u8) - b'A' + 1) as usize
+    if c.is_ascii_digit() {
+        ((c as u8) - b'0') as usize
+    } else {
+        ((c as u8) - b'A' + 1) as usize
+    }
 }
 
 fn total_memory_units(s: &str) -> usize {
     s.chars().map(memory_units).sum()
 }
 
+fn compress(s: &str) -> String {
+    let n = s.len();
+    let m = n / 10;
+    let infix = (n - 2 * m).to_string();
+    s[..m].to_string() + &infix + &s[n - m..]
+}
+
 fn main() {
     let lines: Vec<String> = io::stdin().lock().lines().map(|s| s.unwrap()).collect();
-    let result: usize = lines.into_iter().map(|s| total_memory_units(&s)).sum();
+    let compressed: Vec<String> = lines.into_iter().map(|s| compress(&s)).collect();
+    let result: usize = compressed.into_iter().map(|s| total_memory_units(&s)).sum();
     println!("{}", result);
 }
