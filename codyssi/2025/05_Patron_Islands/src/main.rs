ffefb4e9a9d937e98d54bda09b392a1a2e3db145
Ivan Krechetov <ikr@ikr.li>
2025-03-21 17:54:51 +0100

    Bootstrap

---
+++ codyssi/2025/05_Patron_Islands/src/main.rs
@@ -0,0 +1,9 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    let grid: Vec<Vec<char>> = io::stdin()
+        .lock()
+        .lines()
+        .map(|line| line.unwrap().chars().collect())
+        .collect();
+}
