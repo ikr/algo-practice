44d763c740e2ded72fbb8522320d1025ff97b5c0
Ivan Krechetov <ikr@ikr.su>
2025-03-21 18:00:50 +0100

    Solve part 1

--- codyssi/2025/05_Patron_Islands/src/main.rs
+++ codyssi/2025/05_Patron_Islands/src/main.rs
@@ -1,9 +1,24 @@
 use std::io::{self, BufRead};
 
+fn parse_line(s: &str) -> (i32, i32) {
+    let parts: Vec<_> = s
+        .strip_prefix('(')
+        .and_then(|t| t.strip_suffix(')'))
+        .unwrap()
+        .split(", ")
+        .collect();
+
+    (parts[0].parse().unwrap(), parts[1].parse().unwrap())
+}
+
 fn main() {
-    let grid: Vec<Vec<char>> = io::stdin()
+    let xys: Vec<(i32, i32)> = io::stdin()
         .lock()
         .lines()
-        .map(|line| line.unwrap().chars().collect())
+        .map(|line| parse_line(&line.unwrap()))
         .collect();
+
+    let ds: Vec<i32> = xys.into_iter().map(|(x, y)| x.abs() + y.abs()).collect();
+    let result = ds.iter().max().unwrap() - ds.iter().min().unwrap();
+    println!("{}", result);
 }
