ece849efa443e4e6ca3a5b89295c4581e51dde9c
Ivan Krechetov <ikr@ikr.su>
2025-03-22 11:27:58 +0100

    Solve part 3

--- codyssi/2025/06_Lotus_Scramble/src/main.rs
+++ codyssi/2025/06_Lotus_Scramble/src/main.rs
@@ -1,21 +1,40 @@
 use std::io::Read;
 
+fn value_of(c: char) -> i32 {
+    (if c.is_ascii_lowercase() {
+        ((c as u8) - b'a') + 1
+    } else {
+        assert!(c.is_ascii_uppercase());
+        ((c as u8) - b'A') + 27
+    }) as i32
+}
+
+fn normalize(mut x: i32) -> i32 {
+    while x < 1 {
+        x += 52;
+    }
+    while x > 52 {
+        x -= 52;
+    }
+    x
+}
+
 fn main() {
     let mut input = String::new();
     std::io::stdin().read_to_string(&mut input).unwrap();
     input = input.trim().to_string();
 
-    let result: usize = input
-        .chars()
-        .filter(|c| c.is_alphabetic())
-        .map(|c| {
-            (if c.is_ascii_lowercase() {
-                ((c as u8) - b'a') + 1
-            } else {
-                assert!(c.is_ascii_uppercase());
-                ((c as u8) - b'A') + 27
-            }) as usize
-        })
-        .sum();
-    println!("{}", result);
+    let cs: Vec<char> = input.chars().collect();
+    let mut vs: Vec<i32> = vec![0; cs.len()];
+    vs[0] = value_of(cs[0]);
+
+    for i in 1..cs.len() {
+        if cs[i].is_alphabetic() {
+            vs[i] = value_of(cs[i]);
+        } else {
+            vs[i] = normalize(vs[i - 1] * 2 - 5);
+        }
+    }
+
+    println!("{}", vs.iter().sum::<i32>());
 }
