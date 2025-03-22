482cebbb10c1182b9b0c7e816d6be87fb662634b
Ivan Krechetov <ikr@ikr.li>
2025-03-22 11:12:17 +0100

    Solve part 2

--- codyssi/2025/06_Lotus_Scramble/src/main.rs
+++ codyssi/2025/06_Lotus_Scramble/src/main.rs
@@ -5,6 +5,17 @@ fn main() {
     std::io::stdin().read_to_string(&mut input).unwrap();
     input = input.trim().to_string();
 
-    let result = input.chars().filter(|c| c.is_alphabetic()).count();
+    let result: usize = input
+        .chars()
+        .filter(|c| c.is_alphabetic())
+        .map(|c| {
+            (if c.is_ascii_lowercase() {
+                ((c as u8) - b'a') + 1
+            } else {
+                assert!(c.is_ascii_uppercase());
+                ((c as u8) - b'A') + 27
+            }) as usize
+        })
+        .sum();
     println!("{}", result);
 }
