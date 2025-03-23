9de2462e1e2e7e75708ebb50690c713028f0ef9a
Ivan Krechetov <ikr@ikr.su>
2025-03-23 09:52:31 +0100

    WIP

--- i18npuzzles/2025/12_Sorting_it_out/src/main.rs
+++ i18npuzzles/2025/12_Sorting_it_out/src/main.rs
@@ -1,6 +1,7 @@
 use std::io::{self, BufRead};
 
 use regex::Regex;
+use unicode_normalization::UnicodeNormalization;
 
 #[derive(Clone, Debug)]
 struct Name {
@@ -12,9 +13,61 @@ impl Name {
     fn english_sort_key(&self) -> (String, String) {
         let ab: Vec<String> = [&self.last, &self.first]
             .into_iter()
-            .map(|src| todo!())
+            .map(|src| {
+                src.to_lowercase()
+                    .replace("ı", "i")
+                    .replace("æ", "ae")
+                    .replace("'", "")
+                    .replace("-", "")
+                    .chars()
+                    .map(|c| c.nfd().next().unwrap())
+                    .collect()
+            })
             .collect();
-        (ab[0], ab[1])
+        (ab[0].clone(), ab[1].clone())
+    }
+
+    fn swedish_alphabet() -> Vec<char> {
+        let mut result: Vec<char> = ('a'..='z').collect();
+        result.extend(['å', 'ä', 'ö', ' ']);
+        result
+    }
+
+    fn swedish_sort_key(&self) -> (Vec<usize>, Vec<usize>) {
+        let az = Name::swedish_alphabet();
+        let index_of = |c: char| -> usize {
+            match az.iter().position(|&x| x == c) {
+                Some(i) => i,
+                None => panic!("{} not in the alphabet", c),
+            }
+        };
+        let indicies = |s: &str| -> Vec<usize> { s.chars().map(index_of).collect() };
+
+        let ab: Vec<_> = [&self.last, &self.first]
+            .into_iter()
+            .map(|src| {
+                let s: String = src
+                    .to_lowercase()
+                    .replace("æ", "ä")
+                    .replace("ı", "i")
+                    .replace("ı", "i")
+                    .replace("ø", "ö")
+                    .replace("'", "")
+                    .replace("-", "")
+                    .chars()
+                    .map(|c| {
+                        if az.contains(&c) {
+                            c
+                        } else {
+                            c.nfd().next().unwrap()
+                        }
+                    })
+                    .collect();
+
+                indicies(&s)
+            })
+            .collect();
+        (ab[0].clone(), ab[1].clone())
     }
 }
 
@@ -30,12 +83,24 @@ fn parse_line(s: &str) -> (Name, u64) {
     )
 }
 
+fn median_number(lines: &[(Name, u64)]) -> u64 {
+    let n = lines.len();
+    lines[n / 2].1
+}
+
 fn main() {
-    let lines: Vec<_> = io::stdin()
+    let mut lines: Vec<_> = io::stdin()
         .lock()
         .lines()
         .map(|line| parse_line(&line.unwrap()))
         .collect();
-
     eprintln!("{:?}", lines);
+
+    lines.sort_by_key(|(n, _)| n.english_sort_key());
+    eprintln!("{:?}", lines);
+    eprintln!("{}", median_number(&lines));
+
+    lines.sort_by_key(|(n, _)| n.swedish_sort_key());
+    eprintln!("{:?}", lines);
+    eprintln!("{}", median_number(&lines));
 }
