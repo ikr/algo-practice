d65cf209a1e97632b78907b9b11a6f320adb260f
Ivan Krechetov <ikr@ikr.su>
2025-03-24 11:24:13 +0100

    Solve part 2

--- codyssi/2025/08_Risky_Shortcut/src/main.rs
+++ codyssi/2025/08_Risky_Shortcut/src/main.rs
@@ -1,11 +1,32 @@
 use std::io::{self, BufRead};
 
-fn alpha_count(s: &str) -> usize {
-    s.chars().filter(|c| c.is_alphabetic()).count()
+fn is_alpha_or_dash(c: char) -> bool {
+    c == '-' || c.is_alphabetic()
+}
+
+fn first_reduction_index(s: &str) -> Option<usize> {
+    let ps: Vec<(char, char)> = s
+        .chars()
+        .collect::<Vec<_>>()
+        .windows(2)
+        .map(|ab| (ab[0], ab[1]))
+        .collect();
+
+    ps.into_iter().position(|(a, b)| {
+        (is_alpha_or_dash(a) && b.is_ascii_digit()) || (a.is_ascii_digit() && is_alpha_or_dash(b))
+    })
+}
+
+fn reduce_all(s: &str) -> String {
+    let mut result = s.to_string();
+    while let Some(i) = first_reduction_index(&result) {
+        result = result[..i].to_string() + &result[i + 2..];
+    }
+    result
 }
 
 fn main() {
     let lines: Vec<String> = io::stdin().lock().lines().map(|s| s.unwrap()).collect();
-    let result: usize = lines.into_iter().map(|s| alpha_count(&s)).sum();
+    let result: usize = lines.into_iter().map(|s| reduce_all(&s).len()).sum();
     println!("{}", result);
 }
