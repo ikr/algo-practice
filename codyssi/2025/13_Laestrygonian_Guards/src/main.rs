08b0886d7598c702409403949b48d22514e9f9f8
Ivan Krechetov <ikr@ikr.li>
2025-03-29 16:54:24 +0100

    WA

--- codyssi/2025/13_Laestrygonian_Guards/src/main.rs
+++ codyssi/2025/13_Laestrygonian_Guards/src/main.rs
@@ -1,16 +1,37 @@
 use std::{
-    collections::{BTreeSet, HashMap},
+    collections::HashMap,
     io::{BufRead, stdin},
 };
-
-use itertools::Itertools;
-
-const INF: u32 = 1_000_000_000;
 
 fn parse_line(s: &str) -> (String, String, u32) {
     let ab: Vec<_> = s.split(" | ").collect();
     let cd: Vec<_> = ab[0].split(" -> ").collect();
     (cd[0].to_string(), cd[1].to_string(), ab[1].parse().unwrap())
+}
+
+fn cycle_length(g: &[Vec<(usize, u32)>], path_so_far: &[usize], length_so_far: u32) -> Option<u32> {
+    let u0 = *path_so_far.first().expect("Cycle start must be present");
+    let u = *path_so_far.last().unwrap();
+    let mut p = path_so_far.to_vec();
+
+    for &(v, w) in g[u].iter() {
+        if v == u0 {
+            return Some(length_so_far + w);
+        }
+
+        if path_so_far.contains(&v) {
+            return None;
+        }
+
+        p.push(v);
+        let sub = cycle_length(g, &p, length_so_far + w);
+        if sub.is_some() {
+            return sub;
+        }
+        p.pop();
+    }
+
+    None
 }
 
 fn main() {
@@ -31,10 +52,15 @@ fn main() {
             });
 
     let n = idx.len();
-    let g: Vec<Vec<usize>> = lines
-        .into_iter()
-        .fold(vec![vec![]; n], |mut acc, (u, v, _)| {
-            acc[idx[&u]].push(idx[&v]);
-            acc
-        });
+    let g: Vec<Vec<(usize, u32)>> =
+        lines
+            .into_iter()
+            .fold(vec![vec![]; n], |mut acc, (u, v, w)| {
+                acc[idx[&u]].push((idx[&v], w));
+                acc
+            });
+
+    let ls: Vec<_> = (0..n).filter_map(|u| cycle_length(&g, &[u], 0)).collect();
+    let result = ls.into_iter().max().unwrap();
+    println!("{}", result);
 }
