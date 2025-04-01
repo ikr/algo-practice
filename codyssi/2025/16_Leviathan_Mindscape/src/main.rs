ae23f464110c3037ee49b2c775ccfd91f0ab219a
Ivan Krechetov <ikr@ikr.li>
2025-04-01 20:15:56 +0200

    Decode the input

--- codyssi/2025/16_Leviathan_Mindscape/src/main.rs
+++ codyssi/2025/16_Leviathan_Mindscape/src/main.rs
@@ -8,7 +8,7 @@ enum Subj {
 }
 
 impl Subj {
-    fn parse(s: &str) -> Self {
+    fn decode(s: &str) -> Self {
         if s == "FACE" {
             Self::Face
         } else {
@@ -23,8 +23,48 @@ impl Subj {
     }
 }
 
+#[derive(Clone, Copy, Debug)]
+struct Mutation {
+    subj: Subj,
+    to_add: u8,
+}
+
+impl Mutation {
+    fn decode(s: &str) -> Self {
+        let ab: Vec<_> = s.split(" - VALUE ").collect();
+        let subj = Subj::decode(ab[0]);
+        let to_add: u8 = ab[1].parse().unwrap();
+        Self { subj, to_add }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+enum Rotation {
+    U,
+    R,
+    D,
+    L,
+}
+
+impl Rotation {
+    fn decode(c: char) -> Self {
+        match c {
+            'U' => Self::U,
+            'R' => Self::R,
+            'D' => Self::D,
+            'L' => Self::L,
+            _ => panic!("Invalid rotation source `{}`", c),
+        }
+    }
+}
+
 fn main() {
     let lines: Vec<String> = stdin().lock().lines().map(|line| line.unwrap()).collect();
     let isep = lines.iter().position(|s| s.is_empty()).unwrap();
-    eprintln!("{:?}", lines);
+
+    let mutations: Vec<Mutation> = lines[..isep].iter().map(|s| Mutation::decode(s)).collect();
+    eprintln!("{:?}", mutations);
+
+    let roations: Vec<Rotation> = lines[isep + 1].chars().map(Rotation::decode).collect();
+    eprintln!("{:?}", roations);
 }
