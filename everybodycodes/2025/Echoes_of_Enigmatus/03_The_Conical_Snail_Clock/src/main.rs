f5772d17d1863f06f93fe8e88dd03940e9894463
Ivan Krechetov <ikr@ikr.li>
2025-06-14 13:04:17 +0200

    Solve part 2

--- everybodycodes/2025/Echoes_of_Enigmatus/03_The_Conical_Snail_Clock/src/main.rs
+++ everybodycodes/2025/Echoes_of_Enigmatus/03_The_Conical_Snail_Clock/src/main.rs
@@ -1,10 +1,11 @@
+use ac_library::math;
 use std::io::{BufRead, stdin};
 
 #[derive(Clone, Copy, Debug)]
-struct Coord(u16, u16);
+struct Coord(i64, i64);
 
 impl Coord {
-    fn parse_component(s: &str) -> u16 {
+    fn parse_component(s: &str) -> i64 {
         let parts = s.split('=').collect::<Vec<_>>();
         parts[1].parse().unwrap()
     }
@@ -17,31 +18,12 @@ impl Coord {
         )
     }
 
-    fn disk_diameter(self) -> u16 {
+    fn disk_diameter(self) -> i64 {
         self.0 + self.1 - 1
     }
 
-    fn phase(self) -> u16 {
+    fn phase(self) -> i64 {
         self.0 - 1
-    }
-
-    fn disk_zero(self) -> Coord {
-        let x = 1;
-        let y = self.1 + self.0 - 1;
-        Coord(x, y)
-    }
-
-    fn move_k_times(self, k: u64) -> Coord {
-        let d = self.disk_diameter() as u64;
-        let p0 = self.phase() as u64;
-        let p1 = ((p0 + k) % d) as u16;
-
-        let Coord(x0, y0) = self.disk_zero();
-        Coord(x0 + p1, y0 - p1)
-    }
-
-    fn position_hash(self) -> u64 {
-        self.0 as u64 + (100 * (self.1 as u64))
     }
 }
 
@@ -49,11 +31,19 @@ fn main() {
     let lines: Vec<String> = stdin().lock().lines().map(|line| line.unwrap()).collect();
     let coords: Vec<Coord> = lines.into_iter().map(|s| Coord::parse(&s)).collect();
 
-    let hashes: Vec<u64> = coords
-        .into_iter()
-        .map(|coord| coord.move_k_times(100).position_hash())
+    let diameters: Vec<i64> = coords.iter().map(|coord| coord.disk_diameter()).collect();
+    eprintln!("{:?}", diameters);
+
+    let r: Vec<i64> = coords
+        .iter()
+        .map(|coord| {
+            let d = coord.disk_diameter();
+            let p = coord.phase();
+            (d - 1 - p + 3 * d) % d
+        })
         .collect();
 
-    let result: u64 = hashes.into_iter().sum();
+    let m: Vec<i64> = coords.iter().map(|coord| coord.disk_diameter()).collect();
+    let result = math::crt(&r, &m).0;
     println!("{}", result);
 }
