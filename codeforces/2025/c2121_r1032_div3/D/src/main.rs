9f5000de45542fc5dc1bb84ffceb61172260c956
Ivan Krechetov <ikr@ikr.li>
2025-06-21 10:15:31 +0200

    Pass in a matrix

--- codeforces/2025/c2121_r1032_div3/D/src/main.rs
+++ codeforces/2025/c2121_r1032_div3/D/src/main.rs
@@ -65,13 +65,13 @@ impl Op {
     }
 }
 
-fn sorting_program(mut a: Vec<usize>, mut b: Vec<usize>) -> Vec<Op> {
-    let n = a.len();
+fn sorting_program(mut ab: Vec<Vec<usize>>) -> Vec<Op> {
+    let n = ab[0].len();
     let mut result = vec![];
 
     for i in 0..n {
-        if a[i] > b[i] {
-            (a[i], b[i]) = (b[i], a[i]);
+        if ab[0][i] > ab[1][i] {
+            (ab[0][i], ab[1][i]) = (ab[1][i], ab[0][i]);
             result.push(Op::Swap(i));
         }
     }
@@ -79,53 +79,53 @@ fn sorting_program(mut a: Vec<usize>, mut b: Vec<usize>) -> Vec<Op> {
     for x0 in 1..=n {
         let i = x0 - 1;
 
-        if let Some(j) = b.iter().position(|x| *x == x0) {
+        if let Some(j) = ab[1].iter().position(|x| *x == x0) {
             if i < j {
                 result.extend(Op::k_left_b_shifts(j, j - i));
-                b.remove(j);
-                b.insert(i, x0);
+                ab[1].remove(j);
+                ab[1].insert(i, x0);
             } else if i > j {
                 result.extend(Op::k_right_b_shifts(j, i - j));
-                b.insert(i + 1, x0);
-                b.remove(j);
+                ab[1].insert(i + 1, x0);
+                ab[1].remove(j);
             }
 
             result.push(Op::Swap(i));
-            (a[i], b[i]) = (b[i], a[i]);
+            (ab[0][i], ab[1][i]) = (ab[1][i], ab[0][i]);
         } else {
-            let j: usize = a.iter().position(|x| *x == x0).unwrap();
+            let j: usize = ab[0].iter().position(|x| *x == x0).unwrap();
             if i < j {
                 result.extend(Op::k_left_a_shifts(j, j - i));
-                a.remove(j);
-                a.insert(i, x0);
+                ab[0].remove(j);
+                ab[0].insert(i, x0);
             } else if i > j {
                 result.extend(Op::k_right_a_shifts(j, i - j));
-                a.insert(i + 1, x0);
-                a.remove(j);
+                ab[0].insert(i + 1, x0);
+                ab[0].remove(j);
             }
         }
     }
 
     for x0 in n + 1..=2 * n {
         let i = x0 - n - 1;
-        let j: usize = b.iter().position(|x| *x == x0).unwrap();
+        let j: usize = ab[1].iter().position(|x| *x == x0).unwrap();
 
         if i <= j {
             result.extend(Op::k_left_b_shifts(j, j - i));
-            b.remove(j);
-            b.insert(i, x0);
+            ab[1].remove(j);
+            ab[1].insert(i, x0);
         } else {
             assert!(j < i);
             result.extend(Op::k_right_b_shifts(j, i - j));
-            b.remove(j);
-            b.insert(i - 1, x0);
+            ab[1].remove(j);
+            ab[1].insert(i - 1, x0);
         }
     }
 
     assert!(result.len() <= 1709);
-    assert!(a.is_sorted());
-    assert!(b.is_sorted());
-    assert!(a.into_iter().zip(b.into_iter()).all(|(x, y)| x < y));
+    assert!(ab[0].is_sorted());
+    assert!(ab[1].is_sorted());
+    assert!(ab[0].iter().zip(ab[1].iter()).all(|(x, y)| x < y));
 
     result
 }
@@ -143,7 +143,7 @@ fn main() {
         let a: Vec<usize> = scanner.next_n(n);
         let b: Vec<usize> = scanner.next_n(n);
 
-        let prog = sorting_program(a, b);
+        let prog = sorting_program(vec![a, b]);
         writeln!(writer, "{}", prog.len()).unwrap();
 
         for op in prog {
