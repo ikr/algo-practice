acd5b66fbbfca2b03904e848797512bbc1bcac02
Ivan Krechetov <ikr@ikr.li>
2025-09-04 08:31:55 +0200

    Start with part 2

--- everybodycodes/2025/The_Entertainment_Hub/02_The_Pocket-Money_Popper/src/main.rs
+++ everybodycodes/2025/The_Entertainment_Hub/02_The_Pocket-Money_Popper/src/main.rs
@@ -1,22 +1,45 @@
+use std::collections::VecDeque;
+
 use proconio::input;
 use proconio::marker::Chars;
+
+const REPS: usize = 5;
+const BOLTS: [char; 3] = ['R', 'G', 'B'];
+
+fn initial_halves(pattern: &[char]) -> (VecDeque<char>, VecDeque<char>) {
+    let full = pattern.repeat(REPS);
+    let n = full.len();
+    (
+        full[..n / 2].iter().cloned().collect(),
+        full[n / 2..].iter().cloned().collect(),
+    )
+}
 
 fn main() {
     input! {
         xs: Chars,
     }
 
-    let n = xs.len();
-    let bolts = ['R', 'G', 'B'];
-    let mut k: usize = 0;
-    let mut result = 1;
+    let mut ib: usize = 0;
+    let mut result = 0;
+    let (mut aa, mut bb) = initial_halves(&xs);
 
-    for (i, x) in xs.into_iter().enumerate() {
-        if x != bolts[k] {
-            k = (k + 1) % bolts.len();
-            if i != n - 1 {
+    while let Some(a) = aa.pop_front() {
+        if a == BOLTS[ib] {
+            if (aa.len() + 1 + bb.len()) % 2 == 0 {
+                bb.pop_front();
+            } else {
+                let b = bb.pop_front().unwrap();
+                aa.push_back(b);
+            }
+        } else {
+            ib = (ib + 1) % BOLTS.len();
+            if !aa.is_empty() {
                 result += 1;
             }
+
+            let b = bb.pop_front().unwrap();
+            aa.push_back(b);
         }
     }
 
