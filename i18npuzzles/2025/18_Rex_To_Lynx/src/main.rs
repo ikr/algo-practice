a6e9ccd2c3767ce3fec199fa01f714394a8cc666
Ivan Krechetov <ikr@ikr.li>
2025-09-18 07:12:42 +0200

    Try unicode-bidi

--- i18npuzzles/2025/18_Rex_To_Lynx/src/main.rs
+++ i18npuzzles/2025/18_Rex_To_Lynx/src/main.rs
@@ -1,6 +1,15 @@
 use std::io::{BufRead, stdin};
+use unicode_bidi::BidiInfo;
 
 fn main() {
     let lines: Vec<String> = stdin().lock().lines().map(|line| line.unwrap()).collect();
     eprintln!("{:?}", lines);
+
+    for text in lines {
+        let bidi_info = BidiInfo::new(&text, None);
+        let para = &bidi_info.paragraphs[0];
+        let line = para.range.clone();
+        let display: String = bidi_info.reorder_line(para, line).chars().collect();
+        eprintln!("{text} → {display}");
+    }
 }
