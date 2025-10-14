7191b1d07c908ea32467d1e3ed6f18f64d077408
Ivan Krechetov <ikr@ikr.li>
2025-10-14 14:47:54 +0200

    Implement `perm_boundaries`

--- facebook/2025/Practice/C_Monkey_Around/src/main.rs
+++ facebook/2025/Practice/C_Monkey_Around/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use proconio::input;
 use proconio_derive::fastout;
 
@@ -14,6 +16,28 @@ impl Op {
             Self::Rotate => 2.to_string(),
         }
     }
+}
+
+fn perm_boundaries(xs: &[usize]) -> Vec<(usize, usize)> {
+    let mut seen: HashSet<usize> = HashSet::new();
+    let mut result = vec![];
+
+    for &x in xs {
+        if seen.contains(&x) {
+            let i0: usize = result.last().map_or(0, |(_, prev_end)| *prev_end);
+            let k = seen.len();
+
+            result.push((i0, i0 + k));
+            seen.clear();
+        }
+        seen.insert(x);
+    }
+
+    let i0: usize = result.last().map_or(0, |(_, prev_end)| *prev_end);
+    let k = seen.len();
+    result.push((i0, i0 + k));
+
+    result
 }
 
 fn deconstruct_program(xs: Vec<usize>) -> Vec<Op> {
@@ -39,5 +63,36 @@ fn main() {
         for op in result {
             println!("{}", op.represent());
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_perm_boundaries_a() {
+        assert_eq!(perm_boundaries(&[1]), vec![(0, 1)]);
+    }
+
+    #[test]
+    fn test_perm_boundaries_b() {
+        assert_eq!(
+            perm_boundaries(&[1, 2, 3, 3, 4, 1, 2]),
+            vec![(0, 3), (3, 7)]
+        );
+    }
+
+    #[test]
+    fn test_perm_boundaries_c() {
+        assert_eq!(perm_boundaries(&[4, 3, 2, 1]), vec![(0, 4)]);
+    }
+
+    #[test]
+    fn test_perm_boundaries_d() {
+        assert_eq!(
+            perm_boundaries(&[4, 3, 2, 1, 1, 1, 2]),
+            vec![(0, 4), (4, 5), (5, 7)]
+        );
     }
 }
