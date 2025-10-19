ebed9fb8b42bf3b347213b9351ef81a432e0fbc2
Ivan Krechetov <ikr@ikr.li>
2025-10-19 16:51:51 +0200

    First draft

--- facebook/2025/Round_1/B2_Final_Product/src/main.rs
+++ facebook/2025/Round_1/B2_Final_Product/src/main.rs
@@ -1,4 +1,5 @@
 use ac_library::ModInt1000000007 as Mint;
+use itertools::Itertools;
 use primal::Sieve;
 use proconio::input;
 use proconio_derive::fastout;
@@ -7,7 +8,8 @@ fn capped_factors(sieve: &Sieve, x: usize, cap: &[(usize, usize)]) -> Vec<(usize
     let prime_index = |p: usize| -> Option<usize> { cap.iter().position(|(q, _)| *q == p) };
     let ff = sieve.factor(x).unwrap();
 
-    ff.into_iter()
+    let result = ff
+        .into_iter()
         .filter_map(|(p, i0)| {
             if let Some(j) = prime_index(p) {
                 let i = i0.min(cap[j].1);
@@ -16,13 +18,48 @@ fn capped_factors(sieve: &Sieve, x: usize, cap: &[(usize, usize)]) -> Vec<(usize
                 None
             }
         })
-        .collect()
+        .collect::<Vec<_>>();
+
+    if result.is_empty() {
+        vec![(1, 0)]
+    } else {
+        result
+    }
 }
 
 fn sequences_num(sieve: &Sieve, n: usize, a: usize, b: usize) -> Mint {
     let fb = sieve.factor(b).unwrap();
     let fa = capped_factors(sieve, a, &fb);
-    todo!()
+
+    let exps_a: Vec<Vec<usize>> = fa
+        .iter()
+        .map(|(_, k)| (0..=*k).collect::<Vec<_>>())
+        .collect();
+
+    let mut result = Mint::new(0);
+
+    for ii in exps_a.into_iter().multi_cartesian_product() {
+        let total_exp_a: usize = ii.iter().sum();
+        let lhs_placements_num = Mint::new(n).pow(total_exp_a as u64);
+        let a_prime_index = |p: usize| -> Option<usize> { fa.iter().position(|(q, _)| *q == p) };
+
+        let kk: Vec<usize> = fb
+            .iter()
+            .map(|&(p, k0)| {
+                if let Some(i) = a_prime_index(p) {
+                    k0 - ii[i]
+                } else {
+                    k0
+                }
+            })
+            .collect();
+
+        let total_exp_b: usize = kk.into_iter().sum();
+        let rhs_placements_num = Mint::new(n).pow(total_exp_b as u64);
+        result += lhs_placements_num * rhs_placements_num;
+    }
+
+    result
 }
 
 #[fastout]
@@ -31,7 +68,7 @@ fn main() {
     assert_eq!(sieve.factor(1).unwrap(), vec![]);
 
     input! {
-        cases_num: u16,
+        cases_num: u8,
     }
 
     for t in 1..=cases_num {
