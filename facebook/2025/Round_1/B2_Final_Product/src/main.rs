98a5c0dab47254b492bd89779e3205556154ce9e
Ivan Krechetov <ikr@ikr.li>
2025-10-19 17:23:17 +0200

    New approach => WA on sample case 4

--- facebook/2025/Round_1/B2_Final_Product/src/main.rs
+++ facebook/2025/Round_1/B2_Final_Product/src/main.rs
@@ -4,66 +4,42 @@ use primal::Sieve;
 use proconio::input;
 use proconio_derive::fastout;
 
-fn capped_factors(sieve: &Sieve, x: usize, cap: &[(usize, usize)]) -> Vec<(usize, usize)> {
-    let prime_index = |p: usize| -> Option<usize> { cap.iter().position(|(q, _)| *q == p) };
-    let ff = sieve.factor(x).unwrap();
-
-    let result = ff
-        .into_iter()
-        .filter_map(|(p, i0)| {
-            if let Some(j) = prime_index(p) {
-                let i = i0.min(cap[j].1);
-                Some((p, i))
-            } else {
-                None
-            }
-        })
-        .collect::<Vec<_>>();
-
-    if result.is_empty() {
-        vec![(1, 0)]
-    } else {
-        result
-    }
-}
-
 fn sequences_num(sieve: &Sieve, n: usize, a: usize, b: usize) -> Mint {
     let fb = sieve.factor(b).unwrap();
-    let fa = capped_factors(sieve, a, &fb);
-    eprintln!("n: {n}, a: {a}, b: {b}, fa: {:?}, fb: {:?}", fa, fb);
 
-    let exps_a: Vec<Vec<usize>> = fa
+    let exps_b: Vec<Vec<usize>> = fb
         .iter()
         .map(|(_, k)| (0..=*k).collect::<Vec<_>>())
         .collect();
 
     let mut result = Mint::new(0);
 
-    for ii in exps_a.into_iter().multi_cartesian_product() {
+    for ii in exps_b.into_iter().multi_cartesian_product() {
+        let aa: usize = ii
+            .iter()
+            .enumerate()
+            .map(|(i, &k0)| fb[i].0.pow(k0 as u32))
+            .fold(1, |acc, x| acc * x);
+
+        if aa > a {
+            continue;
+        }
+
         let total_exp_a: usize = ii.iter().sum();
-        let lhs_placements_num = Mint::new(n).pow(total_exp_a as u64);
-        let a_prime_index = |p: usize| -> Option<usize> { fa.iter().position(|(q, _)| *q == p) };
+        let lhs = Mint::new(n).pow(total_exp_a as u64);
 
         let kk: Vec<usize> = fb
             .iter()
-            .map(|&(p, k0)| {
-                if let Some(i) = a_prime_index(p) {
-                    assert!(ii[i] <= k0);
-                    k0 - ii[i]
-                } else {
-                    k0
-                }
+            .enumerate()
+            .map(|(i, &(_, k0))| {
+                assert!(ii[i] <= k0);
+                k0 - ii[i]
             })
             .collect();
 
         let total_exp_b: usize = kk.into_iter().sum();
-        let rhs_placements_num = Mint::new(n).pow(total_exp_b as u64);
-        eprintln!(
-            "lhs: {}, rhs: {}",
-            lhs_placements_num.val(),
-            rhs_placements_num.val()
-        );
-        result += lhs_placements_num * rhs_placements_num;
+        let rhs = Mint::new(n).pow(total_exp_b as u64);
+        result += lhs * rhs;
     }
 
     result
