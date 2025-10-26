45aa173208f5d6cb3925d9f23bb741e63187a2af
Ivan Krechetov <ikr@ikr.li>
2025-10-26 11:19:44 +0100

    Seemingly endless struggle

--- yandex/2025/Trial/A_Two_out_of_Three/src/main.rs
+++ yandex/2025/Trial/A_Two_out_of_Three/src/main.rs
@@ -1,5 +1,7 @@
 use std::io::{BufWriter, Write, stdin, stdout};
 
+const CAP: usize = 1_000_000 * 1_000_000 * 1_000_000;
+
 #[derive(Default)]
 struct Scanner {
     buffer: Vec<String>,
@@ -18,6 +20,34 @@ impl Scanner {
     }
 }
 
+// Stein's algo from https://gist.github.com/cmpute/0a09749f76303b24b7961362bee8d988
+fn gcd(mut m: usize, mut n: usize) -> usize {
+    if m == 0 || n == 0 {
+        return m | n;
+    }
+
+    // find common factors of 2
+    let shift = (m | n).trailing_zeros();
+
+    // divide n and m by 2 until odd
+    // m inside loop
+    n >>= n.trailing_zeros();
+
+    while m != 0 {
+        m >>= m.trailing_zeros();
+        if n > m {
+            std::mem::swap(&mut n, &mut m)
+        }
+        m -= n;
+    }
+
+    n << shift
+}
+
+fn lcm(x: usize, y: usize) -> usize {
+    (x * y) / gcd(x, y)
+}
+
 fn distinct(abc: [usize; 3]) -> Vec<usize> {
     assert!(abc.is_sorted());
     let mut xs = abc.to_vec();
@@ -25,12 +55,53 @@ fn distinct(abc: [usize; 3]) -> Vec<usize> {
     xs
 }
 
+fn nth_number_divisible_by_either(n: usize, ab: [usize; 2]) -> Option<usize> {
+    let [a, b] = ab;
+    assert_ne!(a, b);
+    let mut lo: usize = 0;
+    let mut hi: usize = CAP;
+
+    while lo + 1 < hi {
+        let mid = lo + (hi - lo) / 2;
+        let cur = (mid / a + mid / b) - 2 * mid / lcm(a, b);
+
+        if cur > n {
+            hi = mid;
+        } else {
+            lo = mid;
+        }
+    }
+
+    if lo > CAP { None } else { Some(lo) }
+}
+
 fn nth_number_divisible_by_two_out_of_three(abc: [usize; 3], n: usize) -> Option<usize> {
     if distinct(abc).len() == 1 {
         return None;
     }
 
-    None
+    let [a, b, c] = abc;
+    if a == b {
+        return nth_number_divisible_by_either(n, [b, c]);
+    } else if b == c {
+        return nth_number_divisible_by_either(n, [a, b]);
+    }
+
+    let mut lo: usize = 0;
+    let mut hi: usize = CAP;
+
+    while lo + 1 < hi {
+        let mid = lo + (hi - lo) / 2;
+        let cur = (mid / lcm(a, b) + mid / lcm(b, c) + mid / lcm(a, c)) - 3 * lcm(lcm(a, b), c);
+
+        if cur > n {
+            hi = mid;
+        } else {
+            lo = mid;
+        }
+    }
+
+    if lo > CAP { None } else { Some(lo) }
 }
 
 fn brute_nth_number_divisible_by_two_out_of_three(abc: [usize; 3], n: usize) -> Option<usize> {
@@ -50,7 +121,7 @@ fn main() {
         *x = scanner.next();
     }
     abc.sort();
-    let n: usize = scanner.next();
+    let mut n: usize = scanner.next();
 
     let result = nth_number_divisible_by_two_out_of_three(abc, n);
     if let Some(x) = result {
@@ -67,6 +138,13 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_gcd() {
+        for (a, b, res) in vec![(1, 1, 1), (2, 3, 1), (2, 4, 2), (234_234, 2_398_473, 693)] {
+            assert_eq!(gcd(a, b), res);
+        }
+    }
+
+    #[test]
     fn test_nth_number_divisible_by_two_out_of_three() {
         assert_eq!(
             brute_nth_number_divisible_by_two_out_of_three([2, 3, 5], 10),
@@ -77,7 +155,12 @@ mod tests {
             Some(13999986)
         );
 
-        let samples = vec![([1, 1, 1], 1023), ([1, 1, 2], 1023), ([7, 14, 2], 1023)];
+        let samples = vec![
+            ([1, 1, 1], 1023),
+            ([1, 1, 2], 1023),
+            ([3, 3, 13], 200),
+            ([2, 7, 14], 1023),
+        ];
         for (abc, n) in samples {
             assert_eq!(
                 nth_number_divisible_by_two_out_of_three(abc, n),
