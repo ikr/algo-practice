b1776479f8638980cb4cf6b4dd0c8147c48291b9
Ivan Krechetov <ikr@ikr.li>
2025-11-06 06:10:01 +0100

    Start with part 3

--- everybodycodes/2025/The_Song_of_Ducks_and_Dragons/03_The_Deepest_Fit/src/main.rs
+++ everybodycodes/2025/The_Song_of_Ducks_and_Dragons/03_The_Deepest_Fit/src/main.rs
@@ -5,10 +5,5 @@ fn main() {
     std::io::stdin().read_to_string(&mut buf).unwrap();
 
     let mut csv: Vec<u32> = buf.trim().split(',').map(|s| s.parse().unwrap()).collect();
-    eprintln!("{:?}", csv);
     csv.sort();
-    csv.dedup();
-    let n = csv.len();
-    let result: u32 = csv[..20].into_iter().sum();
-    println!("{result}");
 }
