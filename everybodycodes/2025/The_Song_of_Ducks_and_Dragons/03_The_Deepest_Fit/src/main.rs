5458cae8e04ab3b6c4b7812e9df0411b3b6fcc91
Ivan Krechetov <ikr@ikr.li>
2025-11-06 06:22:45 +0100

    Solve part 3

--- everybodycodes/2025/The_Song_of_Ducks_and_Dragons/03_The_Deepest_Fit/src/main.rs
+++ everybodycodes/2025/The_Song_of_Ducks_and_Dragons/03_The_Deepest_Fit/src/main.rs
@@ -1,9 +1,11 @@
+use itertools::Itertools;
 use std::io::Read;
 
 fn main() {
     let mut buf = String::new();
     std::io::stdin().read_to_string(&mut buf).unwrap();
 
-    let mut csv: Vec<u32> = buf.trim().split(',').map(|s| s.parse().unwrap()).collect();
-    csv.sort();
+    let csv: Vec<u32> = buf.trim().split(',').map(|s| s.parse().unwrap()).collect();
+    let result = csv.into_iter().counts().into_values().max().unwrap();
+    println!("{result}");
 }
