977c0d68edda01b1846f8f61453e9fd09bdfea3c
Ivan Krechetov <ikr@ikr.li>
2025-11-07 07:17:56 +0100

    WIP

--- everybodycodes/2025/The_Song_of_Ducks_and_Dragons/04_Teeth_of_the_Wind/src/main.rs
+++ everybodycodes/2025/The_Song_of_Ducks_and_Dragons/04_Teeth_of_the_Wind/src/main.rs
@@ -1,12 +1,37 @@
 use num_rational::Rational64 as Ra;
 use std::io::{BufRead, stdin};
 
+#[derive(Clone, Copy, Debug)]
+enum El {
+    Tan(i64),
+    Ctr(i64, i64),
+}
+
 fn main() {
     let lines: Vec<String> = stdin().lock().lines().map(|line| line.unwrap()).collect();
-    let xs: Vec<i64> = lines.into_iter().map(|s| s.parse().unwrap()).collect();
-    let ms: Vec<Ra> = xs.windows(2).map(|xy| Ra::new(xy[0], xy[1])).collect();
-    let mm: Ra = ms.into_iter().fold(Ra::new(1, 1), |acc, m| acc * m);
-    let result = Ra::from_integer(10000000000000) / mm;
+    let chain: Vec<El> = lines
+        .into_iter()
+        .map(|s| {
+            if s.contains('|') {
+                let parts: Vec<_> = s.split('|').collect();
+                El::Ctr(parts[0].parse().unwrap(), parts[1].parse().unwrap())
+            } else {
+                El::Tan(s.parse().unwrap())
+            }
+        })
+        .collect();
+
+    let mm: Ra = chain
+        .windows(2)
+        .fold(Ra::from_integer(1), |acc, xy| match (xy[0], xy[1]) {
+            (El::Tan(x), El::Tan(y)) => Ra::new(x, y),
+        });
+
+    // let xs: Vec<i64> = lines.into_iter().map(|s| s.parse().unwrap()).collect();
+    // let ms: Vec<Ra> = xs.windows(2).map(|xy| Ra::new(xy[0], xy[1])).collect();
+    // let mm: Ra = ms.into_iter().fold(Ra::new(1, 1), |acc, m| acc * m);
+
+    let result = Ra::from_integer(100) * mm;
     eprintln!(
         "{}",
         (*result.numer() as u64).div_ceil(*result.denom() as u64)
