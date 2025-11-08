98f6f1b5f316e1cb3667306a43e2b11d58aaf4d6
Ivan Krechetov <ikr@ikr.li>
2025-11-08 10:14:50 +0100

    Refactor

--- everybodycodes/2025/The_Song_of_Ducks_and_Dragons/05_Fishbone_Order/src/main.rs
+++ everybodycodes/2025/The_Song_of_Ducks_and_Dragons/05_Fishbone_Order/src/main.rs
@@ -18,6 +18,35 @@ impl El {
             El::WithLR(x) => x,
         }
     }
+
+    fn accommodate(self, x: i32) -> Option<Self> {
+        match self {
+            El::Single(y) => {
+                if x < y {
+                    Some(El::WithL(y))
+                } else if x > y {
+                    Some(El::WithR(y))
+                } else {
+                    None
+                }
+            }
+            El::WithL(y) => {
+                if x > y {
+                    Some(El::WithLR(y))
+                } else {
+                    None
+                }
+            }
+            El::WithR(y) => {
+                if x < y {
+                    Some(El::WithLR(y))
+                } else {
+                    None
+                }
+            }
+            El::WithLR(_) => None,
+        }
+    }
 }
 
 fn main() {
@@ -33,40 +62,20 @@ fn main() {
 
     let mut result: Vec<El> = vec![];
     for x in xs {
-        let mut patched = false;
-
-        for el in &mut result {
-            match *el {
-                El::Single(y) => {
-                    if x < y {
-                        *el = El::WithL(y);
-                        patched = true;
-                        break;
-                    } else if x > y {
-                        *el = El::WithR(y);
-                        patched = true;
-                        break;
-                    }
+        if let Some((i, new_el)) = result
+            .iter()
+            .enumerate()
+            .filter_map(|(j, el)| {
+                if let Some(new_el) = el.accommodate(x) {
+                    Some((j, new_el))
+                } else {
+                    None
                 }
-                El::WithL(y) => {
-                    if x > y {
-                        *el = El::WithLR(y);
-                        patched = true;
-                        break;
-                    }
-                }
-                El::WithR(y) => {
-                    if x < y {
-                        *el = El::WithLR(y);
-                        patched = true;
-                        break;
-                    }
-                }
-                El::WithLR(_) => {}
-            }
-        }
-
-        if !patched {
+            })
+            .next()
+        {
+            result[i] = new_el;
+        } else {
             result.push(El::Single(x));
         }
     }
