bbef0f4eda1e6039ea7ae79b2e4bc87d51b96a4f
Ivan Krechetov <ikr@ikr.li>
2025-11-08 11:16:51 +0100

    Solve part 3

--- everybodycodes/2025/The_Song_of_Ducks_and_Dragons/05_Fishbone_Order/src/main.rs
+++ everybodycodes/2025/The_Song_of_Ducks_and_Dragons/05_Fishbone_Order/src/main.rs
@@ -1,50 +1,71 @@
-use itertools::{Itertools, MinMaxResult};
+use itertools::Itertools;
 use std::io::BufRead;
 
 #[derive(Clone, Copy)]
 enum El {
     Single(u8),
-    WithL(u8),
-    WithR(u8),
-    WithLR(u8),
+    WithL { lhs: u8, value: u8 },
+    WithR { value: u8, rhs: u8 },
+    WithLR { lhs: u8, value: u8, rhs: u8 },
 }
 
 impl El {
     fn center(self) -> u8 {
         match self {
             El::Single(x) => x,
-            El::WithL(x) => x,
-            El::WithR(x) => x,
-            El::WithLR(x) => x,
+            El::WithL { lhs: _, value } => value,
+            El::WithR { value, rhs: _ } => value,
+            El::WithLR {
+                lhs: _,
+                value,
+                rhs: _,
+            } => value,
         }
+    }
+
+    fn values(self) -> Vec<u8> {
+        match self {
+            El::Single(x) => vec![x],
+            El::WithL { lhs, value } => vec![lhs, value],
+            El::WithR { value, rhs } => vec![value, rhs],
+            El::WithLR { lhs, value, rhs } => vec![lhs, value, rhs],
+        }
+    }
+
+    fn seq_num(self) -> u16 {
+        self.values().into_iter().join("").parse().unwrap()
     }
 
     fn accommodate(self, x: u8) -> Option<Self> {
         match self {
-            El::Single(y) => {
-                if x < y {
-                    Some(El::WithL(y))
-                } else if x > y {
-                    Some(El::WithR(y))
+            El::Single(value) => {
+                if x < value {
+                    Some(El::WithL { lhs: x, value })
+                } else if x > value {
+                    Some(El::WithR { value, rhs: x })
                 } else {
                     None
                 }
             }
-            El::WithL(y) => {
-                if x > y {
-                    Some(El::WithLR(y))
+            El::WithL { lhs, value } => {
+                if x > value {
+                    Some(El::WithLR { lhs, value, rhs: x })
                 } else {
                     None
                 }
             }
-            El::WithR(y) => {
-                if x < y {
-                    Some(El::WithLR(y))
+            El::WithR { value, rhs } => {
+                if x < value {
+                    Some(El::WithLR { lhs: x, value, rhs })
                 } else {
                     None
                 }
             }
-            El::WithLR(_) => None,
+            El::WithLR {
+                lhs: _,
+                value: _,
+                rhs: _,
+            } => None,
         }
     }
 }
@@ -68,13 +89,12 @@ fn fishbone(xs: Vec<u8>) -> Vec<El> {
     result
 }
 
-fn quality(xs: Vec<u8>) -> u64 {
-    fishbone(xs)
-        .into_iter()
-        .map(|el| el.center())
-        .join("")
-        .parse()
-        .unwrap()
+fn quality(xs: &[El]) -> u64 {
+    xs.iter().map(|el| el.center()).join("").parse().unwrap()
+}
+
+fn level_seq_nums(xs: &[El]) -> Vec<u16> {
+    xs.iter().map(|el| el.seq_num()).collect()
 }
 
 fn parse_line(s: String) -> Vec<u8> {
@@ -93,9 +113,36 @@ fn main() {
         .map(|line| parse_line(line.unwrap()))
         .collect();
 
-    let qs: Vec<_> = xss.into_iter().map(quality).collect();
-    match qs.into_iter().minmax() {
-        MinMaxResult::MinMax(lo, hi) => println!("{}", hi - lo),
-        _ => unreachable!(),
-    }
+    let mut fbs: Vec<(usize, Vec<El>)> = xss
+        .into_iter()
+        .enumerate()
+        .map(|(i, xs)| (i + 1, fishbone(xs)))
+        .collect();
+
+    fbs.sort_by(|(i, fb_a), (j, fb_b)| {
+        let qa = quality(fb_a);
+        let qb = quality(fb_b);
+
+        if qa == qb {
+            let sna = level_seq_nums(fb_a);
+            let snb = level_seq_nums(fb_b);
+
+            if sna == snb {
+                i.cmp(j).reverse()
+            } else {
+                sna.cmp(&snb).reverse()
+            }
+        } else {
+            qa.cmp(&qb).reverse()
+        }
+    });
+
+    let checksum: usize = fbs
+        .into_iter()
+        .map(|(id, _)| id)
+        .enumerate()
+        .map(|(i, id)| (i + 1) * id)
+        .sum();
+
+    println!("{checksum}");
 }
