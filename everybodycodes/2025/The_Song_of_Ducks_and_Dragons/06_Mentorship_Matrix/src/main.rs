3b3d4d612b7aab5889a16a4676658c3cbdba6e93
Ivan Krechetov <ikr@ikr.li>
2025-11-11 10:54:24 +0100

    Solve part 3

--- everybodycodes/2025/The_Song_of_Ducks_and_Dragons/06_Mentorship_Matrix/src/main.rs
+++ everybodycodes/2025/The_Song_of_Ducks_and_Dragons/06_Mentorship_Matrix/src/main.rs
@@ -1,5 +1,8 @@
 use std::io::Read;
 
+const DISTANCE_LIMIT: usize = 1000;
+const PATTERN_REPEATS: usize = 1000;
+
 fn mentor_symbol(x: u8) -> u8 {
     x.to_ascii_uppercase()
 }
@@ -8,22 +11,40 @@ fn is_novice_symbol(x: u8) -> bool {
     x.is_ascii_lowercase()
 }
 
+fn mentor_index(x: u8) -> usize {
+    (x - b'A') as usize
+}
+
 fn main() {
     let mut buf = String::new();
     std::io::stdin().read_to_string(&mut buf).unwrap();
-    let xs: Vec<u8> = buf.trim().bytes().collect();
+    let mut xs: Vec<u8> = vec![];
 
-    let result = xs
-        .into_iter()
-        .fold((vec![0usize; 128], 0), |(mut fq, s), x| {
-            if is_novice_symbol(x) {
-                let xx = mentor_symbol(x);
-                (fq.clone(), s + fq[xx as usize])
-            } else {
-                fq[x as usize] += 1;
-                (fq, s)
-            }
+    for _ in 0..PATTERN_REPEATS {
+        xs.extend(buf.trim().bytes())
+    }
+
+    let idx: Vec<Vec<usize>> = xs
+        .iter()
+        .enumerate()
+        .filter(|(_, x)| !is_novice_symbol(**x))
+        .fold(vec![vec![]; 3], |mut acc, (i, &x)| {
+            acc[mentor_index(x)].push(i);
+            acc
         });
 
-    println!("{}", result.1);
+    let result: usize = xs
+        .into_iter()
+        .enumerate()
+        .filter(|(_, x)| is_novice_symbol(*x))
+        .fold(0usize, |acc, (i, x)| {
+            let k = mentor_index(mentor_symbol(x));
+            let lo = i.saturating_sub(DISTANCE_LIMIT);
+            let hi = i + DISTANCE_LIMIT;
+            let a = idx[k].partition_point(|&j| j < lo);
+            let b = idx[k].partition_point(|&j| j <= hi);
+            acc + b - a
+        });
+
+    println!("{result}")
 }
