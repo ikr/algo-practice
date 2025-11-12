271e5644c9e7cbc6a221501ede17332c962a8853
Ivan Krechetov <ikr@ikr.li>
2025-11-12 15:19:10 +0100

    WIP for part 3

--- everybodycodes/2025/The_Song_of_Ducks_and_Dragons/07_Namegraph/src/main.rs
+++ everybodycodes/2025/The_Song_of_Ducks_and_Dragons/07_Namegraph/src/main.rs
@@ -1,30 +1,54 @@
 use std::io::{BufRead, stdin};
 
-fn first(s: &str) -> char {
-    s.chars().next().unwrap()
-}
+const LO: usize = 7;
+const HI: usize = 11;
 
 fn decode_rule(s: &str) -> Vec<(char, char)> {
+    let first = |x: &str| -> char { x.chars().next().unwrap() };
     let parts: Vec<_> = s.split(" > ").collect();
     let a: char = first(parts[0]);
     parts[1].split(',').map(|x| (a, first(x))).collect()
 }
 
+fn is_valid_word(rule_pairs: &[(char, char)], word: &[char]) -> bool {
+    word.windows(2)
+        .all(|xy| rule_pairs.contains(&(xy[0], xy[1])))
+}
+
+fn words_count(rule_pairs: &[(char, char)], prefix: Vec<char>) -> usize {
+    if prefix.len() == HI {
+        return 1;
+    }
+
+    let own_count = if prefix.len() < LO { 0 } else { 1 };
+    let t: char = *prefix.last().unwrap();
+
+    own_count
+        + rule_pairs
+            .iter()
+            .filter_map(|&(x, y)| {
+                if x == t {
+                    let mut new_prefix = prefix.clone();
+                    new_prefix.push(y);
+                    Some(words_count(rule_pairs, new_prefix))
+                } else {
+                    None
+                }
+            })
+            .sum::<usize>()
+}
+
 fn main() {
     let lines: Vec<String> = stdin().lock().lines().map(|line| line.unwrap()).collect();
     let names: Vec<String> = lines[0].split(',').map(|x| x.to_string()).collect();
-    let ps: Vec<(char, char)> = lines[2..].iter().flat_map(|x| decode_rule(x)).collect();
+    let rule_pairs: Vec<(char, char)> = lines[2..].iter().flat_map(|x| decode_rule(x)).collect();
 
     let result: usize = names
         .into_iter()
-        .enumerate()
-        .filter_map(|(i, s)| {
-            if s.chars()
-                .collect::<Vec<_>>()
-                .windows(2)
-                .all(|xy| ps.contains(&(xy[0], xy[1])))
-            {
-                Some(i + 1)
+        .filter_map(|s| {
+            let xs: Vec<_> = s.chars().collect();
+            if is_valid_word(&rule_pairs, &xs) {
+                Some(words_count(&rule_pairs, xs))
             } else {
                 None
             }
