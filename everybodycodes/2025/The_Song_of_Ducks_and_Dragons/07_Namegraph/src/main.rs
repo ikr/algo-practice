65b0a1d0e1cf7a67626de5ef5765981aeefa8d63
Ivan Krechetov <ikr@ikr.li>
2025-11-12 13:39:03 +0100

    Solve part 1

---
+++ everybodycodes/2025/The_Song_of_Ducks_and_Dragons/07_Namegraph/src/main.rs
@@ -0,0 +1,29 @@
+use std::io::{BufRead, stdin};
+
+fn first(s: &str) -> char {
+    s.chars().next().unwrap()
+}
+
+fn decode_rule(s: &str) -> Vec<(char, char)> {
+    let parts: Vec<_> = s.split(" > ").collect();
+    let a: char = first(parts[0]);
+    parts[1].split(',').map(|x| (a, first(x))).collect()
+}
+
+fn main() {
+    let lines: Vec<String> = stdin().lock().lines().map(|line| line.unwrap()).collect();
+    let names: Vec<String> = lines[0].split(',').map(|x| x.to_string()).collect();
+    let ps: Vec<(char, char)> = lines[2..].iter().flat_map(|x| decode_rule(x)).collect();
+
+    let result = names
+        .into_iter()
+        .find(|s| {
+            s.chars()
+                .collect::<Vec<_>>()
+                .windows(2)
+                .all(|xy| ps.contains(&(xy[0], xy[1])))
+        })
+        .unwrap();
+
+    println!("{result}");
+}
