9403fe384c5570d51c80d6027a97c74359039fe2
Ivan Krechetov <ikr@ikr.li>
2025-11-13 07:36:12 +0100

    Solve part 2

--- everybodycodes/2025/The_Song_of_Ducks_and_Dragons/08_The_Art_of_Connection/src/main.rs
+++ everybodycodes/2025/The_Song_of_Ducks_and_Dragons/08_The_Art_of_Connection/src/main.rs
@@ -1,7 +1,26 @@
 use itertools::Itertools;
 use std::io::Read;
 
-const N: usize = 32;
+fn intersect(mut xs: [usize; 4]) -> bool {
+    xs.sort();
+    xs.iter().tuple_windows().any(|(x, y)| x == y)
+}
+
+fn chords_knot(ab: (usize, usize), cd: (usize, usize)) -> bool {
+    let (mut a, mut b) = ab;
+    if a > b {
+        (b, a) = (a, b);
+    }
+
+    let (c, d) = cd;
+
+    if intersect([a, b, c, d]) {
+        return false;
+    }
+
+    let is_rhs = |x: usize| -> bool { a < x && x < b };
+    is_rhs(c) != is_rhs(d)
+}
 
 fn main() {
     let mut buf = String::new();
@@ -13,11 +32,27 @@ fn main() {
         .map(|x| x.to_string().parse().unwrap())
         .collect();
 
-    let result = xs
-        .into_iter()
-        .tuple_windows()
-        .filter(|&(x, y)| x.abs_diff(y) == N / 2)
-        .count();
+    let chords: Vec<(usize, usize)> = xs.into_iter().tuple_windows().collect();
+    let mut result = 0;
+
+    for (i, &ab) in chords.iter().enumerate() {
+        result += chords[..i]
+            .iter()
+            .filter(|&&cd| chords_knot(ab, cd))
+            .count();
+    }
 
     println!("{result}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_chords_not() {
+        assert!(chords_knot((1, 5), (2, 6)));
+        assert!(chords_knot((1, 5), (7, 3)));
+        assert!(!chords_knot((8, 6), (1, 5)));
+    }
 }
