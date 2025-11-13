ccaf5eda9157b8e3766e573c6053f7af063cf19e
Ivan Krechetov <ikr@ikr.li>
2025-11-13 07:54:28 +0100

    WA: 203

--- everybodycodes/2025/The_Song_of_Ducks_and_Dragons/08_The_Art_of_Connection/src/main.rs
+++ everybodycodes/2025/The_Song_of_Ducks_and_Dragons/08_The_Art_of_Connection/src/main.rs
@@ -1,9 +1,19 @@
 use itertools::Itertools;
 use std::io::Read;
 
+const N: usize = 8;
+
 fn intersect(mut xs: [usize; 4]) -> bool {
     xs.sort();
     xs.iter().tuple_windows().any(|(x, y)| x == y)
+}
+
+fn chords_match(ab: (usize, usize), cd: (usize, usize)) -> bool {
+    let mut xs = [ab.0, ab.1];
+    xs.sort();
+    let mut ys = [cd.0, cd.1];
+    ys.sort();
+    xs == ys
 }
 
 fn chords_knot(ab: (usize, usize), cd: (usize, usize)) -> bool {
@@ -14,12 +24,14 @@ fn chords_knot(ab: (usize, usize), cd: (usize, usize)) -> bool {
 
     let (c, d) = cd;
 
-    if intersect([a, b, c, d]) {
-        return false;
+    if chords_match(ab, cd) {
+        true
+    } else if intersect([a, b, c, d]) {
+        false
+    } else {
+        let is_rhs = |x: usize| -> bool { a < x && x < b };
+        is_rhs(c) != is_rhs(d)
     }
-
-    let is_rhs = |x: usize| -> bool { a < x && x < b };
-    is_rhs(c) != is_rhs(d)
 }
 
 fn main() {
@@ -33,14 +45,16 @@ fn main() {
         .collect();
 
     let chords: Vec<(usize, usize)> = xs.into_iter().tuple_windows().collect();
-    let mut result = 0;
 
-    for (i, &ab) in chords.iter().enumerate() {
-        result += chords[..i]
-            .iter()
-            .filter(|&&cd| chords_knot(ab, cd))
-            .count();
-    }
+    let result = (1..=N)
+        .tuple_combinations()
+        .map(|(a, b)| {
+            let r = chords.iter().filter(|&&cd| chords_knot((a, b), cd)).count();
+            eprintln!("{r} at {:?}", (a, b));
+            r
+        })
+        .max()
+        .unwrap();
 
     println!("{result}");
 }
@@ -53,6 +67,7 @@ mod tests {
     fn test_chords_not() {
         assert!(chords_knot((1, 5), (2, 6)));
         assert!(chords_knot((1, 5), (7, 3)));
+        assert!(chords_knot((1, 5), (1, 5)));
         assert!(!chords_knot((8, 6), (1, 5)));
     }
 }
