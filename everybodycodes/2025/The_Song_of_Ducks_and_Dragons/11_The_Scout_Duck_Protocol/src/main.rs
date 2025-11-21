af2217218877b94816b700df37a59273d81a9aa4
Ivan Krechetov <ikr@ikr.li>
2025-11-21 17:33:13 +0100

    Fiddle around

--- everybodycodes/2025/The_Song_of_Ducks_and_Dragons/11_The_Scout_Duck_Protocol/src/main.rs
+++ everybodycodes/2025/The_Song_of_Ducks_and_Dragons/11_The_Scout_Duck_Protocol/src/main.rs
@@ -1,63 +1,64 @@
 use std::io::{BufRead, stdin};
 
-fn ltr_additive(xs0: &[u32]) -> Option<Vec<u32>> {
-    let mut xs: Vec<u32> = xs0.to_vec();
+fn ltr_additive_rounds_until_stable(mut xs: Vec<u64>) -> (Vec<u64>, u64) {
+    let mut rounds: u64 = 0;
 
-    for i in 1..xs.len() {
-        if xs[i - 1] > xs[i] {
-            xs[i - 1] -= 1;
-            xs[i] += 1;
+    loop {
+        let mut cur: u64 = 0;
+
+        for i in 1..xs.len() {
+            if xs[i - 1] > xs[i] {
+                let delta = (xs[i - 1] - xs[i]).div_ceil(2);
+                cur = cur.max(delta);
+                xs[i - 1] -= delta;
+                xs[i] += delta;
+            }
         }
+
+        if cur == 0 {
+            break;
+        }
+        rounds += cur;
     }
-    if xs == xs0 { None } else { Some(xs) }
+
+    (xs, rounds)
 }
 
-fn ltr_subtractive(xs0: &[u32]) -> Option<Vec<u32>> {
-    let mut xs = xs0.to_vec();
+fn ltr_subtractive_rounds_until_stable(mut xs: Vec<u64>) -> u64 {
+    let mut rounds: u64 = 0;
 
-    for i in 1..xs.len() {
-        if xs[i - 1] < xs[i] {
-            xs[i - 1] += 1;
-            xs[i] -= 1;
+    loop {
+        let mut cur: u64 = 0;
+
+        for i in 1..xs.len() {
+            if xs[i] > xs[i - 1] {
+                let delta = (xs[i] - xs[i - 1]).div_ceil(2);
+                cur = cur.max(delta);
+                xs[i - 1] += delta;
+                xs[i] -= delta;
+            }
         }
+
+        if cur == 0 {
+            break;
+        }
+        eprintln!("{:?} / {cur}", xs);
+        rounds += cur;
     }
-    if xs == xs0 { None } else { Some(xs) }
-}
 
-fn run_until_stable_return_rounds<F>(
-    xs: Vec<u32>,
-    rounds_cap: usize,
-    round_f: F,
-) -> (Vec<u32>, usize)
-where
-    F: Fn(&[u32]) -> Option<Vec<u32>>,
-{
-    let seq = (1..=rounds_cap).scan(xs, |acc, _| {
-        if let Some(ys) = round_f(acc) {
-            *acc = ys;
-            Some(acc.clone())
-        } else {
-            None
-        }
-    });
-
-    let (i, ys) = seq.enumerate().last().unwrap();
-    (ys, i + 1)
+    rounds
 }
 
 fn main() {
-    let xs: Vec<u32> = stdin()
+    let xs: Vec<u64> = stdin()
         .lock()
         .lines()
         .map(|line| line.unwrap().parse().unwrap())
         .collect();
 
-    let cap: usize = 10_000_000;
-    let (ys, r0) = run_until_stable_return_rounds(xs, cap, ltr_additive);
-    assert!(r0 < cap);
-
-    let (_, r1) = run_until_stable_return_rounds(ys, cap, ltr_subtractive);
-    assert!(r1 < cap);
-
+    let (ys, r0) = ltr_additive_rounds_until_stable(xs);
+    eprintln!("{:?} / {r0}", ys);
+    let r1 = ltr_subtractive_rounds_until_stable(ys);
+    eprintln!("... / {r1}");
     println!("{}", r0 + r1);
 }
