fd12f6d1761b4d352d1ebae0420c2f8bed3f2c04
Ivan Krechetov <ikr@ikr.li>
2025-11-21 07:44:16 +0100

    Solve part 2

--- everybodycodes/2025/The_Song_of_Ducks_and_Dragons/11_The_Scout_Duck_Protocol/src/main.rs
+++ everybodycodes/2025/The_Song_of_Ducks_and_Dragons/11_The_Scout_Duck_Protocol/src/main.rs
@@ -1,7 +1,7 @@
 use std::io::{BufRead, stdin};
 
-fn ltr_additive(xs0: &[u8]) -> Option<Vec<u8>> {
-    let mut xs: Vec<u8> = xs0.to_vec();
+fn ltr_additive(xs0: &[u32]) -> Option<Vec<u32>> {
+    let mut xs: Vec<u32> = xs0.to_vec();
 
     for i in 1..xs.len() {
         if xs[i - 1] > xs[i] {
@@ -9,11 +9,10 @@ fn ltr_additive(xs0: &[u8]) -> Option<Vec<u8>> {
             xs[i] += 1;
         }
     }
-
     if xs == xs0 { None } else { Some(xs) }
 }
 
-fn ltr_subtractive(xs0: &[u8]) -> Option<Vec<u8>> {
+fn ltr_subtractive(xs0: &[u32]) -> Option<Vec<u32>> {
     let mut xs = xs0.to_vec();
 
     for i in 1..xs.len() {
@@ -22,13 +21,16 @@ fn ltr_subtractive(xs0: &[u8]) -> Option<Vec<u8>> {
             xs[i] -= 1;
         }
     }
-
     if xs == xs0 { None } else { Some(xs) }
 }
 
-fn run_until_stable_return_rounds<F>(xs: Vec<u8>, rounds_cap: usize, round_f: F) -> (Vec<u8>, usize)
+fn run_until_stable_return_rounds<F>(
+    xs: Vec<u32>,
+    rounds_cap: usize,
+    round_f: F,
+) -> (Vec<u32>, usize)
 where
-    F: Fn(&[u8]) -> Option<Vec<u8>>,
+    F: Fn(&[u32]) -> Option<Vec<u32>>,
 {
     let seq = (1..=rounds_cap).scan(xs, |acc, _| {
         if let Some(ys) = round_f(acc) {
@@ -43,21 +45,19 @@ where
     (ys, i + 1)
 }
 
-fn checksum(xs: Vec<u8>) -> usize {
-    xs.into_iter()
-        .enumerate()
-        .map(|(i, x)| (i + 1) * (x as usize))
-        .sum()
-}
-
 fn main() {
-    let xs: Vec<u8> = stdin()
+    let xs: Vec<u32> = stdin()
         .lock()
         .lines()
         .map(|line| line.unwrap().parse().unwrap())
         .collect();
 
-    let (ys, r0) = run_until_stable_return_rounds(xs, 10, ltr_additive);
-    let (zs, _) = run_until_stable_return_rounds(ys, 10 - r0, ltr_subtractive);
-    println!("{}", checksum(zs));
+    let cap: usize = 10_000_000;
+    let (ys, r0) = run_until_stable_return_rounds(xs, cap, ltr_additive);
+    assert!(r0 < cap);
+
+    let (_, r1) = run_until_stable_return_rounds(ys, cap, ltr_subtractive);
+    assert!(r1 < cap);
+
+    println!("{}", r0 + r1);
 }
