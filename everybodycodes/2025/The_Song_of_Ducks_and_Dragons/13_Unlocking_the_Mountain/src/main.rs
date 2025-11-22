d2349e6da6f900fbe7bf3027a443fdcbd0f8274a
Ivan Krechetov <ikr@ikr.li>
2025-11-22 16:49:08 +0100

    Solve part 2

--- everybodycodes/2025/The_Song_of_Ducks_and_Dragons/13_Unlocking_the_Mountain/src/main.rs
+++ everybodycodes/2025/The_Song_of_Ducks_and_Dragons/13_Unlocking_the_Mountain/src/main.rs
@@ -1,25 +1,32 @@
 use std::io::{BufRead, stdin};
 
+fn parse_range(s: &str) -> (u16, u16) {
+    let parts = s.split('-').collect::<Vec<_>>();
+    (parts[0].parse().unwrap(), parts[1].parse().unwrap())
+}
+
 fn main() {
-    let input_seq: Vec<u16> = stdin()
+    let input_ranges: Vec<(u16, u16)> = stdin()
         .lock()
         .lines()
-        .map(|line| line.unwrap().parse().unwrap())
+        .map(|line| parse_range(&line.unwrap()))
         .collect();
 
     let mut lhs: Vec<u16> = vec![];
     let mut xs: Vec<u16> = vec![1];
 
-    for (k, x) in input_seq.into_iter().enumerate() {
-        if k % 2 == 0 {
-            xs.push(x);
-        } else {
-            lhs.push(x);
+    for (k, (a, b)) in input_ranges.into_iter().enumerate() {
+        for x in a..=b {
+            if k % 2 == 0 {
+                xs.push(x);
+            } else {
+                lhs.push(x);
+            }
         }
     }
     lhs.reverse();
     xs.extend(lhs);
 
     let n = xs.len();
-    println!("{}", xs[2025 % n]);
+    println!("{}", xs[20252025 % n]);
 }
