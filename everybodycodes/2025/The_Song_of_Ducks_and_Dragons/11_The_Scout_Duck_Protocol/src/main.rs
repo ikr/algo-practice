82952a0d71bd5bac14e2a99ed7555cb905e9ca62
Ivan Krechetov <ikr@ikr.li>
2025-11-22 10:23:08 +0100

    Experimenting

--- everybodycodes/2025/The_Song_of_Ducks_and_Dragons/11_The_Scout_Duck_Protocol/src/main.rs
+++ everybodycodes/2025/The_Song_of_Ducks_and_Dragons/11_The_Scout_Duck_Protocol/src/main.rs
@@ -31,7 +31,7 @@ fn ltr_subtractive_rounds_until_stable(mut xs: Vec<u64>) -> u64 {
         let mut cur: u64 = 0;
 
         for i in 1..xs.len() {
-            if xs[i] > xs[i - 1] {
+            if xs[i - 1] < xs[i] {
                 let delta = (xs[i] - xs[i - 1]).div_ceil(2);
                 cur = cur.max(delta);
                 xs[i - 1] += delta;
@@ -42,11 +42,51 @@ fn ltr_subtractive_rounds_until_stable(mut xs: Vec<u64>) -> u64 {
         if cur == 0 {
             break;
         }
-        eprintln!("{:?} / {cur}", xs);
         rounds += cur;
     }
 
     rounds
+}
+
+fn ltr_additive(xs0: &[u64]) -> Option<Vec<u64>> {
+    let mut xs: Vec<u64> = xs0.to_vec();
+
+    for i in 1..xs.len() {
+        if xs[i - 1] > xs[i] {
+            xs[i - 1] -= 1;
+            xs[i] += 1;
+        }
+    }
+    if xs == xs0 { None } else { Some(xs) }
+}
+
+fn ltr_subtractive(xs0: &[u64]) -> Option<Vec<u64>> {
+    let mut xs = xs0.to_vec();
+
+    for i in 1..xs.len() {
+        if xs[i - 1] < xs[i] {
+            xs[i - 1] += 1;
+            xs[i] -= 1;
+        }
+    }
+    if xs == xs0 { None } else { Some(xs) }
+}
+
+fn run_until_stable_return_rounds<F>(xs: Vec<u64>, rounds_cap: usize, round_f: F) -> (Vec<u64>, u64)
+where
+    F: Fn(&[u64]) -> Option<Vec<u64>>,
+{
+    let seq = (1..=rounds_cap).scan(xs, |acc, _| {
+        if let Some(ys) = round_f(acc) {
+            *acc = ys;
+            Some(acc.clone())
+        } else {
+            None
+        }
+    });
+
+    let (i, ys) = seq.enumerate().last().unwrap();
+    (ys, (i + 1) as u64)
 }
 
 fn main() {
@@ -56,8 +96,9 @@ fn main() {
         .map(|line| line.unwrap().parse().unwrap())
         .collect();
 
-    let (ys, r0) = ltr_additive_rounds_until_stable(xs);
+    let (ys, r0) = run_until_stable_return_rounds(xs, 10_000_000, ltr_additive);
     eprintln!("{:?} / {r0}", ys);
+    //let (_, r1) = run_until_stable_return_rounds(ys, 10_000_000, ltr_subtractive);
     let r1 = ltr_subtractive_rounds_until_stable(ys);
     eprintln!("... / {r1}");
     println!("{}", r0 + r1);
