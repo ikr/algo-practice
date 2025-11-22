f41b139c75a4a856e9e50e75ef7bb9f3713d8f23
Ivan Krechetov <ikr@ikr.li>
2025-11-22 15:34:00 +0100

    Solve part 3

--- everybodycodes/2025/The_Song_of_Ducks_and_Dragons/12_One_Spark_to_Burn_Them_All/src/main.rs
+++ everybodycodes/2025/The_Song_of_Ducks_and_Dragons/12_One_Spark_to_Burn_Them_All/src/main.rs
@@ -1,23 +1,26 @@
+use itertools::Itertools;
 use std::{
     collections::VecDeque,
     io::{BufRead, stdin},
 };
 
-fn main() {
-    let grid: Vec<Vec<u8>> = stdin()
-        .lock()
-        .lines()
-        .map(|line| line.unwrap().bytes().map(|x| x - b'0').collect())
-        .collect();
+fn true_count(xss: &[Vec<bool>]) -> usize {
+    xss.iter()
+        .map(|row| row.iter().filter(|x| **x).count())
+        .sum()
+}
 
+fn explosion_yield(
+    grid: &[Vec<u8>],
+    exploded: &mut [Vec<bool>],
+    first_barrel: (usize, usize),
+) -> usize {
     let h = grid.len();
     let w = grid[0].len();
+    let baseline = true_count(exploded);
+    exploded[first_barrel.0][first_barrel.1] = true;
+    let mut q: VecDeque<(usize, usize)> = VecDeque::from([first_barrel]);
 
-    let mut exploded: Vec<Vec<bool>> = vec![vec![false; w]; h];
-    exploded[0][0] = true;
-    exploded[h - 1][w - 1] = true;
-
-    let mut q: VecDeque<(usize, usize)> = VecDeque::from([(0, 0), (h - 1, w - 1)]);
     while let Some((i, j)) = q.pop_front() {
         if i != 0 && !exploded[i - 1][j] && grid[i - 1][j] <= grid[i][j] {
             exploded[i - 1][j] = true;
@@ -37,9 +40,31 @@ fn main() {
         }
     }
 
-    let result: usize = exploded
-        .into_iter()
-        .map(|row| row.into_iter().filter(|x| *x).count())
-        .sum();
-    eprintln!("{result}");
+    true_count(exploded) - baseline
+}
+
+fn main() {
+    let grid: Vec<Vec<u8>> = stdin()
+        .lock()
+        .lines()
+        .map(|line| line.unwrap().bytes().map(|x| x - b'0').collect())
+        .collect();
+
+    let h = grid.len();
+    let w = grid[0].len();
+    let mut exploded: Vec<Vec<bool>> = vec![vec![false; w]; h];
+
+    for _ in 0..3 {
+        let first = (0..h)
+            .cartesian_product(0..w)
+            .max_by_key(|ij| {
+                let mut tmp = exploded.clone();
+                explosion_yield(&grid, &mut tmp, *ij)
+            })
+            .unwrap();
+
+        explosion_yield(&grid, &mut exploded, first);
+    }
+
+    println!("{}", true_count(&exploded));
 }
