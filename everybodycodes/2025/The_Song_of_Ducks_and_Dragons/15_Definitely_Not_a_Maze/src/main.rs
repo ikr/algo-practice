c82e3c59cd53b9c1836dd4f548e307ab011e2a96
Ivan Krechetov <ikr@ikr.li>
2025-11-24 16:46:06 +0100

    Start switching to a new model

--- everybodycodes/2025/The_Song_of_Ducks_and_Dragons/15_Definitely_Not_a_Maze/src/main.rs
+++ everybodycodes/2025/The_Song_of_Ducks_and_Dragons/15_Definitely_Not_a_Maze/src/main.rs
@@ -1,12 +1,9 @@
-use std::{
-    collections::{HashMap, HashSet, VecDeque},
-    io::Read,
-};
+use std::io::Read;
 
 #[derive(Clone, Copy, Debug)]
 enum Instr {
-    L(usize),
-    R(usize),
+    L(i32),
+    R(i32),
 }
 
 impl Instr {
@@ -22,11 +19,41 @@ impl Instr {
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 struct Crd(i32, i32);
 
+impl Crd {
+    fn scale_by(self, k: i32) -> Self {
+        Self(k * self.0, k * self.1)
+    }
+
+    fn dot(self, p: Self) -> i32 {
+        self.0 * p.0 + self.1 * p.1
+    }
+
+    fn cross(self, p: Self) -> i32 {
+        self.0 * p.1 - self.1 * p.0
+    }
+
+    fn cross_ab(self, a: Self, b: Self) -> i32 {
+        (a - self).cross(b - self)
+    }
+
+    fn on_segment(s: Self, e: Self, p: Self) -> bool {
+        p.cross_ab(s, e) == 0 && (s - p).dot(e - p) <= 0
+    }
+}
+
 impl std::ops::Add<Crd> for Crd {
     type Output = Crd;
 
     fn add(self, o: Crd) -> Crd {
         Crd(self.0 + o.0, self.1 + o.1)
+    }
+}
+
+impl std::ops::Sub<Crd> for Crd {
+    type Output = Crd;
+
+    fn sub(self, o: Crd) -> Crd {
+        Crd(self.0 - o.0, self.1 - o.1)
     }
 }
 
@@ -71,40 +98,24 @@ impl Dir {
     }
 }
 
-fn shortest_path_length(walls: &HashSet<Crd>, start: Crd, finish: Crd) -> usize {
-    let mut dist: HashMap<Crd, usize> = HashMap::from([(start, 0)]);
-    let mut q: VecDeque<Crd> = VecDeque::from([start]);
-
-    while let Some(u) = q.pop_front() {
-        for dir in Dir::all() {
-            let du = dist.get(&u).unwrap();
-            let v = u + dir.delta();
-
-            if v == finish {
-                return du + 1;
-            }
-
-            if !dist.contains_key(&v) && !walls.contains(&v) {
-                dist.insert(v, du + 1);
-                q.push_back(v);
-            }
-        }
-    }
-
-    unreachable!()
+fn shortest_path_length(wall_segments: Vec<(Crd, Crd)>, start: Crd, finish: Crd) -> u32 {
+    eprintln!("{:?} {:?} → {:?}", wall_segments, start, finish);
+    todo!()
 }
 
 fn main() {
     let mut buf = String::new();
     std::io::stdin().read_to_string(&mut buf).unwrap();
     let instructions: Vec<Instr> = buf.trim().split(',').map(Instr::decode).collect();
+    let instructions_count = instructions.len();
 
     let start = Crd(0, 0);
-    let mut walls: HashSet<Crd> = HashSet::new();
+    let mut finish = start;
+    let mut wall_segments: Vec<(Crd, Crd)> = vec![];
     let mut crd = Crd(0, 0);
     let mut dir = Dir::N;
 
-    for instr in instructions {
+    for (i, instr) in instructions.into_iter().enumerate() {
         let k = match instr {
             Instr::L(kk) => {
                 dir = dir.turn_left();
@@ -116,13 +127,15 @@ fn main() {
             }
         };
 
-        for _ in 0..k {
-            crd = crd + dir.delta();
-            walls.insert(crd);
-        }
+        let next_crd = crd
+            + dir
+                .delta()
+                .scale_by(k - if i == instructions_count - 1 { 1 } else { 0 });
+
+        finish = crd + dir.delta().scale_by(k);
+        wall_segments.push((crd, next_crd));
+        crd = next_crd;
     }
 
-    walls.remove(&crd);
-    let finish = crd;
-    println!("{}", shortest_path_length(&walls, start, finish));
+    println!("{}", shortest_path_length(wall_segments, start, finish));
 }
