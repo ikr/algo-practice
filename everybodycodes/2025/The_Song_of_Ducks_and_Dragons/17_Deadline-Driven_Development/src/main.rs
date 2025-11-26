a214fef0d29d2b4d9d5ee81935c10a7487b87a8e
Ivan Krechetov <ikr@ikr.li>
2025-11-26 18:21:32 +0100

    WIP

--- everybodycodes/2025/The_Song_of_Ducks_and_Dragons/17_Deadline-Driven_Development/src/main.rs
+++ everybodycodes/2025/The_Song_of_Ducks_and_Dragons/17_Deadline-Driven_Development/src/main.rs
@@ -27,18 +27,16 @@ fn main() {
 
     let h = grid.len();
     let w = grid[0].len();
+    eprintln!("{h} x {w}");
     let (zi, zj) = crd_of_x(&grid, 0);
 
-    let result: u16 = (0..h)
-        .cartesian_product(0..w)
-        .filter_map(|(i, j)| {
-            if zi.abs_diff(i).pow(2) + zj.abs_diff(j).pow(2) <= R * R {
-                Some(grid[i][j])
-            } else {
-                None
-            }
-        })
-        .sum();
+    let mut radius: Vec<Vec<usize>> = vec![vec![0; w]; h];
 
-    println!("{result}");
+    for i in 0..h {
+        for j in 0..w {
+            radius[i][j] = (zi.abs_diff(i).pow(2) + zj.abs_diff(j).pow(2)).isqrt();
+        }
+    }
+
+    eprintln!("{:?}", radius);
 }
