82d7f002a4b01f8e05b47dc0106414f4c90726ea
Ivan Krechetov <ikr@ikr.li>
2025-11-26 17:25:44 +0100

    Solve part 3

--- everybodycodes/2025/The_Song_of_Ducks_and_Dragons/16_Harmonics_of_Stone/src/main.rs
+++ everybodycodes/2025/The_Song_of_Ducks_and_Dragons/16_Harmonics_of_Stone/src/main.rs
@@ -1,5 +1,41 @@
 use std::io::Read;
 
+const BLOCKS_SUPPLY: usize = 202520252025000;
+
+fn is_wall_of_length_k_possible(spell: &[usize], num_blocks: usize, k: usize) -> bool {
+    let mut r = num_blocks;
+
+    for &d in spell {
+        let layer = k / d;
+
+        if r < layer {
+            return false;
+        }
+
+        r -= layer;
+    }
+
+    true
+}
+
+fn max_wall_length(spell: Vec<usize>) -> usize {
+    let mut lo: usize = 1;
+    assert!(is_wall_of_length_k_possible(&spell, BLOCKS_SUPPLY, lo));
+    let mut hi: usize = BLOCKS_SUPPLY;
+    assert!(!is_wall_of_length_k_possible(&spell, BLOCKS_SUPPLY, hi));
+
+    while lo + 1 != hi {
+        let mid = lo + (hi - lo) / 2;
+        if is_wall_of_length_k_possible(&spell, BLOCKS_SUPPLY, mid) {
+            lo = mid;
+        } else {
+            hi = mid;
+        }
+    }
+
+    lo
+}
+
 fn main() {
     let mut buf: String = String::new();
     std::io::stdin().read_to_string(&mut buf).unwrap();
@@ -18,5 +54,30 @@ fn main() {
         }
     }
 
-    eprintln!("{:?}", spell);
+    println!("{}", max_wall_length(spell));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_wall_of_length_k_possible() {
+        let spell = [1, 2, 3, 5, 9];
+        for (b, k, expected) in [
+            (1, 1, true),
+            (1, 2, false),
+            (10, 5, true),
+            (10, 6, false),
+            (100, 47, true),
+            (202520252025000, 94439495762954, true),
+        ] {
+            assert_eq!(
+                is_wall_of_length_k_possible(&spell, b, k),
+                expected,
+                "Expected {expected} on {:?}",
+                (b, k)
+            );
+        }
+    }
 }
