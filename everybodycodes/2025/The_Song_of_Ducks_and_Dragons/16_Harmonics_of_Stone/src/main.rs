ff07f6035eb07df86573a1b9ccd47faf741defaa
Ivan Krechetov <ikr@ikr.li>
2025-11-26 15:51:06 +0100

    Solve part 2

--- everybodycodes/2025/The_Song_of_Ducks_and_Dragons/16_Harmonics_of_Stone/src/main.rs
+++ everybodycodes/2025/The_Song_of_Ducks_and_Dragons/16_Harmonics_of_Stone/src/main.rs
@@ -1,11 +1,22 @@
 use std::io::Read;
 
-const W: usize = 90;
-
 fn main() {
     let mut buf: String = String::new();
     std::io::stdin().read_to_string(&mut buf).unwrap();
-    let xs: Vec<usize> = buf.trim().split(',').map(|s| s.parse().unwrap()).collect();
-    let result: usize = xs.into_iter().map(|x| W / x).sum();
+
+    let mut xs: Vec<u8> = buf.trim().split(',').map(|s| s.parse().unwrap()).collect();
+    let n = xs.len();
+    let mut result: usize = 1;
+
+    while let Some(i0) = xs.iter().position(|&x| x != 0) {
+        let d = i0 + 1;
+        result *= d;
+
+        for i in (i0..n).step_by(d) {
+            assert_ne!(xs[i], 0);
+            xs[i] -= 1;
+        }
+    }
+
     println!("{result}");
 }
