d5021dcb4966f3fc0cf5aaf38353be7f6968f743
Ivan Krechetov <ikr@ikr.li>
2025-11-26 18:36:05 +0100

    Solve part 2

--- everybodycodes/2025/The_Song_of_Ducks_and_Dragons/17_Deadline-Driven_Development/src/main.rs
+++ everybodycodes/2025/The_Song_of_Ducks_and_Dragons/17_Deadline-Driven_Development/src/main.rs
@@ -1,9 +1,7 @@
 use itertools::Itertools;
 use std::io::{BufRead, stdin};
 
-const R: usize = 10;
-
-fn crd_of_x(grid: &[Vec<u16>], x: u16) -> (usize, usize) {
+fn crd_of_x(grid: &[Vec<u64>], x: u64) -> (usize, usize) {
     let h = grid.len();
     let w = grid[0].len();
 
@@ -14,13 +12,13 @@ fn crd_of_x(grid: &[Vec<u16>], x: u16) -> (usize, usize) {
 }
 
 fn main() {
-    let grid: Vec<Vec<u16>> = stdin()
+    let grid: Vec<Vec<u64>> = stdin()
         .lock()
         .lines()
         .map(|line| {
             line.unwrap()
                 .bytes()
-                .map(|x| if x == b'@' { 0 } else { (x - b'0') as u16 })
+                .map(|x| if x == b'@' { 0 } else { (x - b'0') as u64 })
                 .collect()
         })
         .collect();
@@ -30,13 +28,32 @@ fn main() {
     eprintln!("{h} x {w}");
     let (zi, zj) = crd_of_x(&grid, 0);
 
+    let mut damage_by_radius: Vec<u64> = vec![0; (h.pow(2) + w.pow(2)).isqrt() + 2];
     let mut radius: Vec<Vec<usize>> = vec![vec![0; w]; h];
 
-    for i in 0..h {
-        for j in 0..w {
-            radius[i][j] = (zi.abs_diff(i).pow(2) + zj.abs_diff(j).pow(2)).isqrt();
+    for (i, row) in grid.into_iter().enumerate() {
+        for (j, cell) in row.into_iter().enumerate() {
+            let r2 = zi.abs_diff(i).pow(2) + zj.abs_diff(j).pow(2);
+
+            let r = if r2.isqrt().pow(2) == r2 {
+                r2.isqrt()
+            } else {
+                r2.isqrt() + 1
+            };
+
+            radius[i][j] = r;
+            damage_by_radius[r] += cell;
         }
     }
 
     eprintln!("{:?}", radius);
+    eprintln!("{:?}", damage_by_radius);
+
+    let (r, damage) = damage_by_radius
+        .into_iter()
+        .enumerate()
+        .max_by_key(|&(_, d)| d)
+        .unwrap();
+
+    println!("{}", (r as u64) * damage);
 }
