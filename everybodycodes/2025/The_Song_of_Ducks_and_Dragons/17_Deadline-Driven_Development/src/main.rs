9aaa3a6b0ab664867e466aba86c527e7857e27de
Ivan Krechetov <ikr@ikr.li>
2025-11-27 08:01:17 +0100

    Start working on part 3

--- everybodycodes/2025/The_Song_of_Ducks_and_Dragons/17_Deadline-Driven_Development/src/main.rs
+++ everybodycodes/2025/The_Song_of_Ducks_and_Dragons/17_Deadline-Driven_Development/src/main.rs
@@ -1,55 +1,88 @@
 use itertools::Itertools;
 use std::io::{BufRead, stdin};
 
-fn crd_of_x(grid: &[Vec<u64>], x: u64) -> (usize, usize) {
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+struct Crd(i32, i32);
+
+impl Crd {
+    fn new(i: usize, j: usize) -> Self {
+        Self(i as i32, j as i32)
+    }
+
+    fn get_in(self, grid: &[Vec<i32>]) -> i32 {
+        grid[self.0 as usize][self.1 as usize]
+    }
+
+    fn dist2(self, p: Self) -> u32 {
+        self.0.abs_diff(p.0).pow(2) + self.1.abs_diff(p.1).pow(2)
+    }
+}
+
+impl std::ops::Add<Crd> for Crd {
+    type Output = Crd;
+
+    fn add(self, o: Crd) -> Crd {
+        Crd(self.0 + o.0, self.1 + o.1)
+    }
+}
+
+fn crds_of_zeros(grid: &[Vec<i32>]) -> [Crd; 2] {
     let h = grid.len();
     let w = grid[0].len();
 
     (0..h)
         .cartesian_product(0..w)
-        .find(|&(i, j)| grid[i][j] == x)
+        .filter_map(|(i, j)| {
+            if grid[i][j] == 0 {
+                Some(Crd::new(i, j))
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>()
+        .try_into()
         .unwrap()
 }
 
-fn main() {
-    let grid: Vec<Vec<u64>> = stdin()
-        .lock()
-        .lines()
-        .map(|line| {
-            line.unwrap()
-                .bytes()
-                .map(|x| if x == b'@' { 0 } else { (x - b'0') as u64 })
-                .collect()
-        })
-        .collect();
+fn grid_cell_value(x: u8) -> i32 {
+    if x == b'@' || x == b'S' {
+        0
+    } else {
+        (x - b'0') as i32
+    }
+}
 
-    let h = grid.len();
-    let w = grid[0].len();
-    let (zi, zj) = crd_of_x(&grid, 0);
+fn radius_table_for(h: usize, w: usize, epicenter_crd: Crd) -> Vec<Vec<i32>> {
+    let mut result: Vec<Vec<i32>> = vec![vec![0; w]; h];
 
-    let mut damage_by_radius: Vec<u64> = vec![0; (h.pow(2) + w.pow(2)).isqrt() + 2];
-    let mut radius: Vec<Vec<usize>> = vec![vec![0; w]; h];
+    for (i, row) in result.iter_mut().enumerate() {
+        for (j, cell) in row.iter_mut().enumerate() {
+            let r2 = epicenter_crd.dist2(Crd::new(i, j));
 
-    for (i, row) in grid.into_iter().enumerate() {
-        for (j, cell) in row.into_iter().enumerate() {
-            let r2 = zi.abs_diff(i).pow(2) + zj.abs_diff(j).pow(2);
-
-            let r = if r2.isqrt().pow(2) == r2 {
+            *cell = (if r2.isqrt().pow(2) == r2 {
                 r2.isqrt()
             } else {
                 r2.isqrt() + 1
-            };
-
-            radius[i][j] = r;
-            damage_by_radius[r] += cell;
+            }) as i32
         }
     }
 
-    let (r, damage) = damage_by_radius
-        .into_iter()
-        .enumerate()
-        .max_by_key(|&(_, d)| d)
-        .unwrap();
+    result
+}
 
-    println!("{}", (r as u64) * damage);
+fn main() {
+    let grid: Vec<Vec<i32>> = stdin()
+        .lock()
+        .lines()
+        .map(|line| line.unwrap().bytes().map(grid_cell_value).collect())
+        .collect();
+
+    eprintln!("{:?}", grid);
+
+    let h = grid.len();
+    let w = grid[0].len();
+    eprintln!("{h} x {w}");
+    let [_, epicenter_crd] = crds_of_zeros(&grid);
+
+    eprintln!("{:?}", radius_table_for(h, w, epicenter_crd));
 }
