9b44a49d01e5e45c200120543386d65207ed8bf0
Ivan Krechetov <ikr@ikr.li>
2025-11-30 19:57:29 +0100

    Solve part 1

--- everybodycodes/2025/The_Song_of_Ducks_and_Dragons/19_Flappy_Quack/src/main.rs
+++ everybodycodes/2025/The_Song_of_Ducks_and_Dragons/19_Flappy_Quack/src/main.rs
@@ -1,17 +1,85 @@
-use std::io::{BufRead, stdin};
+use std::{
+    collections::VecDeque,
+    io::{BufRead, stdin},
+};
 
-fn decode_triplet(s: &str) -> (i16, i16, i16) {
-    let parts: Vec<i16> = s.split(',').map(|x| x.parse().unwrap()).collect();
+fn decode_triplet(s: &str) -> (u16, u16, u16) {
+    let parts: Vec<u16> = s.split(',').map(|x| x.parse().unwrap()).collect();
     (parts[0], parts[1], parts[2])
+}
+
+fn walls_and_windows_from_triplets(triplests: Vec<(u16, u16, u16)>) -> (Vec<u16>, Vec<(u16, u16)>) {
+    let mut walls = vec![];
+    let mut windows = vec![];
+
+    for (col, first_row, window_height) in triplests {
+        walls.push(col);
+        windows.push((first_row, first_row + window_height - 1));
+    }
+
+    (walls, windows)
+}
+
+fn is_passable(walls: &[u16], windows: &[(u16, u16)], crd: (u16, u16)) -> bool {
+    let (row, col) = crd;
+    if let Some(i) = walls.iter().position(|&wall_col| wall_col == col) {
+        let (row_lo, row_hi) = windows[i];
+        row_lo <= row && row <= row_hi
+    } else {
+        true
+    }
+}
+
+fn adjacent(walls: &[u16], windows: &[(u16, u16)], crd: (u16, u16)) -> Vec<((u16, u16), u16)> {
+    assert!(is_passable(walls, windows, crd));
+    let (row, col) = crd;
+    let mut result = vec![];
+
+    if row != 0 && is_passable(walls, windows, (row - 1, col + 1)) {
+        result.push(((row - 1, col + 1), 0))
+    }
+
+    if is_passable(walls, windows, (row + 1, col + 1)) {
+        result.push(((row + 1, col + 1), 1))
+    }
+
+    result
+}
+
+fn is_finish_line(walls: &[u16], windows: &[(u16, u16)], crd: (u16, u16)) -> bool {
+    assert!(is_passable(walls, windows, crd));
+    let n = walls.len();
+    let (row, col) = crd;
+    let (row_lo, row_hi) = windows[n - 1];
+    col == walls[n - 1] && row_lo <= row && row <= row_hi
 }
 
 fn main() {
     let lines: Vec<String> = stdin().lock().lines().map(|line| line.unwrap()).collect();
 
-    let triplests: Vec<(i16, i16, i16)> = lines
+    let triplests: Vec<(u16, u16, u16)> = lines
         .into_iter()
         .map(|line| decode_triplet(&line))
         .collect();
 
-    eprintln!("{:?}", triplests);
+    let (walls, windows) = walls_and_windows_from_triplets(triplests);
+    let mut q: VecDeque<((u16, u16), u16)> = VecDeque::from([((0, 0), 0)]);
+    let mut result = u16::MAX;
+
+    while let Some((u, distance)) = q.pop_front() {
+        if is_finish_line(&walls, &windows, u) {
+            result = result.min(distance);
+        } else {
+            for (v, w) in adjacent(&walls, &windows, u) {
+                if w == 1 {
+                    q.push_back((v, distance + 1))
+                } else {
+                    assert_eq!(w, 0);
+                    q.push_front((v, distance))
+                }
+            }
+        }
+    }
+
+    println!("{result}");
 }
