cbb1333e72f8c18926b73a26dcb328f0184f016e
Ivan Krechetov <ikr@ikr.li>
2025-12-03 07:11:24 +0100

    Solve part 2

--- adventofcode/2025/03_Abc/src/main.rs
+++ adventofcode/2025/03_Abc/src/main.rs
@@ -1,37 +1,31 @@
-use itertools::Itertools;
 use std::io::{BufRead, stdin};
 
-fn number(ds: Vec<u64>) -> u64 {
-    if ds.is_empty() {
-        0
-    } else {
-        let mut ans = 0;
-        let mut mul = 1;
+#[allow(clippy::needless_range_loop)]
+fn max_joltage(xs: Vec<u64>) -> u64 {
+    let n = xs.len();
+    let m: usize = 12;
 
-        for d in ds.into_iter().rev() {
-            ans += d * mul;
-            mul *= 10;
+    // tab[i][l] is max joltage ending with the digit xs[i] and having the length l
+    let mut tab: Vec<Vec<u64>> = vec![vec![0; m + 1]; n];
+
+    for i in 0..n {
+        tab[i][1] = xs[i];
+    }
+
+    for l in 1..=m {
+        for i in 1..n {
+            for j in 0..i {
+                if tab[j][l - 1] != 0 {
+                    tab[i][l] = tab[i][l].max(tab[j][l - 1] * 10 + xs[i]);
+                }
+            }
         }
-
-        ans
-    }
-}
-
-fn max_seq_of_len_k(xs: Vec<u64>, k: uzize) -> Vec<u64> {
-    let mut result: Vec<u64> = vec![];
-    let mut i0: usize = 0;
-
-    while result.len() < k {
-        let i = xs[..k - result.len()]
-            .iter()
-            .enumerate()
-            .max_by_key(|(_, x)| x)
-            .unwrap()
-            .0;
-        result.push(xs[i]);
-        i0 = i + 1;
     }
 
+    let mut result = 0;
+    for i in 0..n {
+        result = result.max(tab[i][12]);
+    }
     result
 }
 
@@ -43,24 +37,5 @@ fn main() {
         .map(|s| s.bytes().map(|x| (x - b'0') as u64).collect())
         .collect();
 
-    let n = grid[0].len();
-
-    let mut result = 0;
-    for row in grid {
-        let mut cur = 0;
-
-        let i: usize = row[..n - 12]
-            .iter()
-            .enumerate()
-            .max_by_key(|&(j, _)| j)
-            .unwrap()
-            .0;
-
-        for xs in row[i..].iter().cloned().combinations(12) {
-            cur = cur.max(number(xs));
-        }
-
-        result += cur;
-    }
-    println!("{result}");
+    println!("{}", grid.into_iter().map(max_joltage).sum::<u64>());
 }
