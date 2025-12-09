da8509e499ece0573d1c5d9d23c1e088c62a9d64
Ivan Krechetov <ikr@ikr.li>
2025-12-09 06:51:00 +0100

    wip

--- adventofcode/2025/09_Movie_Theater/src/main.rs
+++ adventofcode/2025/09_Movie_Theater/src/main.rs
@@ -1,12 +1,19 @@
 use itertools::Itertools;
-use std::io::{BufRead, stdin};
+use std::{
+    collections::{HashSet, VecDeque},
+    io::{BufRead, stdin},
+};
 
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 struct Crd(i64, i64);
 
 impl Crd {
-    fn components(self) -> Vec<i64> {
-        vec![self.0, self.1]
+    fn sign(self) -> Self {
+        Self(sign(self.0), sign(self.1))
+    }
+
+    fn step_direction_to(self, o: Crd) -> Crd {
+        (o - self).sign()
     }
 
     fn area(self) -> i64 {
@@ -24,6 +31,14 @@ impl Crd {
     }
 }
 
+impl std::ops::Add<Crd> for Crd {
+    type Output = Crd;
+
+    fn add(self, o: Crd) -> Crd {
+        Crd(self.0 + o.0, self.1 + o.1)
+    }
+}
+
 impl std::ops::Sub<Crd> for Crd {
     type Output = Crd;
 
@@ -32,15 +47,76 @@ impl std::ops::Sub<Crd> for Crd {
     }
 }
 
+fn sign(x: i64) -> i64 {
+    if x < 0 {
+        -1
+    } else if x == 0 {
+        0
+    } else {
+        1
+    }
+}
+
+fn line_a_to_b(a: Crd, b: Crd) -> Vec<Crd> {
+    let delta = a.step_direction_to(b);
+    let mut result = vec![a];
+    let mut cur = a;
+
+    while cur != b {
+        cur = cur + delta;
+        result.push(cur);
+    }
+
+    result
+}
+
+fn perimeter(corners: &[Crd]) -> HashSet<Crd> {
+    corners
+        .iter()
+        .circular_tuple_windows()
+        .flat_map(|(&a, &b)| line_a_to_b(a, b))
+        .collect()
+}
+
+fn flood_fill_finite(perimeter: &HashSet<Crd>, lim: i64, start: Crd) -> Option<HashSet<Crd>> {
+    assert!(!perimeter.contains(&start));
+    let mut result = perimeter.clone();
+    result.insert(start);
+    let mut q: VecDeque<Crd> = VecDeque::from([start]);
+
+    while let Some(u) = q.pop_front() {
+        for delta in [Crd(-1, 0), Crd(0, 1), Crd(1, 0), Crd(0, -1)] {
+            let v = u + delta;
+
+            if !result.contains(&v) {
+                q.push_back(v);
+                result.insert(v);
+                if result.len() as i64 > lim {
+                    return None;
+                }
+            }
+        }
+    }
+
+    Some(result)
+}
+
 fn main() {
     let lines: Vec<String> = stdin().lock().lines().map(|line| line.unwrap()).collect();
-    let ps: Vec<Crd> = lines.into_iter().map(|s| Crd::decode(&s)).collect();
+    let corners: Vec<Crd> = lines.into_iter().map(|s| Crd::decode(&s)).collect();
 
-    let result = ps
-        .into_iter()
+    let lim = corners
+        .iter()
         .tuple_combinations()
-        .map(|(u, v)| (u - v).area())
+        .map(|(&u, &v)| (u - v).area())
         .max()
         .unwrap();
-    println!("{result}");
+
+    let per = perimeter(&corners);
+
+    for start_delta in [Crd(-1, -1), Crd(-1, 1), Crd(1, 1), Crd(1, -1)] {
+        if let Some(body) = flood_fill_finite(&per, lim, corners[0] + start_delta) {
+            eprintln!("{}", body.len());
+        }
+    }
 }
