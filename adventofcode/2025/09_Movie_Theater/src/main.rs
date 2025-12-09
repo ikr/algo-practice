abbafb4022568bd6300ce78338855444ccb5a3fc
Ivan Krechetov <ikr@ikr.li>
2025-12-09 13:37:47 +0100

    Check that *some* points on the rect sides lie inside of the big poly
    Result: nothing at all gets found

--- adventofcode/2025/09_Movie_Theater/src/main.rs
+++ adventofcode/2025/09_Movie_Theater/src/main.rs
@@ -5,12 +5,35 @@ fn iof(x: bool) -> i64 {
     if x { 1 } else { 0 }
 }
 
+fn sign(x: i64) -> i64 {
+    if x < 0 {
+        -1
+    } else if x == 0 {
+        0
+    } else {
+        1
+    }
+}
+
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 struct Crd(i64, i64);
 
 impl Crd {
     fn area(self) -> i64 {
         (self.0.abs() + 1) * (self.1.abs() + 1)
+    }
+
+    fn mul_by(self, k: i64) -> Self {
+        Self(k * self.0, k * self.1)
+    }
+
+    fn div_by(self, k: i64) -> Self {
+        Self(self.0 / k, self.1 / k)
+    }
+
+    fn manhattan_dist(self, o: Crd) -> i64 {
+        let delta = o - self;
+        delta.0.abs() + delta.1.abs()
     }
 
     fn decode(s: &str) -> Self {
@@ -60,6 +83,41 @@ impl Crd {
         let Crd(d0, d1) = b - a;
         vec![a, a + Crd(d0, 0), b, a + Crd(0, d1)]
     }
+
+    fn sign(self) -> Self {
+        Self(sign(self.0), sign(self.1))
+    }
+
+    fn step_direction_to(self, o: Crd) -> Crd {
+        (o - self).sign()
+    }
+
+    fn trace_to(self, b: Crd) -> Vec<Crd> {
+        assert!(self.0 == b.0 || self.1 == b.1);
+        let delta = self.step_direction_to(b);
+        let dist = self.manhattan_dist(b);
+
+        if dist < 20 {
+            let mut result = vec![self];
+            let mut cur = self;
+
+            while cur != b {
+                cur = cur + delta;
+                result.push(cur);
+            }
+
+            result
+        } else {
+            vec![
+                self,
+                delta.mul_by(1 * dist).div_by(5),
+                delta.mul_by(2 * dist).div_by(5),
+                delta.mul_by(3 * dist).div_by(5),
+                delta.mul_by(4 * dist).div_by(5),
+                b,
+            ]
+        }
+    }
 }
 
 impl std::ops::Add<Crd> for Crd {
@@ -86,11 +144,19 @@ fn main() {
         .iter()
         .tuple_combinations()
         .filter_map(|(&a, &b)| {
+            if a.0 != b.0 && a.1 != b.1 {
+                eprintln!("Rect within {:?} - {:?}:", a, b);
+            }
+
             (a.0 != b.0
                 && a.1 != b.1
                 && Crd::derive_all_rect_corners_from_two_diag(a, b)
                     .into_iter()
-                    .all(|u| u.in_polygon(&corners)))
+                    .circular_tuple_windows()
+                    .all(|(p, q)| {
+                        eprintln!("{:?}", (p, q));
+                        p.trace_to(q).into_iter().all(|u| u.in_polygon(&corners))
+                    }))
             .then_some((b - a).area())
         })
         .max()
@@ -107,6 +173,21 @@ mod tests {
         assert_eq!(
             Crd::derive_all_rect_corners_from_two_diag(Crd(0, 0), Crd(3, 4)),
             vec![Crd(0, 0), Crd(3, 0), Crd(3, 4), Crd(0, 4)]
+        );
+    }
+
+    #[test]
+    fn test_line_to() {
+        assert_eq!(
+            Crd(0, 0).trace_to(Crd(0, 100)),
+            vec![
+                Crd(0, 0),
+                Crd(0, 20),
+                Crd(0, 40),
+                Crd(0, 60),
+                Crd(0, 80),
+                Crd(0, 100)
+            ]
         );
     }
 }
