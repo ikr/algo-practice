93eb71e006932cd47a82864ce33d92a9e1ab4a92
Ivan Krechetov <ikr@ikr.li>
2025-12-10 06:58:01 +0100

    First draft for the part 2; very slow

--- adventofcode/2025/10_Factory/src/main.rs
+++ adventofcode/2025/10_Factory/src/main.rs
@@ -5,71 +5,88 @@ use std::{
 
 #[derive(Clone, Debug)]
 struct Machine {
-    end_bits: u16,
-    toggles: Vec<u16>,
+    end_counts: Vec<u16>,
+    bump_indices: Vec<Vec<usize>>,
 }
 
 impl Machine {
-    fn decode_end_bits(s: &str) -> u16 {
-        s.strip_prefix('[')
-            .and_then(|s| s.strip_suffix(']'))
+    fn decode_end_counts(s: &str) -> Vec<u16> {
+        s.strip_prefix('{')
+            .and_then(|s| s.strip_suffix('}'))
             .unwrap()
-            .bytes()
-            .enumerate()
-            .filter_map(|(i, x)| if x == b'#' { Some(1 << i) } else { None })
-            .fold(0u16, |acc, x| acc | x)
+            .split(',')
+            .map(|sub| sub.parse().unwrap())
+            .collect()
     }
 
-    fn decode_button(s: &str) -> u16 {
-        let offsets: Vec<usize> = s
-            .strip_prefix('(')
+    fn decode_button(s: &str) -> Vec<usize> {
+        s.strip_prefix('(')
             .and_then(|s| s.strip_suffix(')'))
             .unwrap()
             .split(',')
             .map(|sub| sub.parse().unwrap())
-            .collect();
-
-        offsets
-            .into_iter()
-            .map(|i| 1u16 << i)
-            .fold(0u16, |acc, x| acc | x)
+            .collect()
     }
 
     fn decode(s: &str) -> Self {
-        let top_parts: Vec<_> = s.split(" {").collect();
-        let pre_parts: Vec<_> = top_parts[0].split(' ').collect();
-        let end_bits = Self::decode_end_bits(pre_parts[0]);
-        let toggles = pre_parts[1..]
+        let parts: Vec<_> = s.split(' ').collect();
+        let n = parts.len();
+
+        let end_counts = Self::decode_end_counts(parts[n - 1]);
+
+        let bump_indices = parts[1..n - 1]
             .iter()
             .map(|&s| Self::decode_button(s))
             .collect();
-        Self { end_bits, toggles }
+
+        Self {
+            end_counts,
+            bump_indices,
+        }
     }
 
-    fn adjacent(&self, u: u16) -> Vec<u16> {
-        self.toggles.iter().map(|x| u ^ x).collect()
+    fn initial_counts(&self) -> Vec<u16> {
+        vec![0; self.end_counts.len()]
+    }
+
+    fn adjacent(&self, u: &[u16]) -> Vec<Vec<u16>> {
+        self.bump_indices
+            .iter()
+            .map(|bi| {
+                let mut result: Vec<u16> = u.to_vec();
+                for i in bi {
+                    result[*i] += 1;
+                }
+                result
+            })
+            .collect()
     }
 
     fn min_presses(&self) -> usize {
-        let mut dist: HashMap<u16, usize> = HashMap::from([(0, 0)]);
-        let mut q: VecDeque<u16> = VecDeque::from([0]);
+        let mut dist: HashMap<Vec<u16>, usize> = HashMap::from([(self.initial_counts(), 0)]);
+        let mut q: VecDeque<Vec<u16>> = VecDeque::from([self.initial_counts()]);
 
         while let Some(u) = q.pop_front() {
-            for v in self.adjacent(u) {
+            for v in self.adjacent(&u) {
                 if !dist.contains_key(&v) {
-                    dist.insert(v, *dist.get(&u).unwrap() + 1);
+                    if v == self.end_counts {
+                        return *dist.get(&u).unwrap() + 1;
+                    }
+
+                    dist.insert(v.clone(), *dist.get(&u).unwrap() + 1);
                     q.push_back(v);
                 }
             }
         }
 
-        *dist.get(&self.end_bits).unwrap()
+        unreachable!()
     }
 }
 
 fn main() {
     let lines: Vec<String> = stdin().lock().lines().map(|line| line.unwrap()).collect();
     let machines: Vec<Machine> = lines.into_iter().map(|s| Machine::decode(&s)).collect();
+
     let min_presses: Vec<usize> = machines.into_iter().map(|m| m.min_presses()).collect();
     let result: usize = min_presses.into_iter().sum();
     println!("{result}");
