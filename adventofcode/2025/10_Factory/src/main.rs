0a0f7bce0b4d569593f662c88f9aea0f5f882328
Ivan Krechetov <ikr@ikr.li>
2025-12-10 16:58:56 +0100

    Now we eat a lot of memory

--- adventofcode/2025/10_Factory/src/main.rs
+++ adventofcode/2025/10_Factory/src/main.rs
@@ -1,7 +1,9 @@
 use std::{
-    collections::{HashMap, VecDeque},
+    collections::HashMap,
     io::{BufRead, stdin},
 };
+
+const INF: usize = 1_000_000_000;
 
 fn sub<T: std::ops::Sub<Output = T> + Copy>(a: Vec<T>, b: Vec<T>) -> Vec<T> {
     a.into_iter().zip(b).map(|(x, y)| x - y).collect()
@@ -9,12 +11,13 @@ fn sub<T: std::ops::Sub<Output = T> + Copy>(a: Vec<T>, b: Vec<T>) -> Vec<T> {
 
 #[derive(Clone, Debug)]
 struct Machine {
-    end_counts: Vec<i16>,
+    end_joltage: Vec<i16>,
     buttons: Vec<Vec<usize>>,
+    memo: HashMap<(usize, Vec<i16>), usize>,
 }
 
 impl Machine {
-    fn decode_end_counts(s: &str) -> Vec<i16> {
+    fn decode_end_joltage(s: &str) -> Vec<i16> {
         s.strip_prefix('{')
             .and_then(|s| s.strip_suffix('}'))
             .unwrap()
@@ -36,75 +39,72 @@ impl Machine {
         let parts: Vec<_> = s.split(' ').collect();
         let n = parts.len();
 
-        let end_counts = Self::decode_end_counts(parts[n - 1]);
+        let end_joltage = Self::decode_end_joltage(parts[n - 1]);
 
         let buttons = parts[1..n - 1]
             .iter()
             .map(|&s| Self::decode_button(s))
             .collect();
 
+        let memo = HashMap::new();
+
         Self {
-            end_counts,
+            end_joltage,
             buttons,
+            memo,
         }
     }
 
-    fn initial_counts(&self) -> Vec<i16> {
-        vec![0; self.end_counts.len()]
-    }
-
     fn button_bump(&self, button_index: usize) -> Vec<i16> {
-        let mut result = vec![0; self.end_counts.len()];
+        let mut result = vec![0; self.end_joltage.len()];
         for &i in &self.buttons[button_index] {
             result[i] = 1;
         }
         result
     }
 
-    fn adjacent(&self, u: &[i16]) -> Vec<Vec<i16>> {
-        self.buttons
-            .iter()
-            .filter_map(|bi| {
-                let mut result: Vec<i16> = u.to_vec();
-                for &i in bi {
-                    result[i] += 1;
+    fn recur(&mut self, num_buttons_used: usize, joltage: Vec<i16>) -> usize {
+        let key = (num_buttons_used, joltage.clone());
 
-                    if result[i] > self.end_counts[i] {
-                        return None;
-                    }
-                }
-                Some(result)
-            })
-            .collect()
+        if let Some(&cached) = self.memo.get(&key) {
+            cached
+        } else {
+            let value: usize = if joltage.iter().any(|&x| x < 0) {
+                INF
+            } else if joltage.iter().all(|&x| x == 0) {
+                0
+            } else if num_buttons_used == 0 {
+                INF
+            } else {
+                self.recur(num_buttons_used - 1, joltage.clone()).min({
+                    let bump = self.button_bump(num_buttons_used - 1);
+                    let sub = sub(joltage, bump);
+                    1 + self.recur(num_buttons_used, sub)
+                })
+            };
+
+            self.memo.insert(key, value);
+            value
+        }
     }
 
-    fn min_presses(&self) -> usize {
-        let mut dist: HashMap<Vec<i16>, usize> = HashMap::from([(self.initial_counts(), 0)]);
-        let mut q: VecDeque<Vec<i16>> = VecDeque::from([self.initial_counts()]);
-
-        while let Some(u) = q.pop_front() {
-            for v in self.adjacent(&u) {
-                if !dist.contains_key(&v) {
-                    if v == self.end_counts {
-                        eprint!(".");
-                        return *dist.get(&u).unwrap() + 1;
-                    }
-
-                    dist.insert(v.clone(), *dist.get(&u).unwrap() + 1);
-                    q.push_back(v);
-                }
-            }
-        }
-
-        unreachable!()
+    fn min_presses(&mut self) -> usize {
+        self.recur(self.buttons.len(), self.end_joltage.clone())
     }
 }
 
 fn main() {
     let lines: Vec<String> = stdin().lock().lines().map(|line| line.unwrap()).collect();
     let machines: Vec<Machine> = lines.into_iter().map(|s| Machine::decode(&s)).collect();
-    let min_presses: Vec<usize> = machines.into_iter().map(|m| m.min_presses()).collect();
+    let min_presses: Vec<usize> = machines
+        .into_iter()
+        .map(|mut m| {
+            eprint!(".");
+            m.min_presses()
+        })
+        .collect();
     eprintln!();
+
     let result: usize = min_presses.into_iter().sum();
     println!("{result}");
 }
