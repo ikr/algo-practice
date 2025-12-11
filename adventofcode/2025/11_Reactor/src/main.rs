34aa03b77e3747645a20109a94efa469f9a6ab4b
Ivan Krechetov <ikr@ikr.li>
2025-12-11 07:14:58 +0100

    Solve part 2

--- adventofcode/2025/11_Reactor/src/main.rs
+++ adventofcode/2025/11_Reactor/src/main.rs
@@ -63,16 +63,29 @@ fn main() {
 
     let seq = toposort(&g);
 
-    let mut tab: Vec<usize> = vec![0; g.len()];
-    tab[code_of("you")] = 1;
+    // tab[u][d][f] is the number of paths from "svr" to u which pass through "dac" iﬀ d is 1, and
+    // pass through "fft" iﬀ f is 1
+    let mut tab: Vec<Vec<Vec<usize>>> = vec![vec![vec![0; 2]; 2]; g.len()];
+    tab[code_of("svr")][0][0] = 1;
 
-    for i in seq {
-        for &j in &g[i] {
-            tab[j] += tab[i];
+    for u in seq {
+        for &v in &g[u] {
+            if v == code_of("dac") {
+                tab[v][1][0] += tab[u][0][0];
+                tab[v][1][1] += tab[u][0][1];
+            } else if v == code_of("fft") {
+                tab[v][0][1] += tab[u][0][0];
+                tab[v][1][1] += tab[u][1][0];
+            } else {
+                tab[v][0][0] += tab[u][0][0];
+                tab[v][1][0] += tab[u][1][0];
+                tab[v][0][1] += tab[u][0][1];
+                tab[v][1][1] += tab[u][1][1];
+            }
         }
     }
 
-    println!("{}", tab[code_of("out")]);
+    println!("{}", tab[code_of("out")][1][1]);
 }
 
 #[cfg(test)]
