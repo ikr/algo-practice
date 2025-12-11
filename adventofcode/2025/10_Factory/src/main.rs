4d7ea3d48ca57f9d5ce42b5db421c47fff3c707f
Ivan Krechetov <ikr@ikr.li>
2025-12-11 07:50:13 +0100

    Roll back

--- adventofcode/2025/10_Factory/src/main.rs
+++ adventofcode/2025/10_Factory/src/main.rs
@@ -1,35 +1,4 @@
-use pathfinding::prelude::astar;
 use std::io::{BufRead, stdin};
-
-fn heu_dist(mut a: Vec<i16>, mut b: Vec<i16>) -> u16 {
-    assert!(
-        a.iter().zip(b.iter()).all(|(x, y)| x <= y) || a.iter().zip(b.iter()).all(|(x, y)| x >= y)
-    );
-
-    if a.iter().zip(b.iter()).all(|(x, y)| x >= y) {
-        (a, b) = (b, a);
-    }
-
-    let mut result: u16 = 0;
-
-    while let Some(delta) = a
-        .iter()
-        .zip(b.iter())
-        .filter(|(x, y)| **x != **y)
-        .min_by_key(|(x, y)| **y - **x)
-        .map(|(x, y)| y - x)
-    {
-        assert!(delta > 0);
-        for i in 0..a.len() {
-            if a[i] != b[i] {
-                a[i] += delta;
-            }
-        }
-        result += delta as u16;
-    }
-
-    result
-}
 
 #[derive(Clone, Debug)]
 struct Machine {
@@ -95,14 +64,7 @@ impl Machine {
     }
 
     fn min_presses(&mut self) -> u16 {
-        astar(
-            &self.initial_joltage(),
-            |u| self.adjacent(u),
-            |u| heu_dist(u.to_vec(), self.end_joltage.clone()),
-            |u| *u == self.end_joltage,
-        )
-        .expect("Assumed path exists")
-        .1
+        todo!()
     }
 }
 
@@ -120,19 +82,4 @@ fn main() {
 
     let result: usize = min_presses.into_iter().sum();
     println!("{result}");
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_heu_dist() {
-        for (a, b, expected) in [
-            (vec![1], vec![1], 0),
-            (vec![3, 5, 4, 7], vec![1, 2, 2, 0], 7),
-        ] {
-            assert_eq!(heu_dist(a, b), expected);
-        }
-    }
 }
