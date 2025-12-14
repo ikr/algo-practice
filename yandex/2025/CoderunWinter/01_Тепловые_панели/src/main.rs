0fd2f43b3c60fbe4a19d89993010ab0603eace14
Ivan Krechetov <ikr@ikr.li>
2025-12-14 16:18:13 +0100

    AC

--- yandex/2025/CoderunWinter/01_Тепловые_панели/src/main.rs
+++ yandex/2025/CoderunWinter/01_Тепловые_панели/src/main.rs
@@ -1,8 +1,15 @@
 use std::io::Read;
 
+fn isqrt(x: i64) -> Option<i64> {
+    let fx = x as f64;
+    let q = fx.sqrt().round();
+    let i = q as i64;
+    if i * i == x { Some(i) } else { None }
+}
+
 fn width_and_height(r: i64, b: i64) -> (i64, i64) {
-    let d = (r + 2).pow(2) - 16 * r - 16 * b;
-    let h = ((r + 2) + d.isqrt()) / 4;
+    let d = (r + 4).pow(2) - 16 * r - 16 * b;
+    let h = ((r + 4) + isqrt(d).unwrap()) / 4;
     let w = (r - 2 * h + 4) / 2;
     (w.max(h), w.min(h))
 }
