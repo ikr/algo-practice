439b4f2c3946cb4367bfb72e0d5cb9d92d6ceb87
Ivan Krechetov <ikr@ikr.li>
2025-12-21 16:41:31 +0100

    Solve part 1

--- flipflop/2025/03_Bush_Salesman/src/main.rs
+++ flipflop/2025/03_Bush_Salesman/src/main.rs
@@ -1,22 +1,9 @@
 use itertools::Itertools;
 use std::io::{BufRead, stdin};
 
-fn decode_line(s: &str) -> (u8, u8, u8) {
-    s.split(',')
-        .map(|sub| sub.parse().unwrap())
-        .collect_tuple()
-        .unwrap()
-}
-
 fn main() {
     let lines: Vec<String> = stdin().lock().lines().map(|line| line.unwrap()).collect();
-
-    let fqs: Vec<_> = lines
-        .into_iter()
-        .map(|s| decode_line(&s))
-        .counts()
-        .into_iter()
-        .collect();
-
-    let result = fqs.into_iter().max_by_key(|(_, f)| f).unwrap().0;
+    let fqs: Vec<_> = lines.into_iter().counts().into_iter().collect();
+    let result = fqs.into_iter().max_by_key(|(_, f)| *f).unwrap().0;
+    println!("{result}");
 }
