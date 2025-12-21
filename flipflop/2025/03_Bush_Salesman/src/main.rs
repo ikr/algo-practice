dd0359f7770b1fb69fe08d2af1dc6a90028992a7
Ivan Krechetov <ikr@ikr.li>
2025-12-21 17:03:02 +0100

    Solve part 2

--- flipflop/2025/03_Bush_Salesman/src/main.rs
+++ flipflop/2025/03_Bush_Salesman/src/main.rs
@@ -1,9 +1,63 @@
 use itertools::Itertools;
 use std::io::{BufRead, stdin};
 
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum Label {
+    R,
+    G,
+    B,
+    Sp,
+}
+
+impl Label {
+    fn from_dominant_index(i: usize) -> Self {
+        match i {
+            0 => Self::R,
+            1 => Self::G,
+            2 => Self::B,
+            _ => unreachable!(),
+        }
+    }
+
+    fn values_with_other_indices(xs: [u8; 3], i0: usize) -> [u8; 2] {
+        xs.into_iter()
+            .enumerate()
+            .filter_map(|(i, x)| if i == i0 { None } else { Some(x) })
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap()
+    }
+
+    fn from_rgb(rgb: (u8, u8, u8)) -> Self {
+        let xs: [u8; 3] = rgb.into();
+
+        if xs.iter().tuple_combinations().any(|(a, b)| a == b) {
+            Self::Sp
+        } else {
+            Self::from_dominant_index(
+                xs.iter()
+                    .enumerate()
+                    .position(|(i, &x)| {
+                        let ys = Self::values_with_other_indices(xs, i);
+                        ys.into_iter().all(|y| y < x)
+                    })
+                    .unwrap(),
+            )
+        }
+    }
+}
+
+fn decode_line(s: &str) -> (u8, u8, u8) {
+    s.split(',')
+        .map(|sub| sub.parse().unwrap())
+        .collect_tuple()
+        .unwrap()
+}
+
 fn main() {
     let lines: Vec<String> = stdin().lock().lines().map(|line| line.unwrap()).collect();
-    let fqs: Vec<_> = lines.into_iter().counts().into_iter().collect();
-    let result = fqs.into_iter().max_by_key(|(_, f)| *f).unwrap().0;
+    let rgbs: Vec<_> = lines.into_iter().map(|s| decode_line(&s)).collect();
+    let labels: Vec<Label> = rgbs.into_iter().map(Label::from_rgb).collect();
+    let result = labels.into_iter().filter(|&l| l == Label::G).count();
     println!("{result}");
 }
