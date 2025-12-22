3e922b6d802831668221a1e40d215dc79ccb2535
Ivan Krechetov <ikr@ikr.li>
2025-12-22 06:57:26 +0100

    Solve part 1

--- flipflop/2025/05_Strange_tunnels/src/main.rs
+++ flipflop/2025/05_Strange_tunnels/src/main.rs
@@ -1,6 +1,12 @@
-use std::{collections::HashMap, io::Read};
+use std::{collections::HashMap, io::Read, iter::successors};
 
-fn tunnel_index(xs: &[char]) -> HashMap<char, Vec<usize>> {
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum Phase {
+    In,
+    Out,
+}
+
+fn tunnel_index(xs: Vec<char>) -> HashMap<char, Vec<usize>> {
     xs.iter()
         .enumerate()
         .fold(HashMap::new(), |mut acc, (i, &x)| {
@@ -8,9 +14,41 @@ fn tunnel_index(xs: &[char]) -> HashMap<char, Vec<usize>> {
             acc
         })
 }
+
 fn main() {
     let mut buf = String::new();
     std::io::stdin().read_to_string(&mut buf).unwrap();
+
     let xs: Vec<_> = buf.trim().chars().collect();
-    eprintln!("{:?}", tunnel_index(&xs));
+    let n = xs.len();
+    let idx = tunnel_index(xs);
+    let mut g: Vec<Vec<usize>> = vec![vec![]; n];
+
+    for (_, uv) in idx {
+        let [u, v] = uv.try_into().unwrap();
+        g[u].push(v);
+        g[v].push(u);
+    }
+
+    let trace: Vec<(usize, Phase)> = successors(Some((0, Phase::In)), |&(u, p)| match p {
+        Phase::In => Some((g[u][0], Phase::Out)),
+        Phase::Out => {
+            if u + 1 == n {
+                None
+            } else {
+                Some((u + 1, Phase::In))
+            }
+        }
+    })
+    .collect();
+
+    let result: usize = trace
+        .chunks(2)
+        .map(|ab| {
+            let [a, b] = ab.try_into().unwrap();
+            a.0.abs_diff(b.0)
+        })
+        .sum();
+
+    println!("{result}");
 }
