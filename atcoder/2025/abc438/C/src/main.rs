5c1d76bc515af2ae94d024f0641b4672936c2cee
Ivan Krechetov <ikr@ikr.li>
2025-12-27 13:58:40 +0100

    Working on C

---
+++ atcoder/2025/abc438/C/src/main.rs
@@ -0,0 +1,47 @@
+use proconio::input;
+use std::{
+    collections::HashSet,
+    io::{BufWriter, Write, stdout},
+};
+
+fn run_length_encoding(xs: Vec<u32>) -> Vec<(u32, usize)> {
+    xs.into_iter().fold(vec![], |mut acc, x| {
+        if let Some(e) = acc.last_mut()
+            && e.0 == x
+        {
+            e.1 += 1;
+        } else {
+            acc.push((x, 1))
+        }
+        acc
+    })
+}
+
+fn min_terminal_length(xs: Vec<u32>) -> usize {
+    let rle = run_length_encoding(xs).into_iter().filter_map(|(x, k)| {
+        if k.is_multiple_of(4) {
+            None
+        } else {
+            Some((x, k % 4))
+        }
+    });
+
+    let mut cuts: HashSet<usize> = HashSet::new();
+
+    todo!()
+}
+
+fn main() {
+    let stdout = stdout();
+    let handle = stdout.lock();
+    let mut writer = BufWriter::new(handle);
+
+    input! {
+        n: usize,
+        xs: [u32; n],
+    }
+
+    let result = min_terminal_length(xs);
+    writeln!(writer, "{result}").unwrap();
+    writer.flush().unwrap();
+}
