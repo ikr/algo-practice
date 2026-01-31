99c40183e1d8223ee881b73db288e72fa858bca9
Ivan Krechetov <ikr@ikr.li>
2026-01-31 15:24:14 +0100

    New idea

--- atcoder/2026/abc443/D/src/main.rs
+++ atcoder/2026/abc443/D/src/main.rs
@@ -1,72 +1,34 @@
+use itertools::Itertools;
 use proconio::input;
 use std::{
-    collections::{HashSet, VecDeque},
+    collections::BinaryHeap,
     io::{BufWriter, Write, stdout},
 };
 
-fn index_flood_fill_from_minimums(xs: &[u64]) -> Vec<usize> {
-    let n = xs.len();
-    let lo: u64 = *xs.iter().min().unwrap();
-    let mut q: VecDeque<usize> = (0..n).filter(|&i| xs[i] == lo).collect();
-    let mut result: Vec<_> = q.iter().cloned().collect();
-    let mut done: HashSet<usize> = q.iter().cloned().collect();
+fn is_satisfiable_in_k_ops(xs: &[u64], mut k: u64) -> bool {
+    let mut pq: BinaryHeap<(u64, usize)> = xs
+        .iter()
+        .enumerate()
+        .tuple_windows()
+        .map(|((i, &x), (_, &y))| (x.abs_diff(y), i))
+        .collect();
 
-    while let Some(i) = q.pop_front() {
-        if i != 0 && !done.contains(&(i - 1)) {
-            result.push(i - 1);
-            q.push_back(i - 1);
-            done.insert(i - 1);
-        }
-
-        if i != n - 1 && !done.contains(&(i + 1)) {
-            result.push(i + 1);
-            q.push_back(i + 1);
-            done.insert(i + 1);
-        }
-    }
-
-    assert_eq!(result.len(), n);
-    assert_eq!(done.len(), n);
-    result
-}
-
-fn is_satisfiable_in_k_ops(iq: &[usize], mut xs: Vec<u64>, mut k: u64) -> bool {
-    let n = xs.len();
-
-    for &i in iq {
-        let lhs: u64 = if i == 0 { xs[i] } else { xs[i - 1] };
-        let rhs: u64 = if i == n - 1 { xs[i] } else { xs[i + 1] };
-        let lo = lhs.min(xs[i]).min(rhs);
-
-        if xs[i] - lo > 1 {
-            let ops = xs[i] - lo - 1;
-
-            if ops > k {
-                return false;
-            }
-
-            xs[i] -= ops;
-            k -= ops;
-        }
-    }
-
-    true
+    todo!()
 }
 
 fn min_ops(xs: Vec<u64>) -> u64 {
-    let iq = index_flood_fill_from_minimums(&xs);
     let mut lo: u64 = 0;
 
-    if is_satisfiable_in_k_ops(&iq, xs.clone(), lo) {
+    if is_satisfiable_in_k_ops(&xs, lo) {
         return lo;
     }
 
     let mut hi: u64 = xs.len().pow(2) as u64;
-    assert!(is_satisfiable_in_k_ops(&iq, xs.clone(), hi));
+    assert!(is_satisfiable_in_k_ops(&xs, hi));
 
     while lo + 1 < hi {
         let mid = lo + (hi - lo) / 2;
-        if is_satisfiable_in_k_ops(&iq, xs.clone(), mid) {
+        if is_satisfiable_in_k_ops(&xs, mid) {
             hi = mid;
         } else {
             lo = mid;
