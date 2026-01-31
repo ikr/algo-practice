7ed36382b985de150f37c7341887269fc5a681bc
Ivan Krechetov <ikr@ikr.li>
2026-01-31 15:59:43 +0100

    TLE

--- atcoder/2026/abc443/D/src/main.rs
+++ atcoder/2026/abc443/D/src/main.rs
@@ -1,34 +1,69 @@
-use itertools::Itertools;
 use proconio::input;
 use std::{
-    collections::BinaryHeap,
+    collections::BTreeSet,
     io::{BufWriter, Write, stdout},
 };
 
-fn is_satisfiable_in_k_ops(xs: &[u64], mut k: u64) -> bool {
-    let mut pq: BinaryHeap<(u64, usize)> = xs
-        .iter()
-        .enumerate()
-        .tuple_windows()
-        .map(|((i, &x), (_, &y))| (x.abs_diff(y), i))
-        .collect();
+fn is_satisfiable_in_m_ops(mut xs: Vec<u64>, mut m: u64) -> bool {
+    let n = xs.len();
 
-    todo!()
+    let mut pq: BTreeSet<(u64, usize)> =
+        (0..n - 1).map(|i| (xs[i].abs_diff(xs[i + 1]), i)).collect();
+
+    while let Some((delta, j)) = pq.pop_last() {
+        if delta <= 1 {
+            return true;
+        }
+
+        if delta - 1 > m {
+            return false;
+        }
+
+        assert!(j < n - 1);
+        assert_eq!(xs[j].abs_diff(xs[j + 1]), delta);
+
+        if xs[j] < xs[j + 1] {
+            if j + 1 != n - 1 {
+                pq.remove(&(xs[j + 1].abs_diff(xs[j + 2]), j + 1));
+            }
+
+            xs[j + 1] -= delta - 1;
+
+            if j + 1 != n - 1 {
+                pq.insert((xs[j + 1].abs_diff(xs[j + 2]), j + 1));
+            }
+        } else {
+            if j != 0 {
+                pq.remove(&(xs[j - 1].abs_diff(xs[j]), j - 1));
+            }
+
+            xs[j] -= delta - 1;
+
+            if j != 0 {
+                pq.insert((xs[j - 1].abs_diff(xs[j]), j - 1));
+            }
+        }
+
+        pq.insert((xs[j].abs_diff(xs[j + 1]), j));
+        m -= delta - 1;
+    }
+
+    false
 }
 
 fn min_ops(xs: Vec<u64>) -> u64 {
     let mut lo: u64 = 0;
 
-    if is_satisfiable_in_k_ops(&xs, lo) {
+    if is_satisfiable_in_m_ops(xs.clone(), lo) {
         return lo;
     }
 
     let mut hi: u64 = xs.len().pow(2) as u64;
-    assert!(is_satisfiable_in_k_ops(&xs, hi));
+    assert!(is_satisfiable_in_m_ops(xs.clone(), hi));
 
     while lo + 1 < hi {
         let mid = lo + (hi - lo) / 2;
-        if is_satisfiable_in_k_ops(&xs, mid) {
+        if is_satisfiable_in_m_ops(xs.clone(), mid) {
             hi = mid;
         } else {
             lo = mid;
