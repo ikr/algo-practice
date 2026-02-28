2fc4dff6548f4aab177b2364339e849905fdd0d7
Ivan Krechetov <ikr@ikr.li>
2026-02-28 11:16:35 +0100

    AC

--- atcoder/2026/awc0015/D/src/main.rs
+++ atcoder/2026/awc0015/D/src/main.rs
@@ -1,24 +1,31 @@
 use itertools::Itertools;
 use proconio::input;
 use std::{
+    cmp::Reverse,
     collections::BTreeMap,
     io::{BufWriter, Write, stdout},
 };
 
 fn max_sales(c: u64, aa: Vec<u64>, bb: Vec<u64>) -> u64 {
-    let mut aa_fqs: BTreeMap<u64, usize> = aa.into_iter().counts().into_iter().collect();
+    let mut bb_fqs: BTreeMap<Reverse<u64>, usize> = bb
+        .into_iter()
+        .counts()
+        .into_iter()
+        .map(|(k, fq)| (Reverse(k), fq))
+        .collect();
+
     let mut result: u64 = 0;
 
-    for b in bb.into_iter().rev() {
-        if let Some((&a, &fq)) = aa_fqs.range(b..).next() {
+    for a in aa {
+        if let Some((&Reverse(b), &fq)) = bb_fqs.range(Reverse(a)..).next() {
             assert!(a >= b, "{a} isn't greater than {b}");
             assert_ne!(fq, 0);
             result += c;
 
             if fq == 1 {
-                aa_fqs.remove(&b);
+                bb_fqs.remove(&Reverse(b));
             } else {
-                aa_fqs.insert(b, fq - 1);
+                bb_fqs.insert(Reverse(b), fq - 1);
             }
         }
     }
