ee1372d940dfdd9bd4ca2f3e25ea4f229c4621f1
Ivan Krechetov <ikr@ikr.li>
2026-03-05 20:41:37 +0100

    Start with part 3

--- everybodycodes/2026/Melody_Made_of_Code/01_Scales_Bags_and_a_Bit_of_a_Mess/src/main.rs
+++ everybodycodes/2026/Melody_Made_of_Code/01_Scales_Bags_and_a_Bit_of_a_Mess/src/main.rs
@@ -1,8 +1,35 @@
 use itertools::Itertools;
-use std::{
-    cmp::Reverse,
-    io::{BufRead, stdin},
-};
+use std::io::{BufRead, stdin};
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum Kind {
+    Matte,
+    Shiny,
+}
+
+impl Kind {
+    fn from_shine(x: u8) -> Option<Self> {
+        if x <= 30 {
+            Some(Self::Matte)
+        } else if x >= 33 {
+            Some(Self::Shiny)
+        } else {
+            None
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct Group {
+    k: Kind,
+    c: char,
+}
+
+impl Group {
+    fn new(k: Kind, c: char) -> Self {
+        Self { k, c }
+    }
+}
 
 fn decode_component_bits(one_symbol: char, src: &str) -> u8 {
     src.chars()
@@ -32,12 +59,22 @@ impl Tri {
         decode_component_bits('S', &self.s)
     }
 
-    fn total_coloration(&self) -> u16 {
-        ['R', 'G', 'B']
-            .into_iter()
+    fn dominant_color(&self) -> Option<char> {
+        let cs = ['R', 'G', 'B'];
+
+        let xs: Vec<_> = cs
+            .iter()
             .zip([&self.r, &self.g, &self.b])
-            .map(|(one, s)| decode_component_bits(one, s) as u16)
-            .sum()
+            .map(|(one, s)| decode_component_bits(*one, s))
+            .collect();
+
+        let (i0, &hi) = xs.iter().enumerate().max_by_key(|(_, x)| **x).unwrap();
+        (xs.iter().filter(|&x| *x == hi).count() == 1).then_some(cs[i0])
+    }
+
+    fn group(&self) -> Option<Group> {
+        self.dominant_color()
+            .and_then(|dc| Kind::from_shine(self.shine()).map(|k| Group::new(k, dc)))
     }
 }
 
@@ -45,10 +82,10 @@ fn main() {
     let lines: Vec<String> = stdin().lock().lines().map(|line| line.unwrap()).collect();
     let tris: Vec<Tri> = lines.into_iter().map(Tri::parse).collect();
 
-    let hi: Tri = tris
+    let ts: Vec<_> = tris
         .into_iter()
-        .max_by_key(|tri| (tri.shine(), Reverse(tri.total_coloration())))
-        .unwrap();
+        .filter_map(|tri| tri.group().map(|g| (g, tri.id)))
+        .collect();
 
-    println!("{}", hi.id);
+    eprintln!("{:?}", gs);
 }
