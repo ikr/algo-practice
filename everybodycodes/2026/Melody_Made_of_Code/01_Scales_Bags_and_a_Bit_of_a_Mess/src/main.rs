dff3399960af2bbf75dbe7237874b760a7c680e2
Ivan Krechetov <ikr@ikr.li>
2026-03-05 14:49:39 +0100

    Solve part 2

--- everybodycodes/2026/Melody_Made_of_Code/01_Scales_Bags_and_a_Bit_of_a_Mess/src/main.rs
+++ everybodycodes/2026/Melody_Made_of_Code/01_Scales_Bags_and_a_Bit_of_a_Mess/src/main.rs
@@ -1,5 +1,8 @@
 use itertools::Itertools;
-use std::io::{BufRead, stdin};
+use std::{
+    cmp::Reverse,
+    io::{BufRead, stdin},
+};
 
 fn decode_component_bits(one_symbol: char, src: &str) -> u8 {
     src.chars()
@@ -10,29 +13,31 @@ fn decode_component_bits(one_symbol: char, src: &str) -> u8 {
 }
 
 struct Tri {
-    id: u16,
+    id: u32,
     r: String,
     g: String,
     b: String,
+    s: String,
 }
 
 impl Tri {
     fn parse(s: String) -> Self {
         let (a, b) = s.split(':').map(|x| x.to_string()).collect_tuple().unwrap();
-        let id: u16 = a.parse().unwrap();
-        let (r, g, b) = b.split(' ').map(|x| x.to_string()).collect_tuple().unwrap();
-        Self { id, r, g, b }
+        let id: u32 = a.parse().unwrap();
+        let (r, g, b, s) = b.split(' ').map(|x| x.to_string()).collect_tuple().unwrap();
+        Self { id, r, g, b, s }
     }
 
-    fn is_green_dominant(&self) -> bool {
-        let (br, bg, bb) = ['R', 'G', 'B']
+    fn shine(&self) -> u8 {
+        decode_component_bits('S', &self.s)
+    }
+
+    fn total_coloration(&self) -> u16 {
+        ['R', 'G', 'B']
             .into_iter()
             .zip([&self.r, &self.g, &self.b])
-            .map(|(one, s)| decode_component_bits(one, s))
-            .collect_tuple()
-            .unwrap();
-
-        bg > br && bg > bb
+            .map(|(one, s)| decode_component_bits(one, s) as u16)
+            .sum()
     }
 }
 
@@ -40,11 +45,10 @@ fn main() {
     let lines: Vec<String> = stdin().lock().lines().map(|line| line.unwrap()).collect();
     let tris: Vec<Tri> = lines.into_iter().map(Tri::parse).collect();
 
-    let gg: Vec<_> = tris
+    let hi: Tri = tris
         .into_iter()
-        .filter(|tri| tri.is_green_dominant())
-        .collect();
+        .max_by_key(|tri| (tri.shine(), Reverse(tri.total_coloration())))
+        .unwrap();
 
-    let result: u32 = gg.into_iter().map(|tri| tri.id as u32).sum();
-    println!("{result}");
+    println!("{}", hi.id);
 }
