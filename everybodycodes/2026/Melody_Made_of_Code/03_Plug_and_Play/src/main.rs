6842401d45b166992446763fbc04c8af272496c8
Ivan Krechetov <ikr@ikr.li>
2026-03-07 20:24:35 +0100

    Decode the input

--- everybodycodes/2026/Melody_Made_of_Code/03_Plug_and_Play/src/main.rs
+++ everybodycodes/2026/Melody_Made_of_Code/03_Plug_and_Play/src/main.rs
@@ -1,6 +1,37 @@
+use itertools::Itertools;
+use regex::Regex;
 use std::io::{BufRead, stdin};
+
+#[derive(Debug)]
+struct NodeSource {
+    plug: String,
+    left_socket: String,
+    right_socket: String,
+}
+
+impl NodeSource {
+    fn parse(s: String) -> Self {
+        let r = r"^id=(\d+), plug=([A-Z]+ [A-Z]+), leftSocket=([A-Z]+ [A-Z]+), rightSocket=([A-Z]+ [A-Z]+), data=.+$";
+        let re = Regex::new(r).unwrap();
+
+        let (_, _, plug, left_socket, right_socket) = re
+            .captures(&s)
+            .unwrap()
+            .iter()
+            .map(|a| a.unwrap().as_str().to_string())
+            .collect_tuple()
+            .unwrap();
+
+        Self {
+            plug,
+            left_socket,
+            right_socket,
+        }
+    }
+}
 
 fn main() {
     let lines: Vec<String> = stdin().lock().lines().map(|line| line.unwrap()).collect();
-    eprintln!("{:?}", lines);
+    let nss: Vec<NodeSource> = lines.into_iter().map(NodeSource::parse).collect();
+    eprintln!("{:?}", nss);
 }
