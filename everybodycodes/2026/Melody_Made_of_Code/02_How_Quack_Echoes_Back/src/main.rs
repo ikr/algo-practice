e33561fed209becd9b26192244c4a0cb2f33a2b2
Ivan Krechetov <ikr@ikr.li>
2026-03-07 10:11:19 +0100

    Implement `bound_flood_fill`

--- everybodycodes/2026/Melody_Made_of_Code/02_How_Quack_Echoes_Back/src/main.rs
+++ everybodycodes/2026/Melody_Made_of_Code/02_How_Quack_Echoes_Back/src/main.rs
@@ -3,6 +3,8 @@ use std::{
     io::{BufRead, stdin},
 };
 
+const FILL_LIMIT: usize = 3_000;
+
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 struct Crd(i32, i32);
 
@@ -48,6 +50,38 @@ fn crd_of(grid: &[Vec<char>], x: char) -> Option<Crd> {
     None
 }
 
+fn bound_flood_fill(
+    walls: &HashSet<Crd>,
+    mut covered: HashSet<Crd>,
+    u: Crd,
+) -> Option<HashSet<Crd>> {
+    assert!(covered.contains(&u));
+
+    if covered.len() > FILL_LIMIT {
+        return None;
+    }
+
+    let adjacent: Vec<Crd> = Dir::all()
+        .into_iter()
+        .filter_map(|dir| {
+            let v = u + dir.delta();
+            (!walls.contains(&v) && !covered.contains(&v)).then_some(v)
+        })
+        .collect();
+
+    for v in adjacent {
+        covered.insert(v);
+
+        if let Some(more_covered) = bound_flood_fill(walls, covered, v) {
+            covered = more_covered;
+        } else {
+            return None;
+        }
+    }
+
+    Some(covered)
+}
+
 fn main() {
     let grid: Vec<Vec<char>> = stdin()
         .lock()
@@ -78,4 +112,74 @@ fn main() {
     }
 
     println!("{}", coverage.len() - 1);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_bound_flood_fill_negative() {
+        assert_eq!(
+            bound_flood_fill(&HashSet::new(), HashSet::from([Crd(0, 0)]), Crd(0, 0)),
+            None
+        );
+    }
+
+    #[test]
+    fn test_bound_flood_fill_positive_a() {
+        let walls: HashSet<_> = [
+            (0, 0),
+            (0, 1),
+            (0, 2),
+            (1, 0),
+            (1, 2),
+            (2, 0),
+            (2, 1),
+            (2, 2),
+        ]
+        .into_iter()
+        .map(|(i, j)| Crd(i, j))
+        .collect();
+
+        let u = Crd(1, 1);
+        let covered = HashSet::from([u]);
+        assert_eq!(bound_flood_fill(&walls, covered.clone(), u), Some(covered));
+    }
+
+    #[test]
+    fn test_bound_flood_fill_positive_b() {
+        let walls: HashSet<_> = [
+            (0, 0),
+            (0, 1),
+            (0, 2),
+            (0, 3),
+            (1, 0),
+            (1, 3),
+            (2, 0),
+            (2, 3),
+            (3, 0),
+            (3, 3),
+            (4, 0),
+            (4, 1),
+            (4, 2),
+            (4, 3),
+        ]
+        .into_iter()
+        .map(|(i, j)| Crd(i, j))
+        .collect();
+
+        let u = Crd(1, 1);
+        assert_eq!(
+            bound_flood_fill(&walls, HashSet::from([u]), u),
+            Some(HashSet::from([
+                Crd(1, 1),
+                Crd(1, 2),
+                Crd(2, 1),
+                Crd(2, 2),
+                Crd(3, 1),
+                Crd(3, 2),
+            ]))
+        );
+    }
 }
