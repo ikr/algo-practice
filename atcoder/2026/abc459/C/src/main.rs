76a85c0d0204afa3c1e89bf41435e6fe84850799
Ivan Krechetov <ikr@ikr.li>
2026-05-25 13:14:10 +0200

    Resume

--- atcoder/2026/abc459/C/src/main.rs
+++ atcoder/2026/abc459/C/src/main.rs
@@ -1,5 +1,42 @@
+use ac_library::fenwicktree::FenwickTree;
 use proconio::{input, marker::Usize1};
 use std::io::{BufWriter, Write, stdout};
+
+const UB: usize = 300_001;
+
+struct DroppedFreqs {
+    xs: Vec<usize>,
+    drops: usize,
+    freqs: FenwickTree<i64>,
+}
+
+impl DroppedFreqs {
+    fn new(n: usize) -> Self {
+        let mut freqs = FenwickTree::new(UB, 0);
+        freqs.add(0, n as i64);
+
+        Self {
+            xs: vec![0; n],
+            drops: 0,
+            freqs,
+        }
+    }
+
+    fn inc_at(&mut self, i: usize) {
+        let cur = self.xs[i];
+        self.freqs.add(cur, -1);
+
+        self.xs[i] += 1;
+        self.freqs.add(cur + 1, 1);
+
+        todo!();
+    }
+
+    fn num_cells_at_least_k(&self, k: usize) -> i64 {
+        let level = (k + self.drops).min(UB);
+        self.freqs.sum(level..)
+    }
+}
 
 fn main() {
     let stdout = stdout();
@@ -12,8 +49,17 @@ fn main() {
         queries:[(u8, Usize1); q],
     }
 
-    for (t, i) in queries {}
+    let mut dfs = DroppedFreqs::new(n);
 
-    writeln!(writer, "{result}").unwrap();
+    for (t, x) in queries {
+        if t == 1 {
+            dfs.inc_at(x);
+        } else {
+            assert_eq!(t, 2);
+            let result = dfs.num_cells_at_least_k(x);
+            writeln!(writer, "{result}").unwrap();
+        }
+    }
+
     writer.flush().unwrap();
 }
