17ec4bb70fa986122b5057b322e731fcfb048562
Ivan Krechetov <ikr@ikr.li>
2026-06-05 10:26:24 +0200

    Clean up before resuming

--- leetcode/2026/3878_Count_Good_Subarrays/src/main.rs
+++ leetcode/2026/3878_Count_Good_Subarrays/src/main.rs
@@ -2,87 +2,6 @@ use std::io::stdin;
 
 use itertools::Itertools;
 
-// Source:
-// https://github.com/kenkoooo/competitive-programming-rs/blob/master/src/data_structure/segment_tree.rs
-//
-pub struct SegmentTree<T, Op> {
-    seg: Vec<Option<T>>,
-    n: usize,
-    op: Op,
-}
-
-impl<T, Op> SegmentTree<T, Op>
-where
-    T: Copy,
-    Op: Fn(T, T) -> T + Copy,
-{
-    pub fn new(size: usize, op: Op) -> SegmentTree<T, Op> {
-        let mut m = size.next_power_of_two();
-        if m == size {
-            m *= 2;
-        }
-        SegmentTree {
-            seg: vec![None; m * 2],
-            n: m,
-            op,
-        }
-    }
-
-    pub fn update(&mut self, k: usize, value: T) {
-        let mut k = k;
-        k += self.n - 1;
-        self.seg[k] = Some(value);
-        while k > 0 {
-            k = (k - 1) >> 1;
-            let left = self.seg[k * 2 + 1];
-            let right = self.seg[k * 2 + 2];
-            self.seg[k] = Self::op(left, right, self.op);
-        }
-    }
-
-    /// Get the result in the array of the range
-    pub fn query<R: std::ops::RangeBounds<usize>>(&self, range: R) -> Option<T> {
-        let start = match range.start_bound() {
-            std::ops::Bound::Included(t) => *t,
-            std::ops::Bound::Excluded(t) => *t + 1,
-            std::ops::Bound::Unbounded => 0,
-        };
-
-        let end = match range.end_bound() {
-            std::ops::Bound::Included(t) => *t + 1,
-            std::ops::Bound::Excluded(t) => *t,
-            std::ops::Bound::Unbounded => self.n,
-        };
-
-        self.query_range(start..end, 0, 0..self.n)
-    }
-
-    fn query_range(
-        &self,
-        range: std::ops::Range<usize>,
-        k: usize,
-        seg_range: std::ops::Range<usize>,
-    ) -> Option<T> {
-        if seg_range.end <= range.start || range.end <= seg_range.start {
-            None
-        } else if range.start <= seg_range.start && seg_range.end <= range.end {
-            self.seg[k]
-        } else {
-            let mid = (seg_range.start + seg_range.end) >> 1;
-            let x = self.query_range(range.clone(), k * 2 + 1, seg_range.start..mid);
-            let y = self.query_range(range, k * 2 + 2, mid..seg_range.end);
-            Self::op(x, y, self.op)
-        }
-    }
-
-    fn op(a: Option<T>, b: Option<T>, f: Op) -> Option<T> {
-        match (a, b) {
-            (Some(a), Some(b)) => Some(f(a, b)),
-            _ => a.or(b),
-        }
-    }
-}
-
 /// NEW PLAN (no code changes towards it just yet!)
 ///
 /// Suppose xs has n elements. Let's move the "current" index i from 0 to `n - 1`. We'll count all
@@ -104,24 +23,7 @@ where
 /// present as a key in the x -> Rx map, and Lj <= R{Vj}; Mj = min(L{j+1}, R{Vj}) - Lj.
 ///
 fn count_good_subarrays_impl(xs: Vec<i32>) -> usize {
-    let n = xs.len();
-    let ii: Vec<usize> = (0..n).collect();
-    let mut seg = SegmentTree::new(n, |a: i32, b: i32| a | b);
-
-    for (i, &x) in xs.iter().enumerate() {
-        seg.update(i, x);
-    }
-
-    xs.into_iter()
-        .enumerate()
-        .map(|(i0, x0)| -> usize {
-            let l = ii[0..=i0].partition_point(|&i| seg.query(i..=i0).unwrap() != x0);
-            let r = ii[i0..n].partition_point(|&i| seg.query(i0..=i).unwrap() == x0);
-            let sub = (i0 - l + 1) * r;
-            println!("i0:{i0} l:{l} r:{r} sub:{sub}");
-            sub
-        })
-        .sum()
+    todo!()
 }
 
 impl Solution {
