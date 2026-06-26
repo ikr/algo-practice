afd5c5faedfa050476274303fd16b97af646849e
Ivan Krechetov <ikr@ikr.li>
2026-06-26 06:59:11 +0200

    Bin-search

--- atcoder/2026/abc463/D/src/main.rs
+++ atcoder/2026/abc463/D/src/main.rs
@@ -4,8 +4,29 @@ use std::{
     u32,
 };
 
-fn lowest_k_gap(lrs: Vec<(u32, u32)>, k: usize) -> Option<u32> {
+fn is_gap_g_possible(lrs: &[(u32, u32)], k: usize, g: u32) -> bool {
     todo!()
+}
+
+fn lowest_k_gap(lrs: Vec<(u32, u32)>, k: usize) -> Option<u32> {
+    let mut lo: u32 = 1;
+    if !is_gap_g_possible(&lrs, k, lo) {
+        return None;
+    }
+
+    let mut hi: u32 = 1_000_000_007;
+    assert!(!is_gap_g_possible(&lrs, k, hi));
+
+    while lo + 1 != hi {
+        let mid = lo + (hi - lo) / 2;
+        if is_gap_g_possible(&lrs, k, mid) {
+            lo = mid;
+        } else {
+            hi = mid;
+        }
+    }
+
+    Some(lo)
 }
 
 fn main() {
@@ -16,8 +37,10 @@ fn main() {
     input! {
         n: usize,
         k: usize,
-        lrs: [(u32, u32); n],
+        mut lrs: [(u32, u32); n],
     }
+
+    lrs.sort_by_key(|&(_, r)| r);
 
     let result = match lowest_k_gap(lrs, k) {
         Some(a) => a.to_string(),
