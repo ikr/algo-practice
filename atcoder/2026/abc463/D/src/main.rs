87f2beade53e13ffaaf71deedd6eac00ba0e9698
Ivan Krechetov <ikr@ikr.li>
2026-06-26 07:26:54 +0200

    AC

--- atcoder/2026/abc463/D/src/main.rs
+++ atcoder/2026/abc463/D/src/main.rs
@@ -1,25 +1,29 @@
 use proconio::input;
-use std::{
-    io::{BufWriter, Write, stdout},
-    u32,
-};
+use std::io::{BufWriter, Write, stdout};
 
-fn is_gap_g_possible(lrs: &[(u32, u32)], k: usize, g: u32) -> bool {
-    todo!()
+fn is_k_gap_g_possible(lrs: &[(u32, u32)], k: usize, g: u32) -> bool {
+    let (_, sz) = lrs
+        .iter()
+        .skip(1)
+        .fold((lrs[0].1, 1), |(edge, n), &(l, r)| {
+            if edge + g <= l { (r, n + 1) } else { (edge, n) }
+        });
+
+    sz >= k
 }
 
-fn lowest_k_gap(lrs: Vec<(u32, u32)>, k: usize) -> Option<u32> {
+fn lowest_k_gap(lrs: &[(u32, u32)], k: usize) -> Option<u32> {
     let mut lo: u32 = 1;
-    if !is_gap_g_possible(&lrs, k, lo) {
+    if !is_k_gap_g_possible(lrs, k, lo) {
         return None;
     }
 
     let mut hi: u32 = 1_000_000_007;
-    assert!(!is_gap_g_possible(&lrs, k, hi));
+    assert!(!is_k_gap_g_possible(lrs, k, hi));
 
     while lo + 1 != hi {
         let mid = lo + (hi - lo) / 2;
-        if is_gap_g_possible(&lrs, k, mid) {
+        if is_k_gap_g_possible(lrs, k, mid) {
             lo = mid;
         } else {
             hi = mid;
@@ -42,7 +46,7 @@ fn main() {
 
     lrs.sort_by_key(|&(_, r)| r);
 
-    let result = match lowest_k_gap(lrs, k) {
+    let result = match lowest_k_gap(&lrs, k) {
         Some(a) => a.to_string(),
         None => "-1".to_string(),
     };
