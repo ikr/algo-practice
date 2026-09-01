ba02e35e9d24ddaa267b880bdf449163dad3d2d9
Ivan Krechetov <ikr@ikr.li>
2026-09-01 15:26:01 +0200

    wip

--- everybodycodes/2026/The_Digital_Atelier/01_The_Recamán_Drapery/src/main.rs
+++ everybodycodes/2026/The_Digital_Atelier/01_The_Recamán_Drapery/src/main.rs
@@ -1,6 +1,14 @@
+#![warn(clippy::all)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::nursery)]
+#![warn(clippy::cargo)]
+
 use std::{
+    collections::HashSet,
     fmt::Debug,
+    hash::Hash,
     io::{self, BufRead},
+    iter::successors,
     str::FromStr,
 };
 
@@ -9,7 +17,7 @@ where
     T: FromStr,
     T::Err: Debug,
 {
-    src.parse::<T>().expect(&format!("Can't parse {}", src))
+    src.parse::<T>().unwrap()
 }
 
 fn decode_csv<T>(src: &str) -> Vec<T>
@@ -20,14 +28,62 @@ where
     src.split(',').map(|a| decode(a)).collect()
 }
 
+fn split_last<T>(mut xs: Vec<T>) -> Option<(T, Vec<T>)> {
+    xs.pop().map(|x| (x, xs))
+}
+
+fn with<T: Eq + Hash>(mut xs: HashSet<T>, x: T) -> HashSet<T> {
+    xs.insert(x);
+    xs
+}
+
+type Int = u16;
+
+struct State {
+    visited: HashSet<Int>,
+    jump_lengths_stack: Vec<Int>,
+    current: Int,
+}
+
+impl State {
+    fn new(mut jump_lengths: Vec<Int>) -> Self {
+        jump_lengths.reverse();
+        Self {
+            visited: HashSet::new(),
+            jump_lengths_stack: jump_lengths,
+            current: 0,
+        }
+    }
+
+    fn next(self) -> Option<Self> {
+        split_last(self.jump_lengths_stack).map(|(delta, jump_lengths_stack)| {
+            let current = if self.current > delta && !self.visited.contains(&(self.current - delta))
+            {
+                self.current - delta
+            } else {
+                self.current + delta
+            };
+
+            let visited = with(self.visited, current);
+            Self {
+                visited,
+                jump_lengths_stack,
+                current,
+            }
+        })
+    }
+}
+
+fn simulate_return_final_point(jump_lengths: Vec<Int>) -> Int {
+    let mut s = State::new(jump_lengths);
+    while let Some(t) = s.next() {
+        s = t
+    }
+    s.current
+}
+
 fn main() {
-    let lines: Vec<String> = io::stdin()
-        .lock()
-        .lines()
-        .map(|x| x.expect("stdin line read"))
-        .collect();
-
+    let lines: Vec<String> = io::stdin().lock().lines().map(|x| x.unwrap()).collect();
     let xss: Vec<Vec<u16>> = lines.into_iter().map(|line| decode_csv(&line)).collect();
-
-    eprintln!("{:?}", xss);
+    eprintln!("{xss:?}");
 }
