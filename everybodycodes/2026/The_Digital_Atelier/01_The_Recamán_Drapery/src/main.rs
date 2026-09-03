4eaf7f4155b65d2f5e2d3ea7268ee699a1217421
Ivan Krechetov <ikr@ikr.li>
2026-09-03 14:58:51 +0200

    1st draft for p3

--- everybodycodes/2026/The_Digital_Atelier/01_The_Recamán_Drapery/src/main.rs
+++ everybodycodes/2026/The_Digital_Atelier/01_The_Recamán_Drapery/src/main.rs
@@ -1,9 +1,10 @@
 use std::{
-    collections::HashSet,
     fmt::Debug,
     io::{self, BufRead},
     str::FromStr,
 };
+
+const LIM: usize = 10_000;
 
 fn decode<T>(src: &str) -> T
 where
@@ -22,39 +23,53 @@ where
 }
 
 struct State {
-    visited: HashSet<u32>,
-    jump_lengths_stack: Vec<u32>,
-    current: u32,
+    visited: [[bool; LIM]; 2],
+    jump_lengths_stack: Vec<usize>,
+    phase: usize,
+    current: usize,
 }
 
 impl State {
-    fn new(mut jump_lengths: Vec<u32>) -> Self {
+    fn new(mut jump_lengths: Vec<usize>) -> Self {
         jump_lengths.reverse();
         Self {
-            visited: HashSet::new(),
+            visited: {
+                let mut row0 = [false; LIM];
+                row0[0] = true;
+                [row0, [false; LIM]]
+            },
             jump_lengths_stack: jump_lengths,
+            phase: 0,
             current: 0,
         }
     }
 
     fn next(&mut self) {
         if let Some(delta) = self.jump_lengths_stack.pop() {
-            if self.current > delta && !self.visited.contains(&(self.current - delta)) {
+            self.phase = (self.phase + 1) % 2;
+            let previous = self.current;
+
+            if self.current > delta && !self.visited[self.phase][self.current - delta] {
                 self.current -= delta;
             } else {
                 self.current += delta;
 
-                while self.visited.contains(&(self.current)) {
+                while self.visited[self.phase][self.current] {
                     self.current += 1;
                 }
             }
 
-            self.visited.insert(self.current);
+            assert_ne!(previous, self.current);
+            let (lo, hi) = (previous.min(self.current), previous.max(self.current));
+
+            for i in lo..=hi {
+                self.visited[self.phase][i] = true;
+            }
         }
     }
 }
 
-fn simulate_return_final_point(jump_lengths: Vec<u32>) -> u32 {
+fn simulate_return_final_point(jump_lengths: Vec<usize>) -> usize {
     let mut s = State::new(jump_lengths);
     while !s.jump_lengths_stack.is_empty() {
         s.next();
@@ -64,7 +79,8 @@ fn simulate_return_final_point(jump_lengths: Vec<u32>) -> u32 {
 
 fn main() {
     let lines: Vec<String> = io::stdin().lock().lines().map(|x| x.unwrap()).collect();
-    let xss: Vec<Vec<u32>> = lines.into_iter().map(|line| decode_csv(&line)).collect();
-    let rs: Vec<u32> = xss.into_iter().map(simulate_return_final_point).collect();
-    println!("{}", rs.into_iter().sum::<u32>());
+    let xss: Vec<Vec<usize>> = lines.into_iter().map(|line| decode_csv(&line)).collect();
+    let rs: Vec<usize> = xss.into_iter().map(simulate_return_final_point).collect();
+    eprintln!("{rs:?}");
+    println!("{}", rs.into_iter().sum::<usize>());
 }
