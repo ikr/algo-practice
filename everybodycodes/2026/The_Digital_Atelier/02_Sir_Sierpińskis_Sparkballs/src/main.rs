04daf746982598462744c9c55ea0081e3e8ec71b
Ivan Krechetov <ikr@ikr.li>
2026-09-07 13:50:33 +0200

    Solve part 3

--- everybodycodes/2026/The_Digital_Atelier/02_Sir_Sierpińskis_Sparkballs/src/main.rs
+++ everybodycodes/2026/The_Digital_Atelier/02_Sir_Sierpińskis_Sparkballs/src/main.rs
@@ -28,6 +28,29 @@ impl Crd {
     }
 }
 
+struct BeetlesDfs {
+    beacons: Vec<Crd>,
+    visited: HashSet<Crd>,
+}
+
+impl BeetlesDfs {
+    fn new(beacons: Vec<Crd>) -> Self {
+        Self {
+            beacons,
+            visited: HashSet::new(),
+        }
+    }
+
+    fn recur(&mut self, p: Crd) {
+        self.visited.insert(p);
+        for q in p.adjacent_wrt(&self.beacons) {
+            if !self.visited.contains(&q) {
+                self.recur(q);
+            }
+        }
+    }
+}
+
 fn decode_input_line_crd(line: &str) -> Crd {
     let i = line.find('[').unwrap();
     let j = line.find(']').unwrap();
@@ -55,12 +78,14 @@ fn main() {
         .map(|line| decode_input_line_crd(line))
         .collect();
 
-    let gen0: HashSet<Crd> = todo!();
+    let mut dfs = BeetlesDfs::new(beacons.clone());
+    dfs.recur(start);
 
-    let gen1: HashSet<Crd> = gen0
+    let fireflies: HashSet<Crd> = dfs
+        .visited
         .iter()
-        .flat_map(|p| p.neighs().into_iter().filter(|q| !gen0.contains(q)))
+        .flat_map(|p| p.neighs().into_iter().filter(|q| !dfs.visited.contains(q)))
         .collect();
 
-    println!("{}", gen1.len());
+    println!("{}", fireflies.len());
 }
