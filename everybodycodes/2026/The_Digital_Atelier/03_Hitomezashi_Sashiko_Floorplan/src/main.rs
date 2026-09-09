3270d7ca69db125313e40268a992583a8ad58893
Ivan Krechetov <ikr@ikr.li>
2026-09-09 07:39:48 +0200

    Refine

--- everybodycodes/2026/The_Digital_Atelier/03_Hitomezashi_Sashiko_Floorplan/src/main.rs
+++ everybodycodes/2026/The_Digital_Atelier/03_Hitomezashi_Sashiko_Floorplan/src/main.rs
@@ -1,15 +1,40 @@
-use std::io::{BufRead, stdin};
+use std::{
+    collections::HashSet,
+    io::{BufRead, stdin},
+};
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+enum Dir {
+    N,
+    E,
+    S,
+    W,
+}
 
 fn decode_line_int(prefix: &str, line: String) -> usize {
     line.strip_prefix(prefix).unwrap().parse().unwrap()
 }
 
-fn decode_line_flags(prefix: &str, line: String) -> Vec<bool> {
+fn decode_line_flags(prefix: &str, line: String) -> Vec<usize> {
     line.strip_prefix(prefix)
         .unwrap()
         .bytes()
-        .map(|b| b == b'1')
+        .map(|b| (b - b'0') as usize)
         .collect()
+}
+
+struct Grid {
+    width: usize,
+    height: usize,
+    row_offsets: Vec<usize>,
+    column_offsets: Vec<usize>,
+}
+
+impl Grid {
+    fn cell_borders(&self, row: usize, column: usize) -> HashSet<Dir> {
+        assert!(row < self.height && column < self.width);
+        todo!()
+    }
 }
 
 fn main() {
