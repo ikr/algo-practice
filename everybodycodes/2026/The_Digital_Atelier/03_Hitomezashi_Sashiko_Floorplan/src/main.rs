dcd610d8fb3a32aaf587da22d444afe320b44284
Ivan Krechetov <ikr@ikr.li>
2026-09-11 07:22:37 +0200

    Introduce Crd-s with signed coordinates

--- everybodycodes/2026/The_Digital_Atelier/03_Hitomezashi_Sashiko_Floorplan/src/main.rs
+++ everybodycodes/2026/The_Digital_Atelier/03_Hitomezashi_Sashiko_Floorplan/src/main.rs
@@ -14,6 +14,33 @@ fn decode_line_flags(prefix: &str, line: String) -> Vec<usize> {
         .collect()
 }
 
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+struct Crd(i16, i16);
+
+impl std::ops::Add<Crd> for Crd {
+    type Output = Crd;
+
+    fn add(self, o: Crd) -> Crd {
+        Crd(self.0 + o.0, self.1 + o.1)
+    }
+}
+
+impl Crd {
+    fn from_indices(row: usize, column: usize) -> Self {
+        Self(row as i16, column as i16)
+    }
+
+    fn to_indices(self) -> (usize, usize) {
+        (self.0 as usize, self.1 as usize)
+    }
+
+    fn flat_index(self, grid_width: usize) -> usize {
+        let ro = self.0 as usize;
+        let co = self.1 as usize;
+        grid_width * ro + co
+    }
+}
+
 #[derive(Clone, Copy, Debug)]
 enum Dir {
     N,
@@ -26,6 +53,15 @@ impl Dir {
     fn all() -> [Dir; 4] {
         [Self::N, Self::E, Self::S, Self::W]
     }
+
+    fn delta(&self) -> Crd {
+        match self {
+            Dir::N => Crd(-1, 0),
+            Dir::E => Crd(0, 1),
+            Dir::S => Crd(1, 0),
+            Dir::W => Crd(0, -1),
+        }
+    }
 }
 
 struct Grid {
@@ -34,15 +70,17 @@ struct Grid {
 }
 
 impl Grid {
-    fn has_border(&self, row: usize, column: usize, dir: Dir) -> bool {
+    fn has_border(&self, crd: Crd, dir: Dir) -> bool {
         match dir {
             Dir::N => {
+                let (row, column) = crd.to_indices();
                 let rm = self.row_offsets.len();
                 column % 2 == self.row_offsets[row % rm]
             }
-            Dir::E => self.has_border(row, column + 1, Dir::W),
-            Dir::S => self.has_border(row + 1, column, Dir::N),
+            Dir::E => self.has_border(crd + Dir::E.delta(), Dir::W),
+            Dir::S => self.has_border(crd + Dir::S.delta(), Dir::N),
             Dir::W => {
+                let (row, column) = crd.to_indices();
                 let cm = self.column_offsets.len();
                 row % 2 == self.column_offsets[column % cm]
             }
@@ -72,7 +110,11 @@ fn main() {
 
     let result = (0..height)
         .cartesian_product(0..width)
-        .filter(|&(ro, co)| Dir::all().into_iter().all(|dir| g.has_border(ro, co, dir)))
+        .filter(|&(ro, co)| {
+            Dir::all()
+                .into_iter()
+                .all(|dir| g.has_border(Crd::from_indices(ro, co), dir))
+        })
         .count();
 
     println!("{result}");
