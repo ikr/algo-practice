a376a3df0a00d2398af472b42ceab3a612a85469
Ivan Krechetov <ikr@ikr.li>
2026-09-12 15:40:03 +0200

    Start with D

---
+++ atcoder/2026/abc475/D/solution.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T> constexpr int inof(const T x) {
+    return static_cast<int>(x);
+}
+
+template <typename T> constexpr int sz(const T &xs) { return inof(xs.size()); }
+
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &xs) {
+    os << '[';
+    for (auto i = xs.cbegin(); i != xs.cend(); ++i) {
+        if (i != xs.cbegin()) os << ' ';
+        os << *i;
+    }
+    os << ']';
+    return os;
+}
+
+using vi = vector<int>;
+using pii = pair<int, int>;
+
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+
+constexpr int LIM = 1e2;
+constexpr int S =
+    static_cast<int>(std::round(std::sqrt(static_cast<double>(LIM))));
+
+constexpr int R = LIM / 2;
+
+bitset<LIM> isPrime;
+
+vi eratosthenes() {
+    vi pr = {2}, sieve(S + 1);
+    pr.reserve(int(LIM / log(LIM) * 1.1));
+    vector<pii> cp;
+    for (int i = 3; i <= S; i += 2)
+        if (!sieve[i]) {
+            cp.push_back({i, i * i / 2});
+            for (int j = i * i; j <= S; j += 2 * i) sieve[j] = 1;
+        }
+    for (int L = 1; L <= R; L += S) {
+        array<bool, S> block{};
+        for (auto &[p, idx] : cp)
+            for (int i = idx; i < S + L; idx = (i += p)) block[i - L] = 1;
+        rep(i, 0, min(S, R - L)) if (!block[i]) pr.push_back((L + i) * 2 + 1);
+    }
+    for (int i : pr) isPrime[i] = 1;
+    return pr;
+}
+
+template <typename T> vector<int> digits_reversed(T n) {
+    vector<int> ans;
+
+    while (n) {
+        ans.push_back(inof(n % static_cast<T>(10)));
+        n /= static_cast<T>(10);
+    }
+
+    return ans;
+}
+
+template <typename T> vector<int> digits(T n) {
+    auto ans = digits_reversed(n);
+    reverse(begin(ans), end(ans));
+    return ans;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+
+    string s;
+    cin >> s;
+
+    const auto primes = eratosthenes();
+    cerr << primes << '\n';
+
+    vector<vector<int>> idx(26, vector<int>{});
+    for (int i = 0; i != sz(s); ++i) {
+        idx[s[i] - 'a'].push_back(i);
+    }
+
+    vector<vector<int>> g(sz(s), vector<int>{});
+    for (int i = 0; i != sz(idx); ++i) {
+    }
+
+    cerr << idx << '\n';
+
+    return 0;
+}
